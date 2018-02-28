d04c765cf2ae7edf50d31b10d1bd41b1a5fdb1dc
David Mohs <dgit@mohsinc.com>
2018-02-27 20:09:02 -0500

    Catch forgotten `npm install`

--- libproject/devcycle.rb
+++ libproject/devcycle.rb
@@ -1,6 +1,10 @@
 require_relative "common/common"
 
 def dev_up(*args)
+  unless Dir.exist?("node_modules")
+    Common.new.error "Missing node_modules. Run `npm install` to install project dependencies."
+    exit 1
+  end
   Common.new.run_inline_swallowing_interrupt %W{npm start}
 end
 
