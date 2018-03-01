c00bfd023bbcb1ce042ac9062d32c3f72f7d5b33
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-01 13:33:27 -0500

    indication when loading

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,50 +11,54 @@ class WorkspaceList extends Component {
     super(props)
     this.state = {
       pageIndex: 1,
-      itemsPerPage: 25,
-      workspaces: []
+      itemsPerPage: 25
     }
   }
 
   componentWillMount = () => {
-    ajax('https://rawls.dsde-dev.broadinstitute.org/api/workspaces').then(json =>
-      this.setState({ workspaces: json })
-    )
+    ajax('https://rawls.dsde-dev.broadinstitute.org/api/workspaces')
+      .then(json =>
+        this.setState({ workspaces: json })
+      )
   }
 
   render() {
-    return h(Fragment, [
-      h(Table,
-        {
-          data: this.state.workspaces.slice((this.state.pageIndex - 1) * this.state.itemsPerPage,
-            this.state.pageIndex * this.state.itemsPerPage),
-          rowKey: 'workspace.workspaceId',
-          columns: [
-            {
-              title: 'Workspace', dataIndex: 'workspace', key: 'workspace',
-              render: ({ namespace, name }) => `${namespace}/${name}`
-            }
-          ]
-        }
-      ),
-      div({ style: { marginTop: 10 } }, [
-        'Page: ',
-        select({
-            style: { marginRight: '1rem' },
-            onChange: e => this.setState({ pageIndex: parseInt(e.target.value, 10) }),
-            value: this.state.pageIndex
-          },
-          _.map(_.range(1, this.state.workspaces.length / this.state.itemsPerPage + 1),
-            i => option({}, i))),
-        'Items per page: ',
-        select({
-            onChange: e => this.setState({ itemsPerPage: parseInt(e.target.value, 10) }),
-            value: this.state.itemsPerPage
-          },
-          _.map([10, 25, 50, 100],
-            i => option({}, i)))
+    if (this.state.workspaces) {
+      return h(Fragment, [
+        h(Table,
+          {
+            data: this.state.workspaces.slice((this.state.pageIndex - 1) * this.state.itemsPerPage,
+              this.state.pageIndex * this.state.itemsPerPage),
+            rowKey: 'workspace.workspaceId',
+            columns: [
+              {
+                title: 'Workspace', dataIndex: 'workspace', key: 'workspace',
+                render: ({ namespace, name }) => `${namespace}/${name}`
+              }
+            ]
+          }
+        ),
+        div({ style: { marginTop: 10 } }, [
+          'Page: ',
+          select({
+              style: { marginRight: '1rem' },
+              onChange: e => this.setState({ pageIndex: parseInt(e.target.value, 10) }),
+              value: this.state.pageIndex
+            },
+            _.map(_.range(1, this.state.workspaces.length / this.state.itemsPerPage + 1),
+              i => option({}, i))),
+          'Items per page: ',
+          select({
+              onChange: e => this.setState({ itemsPerPage: parseInt(e.target.value, 10) }),
+              value: this.state.itemsPerPage
+            },
+            _.map([10, 25, 50, 100],
+              i => option({}, i)))
+        ])
       ])
-    ])
+    } else {
+      return 'Loading!'
+    }
   }
 }
 
