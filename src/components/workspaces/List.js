46f9bcb6b23c1c3c71c36abb1c9d5564c8969c02
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-05 18:25:15 -0500

    create shared DataViewer for table/card view

--- src/components/workspaces/List.js
+++ src/components/workspaces/List.js
@@ -1,10 +1,8 @@
-import update from 'immutability-helper'
-import Table from 'rc-table'
-import { Component, Fragment } from 'react'
-import { a, button, div, h, input, option, select } from 'react-hyperscript-helpers'
-import _ from 'underscore'
+import { Component } from 'react'
+import { a, h } from 'react-hyperscript-helpers'
 import * as Ajax from '../../ajax'
 import * as Nav from '../../nav'
+import DataViewer from '../DataViewer'
 
 
 class WorkspaceList extends Component {
@@ -19,7 +17,7 @@ class WorkspaceList extends Component {
     }
   }
 
-  componentWillMount = () => {
+  componentWillMount() {
     Ajax.rawls('workspaces').then(json =>
       this.setState({ workspaces: json })
     )
@@ -27,90 +25,36 @@ class WorkspaceList extends Component {
 
   render() {
     if (this.state.workspaces) {
-      const filteredWorkspaces = this.state.workspaces.filter(
-        ({ workspace: { namespace, name } }) => `${namespace}/${name}`.includes(this.state.filter))
-      const listPage = filteredWorkspaces.slice((this.state.pageIndex - 1) *
-        this.state.itemsPerPage,
-        this.state.pageIndex * this.state.itemsPerPage)
+      return DataViewer({
+        filterFunction: ({ workspace: { namespace, name } }, filterString) =>
+          `${namespace}/${name}`.includes(filterString),
+        dataSource: this.state.workspaces,
+        allowViewToggle: true,
+        tableProps: {
+          rowKey: ({ workspace }) => workspace.workspaceId,
+          columns: [
+            {
+              title: 'Workspace', dataIndex: 'workspace', key: 'workspace',
+              render: ({ namespace, name }) =>
+                a({ href: Nav.getLink('workspace', namespace, name) },
+                  `${namespace}/${name}`)
 
-      const renderWorkspaceList = () => {
-        return h(Table,
-          {
-            data: listPage,
-            rowKey: ({ workspace }) => workspace.workspaceId,
-            columns: [
-              {
-                title: 'Workspace', dataIndex: 'workspace', key: 'workspace',
-                render: ({ namespace, name }) => {
-                  return a({ href: Nav.getLink('workspace', namespace, name) },
-                    `${namespace}/${name}`)
-                }
-              }
-            ]
-          }
-        )
-      }
-
-      const renderWorkspaceCards = () => {
-        return h(Fragment,
-          [
-            button(
-              {
-                onClick: () => this.setState(
-                  prev => (update(prev, { cardWidth: { $apply: n => _.max([n - 1, 1]) } })))
-              },
-              '+'),
-            button(
-              {
-                onClick: () => this.setState(
-                  prev => (update(prev, { cardWidth: { $apply: n => n + 1 } })))
-              },
-              '-'),
-            div({ style: { display: 'flex', flexWrap: 'wrap' } },
-              _.map(listPage, ({ workspace: { namespace, name } }) => {
-                return a({
-                  style: {
-                    display: 'block',
-                    height: 100,
-                    width: `${100 / this.state.cardWidth}%`,
-                    border: '1px solid black',
-                    boxSizing: 'border-box'
-                  },
-                  href: Nav.getLink('workspace', namespace, name)
-                }, `${namespace}/${name}`)
-              })
-            )
+            }
           ]
-        )
-      }
-
-      return h(Fragment, [
-        input({
-          placeholder: 'Filter',
-          onChange: e => this.setState({ filter: e.target.value })
-        }),
-        button({ onClick: () => this.setState(prev => (update(prev, { $toggle: ['listView'] }))) },
-          'Toggle display mode'),
-        this.state.listView ? renderWorkspaceList() : renderWorkspaceCards(),
-        div({ style: { marginTop: 10 } }, [
-          'Page: ',
-          select({
-              style: { marginRight: '1rem' },
-              onChange: e => this.setState({ pageIndex: parseInt(e.target.value, 10) }),
-              value: this.state.pageIndex
+        },
+        renderCard: ({ workspace: { namespace, name } }, cardsPerRow) => {
+          return a({
+            style: {
+              display: 'block',
+              height: 100,
+              width: `${100 / cardsPerRow}%`,
+              border: '1px solid black',
+              boxSizing: 'border-box'
             },
-            _.map(_.range(1, filteredWorkspaces.length / this.state.itemsPerPage + 1),
-              i => option({}, i))),
-          'Items per page: ',
-          select({
-              onChange: e => this.setState({ itemsPerPage: parseInt(e.target.value, 10) }),
-              value: this.state.itemsPerPage
-            },
-            _.map([10, 25, 50, 100],
-              i => option({}, i)))
-        ])
-      ])
-
+            href: Nav.getLink('workspace', namespace, name)
+          }, `${namespace}/${name}`)
+        }
+      })
     } else {
       return 'Loading!'
     }
