e97f396f7c9c75f7c774769615068df53330796b
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-08 13:28:57 -0500

    fix hot reloading

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import { h } from 'react-hyperscript-helpers'
 import App from 'src/components/App'
 
 
-const renderApp = () => {
-  ReactDOM.render(
-    h(AppContainer, {}, [App()]),
-    document.getElementById('root')
-  )
-}
-
-renderApp()
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/App', renderApp)
-}
+ReactDOM.render(h(App), document.getElementById('root'))
