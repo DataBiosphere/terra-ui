fbe5e1719b4526ff685268ac69820c9b28412e77
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-13 19:03:12 -0400

    almost have jest working

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,14 @@ const rewireReactHotLoader = require('react-app-rewire-hot-loader')
 const manualOverrides = require('./webpack.config')
 
 /* config-overrides.js */
-module.exports = function override(config, env) {
-  config = mixinDeep(config, manualOverrides)
-  return rewireReactHotLoader(config, env)
+module.exports = {
+  webpack: function override(config, env) {
+    config = mixinDeep(config, manualOverrides)
+    return rewireReactHotLoader(config, env)
+  },
+  jest: function(config) {
+    return mixinDeep(config, {
+      'moduleDirectories': ['node_modules', '']
+    })
+  }
 }
