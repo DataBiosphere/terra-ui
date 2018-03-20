3b56bd1ff493d89debde4a6702bcf171d97e5a28
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-20 16:45:51 -0400

    unused imports

--- src/pages/workspaces/workspace/Dashboard.js
+++ src/pages/workspaces/workspace/Dashboard.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
-import { Component, Fragment } from 'react'
-import { div, h, hh } from 'react-hyperscript-helpers/lib/index'
+import { Component } from 'react'
+import { div, hh } from 'react-hyperscript-helpers/lib/index'
 import * as Ajax from 'src/libs/ajax'
 import { spinner } from 'src/components/icons'
 import * as Style from 'src/libs/style'
@@ -28,8 +28,9 @@ export default hh(class WorkspaceDashboard extends Component {
     const { workspace } = this.state
 
     return _.isEmpty(workspace) ? spinner() :
-      div({style: {margin: '1rem'}}, [
-        div({style: {fontSize: 16, fontWeight: 500, color: Style.colors.title}}, 'ACCESS LEVEL'),
+      div({ style: { margin: '1rem' } }, [
+        div({ style: { fontSize: 16, fontWeight: 500, color: Style.colors.title } },
+          'ACCESS LEVEL'),
         workspace['accessLevel']
       ])
   }
