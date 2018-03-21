5842de373386ef638ee9efb8cb34ab38eb761cdc
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-21 14:16:34 -0400

    fix workspace entities display (#41)

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -27,7 +27,7 @@ export default hh(class WorkspaceData extends Component {
     const { namespace, name } = this.props
 
     Ajax.rawls(`workspaces/${namespace}/${name}/entities`).then(json =>
-      this.setState({ workspaceEntities: _.sortBy(json, 'name') })
+      this.setState({ workspaceEntities: json })
     )
   }
 
@@ -56,7 +56,7 @@ export default hh(class WorkspaceData extends Component {
     )
 
     const entityTable = DataTable({
-      dataSource: selectedEntities,
+      dataSource: _.sortBy(selectedEntities, 'name'),
       tableProps: {
         rowKey: 'name',
         scroll: { x: true },
