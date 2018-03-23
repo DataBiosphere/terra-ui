7cec566f6268aaff93bbb0336025c4d18955d65c
Isaac Zarsky <zarsky@broadinstitute.org>
2018-03-23 13:57:07 -0400

    mock on demand

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -3,12 +3,58 @@ import * as Config from 'src/libs/config'
 import * as Utils from 'src/libs/utils'
 
 
+let mockResponse
+let noConnection
+
+const consoleStyle = 'font-weight: bold; color: darkBlue'
+
+window.saturnMock = {
+  currently: function() {
+    if (noConnection || mockResponse) {
+      if (noConnection) {console.info('%cSimulating no connection', consoleStyle)}
+      if (mockResponse) {
+        console.info('%cSimulating response:', consoleStyle)
+        console.info(mockResponse())
+      }
+    } else {
+      console.info('%cNot mocking responses', consoleStyle)
+    }
+  },
+  malformed: function() {
+    mockResponse = () => new Response('{malformed', { status: 200 })
+  },
+  noConnection: function() {
+    noConnection = true
+  },
+  off: function() {
+    mockResponse = undefined
+    noConnection = undefined
+  },
+  status: function(code) {
+    mockResponse = () => new Response(new Blob([`Body of simulated ${code} response`]),
+      { status: code })
+  }
+}
+
 /**
  * @param {string} url
  * @param {object} [options]
  * @returns {Promise<Response>}
  */
 export const ajax = function(url, options = { headers: {} }) {
+  if (noConnection) {
+    console.info('%cSimulating no connection', consoleStyle)
+    return new Promise(function(resolve, reject) {
+      reject(new TypeError('Simulating no connection'))
+    })
+  } else if (mockResponse) {
+    console.info('%cSimulating response:', consoleStyle)
+    console.info(mockResponse())
+    return new Promise(function(resolve, reject) {
+      resolve(mockResponse())
+    })
+  }
+
   let withAuth = options
 
   withAuth.headers = _.defaults({
