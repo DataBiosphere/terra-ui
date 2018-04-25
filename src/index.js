f9324e42aed36e9ef62291927fcc49ec3e288621
Brett Heath-Wlaz <panentheos@gmail.com>
2018-04-25 16:01:47 -0400

    move css into its own file (#88)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import { h } from 'react-hyperscript-helpers'
 import Main from 'src/pages/Main'
+import 'src/style.css'
 
 
 ReactDOM.render(h(Main), document.getElementById('root'))
