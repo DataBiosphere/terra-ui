82d1f38b6ad5fb4fa0cf97c10bc2143708a528b9
Matt Putnam <matt.s.putnam@gmail.com>
2018-04-26 13:14:28 -0400

    Snapshot testing and some utils (#89)
    * add testId utility
    
    * fail if data-test-id is not found or found multiple times
    
    * more utils for writing tests
    
    * snapshot test for WorkspaceList
    
    * mock dates to avoid time zone issues

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
-import { configure } from 'enzyme'
+import { configure, ReactWrapper } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'mutationobserver-shim'
+import * as Utils from 'src/libs/utils'
 
 
 configure({ adapter: new Adapter() })
@@ -9,3 +10,31 @@ jest.mock('src/libs/ajax')
 jest.mock('src/libs/nav')
 
 window.Element.prototype['insertAdjacentElement'] = () => {} // for custom icons
+
+// Mock dates due to time zone issues
+jest.spyOn(Utils, 'makePrettyDate').mockImplementation(() => '***MOCKED DATE***')
+
+ReactWrapper.prototype.testId = function(id) {
+  const wrapper = this.find(`[data-test-id='${id}']`)
+  const length = wrapper.length
+  switch (length) {
+    case 0:
+      throw `data-test-id '${id}' not found`
+    case 1:
+      return wrapper
+    default:
+      throw `data-test-id '${id}' found ${length} times`
+  }
+}
+
+ReactWrapper.prototype.findType = function(Type) {
+  return this.find(Type().type)
+}
+
+ReactWrapper.prototype.findIcon = function(shape) {
+  return this.find(`[shape="${shape}"]`)
+}
+
+ReactWrapper.prototype.click = function() {
+  return this.simulate('click')
+}
