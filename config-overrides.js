a90b3ee65507eeb52ba61facf0efa1a9c81e0cc2
Isaac Zarsky <zarsky@broadinstitute.org>
2018-05-01 17:09:44 -0400

    add lint rules to runner

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,12 @@
 const _ = require('lodash')
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const rewireEslint = require('react-app-rewire-eslint')
 const manualOverrides = require('./webpack.config')
 
 /* config-overrides.js */
 module.exports = {
   webpack: function override(config, env) {
+    config = rewireEslint(config, env)
     config = _.merge(config, manualOverrides)
     return rewireReactHotLoader(config, env)
   },
