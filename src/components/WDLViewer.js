93e60ce13f6a101f3ec5de17581950999cafeb90
Matt Putnam <putnam@broadinstitute.org>
2018-05-03 15:21:37 -0400

    basic WDL mode, and a little styling

--- src/components/WDLViewer.js
+++ src/components/WDLViewer.js
@@ -4,6 +4,34 @@ import { textarea } from 'react-hyperscript-helpers'
 import { Component } from 'src/libs/wrapped-components'
 
 
+CodeMirror.defineMode('wdl', function() {
+  return {
+    token: (stream) => {
+      stream.eatSpace()
+      if (stream.match(/#.*/)) {
+        return 'comment'
+      } else if (stream.match(/"(?:[^"\\]|\\.)*"/) || stream.match(/'(?:[^'\\]|\\.)*'/)) {
+        return 'string'
+      } else if (stream.match(/(?:import|as|true|false|input|output|call|command|runtime|task|workflow)\b/)) {
+        return 'keyword'
+      } else if (stream.match(/(?:Array|Boolean|File|Float|Int|Map|Object|String|Uri)\b/)) {
+        return 'builtin'
+      } else if (stream.match(/[A-Za-z_][A-Za-z0-9_]*/)) {
+        return 'variable'
+      } else if (stream.match(/\${.*?}/)) {
+        return 'variable-3'
+      } else if (stream.match(/[{}]/)) {
+        return 'bracket'
+      } else if (stream.match(/[0-9]*\.?[0-9]+/)) {
+        return 'number'
+      } else {
+        stream.next()
+      }
+    }
+  }
+})
+
+
 export default class WDLViewer extends Component {
   constructor(props) {
     super(props)
@@ -11,10 +39,10 @@ export default class WDLViewer extends Component {
   }
 
   render() {
-    return textarea({ id: this.id }, this.props.wdl)
+    return textarea({ id: this.id, defaultValue: this.props.wdl })
   }
 
   componentDidMount() {
-    CodeMirror.fromTextArea(document.getElementById(this.id), { mode: 'shell' })
+    CodeMirror.fromTextArea(document.getElementById(this.id), { mode: 'wdl' })
   }
 }
