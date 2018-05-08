9dc86ee318cea61110d4b82dd1eaa9a2c7eaa557
Brett Heath-Wlaz <panentheos@gmail.com>
2018-05-08 16:16:59 -0400

    introduce async/await and formalize config access (#122)

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,11 +1,13 @@
-let loadedConfig
-
-export const loadConfig = () =>
-  fetch('config.json').then(response => response.json()).then(json => loadedConfig = json)
+import _ from 'underscore'
 
 
-export const getGoogleClientId = () => loadedConfig['googleClientId']
-export const getLeoUrlRoot = () => loadedConfig['leoUrlRoot']
-export const getRawlsUrlRoot = () => loadedConfig['rawlsUrlRoot']
-export const getSamUrlRoot = () => loadedConfig['samUrlRoot']
-export const getDockstoreUrlRoot = () => loadedConfig['dockstoreUrlRoot']
+const loadConfig = _.memoize(async () => {
+  const res = await fetch('config.json')
+  return res.json()
+})
+
+export const getGoogleClientId = async () => (await loadConfig()).googleClientId
+export const getLeoUrlRoot = async () => (await loadConfig()).leoUrlRoot
+export const getRawlsUrlRoot = async () => (await loadConfig()).rawlsUrlRoot
+export const getSamUrlRoot = async () => (await loadConfig()).samUrlRoot
+export const getDockstoreUrlRoot = async () => (await loadConfig()).dockstoreUrlRoot
