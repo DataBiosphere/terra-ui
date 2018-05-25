44df0f9a8e4dee3a97677a1891e3ec6d0ed0398d
Isaac Zarsky <zarsky@broadinstitute.org>
2018-05-25 13:43:04 -0400

    add registration page bg (#191)

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,11 @@
 import { Component } from 'react'
-import { div, h2, br } from 'react-hyperscript-helpers'
+import { div } from 'react-hyperscript-helpers'
 import { buttonPrimary } from 'src/components/common'
 import { logo, spinner } from 'src/components/icons'
 import { textInput } from 'src/components/input'
-import { Sam, Orchestration } from 'src/libs/ajax'
-import { getBasicProfile, authStore } from 'src/libs/auth'
+import planet from 'src/images/register-planet.svg'
+import { Orchestration, Sam } from 'src/libs/ajax'
+import { authStore, getBasicProfile } from 'src/libs/auth'
 import * as Style from 'src/libs/style'
 
 
@@ -35,22 +36,38 @@ export default class Register extends Component {
 
   render() {
     const { busy, givenName, familyName, email } = this.state
-    return div({ style: { padding: '4rem' } }, [
-      div({ style: { fontSize: 60, fontWeight: 400, color: Style.colors.title } }, [
-        logo({ size: 100, style: { marginRight: 20 } }), 'SATURN'
+    return div({
+      style: {
+        flexGrow: 1,
+        padding: '5rem', marginBottom: '-2rem',
+        backgroundImage: `url(${planet})`,
+        backgroundRepeat: 'no-repeat', backgroundSize: '750px', backgroundPosition: 'right 0px bottom -600px'
+      }
+    }, [
+      div({ style: { display: 'flex', alignItems: 'center' } }, [
+        logo({ size: 100, style: { marginRight: 20 } }),
+        div({ style: { fontWeight: 500, fontSize: 70, color: Style.colors.title } }, ['SATURN'])
       ]),
-      h2({ style: { marginTop: '4rem', color: Style.colors.textFaded } }, 'New User Registration'),
+      div({
+        style: {
+          marginTop: '4rem', color: Style.colors.titleAlt,
+          fontSize: '1.5rem', fontWeight: 500
+        }
+      }, 'New User Registration'),
       div({ style: { marginTop: '3rem', display: 'flex' } }, [
         div({ style: { lineHeight: '170%' } }, [
-          'First Name', br(),
+          'First Name',
           textInput({
+            style: { display: 'block' },
             value: givenName,
             onChange: e => this.setState({ givenName: e.target.value })
           })
         ]),
         div({ style: { width: '1rem' } }),
         div({ style: { lineHeight: '170%' } }, [
-          'Last Name', br(), textInput({
+          'Last Name',
+          textInput({
+            style: { display: 'block' },
             value: familyName,
             onChange: e => this.setState({ familyName: e.target.value })
           })
