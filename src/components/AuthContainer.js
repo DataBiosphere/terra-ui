5fbb3da5663d76e329f36410e3b32fdcdd4714de
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2018-05-30 11:23:02 -0400

    add separate message for disabled users

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -4,15 +4,17 @@ import { authStore } from 'src/libs/auth'
 import * as Utils from 'src/libs/utils'
 import Register from 'src/pages/Register'
 import SignIn from 'src/pages/SignIn'
+import Disabled from 'src/pages/Disabled'
 
 
 export default Utils.connectAtom(authStore, 'authState')(
-  ({ children, authState: { isSignedIn, isRegistered } }) => {
+  ({ children, authState: { isSignedIn, registrationStatus } }) => {
     return Utils.cond(
       [isSignedIn === undefined, centeredSpinner],
       [isSignedIn === false, h(SignIn)],
-      [isRegistered === undefined, centeredSpinner],
-      [isRegistered === false, h(Register)],
+      [registrationStatus === undefined, centeredSpinner],
+      [registrationStatus === 'unregistered', h(Register)],
+      [registrationStatus === 'disabled', () => h(Disabled)],
       children
     )
   }
