94ecd2cd30086eb6f3ec0408cfdc940f1ad3aa1b
Matt Putnam <putnam@broadinstitute.org>
2018-05-31 17:27:01 -0400

    first pass JSON error viewer

--- src/components/ErrorBanner.js
+++ src/components/ErrorBanner.js
@@ -1,4 +1,5 @@
 import { h } from 'react-hyperscript-helpers'
+import ErrorView from 'src/components/ErrorView'
 import Modal from 'src/components/Modal'
 import { errorStore, reportError } from 'src/libs/error'
 import * as Utils from 'src/libs/utils'
@@ -6,8 +7,13 @@ import * as Utils from 'src/libs/utils'
 export default Utils.connectAtom(errorStore, 'errorState')(
   ({ errorState }) => {
     return errorState ? h(Modal, {
+      width: 800,
       showCancel: false,
       onDismiss: () => reportError(undefined)
-    }, [errorState]) : null
+    }, [
+      h(ErrorView, {
+        error: errorState, collapses: false
+      })
+    ]) : null
   }
 )
--- src/components/ErrorView.js
+++ src/components/ErrorView.js
@@ -6,20 +6,60 @@ import * as Utils from 'src/libs/utils'
 import { Component } from 'src/libs/wrapped-components'
 
 
-export default class ErrorView extends Component {
+class StackTraceView extends Component {
   render() {
-    const { containerStyle, error } = this.props
+    const { lines } = this.props
 
     return h(Collapse, {
+      style: { marginTop: '0.5rem' },
+      title: 'Stack Trace',
+      defaultHidden: true
+    }, [
+      lines.map(({ className, methodName, fileName, lineNumber }, idx) => div({ key: idx }, [
+        `${fileName} ${className}.${methodName} (line ${lineNumber})`
+      ]))
+    ])
+  }
+}
+
+
+class JSONErrorView extends Component {
+  render() {
+    const { statusCode, source, causes, stackTrace, message } = this.props
+
+    return div({
+
+    }, [
+      div({ style: Style.elements.cardTitle }, `Error ${statusCode}: ${message}`),
+      div({}, `Source: ${source}`),
+      stackTrace && stackTrace[0] && h(StackTraceView, { lines: stackTrace }),
+      causes && causes.map((cause, idx) => h(Collapse, {
+        key: idx,
+        style: { marginTop: '0.5rem' },
+        title: causes.length === 1 ? 'Cause' : `Cause ${idx}`,
+        defaultHidden: true
+      }, [h(JSONErrorView, cause)]))
+    ])
+  }
+}
+
+
+export default class ErrorView extends Component {
+  render() {
+    const { collapses=true, containerStyle, error } = this.props
+
+    const content = Utils.cond(
+      [this.errorIsHTML(), () => this.renderHTMLError()],
+      [this.errorIsJSON(), () => this.renderJSONError()],
+      () => error
+    )
+
+    return collapses ? h(Collapse, {
       style: { marginTop: '1rem', ...containerStyle },
       title: 'Error details',
       defaultHidden: true
-    }, [
-      Utils.cond(
-        [this.errorIsHTML(), () => this.renderHTMLError()],
-        () => `Unknown, text is: ${error}`
-      )
-    ])
+    }, [content]) :
+      content
   }
 
   errorIsHTML() {
@@ -53,5 +93,15 @@ export default class ErrorView extends Component {
         })
       ])
     ])
+  }
+
+  errorIsJSON() {
+    const { error } = this.props
+    return error[0] === '{'
+  }
+
+  renderJSONError() {
+    const { error } = this.props
+    return h(JSONErrorView, JSON.parse(error))
   }
 }
