08841249b918aa53e18d3eaf76091346d6d49552
Isaac Zarsky <zarsky@broadinstitute.org>
2018-06-08 12:43:01 -0400

    Show errors in banners (#221)

--- src/libs/error.js
+++ src/libs/error.js
@@ -1,11 +1,13 @@
+import _ from 'lodash/fp'
 import * as Utils from 'src/libs/utils'
 
-export const errorStore = Utils.atom()
+
+export const errorStore = Utils.atom([])
 
 export const reportError = (title, error) => {
-  errorStore.set({ title, error })
+  errorStore.update(old => _.concat(old, { title, error }))
 }
 
 export const clearError = () => {
-  errorStore.set(undefined)
+  errorStore.set([])
 }
