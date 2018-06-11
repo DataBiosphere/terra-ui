69cd9f9ad35dc4c3568010e9f643e3053c49cbcc
Matt Putnam <putnam@broadinstitute.org>
2018-06-11 11:14:51 -0400

    discard oldest

--- src/libs/state-history.js
+++ src/libs/state-history.js
@@ -1,3 +1,4 @@
+import _ from 'lodash/fp'
 import uuid from 'uuid/v4'
 
 
@@ -13,15 +14,38 @@ const getKey = () => {
 }
 
 
-export const get = () => JSON.parse(sessionStorage.getItem(getKey())) || {}
+export const get = () => {
+  const fetched = sessionStorage.getItem(getKey())
+  if (fetched) {
+    return JSON.parse(fetched).value
+  } else {
+    return {}
+  }
+}
 
 export const set = newState => {
-  try {
-    sessionStorage.setItem(getKey(), JSON.stringify(newState))
-  } catch (error) {
-    console.error('An error occurred trying to save state history. This is probably because storage filled up.')
-    console.error(error)
-    sessionStorage.clear()
+  const key = getKey()
+  const value = JSON.stringify({ timestamp: Date.now(), value: newState })
+
+  while (true) {
+    try {
+      sessionStorage.setItem(key, value)
+      return
+    } catch (error) {
+      if (sessionStorage.length === 0) {
+        console.error('An error occurred trying to save state history.')
+        console.error(error)
+        return
+      } else {
+        const oldest = _.flow(
+          _.mapValues(JSON.parse),
+          _.toPairs,
+          _.sortBy(p => p[1].timestamp),
+          _.first
+        )(sessionStorage)
+        sessionStorage.removeItem(oldest[0])
+      }
+    }
   }
 }
 
