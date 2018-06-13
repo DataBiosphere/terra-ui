24a450dbe50aa8aebc3352857bf9ec9935cc05e6
Brett Heath-Wlaz <panentheos@gmail.com>
2018-06-13 15:06:47 -0400

    move Select into common components (#260)

--- src/libs/wrapped-components.js
+++ src/libs/wrapped-components.js
@@ -1,9 +1,4 @@
-import _ from 'lodash/fp'
 import { Component as rComponent } from 'react'
-import { h } from 'react-hyperscript-helpers'
-import rSelect from 'react-select'
-import 'react-select/dist/react-select.css'
-import * as Style from 'src/libs/style'
 
 
 export class Component extends rComponent {
@@ -13,5 +8,3 @@ export class Component extends rComponent {
     this.state = {}
   }
 }
-
-export const Select = props => h(rSelect, _.merge({ style: Style.elements.input }, props))
