97902908d92f15b85901471336b3b57d5435d232
Matt Putnam <putnam@broadinstitute.org>
2018-06-26 18:05:46 -0400

    initial pass of dropdown with icons

--- src/pages/workspaces/workspace/JobHistory.js
+++ src/pages/workspaces/workspace/JobHistory.js
@@ -1,8 +1,10 @@
 import _ from 'lodash/fp'
-import { div, h, span } from 'react-hyperscript-helpers'
+import { Fragment } from 'react'
+import { div, h, span, table, tbody, td, tr } from 'react-hyperscript-helpers'
 import { AutoSizer } from 'react-virtualized'
 import * as breadcrumbs from 'src/components/breadcrumbs'
 import { spinnerOverlay } from 'src/components/common'
+import DropdownBox from 'src/components/DropdownBox'
 import { icon } from 'src/components/icons'
 import { FlexTable, HeaderCell, TextCell } from 'src/components/table'
 import { Rawls } from 'src/libs/ajax'
@@ -46,12 +48,23 @@ const styles = {
 }
 
 
-const iconForStatus = status => {
+const collapseStatus = status => {
   switch (status) {
     case 'Succeeded':
-      return icon('check-circle', styles.statusIcon)
+      return 'Succeeded'
     case 'Aborting':
     case 'Aborted':
+    case 'Failed':
+      return 'Failed'
+    default:
+      return 'Running'
+  }
+}
+
+const iconForStatus = status => {
+  switch (collapseStatus(status)) {
+    case 'Succeeded':
+      return icon('check-circle', styles.statusIcon)
     case 'Failed':
       return icon('warning-standard', styles.statusIcon)
     default:
@@ -60,11 +73,9 @@ const iconForStatus = status => {
 }
 
 const colorForStatus = status => {
-  switch (status) {
+  switch (collapseStatus(status)) {
     case 'Succeeded':
       return Style.colors.success
-    case 'Aborting':
-    case 'Aborted':
     case 'Failed':
       return Style.colors.standout
     default:
@@ -75,6 +86,48 @@ const colorForStatus = status => {
 
 export const flagNewSubmission = submissionId => {
   sessionStorage.setItem('new-submission', submissionId)
+}
+
+
+class StatusCell extends Component {
+  render() {
+    const { workflowStatuses } = this.props
+    const { open } = this.state
+
+    const collapsed = _.flow(
+      _.toPairs,
+      _.map(([status, count]) => ({ [collapseStatus(status)]: count })),
+      _.reduce(_.mergeWith(_.add), {})
+    )(workflowStatuses)
+
+    const collapsedKeys = _.keys(collapsed)
+
+    return h(Fragment, [
+      _.includes('Succeeded', collapsedKeys) && icon('check', { size: 24, style: { marginRight: '0.5rem', color: Style.colors.success } }),
+      _.includes('Failed', collapsedKeys) && icon('warning-standard', { class: 'is-solid', size: 24, style: { marginRight: '0.5rem', color: Style.colors.error } }),
+      _.includes('Running', collapsedKeys) && icon('sync', { size: 24, style: { marginRight: '0.5rem', color: Style.colors.success } }),
+      h(DropdownBox, {
+        open,
+        width: 'auto',
+        onToggle: open => this.setState({ open })
+      }, [
+        table({}, [
+          tbody({}, [
+            tr({}, [
+              td({ align: 'center', style: { padding: '0.5rem' } }, [icon('check', { size: 24, style: { color: Style.colors.success } })]),
+              td({ align: 'center', style: { padding: '0.5rem' } }, [icon('warning-standard', { class: 'is-solid', size: 24, style: { color: Style.colors.error } })]),
+              td({ align: 'center', style: { padding: '0.5rem' } }, [icon('sync', { size: 24, style: { color: Style.colors.success } })])
+            ]),
+            tr({}, [
+              td({ align: 'center', style: { padding: '0.5rem' } }, [collapsed['Succeeded'] || 0]),
+              td({ align: 'center', style: { padding: '0.5rem' } }, [collapsed['Failed'] || 0]),
+              td({ align: 'center', style: { padding: '0.5rem' } }, [collapsed['Running'] || 0])
+            ])
+          ])
+        ])
+      ])
+    ])
+  }
 }
 
 
@@ -168,8 +221,8 @@ class JobHistory extends Component {
               size: { basis: 150, grow: 0 },
               headerRenderer: () => h(HeaderCell, ['Status']),
               cellRenderer: ({ rowIndex }) => {
-                const { status } = submissions[rowIndex]
-                return h(TextCell, status)
+                const { workflowStatuses } = submissions[rowIndex]
+                return h(StatusCell, { workflowStatuses })
               }
             },
             {
