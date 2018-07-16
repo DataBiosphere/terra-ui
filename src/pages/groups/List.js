162d678f339b66db79b061d6976b6ae060b89fc4
Matt Putnam <putnam@broadinstitute.org>
2018-07-16 14:32:44 -0400

    pull styles out

--- src/pages/groups/List.js
+++ src/pages/groups/List.js
@@ -13,46 +13,8 @@ import * as StateHistory from 'src/libs/state-history'
 import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
 import { Component } from 'src/libs/wrapped-components'
+import { styles } from 'src/pages/groups/common'
 
-
-const styles = {
-  cardContainer: {
-    position: 'relative',
-    padding: '0 4rem',
-    display: 'flex', flexWrap: 'wrap'
-  },
-  shortCreateCard: {
-    ...Style.elements.card,
-    display: 'flex', flexDirection: 'column', justifyContent: 'center',
-    width: 180, height: 100,
-    margin: '0.25rem 0.5rem',
-    color: Style.colors.secondary, fontSize: 18, lineHeight: '22px'
-  },
-  longCard: {
-    ...Style.elements.card,
-    display: 'flex', alignItems: 'center',
-    width: '100%', minWidth: 0,
-    margin: '0.25rem 0.5rem',
-    fontSize: 13
-  },
-  longTitle: {
-    fontSize: 16,
-    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
-  },
-  toolbarContainer: {
-    flex: 'none', display: 'flex', alignItems: 'flex-end',
-    margin: '1rem 4.5rem 2rem'
-  },
-  toolbarButtons: {
-    marginLeft: 'auto', display: 'flex',
-    backgroundColor: 'white', borderRadius: 3
-  },
-  toolbarButton: {
-    display: 'flex', justifyContent: 'center', alignItems: 'center',
-    height: '2.25rem', width: '3rem',
-    color: Style.colors.secondary
-  }
-}
 
 class NewGroupModal extends Component {
   render() {
---
+++ src/pages/groups/common.js
@@ -0,0 +1,41 @@
+import * as Style from 'src/libs/style'
+
+
+export const styles = {
+  cardContainer: {
+    position: 'relative',
+    padding: '0 4rem',
+    display: 'flex', flexWrap: 'wrap'
+  },
+  shortCreateCard: {
+    ...Style.elements.card,
+    display: 'flex', flexDirection: 'column', justifyContent: 'center',
+    width: 180, height: 100,
+    margin: '0.25rem 0.5rem',
+    color: Style.colors.secondary, fontSize: 18, lineHeight: '22px'
+  },
+  longCard: {
+    ...Style.elements.card,
+    display: 'flex', alignItems: 'center',
+    width: '100%', minWidth: 0,
+    margin: '0.25rem 0.5rem',
+    fontSize: 13
+  },
+  longTitle: {
+    fontSize: 16,
+    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
+  },
+  toolbarContainer: {
+    flex: 'none', display: 'flex', alignItems: 'flex-end',
+    margin: '1rem 4.5rem 2rem'
+  },
+  toolbarButtons: {
+    marginLeft: 'auto', display: 'flex',
+    backgroundColor: 'white', borderRadius: 3
+  },
+  toolbarButton: {
+    display: 'flex', justifyContent: 'center', alignItems: 'center',
+    height: '2.25rem', width: '3rem',
+    color: Style.colors.secondary
+  }
+}
