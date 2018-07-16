e7e9c3662a16ce9e4eac35895439083df60299fd
Brett Heath-Wlaz <panentheos@gmail.com>
2018-07-16 15:19:50 -0400

    tune up tools page (#365)

--- src/pages/workspaces/workspace/Tools.js
+++ src/pages/workspaces/workspace/Tools.js
@@ -1,8 +1,8 @@
 import _ from 'lodash/fp'
 import { a, div, h } from 'react-hyperscript-helpers'
+import { pure } from 'recompose'
 import * as breadcrumbs from 'src/components/breadcrumbs'
 import { spinnerOverlay } from 'src/components/common'
-import { DataGrid } from 'src/components/table'
 import { Rawls } from 'src/libs/ajax'
 import { reportError } from 'src/libs/error'
 import * as Nav from 'src/libs/nav'
@@ -12,6 +12,35 @@ import { Component } from 'src/libs/wrapped-components'
 import { wrapWorkspace } from 'src/pages/workspaces/workspace/WorkspaceContainer'
 
 
+const styles = {
+  cardContainer: {
+    padding: '1rem 4rem',
+    display: 'flex', flexWrap: 'wrap'
+  },
+  card: {
+    ...Style.elements.card, width: 300, height: 125, margin: '1rem 0.5rem',
+    display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
+  },
+  shortTitle: {
+    flex: 'none',
+    color: Style.colors.secondary, fontSize: 16,
+    lineHeight: '20px', height: '40px',
+    overflow: 'hidden', overflowWrap: 'break-word'
+  }
+}
+
+const ToolCard = pure(({ name, namespace, config }) => {
+  const { namespace: workflowNamespace, name: workflowName, methodRepoMethod: { sourceRepo, methodVersion } } = config
+  return a({
+    style: styles.card,
+    href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
+  }, [
+    div({ style: styles.shortTitle }, [workflowName]),
+    div([`V. ${methodVersion}`]),
+    div([`Source: ${sourceRepo}`])
+  ])
+})
+
 const WorkspaceTools = wrapWorkspace({
   breadcrumbs: props => breadcrumbs.commonPaths.workspaceDashboard(props),
   title: 'Tools', activeTab: 'tools'
@@ -19,50 +48,31 @@ const WorkspaceTools = wrapWorkspace({
 class ToolsContent extends Component {
   constructor(props) {
     super(props)
-    this.state = { itemsPerPage: 6, pageNumber: 1, ...StateHistory.get() }
+    this.state = { ...StateHistory.get() }
   }
 
   async refresh() {
     const { namespace, name } = this.props
 
     try {
+      this.setState({ loading: true })
       const configs = await Rawls.workspace(namespace, name).listMethodConfigs()
-      this.setState({ isFreshData: true, configs })
+      this.setState({ configs })
     } catch (error) {
       reportError('Error loading configs', error)
+    } finally {
+      this.setState({ loading: false })
     }
   }
 
   render() {
-    const { isFreshData, configs, itemsPerPage, pageNumber } = this.state
-    const workspaceId = _.pick(['namespace', 'name'], this.props)
-
-    return div({ style: { padding: '1rem 4rem', flexGrow: 1, position: 'relative' } }, [
-      configs && h(DataGrid, {
-        dataSource: configs,
-        itemsPerPageOptions: [6, 12, 24, 36, 48],
-        itemsPerPage,
-        onItemsPerPageChanged: itemsPerPage => this.setState({ itemsPerPage, pageNumber: 1 }),
-        pageNumber,
-        onPageChanged: n => this.setState({ pageNumber: n }),
-        renderCard: config => {
-          const { name, namespace, methodRepoMethod: { sourceRepo, methodVersion } } = config
-          return a({
-            style: { ...Style.elements.card, width: '30%', margin: '1rem auto' },
-            href: Nav.getLink('workflow', {
-              namespace: workspaceId.namespace,
-              name: workspaceId.name,
-              workflowNamespace: namespace,
-              workflowName: name
-            })
-          }, [
-            div({ style: { ...Style.elements.cardTitle, marginBottom: '0.5rem' } }, name),
-            div(`V. ${methodVersion}`),
-            div(`Source: ${sourceRepo}`)
-          ])
-        }
-      }),
-      !isFreshData && spinnerOverlay
+    const { namespace, name } = this.props
+    const { loading, configs } = this.state
+    return div({ style: styles.cardContainer }, [
+      ..._.map(config => {
+        return h(ToolCard, { key: `${config.namespace}/${config.name}`, namespace, name, config })
+      }, configs),
+      loading && spinnerOverlay
     ])
   }
 
@@ -71,10 +81,7 @@ class ToolsContent extends Component {
   }
 
   componentDidUpdate() {
-    StateHistory.update(_.pick(
-      ['configs', 'itemsPerPage', 'pageNumber'],
-      this.state)
-    )
+    StateHistory.update(_.pick(['configs'], this.state))
   }
 })
 
