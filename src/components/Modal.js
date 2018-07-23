af66fea03c0c57dfd239498d18c374b72cf87fc1
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2018-07-23 09:33:27 -0400

    disable close on overlay click

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -40,6 +40,7 @@ export default class Modal extends Component {
     return h(RModal, {
       parentSelector: () => document.getElementById('modal-root'),
       isOpen: true,
+      shouldCloseOnOverlayClick: false,
       onRequestClose: onDismiss,
       style: { overlay: styles.overlay, content: { ...styles.modal, width } },
       ariaHideApp: false
