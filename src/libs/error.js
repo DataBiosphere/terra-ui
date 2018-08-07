d9b6d5ebf63e993aed4b3bb56eb9d4a43aa390ab
Isaac Zarsky <zarsky@broadinstitute.org>
2018-08-07 15:48:56 -0400

    fixes #467

--- src/libs/error.js
+++ src/libs/error.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp'
-import { signOut } from 'src/libs/auth'
+import { getUser, signOut } from 'src/libs/auth'
 import * as StateHistory from 'src/libs/state-history'
 import * as Utils from 'src/libs/utils'
 
@@ -9,11 +9,12 @@ export const errorStore = Utils.atom([])
 const addError = item => errorStore.update(state => _.concat(state, [item]))
 
 export const reportError = async (title, obj) => {
-  if (obj instanceof Response && obj.status === 401) {
+  if (obj instanceof Response && obj.status === 401 && await getUser().reloadAuthResponse().then(() => false).catch(() => true)) {
     addError({ title: 'Session timed out', error: 'You have been signed out due to inactivity', code: 'sessionTimeout' })
-    return signOut()
+    signOut()
+  } else {
+    addError({ title, error: await (obj instanceof Response ? obj.text() : obj) })
   }
-  addError({ title, error: await (obj instanceof Response ? obj.text() : obj) })
 }
 
 export const clearError = (hard = false) => {
