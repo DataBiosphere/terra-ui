434758989a8b4deec86cd09ae70e70abc90624e1
Liz Zhao <42386483+elizzhao@users.noreply.github.com>
2018-08-31 14:34:21 -0400

    group already exists error is realtime (#596)

--- src/pages/groups/List.js
+++ src/pages/groups/List.js
@@ -19,13 +19,17 @@ import { styles } from 'src/pages/groups/common'
 import { validate } from 'validate.js'
 
 
-const groupNameValidator = {
+const groupNameValidator = existing => ({
   presence: { allowEmpty: false },
   format: {
     pattern: /[A-Za-z0-9_-]*$/,
     message: 'can only contain letters, numbers, underscores, and dashes'
+  },
+  exclusion: {
+    within: existing,
+    message: 'already exists'
   }
-}
+})
 
 const NewGroupModal = ajaxCaller(class NewGroupModal extends Component {
   constructor(props) {
@@ -37,10 +41,10 @@ const NewGroupModal = ajaxCaller(class NewGroupModal extends Component {
   }
 
   render() {
-    const { onDismiss } = this.props
+    const { onDismiss, existingGroups } = this.props
     const { groupName, groupNameTouched, submitting } = this.state
 
-    const errors = validate({ groupName }, { groupName: groupNameValidator })
+    const errors = validate({ groupName }, { groupName: groupNameValidator(existingGroups) })
 
     return h(Modal, {
       onDismiss,
@@ -165,7 +169,6 @@ export const GroupList = ajaxCaller(class GroupList extends Component {
   render() {
     const { groups, isDataLoaded, filter, creatingNewGroup, deletingGroup, updating } = this.state
     const { ajax: { Groups } } = this.props
-
     return h(Fragment, [
       h(TopBar, { title: 'Groups', href: Nav.getLink('groups') }, [
         search({
@@ -197,6 +200,7 @@ export const GroupList = ajaxCaller(class GroupList extends Component {
         !isDataLoaded && spinnerOverlay
       ]),
       creatingNewGroup && h(NewGroupModal, {
+        existingGroups: _.map('groupName', groups),
         onDismiss: () => this.setState({ creatingNewGroup: false }),
         onSuccess: () => this.refresh()
       }),
