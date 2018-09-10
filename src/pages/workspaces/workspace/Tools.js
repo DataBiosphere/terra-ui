a4bfb80987458e6d7ac4957ec5e81edd558b71a7
Kate Voss <kvoss@broadinstitute.org>
2018-09-10 16:22:59 -0400

    [In progress] add list view to Tools

--- src/pages/workspaces/workspace/Tools.js
+++ src/pages/workspaces/workspace/Tools.js
@@ -18,27 +18,53 @@ const styles = {
     padding: '1rem 4rem',
     display: 'flex', flexWrap: 'wrap'
   },
-  card: {
+  shortCard: {
     ...Style.elements.card, width: 300, height: 125, margin: '1rem 0.5rem',
     display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
   },
+/*  shortToolCard: {
+    display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
+  },*/ // possibly not necessary
   shortTitle: {
     flex: 'none',
     color: colors.blue[0], fontSize: 16,
     lineHeight: '20px', height: '40px',
     overflow: 'hidden', overflowWrap: 'break-word'
+  },
+  /*shortCreateCard: {
+    display: 'flex', flexDirection: 'column', justifyContent: 'center',
+    color: colors.blue[0], fontSize: 20, lineHeight: '28px'
+  },*/  // To be added later
+  longCard: {
+    ...Style.elements.card,
+    width: '100%', minWidth: 0, height: 80,
+    margin: '0.25rem 0.5rem'
+  },
+/*  longToolCard: {
+    display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
+  },*/ // possibly not necessary
+  longTitle: {
+    color: colors.blue[0], fontSize: 16,
+    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
   }
 }
 
-const ToolCard = pure(({ name, namespace, config }) => {
+const ToolCard = pure(({ listView, name, namespace, config }) => {
   const { namespace: workflowNamespace, name: workflowName, methodRepoMethod: { sourceRepo, methodVersion } } = config
-  return a({
-    style: styles.card,
+  return listView ? a({
+    style: styles.shortCard,
     href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
   }, [
     div({ style: styles.shortTitle }, [workflowName]),
     div([`V. ${methodVersion}`]),
     div([`Source: ${sourceRepo}`])
+  ]) : a({
+    style: styles.longCard,
+    href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
+  }, [
+    div({ style: styles.longTitle }, [workflowName]),
+    div([`V. ${methodVersion}`]),
+    div({ style: { flex: 'none', width: 400 } }, [`Source: ${sourceRepo}`])
   ])
 })
 
