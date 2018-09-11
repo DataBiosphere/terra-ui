46cef2ae907f35a5baf71831c5bb81a877cc18c0
Kate Voss <kvoss@broadinstitute.org>
2018-09-11 16:28:55 -0400

    Make list view narrower into one line, edit notes

--- src/pages/workspaces/workspace/Tools.js
+++ src/pages/workspaces/workspace/Tools.js
@@ -30,46 +30,46 @@ const styles = {
     lineHeight: '20px', height: '40px',
     overflow: 'hidden', overflowWrap: 'break-word'
   },
-  shortDescription: { // copied from workspaces/List.js
+  shortDescription: {
     flex: 'none',
     lineHeight: '18px', height: '90px',
     overflow: 'hidden'
   },
   /*
-   *shortCreateCard: { // copied from workspaces/List.js
+   *shortCreateCard: {
    *  display: 'flex', flexDirection: 'column', justifyContent: 'center',
    *  color: colors.blue[0], fontSize: 20, lineHeight: '28px'
    *},
-   */ // To be added later
-  longCard: { // copied from workspaces/List.js
+   */ //TODO: add short create card
+  longCard: {
     ...Style.elements.card,
-    width: '100%', minWidth: 0, height: 80,
+    width: '100%', minWidth: 0,
     margin: '0.25rem 0.5rem'
   },
-  longTitle: { // copied from workspaces/List.js
+  longTitle: {
     color: colors.blue[0], fontSize: 16,
     whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', flex: 1
   },
-  longDescription: { // copied from workspaces/List.js
+  longDescription: {
     flex: 1,
     paddingRight: '1rem',
     whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
   },
   /*
-   *longCreateCard: { // copied from workspaces/List.js
+   *longCreateCard: {
    *display: 'flex', flexDirection: 'column', justifyContent: 'center',
    *color: colors.blue[0], fontSize: 16
    *}
-   */ // To be added later
+   */ //TODO: Add long create card
   toolbarContainer: {
     flex: 'none', display: 'flex', alignItems: 'flex-end',
     margin: '1rem 4.5rem'
   },
-  toolbarButtons: { // copied from workspaces/List.js
+  toolbarButtons: {
     marginLeft: 'auto', display: 'flex',
     backgroundColor: 'white', borderRadius: 3
   },
-  toolbarButton: active => ({ // copied from workspaces/List.js
+  toolbarButton: active => ({
     display: 'flex', justifyContent: 'center', alignItems: 'center',
     height: '2.25rem', width: '3rem',
     color: active ? colors.blue[1] : colors.blue[0]
@@ -84,19 +84,16 @@ const ToolCard = pure(({ listView, name, namespace, config }) => {
   }, [
     div({ style: { display: 'flex', alignItems: 'center' } }, [
       div({ style: styles.longTitle }, [workflowName]),
-      div([`V. ${methodVersion}`])
-    ]),
-    //To do: add tool description
-    div({ style: { display: 'flex', alignItems: 'center' } }, [
-      div({ style: { flex: 1 } }),
+      div({style: { marginRight: '1rem' } }, [`V. ${methodVersion}`]),
       div({ style: { flex: 'none' } }, [`Source: ${sourceRepo}`])
     ])
+    // TODO: add tool description
   ]) : a({
     style: styles.shortCard,
     href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
   }, [
     div({ style: styles.shortTitle }, [workflowName]),
-    //To do: add tool description
+    // TODO: add tool description
     div([`V. ${methodVersion}`]),
     div([`Source: ${sourceRepo}`])
   ])
