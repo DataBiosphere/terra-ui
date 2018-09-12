75890144f3403375a595a61990a5d283eb2bd591
Kate Voss <kvoss@broadinstitute.org>
2018-09-12 11:31:56 -0400

    Tools list view (#635)

--- src/pages/workspaces/workspace/Tools.js
+++ src/pages/workspaces/workspace/Tools.js
@@ -1,8 +1,10 @@
 import _ from 'lodash/fp'
+import { Fragment } from 'react'
 import { a, div, h } from 'react-hyperscript-helpers'
 import { pure } from 'recompose'
 import * as breadcrumbs from 'src/components/breadcrumbs'
-import { spinnerOverlay } from 'src/components/common'
+import { Clickable, spinnerOverlay } from 'src/components/common'
+import { icon } from 'src/components/icons'
 import { ajaxCaller } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
 import { reportError } from 'src/libs/error'
@@ -18,22 +20,67 @@ const styles = {
     padding: '1rem 4rem',
     display: 'flex', flexWrap: 'wrap'
   },
-  card: {
+  shortCard: {
     ...Style.elements.card, width: 300, height: 125, margin: '1rem 0.5rem',
     display: 'flex', flexDirection: 'column', justifyContent: 'space-between'
   },
   shortTitle: {
-    flex: 'none',
+    flex: 1,
     color: colors.blue[0], fontSize: 16,
     lineHeight: '20px', height: '40px',
     overflow: 'hidden', overflowWrap: 'break-word'
-  }
+  },
+  shortDescription: {
+    flex: 'none',
+    lineHeight: '18px', height: '90px',
+    overflow: 'hidden'
+  },
+  longMethodVersion: {
+    marginRight: '1rem', width: 90,
+    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
+  },
+  longCard: {
+    ...Style.elements.card,
+    width: '100%', minWidth: 0,
+    margin: '0.25rem 0.5rem'
+  },
+  longTitle: {
+    color: colors.blue[0], fontSize: 16,
+    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', flex: 1
+  },
+  longDescription: {
+    flex: 1,
+    paddingRight: '1rem',
+    whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
+  },
+  toolbarContainer: {
+    flex: 'none', display: 'flex', alignItems: 'flex-end',
+    margin: '1rem 4.5rem'
+  },
+  toolbarButtons: {
+    marginLeft: 'auto', display: 'flex',
+    backgroundColor: 'white', borderRadius: 3
+  },
+  toolbarButton: active => ({
+    display: 'flex', justifyContent: 'center', alignItems: 'center',
+    height: '2.25rem', width: '3rem',
+    color: active ? colors.blue[1] : colors.blue[0]
+  })
 }
 
-const ToolCard = pure(({ name, namespace, config }) => {
+const ToolCard = pure(({ listView, name, namespace, config }) => {
   const { namespace: workflowNamespace, name: workflowName, methodRepoMethod: { sourceRepo, methodVersion } } = config
-  return a({
-    style: styles.card,
+  return listView ? a({
+    style: styles.longCard,
+    href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
+  }, [
+    div({ style: { display: 'flex', alignItems: 'center' } }, [
+      div({ style: styles.longTitle }, [workflowName]),
+      div({ style: styles.longMethodVersion }, [`V. ${methodVersion}`]),
+      div({ style: { flex: 'none', width: 130 } }, [`Source: ${sourceRepo}`])
+    ])
+  ]) : a({
+    style: styles.shortCard,
     href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
   }, [
     div({ style: styles.shortTitle }, [workflowName]),
@@ -42,14 +89,17 @@ const ToolCard = pure(({ name, namespace, config }) => {
   ])
 })
 
-const WorkspaceTools = ajaxCaller(wrapWorkspace({
+export const WorkspaceTools = ajaxCaller(wrapWorkspace({
   breadcrumbs: props => breadcrumbs.commonPaths.workspaceDashboard(props),
   title: 'Tools', activeTab: 'tools'
 },
 class ToolsContent extends Component {
   constructor(props) {
     super(props)
-    this.state = { ...StateHistory.get() }
+    this.state = {
+      listView: false,
+      ...StateHistory.get()
+    }
   }
 
   async refresh() {
@@ -68,13 +118,30 @@ class ToolsContent extends Component {
 
   render() {
     const { namespace, name } = this.props
-    const { loading, configs } = this.state
-    return div({ style: styles.cardContainer }, [
-      _.map(config => {
-        return h(ToolCard, { key: `${config.namespace}/${config.name}`, namespace, name, config })
-      }, configs),
-      configs && !configs.length && div(['No tools added']),
-      loading && spinnerOverlay
+    const { loading, configs, listView } = this.state
+    return h(Fragment, [
+      div({ style: styles.toolbarContainer }, [
+        div({ style: { ...Style.elements.sectionHeader, textTransform: 'uppercase' } }, [
+          'Tools'
+        ]),
+        div({ style: styles.toolbarButtons }, [
+          h(Clickable, {
+            style: styles.toolbarButton(!listView),
+            onClick: () => this.setState({ listView: false })
+          }, [icon('view-cards', { size: 24 })]),
+          h(Clickable, {
+            style: styles.toolbarButton(listView),
+            onClick: () => this.setState({ listView: true })
+          }, [icon('view-list', { size: 24 })])
+        ])
+      ]),
+      div({ style: styles.cardContainer }, [
+        _.map(config => {
+          return h(ToolCard, { key: `${config.namespace}/${config.name}`, namespace, name, config, listView })
+        }, configs),
+        configs && !configs.length && div(['No tools added']),
+        loading && spinnerOverlay
+      ])
     ])
   }
 
@@ -83,7 +150,7 @@ class ToolsContent extends Component {
   }
 
   componentDidUpdate() {
-    StateHistory.update(_.pick(['configs'], this.state))
+    StateHistory.update(_.pick(['configs', 'listView'], this.state))
   }
 }))
 
