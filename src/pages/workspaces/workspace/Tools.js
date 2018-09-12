1ff3d6e5f8929e92fbb6d78d5d1b06fa63b870be
Kate Voss <kvoss@broadinstitute.org>
2018-09-12 09:58:30 -0400

    Remove comments

--- src/pages/workspaces/workspace/Tools.js
+++ src/pages/workspaces/workspace/Tools.js
@@ -35,12 +35,6 @@ const styles = {
     lineHeight: '18px', height: '90px',
     overflow: 'hidden'
   },
-  /*
-   *shortCreateCard: {
-   *  display: 'flex', flexDirection: 'column', justifyContent: 'center',
-   *  color: colors.blue[0], fontSize: 20, lineHeight: '28px'
-   *},
-   */ //TODO: add short create card
   longCard: {
     ...Style.elements.card,
     width: '100%', minWidth: 0,
@@ -55,12 +49,6 @@ const styles = {
     paddingRight: '1rem',
     whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'
   },
-  /*
-   *longCreateCard: {
-   *display: 'flex', flexDirection: 'column', justifyContent: 'center',
-   *color: colors.blue[0], fontSize: 16
-   *}
-   */ //TODO: Add long create card
   toolbarContainer: {
     flex: 'none', display: 'flex', alignItems: 'flex-end',
     margin: '1rem 4.5rem'
@@ -87,13 +75,11 @@ const ToolCard = pure(({ listView, name, namespace, config }) => {
       div({ style: { marginRight: '1rem' } }, [`V. ${methodVersion}`]),
       div({ style: { flex: 'none' } }, [`Source: ${sourceRepo}`])
     ])
-    // TODO: add tool description
   ]) : a({
     style: styles.shortCard,
     href: Nav.getLink('workflow', { namespace, name, workflowNamespace, workflowName })
   }, [
     div({ style: styles.shortTitle }, [workflowName]),
-    // TODO: add tool description
     div([`V. ${methodVersion}`]),
     div([`Source: ${sourceRepo}`])
   ])
