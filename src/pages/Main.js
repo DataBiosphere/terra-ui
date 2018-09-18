d84afcb5d1b0111ae3233126282a8afba5a3787e
Brett Heath-Wlaz <panentheos@gmail.com>
2018-09-18 09:07:22 -0400

    implement error boundary (#654)

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,16 @@ import { hot } from 'react-hot-loader'
 import { h } from 'react-hyperscript-helpers'
 import ConfigOverridesWarning from 'src/components/ConfigOverridesWarning'
 import ErrorBanner from 'src/components/ErrorBanner'
+import ErrorWrapper from 'src/components/ErrorWrapper'
 import Router from 'src/components/Router'
 
 
-const Main = () => h(Fragment, [h(Router), h(ErrorBanner), h(ConfigOverridesWarning)])
+const Main = () => {
+  return h(Fragment, [
+    h(ErrorWrapper, [h(Router)]),
+    h(ErrorBanner),
+    h(ConfigOverridesWarning)
+  ])
+}
 
 export default hot(module)(Main)
