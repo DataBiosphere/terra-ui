c28c7e529907788a5bfb67d91d4632c70e8cd307
Isaac Zarsky <zarsky@broadinstitute.org>
2018-09-26 17:48:28 -0400

    caught something already!

--- src/pages/workspaces/workspace/WorkspaceContainer.js
+++ src/pages/workspaces/workspace/WorkspaceContainer.js
@@ -147,7 +147,7 @@ class WorkspaceContainer extends Component {
         ]),
         h(ClusterManager, {
           namespace, name, clusters, refreshClusters,
-          canCompute: (workspace && workspace.canCompute) || (clusters && clusters.length)
+          canCompute: (workspace && workspace.canCompute) || (clusters && !!clusters.length)
         })
       ]),
       showTabBar && h(WorkspaceTabs, {
