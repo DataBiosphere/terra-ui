e89f9e20b1ed694a3feac7eefb49d4c837bd7b34
Kate Voss <kvoss@broadinstitute.org>
2018-10-05 12:13:19 -0400

    Eslinting the result

--- src/components/ErrorBanner.js
+++ src/components/ErrorBanner.js
@@ -22,53 +22,53 @@ class ErrorBanner extends Component {
 
     return createPortal(
       h(TopBanner, {
-      style: { padding: 0 },
-      isVisible: !!errorState.length,
-      onDismiss: () => {
-        this.setState({ errorNumber: 0 })
-        clearError()
-      }
-    },
-    [
-      div({
-        style: {
-          display: 'flex', alignItems: 'center', padding: '1rem', marginRight: '1rem',
-          borderRight: `1px solid ${colors.orange[2]}`
+        style: { padding: 0 },
+        isVisible: !!errorState.length,
+        onDismiss: () => {
+          this.setState({ errorNumber: 0 })
+          clearError()
         }
-      }, [
-        h(Clickable, {
-          disabled: onFirst,
-          style: { color: onFirst ? colors.orange[2] : null },
-          onClick: () => this.setState({ errorNumber: errorNumber - 1 })
-        }, [icon('angle left')]),
+      },
+      [
         div({
           style: {
-            fontWeight: 500,
-            backgroundColor: 'rgba(255, 255, 255, 0.2', borderRadius: '1rem',
-            padding: '0.5rem 1rem', margin: '-0.25rem 0'
+            display: 'flex', alignItems: 'center', padding: '1rem', marginRight: '1rem',
+            borderRight: `1px solid ${colors.orange[2]}`
           }
-        }, [errorNumber + 1, '/', errorState.length]),
-        h(Clickable, {
-          disabled: onLast,
-          style: { color: onLast ? colors.orange[2] : null },
-          onClick: () => this.setState({ errorNumber: errorNumber + 1 })
-        }, [icon('angle right')])
-      ]),
-      title,
-      h(Clickable, {
-        style: { textDecoration: 'underline', marginLeft: '1rem' },
-        onClick: () => this.setState({ modal: true })
-      }, ['Details...']),
-      modal && h(Modal, {
-        width: 800,
+        }, [
+          h(Clickable, {
+            disabled: onFirst,
+            style: { color: onFirst ? colors.orange[2] : null },
+            onClick: () => this.setState({ errorNumber: errorNumber - 1 })
+          }, [icon('angle left')]),
+          div({
+            style: {
+              fontWeight: 500,
+              backgroundColor: 'rgba(255, 255, 255, 0.2', borderRadius: '1rem',
+              padding: '0.5rem 1rem', margin: '-0.25rem 0'
+            }
+          }, [errorNumber + 1, '/', errorState.length]),
+          h(Clickable, {
+            disabled: onLast,
+            style: { color: onLast ? colors.orange[2] : null },
+            onClick: () => this.setState({ errorNumber: errorNumber + 1 })
+          }, [icon('angle right')])
+        ]),
         title,
-        showCancel: false,
-        showX: true,
-        onDismiss: () => this.setState({ modal: false }),
-        okButton: buttonPrimary({ onClick: () => clearError(true) }, 'Refresh Page')
-      }, [
-        h(ErrorView, { error, collapses: false })
-      ])
+        h(Clickable, {
+          style: { textDecoration: 'underline', marginLeft: '1rem' },
+          onClick: () => this.setState({ modal: true })
+        }, ['Details...']),
+        modal && h(Modal, {
+          width: 800,
+          title,
+          showCancel: false,
+          showX: true,
+          onDismiss: () => this.setState({ modal: false }),
+          okButton: buttonPrimary({ onClick: () => clearError(true) }, 'Refresh Page')
+        }, [
+          h(ErrorView, { error, collapses: false })
+        ])
       ]),
       document.getElementById('modal-root')
     )
