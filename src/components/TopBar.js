c72ed445c3088e3c0137de5d0ea161a899f5067f
Brett Heath-Wlaz <panentheos@gmail.com>
2018-11-05 13:01:00 -0500

    fix nav bar overlap (#835)

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -22,7 +22,7 @@ const styles = {
   },
   nav: {
     background: {
-      position: 'absolute', left: 0, right: 0, top: 0, bottom: 0,
+      position: 'fixed', left: 0, right: 0, top: 0, bottom: 0,
       overflow: 'auto', cursor: 'pointer'
     },
     container: {
