f7e5f10ffbbe41bc50af60aa9eada3dce97da499
Matt Putnam <putnam@broadinstitute.org>
2018-11-20 14:16:26 -0500

    underline component

--- src/components/StepButtons.js
+++ src/components/StepButtons.js
@@ -7,20 +7,26 @@ import colors from 'src/libs/colors'
 import { Component } from 'src/libs/wrapped-components'
 
 
+const buttonWidth = 170
+const buttonHeight = 50
+const dotSpace = 7
+const dotSize = 6
+
+
 const styles = {
   container: {
-    display: 'flex', alignItems: 'center',
+    display: 'flex', alignItems: 'flex-start',
     margin: '2rem 0'
   },
   button: isActive => ({
     display: 'flex', alignItems: 'center',
     flex: 'none',
-    width: 170, height: 50,
-    borderRadius: 25, borderWidth: 2, borderStyle: 'solid',
+    width: buttonWidth, height: buttonHeight,
+    borderRadius: buttonHeight/2, borderWidth: 2, borderStyle: 'solid',
     borderColor: isActive ? colors.blue[0] : colors.gray[3],
     backgroundColor: isActive ? colors.blue[1] : colors.gray[4],
     color: 'white',
-    padding: '0 1.5rem', marginRight: 7
+    padding: '0 1.5rem'
   }),
   buttonLabel: {
     textTransform: 'uppercase', fontWeight: 600, fontSize: 16, marginLeft: '0.5rem'
@@ -30,9 +36,15 @@ const styles = {
 const els = {
   dot: isActive => div({
     style: {
-      width: 6, height: 6, borderRadius: '100%',
-      marginRight: 7,
+      width: dotSize, height: dotSize, borderRadius: '100%',
+      margin: `${(buttonHeight-dotSize)/2}px ${dotSpace}px 0 0`,
       backgroundColor: isActive ? colors.green[0] : colors.gray[3]
+    }
+  }),
+  selectionUnderline: div({
+    style: {
+      margin: '8px auto 0', width: buttonWidth - buttonHeight,
+      border: `4px solid ${colors.blue[0]}`, borderRadius: 4
     }
   })
 }
@@ -41,18 +53,23 @@ const els = {
 const stepButton = ({ i, key, title, selectedIndex, onChangeTab }) => {
   const isActive = i <= selectedIndex
 
+  const button = h(Clickable, {
+    key,
+    style: styles.button(isActive),
+    onClick: () => onChangeTab(key)
+  }, [
+    i < selectedIndex ?
+      // ugh, why are these so different visually?
+      icon('check-circle', { className: 'is-solid', size: 24 }) :
+      icon('edit', { className: 'is-solid', size: 16, style: { margin: 4 } }),
+    span({ style: styles.buttonLabel }, [title])
+  ])
+
   return h(Fragment, [
     i > 0 && h(Fragment, [els.dot(isActive), els.dot(isActive)]),
-    h(Clickable, {
-      key,
-      style: styles.button(isActive),
-      onClick: () => onChangeTab(key)
-    }, [
-      i < selectedIndex ?
-        // ugh, why are these so different visually?
-        icon('check-circle', { className: 'is-solid', size: 24 }) :
-        icon('edit', { className: 'is-solid', size: 16, style: { margin: 4 } }),
-      span({ style: styles.buttonLabel }, [title])
+    div({ style: { marginRight: dotSpace } }, [
+      button,
+      i === selectedIndex && els.selectionUnderline
     ])
   ])
 }
@@ -74,7 +91,7 @@ class StepButtons extends Component {
     const selectedIndex = this.tabMap[activeTab]
 
     return div({ style: styles.container }, [
-      ..._.map(([i, { key, title }]) => stepButton({ i, key, title, selectedIndex, onChangeTab }), _.toPairs(tabs))
+      ..._.map(([i, { key, title }]) => stepButton({ i: i * 1, key, title, selectedIndex, onChangeTab }), _.toPairs(tabs))
     ])
   }
 }
