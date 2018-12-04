fdab1e24ac30dfaaaea6bca1682076bf34bb2c4c
Kate Voss <kvoss@broadinstitute.org>
2018-12-04 13:10:31 -0500

    Hide table headers when there is no content

--- src/pages/workspaces/workspace/tools/WorkflowView.js
+++ src/pages/workspaces/workspace/tools/WorkflowView.js
@@ -74,8 +74,8 @@ const ioTask = ({ name }) => _.nth(-2, name.split('.'))
 const ioVariable = ({ name }) => _.nth(-1, name.split('.'))
 const ioType = ({ inputType, outputType }) => (inputType || outputType).match(/(.*?)\??$/)[1] // unify, and strip off trailing '?'
 
-const WorkflowIOTable = ({ which, inputsOutputs, config, errors, onChange, onSetDefaults, suggestions, includeOptionalInputs }) => {
-  const data = _.filter(includeOptionalInputs || which === 'outputs' ? (() => true) : { optional: false }, inputsOutputs[which])
+const WorkflowIOTable = ({ which, inputsOutputs, config, errors, onChange, onSetDefaults, suggestions }) => {
+  const data = inputsOutputs
   return h(AutoSizer, [
     ({ width, height }) => {
       return h(FlexTable, {
@@ -443,6 +443,7 @@ const WorkflowView = _.flow(
       ..._.map(name => `this.${name}`, attributeNames),
       ..._.map(name => `workspace.${name}`, workspaceAttributes)
     ]
+    const filteredData = _.filter(includeOptionalInputs || key === 'outputs' ? (() => true) : { optional: false }, inputsOutputs[key])
 
     return h(Dropzone, {
       accept: '.json',
@@ -463,24 +464,24 @@ const WorkflowView = _.flow(
         div({ style: { whiteSpace: 'pre' } }, ['  |  Drag or click to ']),
         linkButton({ onClick: () => this.uploader.current.open() }, ['upload json'])
       ]),
-      div({ style: { flex: '1 0 500px' } }, [
-        h(WorkflowIOTable, {
-          which: key,
-          includeOptionalInputs,
-          inputsOutputs,
-          config: modifiedConfig,
-          errors,
-          onChange: canCompute ? ((name, v) => this.setState(_.set(['modifiedConfig', key, name], v))) : undefined,
-          onSetDefaults: canCompute && key === 'outputs' ? () => {
-            this.setState(_.update(['modifiedConfig', 'outputs'], _.flow(
-              _.toPairs,
-              _.map(([k, v]) => [k, v || `this.${_.last(k.split('.'))}`]),
-              _.fromPairs
-            )))
-          } : undefined,
-          suggestions
-        })
-      ])
+      filteredData.length !== 0 ?
+        div({ style: { flex: '1 0 500px' } }, [
+          h(WorkflowIOTable, {
+            which: key,
+            inputsOutputs: filteredData,
+            config: modifiedConfig,
+            errors,
+            onChange: canCompute ? ((name, v) => this.setState(_.set(['modifiedConfig', key, name], v))) : undefined,
+            onSetDefaults: canCompute && key === 'outputs' ? () => {
+              this.setState(_.update(['modifiedConfig', 'outputs'], _.flow(
+                _.toPairs,
+                _.map(([k, v]) => [k, v || `this.${_.last(k.split('.'))}`]),
+                _.fromPairs
+              )))
+            } : undefined,
+            suggestions
+          })
+        ]) : undefined
     ])
   }
 
