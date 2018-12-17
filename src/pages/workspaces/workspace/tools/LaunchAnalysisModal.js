f86c32e6b6c18c37186362bb32f2f27428d81ebb
Matt Putnam <putnam@broadinstitute.org>
2018-12-17 16:49:18 -0500

    launch logic

--- src/pages/workspaces/workspace/tools/LaunchAnalysisModal.js
+++ src/pages/workspaces/workspace/tools/LaunchAnalysisModal.js
@@ -6,6 +6,7 @@ import Modal from 'src/components/Modal'
 import { ajaxCaller } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
 import { Component } from 'src/libs/wrapped-components'
+import EntitySelectionType from 'src/pages/workspaces/workspace/tools/EntitySelectionType'
 
 
 export default ajaxCaller(class LaunchAnalysisModal extends Component {
@@ -19,7 +20,7 @@ export default ajaxCaller(class LaunchAnalysisModal extends Component {
       showCancel: false,
       okButton: !!launchError && buttonPrimary({ onClick: onDismiss }, ['OK'])
     }, [
-      message && div([message, spinner()]),
+      message && div([spinner({ style: { marginRight: '0.5rem' } }), message]),
       launchError && div({ style: { color: colors.red[0] } }, [launchError])
     ])
   }
@@ -27,50 +28,71 @@ export default ajaxCaller(class LaunchAnalysisModal extends Component {
   async componentDidMount() {
     const {
       workspaceId: { namespace, name },
-      processSingle, processAllRows, selectedEntities, newSetName,
-      config: { namespace: configNamespace, name: configName, rootEntityType },
+      processSingle, entitySelectionModel: { type, selectedEntities },
+      config: { rootEntityType },
+      ajax: { Workspaces }
+    } = this.props
+
+    if (processSingle) {
+      this.launch()
+    } else if (type === EntitySelectionType.processAll) {
+      this.setState({ message: 'Fetching data...' })
+      const entities = _.map('name', await Workspaces.workspace(namespace, name).entitiesOfType(rootEntityType))
+      this.createSetAndLaunch(entities)
+    } else if (type === EntitySelectionType.chooseRows) {
+      const entities = _.keys(selectedEntities)
+      this.createSetAndLaunch(entities)
+    } else if (type === EntitySelectionType.chooseExisting) {
+      const { entityType, name } = selectedEntities
+      this.launch(entityType, name, `this.${rootEntityType}s`)
+    }
+  }
+
+  async createSetAndLaunch(entities) {
+    const {
+      workspaceId: { namespace, name },
+      entitySelectionModel: { newSetName },
+      config: { rootEntityType },
+      ajax: { Workspaces }
+    } = this.props
+
+    const setType = `${rootEntityType}_set`
+
+    this.setState({ message: 'Creating data set...' })
+    const newSet = {
+      name: newSetName,
+      entityType: setType,
+      attributes: {
+        [`${rootEntityType}s`]: {
+          itemsType: 'EntityReference',
+          items: _.map(entityName => ({ entityName, entityType: rootEntityType }), entities)
+        }
+      }
+    }
+
+    try {
+      await Workspaces.workspace(namespace, name).createEntity(newSet)
+    } catch (error) {
+      this.setState({ launchError: await error.text(), message: undefined })
+      return
+    }
+
+    await this.launch(setType, newSetName, `this.${rootEntityType}s`)
+  }
+
+  async launch(entityType, entityName, expression) {
+    const {
+      workspaceId: { namespace, name },
+      config: { namespace: configNamespace, name: configName },
       onSuccess,
       ajax: { Workspaces }
     } = this.props
 
-    const workspace = Workspaces.workspace(namespace, name)
-
-    let entityType = undefined
-    let entityName = undefined
-
-    if (!processSingle) {
-      entityType = rootEntityType
-      entityName = newSetName
-
-      let entities = []
-
-      if (processAllRows) {
-        this.setState({ message: 'Fetching data...' })
-        entities = _.map('name', await workspace.entitiesOfType(rootEntityType))
-      } else {
-        entities = selectedEntities
-      }
-
-      this.setState({ message: 'Creating data set...' })
-      const newSet = {
-        name: newSetName,
-        entityType: rootEntityType,
-        attributes: {
-          [`${rootEntityType}s`]: {
-            itemsType: 'EntityReference',
-            items: entities
-          }
-        }
-      }
-
-      await workspace.createEntity(newSet)
-    }
-
     try {
       this.setState({ message: 'Launching analysis...' })
 
-      const { submissionId } = await workspace.methodConfig(configNamespace, configName).launch({
-        entityType, entityName, useCallCache: true
+      const { submissionId } = await Workspaces.workspace(namespace, name).methodConfig(configNamespace, configName).launch({
+        entityType, entityName, expression, useCallCache: true
       })
       onSuccess(submissionId)
     } catch (error) {
