866c4438b3954c43c541a9f38c27bd3fa116828f
Kate Voss <kvoss@broadinstitute.org>
2019-01-16 13:35:05 -0500

    Fixing whitelist user email

--- src/data/prod-whitelist.js
+++ src/data/prod-whitelist.js
@@ -433,5 +433,6 @@ export default [
   '038ea3e1aeef0bc4801419eafc4fe885',
   '25ad8da29408d37383ea0a9bea384a5b',
   '0bfdcbfd818a7425c42a3bee1fccedde',
-  '18aba63c0155b6986cf29c802be2392a'
+  '18aba63c0155b6986cf29c802be2392a',
+  'cf02f00b038c25ea874f597d8031a9b3'
 ]
