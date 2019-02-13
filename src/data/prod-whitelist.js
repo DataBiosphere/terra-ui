021316ce18172f9933614630f495d5b7d2075e28
Kate Voss <kvoss@broadinstitute.org>
2019-02-13 16:29:28 -0500

    Whitelist add (#1191)

--- src/data/prod-whitelist.js
+++ src/data/prod-whitelist.js
@@ -449,5 +449,6 @@ export default [
   '55f8c6401e880386e164a325dabdfa73',
   'd8645cb2ceae041c7b5bd023c2a963bd',
   '06e179ae7d11ae21fbd47059caef8b74',
-  'e5b6381f377b471e4c512cd9b4aab2ff'
+  'e5b6381f377b471e4c512cd9b4aab2ff',
+  '7031c4df07dbd92fe2ed0ae0bf849b3d'
 ]
