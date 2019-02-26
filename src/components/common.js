4a29a1cf8975dd9ba2840e7b88cc9e57571fe695
Brian Reilly <breilly@broadinstitute.org>
2019-02-25 20:17:39 -0500

    New data selection (#1175)
    * Squashed commits from mp-data-select
    
    * post-rebase integration
    
    * Moved entity type selection back to main part of page.
    Moved data selection to a modal.
    
    * Added launch confirmation dialog
    
    * Refactored for clarity around state used for UI only.
    Fixed issue with clearing value of Select component.
    
    * Fixes selecting sets as root entity type
    
    * Only ask for new set name if a new set is needed (>1 row selected)
    
    * Show number of rows when all entities are selected
    
    * Use functions with Utils.cond to avoid eager evaluation
    
    * Removed commented-out stuff after determining that it's not needed

--- src/components/common.js
+++ src/components/common.js
@@ -91,7 +91,8 @@ export const search = function({ wrapperProps, inputProps }) {
           border: 'none', outline: 'none',
           flexGrow: 1,
           verticalAlign: 'bottom', marginLeft: '1rem',
-          fontSize: '1rem'
+          fontSize: '1rem',
+          backgroundColor: 'transparent'
         }
       }, inputProps))
     ])
@@ -250,7 +251,7 @@ export const Select = ({ value, options, ...props }) => {
       multiValueRemove: base => _.merge(base, { ':hover': { backgroundColor: 'unset' } })
     },
     getOptionLabel: ({ value, label }) => label || value.toString(),
-    value: newValue,
+    value: newValue || null, // need null instead of undefined to clear the select
     options: newOptions
   }, props))
 }
