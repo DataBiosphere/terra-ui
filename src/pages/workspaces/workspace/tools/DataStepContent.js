c48da92ca67ae11cc41a6a4baa0d9e0cbea9cb1d
Brian Reilly <breilly@broadinstitute.org>
2019-02-25 20:17:39 -0500

    New data selection (#1175)
    * Squashed commits from mp-data-select
    
    * post-rebase integration
    
    * Moved entity type selection back to main part of page.
    Moved data selection to a modal.
    
    * Added launch confirmation dialog
    
    * Refactored for clarity around state used for UI only.
    Fixed issue with clearing value of Select component.
    
    * Fixes selecting sets as root entity type
    
    * Only ask for new set name if a new set is needed (>1 row selected)
    
    * Show number of rows when all entities are selected
    
    * Use functions with Utils.cond to avoid eager evaluation
    
    * Removed commented-out stuff after determining that it's not needed

---
+++ src/pages/workspaces/workspace/tools/DataStepContent.js
@@ -0,0 +1,134 @@
+import _ from 'lodash/fp'
+import PropTypes from 'prop-types'
+import { Fragment } from 'react'
+import { div, h, span } from 'react-hyperscript-helpers'
+import { buttonPrimary, RadioButton } from 'src/components/common'
+import DataTable from 'src/components/DataTable'
+import { textInput } from 'src/components/input'
+import Modal from 'src/components/Modal'
+import * as Style from 'src/libs/style'
+import { Component } from 'src/libs/wrapped-components'
+import EntitySelectionType from 'src/pages/workspaces/workspace/tools/EntitySelectionType'
+
+
+export default class DataStepContent extends Component {
+  static propTypes = {
+    entityMetadata: PropTypes.objectOf(PropTypes.shape({
+      count: PropTypes.number.isRequired
+    })).isRequired,
+    entitySelectionModel: PropTypes.shape({
+      newSetName: PropTypes.string.isRequired,
+      selectedElements: PropTypes.array,
+      type: PropTypes.oneOf(_.values(EntitySelectionType))
+    }),
+    onDismiss: PropTypes.func,
+    onSuccess: PropTypes.func,
+    rootEntityType: PropTypes.string,
+    workspaceId: PropTypes.object
+  }
+
+  constructor(props) {
+    super(props)
+    const { rootEntityType, entitySelectionModel } = props
+    this.state = { rootEntityType, entitySelectionModel }
+  }
+
+  setEntitySelectionModel(modelUpdates) {
+    this.setState(({ entitySelectionModel }) => {
+      return { entitySelectionModel: { ...entitySelectionModel, ...modelUpdates } }
+    })
+  }
+
+  isValidSelectionModel() {
+    const { entitySelectionModel } = this.state
+    const { newSetName, selectedEntities, type } = entitySelectionModel
+    const { entityType, name } = selectedEntities
+    return (type === EntitySelectionType.processAll ||
+      (type === EntitySelectionType.processFromSet && !!entityType && !!name) ||
+      (_.size(selectedEntities) > 0 && !!newSetName))
+  }
+
+  render() {
+    const {
+      onDismiss, onSuccess,
+      workspaceId, entityMetadata
+    } = this.props
+    const { entitySelectionModel, entitySelectionModel: { type, selectedEntities, newSetName }, rootEntityType } = this.state
+
+    const count = rootEntityType && entityMetadata[rootEntityType].count
+
+    const isSet = _.endsWith('_set', rootEntityType)
+    const setType = `${rootEntityType}_set`
+    const hasSet = _.has(setType, entityMetadata)
+
+    return h(Modal, {
+      okButton: buttonPrimary({
+        disabled: !this.isValidSelectionModel(),
+        onClick: () => onSuccess(entitySelectionModel)
+      }, 'OK'),
+      onDismiss: onDismiss,
+      width: 'calc(100% - 2rem)'
+    }, [
+      div({ style: { ...Style.elements.sectionHeader, marginBottom: '1rem' } }, [`Select ${rootEntityType}s to process`]),
+      rootEntityType && div({
+        style: {
+          padding: '1rem 0.5rem', lineHeight: '1.5rem'
+        }
+      }, [
+        !isSet && h(Fragment, [
+          div([
+            h(RadioButton, {
+              text: `Process all ${count} rows`,
+              checked: type === EntitySelectionType.processAll,
+              onChange: () => this.setEntitySelectionModel({ type: EntitySelectionType.processAll, selectedEntities: {} }),
+              labelStyle: { marginLeft: '0.75rem' }
+            })
+          ]),
+          hasSet && div([
+            h(RadioButton, {
+              text: 'Choose an existing set',
+              checked: type === EntitySelectionType.processFromSet,
+              onChange: () => this.setEntitySelectionModel({ type: EntitySelectionType.processFromSet, selectedEntities: {} }),
+              labelStyle: { marginLeft: '0.75rem' }
+            })
+          ]),
+          div([
+            h(RadioButton, {
+              text: 'Choose specific rows to process',
+              checked: type === EntitySelectionType.chooseRows,
+              onChange: () => this.setEntitySelectionModel({ type: EntitySelectionType.chooseRows, selectedEntities: {} }),
+              labelStyle: { marginLeft: '0.75rem' }
+            })
+          ])
+        ]),
+        type !== EntitySelectionType.processAll && div({
+          style: {
+            display: 'flex', flexDirection: 'column',
+            height: 500, marginTop: '1rem'
+          }
+        }, [
+          h(DataTable, {
+            key: type,
+            entityType: type === EntitySelectionType.processFromSet ? setType : rootEntityType,
+            entityMetadata, workspaceId,
+            selectionModel: {
+              type: (isSet || type === EntitySelectionType.processFromSet) ? 'single' : 'multiple',
+              selected: selectedEntities, setSelected: e => this.setEntitySelectionModel({ selectedEntities: e })
+            }
+          })
+        ]),
+        (type === EntitySelectionType.processAll ||
+        (type === EntitySelectionType.chooseRows && _.size(selectedEntities) > 1)) && div({
+          style: { marginTop: '1rem' }
+        }, [
+          span(['Selected rows will be saved as a new set named:']),
+          textInput({
+            style: { width: 500, marginLeft: '0.25rem' },
+            value: newSetName,
+            onChange: e => this.setEntitySelectionModel({ newSetName: e.target.value })
+          })
+        ])
+      ])
+    ])
+  }
+}
---
+++ src/pages/workspaces/workspace/tools/EntitySelectionType.js
@@ -0,0 +1,6 @@
+export default Object.freeze({
+  processAll: 'process all',
+  processFromSet: 'choose existing',
+  chooseRows: 'choose rows',
+  chooseSet: 'choose set'
+})
