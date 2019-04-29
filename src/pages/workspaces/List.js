f0024de3126f78f84cfb2bb36abe0a6b847a875a
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2019-04-29 09:58:55 -0400

    filter workspaces by submission status

--- src/pages/workspaces/List.js
+++ src/pages/workspaces/List.js
@@ -249,6 +249,7 @@ export const WorkspaceList = _.flow(
       sharingWorkspaceId: undefined,
       accessLevelsFilter: [],
       projectsFilter: [],
+      submissionsFilter: [],
       includePublic: false,
       ...StateHistory.get()
     }
@@ -261,7 +262,7 @@ export const WorkspaceList = _.flow(
 
   render() {
     const { workspaces, loadingWorkspaces, refreshWorkspaces, listView, viewToggleButtons } = this.props
-    const { filter, creatingNewWorkspace, cloningWorkspaceId, deletingWorkspaceId, sharingWorkspaceId, accessLevelsFilter, projectsFilter, includePublic } = this.state
+    const { filter, creatingNewWorkspace, cloningWorkspaceId, deletingWorkspaceId, sharingWorkspaceId, accessLevelsFilter, projectsFilter, submissionsFilter, includePublic } = this.state
     const initialFiltered = _.filter(ws => {
       const { workspace: { namespace, name } } = ws
       return Utils.textMatch(filter, `${namespace}/${name}`) && (includePublic || !ws.public || Utils.canWrite(ws.accessLevel))
@@ -275,7 +276,8 @@ export const WorkspaceList = _.flow(
 
     const data = _.flow(
       _.filter(ws => (_.isEmpty(accessLevelsFilter) || accessLevelsFilter.includes(ws.accessLevel)) &&
-        (_.isEmpty(projectsFilter) || projectsFilter.includes(ws.workspace.namespace))),
+        (_.isEmpty(projectsFilter) || projectsFilter.includes(ws.workspace.namespace)) &&
+        (_.isEmpty(submissionsFilter) || submissionsFilter.includes(workspaceSubmissionStatus(ws)))),
       _.sortBy('workspace.name')
     )(initialFiltered)
 
@@ -291,15 +293,9 @@ export const WorkspaceList = _.flow(
     return h(Fragment, [
       h(TopBar, { title: 'Workspaces' }, [h(WsSearch, { onChange: v => this.setState({ filter: v }) })]),
       h(PageBox, { style: { position: 'relative' } }, [
-        div({ style: { display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginBottom: '1rem' } }, [
+        div({ style: { display: 'flex', alignItems: 'center', marginBottom: '1rem' } }, [
           div({ style: { ...Style.elements.sectionHeader, textTransform: 'uppercase' } }, ['Workspaces']),
-          div({ style: { marginLeft: 'auto' } }, [
-            h(LabeledCheckbox, {
-              checked: includePublic === true,
-              onChange: v => this.setState({ includePublic: v })
-            }, ' Show public workspaces')
-          ]),
-          div({ style: { marginLeft: '1rem', flex: '0 0 300px' } }, [
+          div({ style: { marginLeft: 'auto', flex: '0 0 300px' } }, [
             h(Select, {
               isClearable: true,
               isMulti: true,
@@ -311,7 +307,7 @@ export const WorkspaceList = _.flow(
               getOptionLabel: ({ value }) => Utils.normalizeLabel(value)
             })
           ]),
-          div({ style: { margin: '0 1rem', flex: '0 0 300px' } }, [
+          div({ style: { marginLeft: '1rem', flex: '0 0 300px' } }, [
             h(Select, {
               isClearable: true,
               isMulti: false,
@@ -324,6 +320,27 @@ export const WorkspaceList = _.flow(
               },
               options: namespaceList
             })
+          ]),
+          div({ style: { marginLeft: '1rem', flex: '0 0 300px' } }, [
+            h(Select, {
+              isClearable: true,
+              isMulti: true,
+              isSearchable: false,
+              placeholder: 'Filter by submission status',
+              value: submissionsFilter,
+              hideSelectedOptions: true,
+              onChange: data => this.setState({ submissionsFilter: _.map('value', data) }),
+              options: ['running', 'success', 'failure'],
+              getOptionLabel: ({ value }) => Utils.normalizeLabel(value)
+            })
+          ])
+        ]),
+        div({ style: { display: 'flex', alignItems: 'center', marginBottom: '1rem' } }, [
+          div({ style: { marginLeft: 'auto', marginRight: '1rem' } }, [
+            h(LabeledCheckbox, {
+              checked: includePublic === true,
+              onChange: v => this.setState({ includePublic: v })
+            }, ' Show public workspaces')
           ]),
           viewToggleButtons
         ]),
@@ -362,7 +379,7 @@ export const WorkspaceList = _.flow(
 
   componentDidUpdate() {
     StateHistory.update(_.pick(
-      ['filter', 'accessLevelsFilter', 'projectsFilter', 'includePublic'],
+      ['filter', 'accessLevelsFilter', 'projectsFilter', 'includePublic', 'submissionsFilter'],
       this.state)
     )
   }
