9b5364dc3f361f09a2e3aefcac36e08f33ee1a24
Isaac Zarsky <zarsky@broadinstitute.org>
2019-05-03 17:43:58 -0400

    Install node before npm when both needed
    (because old npm is installed with node)

--- .hooks/check-engine-light.js
+++ .hooks/check-engine-light.js
@@ -6,18 +6,15 @@ const nodeVersion = process.version
 const nodeCorrect = nodeVersion.startsWith('v10.')
 
 if (!npmCorrect || !nodeCorrect) {
-  console.error('\x1b[1m' /* bold */ + '╔'.padEnd(70, '═') + '╗')
-  if (!npmCorrect) {
-    console.error(`║ Must have npm 6.9, you have ${npmVersion}. To fix:`.padEnd(70) + '║')
-    console.error('║ $ npm install -g npm@6.9'.padEnd(70) + '║')
+  console.error('\x1b[1m' /* bold */ + '╔'.padEnd(80, '═') + '╗')
+  if (nodeCorrect) {
+    console.error(`║ Must have npm 6.9, you have ${npmVersion}. To fix:`.padEnd(80) + '║')
+    console.error('║ $ npm install -g npm@6.9'.padEnd(80) + '║')
+  } else {
+    console.error(`║ Must be running node 10, you have ${nodeVersion}. One way to fix (Mac/Homebrew):`.padEnd(80) + '║')
+    console.error('║ $ brew install node@10; brew link node@10 --force --overwrite'.padEnd(80) + '║')
   }
-  !npmCorrect && !nodeCorrect &&
-  console.error('╠'.padEnd(70, '═') + '╣')
-  if (!nodeCorrect) {
-    console.error(`║ Must be running node 10, you have ${nodeVersion}. One way to fix:`.padEnd(70) + '║')
-    console.error('║ $ brew install node@10; brew link node@10 --force --overwrite'.padEnd(70) + '║')
-  }
-  console.error('╚'.padEnd(70, '═') + '╝')
+  console.error('╚'.padEnd(80, '═') + '╝')
   console.error('\x1b[0m' /* not-bold */)
 
   process.exit(1)
