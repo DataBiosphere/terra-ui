fe720ae4a93903e7fad9236e8289d951df2d8589
Isaac Zarsky <zarsky@broadinstitute.org>
2019-05-03 16:30:08 -0400

    and tell devs too

---
+++ .hooks/check-engine-light.js
@@ -0,0 +1,25 @@
+const { execSync } = require('child_process')
+
+const npmVersion = execSync('npm -v').toString().trim()
+const npmCorrect = npmVersion.startsWith('6.9.')
+const nodeVersion = process.version
+const nodeCorrect = nodeVersion.startsWith('v10.')
+
+if (!npmCorrect || !nodeCorrect) {
+  console.error('\x1b[1m' /* bold */ + '╔'.padEnd(70, '═') + '╗')
+  if (!npmCorrect) {
+    console.error(`║ Must have npm 6.9, you have ${npmVersion}. To fix:`.padEnd(70) + '║')
+    console.error('║ $ npm install -g npm@6.9'.padEnd(70) + '║')
+  }
+  !npmCorrect && !nodeCorrect &&
+  console.error('╠'.padEnd(70, '═') + '╣')
+  if (!nodeCorrect) {
+    console.error(`║ Must be running node 10, you have ${nodeVersion}. One way to fix:`.padEnd(70) + '║')
+    console.error('║ $ brew install node@10; brew link node@10 --force --overwrite'.padEnd(70) + '║')
+  }
+  console.error('╚'.padEnd(70, '═') + '╝')
+  console.error('\x1b[0m' /* not-bold */)
+
+  process.exit(1)
+}
+
