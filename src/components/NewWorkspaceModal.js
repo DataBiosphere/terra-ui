eda8315031669067579127361d797332c4d9a0b7
Kate Voss <kvoss@broadinstitute.org>
2019-05-08 10:07:25 -0400

    PR feedback: make modal into cond

--- src/components/NewWorkspaceModal.js
+++ src/components/NewWorkspaceModal.js
@@ -1,6 +1,6 @@
 import _ from 'lodash/fp'
 import PropTypes from 'prop-types'
-import { Component, Fragment } from 'react'
+import { Component } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
 import { buttonPrimary, link, Select, spinnerOverlay } from 'src/components/common'
 import { icon } from 'src/components/icons'
@@ -59,7 +59,8 @@ export default _.flow(
       description: (cloneWorkspace && cloneWorkspace.workspace.attributes.description) || '',
       groups: [],
       nameModified: false,
-      busy: false,
+      loading: false,
+      creating: false,
       createError: undefined
     }
   }
@@ -72,7 +73,7 @@ export default _.flow(
     const { onSuccess, cloneWorkspace, ajax: { Workspaces } } = this.props
     const { namespace, name, description, groups } = this.state
     try {
-      this.setState({ createError: undefined, busy: true })
+      this.setState({ createError: undefined, creating: true })
       const body = {
         namespace,
         name,
@@ -86,7 +87,7 @@ export default _.flow(
       onSuccess(workspace)
     } catch (error) {
       const { message } = await error.json()
-      this.setState({ createError: message, busy: false })
+      this.setState({ createError: message, creating: false })
     }
   }
 
@@ -100,7 +101,7 @@ export default _.flow(
 
   loadProjectsGroups = _.flow(
     withErrorReporting('Error loading data'),
-    Utils.withBusyState(v => this.setState({ busy: v }))
+    Utils.withBusyState(v => this.setState({ loading: v }))
   )(async () => {
     const { ajax: { Billing, Groups } } = this.props
     const [billingProjects, allGroups] = await Promise.all([
@@ -117,103 +118,116 @@ export default _.flow(
   render() {
     const { onDismiss, cloneWorkspace, authState: { profile } } = this.props
     const { trialState } = profile
-    const { namespace, name, billingProjects, allGroups, groups, description, nameModified, busy, createError } = this.state
+    const { namespace, name, billingProjects, allGroups, groups, description, nameModified, loading, createError, creating } = this.state
     const existingGroups = this.getRequiredGroups()
     const hasBillingProjects = !!billingProjects && !!billingProjects.length
     const hasFreeCredits = trialState === 'Enabled'
     const errors = validate({ namespace, name }, constraints, {
       prettify: v => ({ namespace: 'Billing project', name: 'Name' }[v] || validate.prettify(v))
     })
-    return h(Modal, {
-      title: busy ? '' : !hasBillingProjects ? 'Set up Billing' : cloneWorkspace ? 'Clone a Workspace' : 'Create a New Workspace',
-      onDismiss,
-      showCancel: hasBillingProjects,
-      okButton: busy ? 'Loading...' : hasBillingProjects ? buttonPrimary({
-        disabled: errors,
-        tooltip: Utils.summarizeErrors(errors),
-        onClick: () => this.create()
-      }, cloneWorkspace ? 'Clone Workspace' : 'Create Workspace')
-        : hasFreeCredits ?
-          buttonPrimary({
-            onClick: () => {
-              onDismiss()
-              freeCreditsActive.set(true)
-            }
-          }, 'Get Free Credits')
-          : buttonPrimary({
-            onClick: () => Nav.goToPath('billing')
-          }, 'Go to Billing')
-    }, [
-      !busy && h(Fragment,
-        hasBillingProjects ? [
-          h(RequiredFormLabel, ['Workspace name']),
-          validatedInput({
-            inputProps: {
-              autoFocus: true,
-              placeholder: 'Enter a name',
-              value: name,
-              onChange: e => this.setState({ name: e.target.value, nameModified: true })
-            },
-            error: Utils.summarizeErrors(nameModified && errors && errors.name)
-          }),
-          h(RequiredFormLabel, ['Billing project']),
-          h(Select, {
-            isClearable: false,
-            placeholder: 'Select a billing project',
-            value: namespace,
-            onChange: ({ value }) => this.setState({ namespace: value }),
-            options: _.uniq(_.map('projectName', billingProjects)).sort()
-          }),
-          h(FormLabel, ['Description']),
-          h(TextArea, {
-            style: { height: 100 },
-            placeholder: 'Enter a description',
-            value: description,
-            onChange: v => this.setState({ description: v })
-          }),
-          h(FormLabel, [
-            'Authorization domain',
-            h(InfoBox, [
-              'An authorization domain can only be set when creating a workspace. ',
-              'Once set, it cannot be changed. ',
-              'Any cloned workspace will automatically inherit the authorization domain(s) from the original workspace and cannot be removed. ',
-              link({
-                href: 'https://broadinstitute.zendesk.com/hc/en-us/articles/360026775691-Managing-Data-Privacy-and-Access-with-Authorization-Domains',
-                target: '_blank'
-              }, ['Read more about authorization domains'])
-            ])
-          ]),
-          !!existingGroups.length && div({ style: styles.groupNotice }, [
-            div({ style: { marginBottom: '0.2rem' } }, ['Inherited groups:']),
-            ...existingGroups.join(', ')
-          ]),
-          h(Select, {
-            isClearable: false,
-            isMulti: true,
-            placeholder: 'Select groups',
-            disabled: !allGroups || !billingProjects,
-            value: groups,
-            onChange: data => this.setState({ groups: _.map('value', data) }),
-            options: _.difference(_.uniq(_.map('groupName', allGroups)), existingGroups).sort()
-          })
-        ] : [
-          div({ style: { color: colors.orange[0] } }, [
-            icon('error', { size: 16, style: { marginRight: '0.5rem' } }),
-            'You need a billing project to ', cloneWorkspace ? 'clone a' : 'create a new', ' workspace.'
-          ]),
-          div({ style: { marginTop: '0.5rem' } }, [
-            hasFreeCredits &&
-            div({ style: { fontWeight: 500, marginBottom: '0.5rem' } }, ['You have $300 in ',
-              link({
-                href: 'https://broadinstitute.zendesk.com/hc/en-us/articles/360022704371-Getting-started-with-Terra#free-credits-signup',
-                target: '_blank'
-              }, 'free credits'), ' available!'])
+
+    return Utils.cond(
+      [loading, spinnerOverlay],
+      [hasBillingProjects, () => h(Modal, {
+        title: cloneWorkspace ? 'Clone a Workspace' : 'Create a New Workspace',
+        onDismiss,
+        okButton: buttonPrimary({
+          disabled: errors,
+          tooltip: Utils.summarizeErrors(errors),
+          onClick: () => this.create()
+        }, cloneWorkspace ? 'Clone Workspace' : 'Create Workspace')
+      }, [
+        h(RequiredFormLabel, ['Workspace name']),
+        validatedInput({
+          inputProps: {
+            autoFocus: true,
+            placeholder: 'Enter a name',
+            value: name,
+            onChange: e => this.setState({ name: e.target.value, nameModified: true })
+          },
+          error: Utils.summarizeErrors(nameModified && errors && errors.name)
+        }),
+        h(RequiredFormLabel, ['Billing project']),
+        h(Select, {
+          isClearable: false,
+          placeholder: 'Select a billing project',
+          value: namespace,
+          onChange: ({ value }) => this.setState({ namespace: value }),
+          options: _.uniq(_.map('projectName', billingProjects)).sort()
+        }),
+        h(FormLabel, ['Description']),
+        h(TextArea, {
+          style: { height: 100 },
+          placeholder: 'Enter a description',
+          value: description,
+          onChange: v => this.setState({ description: v })
+        }),
+        h(FormLabel, [
+          'Authorization domain',
+          h(InfoBox, [
+            'An authorization domain can only be set when creating a workspace. ',
+            'Once set, it cannot be changed. ',
+            'Any cloned workspace will automatically inherit the authorization domain(s) from the original workspace and cannot be removed. ',
+            link({
+              href: 'https://broadinstitute.zendesk.com/hc/en-us/articles/360026775691-Managing-Data-Privacy-and-Access-with-Authorization-Domains',
+              target: '_blank'
+            }, ['Read more about authorization domains'])
           ])
         ]),
-      createError && div({
-        style: { marginTop: '1rem', color: colors.red[0] }
-      }, [createError]),
-      busy && spinnerOverlay
-    ])
+        !!existingGroups.length && div({ style: styles.groupNotice }, [
+          div({ style: { marginBottom: '0.2rem' } }, ['Inherited groups:']),
+          ...existingGroups.join(', ')
+        ]),
+        h(Select, {
+          isClearable: false,
+          isMulti: true,
+          placeholder: 'Select groups',
+          disabled: !allGroups || !billingProjects,
+          value: groups,
+          onChange: data => this.setState({ groups: _.map('value', data) }),
+          options: _.difference(_.uniq(_.map('groupName', allGroups)), existingGroups).sort()
+        }),
+        createError && div({
+          style: { marginTop: '1rem', color: colors.red[0] }
+        }, [createError]),
+        creating && spinnerOverlay
+      ])],
+      [hasFreeCredits, () => h(Modal, {
+        title: 'Set up Billing',
+        onDismiss,
+        showCancel: false,
+        okButton: buttonPrimary({
+          onClick: () => {
+            onDismiss()
+            freeCreditsActive.set(true)
+          }
+        }, 'Get Free Credits')
+      }, [
+        div({ style: { color: colors.orange[0] } }, [
+          icon('error', { size: 16, style: { marginRight: '0.5rem' } }),
+          'You need a billing project to ', cloneWorkspace ? 'clone a' : 'create a new', ' workspace.'
+        ]),
+        div({ style: { marginTop: '0.5rem', fontWeight: 500, marginBottom: '0.5rem' } }, [
+          'You have $300 in ',
+          link({
+            href: 'https://broadinstitute.zendesk.com/hc/en-us/articles/360022704371-Getting-started-with-Terra#free-credits-signup',
+            target: '_blank'
+          }, 'free credits'), ' available!'
+        ])
+      ])],
+      () => h(Modal, {
+        title: 'Set up Billing',
+        onDismiss,
+        showCancel: false,
+        okButton: buttonPrimary({
+          onClick: () => Nav.goToPath('billing')
+        }, 'Go to Billing')
+      }, [
+        div({ style: { color: colors.orange[0] } }, [
+          icon('error', { size: 16, style: { marginRight: '0.5rem' } }),
+          'You need a billing project to ', cloneWorkspace ? 'clone a' : 'create a new', ' workspace.'
+        ])
+      ])
+    )
   }
 })
