b2987eb063da7269d3c48b75f8d272530161a163
Brian Reilly <breilly@broadinstitute.org>
2019-05-09 16:11:34 -0400

    Request access to auth domain groups (minus special instructions)

---
+++ src/pages/workspaces/workspace/RequestAccessModal.js
@@ -0,0 +1,95 @@
+import _ from 'lodash/fp'
+import { useState } from 'react'
+import { div, h, table, tbody, td, th, thead, tr } from 'react-hyperscript-helpers'
+import { buttonPrimary } from 'src/components/common'
+import { centeredSpinner } from 'src/components/icons'
+import Modal from 'src/components/Modal'
+import { Ajax, useCancellation } from 'src/libs/ajax'
+import { withErrorReporting } from 'src/libs/error'
+import { useOnMount, withBusyState } from 'src/libs/utils'
+
+
+export const RequestAccessModal = ({ onDismiss, workspace }) => {
+  const [groups, setGroups] = useState([])
+  const [accessInstructions, setAccessInstructions] = useState([])
+  const [loadingGroups, setLoadingGroups] = useState(false)
+  const [loadingAccessInstructions, setLoadingAccessInstructions] = useState(false)
+  const signal = useCancellation()
+
+  const { Groups, Workspaces } = Ajax(signal)
+
+  const fetchGroups = _.flow(
+    withBusyState(setLoadingGroups),
+    withErrorReporting('Error loading groups')
+  )(async () => {
+    setGroups(await Groups.list())
+  })
+
+  const fetchAccessInstructions = _.flow(
+    withBusyState(setLoadingAccessInstructions),
+    withErrorReporting('Error loading groups')
+  )(async () => {
+    const i = await Workspaces.workspace(workspace.workspace.namespace, workspace.workspace.name).accessInstructions()
+    console.log(i)
+    setAccessInstructions(i)
+  })
+
+  useOnMount(() => {
+    fetchGroups()
+    fetchAccessInstructions()
+  })
+
+  const loading = loadingGroups || loadingAccessInstructions
+  const groupNames = _.map('groupName', groups)
+  const authDomain = workspace.workspace.authorizationDomain
+  return h(Modal, {
+    title: 'Request Access',
+    width: '40rem',
+    onDismiss
+  }, [
+    div(['You cannot access this Workspace because it contains restricted data. You need permission from the admin(s) of all of the Groups in the Authorization Domain protecting the workspace.']),
+    loading ?
+      centeredSpinner({ size: 32 }) :
+      table({ style: { margin: '1rem', width: '100%' } }, [
+        thead([
+          tr({ style: { height: '2rem' } }, [th({ style: { textAlign: 'left' } }, ['Group Name']), th({ style: { textAlign: 'left' } }, ['Access'])])
+        ]),
+        tbody(
+          _.map(({ membersGroupName: groupName }) => tr({ style: { height: '2rem' } }, [
+            td([groupName]),
+            td([
+              _.includes(groupName, groupNames) ?
+                'Yes' :
+                h(RequestAccessButton, {
+                  groupName,
+                  instructions: accessInstructions[groupName]
+                })
+            ])
+          ]), authDomain)
+        )
+      ])
+  ])
+}
+
+const RequestAccessButton = ({ groupName, instructions }) => {
+  const [requesting, setRequesting] = useState(false)
+  const [requested, setRequested] = useState(false)
+  const signal = useCancellation()
+
+  const { Groups } = Ajax(signal)
+
+  const requestAccess = _.flow(
+    withBusyState(setRequesting),
+    withErrorReporting('Error requesting group access')
+  )(async () => {
+    await Groups.group(groupName).requestAccess()
+  })
+
+  return buttonPrimary({
+    disabled: requesting || requested,
+    onClick: async () => {
+      await requestAccess()
+      setRequested(true)
+    }
+  }, [requested ? 'Request Sent' : 'Request Access'])
+}
