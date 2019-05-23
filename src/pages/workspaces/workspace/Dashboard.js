ded009d8eeb8b84e23537050df93150b97607e49
Kate Voss <kvoss@broadinstitute.org>
2019-05-23 14:50:44 -0400

    Add tags to workspace (#1599)

--- src/pages/workspaces/workspace/Dashboard.js
+++ src/pages/workspaces/workspace/Dashboard.js
@@ -1,3 +1,4 @@
+import debouncePromise from 'debounce-promise'
 import _ from 'lodash/fp'
 import { Fragment } from 'react'
 import { div, h, span } from 'react-hyperscript-helpers'
@@ -5,8 +6,9 @@ import SimpleMDE from 'react-simplemde-editor'
 import 'easymde/dist/easymde.min.css'
 import * as breadcrumbs from 'src/components/breadcrumbs'
 import { buttonPrimary, buttonSecondary, link, linkButton, spinnerOverlay } from 'src/components/common'
-import { icon } from 'src/components/icons'
+import { icon, spinner } from 'src/components/icons'
 import { Markdown } from 'src/components/Markdown'
+import { InfoBox } from 'src/components/PopupTrigger'
 import { SimpleTable } from 'src/components/table'
 import TooltipTrigger from 'src/components/TooltipTrigger'
 import { displayConsentCodes, displayLibraryAttributes } from 'src/data/workspace-attributes'
@@ -14,11 +16,13 @@ import { ajaxCaller } from 'src/libs/ajax'
 import { bucketBrowserUrl } from 'src/libs/auth'
 import colors from 'src/libs/colors'
 import { reportError, withErrorReporting } from 'src/libs/error'
+import { getAppName } from 'src/libs/logos'
 import * as Nav from 'src/libs/nav'
 import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
 import { Component } from 'src/libs/wrapped-components'
 import { wrapWorkspace } from 'src/pages/workspaces/workspace/WorkspaceContainer'
+import AsyncCreatableSelect from 'react-select/lib/AsyncCreatable'
 
 
 const styles = {
@@ -48,6 +52,11 @@ const styles = {
   label: {
     ...Style.elements.sectionHeader,
     marginTop: '1rem', marginBottom: '0.25rem'
+  },
+  tag: {
+    padding: '0.25rem', margin: '0.15rem',
+    backgroundColor: colors.grayBlue[3], borderRadius: 10,
+    overflow: 'hidden', wordWrap: 'break-word'
   }
 }
 
@@ -100,7 +109,8 @@ export const WorkspaceDashboard = _.flow(
       submissionsCount: undefined,
       storageCostEstimate: undefined,
       editDescription: undefined,
-      saving: false
+      saving: false,
+      busy: false
     }
   }
 
@@ -108,6 +118,7 @@ export const WorkspaceDashboard = _.flow(
     this.loadSubmissionCount()
     this.loadStorageCost()
     this.loadConsent()
+    this.loadWsTags()
   }
 
   loadSubmissionCount = withErrorReporting('Error loading data', async () => {
@@ -139,10 +150,43 @@ export const WorkspaceDashboard = _.flow(
           case 404:
             this.setState({ consentStatus: `Structured Data Use Limitations are not available for ${orspId}` })
             break
-          default: throw error
+          default:
+            throw error
         }
       }
     }
+  })
+
+  getTagSuggestions = debouncePromise(withErrorReporting('Error loading tags', async text => {
+    const { ajax: { Workspaces } } = this.props
+    if (text.length > 2) {
+      return _.map(({ tag, count }) => {
+        return { value: tag, label: `${tag} (${count})` }
+      }, _.take(10, await Workspaces.getTags(text)))
+    } else {
+      return []
+    }
+  }), 250)
+
+  loadWsTags = withErrorReporting('Error loading workspace tags', async () => {
+    const { ajax: { Workspaces }, namespace, name } = this.props
+    this.setState({ tagsList: await Workspaces.workspace(namespace, name).getTags() })
+  })
+
+  addTag = _.flow(
+    withErrorReporting('Error adding tag'),
+    Utils.withBusyState(v => this.setState({ busy: v }))
+  )(async tag => {
+    const { ajax: { Workspaces }, namespace, name } = this.props
+    this.setState({ tagsList: await Workspaces.workspace(namespace, name).addTag(tag) })
+  })
+
+  deleteTag = _.flow(
+    withErrorReporting('Error removing tag'),
+    Utils.withBusyState(v => this.setState({ busy: v }))
+  )(async tag => {
+    const { ajax: { Workspaces }, namespace, name } = this.props
+    this.setState({ tagsList: await Workspaces.workspace(namespace, name).deleteTag(tag) })
   })
 
   async save() {
@@ -170,7 +214,7 @@ export const WorkspaceDashboard = _.flow(
         }
       }
     } = this.props
-    const { submissionsCount, storageCostEstimate, editDescription, saving, consentStatus } = this.state
+    const { submissionsCount, storageCostEstimate, editDescription, saving, consentStatus, tagsList, busy } = this.state
     const isEditing = _.isString(editDescription)
 
     return div({ style: { flex: 1, display: 'flex' } }, [
@@ -237,6 +281,37 @@ export const WorkspaceDashboard = _.flow(
           Utils.canWrite(accessLevel) && h(InfoTile, { title: 'Est. $/month' }, [
             storageCostEstimate
           ])
+        ]),
+        div({ style: styles.header }, [
+          'Tags',
+          h(InfoBox, { style: { margin: '-0.1rem 0 0 0.25rem' } }, [
+            `${getAppName()} is not intended to host personally identifiable information. Do not use any patient identifier including name,
+          social security number, or medical record number.`
+          ])
+        ]),
+        div({ style: { marginBottom: '0.5rem' } }, [
+          h(AsyncCreatableSelect, {
+            value: null,
+            noOptionsMessage: () => 'Enter at least 3 characters to search',
+            allowCreateWhileLoading: true,
+            placeholder: 'Add a tag',
+            onChange: ({ value }) => this.addTag(value),
+            styles: { option: base => ({ ...base, wordWrap: 'break-word' }) },
+            loadOptions: this.getTagSuggestions
+          })
+        ]),
+        div({ style: { display: 'flex', flexWrap: 'wrap' } }, [
+          _.map(tag => {
+            return span({ key: tag, style: styles.tag }, [
+              tag,
+              linkButton({
+                tooltip: 'Remove tag',
+                onClick: () => this.deleteTag(tag),
+                style: { marginLeft: '0.25rem' }
+              }, [icon('times', { size: 18 })])
+            ])
+          }, tagsList),
+          busy && spinner({ style: { marginTop: '0.5rem' } })
         ]),
         !_.isEmpty(authorizationDomain) && h(Fragment, [
           div({ style: styles.header }, ['Authorization Domain']),
