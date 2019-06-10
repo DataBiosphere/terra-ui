57edcabc5319374feff9f716e5b61490153dd21f
Isaac Zarsky <zarsky@broadinstitute.org>
2019-06-10 16:57:49 -0400

    missed a character in reference data fix

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -76,7 +76,7 @@ const getReferenceData = _.flow(
   _.toPairs,
   _.filter(([key]) => key.startsWith('referenceData_')),
   _.map(([k, value]) => {
-    const [, datum, key] = /referenceData_([^-]+)_(.+)/.exec(k)
+    const [, datum, key] = /referenceData_([^_]+)_(.+)/.exec(k)
     return { datum, key, value }
   }),
   _.groupBy('datum')
