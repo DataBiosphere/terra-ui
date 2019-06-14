688fc76dc59ef4019d97d4850527ef2f5f436bdf
Kate Voss <kvoss@broadinstitute.org>
2019-06-14 10:48:42 -0500

    Words and colors feedback

--- src/components/RequesterPaysModal.js
+++ src/components/RequesterPaysModal.js
@@ -2,32 +2,23 @@ import { div, h } from 'react-hyperscript-helpers'
 import { buttonPrimary, link, spinnerOverlay } from 'src/components/common'
 import { icon } from 'src/components/icons'
 import Modal from 'src/components/Modal'
-import colors from 'src/libs/colors'
 import * as Nav from 'src/libs/nav'
 import { freeCreditsActive } from 'src/libs/state'
 import { Component } from 'src/libs/wrapped-components'
 import * as Utils from 'src/libs/utils'
 
 
-const requesterPaysMessage = div({ style: { color: colors.warning() } }, [
-  icon('error-standard', { size: 16, style: { marginRight: '0.5rem' } }),
-  'You need a billing project to interact with this workspace because the Google Cloud storage has requester pays enabled. ',
-  link({
-    href: '', // add link to documentation
-    ...Utils.newTabLinkProps
-  }, 'Click here for information on Requester Pays.')
-])
-
 export default class RequesterPaysModal extends Component {
   render() {
-    const hasFreeCredits = true
+    const hasFreeCredits = false
     const loading = false
+    const onDismiss = this.props
 
     return Utils.cond(
       [loading, spinnerOverlay],
       [hasFreeCredits, () => h(Modal, {
         title: 'Set up Billing',
-        //onDismiss,
+        onDismiss,
         showCancel: false,
         okButton: buttonPrimary({
           onClick: () => {
@@ -36,23 +27,42 @@ export default class RequesterPaysModal extends Component {
           }
         }, 'Get Free Credits')
       }, [
-        div({ style: { marginTop: '0.5rem', fontWeight: 500, marginBottom: '0.5rem' } }, [
-          requesterPaysMessage,
-          'You have $300 in ',
+        div('To view or download data in this workspace, please set up a billing project.'),
+        div([
+          'You have $300 in',
           link({
+            style: { marginLeft: '0.25rem' },
             href: 'https://support.terra.bio/hc/en-us/articles/360027940952',
             ...Utils.newTabLinkProps
-          }, 'free credits'), ' available!'
+          }, [
+            'free credits', icon('pop-out', { style: { margin: '0 0.25rem' }, size: 12 })
+          ]), 'available!'
+        ]),
+        div({ style: { marginTop: '1rem' } }, [
+          link({
+            href: '', // add link to documentation
+            ...Utils.newTabLinkProps
+          }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
         ])
       ])],
       () => h(Modal, {
         title: 'Set up Billing',
+        onDismiss,
         showCancel: false,
-        //onDismiss,
         okButton: buttonPrimary({
-          onClick: () => Nav.goToPath('billing')
+          onClick: () => {
+            Nav.goToPath('billing')
+          }
         }, 'Go to Billing')
-      }, [requesterPaysMessage])
+      }, [
+        div('To view or download data in this workspace, please set up a billing project.'),
+        div([
+          link({
+            href: '', // add link to documentation
+            ...Utils.newTabLinkProps
+          }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
+        ])
+      ])
     )
   }
 }
