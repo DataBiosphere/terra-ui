a5eb46a23d0275d96ccb49bf2a9ee187239b85b6
Kate Voss <kvoss@broadinstitute.org>
2019-06-20 14:37:40 -0600

    Mobbing on requester pays modals
    Co-Authored-By: Pete Santos <petesantos@users.noreply.github.com>
    Co-Authored-By: Brian Reilly <breilly2@users.noreply.github.com>
    Co-Authored-By: Isaac Zarsky <zarsky-broad@users.noreply.github.com>
    Co-Authored-By: Brett Heath-Wlaz <panentheos@users.noreply.github.com>

--- src/components/RequesterPaysModal.js
+++ src/components/RequesterPaysModal.js
@@ -1,70 +1,118 @@
+import * as _ from 'lodash/fp'
+import { useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
-import { buttonPrimary, link, spinnerOverlay } from 'src/components/common'
+import { buttonPrimary, link, Select, spinnerOverlay } from 'src/components/common'
 import { icon } from 'src/components/icons'
 import Modal from 'src/components/Modal'
+import { Ajax, useCancellation } from 'src/libs/ajax'
+import { withErrorReporting } from 'src/libs/error'
+import { RequiredFormLabel } from 'src/libs/forms'
 import * as Nav from 'src/libs/nav'
-import { authStore, freeCreditsActive } from 'src/libs/state'
-import { Component } from 'src/libs/wrapped-components'
+import { authStore, freeCreditsActive, workspaceStore } from 'src/libs/state'
 import * as Utils from 'src/libs/utils'
 
 
-export default Utils.connectAtom(authStore, 'authState')(class RequesterPaysModal extends Component {
-  render() {
-    const { authState: { profile } } = this.props
-    const { trialState } = profile
-    const hasFreeCredits = trialState === 'Enabled'
-    const loading = false
-    const onDismiss = this.props
+const RequesterPaysModal = ({ onDismiss, onSuccess }) => {
+  const { profile } = Utils.useAtom(authStore)
+  const { trialState } = profile
+  const hasFreeCredits = trialState === 'Enabled'
+  const [loading, setLoading] = useState(false)
+  const [billingList, setBillingList] = useState([])
+  const [selectedBilling, setSelectedBilling] = useState(workspaceStore.get().userProject)
+  const signal = useCancellation()
 
-    return Utils.cond(
-      [loading, spinnerOverlay],
-      [hasFreeCredits, () => h(Modal, {
-        title: 'Set up Billing',
-        onDismiss,
-        showCancel: false,
-        okButton: buttonPrimary({
-          onClick: () => {
-            //onDismiss()
-            freeCreditsActive.set(true)
-          }
-        }, 'Get Free Credits')
-      }, [
-        div('To view or download data in this workspace, please set up a billing project.'),
-        div([
-          'You have $300 in',
-          link({
-            style: { marginLeft: '0.25rem' },
-            href: 'https://support.terra.bio/hc/en-us/articles/360027940952',
-            ...Utils.newTabLinkProps
-          }, [
-            'free credits', icon('pop-out', { style: { margin: '0 0.25rem' }, size: 12 })
-          ]), 'available!'
-        ]),
-        div({ style: { marginTop: '1rem' } }, [
-          link({
-            href: '', // add link to documentation
-            ...Utils.newTabLinkProps
-          }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
-        ])
-      ])],
-      () => h(Modal, {
-        title: 'Set up Billing',
-        onDismiss,
-        showCancel: false,
-        okButton: buttonPrimary({
-          onClick: () => {
-            Nav.goToPath('billing')
-          }
-        }, 'Go to Billing')
-      }, [
-        div('To view or download data in this workspace, please set up a billing project.'),
-        div([
-          link({
-            href: '', // add link to documentation
-            ...Utils.newTabLinkProps
-          }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
-        ])
+  Utils.useOnMount(() => {
+    const loadBillingProjects = _.flow(
+      Utils.withBusyState(setLoading),
+      withErrorReporting('Error loading billing projects')
+    )(async () => {
+      setBillingList(await Ajax(signal).Billing.listProjects())
+    })
+    loadBillingProjects()
+  })
+
+  return Utils.cond(
+    [loading, () => h(Modal, {
+      title: 'Cannot access data',
+      onDismiss,
+      showCancel: false,
+      okButton: false
+    }, [
+      spinnerOverlay
+    ])],
+    [billingList.length > 0, () => h(Modal, {
+      title: 'Cannot access data',
+      onDismiss,
+      shouldCloseOnOverlayClick: false,
+      okButton: buttonPrimary({
+        disabled: !selectedBilling,
+        onClick: () => {
+          onSuccess(selectedBilling)
+        }
+      }, ['Ok'])
+    }, [
+      'This data is in a requester pays bucket. Choose a billing project to continue:',
+      h(RequiredFormLabel, ['Billing Project']),
+      h(Select, {
+        isClearable: false,
+        value: selectedBilling,
+        placeholder: 'Select a billing project',
+        onChange: ({ value }) => setSelectedBilling(value),
+        options: _.uniq(_.map('projectName', billingList)).sort()
+      }),
+      div({ style: { marginTop: '1rem' } }, [
+        link({
+          href: '', // add link to documentation
+          ...Utils.newTabLinkProps
+        }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
       ])
-    )
-  }
-})
+    ])],
+    [hasFreeCredits, () => h(Modal, {
+      title: 'Cannot access data',
+      onDismiss,
+      okButton: buttonPrimary({
+        onClick: () => {
+          onDismiss()
+          freeCreditsActive.set(true)
+        }
+      }, 'Get Free Credits')
+    }, [
+      div('To view or download data in this workspace, please set up a billing project.'),
+      div([
+        'You have $300 in',
+        link({
+          style: { marginLeft: '0.25rem' },
+          href: 'https://support.terra.bio/hc/en-us/articles/360027940952',
+          ...Utils.newTabLinkProps
+        }, [
+          'free credits', icon('pop-out', { style: { margin: '0 0.25rem' }, size: 12 })
+        ]), 'available!'
+      ]),
+      div({ style: { marginTop: '1rem' } }, [
+        link({
+          href: '', // add link to documentation
+          ...Utils.newTabLinkProps
+        }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
+      ])
+    ])],
+    () => h(Modal, {
+      title: 'Cannot access data',
+      onDismiss,
+      okButton: buttonPrimary({
+        onClick: () => {
+          Nav.goToPath('billing')
+        }
+      }, 'Go to Billing')
+    }, [
+      div('To view or download data in this workspace, please set up a billing project.'),
+      div([
+        link({
+          href: '', // add link to documentation
+          ...Utils.newTabLinkProps
+        }, ['Why is billing required for this workspace?', icon('pop-out', { style: { marginLeft: '0.25rem' }, size: 12 })])
+      ])
+    ])
+  )
+}
+
+export default RequesterPaysModal
--- src/components/bucket-utils.js
+++ src/components/bucket-utils.js
@@ -1,10 +1,7 @@
 import _ from 'lodash/fp'
 import { forwardRef, useState } from 'react'
 import { h } from 'react-hyperscript-helpers'
-import { buttonPrimary, Select } from 'src/components/common'
-import Modal from 'src/components/Modal'
-import { Ajax, useCancellation } from 'src/libs/ajax'
-import { RequiredFormLabel } from 'src/libs/forms'
+import RequesterPaysModal from 'src/components/RequesterPaysModal'
 import { workspaceStore } from 'src/libs/state'
 import * as Utils from 'src/libs/utils'
 
@@ -25,49 +22,19 @@ export const withRequesterPaysHandler = _.curry((handler, fn) => async (...args)
 export const requesterPaysWrapper = ({ onDismiss }) => WrappedComponent => {
   const Wrapper = forwardRef((props, ref) => {
     const [showModal, setShowModal] = useState(false)
-    const [billingList, setBillingList] = useState([])
-    const [selectedBilling, setSelectedBilling] = useState(workspaceStore.get().userProject)
-    const signal = useCancellation()
 
     return Utils.cond(
-      // This is where the free credits modal will be used: SATURN-862
-      [showModal && billingList.length === 0, h(Modal, {
-        title: 'Cannot access data',
-        showCancel: false,
-        okButton: () => onDismiss()
-      }, [
-        'This data is in a requester pays bucket and there are no billing projects setup to bill to.'
-      ])],
-      [showModal && billingList.length > 0,  h(Modal, {
-        title: 'Cannot access data',
+      [showModal, () => h(RequesterPaysModal, {
         onDismiss: () => onDismiss(props),
-        shouldCloseOnOverlayClick: false,
-        showCancel: true,
-        okButton: buttonPrimary({
-          disabled: !selectedBilling,
-          onClick: () => {
-            workspaceStore.set({ userProject: selectedBilling, ...workspaceStore.get() })
-            setShowModal(false)
-          }
-        }, ['Ok'])
-      }, [
-        'This data is in a requester pays bucket. Choose a billing project to continue:',
-        h(RequiredFormLabel, ['Billing Project']),
-        h(Select, {
-          isClearable: false,
-          value: selectedBilling,
-          placeholder: 'Select a billing project',
-          onChange: ({ value }) => setSelectedBilling(value),
-          options: _.uniq(_.map('projectName', billingList)).sort()
-        })
-      ])],
-      [!showModal, h(WrappedComponent, {
-        ref, ...props,
-        onRequesterPaysError: async () => {
-          setBillingList(await Ajax(signal).Billing.listProjects())
-          return setShowModal(true)
+        onSuccess: selectedBilling => {
+          workspaceStore.set({ userProject: selectedBilling, ...workspaceStore.get() })
+          setShowModal(false)
         }
-      })]
+      })],
+      () => h(WrappedComponent, {
+        ref, ...props,
+        onRequesterPaysError: () => setShowModal(true)
+      })
     )
   })
   return Wrapper
