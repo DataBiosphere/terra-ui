67f0981dc69cefe38221c7229ad1ff0244ba9ba5
Pete Santos <psantos@broadinstitute.org>
2019-07-22 09:11:30 -0400

    Prevent escaping of HTML in text knobs

--- src/_stories/TitleBar.stories.js
+++ src/_stories/TitleBar.stories.js
@@ -8,7 +8,7 @@ import TitleBar from 'src/components/TitleBar'
 const container = titleBar => div({ style: { width: `${number('Container Width', 500)}px`, border: '1px solid black' } }, [titleBar])
 
 storiesOf('Title Bar', module)
-  .addDecorator(withKnobs)
+  .addDecorator(withKnobs({ escapeHTML: false }))
   .add('With Title Only', () => container(h(TitleBar, { title: text('Title', 'Test') })))
   .add('With Previous Button', () => container(h(TitleBar, { title: text('Title', 'Test'), onPrevious: action('Previous clicked') })))
   .add('With Dismiss Button', () => container(h(TitleBar, { title: text('Title', 'Test'), onDismiss: action('Dismiss clicked') })))
