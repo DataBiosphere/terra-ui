5b26dc25ef00149b33228c57e311ded884b04ec2
Isaac Zarsky <zarsky@broadinstitute.org>
2019-08-05 13:24:53 -0400

    add react-axe

--- src/appLoader.js
+++ src/appLoader.js
@@ -5,10 +5,12 @@ import 'src/style.css'
 
 import _ from 'lodash/fp'
 import marked from 'marked'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { h } from 'react-hyperscript-helpers'
 import RModal from 'react-modal'
 import { initializeAuth } from 'src/libs/auth'
+import { getConfig } from 'src/libs/config'
 import { initializeTCell } from 'src/libs/tcell'
 import Main from 'src/pages/Main'
 
@@ -21,3 +23,8 @@ marked.setOptions({ sanitize: true, sanitizer: _.escape })
 ReactDOM.render(h(Main), document.getElementById('root'))
 initializeAuth()
 initializeTCell()
+
+if (!getConfig().isProd) {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
