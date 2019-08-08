499a1a794e57f8e03e7ec2eb25395dcf6fbe48bb
Pete Santos <psantos@broadinstitute.org>
2019-08-08 08:27:52 -0400

    Clean up story

--- src/_stories/NewUserModal.stories.js
+++ src/_stories/NewUserModal.stories.js
@@ -1,5 +1,5 @@
 import { action } from '@storybook/addon-actions'
-import { number, select, text, withKnobs } from '@storybook/addon-knobs'
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import { Fragment, useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
@@ -10,6 +10,7 @@ import { ajaxOverridesStore } from 'src/libs/state'
 const INVITE_SUCCEEDED = 201
 const USER_NOT_REGISTERED = 404
 const USER_IS_REGISTERED = 200
+const FAILED_REQUEST = 400
 
 const delayed = ({ ms, resolved, status }) => {
   return new Promise((resolve, reject) => {
@@ -17,7 +18,7 @@ const delayed = ({ ms, resolved, status }) => {
       if (resolved) {
         resolve(new Response({ status }))
       } else {
-        reject({ status })
+        reject({ status, json: () => ({ message: 'Failed to add user' }) })
       }
     }, ms)
   })
@@ -40,19 +41,10 @@ const setOverrides = ({ isRegistered, ms }) => {
   }])
 }
 
-
-const delayedResolve = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
-const delayedReject = ms => new Promise((resolve, reject) => {
-  const rejection = {
-    status: 404,
-    json: () => ({ message: 'Failed to add user' })
-  }
-  return setTimeout(() => reject(rejection), ms)
-})
-
-const Modal = ({ addFn }) => {
+const Modal = () => {
   const ms = number('Latency', 100)
-  const isRegistered = select('Is user registered', { yes: true, no: false }, false)
+  const isRegistered = boolean('User is registered', false)
+  const addSucceeds = boolean('User added successfully', true)
   const title = text('Title', 'Add user to Terra Group')
   const adminLabel = text('Admin Label', 'admin')
   const userLabel = text('User Label', 'user')
@@ -69,7 +61,7 @@ const Modal = ({ addFn }) => {
         userLabel,
         addFunction: (role, userEmail) => {
           action(`Attempting to add user ${userEmail} as a ${role}`)()
-          return addFn(ms)
+          return delayed({ resolved: addSucceeds, ms, status: FAILED_REQUEST })
         },
         onSuccess: () => {
           action('Successfuly added user')()
@@ -83,5 +75,4 @@ const Modal = ({ addFn }) => {
 
 storiesOf('Add User To Group Modal', module)
   .addDecorator(withKnobs)
-  .add(`Successfully add user`, () => h(Modal, { addFn: delayedResolve }))
-  .add(`Reject adding user`, () => h(Modal, { addFn: delayedReject }))
+  .add(`Add user`, () => h(Modal))
