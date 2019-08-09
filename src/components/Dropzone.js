8d5136a5f11fc8514d95f8c4b2d24ca39087bb99
Isaac Zarsky <zarsky@broadinstitute.org>
2019-08-09 12:08:00 -0400

    explanatory comment

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -4,6 +4,7 @@ import { div, input } from 'react-hyperscript-helpers'
 
 
 const Dropzone = ({ disabled = false, onDragOver, onDrop, onDragLeave, style = {}, activeStyle = {}, children, ...props }) => {
+  // dropzone's built-in dragging status doesn't seem to work if there's anything rendered over the root div
   const [dragging, setDragging] = useState(false)
 
   const { getRootProps, getInputProps, open: openUploader, ...dropProps } = useDropzone({
