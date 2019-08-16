d08ba003d1c6eeabbbe02479879a15db778cd720
Pete Santos <psantos@broadinstitute.org>
2019-08-16 12:10:02 -0400

    Update IGV and usermodal stories

--- src/_stories/IGVPanel.stories.js
+++ src/_stories/IGVPanel.stories.js
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions'
 import { number, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import _ from 'lodash/fp'
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
 import { IGVFileSelector } from 'src/components/IGVFileSelector'
 
@@ -21,16 +21,12 @@ const withUniqueEntities = numElements => {
 
 const IGVDrawer = () => {
   const numEntities = number('Number of rows to display', DEFAULT_ENTITIES, { range: true, min: 0, max: 100 })
-  const [openDrawer, setOpenDrawer] = useState(true)
 
   return h(Fragment, [
-    h('button', { onClick: () => setOpenDrawer(true), style: { width: '100px' } }, 'Open Drawer'),
-    div({ id: 'modal-root' }, [
+    div({ style: { display: 'flex', flex: 1, width: '50%', border: '1px solid black', paddingTop: '1rem' } }, [
       h(IGVFileSelector, {
         onSuccess: action('Launch IGV Clicked'),
-        openDrawer,
-        selectedEntities: withUniqueEntities(numEntities),
-        onDismiss: () => setOpenDrawer(false)
+        selectedEntities: withUniqueEntities(numEntities)
       })
     ])
   ])
