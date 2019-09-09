2f6ba8f8e27ff704a28cf513db03b3aff323b8f4
Pete Santos <psantos@broadinstitute.org>
2019-09-09 16:05:50 -0400

    brute-ish force knobs added

--- src/_stories/Interactive.stories.js
+++ src/_stories/Interactive.stories.js
@@ -1,11 +1,10 @@
 
 import { number, text, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
+import _ from 'lodash/fp'
 import { div, h } from 'react-hyperscript-helpers'
 import { Interactive } from 'src/components/common'
 
-
-console.log(Interactive)
 
 const Inner2 = ({ style, hover, children }) => h(Interactive, {
   as: div,
@@ -51,16 +50,16 @@ const SingleHover = () => {
 }
 
 const NestedHover = () => {
-  const color = text(colorLabel, 'white')
-  const backgroundColor = text(bgColorLabel, 'black')
-  const boxShadow = text(boxShadowLabel, '10px 5px 5px gray')
-  const opacity = number(opacityLabel, 1, { range: true, min: 0, max: 1, step: 0.1 })
-  const textDecoration = text(textDecorationLabel, 'underline')
+  const color = _.times(n => text(`${colorLabel} ${n}`, 'white'), 3)
+  const backgroundColor = _.times(n => text(`${bgColorLabel} ${n}`, 'black'), 3)
+  const boxShadow = _.times(n => text(`${boxShadowLabel} ${n}`, '10px 5px 5px gray'), 3)
+  const opacity = _.times(n => number(`${opacityLabel} ${n}`, 1, { range: true, min: 0, max: 1, step: 0.1 }), 3)
+  const textDecoration = _.times(n => text(`${textDecorationLabel} ${n}`, 'underline'), 3)
 
   const initialSetting = { color: 'black', backgroundColor: 'white', boxShadow: 'none', opacity: 1, textDecoration: 'none' }
-  const containerHover = { color, backgroundColor, boxShadow, opacity, textDecoration }
-  const inner1Hover = { color, backgroundColor, boxShadow, opacity, textDecoration }
-  const inner2Hover = { color, backgroundColor, boxShadow, opacity, textDecoration }
+  const containerHover = { color: color[0], backgroundColor: backgroundColor[0], boxShadow: boxShadow[0], opacity: opacity[0], textDecoration: textDecoration[0] }
+  const inner1Hover = { color: color[1], backgroundColor: backgroundColor[1], boxShadow: boxShadow[1], opacity: opacity[1], textDecoration: textDecoration[1] }
+  const inner2Hover = { color: color[2], backgroundColor: backgroundColor[2], boxShadow: boxShadow[2], opacity: opacity[2], textDecoration: textDecoration[2] }
 
   return h(Interactive, {
     as: div,
