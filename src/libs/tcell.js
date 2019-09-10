0fb8f231c32530c49112bef0e40ae9dec4af9d1c
Isaac Zarsky <zarsky@broadinstitute.org>
2019-09-10 10:23:58 -0400

    update tcell url (#1838)

--- src/libs/tcell.js
+++ src/libs/tcell.js
@@ -4,7 +4,7 @@ import { getConfig } from 'src/libs/config'
 export const initializeTCell = () => {
   const { tCell: { appId, apiKey } } = getConfig()
   const script = document.createElement('script')
-  script.setAttribute('src', 'https://jsagent.tcell.io/tcellagent.min.js')
+  script.setAttribute('src', 'https://us.jsagent.tcell.insight.rapid7.com/tcellagent.min.js')
   script.setAttribute('tcellappid', appId)
   script.setAttribute('tcellapikey', apiKey)
   document.head.appendChild(script)
