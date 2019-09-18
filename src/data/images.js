12eacb845956572e9007d5bcba6235624c89b48b
jdcanas <jcanas@broadinstitute.com>
2019-09-18 13:04:57 -0400

    adding images json file

---
+++ src/data/images.js
@@ -0,0 +1,38 @@
+export const images = {
+    "terra-jupyter-base": {
+        "version": "0.0.1",
+        "image": "us.gcr.io/broad-dsp-gcr-public/terra-jupyter-base:0.0.1",
+        "documentation": [
+            "gs://terra-docker-image-documentation/terra-jupyter-base-0.0.1-python-packages.txt"
+        ]
+    },
+    "terra-jupyter-bioconductor": {
+        "version": "0.0.2",
+        "image": "us.gcr.io/broad-dsp-gcr-public/terra-jupyter-bioconductor:0.0.2",
+        "documentation": [
+            "gs://terra-docker-image-documentation/terra-jupyter-bioconductor-0.0.2-python-packages.txt"
+        ]
+    },
+    "terra-jupyter-hail": {
+        "version": "0.0.1",
+        "image": "us.gcr.io/broad-dsp-gcr-public/terra-jupyter-hail:0.0.1",
+        "documentation": [
+            "gs://terra-docker-image-documentation/terra-jupyter-hail-0.0.1-python-packages.txt"
+        ]
+    },
+    "terra-jupyter-python": {
+        "version": "0.0.1",
+        "image": "us.gcr.io/broad-dsp-gcr-public/terra-jupyter-python:0.0.1",
+        "documentation": [
+            "gs://terra-docker-image-documentation/terra-jupyter-python-0.0.1-python-packages.txt"
+        ]
+    },
+    "terra-jupyter-r": {
+        "version": "0.0.3",
+        "image": "us.gcr.io/broad-dsp-gcr-public/terra-jupyter-r:0.0.3",
+        "documentation": [
+            "gs://terra-docker-image-documentation/terra-jupyter-r-0.0.3-python-packages.txt",
+            "gs://terra-docker-image-documentation/terra-jupyter-r-0.0.3-r-packages.txt"
+        ]
+    }
+}
