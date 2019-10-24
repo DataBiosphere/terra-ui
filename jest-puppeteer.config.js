fe2d20f12d0b7288f71dab02b35481eacbce91e3
Pete Santos <psantos@broadinstitute.org>
2019-10-24 14:17:49 -0400

    parameterize env args in config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   launch: {
-    devtools: (() => {
-      if (process.env.DEVTOOLS === 'true') {
-        process.env.IFRAMES === 'true' && console.warn('You cannot enable devtools while testing a UI with iframes in headful mode')
-        return process.env.IFRAMES !== 'true'
+    devtools: (({ devtools, iframes }) => {
+      if (devtools) {
+        iframes && console.warn('You cannot enable devtools while testing a UI with iframes in headful mode')
+        return !iframes
       }
-    })(),
+    })({ devtools: process.env.DEVTOOLS === 'true', iframes: process.env.IFRAMES === 'true' }),
     headless: process.env.HEADLESS !== 'false',
     // Workaround for issue when accessing cross domain iframes in puppeteer while not headless:
     // https://github.com/GoogleChrome/puppeteer/issues/4960
