d104d107d0df94bf4e9d4d6efaba856c9a25d9a7
Pete Santos <psantos@broadinstitute.org>
2019-10-25 15:22:32 -0400

    Tweak url and update test to wait on page.frames()

--- src/_tests/integration/integration-utils.js
+++ src/_tests/integration/integration-utils.js
@@ -1,22 +1,26 @@
-const waitForFrame = async (page, url) => {
-  const frameLoaded = new Promise(resolve => {
-    const navHandler = async frame => {
-      await frame.url().includes(url) && resolve({ frame, navHandler })
-    }
-    page.on('framenavigated', navHandler)
+const waitForFn = async ({ fn, interval = 2000, timeout = 10000 }) => {
+  const readyState = new Promise(resolve => {
+    const start = Date.now()
+    const intervalId = setInterval(async () => {
+      const success = await fn()
+      success && resolve({ success, intervalId })
+      Date.now() - start > timeout && resolve({ intervalId })
+    }, interval)
   })
 
-  const { frame, navHandler } = await frameLoaded
-  page.removeListener('framenavigated', navHandler)
-  return frame
+  const { success, intervalId } = await readyState
+  clearInterval(intervalId)
+  return success
 }
+
+const findInFrames = (page, src) => waitForFn({ fn: () => page.frames().find(frame => frame.url().includes(src)) })
 
 const findIframe = async page => {
   const iframeNode = await page.waitForXPath('//*[@role="main"]/iframe')
   const srcHandle = await iframeNode.getProperty('src')
   const src = await srcHandle.jsonValue()
 
-  return await waitForFrame(page, src)
+  return await findInFrames(page, src)
 }
 
 const findInGrid = async (page, text) => {
