7779354f0fea3a2b87b126e23414cc34cd2d1f71
Emily Hanna <ehanna@broadinstitute.org>
2019-10-28 15:34:57 -0400

    Add logic to "hack" to correct Terra env after Firecloud redirect

--- src/_tests/integration/find-workflow.integration-test.js
+++ src/_tests/integration/find-workflow.integration-test.js
@@ -1,11 +1,14 @@
-const { click, findText, signIntoTerra } = require('./integration-utils')
+const { click, findText, signIntoTerra, findClickable } = require('./integration-utils')
 const firecloud = require('./fire-cloud-utils')
 
 
 const workflowName = 'haplotypecaller-gvcf-gatk4'
+const billingAccount = 'general-dev-billing-account'
+const workspace = process.env.WORKSPACE
+const url = 'http://localhost:3000'
 
-test.skip('integration', async () => {
-  await page.goto('localhost:3000')
+test('integration', async () => {
+  await page.goto('http://localhost:3000')
   await click(page, 'View Examples')
   await signIntoTerra(page)
   await click(page, 'code & workflows')
@@ -17,9 +20,20 @@ test.skip('integration', async () => {
   await click(page, `${workflowName}-configured`)
   await click(page, 'Use Selected Configuration')
   await findText(page, 'Select a workspace')
-  await firecloud.selectWorkspace(page, 'general-dev-billing-account', process.env.WORKSPACE)
+  await firecloud.selectWorkspace(page, billingAccount, workspace)
   await firecloud.click(page, 'import-export-confirm-button')
-  await click(page, 'Yes')
+
+  /* This else/if is necessary to "hack" going back to localhost:3000-Terra after going to Firecloud,
+   without these lines it will redirect to dev-Terra even if started out at localhost:3000-Terra */
+  if (url === 'http://localhost:3000') {
+    await findClickable(page, 'Yes')
+    const yesButtonHrefDetails = (await page.$x('//a[contains(text(), "Yes")]/@href'))[0]
+    const redirectURL = (await page.evaluate(yesButton => yesButton.textContent, yesButtonHrefDetails)).replace('https://bvdp-saturn-dev.appspot.com',
+      url)
+    await page.goto(redirectURL)
+  } else {
+    await click(page, 'Yes')
+  }
 
   await signIntoTerra(page)
   await findText(page, `${workflowName}-configured`)
