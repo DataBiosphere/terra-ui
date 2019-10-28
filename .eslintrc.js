5b8189829cc61ddc20ce25c5baff6b77757b1c15
Isaac Zarsky <zarsky@broadinstitute.org>
2019-10-28 18:14:54 -0400

    oops, ugh

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,7 +76,7 @@ module.exports = {
     'lodash-fp/no-extraneous-args': 'warn',
     'lodash-fp/no-extraneous-function-wrapping': 'warn',
     'lodash-fp/no-extraneous-iteratee-args': 'warn',
-    'lodash-fp/no-extraneous-partials': 'warn',  // available in eslint-plugin-lodash-fp 2.2.0+
+    // 'lodash-fp/no-extraneous-partials': 'warn', // available in eslint-plugin-lodash-fp 2.2.0+
     'lodash-fp/no-partial-of-curried': 'warn',
     'lodash-fp/no-single-composition': 'warn',
     'lodash-fp/no-submodule-destructuring': 'warn',
