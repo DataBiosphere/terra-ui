5bde958ae0fb2af02759f84fda34c9f249feb97e
Isaac Zarsky <zarsky@broadinstitute.org>
2019-10-29 13:41:52 -0400

    make lint happy

--- src/_tests/getBearerToken.js
+++ src/_tests/getBearerToken.js
@@ -6,15 +6,15 @@ const getToken = async () => {
     const [sub, keyString] = process.argv.slice(2)
     const { client_email: email, private_key: key } = JSON.parse(keyString)
 
-    const gtoken = new GoogleToken({
+    const googleToken = new GoogleToken({
       email,
       scope: ['profile', 'email', 'openid'],
       sub,
       key
     })
 
-    const { access_token } = await gtoken.getToken()
-    console.log(access_token)
+    const tokenData = await googleToken.getToken()
+    console.log(tokenData.access_token)
     process.exit(0)
   } catch (err) {
     console.error(err)
