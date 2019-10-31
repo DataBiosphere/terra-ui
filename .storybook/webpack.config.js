4a5c694a9cac3d982b4b6259118d503375cc3b09
Pete Santos <psantos@broadinstitute.org>
2019-10-31 09:22:47 -0400

    move comment to original location

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,12 +5,13 @@ const { useEslintRc } = require('customize-cra')
 module.exports = ({ config }) => {
   // Needed for .stories/config.js
   config.resolve.modules.push(path.resolve(__dirname, '../'))
+
+  // Replace storybook's default svg loader	
   config.module.rules = config.module.rules.map(data => {
     data.test = /svg\|/.test(String(data.test)) ? /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/ : data.test
     return data
   })
 
-  // Replace storybook's default svg loader	
   config.module.rules.push({
     test: /\.svg$/,
     use: [{ loader: require.resolve('svg-react-loader') }]
