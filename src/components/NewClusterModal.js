f92795144511e62813701ad85a93218bee1a08d4
ehanna4 <54322292+ehanna4@users.noreply.github.com>
2019-10-31 09:04:42 -0400

    Update text instructions for creating custom image [SATURN-1192] (#1915)

--- src/components/NewClusterModal.js
+++ src/components/NewClusterModal.js
@@ -281,8 +281,9 @@ export const NewClusterModal = withModalDrawer({ width: 675 })(class NewClusterM
                   ])
                 ]),
                 div({ style: { gridColumnStart: 2, gridColumnEnd: 'span 2', alignSelf: 'start' } }, [
-                  h(Link, { href: imageInstructions, ...Utils.newTabLinkProps }, ['Learn how']),
-                  ' to create your own custom docker image from one of our ',
+                  h(Link, { href: imageInstructions, ...Utils.newTabLinkProps }, ['Custom notebook environments']),
+                  span({ style: { fontWeight: 'bold' } }, [' must ']),
+                  ' be based off one of the ',
                   h(Link, { href: terraBaseImages, ...Utils.newTabLinkProps }, ['Terra base images.'])
                 ])
               ]) :
