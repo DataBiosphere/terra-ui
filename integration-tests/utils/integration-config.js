e4c350de7f8f875e22edf42b4fc078fb9783cd54
Isaac Zarsky <zarsky@broadinstitute.org>
2019-11-05 15:38:38 -0500

    parameterize screenshot dir (#1924)

--- integration-tests/utils/integration-config.js
+++ integration-tests/utils/integration-config.js
@@ -2,12 +2,14 @@ const {
   TEST_URL: testUrl = 'http://localhost:3000',
   TERRA_TOKEN: bearerToken,
   BILLING_PROJECT: billingProject = 'general-dev-billing-account',
-  WORKFLOW_NAME: workflowName = 'haplotypecaller-gvcf-gatk4'
+  WORKFLOW_NAME: workflowName = 'haplotypecaller-gvcf-gatk4',
+  SCREENSHOT_DIR: screenshotDir = 'failure-screenshots'
 } = process.env
 
 module.exports = {
   bearerToken,
   billingProject,
   testUrl,
-  workflowName
+  workflowName,
+  screenshotDir
 }
