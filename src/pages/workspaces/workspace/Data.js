3700adb16fa758ef54203bd8cee8b194a32f9a27
Emily Hanna <ehanna@broadinstitute.org>
2019-11-06 09:39:08 -0500

    update added error messages for workspace variables

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -148,11 +148,11 @@ const LocalVariablesContent = class LocalVariablesContent extends Component {
       ...filteredAttributes, ...(creatingNewVariable ? [['', '']] : [])
     ]
 
-    const inputErrors = editIndex !== undefined && editIndex >= 0 && [
+    const inputErrors = editIndex !== undefined && [
       ...(_.keys(_.unset(amendedAttributes[editIndex][0], attributes)).includes(editKey) ? ['Key must be unique'] : []),
       ...(!/^[\w-]*$/.test(editKey) ? ['Key can only contain letters, numbers, underscores, and dashes'] : []),
-      ...(editKey === 'description' ? ['\`description\` is a reserved keyword'] : []),
-      ...(editKey.startsWith('referenceData_') ? ['\`referenceData_\` is a reserved keyword'] : []),
+      ...(editKey === 'description' ? ['Key cannot be \`description\`'] : []),
+      ...(editKey.startsWith('referenceData_') ? ['Key cannot start with \`referenceData_\`'] : []),
       ...(!editKey ? ['Key is required'] : []),
       ...(!editValue ? ['Value is required'] : []),
       ...(editValue && editType === 'number' && Utils.cantBeNumber(editValue) ? ['Value is not a number'] : []),
