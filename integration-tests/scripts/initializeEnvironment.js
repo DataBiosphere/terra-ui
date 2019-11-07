50d22038b941cdf55ee1d0d19b26080b0cfbddd2
Isaac Zarsky <zarsky@broadinstitute.org>
2019-11-06 20:41:09 -0500

    use fetchOk

--- integration-tests/scripts/initializeEnvironment.js
+++ integration-tests/scripts/initializeEnvironment.js
@@ -2,6 +2,15 @@ const fetch = require('node-fetch')
 const prompts = require('prompts')
 
 
+const fetchOk = async (...args) => {
+  const res = await fetch(...args)
+  if (res.ok) {
+    return res
+  } else {
+    throw await res.text()
+  }
+}
+
 const namespace = 'gatk'
 const name = 'echo_to_file'
 
@@ -43,6 +52,8 @@ const setupMethod = async () => {
   const agoraUrl = `https://agora.dsde-${env}.broadinstitute.org`
   const rawlsUrl = `https://rawls.dsde-${env}.broadinstitute.org`
 
+  const agoraPublicQuery = '?user=public&roles=All'
+
   const fcHeaders = { Authorization: `Bearer ${fcToken}`, 'Content-Type': 'application/json' }
   const googleHeaders = { Authorization: `Bearer ${googleToken}`, 'Content-Type': 'application/json' }
 
@@ -53,7 +64,7 @@ const setupMethod = async () => {
       payload: wdl
     }
 
-    const { snapshotId: methodSnapshot } = await fetch(`${agoraUrl}/api/v1/methods`, {
+    const { snapshotId: methodSnapshot } = await fetchOk(`${agoraUrl}/api/v1/methods`, {
       method: 'POST',
       headers: fcHeaders,
       body: JSON.stringify(methodPayload)
@@ -61,14 +72,14 @@ const setupMethod = async () => {
 
     console.log(`Created method ${namespace}/${name} snapshot ${methodSnapshot}`)
 
-    await fetch(`${agoraUrl}/api/v1/methods/${namespace}/${name}/${methodSnapshot}/permissions?user=public&roles=All`, {
+    await fetchOk(`${agoraUrl}/api/v1/methods/${namespace}/${name}/${methodSnapshot}/permissions${agoraPublicQuery}`, {
       method: 'POST',
       headers: fcHeaders
     })
 
     console.log('Made method public')
 
-    const configTemplate = await fetch(`${rawlsUrl}/api/methodconfigs/template`, {
+    const configTemplate = await fetchOk(`${rawlsUrl}/api/methodconfigs/template`, {
       method: 'POST',
       headers: fcHeaders,
       body: JSON.stringify({ methodNamespace: namespace, methodName: name, methodVersion: methodSnapshot, sourceRepo: 'agora' })
@@ -86,7 +97,7 @@ const setupMethod = async () => {
       entityType: 'Configuration'
     }
 
-    const { snapshotId: configSnapshot } = await fetch(`${agoraUrl}/api/v1/configurations`, {
+    const { snapshotId: configSnapshot } = await fetchOk(`${agoraUrl}/api/v1/configurations`, {
       method: 'POST',
       headers: fcHeaders,
       body: JSON.stringify(configBody)
@@ -94,19 +105,19 @@ const setupMethod = async () => {
 
     console.log(`Created config ${name}-configured snapshot ${configSnapshot}`)
 
-    await fetch(`${agoraUrl}/api/v1/configurations/${namespace}/${name}-configured/${configSnapshot}/permissions?user=public&roles=All`, {
+    await fetchOk(`${agoraUrl}/api/v1/configurations/${namespace}/${name}-configured/${configSnapshot}/permissions${agoraPublicQuery}`, {
       method: 'POST',
       headers: fcHeaders
     })
 
     console.log('Made config public')
 
-    await fetch(`${agoraUrl}/api/v1/methods/${namespace}/permissions?user=public&roles=All`, {
+    await fetchOk(`${agoraUrl}/api/v1/methods/${namespace}/permissions${agoraPublicQuery}`, {
       method: 'POST',
       headers: fcHeaders
     })
 
-    await fetch(`${agoraUrl}/api/v1/configurations/${namespace}/permissions?user=public&roles=All`, {
+    await fetchOk(`${agoraUrl}/api/v1/configurations/${namespace}/permissions${agoraPublicQuery}`, {
       method: 'POST',
       headers: fcHeaders
     })
@@ -115,21 +126,27 @@ const setupMethod = async () => {
 
     const featuredMethodsUrl = `https://www.googleapis.com/storage/v1/b/firecloud-alerts-${env}/o/featured-methods.json`
 
-    const featuredMethods = await fetch(`${featuredMethodsUrl}?alt=media`, { headers: googleHeaders }).then(res => res.json())
+    const featuredMethods = await fetchOk(`${featuredMethodsUrl}?alt=media`, { headers: googleHeaders }).then(res => res.ok && res.json())
 
     if (!featuredMethods || !featuredMethods.some(method => method.name === name && method.namespace === namespace)) {
-      const { acl } = await fetch(`${featuredMethodsUrl}?projection=full`, { headers: googleHeaders }).then(res => res.json())
+      await fetchOk(
+        `https://www.googleapis.com/upload/storage/v1/b/firecloud-alerts-${env}/o?uploadType=media&name=featured-methods.json&predefinedAcl=projectPrivate`,
+        {
+          method: 'POST',
+          headers: googleHeaders,
+          body: JSON.stringify([...(!featuredMethods ? [] : featuredMethods), { namespace, name }])
+        })
 
-      await fetch(`https://www.googleapis.com/upload/storage/v1/b/firecloud-alerts-${env}/o?uploadType=media&name=featured-methods.json`, {
-        method: 'POST',
-        headers: googleHeaders,
-        body: JSON.stringify([...featuredMethods, { namespace, name }])
-      }).then(res => res.json())
-
-      await fetch(featuredMethodsUrl, {
+      await fetchOk(featuredMethodsUrl, {
         method: 'PUT',
         headers: googleHeaders,
-        body: JSON.stringify({ acl, cacheControl: 'public, max-age=0, no-store', contentType: 'application/json' })
+        body: JSON.stringify({ cacheControl: 'public, max-age=0, no-store', contentType: 'application/json' })
+      })
+
+      await fetchOk(`${featuredMethodsUrl}/acl/allUsers`, {
+        method: 'PATCH',
+        headers: googleHeaders,
+        body: JSON.stringify({ entity: 'allUsers', role: 'READER' })
       })
     }
 
