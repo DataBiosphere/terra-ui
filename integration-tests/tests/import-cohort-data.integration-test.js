2b2e1891de4b4c50f62f9949c0ef903c9d6df8da
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2019-11-12 12:52:53 -0500

    refactor integration test utils [SATURN-1211] (#1926)

--- integration-tests/tests/import-cohort-data.integration-test.js
+++ integration-tests/tests/import-cohort-data.integration-test.js
@@ -1,7 +1,6 @@
-const dataExplorer = require('../utils/data-explorer-utils')
 const { testUrl } = require('../utils/integration-config')
 const { withWorkspace } = require('../utils/integration-helpers')
-const { findInGrid, exactClick, click, findIframe, signIntoTerra, select } = require('../utils/integration-utils')
+const { findInGrid, click, clickable, fillIn, findIframe, input, signIntoTerra, select, svgText } = require('../utils/integration-utils')
 
 
 const cohortName = `terra-ui-test-cohort`
@@ -9,18 +8,18 @@ const cohortName = `terra-ui-test-cohort`
 test('import cohort data', withWorkspace(async ({ workspaceName }) => {
   await page.goto(testUrl)
   await signIntoTerra(page)
-  await click(page, 'Browse Data')
-  await click(page, '1000 Genomes Low Coverage')
+  await click(page, clickable({ textContains: 'Browse Data' }))
+  await click(page, clickable({ textContains: '1000 Genomes Low Coverage' }))
 
   const frame = await findIframe(page)
-  await dataExplorer.clickTextInAnyNS(frame, 'Has WGS Low')
-  await click(frame, 'Save cohort')
-  await dataExplorer.fillIn(frame, 'name', cohortName)
-  await dataExplorer.click(frame, 'Save')
+  await click(frame, svgText({ textContains: 'Has WGS Low' }))
+  await click(frame, clickable({ textContains: 'Save cohort' }))
+  await fillIn(frame, input({ placeholder: 'cohort name' }), cohortName)
+  await click(frame, clickable({ text: 'Save' }))
 
   await select(page, 'Select a workspace', workspaceName)
-  await exactClick(page, 'Import')
-  await click(page, 'cohort')
+  await click(page, clickable({ text: 'Import' }))
+  await click(page, clickable({ textContains: 'cohort' }))
   await findInGrid(page, '1000 Genomes')
   await findInGrid(page, cohortName)
 }), 5 * 60 * 1000)
