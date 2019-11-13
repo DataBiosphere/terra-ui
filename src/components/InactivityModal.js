c5f8d33ca13b3533d75a31bfa9195d4e4c624281
Pete Santos <psantos@broadinstitute.org>
2019-11-13 16:01:23 -0500

    Update/cleanup timeout code with hook

--- src/components/InactivityModal.js
+++ src/components/InactivityModal.js
@@ -1,49 +1,61 @@
 import _ from 'lodash/fp'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { div, h, iframe } from 'react-hyperscript-helpers'
 import Modal from 'src/components/Modal'
+import { useCancellation } from 'src/libs/ajax'
 import { authStore } from 'src/libs/state'
 import * as Utils from 'src/libs/utils'
 
 
-const localStorageKey = 'terra-app-timeout'
-
-const timeout = 15000 //15 * 1000 * 60
-const countdownStart = 8000 ///890000 //(14 * 1000 * 60) + 55
-
-const getLastActiveTime = () => parseInt(localStorage.getItem(localStorageKey), 10) || Date.now()
-const computePrecision = (totalCountdown, countdownStart) => Math.max((totalCountdown - countdownStart) - (Date.now() - getLastActiveTime()), 0)
 const displayRemainingTime = remainingSeconds => {
-  return `${Math.floor(remainingSeconds / 60)}`.padStart(2, '0') + ':' + `${remainingSeconds % 60}`.padStart(2, '0')
+  return `${Math.floor(remainingSeconds / 60)}`.padStart(2, '0') + ':' + `${Math.floor(remainingSeconds % 60)}`.padStart(2, '0')
 }
 
-const InactivityModal = Utils.connectAtom(authStore, 'authState')(({ authState: { isSignedIn } }) => {
-  const [timedOut, setTimedOut] = useState(isSignedIn && Date.now() - getLastActiveTime() > timeout)
-  const [countdown, setCountdown] = useState()
-  const [showCountdown, setShowCountdown] = useState()
-  const countdownRef = useRef()
+const useCurrentTime = () => {
+  const [currentTime, setCurrentTime] = useState(Date.now())
+  const signal = useCancellation()
+  const delayRef = useRef(250)
 
-  useEffect(() => {
-    const remaining = timeout - (Date.now() - getLastActiveTime())
-    const precision = remaining > countdownStart ? computePrecision(timeout, countdownStart) : 1000
-
-    if (!timedOut && isSignedIn) {
-      countdownRef.current = setTimeout(() => {
-        const timeRemaining = timeout - (Date.now() - getLastActiveTime())
-        setShowCountdown(timeRemaining < countdownStart)
-        setTimedOut(Date.now() - getLastActiveTime() > timeout)
-        setCountdown(Math.ceil(timeRemaining / 1000))
-      }, precision)
-    } else {
-      clearTimeout(countdownRef.current)
+  Utils.useOnMount(() => {
+    const poll = async () => {
+      while (!signal.aborted) {
+        await Utils.delay(delayRef.current)
+        setCurrentTime(Date.now())
+      }
     }
-  }, [countdown, isSignedIn, timedOut])
+    poll()
+  })
+
+  return [currentTime, delay => { delayRef.current = delay }]
+}
+
+const timeout = 15 * 1000 * 60
+const countdownStart = 3 * 1000 * 60
+
+const InactivityModal = () => {
+  const { isSignedIn, profile: { email } } = Utils.useAtom(authStore)
+  return !!email && !!isSignedIn && h(InactivityTimer, { localStorageKey: `terra-timeout-${email}` })
+}
+
+const InactivityTimer = ({ localStorageKey }) => {
+  const [dismiss, setDismiss] = useState()
+  const lastActiveTime = parseInt(localStorage.getItem(localStorageKey), 10) || Date.now()
+  const [currentTime, setDelay] = useCurrentTime()
+
+  const timeoutTime = lastActiveTime + timeout
+  const timedOut = currentTime > timeoutTime
+  const showCountdown = currentTime > timeoutTime - countdownStart
+  const countdown = Math.max(0, timeoutTime - currentTime)
+
+  setDelay(showCountdown ? 1000 : Math.max(250, countdown - countdownStart))
 
   Utils.useOnMount(() => {
     const targetEvents = ['mousedown', 'keydown']
     const updateLastActive = () => localStorage.setItem(localStorageKey, Date.now().toString())
 
+    !localStorage.getItem(localStorageKey) && localStorage.setItem(localStorageKey, Date.now().toString())
     _.forEach(event => document.addEventListener(event, updateLastActive), targetEvents)
+
     return () => {
       _.forEach(event => document.removeEventListener(event, updateLastActive), targetEvents)
     }
@@ -55,23 +67,23 @@ const InactivityModal = Utils.connectAtom(authStore, 'authState')(({ authState: 
       return h(Modal, {
         title: 'Your Session Has Timed Out',
         showCancel: false,
-        onDismiss: () => {
-          setTimedOut()
-          setShowCountdown()
-        }
+        onDismiss: () => { }
       }, [
         `You will need to log back in to access Terra`,
         iframe({ style: { display: 'none' }, src: 'https://www.google.com/accounts/Logout' })
       ])
     }
   ], [
-    showCountdown, h(Modal, { title: 'Inactive Session', showCancel: false, width: '400px', onDismiss: () => setShowCountdown() },
-      [
-        div({ style: { whiteSpace: 'pre' } },
-          [`Your session will timout in ${displayRemainingTime(countdown)}\nClick or press any key to resume`])
-      ])
+    showCountdown, h(Modal, {
+      title: 'Inactive Session', showCancel: false, width: 400,
+      onDismiss: () => setDismiss(!dismiss)
+    },
+    [
+      div({ style: { whiteSpace: 'pre' } },
+        [`Your session will timout in ${displayRemainingTime(countdown / 1000)}\nClick or press any key to resume`])
+    ])
   ],
   false)
-})
+}
 
 export default InactivityModal
