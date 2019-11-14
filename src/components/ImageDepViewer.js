f1520963f4890b639d500c5dcd5bc2352af83402
jdcanas <jcanas@broadinstitute.com>
2019-11-14 11:34:30 -0500

    wip pr feedback

--- src/components/ImageDepViewer.js
+++ src/components/ImageDepViewer.js
@@ -1,68 +1,115 @@
 import _ from 'lodash/fp'
-import PropTypes from 'prop-types'
-import { Component, Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { div, h, table, tbody, td, thead, tr } from 'react-hyperscript-helpers'
 
 import { Select } from './common'
 
+export const ImageDepViewer = ({ packageDoc }) => {
+  const pages = _.keys(packageDoc)
+  const [language, setLanguage] = useState(pages[0]) //TODO safely do this
 
-export class ImageDepViewer extends Component {
-  static propTypes = {
-    packageDoc: PropTypes.object
+  //we must ensure
+  if (!pages.includes(language)) {
+    setLanguage(pages[0]) //TODO safely do this
   }
 
-  constructor(props) {
-    super(props)
+  const packages = packageDoc ? packageDoc[language] : {}
 
-    const { packageDoc } = props
-    this.state = { language: _.keys(packageDoc)[0] }
-  }
+  return h(Fragment, [
+    div({ style: { display: 'flex', alignItems: 'center' } }, [
+      div({ style: { fontWeight: 'bold', marginRight: '1rem' } }, ['Installed packages']),
+      pages.length === 1 ?
+        `(${language})` :
+        div({ style: { width: 120, textTransform: 'capitalize' } }, [
+          h(Select, {
+            'aria-label': 'Select a language',
+            value: language,
+            onChange: ({ value }) => {
+              this.setState({ language: value })
+            },
+            isSearchable: false,
+            isClearable: false,
+            options: pages
+          })
+        ])
+    ]),
+    div({ style: { display: 'block', alignItems: 'left', padding: '1rem', marginTop: '1rem', backgroundColor: 'white', border: 'none', borderRadius: 5, overflowY: 'auto', flexGrow: 1 } }, [
+      table(
+        [
+          thead([
+            tr([td({ style: { align: 'left', fontWeight: 'bold', paddingRight: '1rem' } }, 'Package'),
+              td({ style: { align: 'left', fontWeight: 'bold' } }, 'Version')])
+          ]),
+          tbody(
+            _.keys(packages).map((name, index) => {
+              return [
+                tr({ key: index }, [td({ style: { paddingRight: '1rem', paddingTop: index === 0 ? '1rem' : '0rem' } }, name),
+                  td({ style: { paddingTop: index === 0 ? '1rem' : '0rem' } }, packages[name])])
+              ]
+            }))
+        ])
 
-  render() {
-    const { packageDoc } = this.props
-
-    const pages = this.props.packageDoc ? _.keys(packageDoc) : []
-    let language = ''
-    if (this.props.packageDoc) {
-      language = this.state.language && pages.includes(this.state.language) ? this.state.language : pages[0]
-    }
-    const packages = this.props.packageDoc ? packageDoc[language] : {}
-
-    return h(Fragment, [
-      div({ style: { display: 'flex', alignItems: 'center' } }, [
-        div({ style: { fontWeight: 'bold', marginRight: '1rem' } }, ['Installed packages']),
-        pages.length === 1 ?
-          `(${language})` :
-          div({ style: { width: 120, textTransform: 'capitalize' } }, [
-            h(Select, {
-              'aria-label': 'Select a language',
-              value: language,
-              onChange: ({ value }) => {
-                this.setState({ language: value })
-              },
-              isSearchable: false,
-              isClearable: false,
-              options: pages
-            })
-          ])
-      ]),
-      div({ style: { display: 'block', alignItems: 'left', padding: '1rem', marginTop: '1rem', backgroundColor: 'white', border: 'none', borderRadius: 5, overflowY: 'auto', flexGrow: 1 } }, [
-        table(
-          [
-            thead([
-              tr([td({ style: { align: 'left', fontWeight: 'bold', paddingRight: '1rem' } }, 'Package'),
-                td({ style: { align: 'left', fontWeight: 'bold' } }, 'Version')])
-            ]),
-            tbody(
-              _.keys(packages).map((name, index) => {
-                return [
-                  tr({ key: index }, [td({ style: { paddingRight: '1rem', paddingTop: index === 0 ? '1rem' : '0rem' } }, name),
-                    td({ style: { paddingTop: index === 0 ? '1rem' : '0rem' } }, packages[name])])
-                ]
-              }))
-          ])
-
-      ])
     ])
-  }
+  ])
 }
+
+// export class ImageDepViewezr extends Component {
+//   static propTypes = {
+//     packageDoc: PropTypes.object
+//   }
+//
+//   constructor(props) {
+//     super(props)
+//
+//     const { packageDoc } = props
+//     this.state = { language: _.keys(packageDoc)[0] }
+//   }
+//
+//   render() {
+//     const { packageDoc } = this.props
+//
+//     const pages = this.props.packageDoc ? _.keys(packageDoc) : []
+//     let language = ''
+//     if (this.props.packageDoc) {
+//       language = this.state.language && pages.includes(this.state.language) ? this.state.language : pages[0]
+//     }
+//     const packages = this.props.packageDoc ? packageDoc[language] : {}
+//
+//     return h(Fragment, [
+//       div({ style: { display: 'flex', alignItems: 'center' } }, [
+//         div({ style: { fontWeight: 'bold', marginRight: '1rem' } }, ['Installed packages']),
+//         pages.length === 1 ?
+//           `(${language})` :
+//           div({ style: { width: 120, textTransform: 'capitalize' } }, [
+//             h(Select, {
+//               'aria-label': 'Select a language',
+//               value: language,
+//               onChange: ({ value }) => {
+//                 this.setState({ language: value })
+//               },
+//               isSearchable: false,
+//               isClearable: false,
+//               options: pages
+//             })
+//           ])
+//       ]),
+//       div({ style: { display: 'block', alignItems: 'left', padding: '1rem', marginTop: '1rem', backgroundColor: 'white', border: 'none', borderRadius: 5, overflowY: 'auto', flexGrow: 1 } }, [
+//         table(
+//           [
+//             thead([
+//               tr([td({ style: { align: 'left', fontWeight: 'bold', paddingRight: '1rem' } }, 'Package'),
+//                 td({ style: { align: 'left', fontWeight: 'bold' } }, 'Version')])
+//             ]),
+//             tbody(
+//               _.keys(packages).map((name, index) => {
+//                 return [
+//                   tr({ key: index }, [td({ style: { paddingRight: '1rem', paddingTop: index === 0 ? '1rem' : '0rem' } }, name),
+//                     td({ style: { paddingTop: index === 0 ? '1rem' : '0rem' } }, packages[name])])
+//                 ]
+//               }))
+//           ])
+//
+//       ])
+//     ])
+//   }
+// }
