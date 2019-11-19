e46173c14b1b41b7d2a81ffcc817d0ec3e151d56
Brian Reilly <breilly@broadinstitute.org>
2019-11-19 13:25:05 -0500

    PR feedback

--- src/components/ImportStatus.js
+++ src/components/ImportStatus.js
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 import { h } from 'react-hyperscript-helpers'
 import { clearNotification, notify } from 'src/components/Notifications'
 import { Ajax, useCancellation } from 'src/libs/ajax'
+import { withErrorReporting } from 'src/libs/error'
 import { pfbImportJobStore } from 'src/libs/state'
 import { DEFAULT, delay, switchCase, useAtom, useOnMount } from 'src/libs/utils'
 
@@ -22,38 +23,44 @@ const ImportStatusItem = ({ job, onDone }) => {
   const signal = useCancellation()
 
   useOnMount(() => {
-    const poll = async () => {
+    const poll = withErrorReporting('Problem checking status of PFB data import', async () => {
       while (true) {
         await delay(5000)
-        await isComplete(targetWorkspace, jobId)
+        await checkForCompletion(targetWorkspace, jobId)
       }
-    }
+    })
     poll()
   })
 
-  const isComplete = async ({ namespace, name }, jobId) => {
+  const checkForCompletion = async ({ namespace, name }, jobId) => {
     const { Workspaces } = Ajax(signal)
 
-    try {
-      const response = await Workspaces.workspace(namespace, name).importPFBStatus(jobId)
-      const { message, status } = response
+    const fetchImportStatus = async () => {
+      try {
+        return await Workspaces.workspace(namespace, name).importPFBStatus(jobId)
+      } catch (error) {
+        // Ignore 404; We're probably asking for status before the status endpoint knows about the job
+        if (error.status === 404) {
+          return { status: 'PENDING' }
+        } else {
+          onDone()
+          throw error
+        }
+      }
+    }
 
-      // SR;DN (still running; do nothing)
-      if (status !== 'RUNNING') {
-        switchCase(status,
-          ['SUCCESS', () => notify('success', 'Data imported successfully.',
-            { message: `Data import to workspace "${namespace} / ${name}" is complete. Please refresh the Data view.` })],
-          ['ERROR', () => notify('error', 'Error importing PFB data.', message)],
-          [DEFAULT, () => notify('error', 'Unexpected error importing PFB data', response)]
-        )
-        clearNotification(jobId)
-        onDone()
-      }
-    } catch (error) {
-      // Ignore 404; We're probably asking for status before the status endpoint knows about the job
-      if (error.status !== 404) {
-        throw error
-      }
+    const response = await fetchImportStatus()
+    const { message, status } = response
+
+    if (!_.includes(status, ['PENDING', 'RUNNING'])) {
+      switchCase(status,
+        ['SUCCESS', () => notify('success', 'Data imported successfully.',
+          { message: `Data import to workspace "${namespace} / ${name}" is complete. Please refresh the Data view.` })],
+        ['ERROR', () => notify('error', 'Error importing PFB data.', message)],
+        [DEFAULT, () => notify('error', 'Unexpected error importing PFB data', response)]
+      )
+      clearNotification(jobId)
+      onDone()
     }
   }
 
