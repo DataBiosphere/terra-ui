90b4e2c78f84176994b5ba49c9d9ddf7ad58776e
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-01-02 08:49:54 -0500

    refactor browser storage to allow component connection [SATURN-1286] (#1967)

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,10 +1,7 @@
 import _ from 'lodash/fp'
 import { loadedConfigStore } from 'src/configStore'
 import { configOverridesStore } from 'src/libs/state'
-import * as Utils from 'src/libs/utils'
 
-
-Utils.syncAtomToSessionStorage(configOverridesStore, 'config-overrides')
 
 export const getConfig = () => {
   console.assert(loadedConfigStore.current, 'Called getConfig before initialization')
