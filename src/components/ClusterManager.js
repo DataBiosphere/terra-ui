c15d65113dbcf0e416057e04e57085bb64132536
Emily Hanna <ehanna@broadinstitute.org>
2020-01-10 18:11:04 -0500

    widget name

--- src/components/ClusterManager.js
+++ src/components/ClusterManager.js
@@ -351,7 +351,7 @@ export default class ClusterManager extends PureComponent {
           disabled: isDisabled
         }, [
           div({ style: { marginLeft: '0.5rem', paddingRight: '0.5rem', color: colors.dark() } }, [
-            div({ style: { fontSize: 12, fontWeight: 'bold' } }, 'Application Compute'),
+            div({ style: { fontSize: 12, fontWeight: 'bold' } }, 'Notebook Runtime'),
             div({ style: { fontSize: 10 } }, [
               span({ style: { textTransform: 'uppercase', fontWeight: 500 } }, currentStatus || 'None'),
               ` (${Utils.formatUSD(totalCost)} hr)`
