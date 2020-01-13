d8f7676bf1d35e2b34253ec7492d10dbb5a5e06c
Isaac Zarsky <zarsky@broadinstitute.org>
2020-01-13 15:27:24 -0500

    late breaking updates, import/order now case insensitive

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'plugins': ['lodash-fp', 'react', 'react-hooks', 'simple-import-sort', 'import'],
+  'plugins': ['lodash-fp', 'react', 'react-hooks', 'import'],
   'extends': 'react-app',
   'globals': {
     'SATURN_VERSION': false,
@@ -25,7 +25,7 @@ module.exports = {
     'array-bracket-spacing': 'warn',
     'block-spacing': 'warn',
     'brace-style': ['warn', '1tbs', { 'allowSingleLine': true }],
-    'camelcase': ['warn', {'properties': 'never'}],
+    'camelcase': ['warn', { 'properties': 'never' }],
     'comma-dangle': 'warn',
     'comma-spacing': 'warn',
     'comma-style': 'warn',
@@ -98,9 +98,8 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'warn',
     'react-hooks/exhaustive-deps': 'warn',
 
-    'simple-import-sort/sort': 'warn',
-
     'import/newline-after-import': ['warn', { 'count': 2 }],
-    'import/no-anonymous-default-export': ['warn', { 'allowObject': true }]
+    'import/no-anonymous-default-export': ['warn', { 'allowObject': true }],
+    'import/order': ['warn', { 'alphabetize': { 'order': 'asc', 'caseInsensitive': true } }]
   }
 }
