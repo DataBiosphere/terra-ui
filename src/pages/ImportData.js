5710c8f381fa84dc435791b80e9e61858dd71e28
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-01-13 09:25:38 -0500

    refactor data import page

--- src/pages/ImportData.js
+++ src/pages/ImportData.js
@@ -1,4 +1,5 @@
-import { Component, Fragment } from 'react'
+import _ from 'lodash/fp'
+import { Fragment, useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
 import { backgroundLogo, spinnerOverlay } from 'src/components/common'
 import { notify } from 'src/components/Notifications'
@@ -6,7 +7,7 @@ import TopBar from 'src/components/TopBar'
 import { WorkspaceImporter } from 'src/components/workspace-utils'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
-import { reportError } from 'src/libs/error'
+import { withErrorReporting } from 'src/libs/error'
 import * as Nav from 'src/libs/nav'
 import { pfbImportJobStore } from 'src/libs/state'
 import * as Style from 'src/libs/style'
@@ -27,76 +28,61 @@ const styles = {
   }
 }
 
-class Importer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isImporting: false }
-  }
+const ImportData = () => {
+  const [isImporting, setIsImporting] = useState(false)
+  const { query: { url, format, ad, wid } } = Nav.useRoute()
 
-  render() {
-    const { queryParams: { url, ad, wid } } = this.props
-    const { isImporting } = this.state
+  const onImport = _.flow(
+    Utils.withBusyState(setIsImporting),
+    withErrorReporting('Import Error')
+  )(async ({ namespace, name }) => {
+    await Utils.switchCase(format,
+      ['PFB', async () => {
+        const { jobId } = await Ajax().Workspaces.workspace(namespace, name).importPFB(url)
+        pfbImportJobStore.update(Utils.append({ targetWorkspace: { namespace, name }, jobId }))
+        notify('info', 'Data import in progress.', {
+          id: jobId,
+          message: 'Data will show up incrementally as the job progresses.'
+        })
+      }],
+      ['entitiesJson', async () => {
+        await await Ajax().Workspaces.workspace(namespace, name).importJSON(url)
+        notify('success', 'Data imported successfully.', { timeout: 3000 })
+      }],
+      [Utils.DEFAULT, async () => {
+        await await Ajax().Workspaces.workspace(namespace, name).importBagit(url)
+        notify('success', 'Data imported successfully.', { timeout: 3000 })
+      }]
+    )
+    Nav.goToPath('workspace-data', { namespace, name })
+  })
 
-    return h(Fragment, [
-      backgroundLogo,
-      h(TopBar, { title: 'Import Data' }),
-      div({ role: 'main', style: styles.container }, [
-        div({ style: styles.card }, [
-          div({ style: styles.title }, ['Importing Data']),
-          div({ style: { fontSize: 16 } }, ['From: ', new URL(url).hostname])
-        ]),
-        div({ style: { ...styles.card, marginLeft: '2rem' } }, [
-          div({ style: styles.title }, ['Destination Workspace']),
-          h(WorkspaceImporter, {
-            authorizationDomain: ad,
-            selectedWorkspaceId: wid,
-            onImport: ws => this.import_(ws)
-          }),
-          isImporting && spinnerOverlay
-        ])
+  return h(Fragment, [
+    backgroundLogo,
+    h(TopBar, { title: 'Import Data' }),
+    div({ role: 'main', style: styles.container }, [
+      div({ style: styles.card }, [
+        div({ style: styles.title }, ['Importing Data']),
+        div({ style: { fontSize: 16 } }, ['From: ', new URL(url).hostname])
+      ]),
+      div({ style: { ...styles.card, marginLeft: '2rem' } }, [
+        div({ style: styles.title }, ['Destination Workspace']),
+        h(WorkspaceImporter, {
+          authorizationDomain: ad,
+          selectedWorkspaceId: wid,
+          onImport
+        }),
+        isImporting && spinnerOverlay
       ])
     ])
-  }
-
-  async import_({ namespace, name }) {
-    this.setState({ isImporting: true })
-    const { queryParams: { url, format } } = this.props
-    const wsAjax = Ajax().Workspaces.workspace(namespace, name)
-
-    try {
-      await Utils.switchCase(format,
-        ['PFB', async () => {
-          const { jobId } = await wsAjax.importPFB(url)
-          pfbImportJobStore.update(Utils.append({ targetWorkspace: { namespace, name }, jobId }))
-          notify('info', 'Data import in progress.', {
-            id: jobId,
-            message: 'Data will show up incrementally as the job progresses.'
-          })
-        }],
-        ['entitiesJson', async () => {
-          await wsAjax.importJSON(url)
-          notify('success', 'Data imported successfully.', { timeout: 3000 })
-        }],
-        [Utils.DEFAULT, async () => {
-          await wsAjax.importBagit(url)
-          notify('success', 'Data imported successfully.', { timeout: 3000 })
-        }]
-      )
-      Nav.goToPath('workspace-data', { namespace, name })
-    } catch (e) {
-      reportError('Import Error', e)
-    } finally {
-      this.setState({ isImporting: false })
-    }
-  }
+  ])
 }
-
 
 export const navPaths = [
   {
     name: 'import-data',
     path: '/import-data',
-    component: Importer,
+    component: ImportData,
     title: 'Import Data'
   }
 ]
