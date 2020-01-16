f71af3c6dd89d2912fa776c292834f8895d33e08
Isaac Zarsky <zarsky@broadinstitute.org>
2020-01-15 19:11:28 -0500

    little cleanup

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -150,7 +150,9 @@ const LocalVariablesContent = ({ workspace, workspace: { workspace: { namespace,
     setAttributes(attributes)
   })
 
-  Utils.useOnMount(loadAttributes)
+  Utils.useOnMount(() => {
+    loadAttributes()
+  })
 
   const stopEditing = () => {
     setEditIndex()
@@ -242,92 +244,87 @@ const LocalVariablesContent = ({ workspace, workspace: { workspace: { namespace,
       })
     ]),
     div({ style: { flex: 1 } }, [
-      h(AutoSizer, [
-        ({ width, height }) => h(FlexTable, {
-          width, height, rowCount: amendedAttributes.length,
-          onScroll: y => saveScroll(0, y),
-          initialY,
-          hoverHighlight: true,
-          noContentMessage: _.isEmpty(amendedAttributes) ? 'No Workspace Data defined' : 'No matching data',
-          columns: [
-            {
-              size: { basis: 400, grow: 0 },
-              headerRenderer: () => h(HeaderCell, ['Key']),
-              cellRenderer: ({ rowIndex }) => editIndex === rowIndex ?
-                h(TextInput, {
-                  'aria-label': 'Workspace data key',
-                  autoFocus: true,
-                  value: editKey,
-                  onChange: setEditKey
-                }) :
-                renderDataCell(amendedAttributes[rowIndex][0], namespace)
-            },
-            {
-              size: { grow: 1 },
-              headerRenderer: () => h(HeaderCell, ['Value']),
-              cellRenderer: ({ rowIndex }) => {
-                const [originalKey, originalValue] = amendedAttributes[rowIndex]
+      h(AutoSizer, [({ width, height }) => h(FlexTable, {
+        width, height, rowCount: amendedAttributes.length,
+        onScroll: y => saveScroll(0, y),
+        initialY,
+        hoverHighlight: true,
+        noContentMessage: _.isEmpty(amendedAttributes) ? 'No Workspace Data defined' : 'No matching data',
+        columns: [{
+          size: { basis: 400, grow: 0 },
+          headerRenderer: () => h(HeaderCell, ['Key']),
+          cellRenderer: ({ rowIndex }) => editIndex === rowIndex ?
+            h(TextInput, {
+              'aria-label': 'Workspace data key',
+              autoFocus: true,
+              value: editKey,
+              onChange: setEditKey
+            }) :
+            renderDataCell(amendedAttributes[rowIndex][0], namespace)
+        }, {
+          size: { grow: 1 },
+          headerRenderer: () => h(HeaderCell, ['Value']),
+          cellRenderer: ({ rowIndex }) => {
+            const [originalKey, originalValue] = amendedAttributes[rowIndex]
 
-                return h(Fragment, [
-                  div({ style: { flex: 1, minWidth: 0, display: 'flex' } }, [
-                    editIndex === rowIndex ?
-                      h(TextInput, {
-                        'aria-label': 'Workspace data value',
-                        value: editValue,
-                        onChange: setEditValue
-                      }) :
-                      renderDataCell(originalValue, namespace)
-                  ]),
-                  editIndex === rowIndex ?
-                    h(Fragment, [
-                      h(Select, {
-                        styles: { container: base => ({ ...base, marginLeft: '1rem', width: 150 }) },
-                        isSearchable: false,
-                        isClearable: false,
-                        menuPortalTarget: document.getElementById('root'),
-                        getOptionLabel: ({ value }) => _.startCase(value),
-                        value: editType,
-                        onChange: ({ value }) => setEditType(value),
-                        options: ['string', 'number', 'boolean', 'string list', 'number list', 'boolean list']
-                      }),
-                      h(Link, {
-                        tooltip: Utils.summarizeErrors(inputErrors) || 'Save changes',
-                        disabled: !!inputErrors.length,
-                        style: { marginLeft: '1rem' },
-                        onClick: () => saveAttribute(originalKey)
-                      }, [icon('success-standard', { size: 23 })]),
-                      h(Link, {
-                        tooltip: 'Cancel editing',
-                        style: { marginLeft: '1rem' },
-                        onClick: stopEditing
-                      }, [icon('times-circle', { size: 23 })])
-                    ]) :
-                    div({ className: 'hover-only' }, [
-                      h(Link, {
-                        disabled: !!Utils.editWorkspaceError(workspace),
-                        tooltip: Utils.editWorkspaceError(workspace) || 'Edit variable',
-                        style: { marginLeft: '1rem' },
-                        onClick: () => {
-                          setEditIndex(rowIndex)
-                          setEditValue(_.isObject(originalValue) ? originalValue.items.join(', ') : originalValue)
-                          setEditKey(originalKey)
-                          setEditType(_.isObject(originalValue) ? `${typeof originalValue.items[0]} list` : typeof originalValue)
-                        }
-                      }, [icon('edit', { size: 19 })]),
-                      h(Link, {
-                        'aria-label': 'Delete variable',
-                        disabled: !!Utils.editWorkspaceError(workspace),
-                        tooltip: Utils.editWorkspaceError(workspace) || 'Delete variable',
-                        style: { marginLeft: '1rem' },
-                        onClick: () => setDeleteIndex(rowIndex)
-                      }, [icon('trash', { size: 19 })])
-                    ])
+            return h(Fragment, [
+              div({ style: { flex: 1, minWidth: 0, display: 'flex' } }, [
+                editIndex === rowIndex ?
+                  h(TextInput, {
+                    'aria-label': 'Workspace data value',
+                    value: editValue,
+                    onChange: setEditValue
+                  }) :
+                  renderDataCell(originalValue, namespace)
+              ]),
+              editIndex === rowIndex ?
+                h(Fragment, [
+                  h(Select, {
+                    styles: { container: base => ({ ...base, marginLeft: '1rem', width: 150 }) },
+                    isSearchable: false,
+                    isClearable: false,
+                    menuPortalTarget: document.getElementById('root'),
+                    getOptionLabel: ({ value }) => _.startCase(value),
+                    value: editType,
+                    onChange: ({ value }) => setEditType(value),
+                    options: ['string', 'number', 'boolean', 'string list', 'number list', 'boolean list']
+                  }),
+                  h(Link, {
+                    tooltip: Utils.summarizeErrors(inputErrors) || 'Save changes',
+                    disabled: !!inputErrors.length,
+                    style: { marginLeft: '1rem' },
+                    onClick: () => saveAttribute(originalKey)
+                  }, [icon('success-standard', { size: 23 })]),
+                  h(Link, {
+                    tooltip: 'Cancel editing',
+                    style: { marginLeft: '1rem' },
+                    onClick: stopEditing
+                  }, [icon('times-circle', { size: 23 })])
+                ]) :
+                div({ className: 'hover-only' }, [
+                  h(Link, {
+                    disabled: !!Utils.editWorkspaceError(workspace),
+                    tooltip: Utils.editWorkspaceError(workspace) || 'Edit variable',
+                    style: { marginLeft: '1rem' },
+                    onClick: () => {
+                      setEditIndex(rowIndex)
+                      setEditValue(_.isObject(originalValue) ? originalValue.items.join(', ') : originalValue)
+                      setEditKey(originalKey)
+                      setEditType(_.isObject(originalValue) ? `${typeof originalValue.items[0]} list` : typeof originalValue)
+                    }
+                  }, [icon('edit', { size: 19 })]),
+                  h(Link, {
+                    'aria-label': 'Delete variable',
+                    disabled: !!Utils.editWorkspaceError(workspace),
+                    tooltip: Utils.editWorkspaceError(workspace) || 'Delete variable',
+                    style: { marginLeft: '1rem' },
+                    onClick: () => setDeleteIndex(rowIndex)
+                  }, [icon('trash', { size: 19 })])
                 ])
-              }
-            }
-          ]
-        })
-      ])
+            ])
+          }
+        }]
+      })])
     ]),
     !creatingNewVariable && editIndex === undefined && !Utils.editWorkspaceError(workspace) && h(FloatingActionButton, {
       label: 'ADD VARIABLE',
@@ -340,12 +337,12 @@ const LocalVariablesContent = ({ workspace, workspace: { workspace: { namespace,
       }
     }),
     deleteIndex !== undefined && h(Modal, {
-      onDismiss: () => this.setState({ deleteIndex: undefined }),
+      onDismiss: () => setDeleteIndex(),
       title: 'Are you sure you wish to delete this variable?',
       okButton: h(ButtonPrimary, {
         onClick: _.flow(
           withErrorReporting('Error deleting workspace variable'),
-          Utils.withBusyState(v => this.setState({ saving: v }))
+          Utils.withBusyState(setBusy)
         )(async () => {
           setDeleteIndex()
           await Ajax().Workspaces.workspace(namespace, name).deleteAttributes([amendedAttributes[deleteIndex][0]])
