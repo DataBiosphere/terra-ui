6840650aef4fe607cd8b9c54abf35b884717fd68
ehanna4 <54322292+ehanna4@users.noreply.github.com>
2020-01-16 16:54:15 -0500

    Add ability to rename a workflow when importing from dockstore [SATURN-1145] (#1990)

--- src/components/workspace-utils.js
+++ src/components/workspace-utils.js
@@ -59,7 +59,7 @@ export const WorkspaceSelector = ({ workspaces, value, onChange, ...props }) => 
 export const WorkspaceImporter = _.flow(
   Utils.withDisplayName('WorkspaceImporter'),
   withWorkspaces
-)(({ workspaces, refreshWorkspaces, onImport, authorizationDomain: ad, selectedWorkspaceId: initialWs }) => {
+)(({ workspaces, refreshWorkspaces, onImport, authorizationDomain: ad, selectedWorkspaceId: initialWs, additionalErrors }) => {
   const [selectedWorkspaceId, setSelectedWorkspaceId] = useState(initialWs)
   const [creatingWorkspace, setCreatingWorkspace] = useState(false)
 
@@ -69,18 +69,23 @@ export const WorkspaceImporter = _.flow(
     h(WorkspaceSelector, {
       workspaces: _.filter(ws => {
         return Utils.canWrite(ws.accessLevel) &&
-            (!ad || _.some({ membersGroupName: ad }, ws.workspace.authorizationDomain))
+          (!ad || _.some({ membersGroupName: ad }, ws.workspace.authorizationDomain))
       }, workspaces),
       value: selectedWorkspaceId,
       onChange: setSelectedWorkspaceId
     }),
     div({ style: { display: 'flex', alignItems: 'center', marginTop: '1rem' } }, [
       h(ButtonPrimary, {
-        disabled: !selectedWorkspace,
+        disabled: !selectedWorkspace || additionalErrors,
+        tooltip: Utils.cond([!selectedWorkspace, 'Select valid a workspace to import'],
+          [additionalErrors, Utils.summarizeErrors(additionalErrors)],
+          'Import workflow to workspace'
+        ),
         onClick: () => onImport(selectedWorkspace.workspace)
       }, ['Import']),
       div({ style: { marginLeft: '1rem', whiteSpace: 'pre' } }, ['Or ']),
       h(Link, {
+        disabled: additionalErrors,
         onClick: () => setCreatingWorkspace(true)
       }, ['create a new workspace'])
     ]),
