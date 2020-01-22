d298a33cd25b0054d646f6035fbddf002a5b7d71
Isaac Zarsky <zarsky@broadinstitute.org>
2020-01-22 18:19:38 -0500

    Clean up import template workspace
    Co-Authored-By: ehanna4 <ehanna4@users.noreply.github.com>

--- src/pages/ImportData.js
+++ src/pages/ImportData.js
@@ -1,23 +1,19 @@
 import _ from 'lodash/fp'
 import { Fragment, useState } from 'react'
-import { div, h, img, span } from 'react-hyperscript-helpers'
-import { withRequesterPaysHandler } from 'src/components/bucket-utils'
+import { div, h, img } from 'react-hyperscript-helpers'
 import Collapse from 'src/components/Collapse'
-import { backgroundLogo, ButtonPrimary, ButtonSecondary, Clickable, Link, RadioButton, Select, spinnerOverlay } from 'src/components/common'
-import { icon } from 'src/components/icons'
+import { backgroundLogo, ButtonPrimary, ButtonSecondary, Clickable, RadioButton, spinnerOverlay } from 'src/components/common'
+import { icon, wdlIcon } from 'src/components/icons'
 import NewWorkspaceModal from 'src/components/NewWorkspaceModal'
 import { notify } from 'src/components/Notifications'
 import TopBar from 'src/components/TopBar'
 import { useWorkspaces, WorkspaceSelector } from 'src/components/workspace-utils'
-import TemplateWorkspaces from 'src/data/template-workspaces'
-import TempTestTemplates from 'src/data/tempTestTemplateWorkspaces.json'
 import jupyterLogo from 'src/images/jupyter-logo.svg'
-import rLogo from 'src/images/r-logo.svg'
-import wdlLogo from 'src/images/wdl-logo.png'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
-import { isDatastage } from 'src/libs/config'
+import { getConfig } from 'src/libs/config'
 import { withErrorReporting } from 'src/libs/error'
+import { getAppName } from 'src/libs/logos'
 import * as Nav from 'src/libs/nav'
 import { pfbImportJobStore } from 'src/libs/state'
 import * as Style from 'src/libs/style'
@@ -38,16 +34,15 @@ const styles = {
   }
 }
 
-const ChoiceButton = ({ iconName, title, detail, style, ...props }) => {
+const ChoiceButton = ({ onClick, iconName, title, detail }) => {
   return h(Clickable, {
+    onClick,
     style: {
-      ...style,
-      padding: '1rem',
+      padding: '1rem', marginTop: '1rem',
       display: 'flex', alignItems: 'center',
       border: `1px solid ${colors.accent()}`, borderRadius: 4
     },
-    hover: { backgroundColor: colors.accent(0.1) },
-    ...props
+    hover: { backgroundColor: colors.accent(0.1) }
   }, [
     icon(iconName, { size: 24, style: { flex: 'none', marginRight: '1rem' } }),
     div({ style: { flex: 1 } }, [
@@ -68,14 +63,22 @@ const ImportData = () => {
   const [selectedWorkspaceId, setSelectedWorkspaceId] = useState(wid)
   const [selectedTemplateWorkspace, setSelectedTemplateWorkspace] = useState()
   const [selected, setSelected] = useState()
-  const [workspaceNotebooks, setWorkspaceNotebooks] = useState()
+  const [templateWorkspaces, setTemplateWorkspaces] = useState()
+
+  Utils.useOnMount(() => {
+    const loadTemplateWorkspaces = async () => {
+      try {
+        const allTemplates = await fetch(`${getConfig().firecloudBucketRoot}/template-workspaces.json`).then(res => res.json())
+        setTemplateWorkspaces(allTemplates[getAppName()] || null)
+      } catch (e) {
+        setTemplateWorkspaces(null)
+      }
+    }
+
+    loadTemplateWorkspaces()
+  })
 
   const selectedWorkspace = _.find({ workspace: { workspaceId: selectedWorkspaceId } }, workspaces)
-
-  const hasTemplateWorkspaces = Utils.cond(
-    [isDatastage() && _.has('DataStage', TemplateWorkspaces), true],
-    () => false
-  )
 
   const setSelectedTemp = ws => {
     setSelected(ws)
@@ -108,62 +111,16 @@ const ImportData = () => {
         })
       }],
       ['entitiesJson', async () => {
-        await await Ajax().Workspaces.workspace(namespace, name).importJSON(url)
+        await Ajax().Workspaces.workspace(namespace, name).importJSON(url)
         notify('success', 'Data imported successfully.', { timeout: 3000 })
       }],
       [Utils.DEFAULT, async () => {
-        await await Ajax().Workspaces.workspace(namespace, name).importBagit(url)
+        await Ajax().Workspaces.workspace(namespace, name).importBagit(url)
         notify('success', 'Data imported successfully.', { timeout: 3000 })
       }]
     )
     Nav.goToPath('workspace-data', { namespace, name })
   })
-
-
-  // const i  = async ({ namespace, name }) => {
-  //
-  //
-  //       const { jobId } = await Ajax().Workspaces.workspace(namespace, name).importPFB(url)
-  //       pfbImportJobStore.update(Utils.append({ targetWorkspace: { namespace, name }, jobId }))
-  //       notify('info', 'Data import in progress.', {
-  //         id: jobId,
-  //         message: 'Data will show up incrementally as the job progresses.'
-  //       })
-  //     }
-
-
-  const isNotebooks2 = ws => {
-
-    const loadNotebookNames = _.flow(
-      withErrorReporting('Error loading notebooks')
-    )(async () => {
-      const deets = await Ajax().Workspaces.workspace(ws.namespace, ws.name).details([
-        'workspace.namespace', 'workspace.bucketName'
-      ])
-      const notebooks = await Ajax().Buckets.listNotebooks(deets.workspace.namespace, deets.workspace.bucketName)
-      console.log(notebooks)
-      // slice removes 'notebooks/' and the .ipynb suffix
-      notebooks.length > 0 ? setWorkspaceNotebooks(notebooks) : setWorkspaceNotebooks()
-      console.log(workspaceNotebooks)
-    })
-
-    loadNotebookNames()
-  }
-
-
-  // const isNotebooks = async ws => {
-  //   try {
-  //     const deets = await Ajax().Workspaces.workspace(ws.namespace, ws.name).details([
-  //       'workspace.namespace', 'workspace.bucketName'
-  //     ])
-  //     const notebooks = await Ajax().Buckets.listNotebooks(deets.workspace.namespace, deets.workspace.bucketName)
-  //     console.log('here')
-  //     notebooks.length > 0 ? setWorkspaceNotebooks(notebooks) : setWorkspaceNotebooks()
-  //   } catch {
-  //     console.log('error')
-  //   }
-  // }
-
 
   return h(Fragment, [
     backgroundLogo,
@@ -205,54 +162,43 @@ const ImportData = () => {
           ['template', () => {
             return h(Fragment, [
               div({ style: styles.title }, ['Start with a template']),
-              div({ style: { marginBottom: '1rem' } },
-                ['Note that the import process may take some time after you are redirected into your destination workspace.']),
+              div({ style: { marginBottom: '1rem' } }, [
+                'Note that the import process may take some time after you are redirected into your destination workspace.'
+              ]),
               div({ style: { overflow: 'auto', height: '20rem' } }, [
-                _.map(ws => (
-                  div({ style: { margin: '1rem 0rem 0rem' }, key: `${ws.name}+${ws.namespace}` }, [
-                    div({ style: { display: 'flex' } }, [h(RadioButton, {
-                      checked: _.isEqual(ws, selected),
-                      onChange: () => setSelectedTemp(ws)
-                    }),
-                    h(Collapse, {
-                      onClick: () => isNotebooks2(ws),
-                      buttonStyle: { color: colors.dark(), fontWeight: '600' },
-                      style: { fontSize: '16', marginLeft: '0.5rem' },
-                      title: ws.name
-                    }, [
-                      div([workspaceNotebooks ? _.sum(workspaceNotebooks) : ''])
-                      // isNotebooks(ws)
-            //  Ajax().Workspaces.workspace(ws.namespace, ws.name).details([
-            //   'workspace.namespace', 'workspace.bucketName'
-            // ])
-                    //   div({ style: { margin: '1rem 0.5rem 1rem' } }, [
-                    //   ws.description,
-                    //   workspaceNotebooks && div({ style: { fontSize: 12 } }, [
-                    //     img({ src: jupyterLogo, style: { height: 22, width: 22, marginRight: '0.5rem' } }),
-                    //     ws.notebooks
-                    //   ])
-                    // ])
-                    ])]),
-                    div({ style: { borderBottom: `1px solid` } }, [
-                      div({ style: { margin: '0 0 1rem 2rem', display: 'flex', alignContent: 'center' } }, [
+                _.map(([i, ws]) => {
+                  const { name, namespace, description, hasNotebooks, hasWorkflows } = ws
+                  const isSelected = _.isEqual(ws, selected)
 
-                        // isNotebooks(ws) &&
-                        //   ws.workflows && div({ style: { fontSize: 12 } }, [
-                        //     img({ src: wdlLogo, style: { height: 22, width: 22, margin: '0 0.5rem' } }),
-                        //     ws.workflows
-                        //   ])
-
-
-                      ])
-                    ])
+                  return div({
+                    key: `${name}/${namespace}`,
+                    style: {
+                      display: 'flex', alignItems: 'baseline',
+                      marginBottom: '1rem',
+                      ...(i > 0 ? { borderTop: Style.standardLine, paddingTop: '1rem' } : {})
+                    }
+                  }, [
+                    h(RadioButton, {
+                      name: 'select-template',
+                      checked: isSelected,
+                      onChange: () => setSelectedTemp(ws),
+                      text: h(Collapse, {
+                        buttonStyle: { color: colors.dark(), fontWeight: 600 },
+                        style: { fontSize: 16, marginLeft: '0.5rem' },
+                        title: h(Fragment, [
+                          name,
+                          hasWorkflows && wdlIcon({ style: { height: 22, width: 22, marginLeft: '0.5rem' } }),
+                          hasNotebooks && img({ src: jupyterLogo, style: { height: 22, width: 22, marginLeft: '0.5rem' } })
+                        ])
+                      }, [description])
+                    })
                   ]
-                  )), TempTestTemplates
-                )
+                  )
+                }, Utils.toIndexPairs(templateWorkspaces))
               ]),
               div({ style: { margin: '0 -2rem', borderTop: `0.5px solid ${colors.dark(0.2)}` } }),
               div({ style: { display: 'flex', alignItems: 'center', marginTop: '1rem' } }, [
                 h(ButtonSecondary, { style: { marginLeft: 'auto' }, onClick: () => setMode() }, ['Back']),
-                console.log(selectedTemplateWorkspace),
                 h(ButtonPrimary, {
                   style: { marginLeft: '3rem' },
                   disabled: !selectedTemplateWorkspace,
@@ -265,22 +211,19 @@ const ImportData = () => {
             return h(Fragment, [
               div({ style: styles.title }, ['Destination Workspace']),
               div({ style: { marginTop: '0.5rem' } }, ['Choose the option below that best suits your needs.']),
-              hasTemplateWorkspaces && h(ChoiceButton, {
-                style: { marginTop: '1rem' },
+              !!templateWorkspaces && h(ChoiceButton, {
                 onClick: () => setMode('template'),
                 iconName: 'copy',
                 title: 'Start with a template',
                 detail: 'Clone from one of our template workspaces that has analyses ready for use'
               }),
               h(ChoiceButton, {
-                style: { marginTop: '1rem' },
                 onClick: () => setMode('existing'),
                 iconName: 'folder-open',
                 title: 'Start with an existing workspace',
                 detail: 'Select one of your workspaces'
               }),
               h(ChoiceButton, {
-                style: { marginTop: '1rem' },
                 onClick: () => setIsCreateOpen(true),
                 iconName: 'plus-circle',
                 title: 'Start with a new workspace',
@@ -311,7 +254,7 @@ const ImportData = () => {
             onImport(w)
           }
         }),
-        isImporting && spinnerOverlay
+        (isImporting || templateWorkspaces === undefined) && spinnerOverlay
       ])
     ])
   ])
