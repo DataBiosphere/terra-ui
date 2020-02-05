2aab5731cbe864a1445c0255ea54e9c80c6d1ee5
ehanna4 <54322292+ehanna4@users.noreply.github.com>
2020-02-05 11:41:52 -0500

    Change tooltip language [SATURN-1221] (#2023)

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -520,7 +520,7 @@ const ToolDrawer = _.flow(
               onClick: () => setToolMode('Notebook'),
               disabled: !notebookButtonEnabled,
               tooltip: Utils.cond(
-                [!entityMetadata.cohort, () => 'Talk to your dataset owner about setting up a Data Explorer. See the "Making custom cohorts with Data Explorer" help article.'],
+                [!entityMetadata.cohort, () => 'Unable to open with notebooks. See the "Making custom cohorts with Data Explorer" help article for more details.'],
                 [isCohort && entitiesCount > 1, () => 'Select exactly one cohort to open in notebook'],
                 [!isCohort, () => 'Only cohorts can be opened with notebooks'],
                 [notebookButtonEnabled, () => 'Create a Python 2 or 3 notebook with this cohort']
