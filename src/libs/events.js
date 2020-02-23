adf1462fa56c29a71d0bceee5c6edc7e2ce04b12
Isaac Zarsky <zarsky@broadinstitute.org>
2020-02-23 12:44:13 -0500

    send event on every page view

--- src/libs/events.js
+++ src/libs/events.js
@@ -1,3 +1,26 @@
-export default {
-  workspaceShare: 'workspace:share'
+import { useEffect } from 'react'
+import { Ajax } from 'src/libs/ajax'
+import { useRoute } from 'src/libs/nav'
+import { authStore } from 'src/libs/state'
+import * as Utils from 'src/libs/utils'
+
+
+const eventsList = {
+  workspaceShare: 'workspace:share',
+  pageView: 'page:view'
 }
+
+export const PageViewReporter = () => {
+  const { name } = useRoute()
+  const { isSignedIn } = Utils.useStore(authStore)
+
+  useEffect(() => {
+    if (isSignedIn) {
+      Ajax().Metrics.captureEvent(eventsList.pageView)
+    }
+  }, [isSignedIn, name])
+
+  return null
+}
+
+export default eventsList
