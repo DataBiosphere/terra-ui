d6305d4d4f737ae8d4973f902f7723fc9929950a
Cameron Ardell <cardell@broadinstitute.org>
2020-04-06 12:09:02 -0400

    Update FooterWrapper.js

--- src/components/FooterWrapper.js
+++ src/components/FooterWrapper.js
@@ -1,7 +1,9 @@
+import { Fragment } from 'react'
 import { a, div, h } from 'react-hyperscript-helpers'
 import { Link } from 'src/components/common'
 import { icon } from 'src/components/icons'
 import colors from 'src/libs/colors'
+import { isBioDataCatalyst } from 'src/libs/config'
 import { footerLogo } from 'src/libs/logos'
 import * as Nav from 'src/libs/nav'
 import * as Utils from 'src/libs/utils'
@@ -31,23 +33,36 @@ const FooterWrapper = ({ children }) => {
       h(Link, { href: Nav.getLink('root') }, [
         footerLogo()
       ]),
-      a({ href: Nav.getLink('privacy'), style: styles.item }, 'Privacy Policy'),
-      div({ style: styles.item }, '|'),
-      a({ href: Nav.getLink('terms-of-service'), style: styles.item }, 'Terms of Service'),
-      div({ style: styles.item }, '|'),
-      a({ href: 'https://support.terra.bio/hc/en-us/articles/360030793091-Terra-FireCloud-Security-Posture', target: '_blank', style: styles.item },
-        ['Security', icon('pop-out', { size: 12, style: { marginLeft: '0.5rem' } })]),
-      div({ style: styles.item }, '|'),
-      a({
-        href: 'https://support.terra.bio/hc/en-us', ...Utils.newTabLinkProps,
-        style: { ...styles.item, display: 'flex', alignItems: 'center' }
-      }, [
-        'Documentation', icon('pop-out', { size: 12, style: { marginLeft: '0.5rem' } })
-      ]),
-      div({ style: { flexGrow: 1 } }),
-      div({ onClick: () => Nav.goToPath('hall-of-fame'), style: { fontWeight: 600, fontSize: '10px' } }, [
-        `Copyright ©${buildTimestamp.getFullYear()}`
-      ])
+      isBioDataCatalyst() ?
+        div({ style: { display: 'grid', gridTemplateColumns: 'repeat(4, auto)', gap: '0.5rem 2rem', fontSize: 10 } }, [
+          a({ href: 'https://biodatacatalyst.nhlbi.nih.gov/privacy', ...Utils.newTabLinkProps }, 'Privacy Policy'),
+          a({ href: 'https://www.nhlbi.nih.gov/about/foia-fee-for-service-office', ...Utils.newTabLinkProps }, 'Freedom of Information Act (FOIA)'),
+          a({ href: 'https://biodatacatalyst.nhlbi.nih.gov/accessibility', ...Utils.newTabLinkProps }, 'Accessibility'),
+          a({ href: 'https://www.hhs.gov/', ...Utils.newTabLinkProps }, 'U.S. Department of Health &  Human Services'),
+          a({ href: 'https://osp.od.nih.gov/scientific-sharing/policies/', ...Utils.newTabLinkProps }, 'Data Sharing Policy'),
+          a({ href: 'https://www.nih.gov/', ...Utils.newTabLinkProps }, 'National Institutes of Health'),
+          a({ href: 'https://www.usa.gov/', ...Utils.newTabLinkProps }, 'USA.gov'),
+          a({ href: 'https://osp.od.nih.gov/scientific-sharing/policies/', ...Utils.newTabLinkProps }, 'National Heart, Lung, and Blood Institute')
+        ]) :
+        h(Fragment, [
+          a({ href: Nav.getLink('privacy'), style: styles.item }, 'Privacy Policy'),
+          div({ style: styles.item }, '|'),
+          a({ href: Nav.getLink('terms-of-service'), style: styles.item }, 'Terms of Service'),
+          div({ style: styles.item }, '|'),
+          a({ href: 'https://support.terra.bio/hc/en-us/articles/360030793091-Terra-FireCloud-Security-Posture', ...Utils.newTabLinkProps, style: styles.item },
+            ['Security', icon('pop-out', { size: 12, style: { marginLeft: '0.5rem' } })]),
+          div({ style: styles.item }, '|'),
+          a({
+            href: 'https://support.terra.bio/hc/en-us', ...Utils.newTabLinkProps,
+            style: { ...styles.item, display: 'flex', alignItems: 'center' }
+          }, [
+            'Documentation', icon('pop-out', { size: 12, style: { marginLeft: '0.5rem' } })
+          ]),
+          div({ style: { flexGrow: 1 } }),
+          div({ onClick: () => Nav.goToPath('hall-of-fame'), style: { fontWeight: 600, fontSize: '10px' } }, [
+            `Copyright ©${buildTimestamp.getFullYear()}`
+          ])
+        ])
     ])
   ])
 }
