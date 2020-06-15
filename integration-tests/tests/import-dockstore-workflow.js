0b00d951e2f5e98aaaa0a6bdc92c9085decb9aae
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-06-15 09:04:11 -0400

    tidy up wrapper usage in integration tests (#2163)

--- integration-tests/tests/import-dockstore-workflow.js
+++ integration-tests/tests/import-dockstore-workflow.js
@@ -1,3 +1,4 @@
+const _ = require('lodash/fp')
 const fetch = require('node-fetch')
 const { withWorkspace } = require('../utils/integration-helpers')
 const { click, clickable, dismissNotifications, findText, select, signIntoTerra } = require('../utils/integration-utils')
@@ -6,23 +7,29 @@ const { withUserToken } = require('../utils/terra-sa-utils')
 
 const testWorkflowIdentifier = 'github.com/DataBiosphere/topmed-workflows/UM_variant_caller_wdl:1.31.0'
 
-const testImportDockstoreWorkflowFn = withUserToken(async options => {
-  const { page, testUrl } = options
+const withDockstoreCheck = test => async options => {
+  const { testUrl } = options
   const { dockstoreUrlRoot } = await fetch(`${testUrl}/config.json`).then(res => res.json())
-
-  if (await fetch(`${dockstoreUrlRoot}/api/api/ga4gh/v1/metadata`).then(res => res.status !== 200)) {
-    console.error('Skipping dockstore test, API appears to be down')
+  const res = await fetch(`${dockstoreUrlRoot}/api/api/ga4gh/v1/metadata`)
+  if (res.status === 200) {
+    await test(options)
   } else {
-    await withWorkspace(async ({ token, workspaceName }) => {
-      await page.goto(`${testUrl}/#import-tool/dockstore/${testWorkflowIdentifier}`)
-      await signIntoTerra(page, token)
-      await dismissNotifications(page)
-      await findText(page, 'workflow TopMedVariantCaller')
-      await select(page, 'Select a workspace', workspaceName)
-      await click(page, clickable({ text: 'Import' }))
-      await findText(page, testWorkflowIdentifier)
-    })(options)
+    console.error('Skipping dockstore test, API appears to be down')
   }
+}
+
+const testImportDockstoreWorkflowFn = _.flow(
+  withWorkspace,
+  withUserToken,
+  withDockstoreCheck
+)(async ({ page, testUrl, token, workspaceName }) => {
+  await page.goto(`${testUrl}/#import-tool/dockstore/${testWorkflowIdentifier}`)
+  await signIntoTerra(page, token)
+  await dismissNotifications(page)
+  await findText(page, 'workflow TopMedVariantCaller')
+  await select(page, 'Select a workspace', workspaceName)
+  await click(page, clickable({ text: 'Import' }))
+  await findText(page, testWorkflowIdentifier)
 })
 
 const testImportDockstoreWorkflow = {
