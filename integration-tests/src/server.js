346c10fd70a15cb77c50b4c5ce10f71c46ad2952
Scout Rubenstein <brubenst@broadinstitute.org>
2020-06-17 12:00:34 -0400

    Log to firestore

--- integration-tests/src/server.js
+++ integration-tests/src/server.js
@@ -1,5 +1,6 @@
 const _ = require('lodash/fp')
 const express = require('express')
+const { Firestore } = require('@google-cloud/firestore')
 const http = require('http')
 const { promiseHandler, Response, withAuth, withPuppeteer } = require('./utils')
 const { testFindWorkflow } = require('../tests/find-workflow')
@@ -20,26 +21,44 @@ const app = express()
 const server = http.createServer(app)
 server.setTimeout(serverTimeout)
 
+const firestore = new Firestore()
+
 const testTimeout = async timeout => {
   await delay(timeout)
   throw new Error(`Test timeout after ${timeout}ms`)
 }
 
+const reportTest = (testGroup, startTime, host, errorMessage) => {
+  const endTime = new Date()
+  const testReport = { testGroup, runtimeInMilliseconds: endTime - startTime, status: errorMessage ? 'error' : 'success', errorMessage, host }
+  firestore.collection('tests').doc().create(testReport)
+}
+
 const runTest = fn => withPuppeteer(async ({ browser, context, name, page, req, timeout }) => {
   const targetEnvParams = { ...envs[req.query.targetEnv] }
+  const testGroup = req.query.testGroup
+  const startTime = new Date()
+  const host = req.headers.host
+  if (host === 'terra-bueller.appspot.com') {
+    throw new Error('Do not call without a version specified')
+  }
   try {
     const test = withScreenshot(name, fn)
+    throw new Error(`Test timeout after ${timeout}ms`)
     const result = await Promise.race([
       test({ browser, context, page, ...targetEnvParams }),
       testTimeout(timeout)
     ])
+    reportTest(testGroup, startTime, host, null)
     return new Response(200, result)
   } catch (e) {
-    return new Response(200, {
+    const error = {
       name: e.name,
       message: e.message,
       stack: e.stack
-    })
+    }
+    reportTest(testGroup, startTime, host, error)
+    return new Response(200, error)
   }
 })
 
