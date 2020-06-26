9d77113b285b597e005c1d06950bfc3acc0d9127
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-06-26 09:43:47 -0400

    refactor workflow launch code

--- src/libs/analysis.js
+++ src/libs/analysis.js
@@ -1,91 +1,63 @@
 import _ from 'lodash/fp'
 import { Ajax } from 'src/libs/ajax'
-import { reportError } from 'src/libs/error'
+import * as Utils from 'src/libs/utils'
 
 
-const isSet = entityType => _.endsWith('_set', entityType)
-
-/**
- * Conditions this needs to handle:
- *
- * Method config "rootEntityType": sample (or other singular type)
- *   input:
- *     1 entity - create submission with a single job
- *       entityType: "sample"
- *       entityName: entity name
- *       expression: ""
- *     set - create submission with set with expression
- *       entityType: "sample_set"
- *       entityName: sample set name
- *       expression: "samples"
- *     many entities - create set, then create submission with set with expression
- *       entityType: "sample_set"
- *       entityName: sample set name
- *       expression: "samples"
- * Method config: sample_set (or other set type)
- *   input:
- *     set - create submission with set
- *       entityType: "sample_set"
- *       entityName: sample set name
- *       expression: ""
- */
 export const launch = async ({
-  workspaceNamespace, workspaceName,
+  workspace: { workspace: { namespace, name, bucketName }, accessLevel },
   config: { namespace: configNamespace, name: configName, rootEntityType },
-  entityType, entityNames, newSetName, expression, useCallCache = true, deleteIntermediateOutputFiles,
-  onCreateSet, onLaunch, onSuccess, onFailure
+  entityType, entityNames, newSetName, useCallCache = true, deleteIntermediateOutputFiles,
+  onProgress
 }) => {
-  const workspace = Ajax().Workspaces.workspace(workspaceNamespace, workspaceName)
-  const methodConfig = workspace.methodConfig(configNamespace, configName)
-
-  const launchParams = await resolveLaunchParams({ entityNames, entityType, onCreateSet, newSetName, rootEntityType, workspace })
-  if (!!launchParams) {
-    try {
-      !!onLaunch && onLaunch()
-      const { submissionId } = await methodConfig.launch({ ...launchParams, useCallCache, deleteIntermediateOutputFiles })
-      onSuccess(submissionId)
-    } catch (error) {
-      onFailure(error)
-    }
-  }
-}
-
-const resolveLaunchParams = async ({ entityNames, entityType, onCreateSet, newSetName, rootEntityType, workspace }) => {
-  const entityName = _.head(entityNames)
-
-  if (_.isEmpty(entityNames)) {
-    reportError('No entities selected')
-  } else if (isSet(rootEntityType)) {
-    if (_.size(entityNames) > 1) {
-      reportError('Cannot create a submission for multiple entity sets')
-    } else if (entityType !== rootEntityType) {
-      reportError(`Cannot use ${entityType} with method config that needs ${rootEntityType}`)
-    } else {
-      return { entityType: rootEntityType, entityName }
-    }
-  } else if (isSet(entityType)) {
-    return { entityType, entityName, expression: `this.${rootEntityType}s` }
-  } else if (_.size(entityNames) === 1) {
-    return { entityType, entityName }
-  } else {
-    const setType = `${entityType}_set`
-    const set = {
+  const createSet = () => {
+    onProgress('createSet')
+    return Ajax().Workspaces.workspace(namespace, name).createEntity({
       name: newSetName,
-      entityType: setType,
+      entityType: `${entityType}_set`,
       attributes: {
-        [`${rootEntityType}s`]: {
+        [`${entityType}s`]: {
           itemsType: 'EntityReference',
-          items: _.map(entityName => ({ entityName, entityType: rootEntityType }), entityNames)
+          items: _.map(entityName => ({ entityName, entityType }), entityNames)
         }
       }
-    }
-    try {
-      !!onCreateSet && onCreateSet()
-      await workspace.createEntity(set)
-    } catch (error) {
-      reportError('Error creating entity set', error)
-      return
-    }
-    return { entityType: setType, entityName: newSetName, expression: `this.${rootEntityType}s` }
+    })
   }
+  onProgress('checkBucketAccess')
+  try {
+    await Ajax().Workspaces.workspace(namespace, name).checkBucketAccess(bucketName, accessLevel)
+  } catch (error) {
+    throw new Error('Error confirming workspace bucket access. This may be a transient problem. Please try again in a few minutes. If the problem persists, please contact support.')
+  }
+  const { entityName, processSet = false } = await Utils.cond(
+    [entityType === undefined, () => ({})],
+    [`${entityType}_set` === rootEntityType, async () => {
+      await createSet()
+      return { entityName: newSetName }
+    }],
+    [entityType === rootEntityType, async () => {
+      if (_.size(entityNames) === 1) {
+        return { entityName: entityNames[0] }
+      } else {
+        await createSet()
+        return { entityName: newSetName, processSet: true }
+      }
+    }],
+    [entityType === `${rootEntityType}_set`, () => {
+      if (_.size(entityNames) > 1) {
+        throw new Error('Cannot launch against multiple sets')
+      }
+      return { entityName: entityNames[0], processSet: true }
+    }]
+  )
+  onProgress('launch')
+  return Ajax().Workspaces.workspace(namespace, name).methodConfig(configNamespace, configName).launch({
+    entityType: Utils.cond(
+      [!entityName, () => undefined],
+      [processSet, () => `${rootEntityType}_set`],
+      () => rootEntityType
+    ),
+    entityName,
+    expression: processSet ? `this.${rootEntityType}s` : undefined,
+    useCallCache, deleteIntermediateOutputFiles
+  })
 }
