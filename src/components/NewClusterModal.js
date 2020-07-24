076ce0cf643af89d7a1ced62f3a5de50239518b4
Justin Canas <justincanas16@gmail.com>
2020-07-24 10:43:58 -0400

    [IA-2029] add community section to dropdown (#2189)
    * add community section to dropdown
    
    * indent
    
    * lint
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- src/components/NewClusterModal.js
+++ src/components/NewClusterModal.js
@@ -332,11 +332,17 @@ export const NewClusterModal = withModalDrawer({ width: 675 })(class NewClusterM
       onChange: onEnvChange,
       isSearchable: true,
       isClearable: false,
-      options: [{ label: 'JUPYTER ENVIRONMENTS', options: _.map(({ label, image }) => ({ label, value: image }), leoImages) },
+      options: [
         {
-          label: 'OTHER ENVIRONMENTS',
-          options: [{ label: 'Custom Environment', value: CUSTOM_MODE }, { label: 'Project-Specific Environment', value: PROJECT_SPECIFIC_MODE }]
-        }]
+          label: 'JUPYTER ENVIRONMENTS', options: _.map(({ label, image }) => ({ label, value: image }),
+            _.filter(({ isCommunity }) => !isCommunity, leoImages))
+        },
+        {
+          label: 'COMMUNITY-MAINTAINED JUPYTER ENVIRONMENTS (verified partners)', options: _.map(({ label, image }) => ({ label, value: image }),
+            _.filter(({ isCommunity }) => isCommunity, leoImages))
+        },
+        { label: 'OTHER ENVIRONMENTS', options: [{ label: 'Custom Environment', value: CUSTOM_MODE }, { label: 'Project-Specific Environment', value: PROJECT_SPECIFIC_MODE }] }
+      ]
     })
 
     const makeImageInfo = style => div({ style: { whiteSpace: 'pre', ...style } }, [
