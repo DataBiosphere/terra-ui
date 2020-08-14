e569d60d6afdc350d98aa60f8fff976858468d19
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-08-14 14:08:24 -0400

    use SimpleFlexTable instead of FlexTable, since we're going to have two tables on the page and stacking them will be easier this way

--- src/pages/Clusters.js
+++ src/pages/Clusters.js
@@ -1,12 +1,11 @@
 import _ from 'lodash/fp'
 import { Fragment, useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
-import { AutoSizer } from 'react-virtualized'
 import { ClusterErrorModal, DeleteClusterModal } from 'src/components/ClusterManager'
 import { Clickable, Link, spinnerOverlay } from 'src/components/common'
 import FooterWrapper from 'src/components/FooterWrapper'
 import { icon } from 'src/components/icons'
-import { FlexTable, Sortable } from 'src/components/table'
+import { SimpleFlexTable, Sortable } from 'src/components/table'
 import TooltipTrigger from 'src/components/TooltipTrigger'
 import TopBar from 'src/components/TopBar'
 import { Ajax } from 'src/libs/ajax'
@@ -61,78 +60,76 @@ const Clusters = () => {
     div({ role: 'main', style: { padding: '1rem', flex: 1, display: 'flex', flexDirection: 'column' } }, [
       div({ style: { ...Style.elements.sectionHeader, textTransform: 'uppercase', marginBottom: '1rem' } }, ['Your notebook runtimes']),
       div({ style: { flex: 1 } }, [
-        clusters && h(AutoSizer, [
-          ({ width, height }) => h(FlexTable, {
-            width, height, rowCount: filteredClusters.length,
-            columns: [
-              {
-                headerRenderer: () => h(Sortable, { sort, field: 'project', onSort: setSort }, ['Billing project']),
-                cellRenderer: ({ rowIndex }) => {
-                  const cluster = filteredClusters[rowIndex]
-                  const inactive = !_.includes(cluster.status, ['Deleting', 'Error']) &&
-                    currentCluster(clustersByProject[cluster.googleProject]) !== cluster
-                  return h(Fragment, [
-                    cluster.googleProject,
-                    inactive && h(TooltipTrigger, {
-                      content: 'This billing project has multiple active runtime environments. Only the most recently created one will be used.'
-                    }, [icon('warning-standard', { style: { marginLeft: '0.25rem', color: colors.warning() } })])
-                  ])
-                }
-              },
-              {
-                size: { basis: 150, grow: 0 },
-                headerRenderer: () => h(Sortable, { sort, field: 'status', onSort: setSort }, ['Status']),
-                cellRenderer: ({ rowIndex }) => {
-                  const cluster = filteredClusters[rowIndex]
-                  return h(Fragment, [
-                    cluster.status,
-                    cluster.status === 'Error' && h(Clickable, {
-                      tooltip: 'View error',
-                      'aria-label': 'View error',
-                      onClick: () => setErrorClusterId(cluster.id)
-                    }, [icon('warning-standard', { style: { marginLeft: '0.25rem', color: colors.danger() } })])
-                  ])
-                }
-              },
-              {
-                size: { basis: 250, grow: 0 },
-                headerRenderer: () => h(Sortable, { sort, field: 'created', onSort: setSort }, ['Created']),
-                cellRenderer: ({ rowIndex }) => {
-                  return makeCompleteDate(filteredClusters[rowIndex].auditInfo.createdDate)
-                }
-              },
-              {
-                size: { basis: 250, grow: 0 },
-                headerRenderer: () => h(Sortable, { sort, field: 'accessed', onSort: setSort }, ['Last accessed']),
-                cellRenderer: ({ rowIndex }) => {
-                  return makeCompleteDate(filteredClusters[rowIndex].auditInfo.dateAccessed)
-                }
-              },
-              {
-                size: { basis: 240, grow: 0 },
-                headerRenderer: () => {
-                  return h(Sortable, { sort, field: 'cost', onSort: setSort }, [`Cost / hr (${formatUSD(totalCost)} total)`])
-                },
-                cellRenderer: ({ rowIndex }) => {
-                  return formatUSD(clusterCost(filteredClusters[rowIndex]))
-                }
-              },
-              {
-                size: { basis: 50, grow: 0 },
-                headerRenderer: () => null,
-                cellRenderer: ({ rowIndex }) => {
-                  const { id, status } = filteredClusters[rowIndex]
-                  return status !== 'Deleting' && h(Link, {
-                    disabled: status === 'Creating',
-                    'aria-label': 'Delete notebook runtime',
-                    tooltip: status === 'Creating' ? 'Cannot delete a runtime while it is being created' : 'Delete notebook runtime',
-                    onClick: () => setDeleteClusterId(id)
-                  }, [icon('trash')])
-                }
+        clusters && h(SimpleFlexTable, {
+          rowCount: filteredClusters.length,
+          columns: [
+            {
+              headerRenderer: () => h(Sortable, { sort, field: 'project', onSort: setSort }, ['Billing project']),
+              cellRenderer: ({ rowIndex }) => {
+                const cluster = filteredClusters[rowIndex]
+                const inactive = !_.includes(cluster.status, ['Deleting', 'Error']) &&
+                  currentCluster(clustersByProject[cluster.googleProject]) !== cluster
+                return h(Fragment, [
+                  cluster.googleProject,
+                  inactive && h(TooltipTrigger, {
+                    content: 'This billing project has multiple active runtime environments. Only the most recently created one will be used.'
+                  }, [icon('warning-standard', { style: { marginLeft: '0.25rem', color: colors.warning() } })])
+                ])
               }
-            ]
-          })
-        ])
+            },
+            {
+              size: { basis: 150, grow: 0 },
+              headerRenderer: () => h(Sortable, { sort, field: 'status', onSort: setSort }, ['Status']),
+              cellRenderer: ({ rowIndex }) => {
+                const cluster = filteredClusters[rowIndex]
+                return h(Fragment, [
+                  cluster.status,
+                  cluster.status === 'Error' && h(Clickable, {
+                    tooltip: 'View error',
+                    'aria-label': 'View error',
+                    onClick: () => setErrorClusterId(cluster.id)
+                  }, [icon('warning-standard', { style: { marginLeft: '0.25rem', color: colors.danger() } })])
+                ])
+              }
+            },
+            {
+              size: { basis: 250, grow: 0 },
+              headerRenderer: () => h(Sortable, { sort, field: 'created', onSort: setSort }, ['Created']),
+              cellRenderer: ({ rowIndex }) => {
+                return makeCompleteDate(filteredClusters[rowIndex].auditInfo.createdDate)
+              }
+            },
+            {
+              size: { basis: 250, grow: 0 },
+              headerRenderer: () => h(Sortable, { sort, field: 'accessed', onSort: setSort }, ['Last accessed']),
+              cellRenderer: ({ rowIndex }) => {
+                return makeCompleteDate(filteredClusters[rowIndex].auditInfo.dateAccessed)
+              }
+            },
+            {
+              size: { basis: 240, grow: 0 },
+              headerRenderer: () => {
+                return h(Sortable, { sort, field: 'cost', onSort: setSort }, [`Cost / hr (${formatUSD(totalCost)} total)`])
+              },
+              cellRenderer: ({ rowIndex }) => {
+                return formatUSD(clusterCost(filteredClusters[rowIndex]))
+              }
+            },
+            {
+              size: { basis: 50, grow: 0 },
+              headerRenderer: () => null,
+              cellRenderer: ({ rowIndex }) => {
+                const { id, status } = filteredClusters[rowIndex]
+                return status !== 'Deleting' && h(Link, {
+                  disabled: status === 'Creating',
+                  'aria-label': 'Delete notebook runtime',
+                  tooltip: status === 'Creating' ? 'Cannot delete a runtime while it is being created' : 'Delete notebook runtime',
+                  onClick: () => setDeleteClusterId(id)
+                }, [icon('trash')])
+              }
+            }
+          ]
+        })
       ]),
       errorClusterId && h(ClusterErrorModal, {
         cluster: _.find({ id: errorClusterId }, clusters),
