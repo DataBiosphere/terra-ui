e9c5cc5ba3f6e09d884b2a502580bfe168edda7d
Sky Rubenstein <blrubenstein@wpi.edu>
2020-08-25 10:47:33 -0400

    Remove ajax override for mob-pd

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -178,7 +178,7 @@ window.ajaxOverrideUtils = {
       wrappedFetch(...args)
   })
 }
-ajaxOverridesStore.set(pdOverrides.diskAndDeletingDisk)
+// ajaxOverridesStore.set(pdOverrides.diskAndDeletingDisk)
 
 const authOpts = (token = getUser().token) => ({ headers: { Authorization: `Bearer ${token}` } })
 const jsonBody = body => ({ body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' } })
