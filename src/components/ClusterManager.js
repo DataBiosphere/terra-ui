b0ef0e227defe3000c0e6c2ee59d0f92c93e0de2
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-10-02 11:08:49 -0400

    rename 'clusters' in ClusterManager

--- src/components/ClusterManager.js
+++ src/components/ClusterManager.js
@@ -51,36 +51,36 @@ const styles = {
   })
 }
 
-const ClusterIcon = ({ shape, onClick, disabled, style, ...props }) => {
+const RuntimeIcon = ({ shape, onClick, disabled, style, ...props }) => {
   return h(Clickable, {
     style: { color: onClick && !disabled ? colors.accent() : colors.dark(0.7), ...styles.verticalCenter, ...style },
     onClick, disabled, ...props
   }, [icon(shape, { size: 20 })])
 }
 
-export const ClusterErrorModal = ({ cluster, onDismiss }) => {
+export const ClusterErrorModal = ({ cluster: runtime, onDismiss }) => {
   const [error, setError] = useState()
   const [userscriptError, setUserscriptError] = useState(false)
-  const [loadingClusterDetails, setLoadingClusterDetails] = useState(false)
+  const [loadingRuntimeDetails, setLoadingRuntimeDetails] = useState(false)
 
-  const loadClusterError = _.flow(
+  const loadRuntimeError = _.flow(
     withErrorReporting('Error loading cloud environment details'),
-    Utils.withBusyState(setLoadingClusterDetails)
+    Utils.withBusyState(setLoadingRuntimeDetails)
   )(async () => {
-    const { errors: clusterErrors } = await Ajax().Runtimes.runtime(cluster.googleProject, cluster.runtimeName).details()
-    if (_.some(({ errorMessage }) => errorMessage.includes('Userscript failed'), clusterErrors)) {
+    const { errors: runtimeErrors } = await Ajax().Runtimes.runtime(runtime.googleProject, runtime.runtimeName).details()
+    if (_.some(({ errorMessage }) => errorMessage.includes('Userscript failed'), runtimeErrors)) {
       setError(
         await Ajax()
           .Buckets
-          .getObjectPreview(cluster.asyncRuntimeFields.stagingBucket, `userscript_output.txt`, cluster.googleProject, true)
+          .getObjectPreview(runtime.asyncRuntimeFields.stagingBucket, `userscript_output.txt`, runtime.googleProject, true)
           .then(res => res.text()))
       setUserscriptError(true)
     } else {
-      setError(clusterErrors[0].errorMessage)
+      setError(runtimeErrors[0].errorMessage)
     }
   })
 
-  Utils.useOnMount(() => { loadClusterError() })
+  Utils.useOnMount(() => { loadRuntimeError() })
 
   return h(Modal, {
     title: `Cloud Environment Creation Failed${userscriptError ? ' due to Userscript Error' : ''}`,
@@ -88,11 +88,11 @@ export const ClusterErrorModal = ({ cluster, onDismiss }) => {
     onDismiss
   }, [
     div({ style: { whiteSpace: 'pre-wrap', overflowWrap: 'break-word', overflowY: 'auto', maxHeight: 500, background: colors.light() } }, [error]),
-    loadingClusterDetails && spinnerOverlay
+    loadingRuntimeDetails && spinnerOverlay
   ])
 }
 
-const ClusterErrorNotification = ({ cluster }) => {
+const RuntimeErrorNotification = ({ runtime }) => {
   const [modalOpen, setModalOpen] = useState(false)
 
   return h(Fragment, [
@@ -105,7 +105,7 @@ const ClusterErrorNotification = ({ cluster }) => {
       }
     }, ['SEE LOG INFO']),
     modalOpen && h(ClusterErrorModal, {
-      cluster,
+      cluster: runtime,
       onDismiss: () => setModalOpen(false)
     })
   ])
@@ -134,24 +134,24 @@ export default class ClusterManager extends PureComponent {
 
   componentDidUpdate(prevProps) {
     const { namespace, name, apps } = this.props
-    const prevCluster = _.last(_.sortBy('auditInfo.createdDate', _.remove({ status: 'Deleting' }, prevProps.clusters))) || {}
-    const cluster = this.getCurrentCluster() || {}
+    const prevRuntime = _.last(_.sortBy('auditInfo.createdDate', _.remove({ status: 'Deleting' }, prevProps.clusters))) || {}
+    const runtime = this.getCurrentRuntime() || {}
     const twoMonthsAgo = _.tap(d => d.setMonth(d.getMonth() - 2), new Date())
     const welderCutOff = new Date('2019-08-01')
-    const createdDate = new Date(cluster.createdDate)
-    const dateNotified = getDynamic(sessionStorage, `notifiedOutdatedCluster${cluster.id}`) || {}
+    const createdDate = new Date(runtime.createdDate)
+    const dateNotified = getDynamic(sessionStorage, `notifiedOutdatedRuntime${runtime.id}`) || {}
     const rStudioLaunchLink = Nav.getLink('workspace-app-launch', { namespace, name, app: 'RStudio' })
     const app = currentApp(apps)
     const prevApp = currentApp(prevProps.apps)
 
-    if (cluster.status === 'Error' && prevCluster.status !== 'Error' && !_.includes(cluster.id, errorNotifiedClusters.get())) {
+    if (runtime.status === 'Error' && prevRuntime.status !== 'Error' && !_.includes(runtime.id, errorNotifiedClusters.get())) {
       notify('error', 'Error Creating Cloud Environment', {
-        message: h(ClusterErrorNotification, { cluster })
+        message: h(RuntimeErrorNotification, { runtime })
       })
-      errorNotifiedClusters.update(Utils.append(cluster.id))
+      errorNotifiedClusters.update(Utils.append(runtime.id))
     } else if (
-      cluster.status === 'Running' && prevCluster.status && prevCluster.status !== 'Running' &&
-      cluster.labels.tool === 'RStudio' && window.location.hash !== rStudioLaunchLink
+      runtime.status === 'Running' && prevRuntime.status && prevRuntime.status !== 'Running' &&
+      runtime.labels.tool === 'RStudio' && window.location.hash !== rStudioLaunchLink
     ) {
       const rStudioNotificationId = notify('info', 'Your cloud environment is ready.', {
         message: h(ButtonPrimary, {
@@ -160,7 +160,7 @@ export default class ClusterManager extends PureComponent {
         }, 'Launch Cloud Environment')
       })
     } else if (isAfter(createdDate, welderCutOff) && !isToday(dateNotified)) { // TODO: remove this notification some time after the data syncing release
-      setDynamic(sessionStorage, `notifiedOutdatedCluster${cluster.id}`, Date.now())
+      setDynamic(sessionStorage, `notifiedOutdatedRuntime${runtime.id}`, Date.now())
       notify('warn', 'Please Update Your Cloud Environment', {
         message: h(Fragment, [
           p(['Last year, we introduced important updates to Terra that are not compatible with the older cloud environment associated with this workspace. You are no longer able to save new changes to notebooks using this older cloud environment.']),
@@ -168,11 +168,11 @@ export default class ClusterManager extends PureComponent {
         ])
       })
     } else if (isAfter(createdDate, twoMonthsAgo) && !isToday(dateNotified)) {
-      setDynamic(sessionStorage, `notifiedOutdatedCluster${cluster.id}`, Date.now())
+      setDynamic(sessionStorage, `notifiedOutdatedRuntime${runtime.id}`, Date.now())
       notify('warn', 'Outdated Cloud Environment', {
         message: 'Your cloud environment is over two months old. Please consider deleting and recreating your cloud environment in order to access the latest features and security updates.'
       })
-    } else if (cluster.status === 'Running' && prevCluster.status === 'Updating') {
+    } else if (runtime.status === 'Running' && prevRuntime.status === 'Updating') {
       notify('success', 'Number of workers has updated successfully.')
     }
     if (prevApp && prevApp.status !== 'RUNNING' && app && app.status === 'RUNNING') {
@@ -188,12 +188,12 @@ export default class ClusterManager extends PureComponent {
     }
   }
 
-  getActiveClustersOldestFirst() {
+  getActiveRuntimesOldestFirst() {
     const { clusters } = this.props
     return trimClustersOldestFirst(clusters)
   }
 
-  getCurrentCluster() {
+  getCurrentRuntime() {
     const { clusters } = this.props
     return currentCluster(clusters)
   }
@@ -211,15 +211,15 @@ export default class ClusterManager extends PureComponent {
     }
   }
 
-  startCluster() {
-    const { googleProject, runtimeName } = this.getCurrentCluster()
+  startRuntime() {
+    const { googleProject, runtimeName } = this.getCurrentRuntime()
     this.executeAndRefresh(
       Ajax().Runtimes.runtime(googleProject, runtimeName).start()
     )
   }
 
-  stopCluster() {
-    const { googleProject, runtimeName } = this.getCurrentCluster()
+  stopRuntime() {
+    const { googleProject, runtimeName } = this.getCurrentRuntime()
     this.executeAndRefresh(
       Ajax().Runtimes.runtime(googleProject, runtimeName).stop()
     )
@@ -231,23 +231,23 @@ export default class ClusterManager extends PureComponent {
     if (!clusters || !apps) {
       return null
     }
-    const currentCluster = this.getCurrentCluster()
-    const currentStatus = collapsedClusterStatus(currentCluster)
+    const currentRuntime = this.getCurrentRuntime()
+    const currentStatus = collapsedClusterStatus(currentRuntime)
 
     const renderIcon = () => {
       switch (currentStatus) {
         case 'Stopped':
-          return h(ClusterIcon, {
+          return h(RuntimeIcon, {
             shape: 'play',
-            onClick: () => this.startCluster(),
+            onClick: () => this.startRuntime(),
             disabled: busy || !canCompute,
             tooltip: canCompute ? 'Start cloud environment' : noCompute,
             'aria-label': 'Start cloud environment'
           })
         case 'Running':
-          return h(ClusterIcon, {
+          return h(RuntimeIcon, {
             shape: 'pause',
-            onClick: () => this.stopCluster(),
+            onClick: () => this.stopRuntime(),
             disabled: busy || !canCompute,
             tooltip: canCompute ? 'Stop cloud environment' : noCompute,
             'aria-label': 'Stop cloud environment'
@@ -257,14 +257,14 @@ export default class ClusterManager extends PureComponent {
         case 'Updating':
         case 'Creating':
         case 'LeoReconfiguring':
-          return h(ClusterIcon, {
+          return h(RuntimeIcon, {
             shape: 'sync',
             disabled: true,
             tooltip: 'Cloud environment update in progress',
             'aria-label': 'Cloud environment update in progress'
           })
         case 'Error':
-          return h(ClusterIcon, {
+          return h(RuntimeIcon, {
             shape: 'warning-standard',
             style: { color: colors.danger(0.9) },
             onClick: () => this.setState({ errorModalOpen: true }),
@@ -273,7 +273,7 @@ export default class ClusterManager extends PureComponent {
             'aria-label': 'View error'
           })
         default:
-          return h(ClusterIcon, {
+          return h(RuntimeIcon, {
             shape: 'play',
             onClick: () => this.setState({ createModalDrawerOpen: true }),
             disabled: busy || !canCompute,
@@ -283,12 +283,12 @@ export default class ClusterManager extends PureComponent {
       }
     }
     const totalCost = _.sum(_.map(clusterCost, clusters)) + _.sum(_.map(persistentDiskCost, persistentDisks))
-    const activeClusters = this.getActiveClustersOldestFirst()
+    const activeRuntimes = this.getActiveRuntimesOldestFirst()
     const activeDisks = _.remove({ status: 'Deleting' }, persistentDisks)
-    const { Creating: creating, Updating: updating, LeoReconfiguring: reconfiguring } = _.countBy(collapsedClusterStatus, activeClusters)
+    const { Creating: creating, Updating: updating, LeoReconfiguring: reconfiguring } = _.countBy(collapsedClusterStatus, activeRuntimes)
     const isDisabled = !canCompute || creating || busy || updating || reconfiguring
 
-    const isRStudioImage = currentCluster?.labels.tool === 'RStudio'
+    const isRStudioImage = currentRuntime?.labels.tool === 'RStudio'
     const appName = isRStudioImage ? 'RStudio' : 'terminal'
     const appLaunchLink = Nav.getLink('workspace-app-launch', { namespace, name, app: appName })
 
@@ -314,14 +314,14 @@ export default class ClusterManager extends PureComponent {
         ])
       ]),
       div({ style: styles.container }, [
-        (activeClusters.length > 1 || activeDisks.length > 1) && h(Link, {
+        (activeRuntimes.length > 1 || activeDisks.length > 1) && h(Link, {
           style: { marginRight: '1rem' },
           href: Nav.getLink('clusters'),
           tooltip: 'Multiple cloud environments found in this billing project. Click to select which to delete.'
         }, [icon('warning-standard', { size: 24, style: { color: colors.danger() } })]),
         h(Link, {
           href: appLaunchLink,
-          onClick: window.location.hash === appLaunchLink && currentStatus === 'Stopped' ? () => this.startCluster() : undefined,
+          onClick: window.location.hash === appLaunchLink && currentStatus === 'Stopped' ? () => this.startRuntime() : undefined,
           tooltip: canCompute ? `Open ${appName}` : noCompute,
           'aria-label': `Open ${appName}`,
           disabled: !canCompute,
@@ -375,7 +375,7 @@ export default class ClusterManager extends PureComponent {
           }
         }),
         errorModalOpen && h(ClusterErrorModal, {
-          cluster: currentCluster,
+          cluster: currentRuntime,
           onDismiss: () => this.setState({ errorModalOpen: false })
         })
       ])
