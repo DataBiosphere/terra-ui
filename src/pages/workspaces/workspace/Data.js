a85c833d76f376e38ef056f07d9deed2d8d293ac
Isaac Zarsky <zarsky@broadinstitute.org>
2020-10-07 14:14:00 -0400

    update deps (#2274)

--- src/pages/workspaces/workspace/Data.js
+++ src/pages/workspaces/workspace/Data.js
@@ -1,4 +1,4 @@
-import * as clipboard from 'clipboard-polyfill'
+import * as clipboard from 'clipboard-polyfill/text'
 import FileSaver from 'file-saver'
 import filesize from 'filesize'
 import JSZip from 'jszip'
