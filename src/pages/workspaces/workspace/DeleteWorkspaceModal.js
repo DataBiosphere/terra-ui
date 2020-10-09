07910a18a4ac3a4f787fbde140c1e685eb5605cf
Sky Rubenstein <blrubenstein@wpi.edu>
2020-10-09 15:21:36 -0400

    Add delete workspace confirmation text

--- src/pages/workspaces/workspace/DeleteWorkspaceModal.js
+++ src/pages/workspaces/workspace/DeleteWorkspaceModal.js
@@ -1,18 +1,21 @@
 import { Component } from 'react'
-import { div, h, span } from 'react-hyperscript-helpers'
+import { div, h, label, span } from 'react-hyperscript-helpers'
 import { ButtonPrimary, Link, spinnerOverlay } from 'src/components/common'
+import { TextInput } from 'src/components/input'
 import Modal from 'src/components/Modal'
 import { Ajax } from 'src/libs/ajax'
 import { bucketBrowserUrl } from 'src/libs/auth'
 import { reportError } from 'src/libs/error'
 import * as Utils from 'src/libs/utils'
+import * as _ from 'lodash/fp'
 
 
 export default class DeleteWorkspaceModal extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deleting: false
+      deleting: false,
+      deleteConfirmation: ''
     }
   }
 
@@ -31,11 +34,12 @@ export default class DeleteWorkspaceModal extends Component {
 
   render() {
     const { workspace: { workspace: { bucketName, name } }, onDismiss } = this.props
-    const { deleting } = this.state
+    const { deleteConfirmation, deleting } = this.state
     return h(Modal, {
       title: 'Delete workspace',
       onDismiss,
       okButton: h(ButtonPrimary, {
+        disabled: _.toLower(deleteConfirmation) !== 'i understand',
         onClick: () => this.deleteWorkspace()
       }, 'Delete workspace')
     }, [
@@ -55,6 +59,15 @@ export default class DeleteWorkspaceModal extends Component {
           marginTop: '1rem'
         }
       }, 'This cannot be undone.'),
+      div({ style: { marginTop: '1rem' } }, [
+        label({ htmlFor: 'understand' }, ['Please type \'I Understand\' to continue:']),
+        h(TextInput, {
+          id: 'understand',
+          placeholder: 'I Understand',
+          value: deleteConfirmation,
+          onChange: v => this.setState({ deleteConfirmation: v })
+        }),
+      ]),
       deleting && spinnerOverlay
     ])
   }
