03e82ba8f688b82ef912d9184b5887889aa5dcf0
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-10-13 10:02:52 -0400

    simplify galaxy drawer styles and clean up code formatting (#2277)

--- src/components/NewGalaxyModal.js
+++ src/components/NewGalaxyModal.js
@@ -18,7 +18,7 @@ import * as Utils from 'src/libs/utils'
 
 const styles = {
   whiteBoxContainer: { padding: '1rem', borderRadius: 3, backgroundColor: 'white' },
-  drawerContent: { display: 'flex', flexDirection: 'column', flex: '0 1 0%', padding: '0.5rem 1.5rem' },
+  drawerContent: { display: 'flex', flexDirection: 'column', flex: 'none', padding: '1.5rem' },
   headerText: { fontSize: 16, fontWeight: 600 }
 }
 
@@ -72,13 +72,6 @@ export const NewGalaxyModal = _.flow(
     )
   }
 
-
-  const renderBottomButtons = () => {
-    return div({ style: { display: 'flex', margin: '1rem 0 1rem', justifyContent: 'flex-end' } }, [
-      renderActionButton()
-    ])
-  }
-
   const renderCreateWarning = () => {
     return h(Fragment, [
       div({ style: { marginBottom: '1rem' } }, ['Environment will consist of an application and cloud compute.']),
@@ -110,9 +103,11 @@ export const NewGalaxyModal = _.flow(
         ' (e.g. results files). Double check that your workflow results were written to ',
         'the data tab in the workspace before clicking “Delete”'
       ]),
-      div({ style: { marginTop: '1rem' } }, ['Deleting your Cloud Environment will stop your ',
+      div({ style: { marginTop: '1rem' } }, [
+        'Deleting your Cloud Environment will stop your ',
         'running Galaxy application and your application costs. You can create a new Cloud Environment ',
-        'for Galaxy later, which will take 8-10 minutes.'])
+        'for Galaxy later, which will take 8-10 minutes.'
+      ])
     ])
   }
 
@@ -124,6 +119,7 @@ export const NewGalaxyModal = _.flow(
 
   const renderDefaultCase = () => {
     const { cpu, memory } = _.find({ name: 'n1-standard-8' }, machineTypes)
+    const cost = hourlyKubernetesAppCost(app || { kubernetesRuntimeConfig: { machineType: 'n1-standard-8' } }) + persistentDiskCost({ size: 30, status: 'Running' })
     return h(Fragment, [
       div([`Environment ${app ? 'consists' : 'will consist'} of an application and cloud compute.`]),
       div({ style: { ...styles.whiteBoxContainer, marginTop: '1rem' } }, [
@@ -134,17 +130,13 @@ export const NewGalaxyModal = _.flow(
               'Galaxy version 20.09.3'
             ]),
             li({ style: { marginTop: '1rem' } }, [
-              'Cloud Compute size of ', span({ style: { fontWeight: 600 } },
-                // Temporarily hard-coded disk size, once it can be customized this should be revisited
-                [`${cpu} CPUS, ${memory} GB of memory, 30 GB disk space`])
+              'Cloud Compute size of ',
+              // Temporarily hard-coded disk size, once it can be customized this should be revisited
+              span({ style: { fontWeight: 600 } }, [`${cpu} CPUS, ${memory} GB of memory, 30 GB disk space`])
             ]),
             li({ style: { marginTop: '1rem' } }, [
               'Running cloud compute costs ',
-              span({ style: { fontWeight: 600 } }, `${Utils.formatUSD(
-                hourlyKubernetesAppCost(
-                  app || { kubernetesRuntimeConfig: { machineType: 'n1-standard-8' } }
-                ) + persistentDiskCost({ size: 30, status: 'Running' })
-              )} per hr`)
+              span({ style: { fontWeight: 600 } }, [Utils.formatUSD(cost), ' per hr'])
             ])
           ]),
           h(Link, { href: 'https://support.terra.bio/hc/en-us/articles/360050566271', ...Utils.newTabLinkProps }, [
@@ -156,13 +148,6 @@ export const NewGalaxyModal = _.flow(
     ])
   }
 
-  const contents = Utils.switchCase(viewMode,
-    ['createWarn', renderCreateWarning],
-    ['deleteWarn', renderDeleteWarning],
-    ['launchWarn', renderLaunchWarning],
-    [Utils.DEFAULT, renderDefaultCase]
-  )
-
   return div({ style: styles.drawerContent }, [
     h(TitleBar, {
       title: Utils.switchCase(viewMode,
@@ -170,11 +155,19 @@ export const NewGalaxyModal = _.flow(
         ['deleteWarn', () => 'Delete Cloud Environment for Galaxy'],
         [Utils.DEFAULT, () => 'Cloud environment']
       ),
+      style: { marginBottom: '0.5rem' },
       onDismiss,
-      onPrevious: !!viewMode ? () => setViewMode(undefined) : undefined,
-      style: { margin: '2rem 0 0 1.5rem' }
+      onPrevious: !!viewMode ? () => setViewMode(undefined) : undefined
     }),
-    div({ style: { padding: '0.5rem 1.5rem 1.5rem', flexGrow: 1, display: 'flex', flexDirection: 'column' } }, [contents, renderBottomButtons()]),
+    Utils.switchCase(viewMode,
+      ['createWarn', renderCreateWarning],
+      ['deleteWarn', renderDeleteWarning],
+      ['launchWarn', renderLaunchWarning],
+      [Utils.DEFAULT, renderDefaultCase]
+    ),
+    div({ style: { display: 'flex', marginTop: '2rem', justifyContent: 'flex-end' } }, [
+      renderActionButton()
+    ]),
     loading && spinnerOverlay
   ])
 })
