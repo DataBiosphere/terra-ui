b2bd7c46fedb63d6c2f8749f290d92bf31eafb84
David An <davidan@broadinstitute.org>
2020-10-22 12:22:49 -0400

    disable spellcheck for input and output fields in workflow configs

--- src/pages/workspaces/workspace/workflows/WorkflowView.js
+++ src/pages/workspaces/workspace/workflows/WorkflowView.js
@@ -156,6 +156,9 @@ const WorkflowIOTable = ({ which, inputsOutputs: data, config, errors, onChange,
               !readOnly ? h(DelayedAutocompleteTextArea, {
                 autosize: true,
                 'aria-label': name,
+                // without quoting both the key and value for spellcheck, it won't make it to the browser
+                // eslint-disable-next-line quote-props
+                'spellcheck': 'false',
                 placeholder: optional ? 'Optional' : 'Required',
                 value,
                 style: isFile ? { paddingRight: '2rem' } : undefined,
