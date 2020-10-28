87afd462336806fd6050525936067649d643a931
Isaac Zarsky <zarsky@broadinstitute.org>
2020-10-28 17:04:29 -0400

    a few low-hanging fruit
    Co-Authored-By: Kai <19541449+Shakespeared@users.noreply.github.com>

--- src/libs/notifications.js
+++ src/libs/notifications.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp'
-import { Component } from 'react'
+import { useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
 import { store } from 'react-notifications-component'
 import { ButtonPrimary, Clickable, Link } from 'src/components/common'
@@ -34,107 +34,102 @@ export const notify = (type, title, props) => {
 
 export const clearNotification = id => store.removeNotification(id)
 
-const NotificationDisplay = Utils.connectStore(notificationStore, 'notificationState')(class NotificationDisplay extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { modal: false, notificationNumber: 0 }
-  }
+const NotificationDisplay = _.flow(
+  Utils.withDisplayName('NotificationDisplay'),
+  Utils.connectStore(notificationStore, 'notificationState')
+)(({ notificationState, id }) => {
+  const [modal, setModal] = useState(false)
+  const [notificationNumber, setNotificationNumber] = useState(0)
 
+  const notifications = _.filter(n => n.id === id, notificationState)
+  const onFirst = notificationNumber === 0
+  const onLast = notificationNumber + 1 === notifications.length
 
-  render() {
-    const { notificationState, id } = this.props
-    const { modal, notificationNumber } = this.state
+  const { title, message, detail, type } = notifications[notificationNumber]
+  const [baseColor, ariaLabel] = Utils.switchCase(type,
+    ['success', () => [colors.success, 'success notification']],
+    ['info', () => [colors.accent, 'info notification']],
+    ['welcome', () => [colors.accent, 'welcome notification']],
+    ['warn', () => [colors.warning, 'warning notification']],
+    ['error', () => [colors.danger, 'error notification']],
+    [Utils.DEFAULT, () => [colors.accent, 'notification']]
+  )
+  const iconType = Utils.switchCase(type,
+    ['success', () => 'success-standard'],
+    ['warn', () => 'warning-standard'],
+    ['error', () => 'error-standard']
+  )
 
-    const notifications = _.filter(n => n.id === id, notificationState)
-    const onFirst = notificationNumber === 0
-    const onLast = notificationNumber + 1 === notifications.length
-
-    const { title, message, detail, type } = notifications[notificationNumber]
-    const [baseColor, ariaLabel] = Utils.switchCase(type,
-      ['success', () => [colors.success, 'success notification']],
-      ['info', () => [colors.accent, 'info notification']],
-      ['welcome', () => [colors.accent, 'welcome notification']],
-      ['warn', () => [colors.warning, 'warning notification']],
-      ['error', () => [colors.danger, 'error notification']],
-      [Utils.DEFAULT, () => [colors.accent, 'notification']]
-    )
-    const iconType = Utils.switchCase(type,
-      ['success', () => 'success-standard'],
-      ['warn', () => 'warning-standard'],
-      ['error', () => 'error-standard']
-    )
-
-    return div({
-      style: {
-        backgroundColor: baseColor(0.15),
-        borderRadius: '4px',
-        boxShadow: '0 0 4px 0 rgba(0,0,0,0.5)',
-        cursor: 'auto',
-        display: 'flex',
-        flexDirection: 'column',
-        fontSize: 12
-      }
+  return div({
+    style: {
+      backgroundColor: baseColor(0.15),
+      borderRadius: '4px',
+      boxShadow: '0 0 4px 0 rgba(0,0,0,0.5)',
+      cursor: 'auto',
+      display: 'flex',
+      flexDirection: 'column',
+      fontSize: 12
+    }
+  }, [
+    // content and close button
+    div({ style: { display: 'flex', padding: '0.75rem 1rem' } }, [
+      // content
+      div({ style: { display: 'flex', flex: 1, flexDirection: 'column' } }, [
+        // icon and title
+        div({ style: { display: 'flex' } }, [
+          !!iconType && icon(iconType, {
+            'aria-hidden': false, 'aria-label': ariaLabel,
+            size: 26,
+            style: { color: baseColor(), flexShrink: 0, marginRight: '0.5rem' }
+          }),
+          div({ style: { fontWeight: 600 } }, [title])
+        ]),
+        !!message && div({ style: { marginTop: '0.5rem' } }, [message]),
+        !!detail && h(Clickable, {
+          style: { marginTop: '0.25rem', textDecoration: 'underline' },
+          onClick: () => setModal(true)
+        }, ['Details'])
+      ]),
+      h(Link, {
+        style: { alignSelf: 'start' },
+        'aria-label': type ? `Dismiss ${type} notification` : 'Dismiss notification',
+        title: 'Dismiss notification',
+        onClick: () => store.removeNotification(id)
+      }, [icon('times', { size: 20 })])
+    ]),
+    notifications.length > 1 && div({
+      style: { alignItems: 'center', borderTop: `1px solid ${baseColor()}`, display: 'flex', fontSize: 10, padding: '0.75rem 1rem' }
     }, [
-      // content and close button
-      div({ style: { display: 'flex', padding: '0.75rem 1rem' } }, [
-        // content
-        div({ style: { display: 'flex', flex: 1, flexDirection: 'column' } }, [
-          // icon and title
-          div({ style: { display: 'flex' } }, [
-            !!iconType && icon(iconType, {
-              'aria-hidden': false, 'aria-label': ariaLabel,
-              size: 26,
-              style: { color: baseColor(), flexShrink: 0, marginRight: '0.5rem' }
-            }),
-            div({ style: { fontWeight: 600 } }, [title])
-          ]),
-          !!message && div({ style: { marginTop: '0.5rem' } }, [message]),
-          !!detail && h(Clickable, {
-            style: { marginTop: '0.25rem', textDecoration: 'underline' },
-            onClick: () => this.setState({ modal: true })
-          }, ['Details'])
-        ]),
-        h(Link, {
-          style: { alignSelf: 'start' },
-          'aria-label': type ? `Dismiss ${type} notification` : 'Dismiss notification',
-          title: 'Dismiss notification',
-          onClick: () => store.removeNotification(id)
-        }, [icon('times', { size: 20 })])
-      ]),
-      notifications.length > 1 && div({
-        style: { alignItems: 'center', borderTop: `1px solid ${baseColor()}`, display: 'flex', fontSize: 10, padding: '0.75rem 1rem' }
+      h(Link, {
+        disabled: onFirst,
+        onClick: () => setNotificationNumber(notificationNumber - 1)
+      }, [icon('angle-left', { size: 12 })]),
+      div({
+        style: {
+          backgroundColor: colors.accent(), color: 'white',
+          fontWeight: 600,
+          borderRadius: 10,
+          padding: '0.2rem 0.5rem'
+        }
       }, [
-        h(Link, {
-          disabled: onFirst,
-          onClick: () => this.setState({ notificationNumber: notificationNumber - 1 })
-        }, [icon('angle-left', { size: 12 })]),
-        div({
-          style: {
-            backgroundColor: colors.accent(), color: 'white',
-            fontWeight: 600,
-            borderRadius: 10,
-            padding: '0.2rem 0.5rem'
-          }
-        }, [
-          notificationNumber + 1, '/', notifications.length
-        ]),
-        h(Link, {
-          disabled: onLast,
-          onClick: () => this.setState({ notificationNumber: notificationNumber + 1 })
-        }, [icon('angle-right', { size: 12 })])
+        notificationNumber + 1, '/', notifications.length
       ]),
-      modal && h(Modal, {
-        width: 800,
-        title,
-        showCancel: false,
-        showX: true,
-        onDismiss: () => this.setState({ modal: false }),
-        okButton: h(ButtonPrimary, { onClick: () => refreshPage() }, 'Refresh Page')
-      }, [
-        h(ErrorView, { error: detail })
-      ])
+      h(Link, {
+        disabled: onLast,
+        onClick: () => setNotificationNumber(notificationNumber + 1)
+      }, [icon('angle-right', { size: 12 })])
+    ]),
+    modal && h(Modal, {
+      width: 800,
+      title,
+      showCancel: false,
+      showX: true,
+      onDismiss: () => setModal(false),
+      okButton: h(ButtonPrimary, { onClick: () => refreshPage() }, 'Refresh Page')
+    }, [
+      h(ErrorView, { error: detail })
     ])
-  }
+  ])
 })
 
 const refreshPage = () => {
