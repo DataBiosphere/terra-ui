aa1a2531ddc927681e9bef8a6d3aeead58e436c1
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2020-11-02 11:22:45 -0500

    convert Register page to function component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import { div, h, label } from 'react-hyperscript-helpers'
 import { ButtonPrimary, ButtonSecondary, IdContainer } from 'src/components/common'
 import { centeredSpinner } from 'src/components/icons'
@@ -20,22 +20,16 @@ const constraints = {
   email: { presence: { allowEmpty: false } }
 }
 
-export default class Register extends Component {
-  constructor(props) {
-    super(props)
-    const { givenName = '', familyName = '', email = '' } = getUser()
-    this.state = {
-      busy: false,
-      givenName,
-      familyName,
-      email
-    }
-  }
+const Register = () => {
+  const user = getUser()
+  const [busy, setBusy] = useState(false)
+  const [givenName, setGivenName] = useState(user.givenName || '')
+  const [familyName, setFamilyName] = useState(user.familyName || '')
+  const [email, setEmail] = useState(user.email || '')
 
-  async register() {
-    const { givenName, familyName, email } = this.state
+  const register = async () => {
     try {
-      this.setState({ busy: true })
+      setBusy(true)
       await Ajax().User.profile.set({
         firstName: givenName,
         lastName: familyName,
@@ -46,70 +40,69 @@ export default class Register extends Component {
       Ajax().Metrics.captureEvent(Events.userRegister)
     } catch (error) {
       reportError('Error registering', error)
-      this.setState({ busy: false })
+      setBusy(false)
     }
   }
 
-  render() {
-    const { busy, givenName, familyName, email } = this.state
-    const errors = validate({ givenName, familyName, email }, constraints)
-    return div({
-      role: 'main',
+  const errors = validate({ givenName, familyName, email }, constraints)
+  return div({
+    role: 'main',
+    style: {
+      flexGrow: 1,
+      padding: '5rem',
+      backgroundImage: `url(${planet})`,
+      backgroundRepeat: 'no-repeat', backgroundSize: '750px', backgroundPosition: 'right 0px bottom -600px'
+    }
+  }, [
+    registrationLogo(),
+    div({
       style: {
-        flexGrow: 1,
-        padding: '5rem',
-        backgroundImage: `url(${planet})`,
-        backgroundRepeat: 'no-repeat', backgroundSize: '750px', backgroundPosition: 'right 0px bottom -600px'
+        marginTop: '4rem', color: colors.dark(0.6),
+        fontSize: '1.5rem', fontWeight: 500
       }
-    }, [
-      registrationLogo(),
-      div({
-        style: {
-          marginTop: '4rem', color: colors.dark(0.6),
-          fontSize: '1.5rem', fontWeight: 500
-        }
-      }, 'New User Registration'),
-      div({ style: { marginTop: '3rem', display: 'flex' } }, [
-        h(IdContainer, [id => div({ style: { lineHeight: '170%' } }, [
-          label({ htmlFor: id }, ['First Name *']),
-          h(TextInput, {
-            id,
-            style: { display: 'block' },
-            value: givenName,
-            onChange: v => this.setState({ givenName: v })
-          })
-        ])]),
-        div({ style: { width: '1rem' } }),
-        h(IdContainer, [id => div({ style: { lineHeight: '170%' } }, [
-          label({ htmlFor: id }, ['Last Name *']),
-          h(TextInput, {
-            id,
-            style: { display: 'block' },
-            value: familyName,
-            onChange: v => this.setState({ familyName: v })
-          })
-        ])])
-      ]),
+    }, 'New User Registration'),
+    div({ style: { marginTop: '3rem', display: 'flex' } }, [
       h(IdContainer, [id => div({ style: { lineHeight: '170%' } }, [
-        label({ htmlFor: id, style: { display: 'block', marginTop: '2rem' } }, ['Contact Email for Notifications *']),
-        div([
-          h(TextInput, {
-            id,
-            value: email,
-            onChange: v => this.setState({ email: v }),
-            style: { width: '50ex' }
-          })
-        ])
+        label({ htmlFor: id }, ['First Name *']),
+        h(TextInput, {
+          id,
+          style: { display: 'block' },
+          value: givenName,
+          onChange: v => setGivenName(v)
+        })
       ])]),
-      div({ style: { marginTop: '3rem' } }, [
-        h(ButtonPrimary, { disabled: errors || busy, onClick: () => this.register() },
-          'Register'
-        ),
-        h(ButtonSecondary, { style: { marginLeft: '1rem' }, onClick: signOut }, 'Cancel'),
-        busy && centeredSpinner({
-          size: 34, style: { display: null, margin: null, marginLeft: '1ex' }
+      div({ style: { width: '1rem' } }),
+      h(IdContainer, [id => div({ style: { lineHeight: '170%' } }, [
+        label({ htmlFor: id }, ['Last Name *']),
+        h(TextInput, {
+          id,
+          style: { display: 'block' },
+          value: familyName,
+          onChange: v => setFamilyName(v)
+        })
+      ])])
+    ]),
+    h(IdContainer, [id => div({ style: { lineHeight: '170%' } }, [
+      label({ htmlFor: id, style: { display: 'block', marginTop: '2rem' } }, ['Contact Email for Notifications *']),
+      div([
+        h(TextInput, {
+          id,
+          value: email,
+          onChange: v => setEmail(v),
+          style: { width: '50ex' }
         })
       ])
+    ])]),
+    div({ style: { marginTop: '3rem' } }, [
+      h(ButtonPrimary, { disabled: errors || busy, onClick: () => register() },
+        'Register'
+      ),
+      h(ButtonSecondary, { style: { marginLeft: '1rem' }, onClick: signOut }, 'Cancel'),
+      busy && centeredSpinner({
+        size: 34, style: { display: null, margin: null, marginLeft: '1ex' }
+      })
     ])
-  }
+  ])
 }
+
+export default Register
