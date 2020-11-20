142d2642323aceecbf38d744a2a81b3c6a417bc0
Isaac Zarsky <zarsky@broadinstitute.org>
2020-11-20 17:01:00 -0500

    make some modals functional

--- src/components/group-common.js
+++ src/components/group-common.js
@@ -1,6 +1,5 @@
 import _ from 'lodash/fp'
-import PropTypes from 'prop-types'
-import { Component, Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { b, div, h, label } from 'react-hyperscript-helpers'
 import { ButtonPrimary, Clickable, IdContainer, LabeledCheckbox, Link, spinnerOverlay } from 'src/components/common'
 import { icon } from 'src/components/icons'
@@ -10,7 +9,7 @@ import { InfoBox } from 'src/components/PopupTrigger'
 import TooltipTrigger from 'src/components/TooltipTrigger'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
-import { reportError, withErrorReporting } from 'src/libs/error'
+import { withErrorReporting } from 'src/libs/error'
 import { FormLabel } from 'src/libs/forms'
 import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
@@ -75,32 +74,21 @@ export const MemberCard = Utils.memoWithName('MemberCard', ({ member: { email, r
   ])
 })
 
-export const NewUserModal = Utils.withCancellationSignal(class NewUserModal extends Component {
-  static propTypes = {
-    addFunction: PropTypes.func.isRequired,
-    adminLabel: PropTypes.string.isRequired,
-    footer: PropTypes.node,
-    onDismiss: PropTypes.func.isRequired,
-    onSuccess: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    userLabel: PropTypes.string.isRequired
-  }
+export const NewUserModal = ({
+  addFunction, addUnregisteredUser = false, adminLabel, userLabel,
+  title, onSuccess, onDismiss, footer
+}) => {
+  const [userEmail, setUserEmail] = useState('')
+  const [suggestions, setSuggestions] = useState([])
+  const [confirmAddUser, setConfirmAddUser] = useState(false)
+  const [roles, setRoles] = useState([userLabel])
+  const [submitError, setSubmitError] = useState(undefined)
+  const [busy, setBusy] = useState(false)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      userEmail: '',
-      suggestions: [],
-      confirmAddUser: false,
-      roles: [props.userLabel],
-      busy: false
-    }
-  }
+  const signal = Utils.useCancellation()
 
-  async componentDidMount() {
-    const { signal } = this.props
-
-    try {
+  Utils.useOnMount(() => {
+    const loadData = withErrorReporting('Error looking up collaborators', async () => {
       const [shareSuggestions, groups] = await Promise.all([
         Ajax(signal).Workspaces.getShareLog(),
         Ajax(signal).Groups.list()
@@ -112,174 +100,135 @@ export const NewUserModal = Utils.withCancellationSignal(class NewUserModal exte
         _.uniq
       )(groups)
 
-      this.setState({ suggestions })
-    } catch (error) {
-      reportError('Error looking up collaborators', error)
-    }
-  }
+      setSuggestions(suggestions)
+    })
 
-  render() {
-    const { adminLabel, userLabel, title, onDismiss, footer } = this.props
-    const { confirmAddUser, userEmail, roles, suggestions, busy, submitError } = this.state
-    const errors = validate({ userEmail }, { userEmail: { email: true } })
-    const isAdmin = _.includes(adminLabel, roles)
-
-    const userEmailInvalid = !!validate({ userEmail }, { userEmail: { email: true } })
-    const canAdd = value => value !== userEmail || !userEmailInvalid
-
-    return Utils.cond(
-      [confirmAddUser, () => h(Modal, {
-        title: 'User is not registered',
-        okButton: h(ButtonPrimary, { onClick: this.inviteUser }, ['Yes']),
-        cancelText: 'No',
-        onDismiss: () => this.setState({ confirmAddUser: false })
-      }, ['Add ', b(userEmail), ' to the group anyway?', busy && spinnerOverlay])],
-      () => h(Modal, {
-        onDismiss,
-        title,
-        okButton: h(ButtonPrimary, {
-          tooltip: Utils.summarizeErrors(errors),
-          onClick: () => this.addUser(),
-          disabled: errors
-        }, ['Add User'])
-      }, [
-        h(IdContainer, [id => h(Fragment, [
-          h(FormLabel, { htmlFor: id, required: true }, ['User email']),
-          h(AutocompleteTextInput, {
-            id,
-            autoFocus: true,
-            openOnFocus: false,
-            value: userEmail,
-            onChange: v => this.setState({ userEmail: v }),
-            renderSuggestion: suggestion => div({ style: styles.suggestionContainer }, [
-              div({ style: { flex: 1 } }, [
-                !canAdd(suggestion) && h(TooltipTrigger, {
-                  content: 'Not a valid email address'
-                }, [
-                  icon('warning-standard', { style: { color: colors.danger(), marginRight: '0.5rem' } })
-                ]),
-                suggestion
-              ])
-            ]),
-            suggestions: [...(!!userEmail && !suggestions.includes(userEmail) ? [userEmail] : []), ...suggestions],
-            style: { fontSize: 16 },
-            type: undefined
-          })
-        ])]),
-        h(FormLabel, ['Role']),
-        h(LabeledCheckbox, {
-          checked: isAdmin,
-          onChange: () => this.setState({ roles: [isAdmin ? userLabel : adminLabel] })
-        }, [
-          label({ style: { margin: '0 2rem 0 0.25rem' } }, [`Can manage users (${adminLabel})`])
-        ]),
-        footer && div({ style: { marginTop: '1rem' } }, [footer]),
-        submitError && div({ style: { marginTop: '0.5rem', textAlign: 'right', color: colors.danger() } }, [submitError]),
-        busy && spinnerOverlay
-      ])
-    )
-  }
-
-  inviteUser = _.flow(
-    withErrorReporting('Error adding user'),
-    Utils.withBusyState(busy => this.setState({ busy }))
-  )(async () => {
-    const { signal } = this.props
-    const { userEmail } = this.state
-    await Ajax(signal).User.inviteUser(userEmail)
-    await this.submit()
+    loadData()
   })
 
-  addUser = _.flow(
-    withErrorReporting('Error adding user'),
-    Utils.withBusyState(busy => this.setState({ busy }))
-  )(async () => {
-    const { addUnregisteredUser = false, signal } = this.props
-    const { userEmail } = this.state
-    addUnregisteredUser && !await Ajax(signal).User.isUserRegistered(userEmail) ? this.setState({ confirmAddUser: true }) : await this.submit()
-  })
-
-  async submit() {
-    const { addFunction, onSuccess } = this.props
-    const { userEmail, roles } = this.state
+  const submit = async () => { // only called by invite and add, which set busy & catch errors
     try {
-      this.setState({ busy: true })
       await addFunction(roles, userEmail)
-      onSuccess()
+      onSuccess() // TODO: this can cause busy to be updated in state after unmount
     } catch (error) {
-      this.setState({ busy: false, confirmAddUser: false })
       if (400 <= error.status && error.status <= 499) {
-        this.setState({ submitError: (await error.json()).message })
+        setSubmitError((await error.json()).message)
       } else {
-        reportError('Error adding user', error)
+        throw error
       }
     }
   }
-})
 
-export const EditUserModal = class EditUserModal extends Component {
-  static propTypes = {
-    adminLabel: PropTypes.string.isRequired,
-    onDismiss: PropTypes.func.isRequired,
-    onSuccess: PropTypes.func.isRequired,
-    saveFunction: PropTypes.func.isRequired,
-    user: PropTypes.shape({
-      email: PropTypes.string.isRequired,
-      roles: PropTypes.array.isRequired
-    }).isRequired,
-    userLabel: PropTypes.string.isRequired
-  }
+  const inviteUser = _.flow(
+    withErrorReporting('Error adding user'),
+    Utils.withBusyState(setBusy)
+  )(async () => {
+    await Ajax(signal).User.inviteUser(userEmail)
+    await submit()
+  })
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      isAdmin: _.includes(props.adminLabel, props.user.roles)
-    }
-  }
+  const addUser = _.flow(
+    withErrorReporting('Error adding user'),
+    Utils.withBusyState(setBusy)
+  )(async () => {
+    addUnregisteredUser && !await Ajax(signal).User.isUserRegistered(userEmail) ? setConfirmAddUser(true) : await submit()
+  })
 
-  render() {
-    const { adminLabel, onDismiss, user: { email } } = this.props
-    const { isAdmin, submitting } = this.state
+  const errors = validate({ userEmail }, { userEmail: { email: true } })
+  const isAdmin = _.includes(adminLabel, roles)
 
-    return h(Modal, {
+  const userEmailInvalid = !!validate({ userEmail }, { userEmail: { email: true } })
+  const canAdd = value => value !== userEmail || !userEmailInvalid
+
+  return Utils.cond(
+    [confirmAddUser, () => h(Modal, {
+      title: 'User is not registered',
+      okButton: h(ButtonPrimary, { onClick: inviteUser }, ['Yes']),
+      cancelText: 'No',
+      onDismiss: () => setConfirmAddUser(false)
+    }, ['Add ', b(userEmail), ' to the group anyway?', busy && spinnerOverlay])],
+    () => h(Modal, {
       onDismiss,
-      title: 'Edit Roles',
+      title,
       okButton: h(ButtonPrimary, {
-        onClick: () => this.submit()
-      }, ['Change Role'])
+        tooltip: Utils.summarizeErrors(errors),
+        onClick: () => addUser(),
+        disabled: errors
+      }, ['Add User'])
     }, [
-      div({ style: { marginBottom: '0.25rem' } }, [
-        'Edit role for ',
-        b([email])
-      ]),
+      h(IdContainer, [id => h(Fragment, [
+        h(FormLabel, { htmlFor: id, required: true }, ['User email']),
+        h(AutocompleteTextInput, {
+          id,
+          autoFocus: true,
+          openOnFocus: false,
+          value: userEmail,
+          onChange: setUserEmail,
+          renderSuggestion: suggestion => div({ style: styles.suggestionContainer }, [
+            div({ style: { flex: 1 } }, [
+              !canAdd(suggestion) && h(TooltipTrigger, {
+                content: 'Not a valid email address'
+              }, [
+                icon('warning-standard', { style: { color: colors.danger(), marginRight: '0.5rem' } })
+              ]),
+              suggestion
+            ])
+          ]),
+          suggestions: [...(!!userEmail && !suggestions.includes(userEmail) ? [userEmail] : []), ...suggestions],
+          style: { fontSize: 16 },
+          type: undefined
+        })
+      ])]),
+      h(FormLabel, ['Role']),
       h(LabeledCheckbox, {
         checked: isAdmin,
-        onChange: () => this.setState({ isAdmin: !isAdmin })
+        onChange: () => setRoles([isAdmin ? userLabel : adminLabel])
       }, [
         label({ style: { margin: '0 2rem 0 0.25rem' } }, [`Can manage users (${adminLabel})`])
       ]),
-      submitting && spinnerOverlay
+      footer && div({ style: { marginTop: '1rem' } }, [footer]),
+      submitError && div({ style: { marginTop: '0.5rem', textAlign: 'right', color: colors.danger() } }, [submitError]),
+      busy && spinnerOverlay
     ])
-  }
+  )
+}
 
-  async submit() {
-    const { adminLabel, userLabel, user: { email, roles }, onSuccess, saveFunction } = this.props
-    const { isAdmin } = this.state
+export const EditUserModal = ({ adminLabel, userLabel, user: { email, roles }, onSuccess, onDismiss, saveFunction }) => {
+  const [isAdmin, setIsAdmin] = useState(_.includes(adminLabel, roles))
+  const [submitting, setSubmitting] = useState(false)
 
+  const submit = _.flow(
+    Utils.withBusyState(setSubmitting),
+    withErrorReporting('Error updating user')
+  )(async () => {
     const applyAdminChange = _.flow(
       _.without([isAdmin ? userLabel : adminLabel]),
       _.union([isAdmin ? adminLabel : userLabel])
     )
 
-    try {
-      this.setState({ submitting: true })
-      await saveFunction(email, roles, applyAdminChange(roles))
-      onSuccess()
-    } catch (error) {
-      this.setState({ submitting: false })
-      reportError('Error updating user', error)
-    }
-  }
+    await saveFunction(email, roles, applyAdminChange(roles))
+    onSuccess()
+  })
+
+  return h(Modal, {
+    onDismiss,
+    title: 'Edit Roles',
+    okButton: h(ButtonPrimary, {
+      onClick: () => submit()
+    }, ['Change Role'])
+  }, [
+    div({ style: { marginBottom: '0.25rem' } }, [
+      'Edit role for ',
+      b([email])
+    ]),
+    h(LabeledCheckbox, {
+      checked: isAdmin,
+      onChange: () => setIsAdmin(!isAdmin)
+    }, [
+      label({ style: { margin: '0 2rem 0 0.25rem' } }, [`Can manage users (${adminLabel})`])
+    ]),
+    submitting && spinnerOverlay
+  ])
 }
 
 export const DeleteUserModal = ({ onDismiss, onSubmit, userEmail }) => {
