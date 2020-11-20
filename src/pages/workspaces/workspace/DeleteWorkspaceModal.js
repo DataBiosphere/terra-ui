ce8364693864a6b3d837176508b81b4ca681e437
Isaac Zarsky <zarsky@broadinstitute.org>
2020-11-20 17:39:43 -0500

    maybe one isn't a modal but hey

--- src/pages/workspaces/workspace/DeleteWorkspaceModal.js
+++ src/pages/workspaces/workspace/DeleteWorkspaceModal.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp'
-import { Component } from 'react'
+import { useState } from 'react'
 import { div, h, label, span } from 'react-hyperscript-helpers'
 import { ButtonPrimary, Link, spinnerOverlay } from 'src/components/common'
 import { TextInput } from 'src/components/input'
@@ -10,65 +10,57 @@ import { reportError } from 'src/libs/error'
 import * as Utils from 'src/libs/utils'
 
 
-export default class DeleteWorkspaceModal extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      deleting: false,
-      deleteConfirmation: ''
-    }
-  }
+const DeleteWorkspaceModal = ({ workspace: { workspace: { namespace, name, bucketName } }, onDismiss, onSuccess }) => {
+  const [deleting, setDeleting] = useState(false)
+  const [deleteConfirmation, setDeleteConfirmation] = useState('')
 
-  async deleteWorkspace() {
-    const { workspace: { workspace: { namespace, name } }, onDismiss, onSuccess } = this.props
+  const deleteWorkspace = async () => {
     try {
-      this.setState({ deleting: true })
+      setDeleting(true)
       await Ajax().Workspaces.workspace(namespace, name).delete()
       onDismiss()
       onSuccess()
     } catch (error) {
       reportError('Error deleting workspace', error)
-      this.setState({ deleting: false })
+      setDeleting(false)
     }
   }
 
-  render() {
-    const { workspace: { workspace: { bucketName, name } }, onDismiss } = this.props
-    const { deleteConfirmation, deleting } = this.state
-    return h(Modal, {
-      title: 'Delete workspace',
-      onDismiss,
-      okButton: h(ButtonPrimary, {
-        disabled: _.toLower(deleteConfirmation) !== 'delete workspace',
-        onClick: () => this.deleteWorkspace()
-      }, 'Delete workspace')
-    }, [
-      div(['Are you sure you want to permanently delete the workspace ',
-        span({ style: { fontWeight: 600, wordBreak: 'break-word' } }, name),
-        '?']),
-      div({ style: { marginTop: '1rem' } }, [
-        'Deleting it will delete the associated ',
-        h(Link, {
-          ...Utils.newTabLinkProps,
-          href: bucketBrowserUrl(bucketName)
-        }, ['Google Cloud Bucket']),
-        ' and all its data.'
-      ]), div({
-        style: {
-          fontWeight: 500,
-          marginTop: '1rem'
-        }
-      }, 'This cannot be undone.'),
-      div({ style: { marginTop: '1rem' } }, [
-        label({ htmlFor: 'delete-workspace-confirmation' }, ['Please type \'Delete Workspace\' to continue:']),
-        h(TextInput, {
-          id: 'delete-workspace-confirmation',
-          placeholder: 'Delete Workspace',
-          value: deleteConfirmation,
-          onChange: v => this.setState({ deleteConfirmation: v })
-        })
-      ]),
-      deleting && spinnerOverlay
-    ])
-  }
+  return h(Modal, {
+    title: 'Delete workspace',
+    onDismiss,
+    okButton: h(ButtonPrimary, {
+      disabled: _.toLower(deleteConfirmation) !== 'delete workspace',
+      onClick: () => deleteWorkspace()
+    }, 'Delete workspace')
+  }, [
+    div(['Are you sure you want to permanently delete the workspace ',
+      span({ style: { fontWeight: 600, wordBreak: 'break-word' } }, name),
+      '?']),
+    div({ style: { marginTop: '1rem' } }, [
+      'Deleting it will delete the associated ',
+      h(Link, {
+        ...Utils.newTabLinkProps,
+        href: bucketBrowserUrl(bucketName)
+      }, ['Google Cloud Bucket']),
+      ' and all its data.'
+    ]), div({
+      style: {
+        fontWeight: 500,
+        marginTop: '1rem'
+      }
+    }, 'This cannot be undone.'),
+    div({ style: { marginTop: '1rem' } }, [
+      label({ htmlFor: 'delete-workspace-confirmation' }, ['Please type \'Delete Workspace\' to continue:']),
+      h(TextInput, {
+        id: 'delete-workspace-confirmation',
+        placeholder: 'Delete Workspace',
+        value: deleteConfirmation,
+        onChange: setDeleteConfirmation
+      })
+    ]),
+    deleting && spinnerOverlay
+  ])
 }
+
+export default DeleteWorkspaceModal
