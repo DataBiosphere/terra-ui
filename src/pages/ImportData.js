454307b3d2509b76beb3ccfdd14dd75c6128cca1
Matt Bemis <mbemis@broadinstitute.org>
2020-12-04 13:38:34 -0500

    pairing

--- src/pages/ImportData.js
+++ src/pages/ImportData.js
@@ -65,9 +65,8 @@ const ImportData = () => {
   const [selectedWorkspaceId, setSelectedWorkspaceId] = useState(wid)
   const [selectedTemplateWorkspaceKey, setSelectedTemplateWorkspaceKey] = useState()
   const [allTemplates, setAllTemplates] = useState()
-  const datatypeName = format.localeCompare('snapshot') ? 'dataset' : 'snapshot'
-
-  const noteMessage = format.localeCompare('snapshot') ? 'Note that the import process may take some time after you are redirected into your destination workspace.' : ''
+  const isDataset = format !== 'snapshot'
+  const noteMessage = 'Note that the import process may take some time after you are redirected into your destination workspace.'
 
   const selectedWorkspace = _.find({ workspace: { workspaceId: selectedWorkspaceId } }, workspaces)
 
@@ -121,13 +120,14 @@ const ImportData = () => {
 
   return h(FooterWrapper, [
     backgroundLogo,
-    h(TopBar, { title: 'Import Data' }),
+    h(TopBar, { title: `Import ${isDataset ? 'Data' : 'Snapshot'}` }),
     div({ role: 'main', style: styles.container }, [
       div({ style: styles.card }, [
-        div({ style: styles.title }, ['Importing Data']),
+        div({ style: styles.title }, [`Importing ${isDataset ? 'Data' : `Snapshot ${snapshotName}`}`]),
         div({ style: { fontSize: 16 } }, ['From: ', new URL(url).hostname]),
         div({ style: { marginTop: '1rem' } }, [
-          ['The ', datatypeName, '(s) you just chose to import to Terra will be made available to you within a workspace of your choice where you can then perform analysis.']
+          `The ${isDataset ? 'dataset' : 'snapshot'}(s) you just chose to import to Terra will be made available to you `,
+          'within a workspace of your choice where you can then perform analysis.'
         ])
       ]),
       div({ style: { ...styles.card, marginLeft: '2rem' } }, [
@@ -144,8 +144,7 @@ const ImportData = () => {
                 value: selectedWorkspaceId,
                 onChange: setSelectedWorkspaceId
               }),
-              div({ style: { marginTop: '0.5rem', lineHeight: '1.5' } },
-                [noteMessage]),
+              isDataset && div({ style: { marginTop: '0.5rem', lineHeight: '1.5' } }, [noteMessage]),
               div({ style: { display: 'flex', alignItems: 'center', marginTop: '1rem' } }, [
                 h(ButtonSecondary, { onClick: () => setMode(), style: { marginLeft: 'auto' } }, ['Back']),
                 h(ButtonPrimary, {
@@ -159,9 +158,7 @@ const ImportData = () => {
           ['template', () => {
             return h(Fragment, [
               div({ style: styles.title }, ['Start with a template']),
-              div({ style: { marginBottom: '1rem', lineHeight: '1.5' } }, [
-                noteMessage
-              ]),
+              isDataset && div({ style: { marginBottom: '1rem', lineHeight: '1.5' } }, [noteMessage]),
               div({ style: { overflow: 'auto', maxHeight: '25rem' } }, [
                 _.map(([i, ws]) => {
                   const { name, namespace, description, hasNotebooks, hasWorkflows } = ws
@@ -228,7 +225,7 @@ const ImportData = () => {
               }),
               isCreateOpen && h(NewWorkspaceModal, {
                 requiredAuthDomain: ad,
-                customMessage: noteMessage,
+                customMessage: isDataset && noteMessage,
                 onDismiss: () => setIsCreateOpen(false),
                 onSuccess: w => {
                   setMode('existing')
@@ -245,7 +242,7 @@ const ImportData = () => {
           cloneWorkspace: _.find({ workspace: selectedTemplateWorkspaceKey }, workspaces),
           title: `Clone ${selectedTemplateWorkspaceKey.name} and Import Data`,
           buttonText: 'Clone and Import',
-          customMessage: noteMessage,
+          customMessage: isDataset && noteMessage,
           onDismiss: () => setIsCloneOpen(false),
           onSuccess: w => {
             setMode('existing')
