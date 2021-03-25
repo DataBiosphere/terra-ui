37ba8d80c72443a356c328e09b08e67971c94ce0
Willy Nojopranoto <nojopranoto@google.com>
2021-03-25 18:47:42 +0000

    Move allRegions array to region-common

--- src/components/NewWorkspaceModal.js
+++ src/components/NewWorkspaceModal.js
@@ -11,6 +11,7 @@ import colors from 'src/libs/colors'
 import { withErrorReporting } from 'src/libs/error'
 import Events from 'src/libs/events'
 import { FormLabel } from 'src/libs/forms'
+import { allRegions } from 'src/components/region-common'
 import * as Nav from 'src/libs/nav'
 import * as Utils from 'src/libs/utils'
 import validate from 'validate.js'
@@ -29,35 +30,6 @@ const constraints = {
     presence: true
   }
 }
-
-const allRegions = [
-  { value: '', label: 'US multi-regional (default)' },
-  { value: 'northamerica-northeast1', label: 'northamerica-northeast1 (Montreal)' },
-  { value: 'southamerica-east1', label: 'southamerica-east1 (Sao Paulo)' },
-  { value: 'us-central1', label: 'us-central1 (Iowa)' },
-  { value: 'us-east1', label: 'us-east1 (South Carolina)' },
-  { value: 'us-east4', label: 'us-east4 (Northern Virginia)' },
-  { value: 'us-west1', label: 'us-west1 (Oregon)' },
-  { value: 'us-west2', label: 'us-west2 (Los Angeles)' },
-  { value: 'us-west3', label: 'us-west3 (Salt Lake City)' },
-  { value: 'us-west4', label: 'us-west4 (Las Vegas)' },
-  { value: 'europe-central2', label: 'europe-central2 (Warsaw)' },
-  { value: 'europe-north1', label: 'europe-north1 (Finland)' },
-  { value: 'europe-west1', label: 'europe-west1 (Belgium)' },
-  { value: 'europe-west2', label: 'europe-west2 (London)' },
-  { value: 'europe-west3', label: 'europe-west3 (Frankfurt)' },
-  { value: 'europe-west4', label: 'europe-west4 (Netherlands)' },
-  { value: 'europe-west6', label: 'europe-west6 (Zurich)' },
-  { value: 'asia-east1', label: 'asia-east1 (Taiwan)' },
-  { value: 'asia-east2', label: 'asia-east2 (Hong Kong)' },
-  { value: 'asia-northeast1', label: 'asia-northeast1 (Tokyo)' },
-  { value: 'asia-northeast2', label: 'asia-northeast2 (Osaka)' },
-  { value: 'asia-northeast3', label: 'asia-northeast3 (Seoul)' },
-  { value: 'asia-south1', label: 'asia-south1 (Mumbai)' },
-  { value: 'asia-southeast1', label: 'asia-southeast1 (Singapore)' },
-  { value: 'asia-southeast2', label: 'asia-southeast2 (Jakarta)' },
-  { value: 'australia-southeast1', label: 'australia-southeast1 (Sydney)' }
-]
 
 const NewWorkspaceModal = Utils.withDisplayName('NewWorkspaceModal', ({
   cloneWorkspace, onSuccess, onDismiss, customMessage, requiredAuthDomain, title, buttonText
