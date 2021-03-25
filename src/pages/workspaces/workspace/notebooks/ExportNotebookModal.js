896ec57525f5e5832d7d7834959676ab57f063ce
Isaac Zarsky <zarsky@broadinstitute.org>
2021-03-25 18:57:21 -0400

    convert notebooks tab and export modal to hooks (#2410)

--- src/pages/workspaces/workspace/notebooks/ExportNotebookModal.js
+++ src/pages/workspaces/workspace/notebooks/ExportNotebookModal.js
@@ -1,12 +1,12 @@
 import _ from 'lodash/fp'
 import PropTypes from 'prop-types'
-import { Component, Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { b, h } from 'react-hyperscript-helpers'
 import { ButtonPrimary, IdContainer, spinnerOverlay } from 'src/components/common'
 import ErrorView from 'src/components/ErrorView'
 import Modal from 'src/components/Modal'
 import { notebookNameInput, notebookNameValidator } from 'src/components/notebook-utils'
-import { withWorkspaces, WorkspaceSelector } from 'src/components/workspace-utils'
+import { useWorkspaces, WorkspaceSelector } from 'src/components/workspace-utils'
 import { Ajax } from 'src/libs/ajax'
 import Events, { extractCrossWorkspaceDetails } from 'src/libs/events'
 import { FormLabel } from 'src/libs/forms'
@@ -17,143 +17,111 @@ import validate from 'validate.js'
 
 const cutName = name => name.slice(10, -6) // removes 'notebooks/' and the .ipynb suffix
 
-export default _.flow(
-  Utils.withCancellationSignal,
-  withWorkspaces
-)(class ExportNotebookModal extends Component {
-  static propTypes = {
-    fromLauncher: PropTypes.bool,
-    onDismiss: PropTypes.func.isRequired,
-    printName: PropTypes.string.isRequired,
-    workspace: PropTypes.object.isRequired
-  }
+const ExportNotebookModal = ({ fromLauncher, onDismiss, printName, workspace }) => {
+  // State
+  const [selectedWorkspaceId, setSelectedWorkspaceId] = useState(undefined)
+  const [error, setError] = useState(undefined)
+  const [copying, setCopying] = useState(false)
+  const [copied, setCopied] = useState(false)
+  const [newName, setNewName] = useState(printName)
+  const [existingNames, setExistingNames] = useState(undefined)
 
-  constructor(props) {
-    super(props)
+  const signal = Utils.useCancellation()
+  const { workspaces } = useWorkspaces()
 
-    this.state = {
-      selectedWorkspaceId: undefined,
-      error: undefined,
-      copied: false,
-      newName: props.printName
-    }
-  }
 
-  getSelectedWorkspace() {
-    const { workspaces } = this.props
-    const { selectedWorkspaceId } = this.state
-    return _.find({ workspace: { workspaceId: selectedWorkspaceId } }, workspaces)
-  }
+  // Helpers
+  const selectedWorkspace = _.find({ workspace: { workspaceId: selectedWorkspaceId } }, workspaces)
 
-  render() {
-    const { copied } = this.state
-    return copied ? this.renderPostCopy() : this.renderCopyForm()
-  }
-
-  renderCopyForm() {
-    const { workspaces, workspace, onDismiss } = this.props
-    const { selectedWorkspaceId, copying, error, newName, existingNames } = this.state
-    const errors = validate(
-      { selectedWorkspaceId, newName },
-      {
-        selectedWorkspaceId: { presence: true },
-        newName: notebookNameValidator(existingNames)
-      },
-      { prettify: v => ({ newName: 'Name' }[v] || validate.prettify(v)) }
-    )
-    return h(Modal, {
-      title: 'Copy to Workspace',
-      onDismiss,
-      okButton: h(ButtonPrimary, {
-        tooltip: Utils.summarizeErrors(errors),
-        disabled: !!errors,
-        onClick: () => this.copy()
-      }, ['Copy'])
-    }, [
-      h(IdContainer, [id => h(Fragment, [
-        h(FormLabel, { htmlFor: id, required: true }, ['Destination']),
-        h(WorkspaceSelector, {
-          id,
-          workspaces: _.filter(Utils.isValidWsExportTarget(workspace), workspaces),
-          value: selectedWorkspaceId,
-          onChange: v => {
-            this.setState({ selectedWorkspaceId: v })
-            this.findNotebooks(v)
-          }
-        })
-      ])]),
-      h(IdContainer, [id => h(Fragment, [
-        h(FormLabel, { htmlFor: id, required: true }, ['Name']),
-        notebookNameInput({
-          error: Utils.summarizeErrors(errors && errors.newName),
-          inputProps: {
-            id,
-            value: newName,
-            onChange: v => this.setState({ newName: v })
-          }
-        })
-      ])]),
-      copying && spinnerOverlay,
-      error && h(ErrorView, { error })
-    ])
-  }
-
-  renderPostCopy() {
-    const { onDismiss, fromLauncher } = this.props
-    const { newName } = this.state
-    const selectedWorkspace = this.getSelectedWorkspace().workspace
-
-    return h(Modal, {
-      title: 'Copy to Workspace',
-      onDismiss,
-      cancelText: 'Stay Here',
-      okButton: h(ButtonPrimary, {
-        onClick: () => {
-          if (fromLauncher) {
-            Nav.goToPath('workspace-notebook-launch', {
-              namespace: selectedWorkspace.namespace,
-              name: selectedWorkspace.name,
-              notebookName: `${newName}.ipynb`
-            })
-          } else {
-            Nav.goToPath('workspace-notebooks', {
-              namespace: selectedWorkspace.namespace,
-              name: selectedWorkspace.name
-            })
-          }
-        }
-      }, ['Go to copied notebook'])
-    }, [
-      'Successfully copied ',
-      b([newName]),
-      ' to ',
-      b([selectedWorkspace.name]),
-      '. Do you want to view the copied notebook?'
-    ])
-  }
-
-  async findNotebooks(v) {
-    const { signal, workspaces } = this.props
+  const findNotebooks = async v => {
     const tempChosenWorkspace = _.find({ workspace: { workspaceId: v } }, workspaces).workspace
     const selectedNotebooks = await Ajax(signal).Buckets.listNotebooks(tempChosenWorkspace.namespace, tempChosenWorkspace.bucketName)
-    const existingNames = _.map(({ name }) => cutName(name), selectedNotebooks)
-    this.setState({ existingNames })
+    setExistingNames(_.map(({ name }) => cutName(name), selectedNotebooks))
   }
 
-  async copy() {
-    const { printName, workspace } = this.props
-    const { newName } = this.state
-    const selectedWorkspace = this.getSelectedWorkspace()
+  const copy = Utils.withBusyState(setCopying, async () => {
     try {
-      this.setState({ copying: true })
       await Ajax()
         .Buckets
         .notebook(workspace.workspace.namespace, workspace.workspace.bucketName, printName)
         .copy(newName, selectedWorkspace.workspace.bucketName)
-      this.setState({ copied: true })
+      setCopied(true)
       Ajax().Metrics.captureEvent(Events.notebookCopy, { oldName: printName, newName, ...extractCrossWorkspaceDetails(workspace, selectedWorkspace) })
     } catch (error) {
-      this.setState({ error: await error.text(), copying: false })
+      setError(await error.text())
     }
-  }
-})
+  })
+
+
+  // Render
+  const errors = validate(
+    { selectedWorkspaceId, newName },
+    {
+      selectedWorkspaceId: { presence: true },
+      newName: notebookNameValidator(existingNames)
+    },
+    { prettify: v => ({ newName: 'Name' }[v] || validate.prettify(v)) }
+  )
+
+  return h(Modal, {
+    title: 'Copy to Workspace',
+    onDismiss,
+    cancelText: copied ? 'Stay Here' : undefined,
+    okButton: h(ButtonPrimary, {
+      tooltip: Utils.summarizeErrors(errors),
+      disabled: !!errors,
+      onClick: copied ?
+        () => Nav.goToPath(fromLauncher ? 'workspace-notebook-launch' : 'workspace-notebooks', {
+          namespace: selectedWorkspace.workspace.namespace,
+          name: selectedWorkspace.workspace.name,
+          notebookName: `${newName}.ipynb`
+        }) :
+        copy
+    }, [copied ? 'Go to copied notebook' : 'Copy'])
+  }, [
+    copied ?
+      h(Fragment, [
+        'Successfully copied ',
+        b([newName]),
+        ' to ',
+        b([selectedWorkspace.workspace.name]),
+        '. Do you want to view the copied notebook?'
+      ]) :
+      h(Fragment, [
+        h(IdContainer, [id => h(Fragment, [
+          h(FormLabel, { htmlFor: id, required: true }, ['Destination']),
+          h(WorkspaceSelector, {
+            id,
+            workspaces: _.filter(Utils.isValidWsExportTarget(workspace), workspaces),
+            value: selectedWorkspaceId,
+            onChange: v => {
+              setSelectedWorkspaceId(v)
+              findNotebooks(v)
+            }
+          })
+        ])]),
+        h(IdContainer, [id => h(Fragment, [
+          h(FormLabel, { htmlFor: id, required: true }, ['Name']),
+          notebookNameInput({
+            error: Utils.summarizeErrors(errors && errors.newName),
+            inputProps: {
+              id,
+              value: newName,
+              onChange: setNewName
+            }
+          })
+        ])])
+      ]),
+    copying && spinnerOverlay,
+    error && h(ErrorView, { error })
+  ])
+}
+
+ExportNotebookModal.propTypes = {
+  fromLauncher: PropTypes.bool,
+  onDismiss: PropTypes.func.isRequired,
+  printName: PropTypes.string.isRequired,
+  workspace: PropTypes.object.isRequired
+}
+
+export default ExportNotebookModal
