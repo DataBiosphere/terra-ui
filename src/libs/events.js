398e9f3147c25bbec58e584908c13f69c6ad5d4d
Kai <kfeldman@broadinstitute.org>
2021-04-07 17:11:59 -0400

    add workspace name and namespace to pageview event

--- src/libs/events.js
+++ src/libs/events.js
@@ -46,11 +46,16 @@ export const extractCrossWorkspaceDetails = (fromWorkspace, toWorkspace) => {
 }
 
 export const PageViewReporter = () => {
-  const { name } = useRoute()
+  const { name, params } = useRoute()
 
   useEffect(() => {
-    Ajax().Metrics.captureEvent(`${eventsList.pageView}:${name}`)
-  }, [name])
+    const isWorkspace = /^#workspaces\/.+\/.+/.test(window.location.hash)
+
+    Ajax().Metrics.captureEvent(
+      `${eventsList.pageView}:${name}`,
+      isWorkspace ? extractWorkspaceDetails(params) : undefined
+    )
+  }, [name]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return null
 }
