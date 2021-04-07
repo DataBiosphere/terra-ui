e8146af52ed77eaffe5e95ba773e5b6e82de1e10
Anu Singh <ansingh@broadinstitute.org>
2021-04-07 17:15:29 -0400

    AS-704: Add integration test for snapshot features (#2430)
    * initial
    
    * ignore for perf env
    
    * delete configs for perf
    
    * review suggestionsg
    
    * remove unnecessary step
    
    * remove .idea
    
    * Update .gitignore
    
    * experiments
    
    * nits
    
    * parse dataRepoUrl from config
    
    * suggestions
    
    * parse data repo url from ui config
    
    * helper fn
    
    * review suggestions
    
    Co-authored-by: ansingh7115 <singh.an@husky.neu.edu>
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>

---
+++ integration-tests/tests/run-workflow-on-snapshot.js
@@ -0,0 +1,89 @@
+const _ = require('lodash/fp')
+const fetch = require('node-fetch')
+const pRetry = require('p-retry')
+const { withWorkspace } = require('../utils/integration-helpers')
+const { click, clickable, delay, dismissNotifications, fillInReplace, findElement, findText, input, select, signIntoTerra, waitForNoSpinners, findInGrid, navChild } = require('../utils/integration-utils')
+const { withUserToken } = require('../utils/terra-sa-utils')
+
+
+const snapshotName = 'testsnapshot'
+const findWorkflowButton = clickable({ textContains: 'Find a Workflow' })
+
+const withDataRepoCheck = test => async options => {
+  const { testUrl } = options
+  const { dataRepoUrlRoot } = await fetch(`${testUrl}/config.json`).then(res => res.json())
+  const res = await fetch(`${dataRepoUrlRoot}/status`)
+  if (res.status === 200) {
+    return test({ ...options, dataRepoUrlRoot })
+  } else {
+    console.error('Skipping data repo snapshot test, API appears to be down')
+  }
+}
+
+const testRunWorkflowOnSnapshotFn = _.flow(
+  withWorkspace,
+  withUserToken,
+  withDataRepoCheck
+)(async ({ dataRepoUrlRoot, page, testUrl, snapshotColumnName, snapshotId, snapshotTableName, token, workflowName, workspaceName }) => {
+  if (!snapshotId) {
+    return
+  }
+  // IMPORT SNAPSHOT
+  await page.goto(`${testUrl}/#import-data?url=${dataRepoUrlRoot}&snapshotId=${snapshotId}&snapshotName=${snapshotName}&format=snapshot`)
+  await signIntoTerra(page, token)
+  await dismissNotifications(page)
+  await click(page, clickable({ textContains: 'Start with an existing workspace' }))
+  await select(page, 'Select a workspace', workspaceName)
+  await click(page, clickable({ text: 'Import' }))
+
+  // ADD WORKFLOW
+  await click(page, navChild('workflows'))
+  await findElement(page, findWorkflowButton)
+  await waitForNoSpinners(page)
+  await click(page, findWorkflowButton)
+  await click(page, clickable({ textContains: workflowName }))
+  await waitForNoSpinners(page)
+  await click(page, clickable({ text: 'Add to Workspace' }))
+
+  // START WORKFLOW ON SNAPSHOT
+  // note that this automatically brings in the highest numbered config, which isn't what happens when going through the method repo in FC
+  await waitForNoSpinners(page)
+  await select(page, 'Entity type selector', snapshotName)
+  await select(page, 'Snapshot table selector', snapshotTableName)
+
+  await click(page, clickable({ textContains: 'Inputs' }))
+  await fillInReplace(page, input({ labelContains: 'echo_strings.echo_to_file.input1' }), `this.${snapshotColumnName}`)
+  await delay(1000) // Without this delay, the input field sometimes reverts back to its default value
+  await click(page, clickable({ text: 'Save' }))
+
+  await click(page, clickable({ textContains: 'Outputs' }))
+  await fillInReplace(page, input({ labelContains: 'echo_strings.echo_to_file.out' }), 'workspace.result')
+  await delay(1000) // Without this delay, the input field sometimes reverts back to its default value
+  await click(page, clickable({ text: 'Save' }))
+
+  await delay(1000) // The Run Analysis button requires time to become enabled after hitting the save button
+  await click(page, clickable({ textContains: 'Run analysis' }))
+
+  await click(page, clickable({ text: 'Launch' }))
+
+  // CHECK WORKFLOW SUCCEEDED AND RESULT IS WRITTEN
+  await pRetry(async () => {
+    try {
+      await findInGrid(page, 'Succeeded', { timeout: 65 * 1000 }) // long enough for the submission details to refresh
+    } catch (e) {
+      throw new Error(e)
+    }
+  }, { retries: 10, factor: 1 })
+
+  await click(page, navChild('data'))
+  await click(page, clickable({ textContains: 'Workspace Data' }))
+  await findText(page, 'result: ')
+})
+
+const testRunWorkflowOnSnapshot = {
+  name: 'run-workflow-on-snapshot',
+  fn: testRunWorkflowOnSnapshotFn,
+  timeout: 15 * 60 * 1000
+}
+
+module.exports = { testRunWorkflowOnSnapshot }
