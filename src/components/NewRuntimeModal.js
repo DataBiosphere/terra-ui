73881b885bc2f7d93d934a921c5473c2e866746c
Willy Nojopranoto <nojopranoto@google.com>
2021-04-09 17:54:08 +0000

    Pass workspace bucket zone/region parameter to Leonardo createRuntime request

--- src/components/NewRuntimeModal.js
+++ src/components/NewRuntimeModal.js
@@ -8,6 +8,7 @@ import { ImageDepViewer } from 'src/components/ImageDepViewer'
 import { NumberInput, TextInput, ValidatedInput } from 'src/components/input'
 import { withModalDrawer } from 'src/components/ModalDrawer'
 import { InfoBox } from 'src/components/PopupTrigger'
+import { regionInfo } from 'src/components/region-common'
 import { SaveFilesHelp } from 'src/components/runtime-common'
 import TitleBar from 'src/components/TitleBar'
 import { cloudServices, machineTypes } from 'src/data/machines'
@@ -145,6 +146,8 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
 
   getInitialState(runtime, disk) {
     const runtimeConfig = runtime?.runtimeConfig
+    console.log('willy initialstate')
+    console.log(runtime)
     return {
       selectedPersistentDiskSize: disk?.size || DEFAULT_DISK_SIZE,
       sparkMode: runtimeConfig?.cloudService === cloudServices.DATAPROC ? (runtimeConfig.numberOfWorkers === 0 ? 'master' : 'cluster') : false,
@@ -187,11 +190,13 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
       cloudService: desiredRuntime.cloudService,
       ...(desiredRuntime.cloudService === cloudServices.GCE ? {
         bootDiskSize: 50,
+        zone: desiredRuntime.zone,
         machineType: desiredRuntime.machineType || defaultGceMachineType,
         ...(desiredRuntime.diskSize ? {
           diskSize: desiredRuntime.diskSize
         } : {})
       } : {
+        region: desiredRuntime.region,
         masterMachineType: desiredRuntime.masterMachineType || defaultDataprocMachineType,
         masterDiskSize: desiredRuntime.masterDiskSize,
         numberOfWorkers: desiredRuntime.numberOfWorkers,
@@ -251,7 +256,7 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
     withErrorReporting('Error creating cloud environment')
   )(async () => {
     const { onSuccess } = this.props
-    const { currentRuntimeDetails, currentPersistentDiskDetails } = this.state
+    const { currentRuntimeDetails, currentPersistentDiskDetails, bucketLocation, bucketLocationType } = this.state
     const { runtime: existingRuntime, persistentDisk: existingPersistentDisk } = this.getExistingEnvironmentConfig()
     const { runtime: desiredRuntime, persistentDisk: desiredPersistentDisk } = this.getDesiredEnvironmentConfig()
     const shouldUpdatePersistentDisk = this.canUpdatePersistentDisk() && !_.isEqual(desiredPersistentDisk, existingPersistentDisk)
@@ -260,10 +265,12 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
     const shouldDeleteRuntime = existingRuntime && !this.canUpdateRuntime()
     const shouldCreateRuntime = !this.canUpdateRuntime() && desiredRuntime
     const { name, bucketName, googleProject } = this.getWorkspaceObj()
+    const { computeZone, clusterRegion } = regionInfo(bucketLocation, bucketLocationType)
 
     const runtimeConfig = desiredRuntime && {
       cloudService: desiredRuntime.cloudService,
       ...(desiredRuntime.cloudService === cloudServices.GCE ? {
+        zone: computeZone,
         machineType: desiredRuntime.machineType || defaultGceMachineType,
         ...(desiredRuntime.diskSize ? {
           diskSize: desiredRuntime.diskSize
@@ -277,6 +284,7 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
           }
         })
       } : {
+        region: clusterRegion,
         masterMachineType: desiredRuntime.masterMachineType || defaultDataprocMachineType,
         masterDiskSize: desiredRuntime.masterDiskSize,
         numberOfWorkers: desiredRuntime.numberOfWorkers,
@@ -325,11 +333,12 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
     const {
       deleteDiskSelected, selectedPersistentDiskSize, viewMode, masterMachineType,
       masterDiskSize, sparkMode, numberOfWorkers, numberOfPreemptibleWorkers, workerMachineType,
-      workerDiskSize, jupyterUserScriptUri, selectedLeoImage, customEnvImage
+      workerDiskSize, jupyterUserScriptUri, selectedLeoImage, customEnvImage, bucketLocation, bucketLocationType
     } = this.state
     const { persistentDisk: existingPersistentDisk, runtime: existingRuntime } = this.getExistingEnvironmentConfig()
     const cloudService = sparkMode ? cloudServices.DATAPROC : cloudServices.GCE
     const desiredNumberOfWorkers = sparkMode === 'cluster' ? numberOfWorkers : 0
+    const { computeZone, clusterRegion } = regionInfo(bucketLocation, bucketLocationType)
     return {
       runtime: Utils.cond(
         [(viewMode !== 'deleteEnvironmentOptions'), () => {
@@ -338,6 +347,7 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
             toolDockerImage: selectedLeoImage === CUSTOM_MODE ? customEnvImage : selectedLeoImage,
             ...(jupyterUserScriptUri && { jupyterUserScriptUri }),
             ...(cloudService === cloudServices.GCE ? {
+              zone: computeZone,
               machineType: masterMachineType || defaultGceMachineType,
               ...(this.shouldUsePersistentDisk() ? {
                 persistentDiskAttached: true
@@ -345,8 +355,9 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
                 diskSize: masterDiskSize
               })
             } : {
+              region: clusterRegion,
               machineType: masterMachineType || defaultDataprocMachineType,
-              masterDiskSize,
+                            masterDiskSize,
               numberOfWorkers: desiredNumberOfWorkers,
               ...(desiredNumberOfWorkers && {
                 numberOfPreemptibleWorkers,
@@ -368,16 +379,18 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
   }
 
   getExistingEnvironmentConfig() {
-    const { currentRuntimeDetails, currentPersistentDiskDetails } = this.state
+    const { currentRuntimeDetails, currentPersistentDiskDetails, bucketLocation, bucketLocationType } = this.state
     const runtimeConfig = currentRuntimeDetails?.runtimeConfig
     const cloudService = runtimeConfig?.cloudService
     const numberOfWorkers = runtimeConfig?.numberOfWorkers || 0
+    const { computeZone, clusterRegion } = regionInfo(bucketLocation, bucketLocationType)
     return {
       runtime: currentRuntimeDetails ? {
         cloudService,
         toolDockerImage: this.getImageUrl(currentRuntimeDetails),
         ...(currentRuntimeDetails?.jupyterUserScriptUri && { jupyterUserScriptUri: currentRuntimeDetails?.jupyterUserScriptUri }),
         ...(cloudService === cloudServices.GCE ? {
+          zone: computeZone,
           machineType: runtimeConfig.machineType || defaultGceMachineType,
           ...(runtimeConfig.persistentDiskId ? {
             persistentDiskAttached: true
@@ -385,6 +398,7 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
             diskSize: runtimeConfig.diskSize
           })
         } : {
+          region: clusterRegion,
           masterMachineType: runtimeConfig.masterMachineType || defaultDataprocMachineType,
           masterDiskSize: runtimeConfig.masterDiskSize || 100,
           numberOfWorkers,
@@ -476,7 +490,7 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
     withErrorReporting('Error loading cloud environment'),
     Utils.withBusyState(v => this.setState({ loading: v }))
   )(async () => {
-    const { googleProject } = this.getWorkspaceObj()
+    const { bucketName, namespace, googleProject, workspaceName } = this.getWorkspaceObj()
     const currentRuntime = this.getCurrentRuntime()
     const currentPersistentDisk = this.getCurrentPersistentDisk()
 
@@ -489,6 +503,8 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
       currentPersistentDisk ? Ajax().Disks.disk(currentPersistentDisk.googleProject, currentPersistentDisk.name).details() : null
     ])
 
+    const { location, locationType } = await Ajax().Workspaces.workspace(namespace, workspaceName).checkBucketLocation(bucketName)
+
     const imageUrl = currentRuntimeDetails ? this.getImageUrl(currentRuntimeDetails) : _.find({ id: 'terra-jupyter-gatk' }, newLeoImages).image
     const foundImage = _.find({ image: imageUrl }, newLeoImages)
     this.setState({
@@ -496,6 +512,8 @@ export const NewRuntimeModal = withModalDrawer({ width: 675 })(class NewRuntimeM
       selectedLeoImage: foundImage ? imageUrl : CUSTOM_MODE,
       customEnvImage: !foundImage ? imageUrl : '',
       jupyterUserScriptUri: currentRuntimeDetails?.jupyterUserScriptUri || '',
+      bucketLocation: location,
+      bucketLocationType: locationType,
       ...this.getInitialState(currentRuntimeDetails, currentPersistentDiskDetails)
     })
   })
--- src/components/region-common.js
+++ src/components/region-common.js
@@ -1,74 +1,76 @@
 // Get a { flag: ..., countryName: ... } object representing a google locationType/location input.
 // 'flag' will always be defined (even if it's a question mark.
 // 'regionDescription' is the same as location when locationType is 'multi-region', or a country name when locationType is 'region'.
+// computeZone is generally the 'a' zone for each region, except for those regions where it is not available.
+// The choice to use the 'a' zone is arbitrary, choosing 'b' zone would also work.
 export const unknownRegionFlag = '❓'
 export const regionInfo = (location, locationType) => {
   switch (locationType) {
     case 'multi-region':
       switch (location) {
         case 'US':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location}` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location}`, computeZone: 'us-central1-a', clusterRegion: 'us-central1' }
         case 'EU':
-          return { flag: '🇪🇺', regionDescription: `${locationType}: ${location}` }
+          return { flag: '🇪🇺', regionDescription: `${locationType}: ${location}`, computeZone: 'europe-central2-a', clusterRegion: 'europe-central2' }
         case 'ASIA':
-          return { flag: '🌏', regionDescription: `${locationType}: ${location}` }
+          return { flag: '🌏', regionDescription: `${locationType}: ${location}`, computeZone: 'asia-east1-a', clusterRegion: 'asia-east1' }
         default:
-          return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}` }
+          return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}`, computeZone: 'UNKNOWN', clusterRegion: 'UNKNOWN' }
       }
     case 'region':
       switch (location) {
         case 'ASIA-EAST1':
-          return { flag: '🇹🇼', regionDescription: `${locationType}: ${location} (Taiwan)` }
+          return { flag: '🇹🇼', regionDescription: `${locationType}: ${location} (Taiwan)`, computeZone: 'asia-east1-a', clusterRegion: 'asia-east1' }
         case 'ASIA-EAST2':
-          return { flag: '🇭🇰', regionDescription: `${locationType}: ${location} (Hong Kong)` }
+          return { flag: '🇭🇰', regionDescription: `${locationType}: ${location} (Hong Kong)`, computeZone: 'asia-east2-a', clusterRegion: 'asia-east2' }
         case 'ASIA-NORTHEAST1':
-          return { flag: '🇯🇵', regionDescription: `${locationType}: ${location} (Tokyo)` }
+          return { flag: '🇯🇵', regionDescription: `${locationType}: ${location} (Tokyo)`, computeZone: 'asia-northeast1-a', clusterRegion: 'asia-northeast1' }
         case 'ASIA-NORTHEAST2':
-          return { flag: '🇯🇵', regionDescription: `${locationType}: ${location} (Osaka)` }
+          return { flag: '🇯🇵', regionDescription: `${locationType}: ${location} (Osaka)`, computeZone: 'asia-northeast2-a', clusterRegion: 'asia-northeast2' }
         case 'ASIA-NORTHEAST3':
-          return { flag: '🇰🇷', regionDescription: `${locationType}: ${location} (Seoul)` }
+          return { flag: '🇰🇷', regionDescription: `${locationType}: ${location} (Seoul)`, computeZone: 'asia-northeast3-a', clusterRegion: 'asia-northeast3' }
         case 'ASIA-SOUTH1':
-          return { flag: '🇮🇳', regionDescription: `${locationType}: ${location} (Mumbai)` }
+          return { flag: '🇮🇳', regionDescription: `${locationType}: ${location} (Mumbai)`, computeZone: 'asia-south1-a', clusterRegion: 'asia-south1' }
         case 'ASIA-SOUTHEAST1':
-          return { flag: '🇸🇬', regionDescription: `${locationType}: ${location} (Singapore)` }
+          return { flag: '🇸🇬', regionDescription: `${locationType}: ${location} (Singapore)`, computeZone: 'asia-southeast1-a', clusterRegion: 'asia-southeast1' }
         case 'ASIA-SOUTHEAST2':
-          return { flag: '🇮🇩', regionDescription: `${locationType}: ${location} (Jakarta)` }
+          return { flag: '🇮🇩', regionDescription: `${locationType}: ${location} (Jakarta)`, computeZone: 'asia-southeast2-a', clusterRegion: 'asia-southeast2' }
         case 'AUSTRALIA-SOUTHEAST1':
-          return { flag: '🇦🇺', regionDescription: `${locationType}: ${location} (Sydney)` }
+          return { flag: '🇦🇺', regionDescription: `${locationType}: ${location} (Sydney)`, computeZone: 'australia-southeast1-a', clusterRegion: 'australia-southeast1' }
         case 'EUROPE-NORTH1':
-          return { flag: '🇫🇮', regionDescription: `${locationType}: ${location} (Finland)` }
+          return { flag: '🇫🇮', regionDescription: `${locationType}: ${location} (Finland)`, computeZone: 'europe-north1-a', clusterRegion: 'europe-north1' }
         case 'EUROPE-WEST1':
-          return { flag: '🇧🇪', regionDescription: `${locationType}: ${location} (Belgium)` }
+          return { flag: '🇧🇪', regionDescription: `${locationType}: ${location} (Belgium)`, computeZone: 'europe-west1-b', clusterRegion: 'europe-west1' }
         case 'EUROPE-WEST2':
-          return { flag: '🇬🇧', regionDescription: `${locationType}: ${location} (London)` }
+          return { flag: '🇬🇧', regionDescription: `${locationType}: ${location} (London)`, computeZone: 'europe-west2-a', clusterRegion: 'europe-west2' }
         case 'EUROPE-WEST3':
-          return { flag: '🇩🇪', regionDescription: `${locationType}: ${location} (Frankfurt)` }
+          return { flag: '🇩🇪', regionDescription: `${locationType}: ${location} (Frankfurt)`, computeZone: 'europe-west3-a', clusterRegion: 'europe-west3' }
         case 'EUROPE-WEST4':
-          return { flag: '🇳🇱', regionDescription: `${locationType}: ${location} (Netherlands)` }
+          return { flag: '🇳🇱', regionDescription: `${locationType}: ${location} (Netherlands)`, computeZone: 'europe-west4-a', clusterRegion: 'europe-west4' }
         case 'EUROPE-WEST6':
-          return { flag: '🇨🇭', regionDescription: `${locationType}: ${location} (Zurich)` }
+          return { flag: '🇨🇭', regionDescription: `${locationType}: ${location} (Zurich)`, computeZone: 'europe-west6-a', clusterRegion: 'europe-west6' }
         case 'NORTHAMERICA-NORTHEAST1':
-          return { flag: '🇨🇦', regionDescription: `${locationType}: ${location} (Montreal)` }
+          return { flag: '🇨🇦', regionDescription: `${locationType}: ${location} (Montreal)`, computeZone: 'northamerica-northeast1-a', clusterRegion: 'northamerica-northeast1' }
         case 'SOUTHAMERICA-EAST1':
-          return { flag: '🇧🇷', regionDescription: `${locationType}: ${location} (Sao Paulo)` }
+          return { flag: '🇧🇷', regionDescription: `${locationType}: ${location} (Sao Paulo)`, computeZone: 'southamerica-east1-a', clusterRegion: 'southamerica-east1' }
         case 'US-CENTRAL1':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Iowa)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Iowa)`, computeZone: 'us-central1-a', clusterRegion: 'us-central1' }
         case 'US-EAST1':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (South Carolina)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (South Carolina)`, computeZone: 'us-east1-b', clusterRegion: 'us-east1' }
         case 'US-EAST4':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Northern Virginia)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Northern Virginia)`, computeZone: 'us-east4-a', clusterRegion: 'us-east4' }
         case 'US-WEST1':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Oregon)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Oregon)`, computeZone: 'us-west1-a', clusterRegion: 'us-west1' }
         case 'US-WEST2':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Los Angeles)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Los Angeles)`, computeZone: 'us-west2-a', clusterRegion: 'us-west2' }
         case 'US-WEST3':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Salt Lake City)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Salt Lake City)`, computeZone: 'us-west3-a', clusterRegion: 'us-west3' }
         case 'US-WEST4':
-          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Las Vegas)` }
+          return { flag: '🇺🇸', regionDescription: `${locationType}: ${location} (Las Vegas)`, computeZone: 'us-west4-a', clusterRegion: 'us-west4' }
         default:
-          return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}` }
+          return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}`, computeZone: 'UNKNOWN', clusterRegion: 'UNKNOWN' }
       }
     default:
-      return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}` }
+      return { flag: unknownRegionFlag, regionDescription: `${locationType}: ${location}`, computeZone: 'UNKNOWN', clusterRegion: 'UNKNOWN' }
   }
 }
