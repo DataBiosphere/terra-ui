21fc46b7453619f782cfcdfd0719a85be2b2daa7
Justin Canas <justincanas16@gmail.com>
2021-05-07 09:31:53 -0400

    [IA-2645] Delete apps when a workspace is deleted (#2446)
    * delete apps on workspace deletion
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- src/pages/workspaces/workspace/DeleteWorkspaceModal.js
+++ src/pages/workspaces/workspace/DeleteWorkspaceModal.js
@@ -1,23 +1,65 @@
 import _ from 'lodash/fp'
 import { useState } from 'react'
-import { div, h, label, span } from 'react-hyperscript-helpers'
+import { div, h, label, p, span } from 'react-hyperscript-helpers'
 import { ButtonPrimary, Link, spinnerOverlay } from 'src/components/common'
+import { warningBoxStyle } from 'src/components/data/data-utils'
+import { icon } from 'src/components/icons'
 import { TextInput } from 'src/components/input'
 import Modal from 'src/components/Modal'
 import { Ajax } from 'src/libs/ajax'
-import { bucketBrowserUrl } from 'src/libs/auth'
+import { bucketBrowserUrl, getUser } from 'src/libs/auth'
+import colors from 'src/libs/colors'
 import { reportError } from 'src/libs/error'
+import { isAppDeletable } from 'src/libs/runtime-utils'
 import * as Utils from 'src/libs/utils'
 
 
 const DeleteWorkspaceModal = ({ workspace: { workspace: { namespace, name, bucketName } }, onDismiss, onSuccess }) => {
   const [deleting, setDeleting] = useState(false)
   const [deleteConfirmation, setDeleteConfirmation] = useState('')
+  const [loadingApps, setLoadingApps] = useState(true)
+  const [apps, setApps] = useState()
+
+  const signal = Utils.useCancellation()
+
+  Utils.useOnMount(() => {
+    const loadApps = async workspaceName => {
+      const [currentWorkspaceAppList] = await Promise.all([
+        Ajax(signal).Apps.listWithoutProject({ creator: getUser().email, saturnWorkspaceName: workspaceName })
+      ])
+      setApps(currentWorkspaceAppList)
+      setLoadingApps(false)
+    }
+    loadApps(name)
+  })
+
+  const [deletableApps, nonDeletableApps] = _.partition(isAppDeletable, apps)
+
+  const getAppDeletionMessage = () => {
+    return !_.isEmpty(nonDeletableApps) ?
+      div({ style: { ...warningBoxStyle, fontSize: 14, display: 'flex', flexDirection: 'column' } }, [
+        div({ style: { display: 'flex', flexDirection: 'row', alignItems: 'center' } }, [
+          icon('warning-standard', { size: 19, style: { color: colors.warning(), flex: 'none', marginRight: '0.5rem' } }),
+          'Undeletable Workspace Warning'
+        ]),
+        p({ style: { fontWeight: 'normal' } }, [`You cannot delete this workspace because there are ${nonDeletableApps.length} application(s) you must delete first. Only applications in ('ERROR', 'RUNNING') status can be automatically deleted.`])
+      ]) :
+      p({ style: { marginLeft: '1rem', fontWeight: 'bold' } }, [`Detected ${deletableApps.length} automatically deletable application(s).`])
+  }
+
+  const hasApps = () => {
+    return deletableApps !== undefined && nonDeletableApps !== undefined &&
+     (!_.isEmpty(deletableApps) ||
+      !_.isEmpty(nonDeletableApps))
+  }
 
   const deleteWorkspace = async () => {
     try {
       setDeleting(true)
       await Ajax().Workspaces.workspace(namespace, name).delete()
+      await Promise.all(
+        _.map(async app => await Ajax().Apps.app(app.googleProject, app.appName).delete(), deletableApps)
+      )
       onDismiss()
       onSuccess()
     } catch (error) {
@@ -26,12 +68,18 @@ const DeleteWorkspaceModal = ({ workspace: { workspace: { namespace, name, bucke
     }
   }
 
+  const isDeleteDisabledFromApps = hasApps() && !_.isEmpty(nonDeletableApps)
+
   return h(Modal, {
     title: 'Delete workspace',
     onDismiss,
     okButton: h(ButtonPrimary, {
-      disabled: _.toLower(deleteConfirmation) !== 'delete workspace',
-      onClick: () => deleteWorkspace()
+      disabled: _.toLower(deleteConfirmation) !== 'delete workspace' || isDeleteDisabledFromApps,
+      onClick: () => deleteWorkspace(),
+      tooltip: Utils.cond(
+        [_.toLower(deleteConfirmation) !== 'delete workspace', () => 'You must type the confirmation message'],
+        [isDeleteDisabledFromApps, () => 'You must ensure all apps in this workspace are deletable'],
+        () => 'Delete Workspace')
     }, 'Delete workspace')
   }, [
     div(['Are you sure you want to permanently delete the workspace ',
@@ -44,13 +92,18 @@ const DeleteWorkspaceModal = ({ workspace: { workspace: { namespace, name, bucke
         href: bucketBrowserUrl(bucketName)
       }, ['Google Cloud Bucket']),
       ' and all its data.'
-    ]), div({
+    ]),
+    hasApps() && div({ style: { marginTop: '1rem' } }, [
+      p(['Deleting it will also delete any associated applications:']),
+      getAppDeletionMessage()
+    ]),
+    !isDeleteDisabledFromApps && div({
       style: {
         fontWeight: 500,
         marginTop: '1rem'
       }
     }, 'This cannot be undone.'),
-    div({ style: { marginTop: '1rem' } }, [
+    !isDeleteDisabledFromApps && div({ style: { marginTop: '1rem' } }, [
       label({ htmlFor: 'delete-workspace-confirmation' }, ['Please type \'Delete Workspace\' to continue:']),
       h(TextInput, {
         id: 'delete-workspace-confirmation',
@@ -59,7 +112,7 @@ const DeleteWorkspaceModal = ({ workspace: { workspace: { namespace, name, bucke
         onChange: setDeleteConfirmation
       })
     ]),
-    deleting && spinnerOverlay
+    (deleting || loadingApps) && spinnerOverlay
   ])
 }
 
