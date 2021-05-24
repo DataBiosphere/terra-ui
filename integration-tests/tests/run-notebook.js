6dde7ba8d1866921efa658905e2709baeccdf297
Sky Rubenstein <blrubenstein@wpi.edu>
2021-05-24 13:57:47 -0400

    Sr v1 workspace integration tests (#2500)
    * Add integration tests to the UI for v1 workspaces
    
    * Update integration tests and add cleanup
    
    * PR Feedback
    
    * PR feedback
    
    * PR feedback, add comments

--- integration-tests/tests/run-notebook.js
+++ integration-tests/tests/run-notebook.js
@@ -1,15 +1,11 @@
 const _ = require('lodash/fp')
-const { withRegisteredUser, withBilling, withWorkspace } = require('../utils/integration-helpers')
+const { withRegisteredUser, withBilling, withWorkspace, withV1Workspace } = require('../utils/integration-helpers')
 const { click, clickable, delay, signIntoTerra, findElement, waitForNoSpinners, select, fillIn, input, findIframe, findText, dismissNotifications } = require('../utils/integration-utils')
 
 
 const notebookName = 'TestNotebook'
 
-const testRunNotebookFn = _.flow(
-  withWorkspace,
-  withBilling,
-  withRegisteredUser
-)(async ({ workspaceName, page, testUrl, token }) => {
+const testRunNotebookHelper = async (billingProject, workspaceName, page, testUrl, token, isWorkspaceSingleUse = true) => {
   await page.goto(testUrl)
   await click(page, clickable({ textContains: 'View Workspaces' }))
   await signIntoTerra(page, token)
@@ -17,42 +13,81 @@ const testRunNotebookFn = _.flow(
   await findElement(page, clickable({ textContains: workspaceName }))
   await waitForNoSpinners(page)
   await click(page, clickable({ textContains: workspaceName }))
-  await delay(5000)
+  await delay(10000)
   await click(page, clickable({ text: 'notebooks' }))
   await click(page, clickable({ textContains: 'Create a' }))
   await fillIn(page, input({ placeholder: 'Enter a name' }), notebookName)
   await select(page, 'Language', 'Python 2')
   await click(page, clickable({ text: 'Create Notebook' }))
   await click(page, clickable({ textContains: notebookName }))
-  await click(page, clickable({ text: 'Edit' }))
-  // There are two separate activities in the UI that could interfere with beginning to interact
-  // with the modal for creating a cloud environment:
-  //   1. AJAX calls to load cloud environment details and available docker images
-  //      - renders a spinner overlay on top of UI elements
-  //   2. The drawer slide-in animation
-  //      - causes UI elements to move before puppeteer clicks on them
-  // Experimentation has shown that there's enough of a gap between clicking 'Edit' and the AJAX
-  // spinner being rendered that simply waiting for no spinners does not work; the test recognizes
-  // that there aren't any spinners before the spinner has a chance to render. Additionally, even
-  // though the slide-in animation is supposedly only 200ms, experimentation has shown that even a
-  // 500ms delay in the test is not enough to guarantee that the UI elements have finished moving.
-  // Therefore, we start with a 1000ms delay, then make sure there are no spinners just in case the
-  // AJAX calls are unexpectedly slow.
-  await delay(1000)
-  waitForNoSpinners(page)
-  await click(page, clickable({ text: 'Create' }))
-  await findElement(page, clickable({ textContains: 'Creating' }))
-  await findElement(page, clickable({ textContains: 'Running' }), { timeout: 10 * 60 * 1000 })
+  // If we get this far we need to be sure to clean up the notebook in case the workspace is going to be re-used
+  try {
+    await click(page, clickable({ text: 'Edit' }))
+    // There are two separate activities in the UI that could interfere with beginning to interact
+    // with the modal for creating a cloud environment:
+    //   1. AJAX calls to load cloud environment details and available docker images
+    //      - renders a spinner overlay on top of UI elements
+    //   2. The drawer slide-in animation
+    //      - causes UI elements to move before puppeteer clicks on them
+    // Experimentation has shown that there's enough of a gap between clicking 'Edit' and the AJAX
+    // spinner being rendered that simply waiting for no spinners does not work; the test recognizes
+    // that there aren't any spinners before the spinner has a chance to render. Additionally, even
+    // though the slide-in animation is supposedly only 200ms, experimentation has shown that even a
+    // 500ms delay in the test is not enough to guarantee that the UI elements have finished moving.
+    // Therefore, we start with a 1000ms delay, then make sure there are no spinners just in case the
+    // AJAX calls are unexpectedly slow.
+    await delay(1000)
+    waitForNoSpinners(page)
+    await click(page, clickable({ text: 'Create' }))
+    await findElement(page, clickable({ textContains: 'Creating' }))
+    await findElement(page, clickable({ textContains: 'Running' }), { timeout: 10 * 60 * 1000 })
 
-  const frame = await findIframe(page)
-  await fillIn(frame, '//textarea', 'print(123456789099876543210990+9876543219)')
-  await click(frame, clickable({ text: 'Run' }))
-  await findText(frame, '123456789099886419754209')
+    const frame = await findIframe(page)
+    await fillIn(frame, '//textarea', 'print(123456789099876543210990+9876543219)')
+    await click(frame, clickable({ text: 'Run' }))
+    await findText(frame, '123456789099886419754209')
+  } finally {
+    if (!isWorkspaceSingleUse) {
+      await delay(1000)
+      const workspaceResponse = await page.evaluate((namespace, name) => {
+        return window.Ajax().Workspaces.workspace(namespace, name).details()
+      }, billingProject, workspaceName)
+      await page.evaluate((billingProject, bucketName, notebookName) => {
+        return window.Ajax().Buckets.notebook(billingProject, bucketName, notebookName).delete()
+      }, billingProject, workspaceResponse.workspace.bucketName, notebookName)
+    }
+  }
+}
+
+const testRunNotebookFn = _.flow(
+  withWorkspace,
+  withBilling,
+  withRegisteredUser
+)(async ({ billingProject, workspaceName, page, testUrl, token }) => {
+  await testRunNotebookHelper(billingProject, workspaceName, page, testUrl, token)
 })
+
+const testRunNotebookWithV1WorkspaceFn = _.flow(
+  withV1Workspace,
+  withBilling,
+  withRegisteredUser
+)(async ({ billingProject, v1WorkspaceName, page, testUrl, token }) => {
+  await testRunNotebookHelper(billingProject, v1WorkspaceName, page, testUrl, token, false)
+})
+
 const testRunNotebook = {
   name: 'run-notebook',
   fn: testRunNotebookFn,
   timeout: 20 * 60 * 1000
 }
 
-module.exports = { testRunNotebook }
+// We need this test for ensuring that we don't break the use of notebooks in V1 workspaces.
+// V1 workspaces will potentially exist in production until we run the PPW migration
+// in production
+const testRunNotebookWithV1Workspace = {
+  name: 'run-notebook-v1-workspace',
+  fn: testRunNotebookWithV1WorkspaceFn,
+  timeout: 20 * 60 * 1000
+}
+
+module.exports = { testRunNotebook, testRunNotebookWithV1Workspace }
--- integration-tests/tests/run-workflow.js
+++ integration-tests/tests/run-workflow.js
@@ -1,6 +1,6 @@
 const _ = require('lodash/fp')
 const pRetry = require('p-retry')
-const { withWorkspace, createEntityInWorkspace } = require('../utils/integration-helpers')
+const { withRegisteredUser, withWorkspace, withV1Workspace, createEntityInWorkspace } = require('../utils/integration-helpers')
 const { click, clickable, dismissNotifications, findElement, fillIn, input, signIntoTerra, waitForNoSpinners, findInGrid, navChild, findInDataTableRow } = require('../utils/integration-utils')
 const { withUserToken } = require('../utils/terra-sa-utils')
 
@@ -8,15 +8,13 @@ const { withUserToken } = require('../utils/terra-sa-utils')
 const testEntity = { name: 'test_entity_1', entityType: 'test_entity', attributes: { input: 'foo' } }
 const findWorkflowButton = clickable({ textContains: 'Find a Workflow' })
 
-const testRunWorkflowFn = _.flow(
-  withWorkspace,
-  withUserToken
-)(async ({ billingProject, page, testUrl, token, workflowName, workspaceName }) => {
+const testRunWorkflowHelper = async (billingProject, page, testUrl, token, workflowName, workspaceName, isWorkspaceSingleUse = true) => {
   await page.goto(testUrl)
   await signIntoTerra(page, token)
   await dismissNotifications(page)
-
-  await createEntityInWorkspace(page, billingProject, workspaceName, testEntity)
+  if (isWorkspaceSingleUse) {
+    await createEntityInWorkspace(page, billingProject, workspaceName, testEntity)
+  }
 
   await click(page, clickable({ textContains: 'View Workspaces' }))
   await waitForNoSpinners(page)
@@ -29,33 +27,60 @@ const testRunWorkflowFn = _.flow(
   await click(page, findWorkflowButton)
   await click(page, clickable({ textContains: workflowName }))
   await waitForNoSpinners(page)
-  await click(page, clickable({ text: 'Add to Workspace' }))
   // note that this automatically brings in the highest numbered config, which isn't what happens when going through the method repo in FC
+  await click(page, clickable({ text: 'Add to Workspace' }))
+  // If we get this far, we need to ensure that we cleanup in case this workspace is going to be reused, thus the try, finally.
+  try {
+    await waitForNoSpinners(page)
+    await click(page, clickable({ text: 'Select Data' }))
+    await click(page, input({ labelContains: 'Choose specific test_entitys to process' }))
+    await click(page, `//*[@role="checkbox" and contains(@aria-label, "${testEntity.name}")]`)
+    await click(page, clickable({ text: 'OK' }))
+    await click(page, clickable({ text: 'Run analysis' }))
 
-  await waitForNoSpinners(page)
-  await click(page, clickable({ text: 'Select Data' }))
-  await click(page, input({ labelContains: 'Choose specific test_entitys to process' }))
-  await click(page, `//*[@role="checkbox" and contains(@aria-label, "${testEntity.name}")]`)
-  await click(page, clickable({ text: 'OK' }))
-  await click(page, clickable({ text: 'Run analysis' }))
+    await Promise.all([
+      page.waitForNavigation(),
+      click(page, clickable({ text: 'Launch' }))
+    ])
 
-  await Promise.all([
-    page.waitForNavigation(),
-    click(page, clickable({ text: 'Launch' }))
-  ])
+    await pRetry(async () => {
+      try {
+        await findInGrid(page, 'Succeeded', { timeout: 65 * 1000 }) // long enough for the submission details to refresh
+      } catch (e) {
+        throw new Error(e)
+      }
+    }, { retries: 10, factor: 1 })
 
-  await pRetry(async () => {
-    try {
-      await findInGrid(page, 'Succeeded', { timeout: 65 * 1000 }) // long enough for the submission details to refresh
-    } catch (e) {
-      throw new Error(e)
+    await click(page, navChild('data'))
+    await click(page, clickable({ textContains: 'test_entity' }))
+    await findInDataTableRow(page, testEntity.name, testEntity.attributes.input)
+  } finally {
+    if (!isWorkspaceSingleUse) {
+      const methodConfigs = await page.evaluate((namespace, name) => {
+        return window.Ajax().Workspaces.workspace(namespace, name).listMethodConfigs()
+      }, billingProject, workspaceName)
+      const methodConfigToDelete = _.find({ methodRepoMethod: { methodName: workflowName } }, methodConfigs)
+      await page.evaluate((workspaceNamespace, workspaceName, methodNamespace, methodName) => {
+        return window.Ajax().Workspaces.workspace(workspaceNamespace, workspaceName).methodConfig(methodNamespace, methodName).delete()
+      }, billingProject, workspaceName, methodConfigToDelete.namespace, methodConfigToDelete.name)
     }
-  }, { retries: 10, factor: 1 })
+  }
+}
 
-  await click(page, navChild('data'))
-  await click(page, clickable({ textContains: 'test_entity' }))
-  await findInDataTableRow(page, testEntity.name, testEntity.attributes.input)
+const testRunWorkflowFn = _.flow(
+  withWorkspace,
+  withUserToken
+)(async ({ billingProject, page, testUrl, token, workflowName, workspaceName }) => {
+  await testRunWorkflowHelper(billingProject, page, testUrl, token, workflowName, workspaceName)
 })
+
+const testRunWorkflowWithV1WorkspaceFn = _.flow(
+  withV1Workspace,
+  withRegisteredUser
+)(async ({ billingProject, page, testUrl, token, workflowName, v1WorkspaceName }) => {
+  await testRunWorkflowHelper(billingProject, page, testUrl, token, workflowName, v1WorkspaceName, false)
+})
+
 
 const testRunWorkflow = {
   name: 'run-workflow',
@@ -63,4 +88,13 @@ const testRunWorkflow = {
   timeout: 15 * 60 * 1000
 }
 
-module.exports = { testRunWorkflow }
+// We need this test for ensuring that we don't break the use of workflows in V1 workspaces.
+// V1 workspaces will potentially exist in production until we run the PPW migration
+// in production
+const testRunWorkflowWithV1Workspace = {
+  name: 'run-workflow-v1-workspace',
+  fn: testRunWorkflowWithV1WorkspaceFn,
+  timeout: 15 * 60 * 1000
+}
+
+module.exports = { testRunWorkflow, testRunWorkflowWithV1Workspace }
