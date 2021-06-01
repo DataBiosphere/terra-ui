1e769dccaa955cea0cbfbc1fe28271e97ce89d59
Saloni Shah <salonipshah11@gmail.com>
2021-06-01 11:05:39 -0400

    Set deletion delay to 3 years

--- src/pages/workspaces/workspace/jobHistory/SubmissionDetails.js
+++ src/pages/workspaces/workspace/jobHistory/SubmissionDetails.js
@@ -115,7 +115,7 @@ const SubmissionDetails = _.flow(
 
   // Note: This 'deleteLimitYears' value should reflect the current 'deletion-delay' value configured for PROD in firecloud-develop's
   // 'cromwell.conf.ctmpl' file:
-  const deletionDelayYears = 6
+  const deletionDelayYears = 3
   const deletionDelayString = `${deletionDelayYears} year${deletionDelayYears > 1 ? 's' : ''}`
   const isDeleted = statusLastChangedDate => differenceInDays(parseISO(statusLastChangedDate), Date.now()) > (deletionDelayYears * 365)
 
