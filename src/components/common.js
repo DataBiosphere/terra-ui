42dc898d8218c065f2bed9107b51b3c8872dad45
Sky Rubenstein <blrubenstein@wpi.edu>
2021-06-04 13:54:35 -0400

    Update src/components/common.js
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>

--- src/components/common.js
+++ src/components/common.js
@@ -360,7 +360,7 @@ export const PageBox = ({ children, variant, style = {}, ...props }) => {
     style: {
       margin: '1.5rem', padding: '1.5rem 1.5rem 0', minHeight: 125, flex: 'none', zIndex: 0,
       ...Utils.switchCase(variant,
-        [PageBoxVariants.LIGHT, () => { return { backgroundColor: colors.light(), margin: 0, padding: '3rem 3rem 1.5rem' } }],
+        [PageBoxVariants.LIGHT, () => ({ backgroundColor: colors.light(), margin: 0, padding: '3rem 3rem 1.5rem' })],
         [Utils.DEFAULT, () => {}]), ...style
     }
   }, props), [children])
