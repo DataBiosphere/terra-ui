2dbd96d00c09857dcff84bba4ea222e17100a87f
Brian Reilly <breilly@broadinstitute.org>
2021-07-14 12:52:00 -0400

    added fetch for showcase.json rough sketch

--- src/pages/library/Showcase.js
+++ src/pages/library/Showcase.js
@@ -1,12 +1,10 @@
 import _ from 'lodash/fp'
-import { Fragment, useState } from 'react'
-import { UnmountClosed as RCollapse } from 'react-collapse'
-import { a, div, h, label } from 'react-hyperscript-helpers'
-import { Clickable, IdContainer, Link, Select } from 'src/components/common'
+import { useState } from 'react'
+import { a, div, h } from 'react-hyperscript-helpers'
 import FooterWrapper from 'src/components/FooterWrapper'
-import { centeredSpinner, icon } from 'src/components/icons'
-import { DelayedSearchInput } from 'src/components/input'
+import { centeredSpinner } from 'src/components/icons'
 import { libraryTopMatter } from 'src/components/library-common'
+import { useWorkspaces } from 'src/components/workspace-utils'
 import covidBg from 'src/images/library/showcase/covid-19.jpg'
 import featuredBg from 'src/images/library/showcase/featured-workspace.svg'
 import gatkLogo from 'src/images/library/showcase/gatk-logo-light.svg'
@@ -18,86 +16,17 @@ import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
 
 
-// Description of the structure of the sidebar. Case is preserved when rendering but all matching is case-insensitive.
-const sidebarSections = [{
-  name: 'Getting Started',
-  labels: ['Workflow Tutorials', 'Notebook Tutorials', 'Data Tutorials', 'RStudio Tutorials', 'Galaxy Tutorials'],
-  keepCollapsed: true
-}, {
-  name: 'Analysis Tools',
-  labels: ['WDLs', 'Jupyter Notebooks', 'RStudio', 'Galaxy', 'Hail', 'Bioconductor', 'GATK', 'Cumulus', 'Spark']
-}, {
-  name: 'Experimental Strategy',
-  labels: ['GWAS', 'Exome Analysis', 'Whole Genome Analysis', 'Fusion Transcript Detection', 'RNA Analysis', 'Machine Learning',
-    'Variant Discovery', 'Epigenomics', 'DNA Methylation', 'Copy Number Variation', 'Structural Variation', 'Functional Annotation']
-}, {
-  name: 'Data Generation Technology',
-  labels: ['10x analysis', 'Bisulfate Sequencing']
-}, {
-  name: 'Scientific Domain',
-  labels: ['Cancer', 'Infectious Diseases', 'MPG', 'Single-cell', 'Immunology']
-}, {
-  name: 'Datasets',
-  labels: ['AnVIL', 'CMG', 'CCDG', 'TopMed', 'HCA', 'TARGET', 'ENCODE', 'BioData Catalyst', 'TCGA', '1000 Genomes', 'BRAIN Initiative',
-    'gnomAD', 'NCI', 'COVID-19']
-}, {
-  name: 'Utilities',
-  labels: ['Format Conversion', 'Developer Tools']
-}, {
-  name: 'Projects',
-  labels: ['HCA', 'AnVIL', 'BRAIN Initiative', 'BioData Catalyst', 'NCI']
-}]
-
-const uniqueSidebarTags = _.flow(
-  _.flatMap(s => _.map(_.toLower, s.labels)),
-  _.uniq
-)(sidebarSections)
-
 const styles = {
   column: { marginRight: '1.5rem', flex: '1 1 0px', maxWidth: 415 },
   header: {
-    fontSize: 19, color: colors.dark(), fontWeight: 'bold',
+    fontSize: 22, color: colors.dark(), fontWeight: 500,
     marginBottom: '1rem'
-  },
-  sidebarRow: {
-    display: 'flex', justifyContent: 'space-between', alignItems: 'baseline'
-  },
-  nav: {
-    background: {
-      position: 'absolute', left: 0, right: 0, top: 0, bottom: 0,
-      overflow: 'auto', cursor: 'pointer'
-    },
-    container: state => ({
-      ...(state === 'entered' ? {} : { opacity: 0, transform: 'translate(-2rem)' }),
-      transition: 'opacity 0.2s ease-out, transform 0.2s ease-out',
-      display: 'flex', flexDirection: 'column'
-    }),
-    navSection: {
-      alignItems: 'center', flex: 'none', padding: '1.2rem 0', fontWeight: 700,
-      borderTop: `1px solid ${colors.dark(0.35)}`
-    }
-  },
-  pill: {
-    width: '4.5rem', padding: '0.25rem', fontWeight: 500, textAlign: 'center',
-    border: '1px solid', borderColor: colors.dark(0.25), borderRadius: '1rem',
-    backgroundColor: 'white'
-  },
-  pillHighlight: {
-    color: 'white', backgroundColor: colors.primary(1), borderColor: colors.primary(1)
   }
 }
 
-const NavItem = ({ children, ...props }) => {
-  return h(Clickable, _.merge({
-    style: { display: 'flex', alignItems: 'center', outlineOffset: -4 }
-  }, props), [children])
-}
-
-const makeCard = variant => workspace => {
-  const { namespace, name, created, description } = workspace
+const makeCard = variant => ({ workspace: { namespace, name, attributes: { description } } }) => {
   return a({
     href: Nav.getLink('workspace-dashboard', { namespace, name }),
-    key: `${namespace}:${name}`,
     style: {
       backgroundColor: 'white',
       height: 175,
@@ -107,9 +36,13 @@ const makeCard = variant => workspace => {
       boxShadow: Style.standardShadow
     }
   }, [
+    div({ style: { flex: 1, minWidth: 0, padding: '15px 20px' } }, [
+      div({ style: { color: colors.accent(), fontSize: 16, lineHeight: '20px', height: 40, marginBottom: 7 } }, [name]),
+      div({ style: { lineHeight: '20px', height: 100, whiteSpace: 'pre-wrap', overflow: 'hidden' } }, [description])
+    ]),
     div({
       style: {
-        backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'auto 100%', borderRadius: '5px 0 0 5px',
+        backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'auto 100%', borderRadius: '0 5px 5px 0',
         width: 87,
         ...Utils.cond(
           [name.toLowerCase().includes('covid'), () => ({ backgroundImage: `url(${covidBg})` })],
@@ -117,142 +50,33 @@ const makeCard = variant => workspace => {
           () => ({ backgroundImage: `url(${featuredBg})`, opacity: 0.75 })
         )
       }
-    }),
-    div({ style: { flex: 1, minWidth: 0, padding: '15px 20px', overflow: 'hidden' } }, [
-      div({ style: { display: 'flex' } }, [
-        div({ style: { flex: 1, color: colors.accent(), fontSize: 16, lineHeight: '20px', height: 40, marginBottom: 7 } }, [name]),
-        created && div([Utils.makeStandardDate(created)])
-      ]),
-      div({ style: { lineHeight: '20px', height: 100, whiteSpace: 'pre-wrap', overflow: 'hidden' } }, [description])
-      // h(MarkdownViewer, [description]) // TODO: should we render this as markdown?
-    ])
+    })
   ])
 }
 
-// collapsible section for sidebar categories
-const SidebarCollapser = ({ title, isOpened, onClick, children }) => {
-  return div({
-    role: 'group'
-  }, [
-    h(NavItem, {
-      onClick,
-      style: { ...styles.sidebarRow, ...styles.nav.navSection }
-    }, [
-      title,
-      div({ style: { flexGrow: 1 } }),
-      icon(isOpened ? 'angle-up' : 'angle-down', { size: 18, style: { flex: 'none' } })
-    ]),
-    div({
-      style: { flex: 'none' }
-    }, [h(RCollapse, { isOpened }, [children])])
-  ])
-}
-
-const groupByFeaturedTags = _.memoize(workspaces => _.flow(
-  _.map(tag => [tag, _.filter(w => _.includes(tag, w.tags.items), workspaces)]),
-  _.fromPairs
-)(uniqueSidebarTags))
-
-const Pill = ({ count, highlight }) => {
-  return div({
-    style: _.merge(styles.pill, highlight ? styles.pillHighlight : {})
-  }, [count])
-}
-
-const Sidebar = ({ onSectionFilter, onTagFilter, sections, selectedSections, selectedTags, workspacesByTag }) => {
-  const [collapsedSections, setCollapsedSections] = useState([])
-
-  const unionSectionWorkspaces = section => {
-    return _.uniq(_.flatMap(tag => workspacesByTag[tag], section.tags))
+// match tags case-insensitively
+const sidebarSections = [
+  {
+    name: 'Getting Started',
+    labels: ['terra tutorials']
   }
+]
 
-  return div({ style: { display: 'flex', flexDirection: 'column' } }, [
-    _.map(section => {
-      return section.keepCollapsed ?
-        h(Clickable, {
-          key: section.name,
-          onClick: () => onSectionFilter(section),
-          style: { ...styles.sidebarRow, ...styles.nav.navSection }
-        }, [
-          div({ style: { flex: 1 } }, [section.name]),
-          h(Pill, {
-            count: _.size(unionSectionWorkspaces(section)),
-            highlight: _.includes(section, selectedSections)
-          })
-        ]) :
-        h(SidebarCollapser, {
-          key: section.name,
-          title: section.name,
-          onClick: () => setCollapsedSections(_.xor([section], collapsedSections)),
-          isOpened: !_.includes(section, collapsedSections)
-        }, [_.map(label => {
-          const tag = _.toLower(label)
-          return h(Clickable, {
-            key: label,
-            style: { display: 'flex', alignItems: 'baseline', margin: '0.5rem 0' },
-            onClick: () => onTagFilter(tag)
-          }, [
-            div({ style: { flex: 1 } }, [label]),
-            h(Pill, {
-              count: _.size(workspacesByTag[tag]),
-              highlight: _.includes(tag, selectedTags)
-            })
-          ])
-        }, section.labels)])
-    }, sections)
-  ])
+const Sidebar = () => {
+  return div('sidebar goes here')
 }
 
 const Showcase = () => {
+  const { workspaces } = useWorkspaces()
   const stateHistory = StateHistory.get()
   const [featuredList, setFeaturedList] = useState(stateHistory.featuredList)
-  const [workspacesByTag, setWorkspacesByTag] = useState({})
-  const [sections, setSections] = useState([])
-
-  const [selectedSections, setSelectedSections] = useState([])
-  const [selectedTags, setSelectedTags] = useState([])
-  const [searchFilter, setSearchFilter] = useState()
-  const [sort, setSort] = useState('most recent')
-
-  const sortWorkspaces = Utils.switchCase(sort,
-    ['most recent', () => _.orderBy(['created'], ['desc'])],
-    ['alphabetical', () => _.orderBy(w => _.toLower(_.trim(w.name)), ['asc'])]
-  )
 
   Utils.useOnMount(() => {
     const loadData = async () => {
-      const showcase = await Ajax().Buckets.getShowcaseWorkspaces()
+      const featuredList = await Ajax().Buckets.getFeaturedWorkspaces()
 
-      // Immediately lowercase the workspace tags so we don't have to think about it again.
-      // Also pre-compute lower name and description.
-      const featuredWorkspaces = _.map(workspace => ({
-        ...workspace,
-        tags: _.update(['items'], _.map(_.toLower), workspace.tags),
-        lowerName: _.toLower(workspace.name), lowerDescription: _.toLower(workspace.description)
-      }), showcase)
-
-      const workspacesByTag = _.omitBy(_.isEmpty, groupByFeaturedTags(featuredWorkspaces))
-
-      // Trim items from the sidebar for which there aren't any featured workspaces.
-      const activeTags = _.keys(workspacesByTag)
-      const activeSections = _.flow(
-        _.map(section => {
-          const activeLabels = _.intersectionBy(_.toLower, section.labels, activeTags)
-          return {
-            ...section,
-            labels: activeLabels,
-            tags: _.map(_.toLower, activeLabels)
-          }
-        }),
-        _.remove(section => _.isEmpty(section.labels))
-      )(sidebarSections)
-
-      setFeaturedList(featuredWorkspaces)
-      setWorkspacesByTag(workspacesByTag)
-      setSections(activeSections)
-
-      // TODO: is this actually doing anything for us?
-      StateHistory.update({ featuredWorkspaces })
+      setFeaturedList(featuredList)
+      StateHistory.update({ featuredList })
     }
 
     loadData()
@@ -269,7 +93,8 @@ const Showcase = () => {
       centeredSpinner() :
       div({ style: { display: 'flex', margin: '1rem 1rem 0' } }, [
         div({ sytle: { width: 300 } }, [
-          div({ style: styles.header }, 'Featured workspaces')
+          div({ style: styles.header }, 'Featured workspaces'),
+          h(Sidebar)
         ]),
         div({ style: { flex: 1 } }, [
           ..._.map(makeCard(), allFeatured)
@@ -278,12 +103,12 @@ const Showcase = () => {
   ])
 }
 
+
 export const navPaths = [
   {
     name: 'library-showcase',
     path: '/library/showcase',
     component: Showcase,
-    title: 'Featured Workspaces',
-    public: true
+    title: 'Showcase & Tutorials'
   }
 ]
