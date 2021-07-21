194d376b7db3deff4d2d21cadac2d5a0b4affec6
xuscrbw <xuw@broadinstitute.org>
2021-07-21 15:11:25 -0400

    adding fixes for bugs

--- integration-tests/tests/run-notebook.js
+++ integration-tests/tests/run-notebook.js
@@ -22,7 +22,7 @@ const testRunNotebookFn = _.flow(
   await waitForNoSpinners(page)
   await click(page, clickable({ textContains: 'Create a' }))
   await fillIn(page, input({ placeholder: 'Enter a name' }), notebookName)
-  await select(page, 'Language', 'Python 2')
+  await select(page, 'Language', 'Python 3')
   await click(page, clickable({ text: 'Create Notebook' }))
   await click(page, clickable({ textContains: notebookName }))
   await click(page, clickable({ text: 'Edit' }))
@@ -46,6 +46,9 @@ const testRunNotebookFn = _.flow(
   await findElement(page, clickable({ textContains: 'Running' }), { timeout: 10 * 60 * 1000 })
 
   const frame = await findIframe(page)
+  await findElement(frame, '//*[@title="Kernel Idle"]')
+  //Delay here to allow runtime to finish creating.
+  await delay(10000)
   await fillIn(frame, '//textarea', 'print(123456789099876543210990+9876543219)')
   await click(frame, clickable({ text: 'Run' }))
   await findText(frame, '123456789099886419754209')
