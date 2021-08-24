d600011d24e6781f3ae2ed1af9177dfd0ff9560b
TC <tcjiang9@gmail.com>
2021-08-24 15:09:53 -0700

    Factor to components (#2588)
    * very rough layout update
    
    * Remove filtering into showcase columns
    
    * added DashboardPublic + added route to use DashboardPublic on no signedIn
    
    * finished basic format for dashboardPublic.js
    
    * added fetch for showcase.json
    rough sketch
    
    * Show all workspaces from json
    
    * rough side bar
    
    * improvements to card rendering and sizing
    
    * get count of labels
    
    * case-insensitive tag filtering
    
    * added search (still needs styling)
    
    * added showcase sort
    
    * showcase multi-tags
    
    * reworked the layout to make sure the title, search, and sort-by can all be baseline aligned
    
    * `function` to => function
    
    * updated categories and tags
    fixed a bug with multi-tag filter
    
    * added collapsing sections to showcase.js
    
    * pills
    
    * suppress labels with no matching workspaces
    
    * lots of style tweaks to get it much closer to the original designs
    
    * rework filtering to update counts as filters are applied
    
    * simplified tag toggling code
    
    * fixed sidebar initial open state
    fixed react warning
    
    * added special handling of Getting Started section
    
    * minor case tweaks
    
    * moved `sidebarSections` to the top of the file to make it easier to find and maintain
    
    * make text search case-insensitive
    
    * PR feedback
    
    * additional PR feedback from Mohs (and prior changes to SignInButton.js that I somehow missed committing before)
    
    * PR feedback from Isaac (and a bugfix on the 'clear' button)
    
    * another change inspired by Isaac's feedback
    
    * memoize groupByFeaturedTags
    
    * Factored everything but showcase component
    
    * sort of working
    
    * more refactors, run lint
    
    * clean up unused imports
    
    * fix tab switching, clean up imports
    
    * lint
    
    * incorporate pr feedback
    
    * lint
    
    * remove unused import
    
    * fix bug with StateHistory
    
    Co-authored-by: Brian Reilly <breilly@broadinstitute.org>
    Co-authored-by: Justin H Yang <juyang@broadinstitute.org>

--- src/pages/library/DataBrowser.js
+++ src/pages/library/DataBrowser.js
@@ -1,12 +1,29 @@
-import { h } from 'react-hyperscript-helpers'
-import FooterWrapper from 'src/components/FooterWrapper'
-import { libraryTopMatter } from 'src/components/library-common'
+import { SearchAndFilterComponent } from 'src/pages/library/common'
+
+
+// Description of the structure of the sidebar. Case is preserved when rendering but all matching is case-insensitive.
+const sidebarSections = [{
+  name: 'Test',
+  labels: ['test1', 'test2']
+}]
 
 
 const DataBrowser = () => {
-  return h(FooterWrapper, { alwaysShow: true }, [
-    libraryTopMatter('browse & explore')
-  ])
+  const featuredList = [{
+    namespace: 'test-test',
+    name: 'test1',
+    created: '2020-01-13T18:25:28.340Z',
+    tags: {
+      itemsType: 'AttributeValue',
+      items: ['test1']
+    },
+    description: 'test desc',
+    lowerName: 'test1',
+    lowerDescription: 'test desc',
+    keepCollapsed: true
+  }]
+
+  return SearchAndFilterComponent(featuredList, sidebarSections, 'browse & explore')
 }
 
 export const navPaths = [{
