54c11e737af9da718686c2cf27bba61ec19383f7
petesantos <psantos@broadinstitute.org>
2021-08-24 17:35:32 -0400

    formatting

--- src/pages/CodeBlock.js
+++ src/pages/CodeBlock.js
@@ -10,12 +10,24 @@ import * as Utils from 'src/libs/utils'
 
 const { div, h, code, pre } = hyperscript
 
+const ConsoleArea = Utils.forwardRefWithName('ConsoleArea', ({ style }, ref) => {
+  return div({ ref, style: { border: '1px solid black', padding: '1rem', height: '3rem', ...style } })
+})
+
 export const Playground = Utils.forwardRefWithName('Playground', ({ scopes, children }, ref) => {
+  const elem = useRef()
+  const overrides = {
+    console: { log: (...args) => elem.current.textContent = args }
+  }
+
   return div({ className: 'language-js' }, [
-    h(LiveProvider, { noInline: false, theme, scope: { ...scopes, ...hyperscript, ...react }, code: children }, [
-      h(LiveEditor),
-      h(LivePreview),
-      h(LiveError)
+    h(LiveProvider, { noInline: false, theme, scope: { ...overrides, ...scopes, ...hyperscript, ...react }, code: children }, [
+      div({ style: { display: 'grid', gridTemplateColumns: '1fr', gap: '1rem' } }, [
+        h(LiveEditor),
+        h(LivePreview),
+        h(ConsoleArea, { ref: elem }),
+        h(LiveError)
+      ])
     ])
   ])
 })
@@ -26,8 +38,10 @@ export const CodeBlock = ({ children, live = false, style, scopes }) => {
     elem?.current && Prism.highlightAll(elem.current)
   }, [elem])
 
-  return live ? h(Playground, { scopes }, children) :
-    pre({ className: 'line-numbers', style: { ...style, border: '1px solid black' } }, [
-      code({ ref: elem, className: 'language-js' }, children)
-    ])
+  return div({ style: { width: '90%' } }, [
+    live ? h(Playground, { scopes }, children) :
+      pre({ className: 'line-numbers', style: { ...style, border: '1px solid black' } }, [
+        code({ ref: elem, className: 'language-js' }, children)
+      ])
+  ])
 }
