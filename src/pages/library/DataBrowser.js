ec6df4481a344e9ae3e94f4828137ced7aca94e5
clu@asymmetrik.com <clu@asymmetrik.com>
2021-08-24 21:43:28 -0400

    [DR-1986] Genericized SearchAndFilterComponent, and added the dataset browser table view according to the mock up. Still missing functionality for sorting and expansion

--- src/pages/library/DataBrowser.js
+++ src/pages/library/DataBrowser.js
@@ -9,21 +9,52 @@ const sidebarSections = [{
 
 
 const DataBrowser = () => {
-  const featuredList = [{
-    namespace: 'test-test',
-    name: 'test1',
-    created: '2020-01-13T18:25:28.340Z',
-    tags: {
-      itemsType: 'AttributeValue',
-      items: ['test1']
+  const featuredList = [
+    {
+      namespace: 'test-test',
+      name: 'this is a really really long name that should wrap onto another line',
+      created: '2020-01-13T18:25:28.340Z',
+      lastUpdated: '2020-01-13T18:25:28.340Z',
+      tags: {
+        itemsType: 'AttributeValue',
+        items: ['test1']
+      },
+      description: 'test desc',
+      lowerName: 'test1',
+      lowerDescription: 'test desc',
+      project: {
+        id: '112',
+        name: 'NIH'
+      },
+      subjects: 123,
+      dataType: 'RNA Seq',
+      keepCollapsed: true,
+      locked: true
     },
-    description: 'test desc',
-    lowerName: 'test1',
-    lowerDescription: 'test desc',
-    keepCollapsed: true
-  }]
+    {
+      namespace: 'harry-potter',
+      name: 'Harry Potter',
+      created: '2020-01-13T18:25:28.340Z',
+      lastUpdated: '2020-01-13T18:25:28.340Z',
+      tags: {
+        itemsType: 'AttributeValue',
+        items: ['wizarding']
+      },
+      description: 'The boy who lived',
+      lowerName: 'harry potter',
+      lowerDescription: 'lower description',
+      project: {
+        id: '112',
+        name: 'NIH'
+      },
+      subjects: 10,
+      dataType: 'RNA Seq',
+      keepCollapsed: true,
+      locked: false
+    }
+  ]
 
-  return SearchAndFilterComponent(featuredList, sidebarSections, 'browse & explore')
+  return SearchAndFilterComponent(featuredList, sidebarSections, 'browse & explore', 'datasets')
 }
 
 export const navPaths = [{
