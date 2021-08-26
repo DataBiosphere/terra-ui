d0f219cb55e372d5e73ecd8331378ca2d1be6ed6
clu@asymmetrik.com <clu@asymmetrik.com>
2021-08-25 20:53:32 -0400

    fixed lint

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -87,7 +87,7 @@ export const formatBytes = (bytes, decimals = 2) => {
 
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
 const dateFormat = new Intl.DateTimeFormat('default', { day: 'numeric', month: 'short', year: 'numeric' })
