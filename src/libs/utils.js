56c71fed400c1fa9b551d2d04d68e7ed19f48c9a
clu@asymmetrik.com <clu@asymmetrik.com>
2021-08-25 20:32:58 -0400

    [DR-1986] Added a "formatBytes" function to extract the proper filesize of selected datasets

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -76,6 +76,20 @@ export const connectStore = (theStore, name) => WrappedComponent => {
   })
 }
 
+export const formatBytes = (bytes, decimals = 2) => {
+  if (bytes === 0) {
+    return '0 Bytes'
+  }
+
+  const k = 1024
+  const dm = Math.floor(0, decimals)
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
 const dateFormat = new Intl.DateTimeFormat('default', { day: 'numeric', month: 'short', year: 'numeric' })
 const monthYearFormat = new Intl.DateTimeFormat('default', { month: 'short', year: 'numeric' })
 const completeDateFormat = new Intl.DateTimeFormat('default', { day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric' })
