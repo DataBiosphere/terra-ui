deee2019d83cf2ef85bc44da7f5499a79f1ab5d0
Edmund Higham <edhigham@gmail.com>
2021-10-12 15:29:44 -0400

    apply suggestions to billing/Project.js

--- src/pages/billing/Project.js
+++ src/pages/billing/Project.js
@@ -29,14 +29,14 @@ const workspaceLastModifiedWidth = 150
 const workspaceExpandIconSize = 20
 const billingAccountIconSize = 16
 
-const BillingAccountIcon = {
+const billingAccountIcon = {
   updating: { shape: 'sync', color: colors.warning() },
   done: { shape: 'check', color: colors.accent() },
   error: { shape: 'warning-standard', color: colors.danger() }
 }
 
 const getBillingAccountIcon = status => {
-  const { shape, color } = BillingAccountIcon[status]
+  const { shape, color } = billingAccountIcon[status]
   return icon(shape, { size: billingAccountIconSize, color })
 }
 
@@ -60,7 +60,7 @@ const WorkspaceCardHeaders = Utils.memoWithName('WorkspaceCardHeaders', ({ needs
   ])
 })
 
-const ExpandedInfoRow = Utils.memoWithName('ExpandedInfoRow', ({ title, details, errorMessage }) => {
+const ExpandedInfoRow = ({ title, details, errorMessage }) => {
   const expandedInfoStyles = {
     row: { display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' },
     title: { fontWeight: 600, width: '20%', padding: '0.5rem 1rem 0 2rem', height: '1rem' },
@@ -76,7 +76,7 @@ const ExpandedInfoRow = Utils.memoWithName('ExpandedInfoRow', ({ title, details,
     div({ style: expandedInfoStyles.details }, [details]),
     errorMessage && div({ style: expandedInfoStyles.errorMessage }, [errorMessage])
   ])
-})
+}
 
 const WorkspaceCard = Utils.memoWithName('WorkspaceCard', ({ workspace, billingAccountStatus, isExpanded, onExpand }) => {
   const { namespace, name, createdBy, lastModified, googleProject, billingAccountDisplayName, billingAccountErrorMessage } = workspace
@@ -137,7 +137,7 @@ const WorkspaceCard = Utils.memoWithName('WorkspaceCard', ({ workspace, billingA
   ])
 })
 
-const BillingAccountSummaryPanel = (() => {
+const BillingAccountSummaryPanel = ({ counts: { done, error, updating } }) => {
   const StatusAndCount = ({ status, count }) => div({ style: { display: 'float' } }, [
     div({ style: { float: 'left' } }, [getBillingAccountIcon(status)]),
     div({ style: { float: 'left', marginLeft: '0.5rem' } }, [`${status} (${count})`])
@@ -147,41 +147,39 @@ const BillingAccountSummaryPanel = (() => {
     h(StatusAndCount, { status, count })
   ])
 
-  return Utils.memoWithName(
-    'BillingAccountSummaryPanel',
-    ({ counts: { done, error, updating } }) => div({
-      style: {
-        padding: '0.5rem 2rem 1rem',
-        position: 'absolute',
-        top: topBarHeight,
-        right: '3rem',
-        width: '30rem',
-        backgroundColor: colors.light(0.5),
-        boxShadow: '0 2px 5px 0 rgba(0,0,0,0.25)'
-      }
-    }, [
-      div({ style: { padding: '1rem 0' } }, 'Your billing account is updating...'),
-      div({ style: { display: 'flex', justifyContent: 'flex-start' } }, [
-        maybeAddStatus('updating', updating),
-        maybeAddStatus('done', done),
-        maybeAddStatus('error', error)
-      ]),
-      error > 0 && div({ style: { padding: '1rem 0 0' } }, [
-        'Try again or ',
-        h(Link, { onClick: () => contactUsActive.set(true) }, [
-          'contact us regarding unresolved errors'
-        ]), '.'
-      ])
-    ]))
-})()
+  return div({
+    style: {
+      padding: '0.5rem 2rem 1rem',
+      position: 'absolute',
+      top: topBarHeight,
+      right: '3rem',
+      width: '30rem',
+      backgroundColor: colors.light(0.5),
+      boxShadow: '0 2px 5px 0 rgba(0,0,0,0.25)'
+    }
+  }, [
+    div({ style: { padding: '1rem 0' } }, 'Your billing account is updating...'),
+    div({ style: { display: 'flex', justifyContent: 'flex-start' } }, [
+      maybeAddStatus('updating', updating),
+      maybeAddStatus('done', done),
+      maybeAddStatus('error', error)
+    ]),
+    error > 0 && div({ style: { padding: '1rem 0 0' } }, [
+      'Try again or ',
+      h(Link, { onClick: () => contactUsActive.set(true) }, [
+        'contact us regarding unresolved errors'
+      ]), '.'
+    ])
+  ])
+}
 
 const groupByBillingAccountStatus = (billingProject, workspaces) => {
-  const group = workspace => Utils.cond(
-    [billingProject.billingAccount === workspace.billingAccount, () => 'done'],
-    ['billingAccountErrorMessage' in workspace, () => 'error'],
-    [Utils.DEFAULT, () => 'updating']
-  )
+  const group = workspace => billingProject.billingAccount === workspace.billingAccount ? 'done' :
+    !!workspace.billingAccountErrorMessage ? 'error' :
+      'updating'
 
+  // return `Set`s of workspaces mapped by their status to reduce the complexity of looking up a
+  // workspace's status.
   return _.mapValues(ws => new Set(ws), _.groupBy(group, workspaces))
 }
 
@@ -210,10 +208,10 @@ const ProjectDetail = ({ billingProject, reloadBillingProject, billingAccounts, 
 
   const adminCanEdit = _.filter(({ roles }) => _.includes(billingRoles.owner, roles), projectUsers).length > 1
 
-  const workspacesInProject = useMemo(() => _.flow(
-    _.map('workspace'),
-    _.filter({ namespace: billingProject.projectName })
-  )(workspaces), [billingProject, workspaces])
+  const workspacesInProject = useMemo(() => _.filter(
+    { namespace: billingProject.projectName },
+    _.map('workspace', workspaces)
+  ), [billingProject, workspaces])
 
   const groups = groupByBillingAccountStatus(billingProject, workspacesInProject)
   const billingAccountsOutOfDate = !(_.isEmpty(groups.error) && _.isEmpty(groups.updating))
