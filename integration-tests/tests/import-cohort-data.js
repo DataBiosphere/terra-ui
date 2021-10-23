6884bda5b449eff921610666fd34f3c385a81c28
Willy Nojopranoto <nojopranoto@google.com>
2021-10-23 18:49:26 +0000

    Have the tests wait a bit before clicking

--- integration-tests/tests/import-cohort-data.js
+++ integration-tests/tests/import-cohort-data.js
@@ -1,6 +1,6 @@
 const _ = require('lodash/fp')
 const { withWorkspace } = require('../utils/integration-helpers')
-const { findInGrid, click, clickable, dismissNotifications, fillIn, findIframe, input, signIntoTerra, select, svgText, waitForNoSpinners, findElement } = require('../utils/integration-utils')
+const { findInGrid, click, clickable, delay, dismissNotifications, fillIn, findIframe, input, signIntoTerra, select, svgText, waitForNoSpinners, findElement } = require('../utils/integration-utils')
 const { withUserToken } = require('../utils/terra-sa-utils')
 
 
@@ -27,6 +27,8 @@ const testImportCohortDataFn = _.flow(
   await click(page, clickable({ textContains: 'an existing workspace' }))
   await select(page, 'Select a workspace', workspaceName)
   await click(page, clickable({ text: 'Import' }))
+  // Wait a bit for the workspace to load
+  await delay(5000)
   await click(page, clickable({ textContains: 'cohort' }))
   await findInGrid(page, '1000 Genomes')
   await findInGrid(page, cohortName)
