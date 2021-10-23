8c03a4ccbbb65780f2403365086bace3cffb167f
Willy Nojopranoto <nojopranoto@google.com>
2021-10-23 18:55:00 +0000

    The preview-drs-uri test as well

--- integration-tests/tests/preview-drs-uri.js
+++ integration-tests/tests/preview-drs-uri.js
@@ -1,7 +1,7 @@
 const _ = require('lodash/fp')
 const { withWorkspace, createEntityInWorkspace } = require('../utils/integration-helpers')
 const { withUserToken } = require('../utils/terra-sa-utils')
-const { findText, navChild, fillIn, click, clickable, elementInDataTableRow, waitForNoSpinners, input, signIntoTerra, dismissNotifications } = require('../utils/integration-utils')
+const { findText, navChild, fillIn, click, clickable, delay, elementInDataTableRow, waitForNoSpinners, input, signIntoTerra, dismissNotifications } = require('../utils/integration-utils')
 
 
 const testPreviewDrsUriFn = _.flow(
@@ -28,6 +28,8 @@ const testPreviewDrsUriFn = _.flow(
   await click(page, clickable({ textContains: workspaceName }))
 
   await click(page, navChild('data'))
+  // Wait a bit for the workspace to load
+  await delay(5000)
   await click(page, clickable({ textContains: testEntity.entityType }))
   await click(page, elementInDataTableRow(testEntity.name, testEntity.attributes.file_uri))
   await waitForNoSpinners(page)
