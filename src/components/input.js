d770ab7007b211bfc28647cd51dbd69698d48ba2
clu@asymmetrik.com <clu@asymmetrik.com>
2021-11-04 18:01:49 -0400

    ??? Updating to hopefully address pr comments?

--- src/components/input.js
+++ src/components/input.js
@@ -241,10 +241,10 @@ const withAutocomplete = WrappedComponent => ({
     { selectedItem: value }
 
   const inputEl = useRef()
-  let clearFn
+  let clearSelectionRef
   Utils.useOnMount(() => {
     inputEl.current.addEventListener('search', () => {
-      clearFn?.()
+      clearSelectionRef?.()
     })
   })
 
@@ -261,7 +261,7 @@ const withAutocomplete = WrappedComponent => ({
     labelId
   }, [
     ({ getInputProps, getMenuProps, getItemProps, isOpen, openMenu, toggleMenu, clearSelection, highlightedIndex }) => {
-      clearFn = clearSelection
+      clearSelectionRef = clearSelection
       return div({
         onFocus: openOnFocus ? openMenu : undefined,
         style: { width: style?.width || '100%', display: 'inline-flex', position: 'relative', outline: 'none' }
