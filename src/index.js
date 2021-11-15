2216bc05fd8dc3bebfdbc54ff9446a40c5b68a4d
Isaac Zarsky <zarsky@broadinstitute.org>
2021-11-15 15:01:25 -0500

    fix for deeper route without hash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { loadedConfigStore } from 'src/configStore'
 
 
 const loadApp = async () => {
-  const res = await fetch('config.json')
+  const res = await fetch('/config.json')
   loadedConfigStore.current = await res.json()
 
   import('src/appLoader')
