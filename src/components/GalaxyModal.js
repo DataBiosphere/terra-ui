6848b9637825194e5d50b367e98f9e27b4008f44
Qi Wang <qi@broadinstitute.org>
2021-11-30 14:55:33 -0500

    bump galaxy version

--- src/components/GalaxyModal.js
+++ src/components/GalaxyModal.js
@@ -402,7 +402,7 @@ export const GalaxyModalBase = Utils.withDisplayName('GalaxyModal')(
         div({ style: { ...computeStyles.whiteBoxContainer, marginTop: '1rem' } }, [
           div([
             div({ style: computeStyles.headerText }, ['Application configuration']),
-            div({ style: { marginTop: '0.5rem' } }, ['Galaxy version 21.05']),
+            div({ style: { marginTop: '0.5rem' } }, ['Galaxy version 21.09']),
             h(Link, { href: 'https://support.terra.bio/hc/en-us/articles/360050566271', ...Utils.newTabLinkProps }, [
               'Learn more about Galaxy interactive environments',
               icon('pop-out', { size: 12, style: { marginTop: '1rem', marginLeft: '0.25rem' } })
