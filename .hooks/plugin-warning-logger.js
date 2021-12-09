e6792cd29f52a80167327714dcb61137390f3b14
Isaac Zarsky <zarsky@broadinstitute.org>
2021-12-09 15:03:30 -0500

    upgrade to node 16

--- .hooks/plugin-warning-logger.js
+++ .hooks/plugin-warning-logger.js
@@ -5,10 +5,10 @@ module.exports = {
       validateProject() {
         const nodeVersion = process.version
 
-        if (!nodeVersion.startsWith('v14.')) {
+        if (!nodeVersion.startsWith('v16.')) {
           console.error('\x1b[1m' /* bold */ + '╔'.padEnd(79, '═') + '╗')
-          console.error(`║ Must be running node 14, you have ${nodeVersion}. One way to fix (Mac/Homebrew):`.padEnd(79) + '║')
-          console.error('║ $ brew install node@14; brew link node@14 --force --overwrite'.padEnd(79) + '║')
+          console.error(`║ Must be running node 16, you have ${nodeVersion}. One way to fix (Mac/Homebrew):`.padEnd(79) + '║')
+          console.error('║ $ brew install node@16; brew link node@16 --force --overwrite'.padEnd(79) + '║')
           console.error('╚'.padEnd(79, '═') + '╝')
           console.error('\x1b[0m' /* not-bold */)
 
