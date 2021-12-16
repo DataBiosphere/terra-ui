abb60133a9fb304371e1b4f9e2297e780099629e
Kaan Yuksel <kyuksel@broadinstitute.org>
2021-12-16 09:09:29 -0500

    Lint LocalVariablesContent.js

--- src/_tests/components/data/LocalVariablesContent.test.js
+++ src/_tests/components/data/LocalVariablesContent.test.js
@@ -1,25 +1,26 @@
-import {getDisplayedAttribute, renameAttribute} from 'src/components/data/LocalVariablesContent'
+import { getDisplayedAttribute, renameAttribute } from 'src/components/data/LocalVariablesContent'
+
 
 describe('getDisplayedAttribute', () => {
   it('gets a displayed attribute with a description', () => {
     const input = [
-      {key: 'the_key', value: 'the_value'},
-      {key: 'the_key', description: 'the_description'}
+      { key: 'the_key', value: 'the_value' },
+      { key: 'the_key', description: 'the_description' }
     ]
     const expected = ['the_key', 'the_value', 'the_description']
     expect(getDisplayedAttribute(input)).toStrictEqual(expected)
   })
   it('gets a displayed attribute with an empty-string description', () => {
     const input = [
-      {key: 'the_key', value: 'the_value'},
-      {key: 'the_key', description: ''}
+      { key: 'the_key', value: 'the_value' },
+      { key: 'the_key', description: '' }
     ]
     const expected = ['the_key', 'the_value', '']
     expect(getDisplayedAttribute(input)).toStrictEqual(expected)
   })
   it('gets a displayed attribute with an undefined description', () => {
     const input = [
-      {key: 'the_key', value: 'the_value'},
+      { key: 'the_key', value: 'the_value' }
     ]
     const expected = ['the_key', 'the_value', '']
     expect(getDisplayedAttribute(input)).toStrictEqual(expected)
@@ -29,12 +30,12 @@ describe('getDisplayedAttribute', () => {
 describe('renameAttribute', () => {
   it('renames a value attribute', () => {
     const input = ['the_key', 'the_value']
-    const expected = {key: 'the_key', value: 'the_value'}
+    const expected = { key: 'the_key', value: 'the_value' }
     expect(renameAttribute(input)).toStrictEqual(expected)
   })
   it('renames a description attribute', () => {
     const input = ['the_key__DESCRIPTION__', 'the_description']
-    const expected = {key: 'the_key', description: 'the_description'}
+    const expected = { key: 'the_key', description: 'the_description' }
     expect(renameAttribute(input)).toStrictEqual(expected)
   })
 })
