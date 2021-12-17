7c26a46727a4692d7885795d078b828496e267ce
clu@asymmetrik.com <clu@asymmetrik.com>
2021-12-17 14:41:31 -0500

    updated decimals to have a 0 in front of them

--- src/pages/library/DataBrowserPreview.js
+++ src/pages/library/DataBrowserPreview.js
@@ -23,16 +23,16 @@ const styles = {
       textTransform: 'uppercase', fontWeight: 600, fontSize: '0.75rem'
     },
     headerRowStyle: {
-      borderTop: '1px solid rgba(0,0,0,.3)', borderBottom: '1px solid rgba(0,0,0,.3)'
+      borderTop: '1px solid rgba(0,0,0,0.3)', borderBottom: '1px solid rgba(0,0,0,0.3)'
     },
     rowStyle: {
-      borderBottom: '1px solid rgba(0,0,0,.1)'
+      borderBottom: '1px solid rgba(0,0,0,0.1)'
     },
     evenRowStyle: {
       backgroundColor: 'white'
     },
     oddRowStyle: {
-      backgroundColor: 'rgba(233,233,233,.1)'
+      backgroundColor: 'rgba(233,233,233,0.1)'
     }
   }
 }
