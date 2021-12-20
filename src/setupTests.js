52c33db7812d180f0953bad72a98cb751e55f6fe
Isaac Zarsky <zarsky@broadinstitute.org>
2021-12-20 16:54:06 -0500

    missed one

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,3 @@
-import * as Utils from 'src/libs/utils'
-
-
 jest.mock('src/configStore', () => ({
   loadedConfigStore: { current: { jest: true } }
 }))
-
-// Mock dates due to time zone issues
-jest.spyOn(Utils, 'makePrettyDate').mockImplementation(() => '***MOCKED DATE***')
