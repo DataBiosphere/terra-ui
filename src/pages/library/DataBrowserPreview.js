49898348d9533cde652b66338d8b304928842a55
lu-c <clu@asymmetrik.com>
2022-01-05 20:34:10 -0500

    [DC-101] Updated dropdown styling (#2740)
    * [DC-89] added column selector
    
    * no message
    
    * [DC-94] added drawer, i think the behavior is slightly different from what I expected though. I might touch this ticket again later
    
    * updted to use react-json-view, and added title into the row
    
    * Updated table styles
    
    * Updated to have even and odd correct
    
    * lint
    
    * updated styles to make code more concise
    
    * updated decimals to have a 0 in front of them
    
    * updated to use the colors
    
    * removed unset buttons
    
    * Added new select styling
    
    * Update src/pages/library/DataBrowserPreview.js
    
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>
    
    * Update src/pages/library/DataBrowserPreview.js
    
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>
    
    * Update src/pages/library/DataBrowserPreview.js
    
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>
    
    * Update src/pages/library/DataBrowserPreview.js
    
    Co-authored-by: kyuksel <kyuksel@broadinstitute.org>
    
    * added lodash unit test.
    
    * added
    
    * moved selectOptions into the loadData function, so this calculate method isn't being run on every update cycle, but only when the data actually changes.
    
    Fixed bug where the column headings would update late
    
    * updated again
    
    * Updated toPairs to Utils.toIndexPairs, deleted lodash unit test
    
    * lint
    
    * updated styling
    
    * Iz data browser preview noodling (#2746)
    
    * a bit of rethinking
    
    * unnecessary reordering
    
    * undefined is a default thing
    
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>
    
    * debug failed pr deploy
    
    * what if don't notify
    
    * explicit empty token check
    
    * debug
    
    * tell me this isn't it
    
    * break stuff
    
    * ugh
    
    * love me plz
    
    * Bash debugging from unofficial strict mode
    
    * Try ignoring the non-zero exit for possibly failing command
    
    * treat like strings!
    
    * unintentional change
    
    * how about this then
    
    * name an unclear step
    
    Co-authored-by: Isaac Zarsky <zarsky@broadinstitute.org>
    Co-authored-by: kyuksel <kyuksel@broadinstitute.org>
    Co-authored-by: David Mohs <dgit@mohsinc.com>

--- src/pages/library/DataBrowserPreview.js
+++ src/pages/library/DataBrowserPreview.js
@@ -1,10 +1,10 @@
 import _ from 'lodash/fp'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { div, h, h1, h2 } from 'react-hyperscript-helpers'
 import ReactJson from 'react-json-view'
-import { ButtonPrimary, Link, Select } from 'src/components/common'
+import { ButtonPrimary, GroupedSelect, Link } from 'src/components/common'
 import FooterWrapper from 'src/components/FooterWrapper'
-import { centeredSpinner, icon, spinner } from 'src/components/icons'
+import { centeredSpinner, icon } from 'src/components/icons'
 import { libraryTopMatter } from 'src/components/library-common'
 import ModalDrawer from 'src/components/ModalDrawer'
 import { ColumnSelector, SimpleTable } from 'src/components/table'
@@ -44,96 +44,93 @@ const activeTab = 'browse & explore'
 
 const DataBrowserPreview = ({ id }) => {
   const signal = useCancellation()
-  const [loading, setLoading] = useState()
-  const { dataCatalog } = useDataCatalog()
-  const dataMap = _.keyBy('dct:identifier', dataCatalog)
-  const snapshot = dataMap[id]
-
-  // Snapshot access granted
-  const [tables, setTables] = useState()
-  const [selectedTable, setSelectedTable] = useState('')
-  const [previewData, setPreviewData] = useState()
-  const [columnSettings, setColumnSettings] = useState([])
+  const [loading, setLoading] = useState(false)
+  const { dataCatalog, loading: catalogLoading } = useDataCatalog()
+  const [tables, setTables] = useState(undefined)
+  const [selectedTable, setSelectedTable] = useState()
+  const [previewRows, setPreviewRows] = useState()
+  const [columns, setColumns] = useState()
   const [viewJSON, setViewJSON] = useState()
+  const [selectOptions, setSelectOptions] = useState()
 
-  const tableMap = _.keyBy('name', tables)
-  const tableNames = _.map('name', tables)
+  useOnMount(() => {
+    const loadData = async () => {
+      const { tables: newTables } = await Ajax(signal).DataRepo.getPreviewMetadata(id)
 
-  const selectTable = ({ value }) => {
+      const [hasRows, noRows] = _.flow(
+        _.sortBy('name'),
+        _.map(({ name, rowCount }) => ({ value: name, rowCount })),
+        _.partition(({ rowCount }) => rowCount > 0)
+      )(newTables)
+
+      const newSelectOptions = [{ label: '', options: hasRows }, { label: 'Tables without data', options: noRows }]
+
+      setTables(newTables)
+      setSelectOptions(newSelectOptions)
+      setSelectedTable(hasRows[0]?.value || noRows[0]?.value)
+    }
+
+    loadData()
+  })
+
+  useEffect(() => {
+    const formatTableCell = ({ cellKey, cellContent, rowIndex, table }) => {
+      const maybeJSON = Utils.maybeParseJSON(cellContent)
+      return Utils.cond(
+        [!Utils.cantBeNumber(cellContent), () => cellContent],
+        [!!maybeJSON, () => h(ButtonPrimary, {
+          style: { fontSize: 16, textTransform: 'none' },
+          onClick: () => setViewJSON({ title: `${table}, Row ${rowIndex} - ${cellKey}`, cellData: maybeJSON })
+        }, ['View JSON'])],
+        [Utils.DEFAULT, () => cellContent]
+      )
+    }
+
     const loadTable = _.flow(
       Utils.withBusyState(setLoading),
       withErrorReporting('Error loading table')
     )(async () => {
-      setSelectedTable(value)
-      const previewTableData = await Ajax(signal).DataRepo.getPreviewTable({
-        id, limit: 50, offset: 0,
-        table: value
-      })
+      const { columns: newTableColumns } = _.find({ name: selectedTable }, tables) || {}
 
-      const columnNames = _.map('name', tableMap[selectedTable]?.columns)
+      const newDisplayColumns = _.flow(
+        Utils.toIndexPairs,
+        _.map(([index, { name }]) => ({
+          // name field is used in the column selector
+          // key field is used in the Simple Table
+          name, key: name,
+          visible: index < 6,
+          header: div({ style: styles.table.header }, [name])
+        }))
+      )(newTableColumns)
 
-      setPreviewData(_.flow([
+      setColumns(newDisplayColumns)
+
+      const previewTableData = await Ajax(signal).DataRepo.getPreviewTable({ id, limit: 50, offset: 0, table: selectedTable })
+
+      const newPreviewRows = _.flow(
         _.getOr([], 'result'),
-        _.toPairs,
+        Utils.toIndexPairs,
         _.map(([rowIndex, row]) => {
-          return _.reduce((obj, param) => {
-            obj[param] = formatTableCell({ cellKey: param, cellContent: row[param], rowIndex, table: value })
-            return obj
-          }, {}, columnNames)
+          return _.reduce((acc, { name }) => {
+            const formattedCell = formatTableCell({ cellKey: name, cellContent: row[name], rowIndex, table: selectedTable })
+            return _.set([name], formattedCell, acc)
+          }, {}, newTableColumns)
         })
-      ])(previewTableData))
+      )(previewTableData)
 
-      setColumnSettings(
-        _.flow([
-          _.toPairs,
-          _.map(([index, col]) => {
-            return {
-              // name field is used in the column selector
-              // key field is used in the Simple Table
-              name: col.name, key: col.name,
-              visible: index < 6,
-              header: div({ style: styles.table.header }, [col.name])
-            }
-          })
-        ])(tableMap[selectedTable]?.columns)
-      )
+      setPreviewRows(newPreviewRows)
     })
-    loadTable()
-  }
 
-  const formatTableCell = ({ cellKey, cellContent, rowIndex, table }) => {
-    const maybeJSON = Utils.maybeParseJSON(cellContent)
-    return Utils.cond(
-      [!Utils.cantBeNumber(cellContent), () => cellContent],
-      [maybeJSON, () => {
-        const contentAsJSON = {
-          title: `${table}, Row ${rowIndex} - ${cellKey}`,
-          cellData: maybeJSON
-        }
-
-        return h(ButtonPrimary, {
-          style: { fontSize: 16, textTransform: 'none' },
-          onClick: () => { setViewJSON(contentAsJSON) }
-        }, ['View JSON'])
-      }],
-      [Utils.DEFAULT, () => cellContent]
-    )
-  }
-
-  useOnMount(() => {
-    const loadData = async () => {
-      if (snapshot?.access === snapshotAccessTypes.GRANTED) {
-        const metadata = await Ajax(signal).DataRepo.getPreviewMetadata(id)
-        setTables(metadata.tables)
-        selectTable({ value: metadata.tables[0].name })
-      }
+    if (!!tables && !!selectedTable && snapshot?.access === snapshotAccessTypes.GRANTED) {
+      loadTable()
     }
-    loadData()
-  })
+  }, [selectedTable]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const snapshot = _.find({ 'dct:identifier': id }, dataCatalog)
 
   return h(FooterWrapper, { alwaysShow: true }, [
     libraryTopMatter(activeTab),
-    !snapshot ?
+    catalogLoading || !tables ?
       centeredSpinner() :
       div({ style: { padding: 20 } }, [
         div({ style: { display: 'flex', flexDirection: 'row', alignItems: 'top', justifyContent: 'space-between', width: '100%', lineHeight: '26px' } }, [
@@ -154,38 +151,46 @@ const DataBrowserPreview = ({ id }) => {
             }
           })
         ]),
-        snapshot.access === snapshotAccessTypes.GRANTED && div({
-          style: { display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%', marginBottom: 30 }
-        }, [
-          h(Select, {
-            'aria-label': 'data type',
-            styles: { container: base => ({ ...base, marginLeft: '1rem', width: 350 }) },
-            isSearchable: true,
-            isClearable: false,
-            value: selectedTable,
-            getOptionLabel: ({ value }) => `${_.startCase(value)}${tableMap[value].rowCount === 0 ? ' (0 Rows)' : ''}`,
-            onChange: ({ value }) => selectTable({ value }),
-            options: tableNames
-          }),
-          loading && spinner({ style: { marginLeft: '1rem' } })
-        ]),
-        tableMap && tableMap[selectedTable] && div({ style: { position: 'relative', padding: '0 15px' } }, [
-          h(SimpleTable, {
-            'aria-label': `${_.startCase(selectedTable)} Preview Data`,
-            columns: _.filter('visible', columnSettings),
-            cellStyle: { border: 'none', paddingRight: 15, wordBreak: 'break-all', display: 'flex', alignItems: 'center' },
-            ...styles.table,
-            useHover: false,
-            rows: previewData
-          }),
-          columnSettings.length > 0 && h(ColumnSelector, { onSave: setColumnSettings, columnSettings, style: { backgroundColor: 'transparent', height: '2.5rem', width: '2.5rem', border: 0, right: 15 } })
-        ]),
-        previewData?.length === 0 && div({ style: { width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' } }, ['(No Data)'])
+        snapshot.access === snapshotAccessTypes.GRANTED && h(GroupedSelect, {
+          'aria-label': 'data type',
+          styles: { container: base => ({ ...base, marginLeft: '1rem', width: 350, marginBottom: 30 }) },
+          isSearchable: true,
+          isClearable: false,
+          value: selectedTable,
+          getOptionLabel: ({ rowCount, value }) => div({ style: { color: colors.dark(!!rowCount ? 1 : 0.5) } }, [_.startCase(value)]),
+          formatGroupLabel: ({ label }) => {
+            return !!label && div({
+              style: { marginTop: 5, paddingTop: 15, borderTop: `1px solid ${colors.dark(0.5)}`, color: colors.dark(0.8) }
+            }, [label])
+          },
+          onChange: ({ value }) => setSelectedTable(value),
+          options: selectOptions
+        }),
+        loading ?
+          centeredSpinner() :
+          div({ style: { position: 'relative', padding: '0 15px' } }, [
+            h(SimpleTable, {
+              'aria-label': `${_.startCase(selectedTable)} Preview Data`,
+              columns: _.filter('visible', columns),
+              cellStyle: { border: 'none', paddingRight: 15, wordBreak: 'break-all', display: 'flex', alignItems: 'center' },
+              ...styles.table,
+              useHover: false,
+              rows: previewRows
+            }),
+            !_.isEmpty(columns) && h(ColumnSelector, {
+              onSave: setColumns, columnSettings: columns,
+              style: { backgroundColor: 'unset', height: '2.5rem', width: '2.5rem', border: 0, right: 15 }
+            }),
+            _.isEmpty(previewRows) && div({
+              style: { width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }
+            }, ['(No Data)'])
+          ])
       ]),
-    viewJSON && h(ModalDrawer, {
+    !!viewJSON && h(ModalDrawer, {
       'aria-label': 'View Json', isOpen: true, width: 675,
-      onDismiss: () => { setViewJSON() },
-      children: div({ style: { padding: '0 25px 25px' } }, [
+      onDismiss: () => { setViewJSON() }
+    }, [
+      div({ style: { padding: '0 25px 25px' } }, [
         h2([viewJSON.title]),
         h(ReactJson, {
           style: { whiteSpace: 'pre-wrap', wordBreak: 'break-word', backgroundColor: 'white' },
@@ -197,7 +202,7 @@ const DataBrowserPreview = ({ id }) => {
           src: viewJSON.cellData
         })
       ])
-    })
+    ])
   ])
 }
 
@@ -205,5 +210,5 @@ export const navPaths = [{
   name: 'library-catalog-preview',
   path: '/library/browser/:id/preview',
   component: DataBrowserPreview,
-  title: ({ id }) => `Catalog - Dataset Preview`
+  title: 'Catalog - Dataset Preview'
 }]
