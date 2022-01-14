16e75f129db9665b2a2d34da02cbe93491b640ca
Saman Ehsan <se8ea@virginia.edu>
2022-01-14 15:28:18 -0500

    Test clean-up

--- integration-tests/tests/preview-dataset.js
+++ integration-tests/tests/preview-dataset.js
@@ -1,21 +1,13 @@
 const _ = require('lodash/fp')
-const { signIntoTerra, click, clickable, clickTableCell, findElement, findText, findTableCellText, getTableCellPath, getTableHeaderPath, input, waitForNoSpinners } = require('../utils/integration-utils')
-const { withWorkspace } = require('../utils/integration-helpers')
+const { signIntoTerra, click, clickable, findText, findTableCellText, getTableCellPath, getTableHeaderPath, waitForNoSpinners } = require('../utils/integration-utils')
 const { withUserToken } = require('../utils/terra-sa-utils')
 const { dismissNotifications } = require('../utils/integration-utils')
 
 const datasetName = 'Cell hashing with barcoded antibodies enables multiplexing and doublet detection for single cell genomics'
 
-const assertEquals = (expected, actual) => {
-  if (expected !== actual) {
-    throw new Error("Expected value " + expected + " does not equal actual " + actual)
-  }
-}
-
 const testPreviewDatasetFn = _.flow(
-  withWorkspace,
   withUserToken
-)(async ({ testUrl, page, token, workspaceName }) => {
+)(async ({ testUrl, page, token }) => {
   await page.goto(testUrl)
   await waitForNoSpinners(page)
 
@@ -44,24 +36,14 @@ const testPreviewDatasetFn = _.flow(
   await findTableCellText(page, getTableCellPath(previewTableName, 2, 2), '1558104905.862')
   await findTableCellText(page, getTableCellPath(previewTableName, 2, 3), 'View JSON')
   await click(page, clickable({ textContains: 'View JSON'  }))
-  await findElement(page, '//div[contains(@class, "react-json-view")]')
+  await findText(page, 'describedBy')
   await page.keyboard.press('Escape')
 
   // Click on a table with no data
-  await click(page, '//*[@role="combobox"]//div')
-  const tableNameNoData = "Analysis File"
-  await click(page, `//*[@role="listbox"]//*[@role="option" and contains(normalize-space(.),"${tableNameNoData}")]`)
+  await click(page, clickable({ text: 'data type', isDescendant: true }))
+  await click(page, clickable({ text: 'Analysis File'}))
   await waitForNoSpinners(page)
   await findText(page, 'No Data')
-
-  // Verify table counts
-  await click(page, '//*[@role="combobox"]//div')
-  const tablesWithData = await page.$x('//*[@role="combobox"]//*[contains(@id, "option-0")]')
-  const countTablesWithData = _.size(tablesWithData)
-  assertEquals(countTablesWithData, 11)
-  const tablesWithNoData = await page.$x('//*[@role="combobox"]//*[contains(@id, "option-1")]')
-  const countTablesWithNoData = _.size(tablesWithNoData)
-  assertEquals(countTablesWithNoData, 16)
 })
 
 const testPreviewDataset = {
--- integration-tests/utils/integration-utils.js
+++ integration-tests/utils/integration-utils.js
@@ -33,7 +33,7 @@ const findInGrid = (page, textContains, options) => {
 }
 
 const clickable = ({ text, textContains, isDescendant = false }) => {
-  const base = `(//a | //button | //input[@type="checkbox"] | //*[@role="button" or @role="checkbox"] | //*[@role="link"])${isDescendant ? '//*' : ''}`
+  const base = `(//a | //button | //input[@type="checkbox"] | //*[@role="button" or @role="checkbox"] | //*[@role="link"] | //*[@role="combobox"] | //*[@role="option"])${isDescendant ? '//*' : ''}`
   if (text) {
     return `${base}[normalize-space(.)="${text}" or @title="${text}" or @aria-label="${text}" or @aria-labelledby=//*[normalize-space(.)="${text}"]/@id]`
   } else if (textContains) {
