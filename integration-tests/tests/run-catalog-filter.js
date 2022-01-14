cce24b9e772d9e1969e46f8b5b15b3eb873f7cf9
lu-c <clu@asymmetrik.com>
2022-01-14 16:28:18 -0500

    [DC-102] Expanding unit tests a little (#2763)
    * Trying out writing unit tests
    
    * fixed unit tests
    
    * removed unused import

--- integration-tests/tests/run-catalog-filter.js
+++ integration-tests/tests/run-catalog-filter.js
@@ -30,6 +30,7 @@ const testCatalogFilterFn = withUserToken(async ({ testUrl, page, token }) => {
 
   const totalDatasetSize = await getDatasetCount(page)
 
+  // Testing filter by search text
   await fillIn(page, input({ labelContains: 'Search Datasets' }), searchText)
   await page.keyboard.press('Enter')
   const datasetSizeAfterSearch = await getDatasetCount(page)
@@ -38,11 +39,26 @@ const testCatalogFilterFn = withUserToken(async ({ testUrl, page, token }) => {
     throw new Error(`Rows not filtered after searching for '${searchText}'`)
   }
 
+  // Testing filter by facet
   await click(page, clickable({ text: filterItem, isDescendant: true }))
   const datasetSizeAfterFilter = await getDatasetCount(page)
 
   if (datasetSizeAfterFilter >= datasetSizeAfterSearch) {
     throw new Error(`Filter for '${filterItem}' was not applied to the table`)
+  }
+  
+  // Testing filter by multiple same facets
+  await click(page, clickable({ text: 'Controlled', isDescendant: true }))
+  const datasetSizeAfterFilter2 = await getDatasetCount(page)
+  if (datasetSizeAfterFilter2 !== 0) {
+    throw new Error(`Filters for 'Controlled' should be ANDed between the same facet category in the table'`)
+  }
+
+  // Testing clearing filters
+  await click(page, clickable({ textContains: 'clear' }))
+  const datasetSizeAfterClear = await getDatasetCount(page)
+  if (datasetSizeAfterClear !== datasetSizeAfterSearch) {
+    throw new Error(`Clear Filter was not applied to the table, ${datasetSizeAfterClear}, ${totalDatasetSize}`)
   }
 })
 
