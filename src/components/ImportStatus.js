f7aa4d345daa016db68d2737426fca333e0d5355
David An <davidan@broadinstitute.org>
2022-02-08 10:44:26 -0500

    backticks to straight single quotes

--- src/components/ImportStatus.js
+++ src/components/ImportStatus.js
@@ -56,7 +56,7 @@ const ImportStatusItem = ({ job: { targetWorkspace, jobId }, onDone }) => {
         p([`When data is imported from external sources, like PFB or TDR, prefixes ("pfb:" or "tdr:", respectively)
             will be prepended to column names. Prefix values must be included in attribute references as described `,
         h(Link, {
-          'aria-label': `Support article`,
+          'aria-label': 'Support article',
           href: 'https://support.terra.bio/hc/en-us/articles/360051722371-Data-table-attribute-namespace-support-pfb-prefix-#h_01ENT95Y0KM48QFRMJ44DEXS7S',
           ...Utils.newTabLinkProps
         }, ['here.'])])
