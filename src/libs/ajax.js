03459a28a3d0f43fb89de0d101e2f12557d7bfb0
Marcus Talbott <mtalbott@broadinstitute.org>
2022-02-08 16:47:35 -0500

    remove extra await

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -239,7 +239,7 @@ const User = signal => ({
   getSamTosAccepted: async () => {
     try {
       const res = await fetchSam('register/user/v1/termsofservice/status', _.merge(authOpts(), { signal }))
-      return await res.json()
+      return res.json()
     } catch (error) {
       if (error.status === 404) {
         return null
