ca7a0ca4ffc067bbc8de84bdb0289003b7164520
Nick Watts <nwatts@broadinstitute.org>
2022-02-28 15:38:27 -0500

    Add comment

--- src/pages/workspaces/workspace/jobHistory/SubmissionDetails.js
+++ src/pages/workspaces/workspace/jobHistory/SubmissionDetails.js
@@ -242,6 +242,7 @@ const SubmissionDetails = _.flow(
           })
         ])
       ]),
+      // 48px is based on the default row height of FlexTable
       div({ style: { flex: `1 0 calc(${1 + Math.min(filteredWorkflows.length, 5.5)} * 48px)` } }, [
         h(AutoSizer, [({ width, height }) => h(FlexTable, {
           'aria-label': 'submission details',
