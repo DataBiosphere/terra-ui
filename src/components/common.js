6299208d5e449223a01677e156f8273d2585d18a
cstrandw <97535660+cstrandw@users.noreply.github.com>
2022-03-07 11:41:23 -0500

    [IA-2172] Added links to copy image url and for image changelogs (#2847)
    Added copy function for image url and changelog links to version

--- src/components/common.js
+++ src/components/common.js
@@ -539,8 +539,8 @@ export const WarningTitle = ({ children, iconSize = 36 }) => {
 export const ClipboardButton = ({ text, onClick, children, ...props }) => {
   const [copied, setCopied] = useState(false)
   return h(Link, {
-    ...props,
     tooltip: copied ? 'Copied to clipboard' : 'Copy to clipboard',
+    ...props,
     onClick: _.flow(
       withErrorReporting('Error copying to clipboard'),
       Utils.withBusyState(setCopied)
