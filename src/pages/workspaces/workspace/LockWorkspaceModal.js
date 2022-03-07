8315e064c33c52923b0b85a8020d7fb30e9840d6
Matt Bemis <mbemis@broadinstitute.org>
2022-03-07 16:26:11 -0500

    toggle warning message based on state

--- src/pages/workspaces/workspace/LockWorkspaceModal.js
+++ src/pages/workspaces/workspace/LockWorkspaceModal.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { div, h, span } from 'react-hyperscript-helpers'
+import { div, h } from 'react-hyperscript-helpers'
 import { ButtonPrimary, spinnerOverlay } from 'src/components/common'
 import Modal from 'src/components/Modal'
 import { Ajax } from 'src/libs/ajax'
@@ -7,18 +7,18 @@ import { reportError } from 'src/libs/error'
 
 
 const LockWorkspaceModal = ({ workspace: { workspace: { namespace, name, isLocked } }, onDismiss, onSuccess }) => {
-  const [locking, setLocking] = useState(false)
+  const [togglingLock, setTogglingLock] = useState(false)
   const titleText = isLocked ? 'Unlock Workspace' : 'Lock Workspace'
 
   const toggleWorkspaceLock = async () => {
     try {
-      setLocking(true)
+      setTogglingLock(true)
       isLocked ? await Ajax().Workspaces.workspace(namespace, name).unlock() : await Ajax().Workspaces.workspace(namespace, name).lock()
       onDismiss()
       onSuccess()
     } catch (error) {
       reportError('Error toggling workspace lock', error)
-      setLocking(false)
+      setTogglingLock(false)
     }
   }
   return h(Modal, {
@@ -29,10 +29,10 @@ const LockWorkspaceModal = ({ workspace: { workspace: { namespace, name, isLocke
       tooltip: titleText
     }, titleText)
   }, [
-    div(['Are you sure you want to lock the workspace ',
-      span({ style: { fontWeight: 600, wordBreak: 'break-word' } }, name),
-      '?']),
-    locking && spinnerOverlay
+    isLocked ?
+      div(['Are you sure you want to unlock this workspace? Collaborators will be able to modify the workspace while it is unlocked.']) :
+      div(['Are you sure you want to lock this workspace? Collaborators will not be able to modify the workspace while it is locked.']),
+    togglingLock && spinnerOverlay
   ])
 }
 
