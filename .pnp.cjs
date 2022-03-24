56e7d12d6f111ee08aceac1cb996eccea6c3bf95
Isaac Zarsky <zarsky@broadinstitute.org>
2022-03-24 16:11:29 -0400

    CI integration fix

--- .pnp.cjs
+++ .pnp.cjs
@@ -11948,12 +11948,25 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         ["npm:6.0.3", {\
           "packageLocation": "./.yarn/cache/jest-environment-puppeteer-npm-6.0.3-1e4974b622-48730cf6a4.zip/node_modules/jest-environment-puppeteer/",\
           "packageDependencies": [\
-            ["jest-environment-puppeteer", "npm:6.0.3"],\
+            ["jest-environment-puppeteer", "npm:6.0.3"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:4b5760c811956179cba857863592c6b31b2489ce53efac2db5b08b6e47b390074db0c5e3228b897f68e40a818b8c7493b97f2be9d218b0f579245018d74d7cad#npm:6.0.3", {\
+          "packageLocation": "./.yarn/__virtual__/jest-environment-puppeteer-virtual-7704e3d881/0/cache/jest-environment-puppeteer-npm-6.0.3-1e4974b622-48730cf6a4.zip/node_modules/jest-environment-puppeteer/",\
+          "packageDependencies": [\
+            ["jest-environment-puppeteer", "virtual:4b5760c811956179cba857863592c6b31b2489ce53efac2db5b08b6e47b390074db0c5e3228b897f68e40a818b8c7493b97f2be9d218b0f579245018d74d7cad#npm:6.0.3"],\
+            ["@types/puppeteer", null],\
             ["chalk", "npm:4.1.2"],\
             ["cwd", "npm:0.10.0"],\
             ["jest-dev-server", "npm:6.0.3"],\
             ["jest-environment-node", "npm:27.5.1"],\
-            ["merge-deep", "npm:3.0.3"]\
+            ["merge-deep", "npm:3.0.3"],\
+            ["puppeteer", "npm:13.5.1"]\
+          ],\
+          "packagePeers": [\
+            "@types/puppeteer",\
+            "puppeteer"\
           ],\
           "linkType": "HARD"\
         }]\
@@ -12117,7 +12130,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jest-puppeteer", "virtual:dbab8a24bb1e83f64d01070d471958b56ec3d11fd3ae5a8c71ee6e018bca5308c73bd59656625959ac80a6ed20fc47a8e80415eaa84ea83f83b375ceeaa8e710#npm:6.1.0"],\
             ["@types/puppeteer", null],\
             ["expect-puppeteer", "npm:6.1.0"],\
-            ["jest-environment-puppeteer", "npm:6.0.3"],\
+            ["jest-environment-puppeteer", "virtual:4b5760c811956179cba857863592c6b31b2489ce53efac2db5b08b6e47b390074db0c5e3228b897f68e40a818b8c7493b97f2be9d218b0f579245018d74d7cad#npm:6.0.3"],\
             ["puppeteer", "npm:13.5.1"]\
           ],\
           "packagePeers": [\
