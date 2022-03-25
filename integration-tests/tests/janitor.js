fa63cb861ac716d94de45816fdf0ed96d42617dd
cahrens <roberts@broadinstitute.org>
2022-03-25 09:17:56 -0400

    Try actually deleting.

--- integration-tests/tests/janitor.js
+++ integration-tests/tests/janitor.js
@@ -19,20 +19,20 @@ const runJanitor = withUserToken(async ({ billingProject, page, testUrl, token }
   const workspaces = await page.evaluate(async () => await window.Ajax().Workspaces.list())
   const oldWorkspaces = _.filter(({ workspace: { namespace, name, createdDate } }) => {
     const age = dateFns.differenceInDays(new Date(createdDate), new Date())
-    rawConsole.info(`${namespace} ${name}, age ${age} days`)
+    // rawConsole.info(`${namespace} ${name}, age ${age} days`)
     return namespace === billingProject && _.startsWith(testWorkspaceNamePrefix, name) && age > olderThanDays
   }, workspaces)
 
   rawConsole.log(`Deleting ${oldWorkspaces.length} workspaces with prefix "${testWorkspaceNamePrefix}" created more than ${olderThanDays} days ago.`)
 
-  // return Promise.all(_.map(async ({ workspace: { namespace, name } }) => {
-  //   try {
-  //     await page.evaluate((namespace, name) => window.Ajax().Workspaces.workspace(namespace, name).delete(), namespace, name)
-  //     rawConsole.info(`Deleted old workspace: ${name}`)
-  //   } catch (e) {
-  //     rawConsole.info(`Failed to delete old workspace: ${name} with billing project ${namespace}`)
-  //   }
-  // }, oldWorkspaces))
+  return Promise.all(_.map(async ({ workspace: { namespace, name } }) => {
+    try {
+      await page.evaluate((namespace, name) => window.Ajax().Workspaces.workspace(namespace, name).delete(), namespace, name)
+      rawConsole.info(`Deleted old workspace: ${name}`)
+    } catch (e) {
+      rawConsole.info(`Failed to delete old workspace: ${name} with billing project ${namespace}`)
+    }
+  }, oldWorkspaces))
 })
 
 const janitor = {
