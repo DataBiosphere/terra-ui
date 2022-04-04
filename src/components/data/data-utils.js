dc843df3c03573c50f013618196e38dc6bac62b9
Matt Bemis <mbemis@broadinstitute.org>
2022-04-04 16:02:04 -0400

    improve accessibility

--- src/components/data/data-utils.js
+++ src/components/data/data-utils.js
@@ -382,7 +382,7 @@ export const EntityUploader = ({ onSuccess, onDismiss, namespace, name, entityTy
         }, [
           icon('warning-standard', { size: 19, style: { color: colors.warning(), flex: 'none', marginRight: '0.5rem', marginLeft: '-0.5rem' } }),
           'We have detected empty cells in your TSV. Please choose an option:',
-          div({ style: { paddingTop: '0.5rem' } }, [
+          div({ style: { paddingTop: '0.5rem' }, role: 'radiogroup', 'aria-label': 'We have detected empty cells in your TSV. Please choose an option.' }, [
             h(RadioButton, {
               text: 'Ignore empty cells (default)',
               name: 'ignore-empty-cells',
@@ -394,7 +394,7 @@ export const EntityUploader = ({ onSuccess, onDismiss, namespace, name, entityTy
           div({ style: { paddingTop: '0.5rem' } }, [
             h(RadioButton, {
               text: 'Overwrite existing cells with empty cells',
-              name: 'overwrite-existing-cells',
+              name: 'ignore-empty-cells',
               checked: deleteEmptyValues,
               onChange: () => setDeleteEmptyValues(true),
               labelStyle: { padding: '0.5rem', fontWeight: 'normal' }
