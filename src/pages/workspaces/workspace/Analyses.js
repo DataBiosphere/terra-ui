f5c55b7af79bd6749a3b3ccb213c4ee2da8062f1
cstrandw <97535660+cstrandw@users.noreply.github.com>
2022-04-11 10:35:37 -0400

    [IA-3080] Usability testing feedback (top 2 nice-to-have points) (#2917)

--- src/pages/workspaces/workspace/Analyses.js
+++ src/pages/workspaces/workspace/Analyses.js
@@ -21,7 +21,7 @@ import { ariaSort } from 'src/components/table'
 import TooltipTrigger from 'src/components/TooltipTrigger'
 import galaxyLogo from 'src/images/galaxy-logo.png'
 import jupyterLogo from 'src/images/jupyter-logo.svg'
-import rstudioBioLogo from 'src/images/r-bio-logo.png'
+import rstudioBioLogo from 'src/images/r-bio-logo.svg'
 import rstudioSquareLogo from 'src/images/rstudio-logo-square.png'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
@@ -327,7 +327,7 @@ const Analyses = _.flow(
     div({ style: { fontSize: 48 } }, ['A place for all your analyses ']),
     div({ style: { display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' } }, [
       img({ src: jupyterLogo, style: { height: 120, width: 80, marginRight: '5rem' } }),
-      img({ src: rstudioBioLogo, style: { width: 406, height: 75, marginRight: '1rem' } }),
+      img({ src: rstudioBioLogo, style: { width: 400, marginRight: '5rem' } }),
       div([
         img({ src: galaxyLogo, style: { height: 60, width: 208 } })
         // span({ style: { marginTop: '3.5rem'} }, ['Galaxy'])
