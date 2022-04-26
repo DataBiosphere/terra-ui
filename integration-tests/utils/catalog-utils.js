a663188ae49b4a81839107f621d1b33f22fd31fe
Stephen Lucas <slucas@broadinstitute.org>
2022-04-26 16:37:07 -0400

    remove row/column from `catalog-utils.js`

--- integration-tests/utils/catalog-utils.js
+++ integration-tests/utils/catalog-utils.js
@@ -15,7 +15,7 @@ const linkDataToWorkspace = async (page, testUrl, token) => {
   await enableDataCatalog(page, testUrl, token)
   await click(page, clickable({ textContains: 'browse & explore' }))
   await click(page, checkbox({ text: 'Granted', isDescendant: true }))
-  await clickTableCell(page, 'dataset list', 2, 2)
+  await clickTableCell(page, { tableName: 'dataset list', columnHeader: 'Dataset Name', textContains: 'Discoverable Catalog Snapshot 1' })
   await noSpinnersAfter(page, { action: () => click(page, clickable({ textContains: 'Link to a workspace' })) })
 }
 
