e7701d22276bc065604b5d6a9272044e93cba3b4
aweng98 <bweng@broadinstitute.org>
2022-04-26 21:20:47 -0400

    move jest-junit from package.json to jest.config.js

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  testRegex: '\\.integration-test\\.js$'
+  testRegex: '\\.integration-test\\.js$',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '/tmp/test-results',
+        outputName: 'integration-test-results.xml',
+        suiteName: 'Integration tests',
+        addFileAttribute: 'true',
+        includeConsoleOutput: true
+      }
+    ]
+  ]
 }
