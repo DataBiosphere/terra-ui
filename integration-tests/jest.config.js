f140c2848d790c5f35780ae0c11cf61bdc1cef77
aweng98 <35533885+aweng98@users.noreply.github.com>
2022-04-28 15:34:06 -0400

    [QA-1837] Move jest-junit config (#2986)
    * move jest-junit from package.json to jest.config.js
    
    * addFileAttribute value is a string
    
    * remove jest-junit config from package.json
    
    Co-authored-by: Kaan Yuksel <kyuksel@broadinstitute.org>

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  verbose: false,
   preset: 'jest-puppeteer',
-  testRegex: '\\.integration-test\\.js$'
+  testRegex: '\\.integration-test\\.js$',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'Integration tests',
+        addFileAttribute: 'true',
+        includeConsoleOutput: true
+      }
+    ]
+  ]
 }
