512d80f9a7c6f6947c81b02c1b626df74c58e203
Kaan Yuksel <kyuksel@broadinstitute.org>
2022-05-02 17:57:20 -0400

    PR feedback

--- integration-tests/jest-reporter.js
+++ integration-tests/jest-reporter.js
@@ -1,5 +1,6 @@
+const _ = require('lodash/fp')
 const { createWriteStream, existsSync, mkdirSync } = require('fs')
-const path = require('path')
+const { parse } = require('path')
 
 
 module.exports = class JestReporter {
@@ -7,31 +8,30 @@ module.exports = class JestReporter {
     this.logRootDir = process.env.LOG_DIR || '/tmp/test-results'
   }
 
-  onTestStart(test) {
-    console.info(`\n**  Running ${path.parse(test.path).name} at ${this.timeNow()}\n`)
+  onTestStart({ path }) {
+    const { name } = parse(path)
+    console.info(`\n**  Running ${name} at ${this.timeNow()}\n`)
   }
 
-  onTestResult(_testRunConfig, testResult, _runResults) {
-    const testName = path.parse(testResult.testFilePath).name
-    const hasFailure = testResult.testResults.some(result => result.status === 'failed')
-    const logDir = hasFailure ? `${this.logRootDir}/failures` : `${this.logRootDir}/successes`
+  onTestResult(_testRunConfig, { testFilePath, testResults, console }, _runResults) {
+    const testName = parse(testFilePath).name
+    const hasFailure = _.some(({ status }) => status === 'failed', testResults)
+    const logDir = `${this.logRootDir}/${hasFailure ? 'failures' : 'successes'}`
     const logFileName = `${logDir}/${testName}-${Date.now()}.log`
-    !existsSync(logDir) && mkdirSync(logDir, { recursive: true })
+    // Since Node 10, mkdirSync knows to not do anything if the dir exists (https://stackoverflow.com/a/71767385/2851999) so not checking explicitly
+    // We may want to specify what to do if an error occurs. Not sure if throwing is what we'd want.
+    mkdirSync(logDir, { recursive: true }, err => err && throw err)
 
     const writableStream = createWriteStream(logFileName)
-    writableStream.on('error', error => {
-      console.error(`Error occurred while writing Console logs to ${logFileName}.\n${error.message}`)
+    writableStream.on('error', ({ message }) => {
+      console.error(`Error occurred while writing Console logs to ${logFileName}.\n${message}`)
     })
 
-    if (testResult.console && testResult.console.length > 0) {
-      testResult.console.forEach(log => {
-        writableStream.write(`${log.message}\n`)
-      })
-    }
+    _.forEach(({ message }) => writableStream.write(`${message}\n`), console)
 
     // Write pass/failure summaries
     writableStream.write('\n\nTests Summary\n')
-    testResult.testResults.forEach(result => {
+    _.forEach(result => {
       writableStream.write('----------------------------------------------\n')
       writableStream.write(`Test: ${result.title}\n`)
       writableStream.write(`Status: ${result.status}\n`)
@@ -40,7 +40,7 @@ module.exports = class JestReporter {
         writableStream.write(`Failure message: ${failure}\n`)
       }
       writableStream.write('\n')
-    })
+    }, testResults)
 
     writableStream.end()
     writableStream.on('finish', () => {
