a2b65817059cc7374fd97b0745dbc50976c0b4a5
aweng98 <bweng@broadinstitute.org>
2022-05-02 22:27:24 -0400

    fix register-user test

--- integration-tests/tests/register-user.js
+++ integration-tests/tests/register-user.js
@@ -1,12 +1,13 @@
 const { withUser } = require('../utils/integration-helpers')
 const { fillIn, findText, click, clickable, input, signIntoTerra } = require('../utils/integration-utils')
+const { fillInReplace } = require('../utils/integration-utils')
 
 
 const testRegisterUserFn = withUser(async ({ page, testUrl, token }) => {
-  await signIntoTerra(page, { token, testUrl })
-
+  await page.goto(testUrl, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'], timeout: 60 * 1000 })
   await click(page, clickable({ textContains: 'View Workspaces' }))
-  await fillIn(page, input({ labelContains: 'First Name' }), 'Integration')
+  await signIntoTerra(page, { token })
+  await fillInReplace(page, input({ labelContains: 'First Name' }), 'Integration')
   await fillIn(page, input({ labelContains: 'Last Name' }), 'Test')
   await click(page, clickable({ textContains: 'Register' }))
   await click(page, clickable({ textContains: 'Accept' }))
