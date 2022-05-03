248cb4a64056973fc3f76d3e18a5227ecf115db3
Stephen Lucas <slucas@broadinstitute.org>
2022-05-03 15:34:12 -0400

    simplifed `assertRowMatches()`

--- integration-tests/tests/preview-dataset.js
+++ integration-tests/tests/preview-dataset.js
@@ -19,10 +19,9 @@ const testPreviewDatasetFn = withUserToken(async ({ testUrl, page, token }) => {
   const tableName = 'Participant Preview Data'
   await assertRowMatches(page, {
     tableName,
-    expectedColumnValues: [['age', 36],['biological_sex', 'male']],
+    expectedColumnValues: [['age', 36], ['biological_sex', 'male']],
     withKey: { column: 'participant_id', textContains: 'participant1' }
   })
-
 })
 
 const testPreviewDataset = {
