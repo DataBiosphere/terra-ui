918441028f9d3732aac0c1835cf912127a6980c8
Stephen Lucas <slucas@broadinstitute.org>
2022-05-10 15:23:34 -0400

    Renamed `getTableTextWithinColumn()` to 'getTableCellByContents()'

--- integration-tests/utils/integration-utils.js
+++ integration-tests/utils/integration-utils.js
@@ -73,7 +73,7 @@ const getTableColIndex = async (page, { tableName, columnHeader }) => {
 const getTableCellByContents = async (page, { tableName, columnHeader, textContains, isDescendant = false }) => {
   const colIndex = await getTableColIndex(page, { tableName, columnHeader })
   const baseXpath = `//*[@role="table" and @aria-label="${tableName}"]//*[@role="row"]//*[@role="cell" and @aria-colindex = "${colIndex}"]`
-  const xpath = `${baseXpath}${isDescendant ? '//*' : ''}[text() = "${textContains}"]`
+  const xpath = `${baseXpath}${isDescendant ? '//*' : ''}[contains(normalize-space(.),"${textContains}")]`
   return xpath
 }
 
