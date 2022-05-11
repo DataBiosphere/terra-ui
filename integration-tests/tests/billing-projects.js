fd98b5cbba3b72dc34744ab9a10eef2891f7a42c
Robert Title <rtitle@broadinstitute.org>
2022-05-10 22:41:12 -0400

    Does this fix the test?

--- integration-tests/tests/billing-projects.js
+++ integration-tests/tests/billing-projects.js
@@ -1,12 +1,15 @@
 // This test is owned by the Workspaces Team.
 const _ = require('lodash/fp')
-const { assertTextNotFound, click, clickable, findText, noSpinnersAfter, select, signIntoTerra } = require('../utils/integration-utils')
+const { assertTextNotFound, click, clickable, findText, noSpinnersAfter, select, signIntoTerra, waitForNoSpinners } = require('../utils/integration-utils')
 const { withUserToken } = require('../utils/terra-sa-utils')
 
 
 const billingProjectsPage = (testPage, testUrl) => {
   return {
-    visit: async () => await testPage.goto(`${testUrl}/#billing`),
+    visit: async () => {
+      await testPage.goto(`${testUrl}/#billing`)
+      await waitForNoSpinners(testPage)
+    },
 
     selectSpendReport: async () => {
       await click(testPage, clickable({ text: 'Spend report' }))
