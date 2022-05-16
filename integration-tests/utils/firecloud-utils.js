bf6e419d4a9a62448e6b58f96ce68ccc58a02e25
aweng98 <35533885+aweng98@users.noreply.github.com>
2022-05-16 17:27:39 -0400

    [QA-1825] Integration test: Rework flaky signIntoFirecloud function (#3036)
    * rework signIntoFirecloud function

--- integration-tests/utils/firecloud-utils.js
+++ integration-tests/utils/firecloud-utils.js
@@ -1,14 +1,12 @@
-const { click, findText, waitForNoSpinners, delay } = require('./integration-utils')
+const { click, findText, waitForNoSpinners } = require('./integration-utils')
 
 
 const selectWorkspace = async (page, billingAccount, workspace) => {
-  await click(page, `//*[@data-test-id="workspace-selector"]`)
+  await click(page, '//*[@data-test-id="workspace-selector"]')
   return click(page, `//ul/li[contains(normalize-space(.),"${billingAccount}/${workspace}")]`)
 }
 
 const signIntoFirecloud = async (page, token) => {
-  await waitForNoSpinners(page)
-  await findText(page, 'content you are looking for is currently only accessible')
   /*
    * The FireCloud not-signed-in page renders the sign-in button while it is still doing some
    * initialization. If you log the status of the App components state for user-status and auth2
@@ -21,7 +19,19 @@ const signIntoFirecloud = async (page, token) => {
    * user-status). Instead of reworking the sign-in logic for a case that (for the most part) only
    * a computer will operate fast enough to encounter, we'll just slow the computer down a little.
    */
-  await delay(1000) // wait a second for full load not accounted for by spinners
+  await page.waitForResponse(response => {
+    return response.url().startsWith('https://accounts.google.com/o/oauth2/iframerpc') &&
+        response.request().method() === 'GET' &&
+        response.status() === 200
+  }, { timeout: 30 * 1000 }
+  )
+
+  await page.waitForXPath('//title[text()="FireCloud | Broad Institute"]')
+  await findText(page, 'content you are looking for is currently only accessible')
+  await waitForNoSpinners(page)
+
+  console.log(`Sign in Firecloud: ${page.url()}`)
+  await page.waitForFunction('!!window["forceSignedIn"]')
   await page.evaluate(token => window.forceSignedIn(token), token) // Note: function for Fire Cloud is forceSignedIn() while Terra is forceSignIn()
 }
 
