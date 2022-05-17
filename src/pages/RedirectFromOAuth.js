7f2e07b3efa3f2b76988484ef18c59489b0e308d
Robert Title <rtitle@broadinstitute.org>
2022-05-17 17:06:22 -0400

    Render fewer things for signInRedirect callback

--- src/pages/RedirectFromOAuth.js
+++ src/pages/RedirectFromOAuth.js
@@ -1,16 +1,31 @@
 import { UserManager } from 'oidc-client-ts'
-import { div, h } from 'react-hyperscript-helpers'
-import { centeredSpinner } from 'src/components/icons'
-import { getOidcConfig } from 'src/libs/auth'
+import { div, img } from 'react-hyperscript-helpers'
 import { useOnMount } from 'src/libs/react-utils'
 
 
-const RedirectFromOAuth = silent => {
-  const userManager = new UserManager(getOidcConfig())
+const RedirectFromOAuth = (silent = false) => {
+  const userManager = new UserManager({
+    popup_redirect_uri: `${window.origin}/redirect-from-oauth`,
+    silent_redirect_uri: `${window.origin}/redirect-from-oauth-silent`
+  })
   const url = window.location.href.replace('#', '')
   useOnMount(() => silent === true ? userManager.signinSilentCallback(url) : userManager.signinPopupCallback(url))
-  return div({ role: 'main' }, [
-    h(centeredSpinner, { style: { position: 'fixed' } })
+
+  const spinnerSize = 54
+  return div({ role: 'main', style: { position: 'absolute', top: 0, left: 0, height: '100%', width: '100%' } }, [
+    img({
+      src: 'loading-spinner.svg',
+      style: {
+        width: spinnerSize,
+        height: spinnerSize,
+        display: 'block',
+        position: 'sticky',
+        top: `calc(50% - ${spinnerSize / 2}px)`,
+        bottom: `calc(50% - ${spinnerSize / 2}px)`,
+        left: `calc(50% - ${spinnerSize / 2}px)`,
+        right: `calc(50% - ${spinnerSize / 2}px)`
+      }
+    })
   ])
 }
 
@@ -30,3 +45,5 @@ export const navPaths = [
     title: 'Redirect From OAuth'
   }
 ]
+
+export default RedirectFromOAuth
