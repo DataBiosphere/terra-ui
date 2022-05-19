dbc708c2478b419c8fbc984a6960df98ddc9c5de
Stephen Lucas <slucas@broadinstitute.org>
2022-05-19 15:47:04 -0400

    undo

--- integration-tests/jest/request-access.integration-test.js
+++ integration-tests/jest/request-access.integration-test.js
@@ -1,6 +1,8 @@
+const { registerTest } = require('./jest-utils')
+const { testRequestAccess } = require('../tests/request-access')
 // TODO: Reenable once data catalog supports controlled access datasets
 // To turn on:
 // 1. uncomment the below line
 // 2. rename the .disabled. to be .integration-test.
 // 3. add imports
-// registerTest(testRequestAccess)
+registerTest(testRequestAccess)
