8d3a45451ecff9c9caf5b2d5617b6f8354ac39d3
xuscrbw <xuw@broadinstitute.org>
2022-05-19 17:48:00 -0400

    comment out more lines

--- integration-tests/jest/1-analysis-context-bar.integration-test.js
+++ integration-tests/jest/1-analysis-context-bar.integration-test.js
@@ -1,5 +1,5 @@
-const { registerTest } = require('./jest-utils')
-const { testAnalysisContextBar } = require('../tests/analysis-context-bar')
+//const { registerTest } = require('./jest-utils')
+//const { testAnalysisContextBar } = require('../tests/analysis-context-bar')
 
 
 //TODO IA-3436: add registerTest(testAnalysisContextBar) back once we merge Leo to prod
--- integration-tests/jest/1-run-notebook.integration-test.js
+++ integration-tests/jest/1-run-notebook.integration-test.js
@@ -1,5 +1,5 @@
-const { registerTest } = require('./jest-utils')
-const { testRunNotebook } = require('../tests/run-notebook')
+//const { registerTest } = require('./jest-utils')
+//const { testRunNotebook } = require('../tests/run-notebook')
 
 
 //TODO IA-3436: add registerTest(testRunNotebook) once Leo is merged to Prod
