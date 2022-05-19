02f1aafa0dbbedf601185e0154d6e7301d68c233
Nick Watts <1156625+nawatts@users.noreply.github.com>
2022-05-19 08:19:26 -0400

    [SU-89] Allow creating a set from the data table UI (#3050)
    * Allow creating a set from the data table UI
    
    * Reorganize edit menu
    
    * Narrow try block

--- src/components/PopupTrigger.js
+++ src/components/PopupTrigger.js
@@ -1,6 +1,6 @@
 import _ from 'lodash/fp'
 import { Children, cloneElement, Fragment, useEffect, useImperativeHandle, useRef, useState } from 'react'
-import { div, h } from 'react-hyperscript-helpers'
+import { div, h, hr } from 'react-hyperscript-helpers'
 import onClickOutside from 'react-onclickoutside'
 import { Clickable, FocusTrapper } from 'src/components/common'
 import { icon } from 'src/components/icons'
@@ -130,6 +130,13 @@ export const MenuButton = forwardRefWithName('MenuButton', ({ disabled, children
       hover: !disabled ? { backgroundColor: colors.light(0.4), color: colors.accent() } : undefined
     }, props), [children])
   ])
+})
+
+export const MenuDivider = () => hr({
+  style: {
+    borderWidth: '0 0 1px', borderStyle: 'solid', borderColor: colors.dark(0.55),
+    margin: '0.25rem 0'
+  }
 })
 
 export const MenuTrigger = ({ children, content, popupProps = {}, ...props }) => {
