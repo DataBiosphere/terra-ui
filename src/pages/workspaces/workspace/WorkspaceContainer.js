aaabbecf7dac0b277fed769cfc74a52e680498f6
Gabriela <gcarrill@broadinstitute.org>
2022-06-14 17:31:15 -0400

    fix

--- src/pages/workspaces/workspace/WorkspaceContainer.js
+++ src/pages/workspaces/workspace/WorkspaceContainer.js
@@ -293,10 +293,11 @@ export const wrapWorkspace = ({ breadcrumbs, activeTab, title, topBarContent, sh
     const prevGoogleProject = usePrevious(googleProject)
     const prevAzureContext = usePrevious(azureContext)
 
-    const { runtimes, refreshRuntimes, persistentDisks, appDataDisks } = useCloudEnvironmentPolling(googleProject, workspace?.workspace.namespace)
-    const { apps, refreshApps } = useAppPolling(googleProject, name)
     const isGoogleWorkspace = !!googleProject
     const isAzureWorkspace = !!azureContext
+    
+    const { runtimes, refreshRuntimes, persistentDisks, appDataDisks } = isGoogleWorkspace ? useCloudEnvironmentPolling(googleProject, workspace?.workspace.googleProject) : useCloudEnvironmentPolling(googleProject, workspace?.workspace.namespace)
+    const { apps, refreshApps } = useAppPolling(googleProject, name)
     // The following if statements are necessary to support the context bar properly loading runtimes for google/azure
     // Note that the refreshApps function currently is not supported for azure
     if (googleProject !== prevGoogleProject && isGoogleWorkspace) {
