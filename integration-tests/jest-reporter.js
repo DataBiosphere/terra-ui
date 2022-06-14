9b818afc7e02a0334657bc2dea4735c993dbe4f5
aweng98 <bweng@broadinstitute.org>
2022-06-14 15:45:35 -0400

    pr feedback

--- integration-tests/jest-reporter.js
+++ integration-tests/jest-reporter.js
@@ -15,8 +15,19 @@ module.exports = class JestReporter {
 
   onTestResult(_testRunConfig, testResult, _runResults) {
     const { testFilePath, testResults } = testResult
-    const { name } = parse(testFilePath)
-    const logFileName = `${this.getLogPath(testResults)}/${name}-${Date.now()}.log`
+    const { name: testName } = parse(testFilePath)
+    const hasFailure = _.some(({ status }) => status === 'failed', testResults)
+    const logDir = `${this.logRootDir}/${hasFailure ? 'failures' : 'successes'}`
+    const logFileName = `${logDir}/${testName}-${Date.now()}.log`
+
+    // Since Node 10, mkdirSync knows to not do anything if the dir exists (https://stackoverflow.com/a/71767385/2851999) so not checking explicitly
+    // We may want to specify what to do if an error occurs. Not sure if throwing is what we'd want.
+    try {
+      mkdirSync(logDir, { recursive: true })
+    } catch (err) {
+      console.error(err)
+      throw err
+    }
 
     const writableStream = createWriteStream(logFileName)
     writableStream.on('error', ({ message }) => console.error(`Error occurred while writing Console logs to ${logFileName}.\n${message}`))
@@ -40,34 +51,15 @@ module.exports = class JestReporter {
     }, testResults)
 
     writableStream.end()
-    writableStream.on('finish', () => console.log(`**  Saved ${name} results: ${logFileName}`))
+    writableStream.on('finish', () => console.log(`**  Saved ${testName} results: ${logFileName}`))
   }
 
   // Called after all tests have completed
   onRunComplete(_test, runResults) {
     // Save run summary to a file.
-    const logDir = this.getLogPath(runResults)
     const summaryFile = `tests-summary.json`
-    writeFileSync(`${logDir}/${summaryFile}`, JSON.stringify(runResults, null, 2))
-    console.log(`**  Saved all tests summary: ${logDir}/${summaryFile}`)
-    return runResults
-  }
-
-  getLogPath(result) {
-    const { testResults } = result
-    const hasFailure = _.some(({ status }) => status === 'failed', testResults)
-    const logDir = `${this.logRootDir}/${hasFailure ? 'failures' : 'successes'}`
-
-    // Since Node 10, mkdirSync knows to not do anything if the dir exists (https://stackoverflow.com/a/71767385/2851999) so not checking explicitly
-    // We may want to specify what to do if an error occurs. Not sure if throwing is what we'd want.
-    try {
-      mkdirSync(logDir, { recursive: true })
-    } catch (err) {
-      console.error(err)
-      throw err
-    }
-
-    return logDir
+    writeFileSync(`${this.logRootDir}/${summaryFile}`, JSON.stringify(runResults, null, 2))
+    console.log(`**  Saved all tests summary: ${this.logRootDir}/${summaryFile}`)
   }
 
   timeNow() {
--- integration-tests/utils/integration-utils.js
+++ integration-tests/utils/integration-utils.js
@@ -328,7 +328,7 @@ const logError = page => {
 
 // Emitted when an uncaught exception happens within the page
 const logPageError = page => {
-  const handle = msg => console.error('page.error', msg)
+  const handle = msg => console.error('page.pageerror', msg)
   page.on('pageerror', handle)
   return () => page.off('pageerror', handle)
 }
