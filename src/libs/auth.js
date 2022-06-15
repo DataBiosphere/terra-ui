7c6a618fc421ccae9961cf2daa8d412238de3bdd
Robert Title <rtitle@broadinstitute.org>
2022-06-15 11:47:33 -0400

    Break revokeTokens to a new method, and handle B2C case

--- src/libs/auth.js
+++ src/libs/auth.js
@@ -60,18 +60,27 @@ export const signOut = () => {
   cookieReadyStore.reset()
   sessionStorage.clear()
   const auth = getAuthInstance()
-  auth.revokeTokens()
-    // revokeTokens can fail if the the token has already been revoked.
-    // Recover from invalid_token errors to make sure signOut completes successfully.
-    .catch(e => {
-      if (e.error === 'invalid_token') {
-        return null
-      } else {
-        throw e
-      }
-    })
+  revokeTokens()
     .finally(() => auth.removeUser())
     .finally(() => auth.clearStaleState())
+}
+
+const revokeTokens = () => {
+  const auth = getAuthInstance()
+  if (auth.settings.metadata.revocation_endpoint) {
+    // revokeTokens can fail if the the token has already been revoked.
+    // Recover from invalid_token errors to make sure signOut completes successfully.
+    return auth.revokeTokens()
+      .catch(e => {
+        if (e.error === 'invalid_token') {
+          return null
+        } else {
+          throw e
+        }
+      })
+  } else {
+    return Promise.resolve()
+  }
 }
 
 const getSigninArgs = includeBillingScope => {
