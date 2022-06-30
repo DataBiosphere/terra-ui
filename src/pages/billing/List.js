eb6923bc4c01ca269208920a556876ae8336bf08
Marcus Talbott <mtalbott@broadinstitute.org>
2022-06-30 15:25:06 -0400

    don't display Google billing accounts that Terra doesn't have access to when creating billing projects

--- src/pages/billing/List.js
+++ src/pages/billing/List.js
@@ -309,6 +309,7 @@ export const BillingList = ({ queryParams: { selectedName } }) => {
   )(() => {
     if (Auth.hasBillingScope()) {
       return Ajax(signal).Billing.listAccounts()
+        .then(_.filter('firecloudHasAccess'))
         .then(_.keyBy('accountName'))
         .then(setBillingAccounts)
     }
