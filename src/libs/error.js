18858022a512716a30a2e2912e731e7d1143d5be
Robert Title <rtitle@broadinstitute.org>
2022-07-05 12:04:42 -0400

    [TOAZ-146] [B2C] Use separate policy for sensitive scopes from Terra UI (#3189)
    * TOAZ-146: [B2C] Use separate policy for sensitive scopes from Terra UI
    
    * Fixes for scope handling
    
    * Comment
    
    * Comment
    
    * Return empty object
    
    * Fix TopBar signIn function

--- src/libs/error.js
+++ src/libs/error.js
@@ -4,9 +4,12 @@ import { notify, sessionTimeoutProps } from 'src/libs/notifications'
 
 
 export const reportError = async (title, obj) => {
-  if (obj instanceof Response && obj.status === 401 && !(await reloadAuthToken())) {
-    notify('info', 'Session timed out', sessionTimeoutProps)
-    signOut()
+  if (obj instanceof Response && obj.status === 401) {
+    if (!await reloadAuthToken()) {
+      notify('info', 'Session timed out', sessionTimeoutProps)
+      signOut()
+    }
+    // Don't report an error if we've successfully reloaded the auth token
   } else {
     notify('error', title, { detail: await (obj instanceof Response ? obj.text() : obj) })
   }
