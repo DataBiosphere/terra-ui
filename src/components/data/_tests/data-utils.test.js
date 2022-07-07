98ec5523803321a30b88c1dcfd534da08ece11ac
Nick Watts <nwatts@broadinstitute.org>
2022-07-07 16:56:27 -0400

    Add tests for entityAttributeText

--- src/components/data/_tests/data-utils.test.js
+++ src/components/data/_tests/data-utils.test.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 
 import { render } from '@testing-library/react'
 import _ from 'lodash/fp'
-import { concatenateAttributeNames, convertAttributeValue, getAttributeType, prepareAttributeForUpload, renderDataCell } from 'src/components/data/data-utils.js'
+import { concatenateAttributeNames, convertAttributeValue, entityAttributeText, getAttributeType, prepareAttributeForUpload, renderDataCell } from 'src/components/data/data-utils'
 
 
 describe('concatenateAttributeNames', () => {
@@ -96,6 +96,63 @@ describe('getAttributeType', () => {
     expect(getAttributeType(null)).toEqual({ type: 'string', isList: false })
     expect(getAttributeType({ items: [], itemsType: 'AttributeValue' })).toEqual({ type: 'string', isList: true })
     expect(getAttributeType({ items: [null], itemsType: 'AttributeValue' })).toEqual({ type: 'string', isList: true })
+  })
+})
+
+describe('entityAttributeText', () => {
+  describe('basic data types', () => {
+    it('returns value for strings', () => {
+      expect(entityAttributeText('abc')).toEqual('abc')
+      expect(entityAttributeText({ items: ['a', 'b', 'c'], itemsType: 'AttributeValue' })).toEqual('a, b, c')
+    })
+
+    it('returns stringified value for numbers', () => {
+      expect(entityAttributeText(42)).toEqual(42)
+      expect(entityAttributeText({ items: [1, 2, 3], itemsType: 'AttributeValue' })).toEqual('1, 2, 3')
+    })
+
+    it('returns stringified value for booleans', () => {
+      expect(entityAttributeText(true)).toEqual(true)
+      expect(entityAttributeText(false)).toEqual(false)
+      expect(entityAttributeText({ items: [true, false], itemsType: 'AttributeValue' })).toEqual('true, false')
+    })
+
+    it('returns entity name for references', () => {
+      expect(entityAttributeText({ entityType: 'thing', entityName: 'thing_one' })).toEqual('thing_one')
+      expect(entityAttributeText({
+        items: [
+          { entityType: 'thing', entityName: 'thing_one' },
+          { entityType: 'thing', entityName: 'thing_two' }
+        ],
+        itemsType: 'EntityReference'
+      })).toEqual('thing_one, thing_two')
+    })
+  })
+
+  it('formats empty lists', () => {
+    expect(entityAttributeText({ items: [], itemsType: 'AttributeValue' })).toEqual('')
+  })
+
+  it('can return JSON encoded list items', () => {
+    expect(entityAttributeText({ items: ['a', 'b', 'c'], itemsType: 'AttributeValue' }, true)).toEqual('["a","b","c"]')
+    expect(entityAttributeText({ items: [1, 2, 3], itemsType: 'AttributeValue' }, true)).toEqual('[1,2,3]')
+    expect(entityAttributeText({
+      items: [
+        { entityType: 'thing', entityName: 'thing_one' },
+        { entityType: 'thing', entityName: 'thing_two' }
+      ],
+      itemsType: 'EntityReference'
+    }, true)).toEqual('[{"entityType":"thing","entityName":"thing_one"},{"entityType":"thing","entityName":"thing_two"}]')
+  })
+
+  describe('JSON values', () => {
+    it('stringifies arrays of objects', () => {
+      expect(entityAttributeText([
+        { key1: 'value1' },
+        { key2: 'value2' },
+        { key3: 'value3' }
+      ])).toEqual('[{"key1":"value1"},{"key2":"value2"},{"key3":"value3"}]')
+    })
   })
 })
 
