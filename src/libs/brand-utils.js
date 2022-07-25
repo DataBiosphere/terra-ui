c8e4697cea9233df7c2e2fb58fdf2dcdd8472094
Kaan Yuksel <kyuksel@broadinstitute.org>
2022-07-25 14:44:18 -0400

    Move pickBrandLogo

--- src/libs/brand-utils.js
+++ src/libs/brand-utils.js
@@ -1,3 +1,19 @@
+import anvilLogo from 'src/images/brands/anvil/ANVIL-Logo.svg'
+import anvilLogoWhite from 'src/images/brands/anvil/ANVIL-Logo-White.svg'
+import baselineLogo from 'src/images/brands/baseline/baseline-logo-color.svg'
+import baselineLogoWhite from 'src/images/brands/baseline/baseline-logo-white.svg'
+import bioDataCatalystLogo from 'src/images/brands/bioDataCatalyst/bioDataCatalyst-Logo-color.svg'
+import bioDataCatalystLogoWhite from 'src/images/brands/bioDataCatalyst/bioDataCatalyst-Logo-white.svg'
+import datastageLogo from 'src/images/brands/datastage/DataSTAGE-Logo.svg'
+import datastageLogoWhite from 'src/images/brands/datastage/DataSTAGE-Logo-White.svg'
+import elwaziLogo from 'src/images/brands/elwazi/elwazi-logo-color.svg'
+import elwaziLogoWhite from 'src/images/brands/elwazi/elwazi-logo-white.svg'
+import fcLogo from 'src/images/brands/firecloud/FireCloud-Logo.svg'
+import fcLogoWhite from 'src/images/brands/firecloud/FireCloud-Logo-White.svg'
+import projectSingularLogo from 'src/images/brands/projectSingular/project-singular-logo-black.svg'
+import projectSingularLogoWhite from 'src/images/brands/projectSingular/project-singular-logo-white.svg'
+import rareXLogo from 'src/images/brands/rareX/rarex-logo-color.svg'
+import rareXLogoWhite from 'src/images/brands/rareX/rarex-logo-white.svg'
 import { brands } from 'src/libs/brands'
 import { getConfig } from 'src/libs/config'
 import * as Utils from 'src/libs/utils'
@@ -12,7 +28,8 @@ export const isElwazi = () => (window.location.hostname === brands.elwazi.hostNa
 export const isFirecloud = () => (window.location.hostname === brands.firecloud.hostName) || getConfig().isFirecloud
 export const isProjectSingular = () => (window.location.hostname === brands.projectSingular.hostName) || getConfig().isProjectSingular
 export const isRareX = () => (window.location.hostname === brands.rareX.hostName) || getConfig().isRareX
-export const isTerra = () => (window.location.hostname === brands.terra.hostName) || getConfig().isTerra || (!isFirecloud() && !isDatastage() && !isAnvil() && !isBioDataCatalyst() && !isBaseline() && !isElwazi() && !isProjectSingular() && !isRareX())
+export const isTerra = () => (window.location.hostname === brands.terra.hostName) || getConfig().isTerra ||
+  (!isFirecloud() && !isDatastage() && !isAnvil() && !isBioDataCatalyst() && !isBaseline() && !isElwazi() && !isProjectSingular() && !isRareX())
 
 export const getEnabledBrand = () => Utils.cond(
   [isAnvil(), () => brands.anvil],
@@ -25,4 +42,15 @@ export const getEnabledBrand = () => Utils.cond(
   [isRareX(), () => brands.rareX],
   [isTerra(), () => brands.terra],
   () => brands.terra
+)
+
+export const pickBrandLogo = (color = false) => Utils.cond(
+  [isFirecloud(), () => color ? fcLogo : fcLogoWhite],
+  [isDatastage(), () => color ? datastageLogo : datastageLogoWhite],
+  [isAnvil(), () => color ? anvilLogo : anvilLogoWhite],
+  [isBioDataCatalyst(), () => color ? bioDataCatalystLogo : bioDataCatalystLogoWhite],
+  [isBaseline(), () => color ? baselineLogo : baselineLogoWhite],
+  [isElwazi(), () => color ? elwaziLogo : elwaziLogoWhite],
+  [isProjectSingular(), () => color ? projectSingularLogo : projectSingularLogoWhite],
+  [isRareX(), () => color ? rareXLogo : rareXLogoWhite]
 )
--- src/libs/logos.js
+++ src/libs/logos.js
@@ -1,40 +1,10 @@
 import { b, div, img } from 'react-hyperscript-helpers'
-import anvilLogo from 'src/images/brands/anvil/ANVIL-Logo.svg'
-import anvilLogoWhite from 'src/images/brands/anvil/ANVIL-Logo-White.svg'
-import baselineLogo from 'src/images/brands/baseline/baseline-logo-color.svg'
-import baselineLogoWhite from 'src/images/brands/baseline/baseline-logo-white.svg'
-import bioDataCatalystLogo from 'src/images/brands/bioDataCatalyst/bioDataCatalyst-Logo-color.svg'
-import bioDataCatalystLogoWhite from 'src/images/brands/bioDataCatalyst/bioDataCatalyst-Logo-white.svg'
-import datastageLogo from 'src/images/brands/datastage/DataSTAGE-Logo.svg'
-import datastageLogoWhite from 'src/images/brands/datastage/DataSTAGE-Logo-White.svg'
-import elwaziLogo from 'src/images/brands/elwazi/elwazi-logo-color.svg'
-import elwaziLogoWhite from 'src/images/brands/elwazi/elwazi-logo-white.svg'
-import fcLogo from 'src/images/brands/firecloud/FireCloud-Logo.svg'
-import fcLogoWhite from 'src/images/brands/firecloud/FireCloud-Logo-White.svg'
-import projectSingularLogo from 'src/images/brands/projectSingular/project-singular-logo-black.svg'
-import projectSingularLogoWhite from 'src/images/brands/projectSingular/project-singular-logo-white.svg'
-import rareXLogo from 'src/images/brands/rareX/rarex-logo-color.svg'
-import rareXLogoWhite from 'src/images/brands/rareX/rarex-logo-white.svg'
 import terraLogo from 'src/images/brands/terra/logo.svg'
 import terraLogoWhite from 'src/images/brands/terra/logo-grey.svg'
 import terraLogoShadow from 'src/images/brands/terra/logo-wShadow.svg'
-import {
-  getEnabledBrand, isAnvil, isBaseline, isBioDataCatalyst, isDatastage, isElwazi, isFirecloud, isProjectSingular, isRareX, isTerra
-} from 'src/libs/brand-utils'
+import { getEnabledBrand, isTerra, pickBrandLogo } from 'src/libs/brand-utils'
 import colors from 'src/libs/colors'
-import * as Utils from 'src/libs/utils'
 
-
-const pickBrandLogo = (color = false) => Utils.cond(
-  [isFirecloud(), () => color ? fcLogo : fcLogoWhite],
-  [isDatastage(), () => color ? datastageLogo : datastageLogoWhite],
-  [isAnvil(), () => color ? anvilLogo : anvilLogoWhite],
-  [isBioDataCatalyst(), () => color ? bioDataCatalystLogo : bioDataCatalystLogoWhite],
-  [isBaseline(), () => color ? baselineLogo : baselineLogoWhite],
-  [isElwazi(), () => color ? elwaziLogo : elwaziLogoWhite],
-  [isProjectSingular(), () => color ? projectSingularLogo : projectSingularLogoWhite],
-  [isRareX(), () => color ? rareXLogo : rareXLogoWhite]
-)
 
 export const terraLogoMaker = (logoVariant, style) => img({ alt: 'Terra', role: 'img', src: logoVariant, style })
 
