d138eeae768025f73175eff8eb7a1ae88db33f96
Kaan Yuksel <kyuksel@broadinstitute.org>
2022-08-05 16:34:57 -0400

    PR feedback: Be consistent with 1 vs. undefined

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -354,7 +354,7 @@ const TopBar = ({ showMenu = true, title, href, children }) => {
         ...styles.topBar,
         // This div contains the primary fallback color for a11y which will be used to calculate color contrast when the background is manually hidden
         // A 1.47 intensity results in precisely a 4.5:1 ratio against the white text
-        backgroundColor: isTerra() ? colors.primary(1.47) : colors.light(isRadX() ? 0.4 : undefined)
+        backgroundColor: isTerra() ? colors.primary(1.47) : colors.light(isRadX() ? 0.4 : 1)
       }
     }, [
       div({
