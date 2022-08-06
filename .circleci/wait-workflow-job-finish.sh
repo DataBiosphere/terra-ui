65317f80e38330ff80b372383c877f7e04d98056
aweng98 <bweng@broadinstitute.org>
2022-08-05 23:12:35 -0400

    exit instead break

--- .circleci/wait-workflow-job-finish.sh
+++ .circleci/wait-workflow-job-finish.sh
@@ -9,11 +9,11 @@ set -o pipefail
 # Wait up to 15 minutes for job to finish
 x=0
 NODE_INDEX=0
-while [[ $x -le 900 ]]; do
+while [[ $x -le 30 ]]; do
   # Find number of nodes still is running
   runningNodesCount=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM" --header 'Circle-Token: "'$CIRCLECI_TOKEN'"' | jq -r '.parallel_runs[] | select(.index != "'$NODE_INDEX'") | select(.status == "running")' | grep "running" | wc -l)
   if [[ $runningNodesCount -eq 0 ]]; then
-    break
+    exit 0
   fi
 
   sleep 10
