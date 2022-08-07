c6839024d2ab10246535a4fe6b9052d403a8a3b6
aweng98 <bweng@broadinstitute.org>
2022-08-06 21:02:32 -0400

    single bracket

--- .circleci/wait-workflow-job-finish.sh
+++ .circleci/wait-workflow-job-finish.sh
@@ -9,13 +9,13 @@ set -o pipefail
 # Wait up to 5 minutes for tests to finish on parallel run nodes except CIRCLE_NODE_INDEX=0: 0
 x=0
 EXCLUDE_NODE_INDEX=0
-while [[ $x -le 300 ]]; do
+while [ $x -le 300 ]; do
   # Find number of nodes still is running
   job_detail=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM" --header 'Circle-Token: "'$CIRCLECI_TOKEN'"')
   job_running_nodes_count=$(echo $job_detail | jq -r '.parallel_runs[] | select(.status == "running") | select(.index != '$EXCLUDE_NODE_INDEX')' | grep "running" | wc -l)
   echo $job_running_nodes_count
 
-  if [[ $job_running_nodes_count -eq 0 ]]; then
+  if [ $job_running_nodes_count -eq 0 ]; then
     echo "$job_running_nodes_count -eq 0"
     exit 0
   fi
