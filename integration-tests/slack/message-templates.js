e8b1b2452dbb0b593b262cf704e1b4bf2fcd5f0c
aweng98 <bweng@broadinstitute.org>
2022-08-08 15:13:58 -0400

    ui integration test slack message templates

---
+++ integration-tests/slack/message-templates.js
@@ -0,0 +1,49 @@
+const {
+  CIRCLE_JOB,
+  CIRCLE_BUILD_NUM,
+  CIRCLE_SHA1
+} = process.env
+
+const getHeaderBlock = numFailedTests => {
+  if (numFailedTests === 0) {
+    return {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: ':circleci-pass:  Terra-UI Test has Passed on CircleCI'
+      }
+    }
+  }
+
+  return {
+    type: 'header',
+    text: {
+      type: 'plain_text',
+      text: ':circleci-fail:  Terra-UI Test has Failed on CircleCI'
+    }
+  }
+}
+
+const getJobDetailBlock = () => {
+  return {
+    type: 'section',
+    fields: [
+      {
+        type: 'mrkdwn',
+        text: `*Job ${CIRCLE_BUILD_NUM}*: <https://circleci.com/gh/DataBiosphere/terra-ui/${CIRCLE_BUILD_NUM} | ${CIRCLE_JOB}>`
+      },
+      {
+        type: 'mrkdwn',
+        text: `*Commit*: <https://github.com/DataBiosphere/terra-ui/commit/${CIRCLE_SHA1} | ${`${CIRCLE_SHA1}`.slice(0, 7)}>`
+      }
+    ]
+  }
+}
+
+// any non-zero number to indicate test(s) has failed in circleci job
+const getMessageBlockTemplate = (numFailedTests = 0) => [
+  getHeaderBlock(numFailedTests),
+  getJobDetailBlock()
+]
+
+module.exports = { getMessageBlockTemplate }
