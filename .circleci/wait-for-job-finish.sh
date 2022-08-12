52e576b66287eee9c98fc7c5abfd5763e516ff31
aweng98 <bweng@broadinstitute.org>
2022-08-12 12:45:17 -0400

    PR feedback

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -6,29 +6,26 @@ set -o nounset
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-x=0
-EXCLUDE_NODE_INDEX=0
+counter=0
 
 # Wait up to 5 minutes for job to finish. A job can run on multiple nodes: parallelism > 1
-while [ $x -le 300 ]; do
+while [ $counter -le 300 ]; do
   # Find number of nodes in running
   job_detail=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM" --header 'Circle-Token: "'$CIRCLECI_USER_TOKEN'"')
-  job_running_nodes_count=$(echo $job_detail | jq -r '.parallel_runs[] | select(.status == "running") | select(.index != '$EXCLUDE_NODE_INDEX')' | grep "running" | wc -l)
-  echo "job_running_nodes_count: $job_running_nodes_count"
+  job_running_nodes_count=$(echo $job_detail | jq -r '.parallel_runs[] | select(.status == "running") | select(.index != '$CIRCLE_NODE_INDEX')' | grep "running" | wc -l)
 
   if [ $job_running_nodes_count -eq 0 ]; then
     exit 0
   fi
 
-  echo "sleep 10 seconds"
   sleep 10
-  x=$(($x + 10))
+  counter=$(($counter + 10))
 done
 
-echo "Waited total $x seconds"
+echo "Waited total $counter seconds"
 date
 
 # Something is wrong. Log response for error troubleshooting
 curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM" --header 'Circle-Token: "'$CIRCLECI_USER_TOKEN'"' | jq -r '.'
-echo "ERROR: Exceeded maximum waiting time 5 minutes is exceeded."
+echo "ERROR: Exceeded maximum waiting time 5 minutes."
 exit 1
