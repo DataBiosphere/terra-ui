d607a878044f2846e277d8166dc9083c1d29e4d4
Nick Watts <1156625+nawatts@users.noreply.github.com>
2022-08-29 11:22:55 -0400

    [SU-172] Add provenance information for data table columns (#3332)
    * Add provenance information for data table columns
    
    * Change div to span in spinner to allow use in phrasing content
    
    * Add feature flag
    
    * Only load provenance once it's requested
    
    * Remove cancel button from modal
    
    * Fix column actions menu when table is not editable
    
    * Clarify transformations
    
    * Replace _.omit with spread

--- src/libs/config.js
+++ src/libs/config.js
@@ -13,4 +13,5 @@ export const getConfig = () => {
  */
 export const isCromwellAppVisible = () => getConfig().isCromwellAppVisible
 export const isDataBrowserFrontPage = () => false
+export const isDataTableProvenanceEnabled = () => getConfig().isDataTableProvenanceEnabled
 export const isDataTableVersioningEnabled = () => getConfig().isDataTableVersioningEnabled
---
+++ src/libs/data-table-provenance.js
@@ -0,0 +1,90 @@
+import _ from 'lodash/fp'
+import { useCallback, useState } from 'react'
+import { Ajax } from 'src/libs/ajax'
+import { reportError } from 'src/libs/error'
+import { useCancellation } from 'src/libs/react-utils'
+import * as Utils from 'src/libs/utils'
+
+
+export const maxSubmissionsQueriedForProvenance = 25
+
+const getSubmissionsWithRootEntityType = async (workspace, entityType, { signal } = {}) => {
+  const { workspace: { namespace, name } } = workspace
+
+  // The root entity type is stored on method configurations, not submissions. To avoid fetching
+  // every submission's configuration, first use the list submissions endpoint and filter by
+  // submission entity type.
+  //
+  // If a workflow is run on a single entity, the submission entity type will be the root entity type.
+  // If a workflow is run on multiple entities, the submission entity type will be a set of the root entity type.
+  //
+  // After fetching method configurations, another filter is required remove submissions whose root entity type
+  // was a set of the desired entity type.
+  const allSubmissions = await Ajax(signal).Workspaces.workspace(namespace, name).listSubmissions()
+  const submissionsMaybeUsingEntityType = _.flow(
+    _.filter(submission => {
+      const submissionEntityType = submission.submissionEntity.entityType
+      return submissionEntityType === entityType || submissionEntityType === `${entityType}_set`
+    }),
+    _.slice(0, maxSubmissionsQueriedForProvenance)
+  )(allSubmissions)
+
+  const submissionConfigurations = await Promise.all(
+    _.map(
+      submission => Ajax(signal).Workspaces.workspace(namespace, name).submission(submission.submissionId).getConfiguration(),
+      submissionsMaybeUsingEntityType
+    )
+  )
+
+  return _.flow(
+    _.map(([submission, configuration]) => ({ submission, configuration })),
+    _.filter(({ configuration }) => configuration.rootEntityType === entityType),
+    _.orderBy(({ submission }) => new Date(submission.submissionDate), 'desc')
+  )(_.zip(submissionsMaybeUsingEntityType, submissionConfigurations))
+}
+
+const getColumnFromOutputExpression = expr => {
+  const match = /^this\.([^.]+)$/.exec(expr)
+  return match ? match[1] : null
+}
+
+const getColumnProvenance = async (workspace, entityType, { signal } = {}) => {
+  const submissions = await getSubmissionsWithRootEntityType(workspace, entityType, { signal })
+
+  return _.flow(
+    _.flatMap(submission => _.map(([output, expression]) => ({ ...submission, output, expression }), _.toPairs(submission.configuration.outputs))),
+    _.map(({ expression, ...output }) => ({ ...output, column: getColumnFromOutputExpression(expression) })),
+    _.filter(output => !!output.column),
+    _.reduce((acc, { submission, configuration, output, column }) => {
+      const provenanceEntry = { submissionId: submission.submissionId, submissionDate: submission.submissionDate, configuration, output }
+      return _.update(column, _.flow(_.defaultTo([]), Utils.append(provenanceEntry)), acc)
+    }, {})
+  )(submissions)
+}
+
+export const useColumnProvenance = (workspace, entityType) => {
+  const signal = useCancellation()
+  const [loading, setLoading] = useState(false)
+  const [columnProvenance, setColumnProvenance] = useState(null)
+  const [error, setError] = useState(null)
+
+  const loadColumnProvenance = useCallback(async () => {
+    setError(null)
+    setLoading(true)
+    try {
+      setColumnProvenance(await getColumnProvenance(workspace, entityType, { signal }))
+    } catch (error) {
+      setError(error)
+      reportError('Error loading column provenance', error)
+    } finally {
+      setLoading(false)
+    }
+  }, [workspace, entityType, signal])
+
+  return {
+    columnProvenance,
+    loading,
+    error,
+    loadColumnProvenance: loading ? _.noop : () => { loadColumnProvenance() }
+  }
+}
