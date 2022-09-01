12d7bc81146043a70192e185fbbe1b632d15cf9b
aweng98 <bweng@broadinstitute.org>
2022-09-01 11:08:51 -0400

    pr feedback

--- integration-tests/utils/integration-utils.js
+++ integration-tests/utils/integration-utils.js
@@ -109,11 +109,7 @@ const click = async (page, xpath, options) => {
   try {
     return (await page.waitForXPath(xpath, defaultToVisibleTrue(options))).click()
   } catch (e) {
-    // Handle Error: Node is detached from document
-    //   at ElementHandle._scrollIntoViewIfNeeded (/mnt/ramdisk/.yarn/unplugged/puppeteer-npm-13.5.1-231e586c7a/node_modules/puppeteer/src/common/JSHandle.ts:475:22)
-    //   at processTicksAndRejections (node:internal/process/task_queues:96:5)
-    //   at ElementHandle.click (/mnt/ramdisk/.yarn/unplugged/puppeteer-npm-13.5.1-231e586c7a/node_modules/puppeteer/src/common/JSHandle.ts:617:5)
-    if (e instanceof Error && e.message?.includes('Node is detached from document')) {
+    if (e.message.includes('Node is detached from document')) {
       return (await page.waitForXPath(xpath, defaultToVisibleTrue(options))).click()
     }
     throw e
