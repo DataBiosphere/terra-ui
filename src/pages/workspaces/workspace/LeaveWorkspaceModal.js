1108c501dd6b4f3df61fabe21edebdaaa95c9fa4
Matt Bemis <mbemis@broadinstitute.org>
2022-09-07 16:19:35 -0400

    fix react warning

--- src/pages/workspaces/workspace/LeaveWorkspaceModal.js
+++ src/pages/workspaces/workspace/LeaveWorkspaceModal.js
@@ -5,24 +5,31 @@ import { icon } from 'src/components/icons'
 import Modal from 'src/components/Modal'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
-import { withErrorReportingInModal } from 'src/libs/error'
+import { reportError } from 'src/libs/error'
+import Events from 'src/libs/events'
 
 
 const LeaveWorkspaceModal = ({ workspace: { workspace: { workspaceId } }, onDismiss, onSuccess }) => {
   const [leaving, setLeaving] = useState(false)
   const helpText = 'Leave Workspace'
 
-  const onFailureDismiss = () => {
-    setLeaving(false)
-    onDismiss()
+  const leaveWorkspace = async () => {
+    try {
+      setLeaving(true)
+      await Ajax().Workspaces.leave(workspaceId)
+      Ajax().Metrics.captureEvent(Events.workspaceShare, { workspaceId })
+      setLeaving(false)
+      onDismiss()
+      onSuccess()
+    } catch (error) {
+      const { message } = await error.json()
+      Ajax().Metrics.captureEvent(Events.workspaceLeave, { workspaceId, errorMessage: message })
+      reportError(message)
+      setLeaving(false)
+      onDismiss()
+    }
   }
 
-  const leaveWorkspace = withErrorReportingInModal('Error leaving workspace', onFailureDismiss, async () => {
-    setLeaving(true)
-    await Ajax().Workspaces.leave(workspaceId)
-    onDismiss()
-    onSuccess()
-  })
   return h(Modal, {
     title: span({ style: { display: 'flex', alignItems: 'center' } }, [
       icon('warning-standard', { size: 24, color: colors.warning() }),
