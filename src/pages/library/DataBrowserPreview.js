8791f7d99466b04a2281990a55a68bbaf987c1a6
Sky Rubenstein <blrubenstein@wpi.edu>
2022-09-07 11:46:28 -0400

    Add typeof object to the preview

--- src/pages/library/DataBrowserPreview.js
+++ src/pages/library/DataBrowserPreview.js
@@ -83,6 +83,7 @@ const DataBrowserPreview = ({ id }) => {
           style: { fontSize: 16, textTransform: 'none' },
           onClick: () => setViewJSON({ title: `${table}, Row ${rowIndex} - ${cellKey}`, cellData: maybeJSON })
         }, ['View JSON'])],
+        [typeof cellContent === 'object', () => JSON.stringify(cellContent)],
         [Utils.DEFAULT, () => cellContent ? cellContent.toString() : '']
       )
     }
