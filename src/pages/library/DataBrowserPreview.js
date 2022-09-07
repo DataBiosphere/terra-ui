13e29cff8c649cbdd6ac454e2d66aeef99c9337e
Sky Rubenstein <blrubenstein@wpi.edu>
2022-09-07 10:28:07 -0400

    Add toString

--- src/pages/library/DataBrowserPreview.js
+++ src/pages/library/DataBrowserPreview.js
@@ -83,7 +83,7 @@ const DataBrowserPreview = ({ id }) => {
           style: { fontSize: 16, textTransform: 'none' },
           onClick: () => setViewJSON({ title: `${table}, Row ${rowIndex} - ${cellKey}`, cellData: maybeJSON })
         }, ['View JSON'])],
-        [Utils.DEFAULT, () => cellContent]
+        [Utils.DEFAULT, () => cellContent ? cellContent.toString() : '']
       )
     }
 
