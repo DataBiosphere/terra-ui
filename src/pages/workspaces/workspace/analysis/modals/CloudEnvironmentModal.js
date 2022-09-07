9c1a6b04ac7208544a9c3ce0b2a8cb3efee848a3
Justin Canas <justincanas16@gmail.com>
2022-09-06 17:12:00 -0700

    [IA-3589][IA-3648] Small UX changes (#3344)
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- src/pages/workspaces/workspace/analysis/modals/CloudEnvironmentModal.js
+++ src/pages/workspaces/workspace/analysis/modals/CloudEnvironmentModal.js
@@ -130,7 +130,7 @@ export const CloudEnvironmentModal = ({
       style: { ...toolButtonStyles, color: onClick && !disabled ? colors.accent() : colors.dark(0.3), ...style },
       onClick, disabled, ...props
     }, [
-      icon(shape, { size: 20 }),
+      icon(shape, { style: { marginBottom: '.25rem' }, size: 20 }),
       ...messageChildren
     ])
   }
@@ -170,8 +170,7 @@ export const CloudEnvironmentModal = ({
     shape: 'pause',
     toolLabel,
     disabled: true,
-    messageChildren: [span('Pause'),
-      span('Environment')],
+    messageChildren: [span('Pause')],
     tooltip: 'No Environment found',
     style: { borderColor: colors.dark(0.3) }
   })
@@ -194,8 +193,7 @@ export const CloudEnvironmentModal = ({
           toolLabel,
           onClick: () => startApp(toolLabel),
           disabled: busy || !canCompute,
-          messageChildren: [span('Resume'),
-            span('Environment')],
+          messageChildren: [span('Resume')],
           tooltip: canCompute ? 'Resume Environment' : noCompute
         })
       case 'Running':
@@ -204,8 +202,7 @@ export const CloudEnvironmentModal = ({
           toolLabel,
           onClick: () => stopApp(toolLabel),
           disabled: busy || !canCompute,
-          messageChildren: [span('Pause'),
-            span('Environment')],
+          messageChildren: [span('Pause')],
           tooltip: canCompute ? 'Pause Environment' : noCompute
         })
       case 'Starting':
@@ -222,7 +219,7 @@ export const CloudEnvironmentModal = ({
           toolLabel,
           disabled: true,
           tooltip: 'Environment update in progress',
-          messageChildren: [span('Environment'), span(getComputeStatusForDisplay(status))],
+          messageChildren: [span(getComputeStatusForDisplay(status))],
           style: { color: colors.dark(0.7) }
         })
       case 'Error':
@@ -367,15 +364,14 @@ export const CloudEnvironmentModal = ({
             onClick: () => setViewMode(toolLabel)
           }, [
             icon('cog', { size: 20 }),
-            span('Environment'),
-            span('Settings')
+            span({ style: { marginTop: '.25rem' } }, ['Settings'])
           ]),
           // Status button with stop/start functionality
           renderStatusClickable(toolLabel),
           // Launch
           h(Clickable, { ...getToolLaunchClickableProps(toolLabel) }, [
             icon('rocket', { size: 20 }),
-            span('Open')
+            span({ style: { marginTop: '.25rem' } }, ['Open'])
           ])
         ])
       ])
