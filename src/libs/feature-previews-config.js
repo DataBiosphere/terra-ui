15e7242e9f9994eb66c625ac240d1306c5046969
Nick Watts <nwatts@broadinstitute.org>
2022-09-15 11:31:48 -0400

    Add group for data provenance and versioning previews

--- src/libs/feature-previews-config.js
+++ src/libs/feature-previews-config.js
@@ -2,12 +2,14 @@ const featurePreviewsConfig = [
   {
     id: 'data-table-versioning',
     title: 'Data Table Versioning',
-    description: 'Enabling this feature will allow you to save uniquely named versions of data tables. These saved versions will appear in the Data tab and can be restored at any time.'
+    description: 'Enabling this feature will allow you to save uniquely named versions of data tables. These saved versions will appear in the Data tab and can be restored at any time.',
+    groups: ['preview-data-versioning-and-provenance']
   },
   {
     id: 'data-table-provenance',
     title: 'Data Table Provenance',
-    description: 'Enabling this feature will allow you to view information about the workflow that generated data table columns and files.'
+    description: 'Enabling this feature will allow you to view information about the workflow that generated data table columns and files.',
+    groups: ['preview-data-versioning-and-provenance']
   }
 ]
 
