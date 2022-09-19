90ed8faab9c11cfac1f77cbd0404a6d14acd4f65
Nick Watts <nwatts@broadinstitute.org>
2022-09-19 17:19:07 -0400

    Add option to toggle submission notifications from workspace dashboard

---
+++ src/pages/workspaces/workspace/Dashboard.test.js
@@ -0,0 +1,84 @@
+import '@testing-library/jest-dom'
+
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils'
+import { h } from 'react-hyperscript-helpers'
+import { Ajax } from 'src/libs/ajax'
+import { authStore } from 'src/libs/state'
+import { WorkspaceNotifications } from 'src/pages/workspaces/workspace/Dashboard'
+
+
+jest.mock('src/libs/ajax')
+
+describe('WorkspaceNotifications', () => {
+  const testWorkspace = { workspace: { namespace: 'test', name: 'test' } }
+
+  afterEach(() => {
+    authStore.reset()
+  })
+
+  it.each([
+    {
+      profile: {
+        'notifications/SuccessfulSubmissionNotification/test/test': 'true',
+        'notifications/FailedSubmissionNotification/test/test': 'true',
+        'notifications/AbortedSubmissionNotification/test/test': 'true'
+      },
+      expectedState: true
+    },
+    {
+      profile: {},
+      expectedState: true
+    },
+    {
+      profile: {
+        'notifications/SuccessfulSubmissionNotification/test/test': 'false',
+        'notifications/FailedSubmissionNotification/test/test': 'false',
+        'notifications/AbortedSubmissionNotification/test/test': 'false'
+      },
+      expectedState: false
+    }
+  ])('renders checkbox with submission notifications status', ({ profile, expectedState }) => {
+    authStore.set({ profile })
+
+    const { getByLabelText } = render(h(WorkspaceNotifications, { workspace: testWorkspace }))
+    const submissionNotificationsCheckbox = getByLabelText('Submission notifications')
+    expect(submissionNotificationsCheckbox.getAttribute('aria-checked')).toBe(`${expectedState}`)
+  })
+
+  it('updates preferences when checkbox is clicked', async () => {
+    const user = userEvent.setup()
+
+    const setPreferences = jest.fn().mockReturnValue(Promise.resolve())
+    Ajax.mockImplementation(() => ({
+      Metrics: {
+        captureEvent: jest.fn()
+      },
+      User: {
+        profile: {
+          get: jest.fn().mockReturnValue(Promise.resolve({ keyValuePairs: [] })),
+          setPreferences
+        }
+      }
+    }))
+
+    authStore.set({
+      profile: {
+        'notifications/SuccessfulSubmissionNotification/test/test': 'false',
+        'notifications/FailedSubmissionNotification/test/test': 'false',
+        'notifications/AbortedSubmissionNotification/test/test': 'false'
+      }
+    })
+
+    const { getByLabelText } = render(h(WorkspaceNotifications, { workspace: testWorkspace }))
+    const submissionNotificationsCheckbox = getByLabelText('Submission notifications')
+
+    await act(() => user.click(submissionNotificationsCheckbox))
+    expect(setPreferences).toHaveBeenCalledWith({
+      'notifications/SuccessfulSubmissionNotification/test/test': 'true',
+      'notifications/FailedSubmissionNotification/test/test': 'true',
+      'notifications/AbortedSubmissionNotification/test/test': 'true'
+    })
+  })
+})
