82542a8fc72327049b7b6ec961b655331e8eca44
Nick Watts <nwatts@broadinstitute.org>
2022-09-21 15:03:37 -0400

    Add type definitions for react-hyperscript-helpers

---
+++ types/react-hyperscript-helpers.types.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import { PropsWithChildren, ReactElement } from 'react'
+import { div, h } from 'react-hyperscript-helpers'
+
+
+// Empty element
+div()
+
+// Props only
+div({ 'aria-label': 'label', 'data-testid': 'test-id', className: 'a-class', style: { display: 'flex' } })
+
+// Children only
+div(['Content'])
+div([div()])
+div([false])
+
+// Props and children
+div({ style: { display: 'flex' } }, ['Content'])
+div({ className: 'a-class' }, [div()])
+
+interface TestComponentProps {
+  stringProp: string
+  optionalNumberProp?: number
+}
+
+const TestComponent = (props: PropsWithChildren<TestComponentProps>) => div()
+
+// Props
+h(TestComponent, { stringProp: 'value' })
+h(TestComponent, { stringProp: 'value', optionalNumberProp: 1 })
+
+// Props and children
+h(TestComponent, { stringProp: 'value' }, [div()])
+h(TestComponent, { stringProp: 'value' }, ['Content'])
+
+// Children
+h(TestComponent, [div()])
+h(TestComponent, [div(), div()])
+h(TestComponent, ['Content'])
+
+// Component that takes a function as a child
+interface FunctionChildComponentProps {
+  children: (args: { value: string }) => ReactElement
+}
+
+const FunctionChildComponent = (props: FunctionChildComponentProps) => props.children({ value: 'test' })
+
+h(FunctionChildComponent, [(args: { value: string }) => div()])
--- types/react-hyperscript-helpers/index.d.ts
+++ types/react-hyperscript-helpers/index.d.ts
@@ -1,161 +1,181 @@
+/* eslint-disable no-unused-vars */
+
 import type * as React from 'react'
 
 
 declare module 'react-hyperscript-helpers' {
   type Component<P> = React.FunctionComponent<P> | React.ComponentClass<P>
-  type ComponentOrTag<P> = Component<P> | string
-  type Props<P> = React.Attributes & P | null
-  type Children = React.ReactNode[]
 
-  export function h<P extends {}>(type: ComponentOrTag<P>, selector: string, props: Props<P>, children: Children): React.ReactElement<P>
-  export function h<P extends {}>(type: ComponentOrTag<P>, selectorOrProps: string | Props<P>, children: Children): React.ReactElement<P>
-  export function h<P extends {}>(type: ComponentOrTag<P>, selectorOrPropsOrChildren?: string | Props<P> | Children): React.ReactElement<P>
+  type Children<Props> = Props extends { children?: React.ReactNode } ? React.ReactNode[] : (Props extends { children?: unknown } ? [Props['children']] : never)
 
-  interface htmlHelper<AttributesType extends React.HTMLAttributes<ElementType>, ElementType extends HTMLElement> {
-    (selector: string, props: React.DetailedHTMLProps<AttributesType, ElementType>, children: Children): React.DetailedReactHTMLElement<AttributesType, ElementType>
-    (selectorOrProps: string | React.DetailedHTMLProps<AttributesType, ElementType>, children: Children): React.DetailedReactHTMLElement<AttributesType, ElementType>
-    (selectorOrPropsOrChildren?: string | React.DetailedHTMLProps<AttributesType, ElementType> | Children): React.DetailedReactHTMLElement<AttributesType, ElementType>
+  interface hHelper {
+    <Props extends {}>(
+      component: Component<Props>,
+      props: Omit<Props, 'children'>,
+      children: Children<Props>
+    ): React.ReactElement<any, any>;
+
+    <Props extends {}>(
+      component: Component<Props>,
+      propsOrChildren: Props extends { children?: unknown } ? Props | Children<Props> : Props
+    ): React.ReactElement<any, any>;
+
+    <Props extends {}>(component: Component<Props>): React.ReactElement<any, any>;
   }
 
-  type SVGElementProps = ClassAttributes<SVGElement> & SVGAttributes<SVGElement> | null
+  export const h: hHelper
 
-  interface svgHelper {
-    (selector: string, props: SVGElementProps, children: Children): React.ReactSVGElement
-    (selectorOrProps: string | SVGElementProps, children: Children): React.ReactSVGElement
-    (selectorOrPropsOrChildren?: string | SVGElementProps | Children): React.ReactSVGElement
+  type TagName = keyof JSX.IntrinsicElements
+
+  type HTMLElementProps<T extends TagName> = JSX.IntrinsicElements[T]
+
+  type DataAttributeKey = `data-${string}`
+
+  type WithDataAttributes<T> = T & {
+    [dataAttribute: DataAttributeKey]: string;
   }
 
-  // TODO: Use more specific attribute/element types
-  export const a: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const abbr: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const address: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const area: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const article: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const aside: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const audio: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const b: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const base: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const bdi: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const bdo: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const big: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const blockquote: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const body: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const br: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const button: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const canvas: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const caption: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const cite: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const code: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const col: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const colgroup: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const data: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const datalist: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const dd: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const del: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const details: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const dfn: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const dialog: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const div: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const dl: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const dt: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const em: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const embed: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const fieldset: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const figcaption: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const figure: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const footer: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const form: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h1: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h2: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h3: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h4: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h5: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const h6: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const head: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const header: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const hgroup: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const hr: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const html: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const i: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const iframe: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const img: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const input: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const ins: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const kbd: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const keygen: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const label: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const legend: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const li: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const link: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const main: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const map: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const mark: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const menu: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const menuitem: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const meta: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const meter: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const nav: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const noscript: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const object: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const ol: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const optgroup: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const option: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const output: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const p: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const param: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const picture: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const pre: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const progress: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const q: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const rp: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const rt: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const ruby: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const s: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const samp: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const script: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const section: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const select: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const small: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const source: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const span: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const strong: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const style: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const sub: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const summary: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const sup: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const table: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const tbody: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const td: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const textarea: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const tfoot: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const th: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const thead: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const time: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const title: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const tr: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const track: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const u: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const ul: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const video: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
-  export const wbr: htmlHelper<React.HTMLAttributes<HTMLElement>, HTMLElement>
+  type htmlHelper<T extends TagName> = {
+    (
+      props: Omit<WithDataAttributes<HTMLElementProps<T>>, 'children'>,
+      children: React.ReactNode[]
+    ): React.ReactElement<any, any>
 
-  export const circle: svgHelper
-  export const clipPath: svgHelper
-  export const defs: svgHelper
-  export const ellipse: svgHelper
-  export const g: svgHelper
-  export const image: svgHelper
-  export const line: svgHelper
-  export const linearGradient: svgHelper
-  export const mask: svgHelper
-  export const path: svgHelper
-  export const pattern: svgHelper
-  export const polygon: svgHelper
-  export const polyline: svgHelper
-  export const radialGradient: svgHelper
-  export const rect: svgHelper
-  export const stop: svgHelper
-  export const svg: svgHelper
-  export const text: svgHelper
-  export const tspan: svgHelper
+    (
+      propsOrChildren?: WithDataAttributes<HTMLElementProps<T>> | React.ReactNode[]
+    ): React.ReactElement<any, any>
+  }
+
+  export const a: htmlHelper<'a'>
+  export const abbr: htmlHelper<'abbr'>
+  export const address: htmlHelper<'address'>
+  export const area: htmlHelper<'area'>
+  export const article: htmlHelper<'article'>
+  export const aside: htmlHelper<'aside'>
+  export const audio: htmlHelper<'audio'>
+  export const b: htmlHelper<'b'>
+  export const base: htmlHelper<'base'>
+  export const bdi: htmlHelper<'bdi'>
+  export const bdo: htmlHelper<'bdo'>
+  export const big: htmlHelper<'big'>
+  export const blockquote: htmlHelper<'blockquote'>
+  export const body: htmlHelper<'body'>
+  export const br: htmlHelper<'br'>
+  export const button: htmlHelper<'button'>
+  export const canvas: htmlHelper<'canvas'>
+  export const caption: htmlHelper<'caption'>
+  export const cite: htmlHelper<'cite'>
+  export const code: htmlHelper<'code'>
+  export const col: htmlHelper<'col'>
+  export const colgroup: htmlHelper<'colgroup'>
+  export const data: htmlHelper<'data'>
+  export const datalist: htmlHelper<'datalist'>
+  export const dd: htmlHelper<'dd'>
+  export const del: htmlHelper<'del'>
+  export const details: htmlHelper<'details'>
+  export const dfn: htmlHelper<'dfn'>
+  export const dialog: htmlHelper<'dialog'>
+  export const div: htmlHelper<'div'>
+  export const dl: htmlHelper<'dl'>
+  export const dt: htmlHelper<'dt'>
+  export const em: htmlHelper<'em'>
+  export const embed: htmlHelper<'embed'>
+  export const fieldset: htmlHelper<'fieldset'>
+  export const figcaption: htmlHelper<'figcaption'>
+  export const figure: htmlHelper<'figure'>
+  export const footer: htmlHelper<'footer'>
+  export const form: htmlHelper<'form'>
+  export const h1: htmlHelper<'h1'>
+  export const h2: htmlHelper<'h2'>
+  export const h3: htmlHelper<'h3'>
+  export const h4: htmlHelper<'h4'>
+  export const h5: htmlHelper<'h5'>
+  export const h6: htmlHelper<'h6'>
+  export const head: htmlHelper<'head'>
+  export const header: htmlHelper<'header'>
+  export const hgroup: htmlHelper<'hgroup'>
+  export const hr: htmlHelper<'hr'>
+  export const html: htmlHelper<'html'>
+  export const i: htmlHelper<'i'>
+  export const iframe: htmlHelper<'iframe'>
+  export const img: htmlHelper<'img'>
+  export const input: htmlHelper<'input'>
+  export const ins: htmlHelper<'ins'>
+  export const kbd: htmlHelper<'kbd'>
+  export const keygen: htmlHelper<'keygen'>
+  export const label: htmlHelper<'label'>
+  export const legend: htmlHelper<'legend'>
+  export const li: htmlHelper<'li'>
+  export const link: htmlHelper<'link'>
+  export const main: htmlHelper<'main'>
+  export const map: htmlHelper<'map'>
+  export const mark: htmlHelper<'mark'>
+  export const menu: htmlHelper<'menu'>
+  export const menuitem: htmlHelper<'menuitem'>
+  export const meta: htmlHelper<'meta'>
+  export const meter: htmlHelper<'meter'>
+  export const nav: htmlHelper<'nav'>
+  export const noscript: htmlHelper<'noscript'>
+  export const object: htmlHelper<'object'>
+  export const ol: htmlHelper<'ol'>
+  export const optgroup: htmlHelper<'optgroup'>
+  export const option: htmlHelper<'option'>
+  export const output: htmlHelper<'output'>
+  export const p: htmlHelper<'p'>
+  export const param: htmlHelper<'param'>
+  export const picture: htmlHelper<'picture'>
+  export const pre: htmlHelper<'pre'>
+  export const progress: htmlHelper<'progress'>
+  export const q: htmlHelper<'q'>
+  export const rp: htmlHelper<'rp'>
+  export const rt: htmlHelper<'rt'>
+  export const ruby: htmlHelper<'ruby'>
+  export const s: htmlHelper<'s'>
+  export const samp: htmlHelper<'samp'>
+  export const script: htmlHelper<'script'>
+  export const section: htmlHelper<'section'>
+  export const select: htmlHelper<'select'>
+  export const small: htmlHelper<'small'>
+  export const source: htmlHelper<'source'>
+  export const span: htmlHelper<'span'>
+  export const strong: htmlHelper<'strong'>
+  export const style: htmlHelper<'style'>
+  export const sub: htmlHelper<'sub'>
+  export const summary: htmlHelper<'summary'>
+  export const sup: htmlHelper<'sup'>
+  export const table: htmlHelper<'table'>
+  export const tbody: htmlHelper<'tbody'>
+  export const td: htmlHelper<'td'>
+  export const textarea: htmlHelper<'textarea'>
+  export const tfoot: htmlHelper<'tfoot'>
+  export const th: htmlHelper<'th'>
+  export const thead: htmlHelper<'thead'>
+  export const time: htmlHelper<'time'>
+  export const title: htmlHelper<'title'>
+  export const tr: htmlHelper<'tr'>
+  export const track: htmlHelper<'track'>
+  export const u: htmlHelper<'u'>
+  export const ul: htmlHelper<'ul'>
+  export const video: htmlHelper<'video'>
+  export const wbr: htmlHelper<'wbr'>
+
+  export const circle: htmlHelper<'circle'>
+  export const clipPath: htmlHelper<'clipPath'>
+  export const defs: htmlHelper<'defs'>
+  export const ellipse: htmlHelper<'ellipse'>
+  export const g: htmlHelper<'g'>
+  export const image: htmlHelper<'image'>
+  export const line: htmlHelper<'line'>
+  export const linearGradient: htmlHelper<'linearGradient'>
+  export const mask: htmlHelper<'mask'>
+  export const path: htmlHelper<'path'>
+  export const pattern: htmlHelper<'pattern'>
+  export const polygon: htmlHelper<'polygon'>
+  export const polyline: htmlHelper<'polyline'>
+  export const radialGradient: htmlHelper<'radialGradient'>
+  export const rect: htmlHelper<'rect'>
+  export const stop: htmlHelper<'stop'>
+  export const svg: htmlHelper<'svg'>
+  export const text: htmlHelper<'text'>
+  export const tspan: htmlHelper<'tspan'>
 }
