7c9d25195b0e565223e03de5ba04db79d4b8e5b7
Nick Watts <nwatts@broadinstitute.org>
2022-09-22 16:23:39 -0400

    Simplify

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,24 +1,19 @@
 import { img } from 'react-hyperscript-helpers'
 import { getUser } from 'src/libs/auth'
-import * as Utils from 'src/libs/utils'
 
 
 const ProfilePicture = ({ size, style, ...props } = {}) => {
   // Note Azure logins don't currently have an imageUrl, so don't render anything.
   // See TOAZ-147 to support this in the future.
   const imageUrl = getUser().imageUrl
-
-  return Utils.cond(
-    [!imageUrl, () => null],
-    () => img({
-      alt: 'Google profile image',
-      src: imageUrl,
-      height: size, width: size,
-      style: { borderRadius: '100%', ...style },
-      referrerPolicy: 'no-referrer',
-      ...props
-    })
-  )
+  return !!imageUrl && img({
+    alt: 'Google profile image',
+    src: imageUrl,
+    height: size, width: size,
+    style: { borderRadius: '100%', ...style },
+    referrerPolicy: 'no-referrer',
+    ...props
+  })
 }
 
 export default ProfilePicture
