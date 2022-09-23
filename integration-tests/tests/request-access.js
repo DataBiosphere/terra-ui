66756840970cc2de9d06a53224a89adc64cb7ecb
Sky Rubenstein <blrubenstein@wpi.edu>
2022-09-23 15:46:33 -0400

    Add request access for PHS ID datasets

--- integration-tests/tests/request-access.js
+++ integration-tests/tests/request-access.js
@@ -1,23 +1,34 @@
-const { checkbox, click, clickable, clickTableCell, findText, waitForNoSpinners } = require('../utils/integration-utils')
+const { checkbox, click, clickable, clickTableCell, waitForNoSpinners } = require('../utils/integration-utils')
 const { navigateToDataCatalog } = require('../utils/integration-helpers')
 const { registerTest } = require('../utils/jest-utils')
 const { withUserToken } = require('../utils/terra-sa-utils')
+const _ = require('lodash/fp')
 
+
+async function getHrefFromClickable(page, selector) {
+  return await (await (await page.waitForXPath(
+    clickable(selector),
+    _.defaults({ visible: true })
+  )).getProperty('href')).jsonValue()
+}
+
+const dbGapUrlRoot = 'https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id='
 
 const testRequestAccessFn = withUserToken(async ({ testUrl, page, token }) => {
   await navigateToDataCatalog(page, testUrl, token)
   await click(page, checkbox({ text: 'Controlled', isDescendant: true }))
 
   // Request access from the browse & explore page
-  await click(page, clickable({ textContains: 'Request Access' }))
-  await findText(page, 'Request Access')
-  await click(page, clickable({ textContains: 'Close modal' }))
+  if (!_.includes(dbGapUrlRoot, await getHrefFromClickable(page, { textContains: 'Request Access' }))) {
+    throw new Error('Discoverable dataset did not have a dbGap href')
+  }
 
   // Request access from the dataset details page
   await clickTableCell(page, { tableName: 'dataset list', columnHeader: 'Dataset Name', text: 'Discoverable Catalog Snapshot 1', isDescendant: true })
   await waitForNoSpinners(page)
-  await click(page, clickable({ textContains: 'Request Access' }))
-  await findText(page, 'Request Access')
+  if (!_.includes(dbGapUrlRoot, await getHrefFromClickable(page, { textContains: 'Request Access' }))) {
+    throw new Error('Discoverable dataset did not have a dbGap href')
+  }
 })
 
 registerTest({
