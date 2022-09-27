904cf21723a8724c14cdcebcccd26255f1cdf58c
n8landolt <nlandolt@broadinstitute.org>
2022-09-27 11:22:31 -0400

    Lowered contrast from 0.72 to 0.5 for disabled Links. Reduced margin from icons to text in cloudEnv page.

--- src/components/common.js
+++ src/components/common.js
@@ -92,8 +92,8 @@ export const Clickable = forwardRefWithName('Clickable',
 export const Link = forwardRefWithName('Link', ({ disabled, variant, children, baseColor = colors.accent, ...props }, ref) => {
   return h(Clickable, _.merge({
     ref,
-    style: { // 0.72 is the min to meet ANDI's contrast requirement
-      color: disabled ? colors.dark(0.72) : baseColor(variant === 'light' ? 0.3 : 1),
+    style: { // Note: 0.72 is the min to meet ANDI's contrast requirement
+      color: disabled ? colors.dark(0.5) : baseColor(variant === 'light' ? 0.3 : 1),
       cursor: disabled ? 'not-allowed' : 'pointer',
       fontWeight: 500, display: 'inline'
     },
