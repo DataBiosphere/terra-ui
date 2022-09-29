2c5e30dc49f5585ecaadbce167ae76f2000a185a
Nick Watts <nwatts@broadinstitute.org>
2022-09-29 16:37:56 -0400

    Capitalization

--- types/react-hyperscript-helpers/index.d.ts
+++ types/react-hyperscript-helpers/index.d.ts
@@ -8,7 +8,7 @@ declare module 'react-hyperscript-helpers' {
 
   type Children<Props> = Props extends { children?: React.ReactNode } ? React.ReactNode[] : (Props extends { children?: unknown } ? [Props['children']] : never)
 
-  interface hHelper {
+  interface HHelper {
     <Props extends {}>(
       component: Component<Props>,
       props: Omit<Props, 'children'>,
@@ -23,7 +23,7 @@ declare module 'react-hyperscript-helpers' {
     <Props extends {}>(component: Component<Props>): React.ReactElement<any, any>;
   }
 
-  export const h: hHelper
+  export const h: HHelper
 
   type TagName = keyof JSX.IntrinsicElements
 
@@ -35,7 +35,7 @@ declare module 'react-hyperscript-helpers' {
     [dataAttribute: DataAttributeKey]: string;
   }
 
-  type htmlHelper<T extends TagName> = {
+  type HtmlHelper<T extends TagName> = {
     (
       props: Omit<WithDataAttributes<HTMLElementProps<T>>, 'children'>,
       children: React.ReactNode[]
@@ -46,136 +46,136 @@ declare module 'react-hyperscript-helpers' {
     ): React.ReactElement<any, any>
   }
 
-  export const a: htmlHelper<'a'>
-  export const abbr: htmlHelper<'abbr'>
-  export const address: htmlHelper<'address'>
-  export const area: htmlHelper<'area'>
-  export const article: htmlHelper<'article'>
-  export const aside: htmlHelper<'aside'>
-  export const audio: htmlHelper<'audio'>
-  export const b: htmlHelper<'b'>
-  export const base: htmlHelper<'base'>
-  export const bdi: htmlHelper<'bdi'>
-  export const bdo: htmlHelper<'bdo'>
-  export const big: htmlHelper<'big'>
-  export const blockquote: htmlHelper<'blockquote'>
-  export const body: htmlHelper<'body'>
-  export const br: htmlHelper<'br'>
-  export const button: htmlHelper<'button'>
-  export const canvas: htmlHelper<'canvas'>
-  export const caption: htmlHelper<'caption'>
-  export const cite: htmlHelper<'cite'>
-  export const code: htmlHelper<'code'>
-  export const col: htmlHelper<'col'>
-  export const colgroup: htmlHelper<'colgroup'>
-  export const data: htmlHelper<'data'>
-  export const datalist: htmlHelper<'datalist'>
-  export const dd: htmlHelper<'dd'>
-  export const del: htmlHelper<'del'>
-  export const details: htmlHelper<'details'>
-  export const dfn: htmlHelper<'dfn'>
-  export const dialog: htmlHelper<'dialog'>
-  export const div: htmlHelper<'div'>
-  export const dl: htmlHelper<'dl'>
-  export const dt: htmlHelper<'dt'>
-  export const em: htmlHelper<'em'>
-  export const embed: htmlHelper<'embed'>
-  export const fieldset: htmlHelper<'fieldset'>
-  export const figcaption: htmlHelper<'figcaption'>
-  export const figure: htmlHelper<'figure'>
-  export const footer: htmlHelper<'footer'>
-  export const form: htmlHelper<'form'>
-  export const h1: htmlHelper<'h1'>
-  export const h2: htmlHelper<'h2'>
-  export const h3: htmlHelper<'h3'>
-  export const h4: htmlHelper<'h4'>
-  export const h5: htmlHelper<'h5'>
-  export const h6: htmlHelper<'h6'>
-  export const head: htmlHelper<'head'>
-  export const header: htmlHelper<'header'>
-  export const hgroup: htmlHelper<'hgroup'>
-  export const hr: htmlHelper<'hr'>
-  export const html: htmlHelper<'html'>
-  export const i: htmlHelper<'i'>
-  export const iframe: htmlHelper<'iframe'>
-  export const img: htmlHelper<'img'>
-  export const input: htmlHelper<'input'>
-  export const ins: htmlHelper<'ins'>
-  export const kbd: htmlHelper<'kbd'>
-  export const keygen: htmlHelper<'keygen'>
-  export const label: htmlHelper<'label'>
-  export const legend: htmlHelper<'legend'>
-  export const li: htmlHelper<'li'>
-  export const link: htmlHelper<'link'>
-  export const main: htmlHelper<'main'>
-  export const map: htmlHelper<'map'>
-  export const mark: htmlHelper<'mark'>
-  export const menu: htmlHelper<'menu'>
-  export const menuitem: htmlHelper<'menuitem'>
-  export const meta: htmlHelper<'meta'>
-  export const meter: htmlHelper<'meter'>
-  export const nav: htmlHelper<'nav'>
-  export const noscript: htmlHelper<'noscript'>
-  export const object: htmlHelper<'object'>
-  export const ol: htmlHelper<'ol'>
-  export const optgroup: htmlHelper<'optgroup'>
-  export const option: htmlHelper<'option'>
-  export const output: htmlHelper<'output'>
-  export const p: htmlHelper<'p'>
-  export const param: htmlHelper<'param'>
-  export const picture: htmlHelper<'picture'>
-  export const pre: htmlHelper<'pre'>
-  export const progress: htmlHelper<'progress'>
-  export const q: htmlHelper<'q'>
-  export const rp: htmlHelper<'rp'>
-  export const rt: htmlHelper<'rt'>
-  export const ruby: htmlHelper<'ruby'>
-  export const s: htmlHelper<'s'>
-  export const samp: htmlHelper<'samp'>
-  export const script: htmlHelper<'script'>
-  export const section: htmlHelper<'section'>
-  export const select: htmlHelper<'select'>
-  export const small: htmlHelper<'small'>
-  export const source: htmlHelper<'source'>
-  export const span: htmlHelper<'span'>
-  export const strong: htmlHelper<'strong'>
-  export const style: htmlHelper<'style'>
-  export const sub: htmlHelper<'sub'>
-  export const summary: htmlHelper<'summary'>
-  export const sup: htmlHelper<'sup'>
-  export const table: htmlHelper<'table'>
-  export const tbody: htmlHelper<'tbody'>
-  export const td: htmlHelper<'td'>
-  export const textarea: htmlHelper<'textarea'>
-  export const tfoot: htmlHelper<'tfoot'>
-  export const th: htmlHelper<'th'>
-  export const thead: htmlHelper<'thead'>
-  export const time: htmlHelper<'time'>
-  export const title: htmlHelper<'title'>
-  export const tr: htmlHelper<'tr'>
-  export const track: htmlHelper<'track'>
-  export const u: htmlHelper<'u'>
-  export const ul: htmlHelper<'ul'>
-  export const video: htmlHelper<'video'>
-  export const wbr: htmlHelper<'wbr'>
+  export const a: HtmlHelper<'a'>
+  export const abbr: HtmlHelper<'abbr'>
+  export const address: HtmlHelper<'address'>
+  export const area: HtmlHelper<'area'>
+  export const article: HtmlHelper<'article'>
+  export const aside: HtmlHelper<'aside'>
+  export const audio: HtmlHelper<'audio'>
+  export const b: HtmlHelper<'b'>
+  export const base: HtmlHelper<'base'>
+  export const bdi: HtmlHelper<'bdi'>
+  export const bdo: HtmlHelper<'bdo'>
+  export const big: HtmlHelper<'big'>
+  export const blockquote: HtmlHelper<'blockquote'>
+  export const body: HtmlHelper<'body'>
+  export const br: HtmlHelper<'br'>
+  export const button: HtmlHelper<'button'>
+  export const canvas: HtmlHelper<'canvas'>
+  export const caption: HtmlHelper<'caption'>
+  export const cite: HtmlHelper<'cite'>
+  export const code: HtmlHelper<'code'>
+  export const col: HtmlHelper<'col'>
+  export const colgroup: HtmlHelper<'colgroup'>
+  export const data: HtmlHelper<'data'>
+  export const datalist: HtmlHelper<'datalist'>
+  export const dd: HtmlHelper<'dd'>
+  export const del: HtmlHelper<'del'>
+  export const details: HtmlHelper<'details'>
+  export const dfn: HtmlHelper<'dfn'>
+  export const dialog: HtmlHelper<'dialog'>
+  export const div: HtmlHelper<'div'>
+  export const dl: HtmlHelper<'dl'>
+  export const dt: HtmlHelper<'dt'>
+  export const em: HtmlHelper<'em'>
+  export const embed: HtmlHelper<'embed'>
+  export const fieldset: HtmlHelper<'fieldset'>
+  export const figcaption: HtmlHelper<'figcaption'>
+  export const figure: HtmlHelper<'figure'>
+  export const footer: HtmlHelper<'footer'>
+  export const form: HtmlHelper<'form'>
+  export const h1: HtmlHelper<'h1'>
+  export const h2: HtmlHelper<'h2'>
+  export const h3: HtmlHelper<'h3'>
+  export const h4: HtmlHelper<'h4'>
+  export const h5: HtmlHelper<'h5'>
+  export const h6: HtmlHelper<'h6'>
+  export const head: HtmlHelper<'head'>
+  export const header: HtmlHelper<'header'>
+  export const hgroup: HtmlHelper<'hgroup'>
+  export const hr: HtmlHelper<'hr'>
+  export const html: HtmlHelper<'html'>
+  export const i: HtmlHelper<'i'>
+  export const iframe: HtmlHelper<'iframe'>
+  export const img: HtmlHelper<'img'>
+  export const input: HtmlHelper<'input'>
+  export const ins: HtmlHelper<'ins'>
+  export const kbd: HtmlHelper<'kbd'>
+  export const keygen: HtmlHelper<'keygen'>
+  export const label: HtmlHelper<'label'>
+  export const legend: HtmlHelper<'legend'>
+  export const li: HtmlHelper<'li'>
+  export const link: HtmlHelper<'link'>
+  export const main: HtmlHelper<'main'>
+  export const map: HtmlHelper<'map'>
+  export const mark: HtmlHelper<'mark'>
+  export const menu: HtmlHelper<'menu'>
+  export const menuitem: HtmlHelper<'menuitem'>
+  export const meta: HtmlHelper<'meta'>
+  export const meter: HtmlHelper<'meter'>
+  export const nav: HtmlHelper<'nav'>
+  export const noscript: HtmlHelper<'noscript'>
+  export const object: HtmlHelper<'object'>
+  export const ol: HtmlHelper<'ol'>
+  export const optgroup: HtmlHelper<'optgroup'>
+  export const option: HtmlHelper<'option'>
+  export const output: HtmlHelper<'output'>
+  export const p: HtmlHelper<'p'>
+  export const param: HtmlHelper<'param'>
+  export const picture: HtmlHelper<'picture'>
+  export const pre: HtmlHelper<'pre'>
+  export const progress: HtmlHelper<'progress'>
+  export const q: HtmlHelper<'q'>
+  export const rp: HtmlHelper<'rp'>
+  export const rt: HtmlHelper<'rt'>
+  export const ruby: HtmlHelper<'ruby'>
+  export const s: HtmlHelper<'s'>
+  export const samp: HtmlHelper<'samp'>
+  export const script: HtmlHelper<'script'>
+  export const section: HtmlHelper<'section'>
+  export const select: HtmlHelper<'select'>
+  export const small: HtmlHelper<'small'>
+  export const source: HtmlHelper<'source'>
+  export const span: HtmlHelper<'span'>
+  export const strong: HtmlHelper<'strong'>
+  export const style: HtmlHelper<'style'>
+  export const sub: HtmlHelper<'sub'>
+  export const summary: HtmlHelper<'summary'>
+  export const sup: HtmlHelper<'sup'>
+  export const table: HtmlHelper<'table'>
+  export const tbody: HtmlHelper<'tbody'>
+  export const td: HtmlHelper<'td'>
+  export const textarea: HtmlHelper<'textarea'>
+  export const tfoot: HtmlHelper<'tfoot'>
+  export const th: HtmlHelper<'th'>
+  export const thead: HtmlHelper<'thead'>
+  export const time: HtmlHelper<'time'>
+  export const title: HtmlHelper<'title'>
+  export const tr: HtmlHelper<'tr'>
+  export const track: HtmlHelper<'track'>
+  export const u: HtmlHelper<'u'>
+  export const ul: HtmlHelper<'ul'>
+  export const video: HtmlHelper<'video'>
+  export const wbr: HtmlHelper<'wbr'>
 
-  export const circle: htmlHelper<'circle'>
-  export const clipPath: htmlHelper<'clipPath'>
-  export const defs: htmlHelper<'defs'>
-  export const ellipse: htmlHelper<'ellipse'>
-  export const g: htmlHelper<'g'>
-  export const image: htmlHelper<'image'>
-  export const line: htmlHelper<'line'>
-  export const linearGradient: htmlHelper<'linearGradient'>
-  export const mask: htmlHelper<'mask'>
-  export const path: htmlHelper<'path'>
-  export const pattern: htmlHelper<'pattern'>
-  export const polygon: htmlHelper<'polygon'>
-  export const polyline: htmlHelper<'polyline'>
-  export const radialGradient: htmlHelper<'radialGradient'>
-  export const rect: htmlHelper<'rect'>
-  export const stop: htmlHelper<'stop'>
-  export const svg: htmlHelper<'svg'>
-  export const text: htmlHelper<'text'>
-  export const tspan: htmlHelper<'tspan'>
+  export const circle: HtmlHelper<'circle'>
+  export const clipPath: HtmlHelper<'clipPath'>
+  export const defs: HtmlHelper<'defs'>
+  export const ellipse: HtmlHelper<'ellipse'>
+  export const g: HtmlHelper<'g'>
+  export const image: HtmlHelper<'image'>
+  export const line: HtmlHelper<'line'>
+  export const linearGradient: HtmlHelper<'linearGradient'>
+  export const mask: HtmlHelper<'mask'>
+  export const path: HtmlHelper<'path'>
+  export const pattern: HtmlHelper<'pattern'>
+  export const polygon: HtmlHelper<'polygon'>
+  export const polyline: HtmlHelper<'polyline'>
+  export const radialGradient: HtmlHelper<'radialGradient'>
+  export const rect: HtmlHelper<'rect'>
+  export const stop: HtmlHelper<'stop'>
+  export const svg: HtmlHelper<'svg'>
+  export const text: HtmlHelper<'text'>
+  export const tspan: HtmlHelper<'tspan'>
 }
