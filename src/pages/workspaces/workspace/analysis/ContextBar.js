0f189bda8940c676f4c0f5b75d6f168027af0ec2
cahrens <roberts@broadinstitute.org>
2022-10-03 16:06:20 -0400

    Fix label issue (IA-3369).

--- src/pages/workspaces/workspace/analysis/ContextBar.js
+++ src/pages/workspaces/workspace/analysis/ContextBar.js
@@ -195,7 +195,6 @@ export const ContextBar = ({
           getEnvironmentStatusIcons()
         ]),
         h(Clickable, {
-          'aria-label': 'Terminal button',
           style: { borderTop: `1px solid ${colors.accent()}`, paddingLeft: '1rem', alignItems: 'center', ...contextBarStyles.contextBarButton, color: !isTerminalEnabled ? colors.dark(0.7) : contextBarStyles.contextBarButton.color },
           hover: contextBarStyles.hover,
           'data-testid': 'terminal-button-id',
@@ -213,7 +212,7 @@ export const ContextBar = ({
           tooltipDelay: 100,
           useTooltipAsLabel: false,
           ...Utils.newTabLinkProps
-        }, [icon('terminal', { size: 40 })])
+        }, [icon('terminal', { size: 40 }), span({ className: 'sr-only' }, ['Terminal button'])])
       ])
     ])
   ])
