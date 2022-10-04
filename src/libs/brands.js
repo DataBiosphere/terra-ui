e775327ae63a76ef0d560a3ced2bffc3f0d5184c
Sky Rubenstein <blrubenstein@wpi.edu>
2022-10-04 11:35:43 -0400

    Correct consortium value

--- src/libs/brands.js
+++ src/libs/brands.js
@@ -188,7 +188,7 @@ export const brands = {
       color: radXLogo,
       white: radXLogoWhite
     },
-    catalogConsortiumsToInclude: ['RADx-UP', 'RADx-DHP', 'RADx-Rad', 'RADx-Tech']
+    catalogConsortiumsToInclude: ['RADx-UP', 'RADx-DHT', 'RADx-Rad', 'RADx-Tech']
   },
   rareX: {
     name: `The RARE${nonBreakingHyphen}X Data Analysis Platform`,
