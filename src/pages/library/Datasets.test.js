f3db3802bf1ea0349da105c257b61edd511357a1
Sky Rubenstein <blrubenstein@wpi.edu>
2022-10-11 10:29:41 -0400

    Fix tests

--- src/pages/library/Datasets.test.js
+++ src/pages/library/Datasets.test.js
@@ -47,21 +47,19 @@ describe('Datasets', () => {
 
   it('only shows RadX datasets when in RadX', () => {
     getEnabledBrand.mockReturnValue(brands.radX)
-    const displayedDatasets = filterAndNormalizeDatasets({
-      result: [
-        { 'dct:title': 'radx-up', 'TerraDCAT_ap:hasDataCollection': [{ 'dct:title': 'RADx-UP' }] }, { 'dct:title': 'not-radx' }
-      ]
-    })
+    const displayedDatasets = filterAndNormalizeDatasets(
+      [{ 'dct:title': 'radx-up', 'TerraDCAT_ap:hasDataCollection': [{ 'dct:title': 'RADx-UP' }] }, { 'dct:title': 'not-radx' }],
+      brands.radX.catalogDataCollectionsToInclude
+    )
     expect(displayedDatasets.length).toBe(1)
     expect(displayedDatasets[0]['dct:title']).toBe('radx-up')
   })
 
   it('shows all datasets in terra', () => {
     getEnabledBrand.mockReturnValue(brands.terra)
-    expect(filterAndNormalizeDatasets({
-      result: [
-        { 'dct:title': 'radx-up', 'TerraDCAT_ap:hasDataCollection': [{ 'dct:title': 'RADx-UP' }] }, { 'dct:title': 'not-radx' }
-      ]
-    }).length).toBe(2)
+    expect(filterAndNormalizeDatasets(
+      [{ 'dct:title': 'radx-up', 'TerraDCAT_ap:hasDataCollection': [{ 'dct:title': 'RADx-UP' }] }, { 'dct:title': 'not-radx' }],
+      brands.terra.catalogDataCollectionsToInclude
+    ).length).toBe(2)
   })
 })
