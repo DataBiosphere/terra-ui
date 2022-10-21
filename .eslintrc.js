42f7f815b905cb819c8d5df5ed228d51a670c661
Kaan Yuksel <kyuksel@broadinstitute.org>
2022-10-21 11:35:10 -0400

    PR feedback: better messaging

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,7 +65,7 @@ module.exports = {
     'arrow-parens': ['warn', 'as-needed'],
     'arrow-spacing': 'warn',
     'no-duplicate-imports': 'warn',
-    'no-restricted-syntax': ['warn', { 'selector': 'TSEnumDeclaration', 'message': "Don't declare enums" }],
+    'no-restricted-syntax': ['warn', { 'selector': 'TSEnumDeclaration', 'message': 'Use a union of literals instead of an enum' }],
     // TODO: Set 'variables' to 'true' after fixing the existing issues
     'no-use-before-define': ['warn', { 'functions': true, 'classes': true, 'variables': false }],
     'no-useless-rename': 'warn',
