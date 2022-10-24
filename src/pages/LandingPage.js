5ac933f670357805b82f4374ec6e0bd51517fbe4
cahrens <roberts@broadinstitute.org>
2022-10-24 17:07:57 -0400

    Try useEffect.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import _ from 'lodash/fp'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { div, h, h2 } from 'react-hyperscript-helpers'
 import { ButtonOutline, ButtonPrimary, Clickable, HeroWrapper, Link } from 'src/components/common'
 import { icon } from 'src/components/icons'
@@ -8,11 +8,11 @@ import terraHero from 'src/images/terra-hero.png'
 import { Ajax } from 'src/libs/ajax'
 import { getEnabledBrand, isFirecloud, isTerra } from 'src/libs/brand-utils'
 import colors from 'src/libs/colors'
-import { reportErrorAndRethrow } from 'src/libs/error'
+import { withErrorIgnoring } from 'src/libs/error'
 import Events from 'src/libs/events'
 import * as Nav from 'src/libs/nav'
 import { setLocalPref } from 'src/libs/prefs'
-import { useCancellation, useOnMount, useStore } from 'src/libs/react-utils'
+import { useCancellation, useStore } from 'src/libs/react-utils'
 import { authStore } from 'src/libs/state'
 import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
@@ -72,20 +72,21 @@ const makeDocLinks = _.map(
 
 const LandingPage = () => {
   const { isSignedIn } = useStore(authStore)
-  const isAuthInitialized = isSignedIn !== undefined
   const [billingProjects, setBillingProjects] = useState()
   const signal = useCancellation()
-  const loadProjects = () => {
-    reportErrorAndRethrow('Error loading billing projects list')(async () => setBillingProjects(await Ajax(signal).Billing.listProjects()))
-  }
 
-  useOnMount(() => {
-    loadProjects()
-  })
+  useEffect(() => {
+    const loadProjects = withErrorIgnoring(async () => {
+      const projects = await Ajax(signal).Billing.listProjects()
+      setBillingProjects(projects)
+    })
+    if (isSignedIn) {
+      loadProjects()
+    }
+  }, [isSignedIn, setBillingProjects, signal])
 
   return h(HeroWrapper, { bigSubhead: true }, [
-    isTerra() && isAuthInitialized && isSignedIn && billingProjects !== undefined && _.isEmpty(billingProjects) &&
-    div({
+    isTerra() && !_.isUndefined(billingProjects) && _.isEmpty(billingProjects) && div({
       style: {
         ...styles.callToActionBanner,
         width: `calc(${styles.card.width * 3}px + ${styles.card.marginRight} * 2)`,
