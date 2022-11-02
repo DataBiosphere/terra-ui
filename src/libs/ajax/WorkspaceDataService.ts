f6e204dd1cad7c7538ed5979d89193280908b0c6
David An <davidan@broadinstitute.org>
2022-11-02 14:06:00 -0400

    add return types for WorkspaceDataService ajax methods

--- src/libs/ajax/WorkspaceDataService.ts
+++ src/libs/ajax/WorkspaceDataService.ts
@@ -1,24 +1,24 @@
 import _ from 'lodash/fp'
 import { authOpts, fetchWDS, jsonBody } from 'src/libs/ajax/ajax-common'
-import { SearchRequest } from 'src/libs/ajax/data-table-providers/WdsDataTableProvider'
+import { RecordQueryResponse, RecordTypeSchema, SearchRequest } from 'src/libs/ajax/data-table-providers/WdsDataTableProvider'
 
 
 export const WorkspaceDataService = signal => ({
-  getSchema: async (instanceId: string) => {
+  getSchema: async (instanceId: string): Promise<RecordTypeSchema[]> => {
     const res = await fetchWDS(`${instanceId}/types/v0.2`, _.merge(authOpts(), { signal }))
     return res.json()
   },
-  getRecords: async (instanceId: string, recordType: string, parameters: SearchRequest) => {
+  getRecords: async (instanceId: string, recordType: string, parameters: SearchRequest): Promise<RecordQueryResponse> => {
     const res = await fetchWDS(`${instanceId}/search/v0.2/${recordType}`,
       _.mergeAll([authOpts(), jsonBody(parameters), { signal, method: 'POST' }]))
     return res.json()
   },
-  deleteTable: async (instanceId: string, recordType: string) => {
+  deleteTable: async (instanceId: string, recordType: string): Promise<void> => {
     const res = await fetchWDS(`${instanceId}/types/v0.2/${recordType}`,
       _.mergeAll([authOpts(), { signal, method: 'DELETE' }]))
     return res
   },
-  downloadTsv: async (instanceId: string, recordType: string) => {
+  downloadTsv: async (instanceId: string, recordType: string): Promise<Response> => {
     const res = await fetchWDS(`${instanceId}/tsv/v0.2/${recordType}`, _.merge(authOpts(), { signal }))
     return res
   }
