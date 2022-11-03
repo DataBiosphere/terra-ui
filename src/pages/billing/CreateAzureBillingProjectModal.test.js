d3780edc3e65aa6460389a929ea257cb74887fd0
Nick Watts <nwatts@broadinstitute.org>
2022-11-02 20:53:13 -0400

    Restore modal mock

--- src/pages/billing/CreateAzureBillingProjectModal.test.js
+++ src/pages/billing/CreateAzureBillingProjectModal.test.js
@@ -12,6 +12,14 @@ import { billingProjectNameValidator } from 'src/pages/billing/List'
 import { v4 as uuid } from 'uuid'
 
 
+jest.mock('src/components/Modal', () => {
+  const originalModule = jest.requireActual('src/components/Modal')
+  return {
+    ...originalModule,
+    __esModule: true,
+    default: props => originalModule.default({ onAfterOpen: jest.fn(), ...props })
+  }
+})
 jest.mock('src/libs/ajax')
 
 describe('CreateAzureBillingProjectModal', () => {
