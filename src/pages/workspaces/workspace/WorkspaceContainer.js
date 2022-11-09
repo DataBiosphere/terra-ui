31a1f02d368ffd6b00803b1372270c2a246505b1
aaronkanzer <kanzer@wm21c-229.broadinstitute.org>
2022-11-09 13:43:12 -0500

    Whoops

--- src/pages/workspaces/workspace/WorkspaceContainer.js
+++ src/pages/workspaces/workspace/WorkspaceContainer.js
@@ -81,7 +81,7 @@ const WorkspaceTabs = ({
 
   const tabs = [
     { name: 'dashboard', link: 'workspace-dashboard' },
-    ...(isFeaturePreviewEnabled('workspace-data-service') ? [{ name: 'data', link: 'workspace-data' }] : []),
+    ...(isFeaturePreviewEnabled('workspace-data-service') || isGoogleWorkspace ? [{ name: 'data', link: 'workspace-data' }] : []),
     // the spread operator results in no array entry if the config value is false
     // we want this feature gated until it is ready for release
     { name: 'analyses', link: analysisTabName },
