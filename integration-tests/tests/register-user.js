ad93fb97cfd1474b60330be15dbfecf3904e0d1f
cahrens <roberts@broadinstitute.org>
2022-11-17 15:51:29 -0500

    Add some a11y checks (WOR-643).

--- integration-tests/tests/register-user.js
+++ integration-tests/tests/register-user.js
@@ -1,5 +1,5 @@
 const { withUser } = require('../utils/integration-helpers')
-const { fillIn, findText, click, clickable, input, signIntoTerra } = require('../utils/integration-utils')
+const { fillIn, findText, click, clickable, input, signIntoTerra, verifyAccessibility } = require('../utils/integration-utils')
 const { fillInReplace, gotoPage } = require('../utils/integration-utils')
 const { registerTest } = require('../utils/jest-utils')
 
@@ -7,12 +7,15 @@ const { registerTest } = require('../utils/jest-utils')
 const testRegisterUserFn = withUser(async ({ page, testUrl, token }) => {
   await gotoPage(page, testUrl)
   await click(page, clickable({ textContains: 'View Workspaces' }))
+  await verifyAccessibility(page)
   await signIntoTerra(page, { token })
   await fillInReplace(page, input({ labelContains: 'First Name' }), 'Integration')
   await fillIn(page, input({ labelContains: 'Last Name' }), 'Test')
+  await verifyAccessibility(page)
   await click(page, clickable({ textContains: 'Register' }))
   await click(page, clickable({ textContains: 'Accept' }), { timeout: 90000 })
   await findText(page, 'To get started, Create a New Workspace')
+  await verifyAccessibility(page)
 })
 
 registerTest({
