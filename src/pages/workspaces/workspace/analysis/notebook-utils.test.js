5715a9193f3c98daba31fd0c348938a8b77b03c3
n8landolt <nlandolt@broadinstitute.org>
2022-11-18 16:08:41 -0500

    Fixed import issue in notebook-utils-test.js.

--- src/pages/workspaces/workspace/analysis/notebook-utils.test.js
+++ src/pages/workspaces/workspace/analysis/notebook-utils.test.js
@@ -1,10 +1,10 @@
 
-import { allAppTypes, appTools, Galaxy, isAppToolLabel, toolLabels } from 'src/pages/workspaces/workspace/analysis/tool-utils'
+import { allAppTypes, appTools, isAppToolLabel, toolLabels } from 'src/pages/workspaces/workspace/analysis/tool-utils'
 
 
 describe('getAllAppTypes and isToolAnApp', () => {
   it('getAllAppTypes includes tools with a defined appType', () => {
-    expect(allAppTypes.sort).toBe([Galaxy.appType, appTools.Cromwell.appType].sort)
+    expect(allAppTypes.sort).toBe([appTools.Galaxy.appType, appTools.Cromwell.appType].sort)
   })
   it('isToolAnApp returns if a tool label corresponds to an app', () => {
     expect(isAppToolLabel(toolLabels.Cromwell)).toBeTruthy()
