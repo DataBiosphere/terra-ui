bb59c109679408613912cd26cacfd2369b8cabdb
tlangs <tlangs@broadinstitute.org>
2022-12-06 14:38:59 -0500

    PR comments

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,37 +1,30 @@
 import { useState } from 'react'
-import { div, h, h1 } from 'react-hyperscript-helpers'
-import { backgroundLogo } from 'src/components/common'
+import { div, h } from 'react-hyperscript-helpers'
 import { centeredSpinner } from 'src/components/icons'
 import { MarkdownViewer, newWindowLinkRenderer } from 'src/components/markdown'
 import { Ajax } from 'src/libs/ajax'
-import colors from 'src/libs/colors'
+import { withErrorReporting } from 'src/libs/error'
 import { useOnMount } from 'src/libs/react-utils'
-import * as Style from 'src/libs/style'
 
 
 const PrivacyPolicy = () => {
   const [privacyPolicy, setPrivacyPolicy] = useState()
 
   useOnMount(() => {
-    const loadPrivacyPolicyAndUpdateState = (async () => {
-      setPrivacyPolicy(await Ajax().User.getPrivacyPolicy())
-    })
+    const loadPrivacyPolicyAndUpdateState =
+      withErrorReporting('There was an error retrieving our privacy policy.')(async () => {
+        setPrivacyPolicy(await Ajax().User.getPrivacyPolicy())
+      })
     loadPrivacyPolicyAndUpdateState()
   })
 
-  return div({ role: 'main', style: { padding: '1rem', minHeight: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' } }, [
-    backgroundLogo,
-    div({ style: { backgroundColor: 'white', borderRadius: 5, width: 800, maxHeight: '100%', padding: '2rem', boxShadow: Style.standardShadow } }, [
-      h1({ style: { color: colors.dark(), fontSize: 38, fontWeight: 400 } }, ['Terra Privacy Policy']),
-      div({ style: { height: '50vh', overflowY: 'auto', lineHeight: 1.5, marginTop: '1rem', paddingRight: '1rem' } }, [
-        !privacyPolicy ? centeredSpinner() : h(MarkdownViewer, {
-          renderers: {
-            link: newWindowLinkRenderer,
-            heading: (text, level) => `<h${level} style="margin-bottom: 0">${text}</h${level}>`
-          }
-        }, [privacyPolicy])
-      ])
-    ])
+  return div({ style: { width: 800, maxHeight: '100%', padding: '1rem' } }, [
+    !privacyPolicy ? centeredSpinner() : h(MarkdownViewer, {
+      renderers: {
+        link: newWindowLinkRenderer,
+        heading: (text, level) => `<h${level} style="margin-bottom: 0">${text}</h${level}>`
+      }
+    }, [privacyPolicy])
   ])
 }
 
