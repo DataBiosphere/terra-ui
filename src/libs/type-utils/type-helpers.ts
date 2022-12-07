e60ff36bfebbe4eb5b5bc3edb8cf385d3498463b
Miguel Silva <msilva@broadinstitute.org>
2022-12-07 13:25:59 -0500

    IA-3762 useLoadedData helper hook - useLoadedData will handle typical data request mechanics and translate them to 'Loading', 'Ready' or 'Error' status outcomes in standardized LoadedState<T> result object.  Returns a tuple similar to useState hook.  See jsDoc and unit tests. - part of work for Export Analysis Modal Typescript and Unit Tests

--- src/libs/type-utils/type-helpers.ts
+++ src/libs/type-utils/type-helpers.ts
@@ -1,4 +1,32 @@
 // This syntax gives us nominal typing
 // Nominal types are not interchangable even if the underlying type is the same
 // A func that takes (a: A, b: B) where A and B are both strings cannot be called like (b,a) with this restriction
+import { AnyPromiseFn } from 'src/libs/type-utils/general-types'
+
+
 export type NominalType<BaseType, Name extends string> = BaseType & { __typeToken: Name }
+
+// modified from https://bobbyhadz.com/blog/javascript-check-if-function-returns-promise
+/**
+ * Type predicate that will check if a variable (p) is a Promise
+ * @param p the variable to check
+ */
+export const isPromise = (p: unknown): p is Promise<unknown> => {
+  const isPromise = (typeof p === 'object' && typeof (p as Promise<unknown>).then === 'function')
+  return isPromise
+}
+
+// modified from https://bobbyhadz.com/blog/javascript-check-if-function-returns-promise
+/**
+ * Type predicate that will check if a variable (f) is a function that returns a Promise
+ * @param f the variable to check
+ */
+export const isPromiseReturn = (f: unknown): f is AnyPromiseFn<unknown> => {
+  const isPromiseFn = (
+    !!f && (
+      (f as Function).constructor.name === 'AsyncFunction' ||
+          (typeof f === 'function' && isPromise(f()))
+    )
+  )
+  return isPromiseFn
+}
