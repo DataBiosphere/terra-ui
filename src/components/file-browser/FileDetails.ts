bd80036b6189890d0d8568e06a066879b10e2910
Nick Watts <1156625+nawatts@users.noreply.github.com>
2022-12-12 09:31:01 -0500

    [SU-249] Add file details modal (#3574)

---
+++ src/components/file-browser/FileDetails.ts
@@ -0,0 +1,30 @@
+import filesize from 'filesize'
+import { Fragment } from 'react'
+import { dd, div, dl, dt, h } from 'react-hyperscript-helpers'
+import { FileBrowserFile } from 'src/libs/ajax/file-browser-providers/FileBrowserProvider'
+
+
+interface FileDetailsProps {
+  file: FileBrowserFile
+}
+
+export const FileDetails = (props: FileDetailsProps) => {
+  const { file } = props
+
+  return h(Fragment, [
+    dl([
+      div({ style: { marginBottom: '0.5rem' } }, [
+        dt({ style: { marginBottom: '0.25rem', fontWeight: 500 } }, ['Last modified']),
+        dd({ style: { marginLeft: '0.5rem' } }, [new Date(file.updatedAt).toLocaleString()]),
+      ]),
+      div({ style: { marginBottom: '0.5rem' } }, [
+        dt({ style: { marginBottom: '0.25rem', fontWeight: 500 } }, ['Created']),
+        dd({ style: { marginLeft: '0.5rem' } }, [new Date(file.createdAt).toLocaleString()]),
+      ]),
+      div({ style: { marginBottom: '0.5rem' } }, [
+        dt({ style: { marginBottom: '0.25rem', fontWeight: 500 } }, ['Size']),
+        dd({ style: { marginLeft: '0.5rem' } }, [filesize(file.size)]),
+      ]),
+    ])
+  ])
+}
