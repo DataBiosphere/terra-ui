1d6a13a89e36b5dd97b2509559ca1fb5af6b837e
cahrens <roberts@broadinstitute.org>
2022-12-15 14:41:52 -0500

    Try moving outside of comp.

--- src/components/AxeCore.js
+++ src/components/AxeCore.js
@@ -5,24 +5,24 @@ import ReactDOM from 'react-dom'
 const axe = require('@axe-core/react')
 
 
+console.log('axe accessibility checking is running, and this can negatively impact UI performance') // eslint-disable-line no-console
+console.log('to disable: window.configOverridesStore.set({ isAxeEnabled: false })') // eslint-disable-line no-console
+
+const config = {
+  tags: ['wcag2a', 'wcag2aa'],
+  rules: [
+    {
+      id: 'color-contrast',
+      excludeHidden: true
+    }
+  ]
+}
+
+axe(React, ReactDOM, 1000, config)
+
 const AxeCore = () => {
-  console.log('axe accessibility checking is running, and this can negatively impact UI performance') // eslint-disable-line no-console
-  console.log('to disable: window.configOverridesStore.set({ isAxeEnabled: false })') // eslint-disable-line no-console
-
-  const config = {
-    tags: ['wcag2a', 'wcag2aa'],
-    rules: [
-      {
-        id: 'color-contrast',
-        excludeHidden: true
-      }
-    ]
-  }
-
-  axe(React, ReactDOM, 1000, config)
-
   // Return null because this component doesn't actually render anything.
-  // It exists purely for starting axe-core.
+  // It exists purely for initializing axe-core.
   return null
 }
 
