70db843659ffa6122ce110e2489d5785d78e1316
aaronkanzer <36093535+aaronkanzer@users.noreply.github.com>
2022-12-15 11:06:19 -0500

    AJ-676: Leverage proxyUrls from Leo for WDS (#3569)
    Co-authored-by: Bria Morgan <bmorgan@broadinstitute.org>

--- src/libs/ajax/WorkspaceDataService.ts
+++ src/libs/ajax/WorkspaceDataService.ts
@@ -9,29 +9,29 @@ import {
 
 
 export const WorkspaceData = signal => ({
-  getSchema: async (instanceId: string): Promise<RecordTypeSchema[]> => {
-    const res = await fetchWDS(`${instanceId}/types/v0.2`, _.merge(authOpts(), { signal }))
+  getSchema: async (root: string, instanceId: string): Promise<RecordTypeSchema[]> => {
+    const res = await fetchWDS(root)(`${instanceId}/types/v0.2`, _.merge(authOpts(), { signal }))
     return res.json()
   },
-  getRecords: async (instanceId: string, recordType: string, parameters: SearchRequest): Promise<RecordQueryResponse> => {
-    const res = await fetchWDS(`${instanceId}/search/v0.2/${recordType}`,
+  getRecords: async (root: string, instanceId: string, recordType: string, parameters: SearchRequest): Promise<RecordQueryResponse> => {
+    const res = await fetchWDS(root)(`${instanceId}/search/v0.2/${recordType}`,
       _.mergeAll([authOpts(), jsonBody(parameters), { signal, method: 'POST' }]))
     return res.json()
   },
-  deleteTable: async (instanceId: string, recordType: string): Promise<Response> => {
-    const res = await fetchWDS(`${instanceId}/types/v0.2/${recordType}`,
+  deleteTable: async (root: string, instanceId: string, recordType: string): Promise<Response> => {
+    const res = await fetchWDS(root)(`${instanceId}/types/v0.2/${recordType}`,
       _.mergeAll([authOpts(), { signal, method: 'DELETE' }]))
     return res
   },
-  downloadTsv: async (instanceId: string, recordType: string): Promise<Blob> => {
-    const res = await fetchWDS(`${instanceId}/tsv/v0.2/${recordType}`, _.merge(authOpts(), { signal }))
+  downloadTsv: async (root: string, instanceId: string, recordType: string): Promise<Blob> => {
+    const res = await fetchWDS(root)(`${instanceId}/tsv/v0.2/${recordType}`, _.merge(authOpts(), { signal }))
     const blob = await res.blob()
     return blob
   },
-  uploadTsv: async (instanceId: string, recordType: string, file: File): Promise<TsvUploadResponse> => {
+  uploadTsv: async (root: string, instanceId: string, recordType: string, file: File): Promise<TsvUploadResponse> => {
     const formData = new FormData()
     formData.set('records', file)
-    const res = await fetchWDS(`${instanceId}/tsv/v0.2/${recordType}`, _.mergeAll([authOpts(), { body: formData, signal, method: 'POST' }]))
+    const res = await fetchWDS(root)(`${instanceId}/tsv/v0.2/${recordType}`, _.mergeAll([authOpts(), { body: formData, signal, method: 'POST' }]))
     return res.json()
   }
 })
