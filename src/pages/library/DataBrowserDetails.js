58909864bbcc27f7e84371a49359566eb683b711
Sky Rubenstein <blrubenstein@wpi.edu>
2022-12-19 11:19:08 -0500

    Fix code smells

--- src/pages/library/DataBrowserDetails.js
+++ src/pages/library/DataBrowserDetails.js
@@ -128,6 +128,9 @@ export const SidebarComponent = ({ dataObj, id }) => {
   const [tdrSnapshotPreparePolling, setTdrSnapshotPreparePolling] = useState(false)
   const sidebarButtonWidth = 230
   const access = getDatasetAccessType(dataObj)
+  const supportedFeatureTooltipMessage = access === datasetAccessTypes.GRANTED ? '' : uiMessaging.controlledFeatureTooltip
+  const actionTooltip = message => (isWorkspace(dataObj) || isDatarepoSnapshot(dataObj)) ?
+    supportedFeatureTooltipMessage : uiMessaging.unsupportedDatasetTypeTooltip(message)
 
 
   const importDataToWorkspace = dataset => {
@@ -237,9 +240,7 @@ export const SidebarComponent = ({ dataObj, id }) => {
         h(Fragment, [
           h(ButtonOutline, {
             disabled: (isWorkspace(dataObj) || isDatarepoSnapshot(dataObj)) && access !== datasetAccessTypes.GRANTED,
-            tooltip: (isWorkspace(dataObj) || isDatarepoSnapshot(dataObj)) ?
-              access === datasetAccessTypes.GRANTED ? '' : uiMessaging.controlledFeatureTooltip :
-              uiMessaging.unsupportedDatasetTypeTooltip('preview'),
+            tooltip: actionTooltip('preview'),
             style: { fontSize: 16, textTransform: 'none', height: 'unset', width: sidebarButtonWidth, marginTop: 20 },
             onClick: () => {
               Ajax().Metrics.captureEvent(`${Events.catalogView}:previewData`, {
@@ -256,9 +257,7 @@ export const SidebarComponent = ({ dataObj, id }) => {
           ]),
           h(ButtonPrimary, {
             disabled: (isWorkspace(dataObj) || isDatarepoSnapshot(dataObj)) && (access !== datasetAccessTypes.GRANTED || tdrSnapshotPreparePolling),
-            tooltip: (isWorkspace(dataObj) || isDatarepoSnapshot(dataObj)) ?
-              access === datasetAccessTypes.GRANTED ? '' : uiMessaging.controlledFeatureTooltip :
-              uiMessaging.unsupportedDatasetTypeTooltip('preparing for analysis'),
+            tooltip: actionTooltip('preparing for analysis'),
             style: { fontSize: 16, textTransform: 'none', height: 'unset', width: sidebarButtonWidth, marginTop: 20 },
             onClick: () => {
               importDataToWorkspace(dataObj)
