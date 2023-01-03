15233c95ce85db0a59f84c1a54a97f5a7bfda0d0
jdcanas <jcanas@broadinstitute.com>
2023-01-03 15:19:05 -0800

    attempt fix

--- src/pages/Environments.js
+++ src/pages/Environments.js
@@ -456,8 +456,8 @@ const Environments = () => {
   // created, workspace namespace (a.k.a billing project) value used to equal the google project.
   // Therefore we use google project if the namespace label is not defined.
   const renderWorkspaceForApps = app => {
-    const { appType, googleProject, labels: { saturnWorkspaceNamespace = googleProject, saturnWorkspaceName } } = app
-    return getWorkspaceCell(saturnWorkspaceNamespace, saturnWorkspaceName, appType, false)
+    const { appType, cloudContext: { cloudResource }, labels: { saturnWorkspaceName } } = app
+    return getWorkspaceCell(cloudResource, saturnWorkspaceName, appType, false)
   }
 
   const renderWorkspaceForRuntimes = runtime => {
