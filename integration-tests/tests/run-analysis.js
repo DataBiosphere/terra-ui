c0d416875ed08db77082d3f64393f0e783089d49
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-01-15 11:39:20 -0500

    [PROD-780] Disable run-analysis and run-rstudio e2e tests (#3677)

--- integration-tests/tests/run-analysis.js
+++ integration-tests/tests/run-analysis.js
@@ -75,4 +75,5 @@ registerTest({
   name: 'run-analysis',
   fn: testRunAnalysisFn,
   timeout: 20 * 60 * 1000,
+  targetEnvironments: [], // Disabled due to flakiness, perhaps caused by IAM propagation delays
 })
