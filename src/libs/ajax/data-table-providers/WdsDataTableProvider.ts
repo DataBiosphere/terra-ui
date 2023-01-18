e625398e8f5ea520361d9ab07a4e72ffbe329d24
Jordan Safer <jordansafer2@gmail.com>
2023-01-18 16:33:24 -0500

    Hide upload while WDS is provisioning

--- src/libs/ajax/data-table-providers/WdsDataTableProvider.ts
+++ src/libs/ajax/data-table-providers/WdsDataTableProvider.ts
@@ -86,7 +86,7 @@ const getRelationParts = (val: unknown): string[] => {
 export const getWdsUrl = apps => {
   // look explicitly for an app named 'cbas-wds-default'. If found, use it, even if it isn't running
   // this handles the case where the user has explicitly shut down the app
-  const namedApp = apps.filter(app => app.appType === 'CROMWELL' && app.appName === 'cbas-wds-default')
+  const namedApp = apps.filter(app => app.appType === 'CROMWELL' && app.appName === 'cbas-wds-default' && app.status === 'RUNNING')
   if (namedApp.length === 1) {
     return namedApp[0].proxyUrls.wds
   }
