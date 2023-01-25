5876814dc0e5626de0f78a742807d86d013cb6f8
Bria Morgan <bmorgan@broadinstitute.org>
2023-01-25 11:03:29 -0500

    linting

--- src/components/data/WdsTroubleshooter.js
+++ src/components/data/WdsTroubleshooter.js
@@ -1,7 +1,7 @@
 import _ from 'lodash/fp'
 import { Fragment, useState } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
-import { ButtonPrimary } from 'src/components/common'
+import { ButtonPrimary, ClipboardButton } from 'src/components/common'
 import Modal from 'src/components/Modal'
 import { Ajax } from 'src/libs/ajax'
 import { useCancellation, useOnMount } from 'src/libs/react-utils'
@@ -14,7 +14,7 @@ const getWdsUrl = apps => {
   // this handles the case where the user has explicitly shut down the app
   const namedApp = apps.filter(app => app.appType === 'CROMWELL' && app.appName === 'cbas-wds-default')
   if (namedApp.length === 1) {
-    return namedApp[0].proxyUrls.wds
+    return namedApp[0]
   }
   // if we didn't find the expected app 'cbas-wds-default', go hunting:
   const candidates = apps.filter(app => app.appType === 'CROMWELL' && app.status === 'RUNNING')
@@ -35,6 +35,8 @@ export const WdsTroubleshooter = ({ onDismiss, workspaceId }) => {
   const [wdsResponsive, setWdsResponsive] = useState(false)
   const [version, setVersion] = useState({})
   const [wdsStatus, setWdsStatus] = useState({})
+  const [wdsDbStatus, setWdsDbStatus] = useState({})
+  const [wdsPingStatus, setWdsPingStatus] = useState({})
   const [appFound, setAppFound] = useState('')
   const [appRunning, setAppRunning] = useState('')
   const [proxyUrl, setProxyUrl] = useState('')
@@ -55,6 +57,8 @@ export const WdsTroubleshooter = ({ onDismiss, workspaceId }) => {
       })
       Ajax(signal).WorkspaceData.getStatus(foundApp.proxyUrls?.wds).then(res => {
         setWdsStatus(res.status)
+        setWdsDbStatus(res.components?.db?.status)
+        setWdsPingStatus(res.components?.ping?.status)
       })
     })
   })
@@ -70,12 +74,30 @@ export const WdsTroubleshooter = ({ onDismiss, workspaceId }) => {
     }, ['Done'])
   }, [
     h(div, {}, ['Leo app listing: ', h(Fragment, [leoOk.length, ' app(s) total'])]),
-    h(div, {}, ['app found: ', h(Fragment, [appFound])]),
-    h(div, {}, ['app running: ', h(Fragment, [appRunning])]),
-    h(div, {}, ['app proxy url: ', h(div, { style: _.merge({ width: '400px' }, Style.noWrapEllipsis) }, [proxyUrl])]),
+    h(div, {}, ['WDS app found: ', h(Fragment, [appFound, h(ClipboardButton, {
+      'aria-label': 'Copy found app name to clipboard',
+      className: 'cell-hover-only',
+      style: { marginLeft: '1rem' },
+      text: appFound
+    })])]),
+    h(div, {}, ['WDS app status: ', h(Fragment, [appRunning])]),
+    h(div, {}, ['App proxy url: ', h(Fragment, [h(div, { style: _.merge({ width: '400px' }, Style.noWrapEllipsis) }, [proxyUrl]), h(ClipboardButton, {
+      'aria-label': 'Copy proxy url to clipboard',
+      className: 'cell-hover-only',
+      style: { marginLeft: '1rem' },
+      text: proxyUrl
+    })])]),
     h(div, {}, ['WDS responding: ', h(Fragment, [JSON.stringify(wdsResponsive)])]),
-    h(div, {}, ['WDS version: ', h(Fragment, [JSON.stringify(version)])]),
+    h(div, {}, ['WDS version: ', h(Fragment, [JSON.stringify(version), h(ClipboardButton, {
+      'aria-label': 'Copy WDS version to clipboard',
+      className: 'cell-hover-only',
+      style: { marginLeft: '1rem' },
+      text: version
+    })])]),
     h(div, {}, ['WDS status: ', h(Fragment, [JSON.stringify(wdsStatus)])]),
-    h(div, {}, ['WDS default instance exists: ', h(Fragment, ['tbd - need API support'])]),
+    h(div, {}, ['WDS DB status: ', h(Fragment, [JSON.stringify(wdsDbStatus)])]),
+    h(div, {}, ['WDS ping status: ', h(Fragment, [JSON.stringify(wdsPingStatus)])]),
+    //TODO: Implement doesSchemaExist API in WDS, then call it
+    // h(div, {}, ['WDS default instance exists: ', h(Fragment, ['tbd - need API support'])]),
   ])
 }
