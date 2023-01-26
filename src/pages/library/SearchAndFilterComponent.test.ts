8271ac287c6a24d2b31bbde55a01c59d520e34ed
Sky Rubenstein <blrubenstein@wpi.edu>
2023-01-26 10:02:50 -0500

    [DC-631] Refactor filter code early PR (#3721)

---
+++ src/pages/library/SearchAndFilterComponent.test.ts
@@ -0,0 +1,78 @@
+import * as _ from 'lodash/fp'
+import {
+  FilterSection,
+  getMatchingDataForSection,
+  getMatchingDataForSectionList,
+  listItemsMatchForSectionEntry, sectionEntrySelected
+} from 'src/pages/library/SearchAndFilterComponent'
+
+
+type TestStrings =
+    'A quick brown fox jumped over a lazy gray dog' |
+    'aeio' |
+    'stock' |
+    'styx' |
+    'xylophone'
+
+const testStrings: Record<TestStrings, TestStrings> = {
+  'A quick brown fox jumped over a lazy gray dog': 'A quick brown fox jumped over a lazy gray dog',
+  aeio: 'aeio',
+  stock: 'stock',
+  styx: 'styx',
+  xylophone: 'xylophone'
+}
+
+// Arrange
+const FILTERS: FilterSection<string>[] = [
+  {
+    header: 'Contains vowel',
+    matchBy: (listValue, sectionEntry) => _.includes(sectionEntry, listValue),
+    values: ['a', 'e', 'i', 'o', 'u']
+  },
+  {
+    header: 'Starts with',
+    matchBy: (listValue, sectionEntry) => _.startsWith(sectionEntry, listValue),
+    values: ['x', 'y']
+  }
+]
+
+describe('library/common', () => {
+  it('matches correctly for a section entry', () => {
+    // Act
+    const listItemsMatching = listItemsMatchForSectionEntry('a', FILTERS[0].matchBy, _.values(testStrings))
+    // Assert
+    expect(listItemsMatching).toContain(testStrings['A quick brown fox jumped over a lazy gray dog'])
+    expect(listItemsMatching).toContain(testStrings.aeio)
+    expect(listItemsMatching).not.toContain(testStrings.stock)
+    expect(listItemsMatching).not.toContain(testStrings.styx)
+    expect(listItemsMatching).not.toContain(testStrings.xylophone)
+  })
+
+  it('matches on union for a section', () => {
+    // Act
+    const listItemsMatching = getMatchingDataForSection(FILTERS[0], _.values(testStrings))
+    // Assert
+    expect(listItemsMatching).toContain(testStrings['A quick brown fox jumped over a lazy gray dog'])
+    expect(listItemsMatching).toContain(testStrings.aeio)
+    expect(listItemsMatching).toContain(testStrings.stock)
+    expect(listItemsMatching).not.toContain(testStrings.styx)
+    expect(listItemsMatching).toContain(testStrings.xylophone)
+  })
+
+  it('matches on intersection for a list of sections', () => {
+    // Act
+    const listItemsMatching = getMatchingDataForSectionList(FILTERS, _.values(testStrings))
+    // Assert
+    expect(listItemsMatching).not.toContain(testStrings['A quick brown fox jumped over a lazy gray dog'])
+    expect(listItemsMatching).not.toContain(testStrings.aeio)
+    expect(listItemsMatching).not.toContain(testStrings.stock)
+    expect(listItemsMatching).not.toContain(testStrings.styx)
+    expect(listItemsMatching).toContain(testStrings.xylophone)
+  })
+
+  it('checks properly if a section entry is selected', () => {
+    // Assert
+    expect(sectionEntrySelected(FILTERS[0], 'a', FILTERS)).toBeTruthy()
+    expect(sectionEntrySelected(FILTERS[0], 'x', FILTERS)).toBeFalsy()
+  })
+})
--- src/pages/library/dataBrowser-utils.test.js
+++ src/pages/library/dataBrowser-utils.test.js
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react'
-import { h } from 'react-hyperscript-helpers'
+import * as _ from 'lodash/fp'
 import { brands } from 'src/libs/brands'
 import { dataCatalogStore } from 'src/libs/state'
 import {
-  datarepoSnapshotUrlFragment, datasetAccessTypes, DatasetReleasePolicyDisplayInformation, getDatasetAccessType,
-  prepareDatasetsForDisplay, workspaceUrlFragment
+  datarepoSnapshotUrlFragment, datasetAccessTypes, getAssayCategoryListFromDataset, getConsortiumTitlesFromDataset, getDataModalityListFromDataset,
+  getDatasetAccessType,
+  getDatasetReleasePoliciesDisplayInformation,
+  makeDatasetReleasePolicyDisplayInformation, prepareDatasetsForDisplay, workspaceUrlFragment
 } from 'src/pages/library/dataBrowser-utils'
+import { TEST_DATASET_ONE } from 'src/pages/library/test-datasets'
 
 
 beforeEach(() => {
@@ -31,17 +34,53 @@ describe('dataBrowser-utils', () => {
   })
 
   it('finds the correct data use policy to display if it exists', () => {
-    const { getByText } = render(h(DatasetReleasePolicyDisplayInformation, { 'TerraDCAT_ap:hasDataUsePermission': 'DUO:0000007' }))
+    const { getByText } = render(makeDatasetReleasePolicyDisplayInformation('DUO:0000007'))
     expect(getByText('Disease specific research')).toBeTruthy()
   })
 
   it('uses unspecified as the data use policy if undefined', () => {
-    const { getByText } = render(h(DatasetReleasePolicyDisplayInformation, { 'TerraDCAT_ap:hasDataUsePermission': undefined }))
+    const { getByText } = render(makeDatasetReleasePolicyDisplayInformation(undefined))
     expect(getByText('Unspecified')).toBeTruthy()
   })
 
   it('uses given data use policy as the data use policy if unknown', () => {
-    const { getByText } = render(h(DatasetReleasePolicyDisplayInformation, { 'TerraDCAT_ap:hasDataUsePermission': 'Something else' }))
+    const { getByText } = render(makeDatasetReleasePolicyDisplayInformation('Something else'))
     expect(getByText('Something else')).toBeTruthy()
+  })
+
+  it('generates consortium titles properly', () => {
+    const expectedValues = ['The Dog Land', 'Cats R Us']
+    const result = getConsortiumTitlesFromDataset(TEST_DATASET_ONE)
+    _.forEach(expectedValue => expect(result).toContain(expectedValue), expectedValues)
+  })
+
+  it('generates data use policy properly', () => {
+    expect(
+      getDatasetReleasePoliciesDisplayInformation(TEST_DATASET_ONE['TerraDCAT_ap:hasDataUsePermission']))
+      .toMatchObject({ label: 'GRU', description: 'General research use' })
+  })
+
+  it('generates data use policy in the case of an unknown policy correctly', () => {
+    expect(
+      getDatasetReleasePoliciesDisplayInformation('abcdef'))
+      .toMatchObject({ label: 'abcdef' })
+  })
+
+  it('generates data use policy in the case of an undefined policy correctly', () => {
+    expect(
+      getDatasetReleasePoliciesDisplayInformation(undefined))
+      .toMatchObject({ label: 'Unspecified', description: 'No specified dataset release policy' })
+  })
+
+  it('generates data modality list correctly', () => {
+    const expectedValues = ['Epigenomic', 'Genomic', 'Transcriptomic']
+    const result = getDataModalityListFromDataset(TEST_DATASET_ONE)
+    _.forEach(expectedValue => expect(result).toContain(expectedValue), expectedValues)
+  })
+
+  it('generates data assay categories correctly', () => {
+    const expectedValues = ['nuq-seq', 'RNA-seq']
+    const result = getAssayCategoryListFromDataset(TEST_DATASET_ONE)
+    _.forEach(expectedValue => expect(result).toContain(expectedValue), expectedValues)
   })
 })
