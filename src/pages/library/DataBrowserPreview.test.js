95a501b19dd044557eb2f46b438a5780bce5f493
Sky Rubenstein <blrubenstein@wpi.edu>
2023-01-26 13:02:40 -0500

    Test changes

--- src/pages/library/DataBrowserPreview.test.js
+++ src/pages/library/DataBrowserPreview.test.js
@@ -1,31 +1,64 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { formatTableCell } from 'src/pages/library/DataBrowserPreview'
 
 
 describe('DataBrowserPreview', () => {
-  it('renders json as view json', () => {
-    const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent: '{"key": "value"}', rowIndex: 0, table: 'table', setViewJSON: () => {} }))
+  const testJsonButton = async (cellContent, cellContentTransformation = cellContent => cellContent) => {
+    // Arrange
+    let clicked = false
+    const user = userEvent.setup()
+    // Act
+    const { getByText } = render(formatTableCell({
+      cellKey: 'string', cellContent, rowIndex: 0, table: 'table', setViewJSON: args => {
+        clicked = args
+      }
+    }))
+    const jsonButton = getByText('View JSON')
+    await user.click(jsonButton)
+    // Assert
     expect(getByText('View JSON')).toBeTruthy()
+    expect(clicked).toStrictEqual({
+      cellData: cellContentTransformation(cellContent),
+      title: 'table, Row 0 - string'
+    })
+  }
+
+  it('renders json as view json clickable button', async () => {
+    // Arrange
+    const cellContent = '{"key": "value"}'
+    // Act & Assert
+    await testJsonButton(cellContent, cellContent => JSON.parse(cellContent))
   })
 
-  it('renders object as view json', () => {
-    const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent: { key: 'value' }, rowIndex: 0, table: 'table', setViewJSON: () => {} }))
-    expect(getByText('View JSON')).toBeTruthy()
+  it('renders object as view json clickable button', async () => {
+    const cellContent = { key: 'value' }
+    // Act & Assert
+    await testJsonButton(cellContent)
   })
 
-  it('renders array as view json', () => {
-    const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent: ['a', 'b', 'c'], rowIndex: 0, table: 'table', setViewJSON: () => {} }))
-    expect(getByText('View JSON')).toBeTruthy()
+  it('renders array as view json', async () => {
+    // Arrange
+    const cellContent = ['a', 'b', 'c']
+    // Act & Assert
+    await testJsonButton(cellContent)
   })
 
   it('renders numbers as number', () => {
-    const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent: 1, rowIndex: 0, table: 'table', setViewJSON: () => {} }))
-    expect(getByText('1')).toBeTruthy()
+    // Arrange
+    const cellContent = 1
+    // Act
+    const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent, rowIndex: 0, table: 'table', setViewJSON: () => {} }))
+    // Assert
+    expect(getByText(cellContent.toString())).toBeTruthy()
   })
 
   it('renders non number, non object as toString version', () => {
+    // Arrange
     const cellContent = 'abc'
+    // Act
     const { getByText } = render(formatTableCell({ cellKey: 'string', cellContent, rowIndex: 0, table: 'table', setViewJSON: () => {} }))
+    // Assert
     expect(getByText(cellContent)).toBeTruthy()
   })
 })
