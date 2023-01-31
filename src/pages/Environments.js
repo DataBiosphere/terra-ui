242ea1c27fa3a9f394675c2d9f43a3e9213a7a27
trock-broadinst <123021647+trock-broadinst@users.noreply.github.com>
2023-01-31 15:36:13 -0500

    add revised text block

--- src/pages/Environments.js
+++ src/pages/Environments.js
@@ -55,8 +55,19 @@ const DeleteRuntimeModal = ({
   }, [
     div({ style: { lineHeight: 1.5 } }, [
       persistentDiskId ?
-        (isGcpContext(cloudContext) && h(LabeledCheckbox, { checked: deleteDisk, onChange: setDeleteDisk }, [
+        (isGcpContext(cloudContext) ? h(LabeledCheckbox, { checked: deleteDisk, onChange: setDeleteDisk }, [
           span({ style: { fontWeight: 600 } }, [' Also delete the persistent disk and all files on it'])
+        ]) : div({
+          style: {
+            backgroundColor: colors.accent(0.2),
+            display: 'flex',
+            borderRadius: 5,
+            padding: '0.5rem 1rem',
+            marginTop: '1rem',
+            marginBottom: '1rem'
+          }
+        }, [
+          p(['Deleting your Virtual Machine will also delete the attached persistent disk'])
         ])) :
         p([
           'Deleting this cloud environment will also ', span({ style: { fontWeight: 600 } }, ['delete any files on the associated hard disk.'])
