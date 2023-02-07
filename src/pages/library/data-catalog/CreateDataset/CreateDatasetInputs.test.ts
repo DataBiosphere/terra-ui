1dbe4744fccf0fd5b86fdc979560ce6b01064ce4
Sky Rubenstein <blrubenstein@wpi.edu>
2023-02-07 14:18:28 -0500

    Add number input

--- src/pages/library/data-catalog/CreateDataset/CreateDatasetInputs.test.ts
+++ src/pages/library/data-catalog/CreateDataset/CreateDatasetInputs.test.ts
@@ -1,32 +1,40 @@
 import { fireEvent, render } from '@testing-library/react'
 import { h } from 'react-hyperscript-helpers'
-import { StringInput } from 'src/pages/library/data-catalog/CreateDataset/CreateDatasetInputs'
+import {
+  CatalogNumberInput,
+  StringInput
+} from 'src/pages/library/data-catalog/CreateDataset/CreateDatasetInputs'
 
 
 describe('CreateDatasetInputs', () => {
+  const testElementWithValue = (element, getByText, getCurrentValue, getExpectedValue) => {
+    expect(getByText('Title')).toBeTruthy()
+    expect(element?.value).toBe(getCurrentValue().toString())
+    element !== null && fireEvent.change(element, { target: { value: getExpectedValue() } })
+    expect(getCurrentValue()).toBe(getExpectedValue())
+  }
   it('Renders a StringInput with the title and value', () => {
+    let currentValue = 'Value'
+    const expectedValue = 'New Value'
     const { getByText, getByLabelText } = render(h(StringInput, {
       title: 'Title',
-      value: 'Value',
-      placeholder: '',
-      onChange: value => value
-    }))
-    const input = getByLabelText('Title').closest('input')
-    expect(getByText('Title')).toBeTruthy()
-    expect(input?.value).toBe('Value')
-  })
-
-  it('Calls onChange with StringInput', () => {
-    let currentValue = ''
-    const expectedValue = 'Value'
-    const { getByLabelText } = render(h(StringInput, {
-      title: 'Title',
-      value: '',
+      value: currentValue,
       placeholder: '',
       onChange: value => currentValue = value
     }))
     const input = getByLabelText('Title').closest('input')
-    input !== null && fireEvent.change(input, { target: { value: expectedValue } })
-    expect(currentValue).toBe(expectedValue)
+    testElementWithValue(input, getByText, () => currentValue, () => expectedValue)
+  })
+
+  it('Renders a NumberInput with the title and value', () => {
+    let currentValue = 0
+    const expectedValue = 5
+    const { getByText, getByLabelText } = render(h(CatalogNumberInput, {
+      title: 'Title',
+      value: currentValue,
+      onChange: value => currentValue = value
+    }))
+    const input = getByLabelText('Title').closest('input')
+    testElementWithValue(input, getByText, () => currentValue, () => expectedValue)
   })
 })
