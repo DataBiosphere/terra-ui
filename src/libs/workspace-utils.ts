c6883c0e31462eb998689d259fc6e38fad40903b
jdcanas <jcanas@broadinstitute.com>
2023-02-08 12:18:19 -0800

    fix

--- src/libs/workspace-utils.ts
+++ src/libs/workspace-utils.ts
@@ -56,7 +56,7 @@ export const isAzureWorkspace = (workspace: BaseWorkspace): workspace is AzureWo
 }
 
 export const isGoogleWorkspace = (workspace: BaseWorkspace): workspace is GoogleWorkspace => {
-  return workspace && isGoogleWorkspaceInfo(workspace.workspace)
+  return isGoogleWorkspaceInfo(workspace.workspace)
 }
 
 export const getCloudProviderFromWorkspace = (workspace: BaseWorkspace): CloudProviderType => isAzureWorkspace(workspace) ? cloudProviderTypes.AZURE : cloudProviderTypes.GCP
