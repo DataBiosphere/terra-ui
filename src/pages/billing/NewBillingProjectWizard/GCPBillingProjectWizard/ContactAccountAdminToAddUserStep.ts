7bb96856c73abc7c56d042303fb9c2484489cfa3
Blake Geno <bgeno@broadinstitute.org>
2023-02-10 17:43:51 -0500

    add component using input element for checkbox

--- src/pages/billing/NewBillingProjectWizard/GCPBillingProjectWizard/ContactAccountAdminToAddUserStep.ts
+++ src/pages/billing/NewBillingProjectWizard/GCPBillingProjectWizard/ContactAccountAdminToAddUserStep.ts
@@ -1,6 +1,8 @@
-import { div, h, span } from 'react-hyperscript-helpers'
-import { LabeledCheckbox } from 'src/components/common'
+import { CSSProperties, Fragment, ReactNode } from 'react'
+import { div, h, HTMLElementProps, input, label, span } from 'react-hyperscript-helpers'
+import { useUniqueId } from 'src/components/common'
 import { Ajax } from 'src/libs/ajax'
+import colors from 'src/libs/colors'
 import Events from 'src/libs/events'
 import { styles } from 'src/pages/billing/NewBillingProjectWizard/GCPBillingProjectWizard/GCPBillingProjectWizard'
 import { ExternalLink } from 'src/pages/billing/NewBillingProjectWizard/StepWizard/ExternalLink'
@@ -26,24 +28,17 @@ export const ContactAccountAdminToAddUserStep = ({ isActive, ...props }: Contact
       ' as a Billing Account User',
       span({ style: { fontWeight: 'bold' } }, [' to your organization\'s billing account.'])
     ]),
-    div({ style: { width: '30%' } }, [
-      h(LabeledCheckbox, {
+    div({ style: { width: '30%', display: 'flex', flexDirection: 'row' } }, [
+      h(CheckboxInput, {
         checked: props.verifiedUsersAdded,
         disabled: !isActive && !props.isFinished, // disabled when we haven't gotten to this step - still allow users to go back, though
         onChange: () => {
           Ajax().Metrics.captureEvent(Events.billingGCPCreationStep3VerifyUserAdded)
           props.setVerifiedUsersAdded(!props.verifiedUsersAdded)
-        }
-      }, [
-        // this negative margin here is hideous but necessary:
-        // giving the div that contains the checkbox {display: flex, flexDirection: row} gives the label the correct layout
-        // but it also breaks the checkbox, because it's implemented as layered svg icons, making it very sensitive to layout changes
-        // if the component is changed to use an input element with checkbox, we should be able to just use flexbox and be done with all the tweaks
-        span({ style: { float: 'right', ...styles.radioButtonLabel, marginLeft: '2rem', marginTop: '-1.3rem' } }, [
-          'I have verified the user has been added to my account (requires reauthentication)'
-        ])
-      ]),
-    ])
+        },
+      }, ['I have verified the user has been added to my account (requires reauthentication)']),
+    ]),
+
   ]),
   ExternalLink({
     style: { marginTop: '2rem' },
@@ -52,3 +47,25 @@ export const ContactAccountAdminToAddUserStep = ({ isActive, ...props }: Contact
   })
 ])
 
+interface RealCheckboxProps extends HTMLElementProps<'input'> {
+  style?: CSSProperties
+  labelStyle?: CSSProperties
+  children?: ReactNode[]
+
+}
+
+const CheckboxInput = ({ children = [], ...props }: RealCheckboxProps) => {
+  const id = useUniqueId()
+  return h(Fragment, [
+    input({
+      type: 'checkbox', id,
+      style: {
+        backgroundColor: props.disabled ? colors.light(1.2) : (props.checked ? colors.accent() : 'white'),
+        borderColor: props.checked ? colors.accent(1.2) : colors.dark(0.75),
+        color: props.disabled ? colors.dark(0.75) : 'white', ...props.style
+      },
+      ...props
+    }),
+    label({ htmlFor: id, style: { ...styles.radioButtonLabel, ...props.labelStyle } }, [children])
+  ])
+}
