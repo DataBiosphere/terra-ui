9eb249a5bba77e98cd1c7f4a75d7e6bdc4adbfe3
Sky Rubenstein <blrubenstein@wpi.edu>
2023-02-13 14:37:22 -0500

    Add tests for accessURL and creator

--- src/pages/library/data-catalog/CreateDataset/CreateDataset.test.ts
+++ src/pages/library/data-catalog/CreateDataset/CreateDataset.test.ts
@@ -29,4 +29,42 @@ describe('CreateDataset', () => {
   })
 
   // No description test due to label bug with markdown input
+
+  it('Validates for accessURL', async () => {
+    const user = userEvent.setup()
+
+    render(h(CreateDataset, {
+      storageSystem: 'wks',
+      storageSourceId: 'abcdef'
+    }))
+    expect(await screen.queryAllByText('Dcat:access url can\'t be blank').length).toBe(1)
+    // One extra for the publication list
+    expect(await screen.queryAllByText('Dcat:access url is not a valid url').length).toBe(2)
+    const accessURLInput = screen.getByLabelText('Access URL *')
+    await user.type(accessURLInput, 'title')
+    expect(await screen.queryByText('Dcat:access url can\'t be blank')).toBeFalsy()
+    expect(await screen.queryAllByText('Dcat:access url is not a valid url').length).toBe(3)
+    await user.clear(accessURLInput)
+    await user.type(accessURLInput, 'https://url.com')
+    expect(await screen.queryAllByText('Dcat:access url is not a valid url').length).toBe(1)
+    await user.clear(accessURLInput)
+    // Count increases because we show the red error message after access URL has been touched
+    expect(await screen.queryAllByText('Dcat:access url can\'t be blank').length).toBe(2)
+  })
+
+  it('Validates for creator', async () => {
+    const user = userEvent.setup()
+
+    render(h(CreateDataset, {
+      storageSystem: 'wks',
+      storageSourceId: 'abcdef'
+    }))
+    expect(await screen.queryAllByText('Dct:creator can\'t be blank').length).toBe(1)
+    const creatorInput = screen.getByLabelText('Dataset Creator *')
+    await user.type(creatorInput, 'creator')
+    expect(await screen.queryByText('Dct:creator can\'t be blank')).toBeFalsy()
+    await user.clear(creatorInput)
+    // Count increases because we show the red error message after creator has been touched
+    expect(await screen.queryAllByText('Dct:creator can\'t be blank').length).toBe(2)
+  })
 })
