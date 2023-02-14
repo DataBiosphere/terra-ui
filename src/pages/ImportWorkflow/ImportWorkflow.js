9b1374caccd7c745302a955145a539da784b1bdf
Nick Watts <nwatts@broadinstitute.org>
2023-02-14 13:22:16 -0500

    Add some tests for ImportWorkflow page

--- src/pages/ImportWorkflow/ImportWorkflow.js
+++ src/pages/ImportWorkflow/ImportWorkflow.js
@@ -11,14 +11,16 @@ import { WorkspaceImporter } from 'src/components/workspace-utils'
 import importBackground from 'src/images/hex-import-background.svg'
 import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
-import { reportError, withErrorReporting } from 'src/libs/error'
+import { reportError } from 'src/libs/error'
 import Events, { extractWorkspaceDetails } from 'src/libs/events'
 import * as Nav from 'src/libs/nav'
-import { useCancellation, useOnMount } from 'src/libs/react-utils'
+import { useCancellation } from 'src/libs/react-utils'
 import * as Style from 'src/libs/style'
 import * as Utils from 'src/libs/utils'
 import { workflowNameValidation } from 'src/libs/workflow-utils'
 import validate from 'validate.js'
+
+import { useDockstoreWdl } from './useDockstoreWdl'
 
 
 const styles = {
@@ -37,25 +39,13 @@ const styles = {
   }
 }
 
-const DockstoreImporter = ({ path, version, source }) => {
+export const ImportWorkflow = ({ path, version, source }) => {
   const [isBusy, setIsBusy] = useState(false)
-  const [wdl, setWdl] = useState(undefined)
-  const [workflowName, setWorkflowName] = useState('')
+  const [workflowName, setWorkflowName] = useState(_.last(path.split('/')))
+
+  const { wdl, status: wdlStatus } = useDockstoreWdl({ path, version, isTool: source === 'dockstoretools' })
 
   const signal = useCancellation()
-
-  const loadWdl = _.flow(
-    withErrorReporting('Error loading WDL'),
-    Utils.withBusyState(setIsBusy)
-  )(async () => {
-    const wdl = await Ajax(signal).Dockstore.getWdl({ path, version, isTool: source === 'dockstoretools' })
-    setWdl(wdl)
-    setWorkflowName(_.last(path.split('/')))
-  })
-
-  useOnMount(() => {
-    loadWdl()
-  })
 
   const doImport = Utils.withBusyState(setIsBusy, async workspace => {
     const { name, namespace } = workspace
@@ -138,20 +128,20 @@ const DockstoreImporter = ({ path, version, source }) => {
           h(WorkspaceImporter, { id, onImport: doImport, additionalErrors: errors })
         ])
       ]),
-      isBusy && spinnerOverlay
+      (wdlStatus === 'Loading' || isBusy) && spinnerOverlay
     ])
   ])
 }
 
 
-const Importer = ({ source, item }) => {
+const ImportWorkflowPage = ({ source, item }) => {
   const [path, version] = item.split(':')
 
   return h(FooterWrapper, [
     h(TopBar, { title: 'Import Workflow' }),
     div({ role: 'main', style: { flexGrow: 1 } }, [
       Utils.cond(
-        [source === 'dockstore' || source === 'dockstoretools', () => h(DockstoreImporter, { path, version, source })],
+        [source === 'dockstore' || source === 'dockstoretools', () => h(ImportWorkflow, { path, version, source })],
         () => `Unknown source '${source}'`
       )
     ])
@@ -163,7 +153,7 @@ export const navPaths = [
   {
     name: 'import-workflow',
     path: '/import-workflow/:source/:item(.*)',
-    component: Importer,
+    component: ImportWorkflowPage,
     encode: _.identity,
     title: 'Import Workflow'
   }, {
---
+++ src/pages/ImportWorkflow/useDockstoreWdl.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { Dockstore, DockstoreWorkflowVersionDescriptor } from 'src/libs/ajax/Dockstore'
+import { useLoadedData } from 'src/libs/ajax/loaded-data/useLoadedData'
+import { reportError } from 'src/libs/error'
+import { useCancellation } from 'src/libs/react-utils'
+
+
+export const useDockstoreWdl = (workflow: DockstoreWorkflowVersionDescriptor) => {
+  const { path, version, isTool } = workflow
+
+  const [wdlData, loadWdlData] = useLoadedData<string>({
+    onError: err => reportError('Error loading WDL', err),
+  })
+
+  const signal = useCancellation()
+  useEffect(
+    () => { loadWdlData(() => Dockstore(signal).getWdl({ path, version, isTool })) },
+    // loadWdlData changes on each render, so cannot depend on it
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [path, version, isTool],
+  )
+
+  if (wdlData.status !== 'None') {
+    const { state: wdl, ...rest } = wdlData
+    return { ...rest, wdl }
+  } else {
+    return wdlData
+  }
+}
