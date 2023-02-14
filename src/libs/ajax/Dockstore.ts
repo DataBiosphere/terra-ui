af3cb8b7675ad493cac9b2ae5a96cafffa95a4ce
Nick Watts <nwatts@broadinstitute.org>
2023-02-14 11:18:19 -0500

    Split Dockstore ajax methods into a separate module

---
+++ src/libs/ajax/Dockstore.ts
@@ -0,0 +1,70 @@
+import * as qs from 'qs'
+
+import { fetchDockstore, fetchOk } from './ajax-common'
+
+
+type DockstoreWorkflowDescriptor = {
+  path: string
+  isTool: boolean
+}
+
+type DockstoreWorkflowVersionDescriptor = DockstoreWorkflowDescriptor & {
+  version: string
+}
+
+type DockstoreWorkflowVersion = {
+  'descriptor-type': unknown[]
+  dockerfile: boolean
+  id: string
+  image: string
+  'meta-version': string
+  name: string
+  url: string
+  verified: boolean
+  'verified-source': string
+}
+
+type DockstoreWorkflow = {
+  author: string
+  contains: unknown[]
+  description: string
+  id: string
+  'meta-version': string
+  organization: string
+  signed: boolean
+  toolclass: {
+    description: string
+    workflow: string
+    name: string
+  }
+  toolname: string
+  url: string
+  verified: boolean
+  'verified-source': string
+  versions: DockstoreWorkflowVersion[]
+}
+
+type ListToolsParams = {
+  organization?: string
+}
+
+// %23 = '#', %2F = '/'
+const workflowVersionsPath = ({ path, isTool }: DockstoreWorkflowDescriptor) => {
+  return `api/ga4gh/v1/tools/${isTool ? '' : '%23workflow%2F'}${encodeURIComponent(path)}/versions`
+}
+
+export const Dockstore = (signal?: AbortSignal) => ({
+  getWdl: async ({ path, version, isTool }: DockstoreWorkflowVersionDescriptor): Promise<string> => {
+    const wdlPath = `${workflowVersionsPath({ path, isTool })}/${encodeURIComponent(version)}/WDL/descriptor`
+    const { url } = await fetchDockstore(wdlPath, { signal }).then(res => res.json())
+    return fetchOk(url, { signal }).then(res => res.text())
+  },
+
+  getVersions: ({ path, isTool }: DockstoreWorkflowDescriptor): Promise<DockstoreWorkflowVersion[]> => {
+    return fetchDockstore(workflowVersionsPath({ path, isTool }), { signal }).then(res => res.json())
+  },
+
+  listTools: (params: ListToolsParams = {}): Promise<DockstoreWorkflow[]> => {
+    return fetchDockstore(`api/ga4gh/v1/tools?${qs.stringify(params)}`, { signal }).then(res => res.json())
+  },
+})
