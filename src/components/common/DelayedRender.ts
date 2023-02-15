73d0a6152c98f7d9908a3aad7b4082a497aec718
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-02-15 10:54:56 -0500

    [UIE-16] Move DelayedRender to common components (#3795)

---
+++ src/components/common/DelayedRender.ts
@@ -0,0 +1,17 @@
+import { Fragment, PropsWithChildren, useEffect, useState } from 'react'
+import { h } from 'react-hyperscript-helpers'
+
+
+type DelayedRenderProps = PropsWithChildren<{
+  delay?: number
+}>
+
+export const DelayedRender = ({ children = null, delay = 1000 }: DelayedRenderProps) => {
+  const [shouldRender, setShouldRender] = useState(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => setShouldRender(true), delay)
+    return () => clearTimeout(timeout)
+  }, [delay])
+
+  return shouldRender ? h(Fragment, [children]) : null
+}
