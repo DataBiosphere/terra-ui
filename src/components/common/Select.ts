33c02c76a16eb2ff2499973c998685a51c214eec
Nick Watts <nwatts@broadinstitute.org>
2023-02-22 10:11:15 -0500

    Add types

--- src/components/common/Select.ts
+++ src/components/common/Select.ts
@@ -1,7 +1,14 @@
 import _ from 'lodash/fp'
 import { Children, useCallback, useEffect, useRef } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
-import RSelect, { components as RSelectComponents } from 'react-select'
+import RSelect, {
+  components as RSelectComponents,
+  GroupBase as RSelectGroupBase,
+  MultiValue as RSelectMultiValue,
+  Props as RSelectProps,
+  PropsValue as RSelectPropsValue,
+  SingleValue as RSelectSingleValue,
+} from 'react-select'
 import RAsyncCreatableSelect from 'react-select/async-creatable'
 import { AutoSizer, List } from 'react-virtualized'
 import { icon } from 'src/components/icons'
@@ -9,7 +16,7 @@ import colors from 'src/libs/colors'
 import { useLabelAssert, useOnMount, useUniqueId } from 'src/libs/react-utils'
 
 
-const commonSelectProps = {
+const commonSelectProps: Partial<RSelectProps> = {
   theme: base => _.merge(base, {
     colors: {
       primary: colors.accent(),
@@ -61,7 +68,7 @@ const commonSelectProps = {
   }
 }
 
-const formatGroupLabel = group => (
+const formatGroupLabel = (group: { label?: string }) => (
   div({
     style: {
       color: colors.dark(),
@@ -72,58 +79,126 @@ const formatGroupLabel = group => (
     }
   }, [group.label]))
 
-const BaseSelect = ({ value, newOptions, id, findValue, ...props }) => {
-  const newValue = props.isMulti ? _.map(findValue, value) : findValue(value)
+type BaseSelectProps<
+  Value,
+  Option extends { value: Value; label?: string },
+  IsMulti extends boolean,
+  Group extends RSelectGroupBase<Option>,
+> = Omit<RSelectProps<Option, IsMulti, Group>, 'getOptionLabel' | 'value'> & {
+  findValue: (value: Value) => Option | null
+  // react-select requires getOptionLabel. We provide a default, so it's optional.
+  getOptionLabel?: (option: Option) => string
+  // react-select's type for value does not take IsMulti into account.
+  value: IsMulti extends true ? RSelectMultiValue<Value> : RSelectSingleValue<Value>
+}
+
+/**
+ * Wraps RSelect.
+ *
+ * Provides defaults for formatGroupLabel, getOptionLabel, and inputId.
+ *
+ * Also (via findValue) allows passing value as the selected value instead of the selected option.
+ * For example:
+ * const options = [{ value: 'foo' }, { value: 'bar' }]
+ * h(BaseSelect, { options, value: 'foo' })
+ * instead of:
+ * const options = [{ value: 'foo' }, { value: 'bar' }]
+ * h(BaseSelect, { options, value: options[0] })
+ */
+const BaseSelect = <
+  Value,
+  Option extends { value: Value; label?: string },
+  IsMulti extends boolean,
+  Group extends RSelectGroupBase<Option>,
+> ({ value, id, findValue, ...props }: BaseSelectProps<Value, Option, IsMulti, Group>) => {
+  const newValue: RSelectPropsValue<Option> = props.isMulti ? _.compact(_.map(findValue, value as Value[])) : findValue(value as Value)
   const myId = useUniqueId()
   const inputId = id || myId
 
-  return h(RSelect, _.merge({
+  const ParameterizedRSelect = RSelect as typeof RSelect<Option, IsMulti, Group>
+  return h(ParameterizedRSelect, _.merge({
     inputId,
     ...commonSelectProps,
     getOptionLabel: ({ value, label }) => label || value.toString(),
     value: newValue || null, // need null instead of undefined to clear the select
-    options: newOptions,
     formatGroupLabel
   }, props))
 }
 
-/**
- * @param {Object} props - see {@link https://react-select.com/props#select-props}
- * @param props.value - a member of options
- * @param {Array} props.options - can be of any type; if objects, they should each contain a value and label, unless defining getOptionLabel
- * @param {string} [props.placeholder] - The placeholder value for the select
- * @param {Function} props.onChange - The function to call when a user makes a selection
- * @param {boolean} [props.isClearable] - whether the select can be cleared
- * @param {boolean} [props.isSearchable] - whether the select can be cleared
- * @param {boolean} [props.isMulti] - whether the select is multiselect or not
- * @param {string} [props.menuPlacement] - determines where the menu is placed
- * @param {Object} [props.styles] - custom styling for the select
- * @param {boolean} [props.isDisabled] - whether the select is disabled
- * @param {Function} [props.getOptionLabel] - a function to custom style the options
- * @param props.id - The HTML ID to give the form element
- */
-export const Select = ({ value, options, ...props }) => {
-  useLabelAssert('Select', { ...props, allowId: true })
-
-  const newOptions = options && !_.isObject(options[0]) ? _.map(value => ({ value }), options) : options
-  const findValue = target => _.find({ value: target }, newOptions)
-
-  return h(BaseSelect, { value, newOptions, findValue, ...props })
+export type SelectProps<
+  Value,
+  IsMulti extends boolean,
+  Option extends { value: Value; label?: string },
+> = Omit<BaseSelectProps<Value, Option, IsMulti, never>, 'findValue' | 'options'> & {
+  options: (Value | Option)[]
 }
 
 /**
- * @param {Object} props - see {@link https://react-select.com/props#select-props}
- * @param props.value - a member of an inner options object
- * @param {Array} props.options - an object with toplevel pairs of label:options where label is a group label and options is an array of objects containing value:label pairs
- * @param props.id - The HTML ID to give the form element
+ * @param props - see {@link https://react-select.com/props#select-props}
+ * @param props.value - a member of options
+ * @param props.options - can be of any type; if objects, they should each contain a value and label, unless defining getOptionLabel
+ * @param props.onChange - The function to call when a user makes a selection
+ * @param [props.getOptionLabel] - a function to custom style the options
+ * @param [props.id] - The HTML ID to give the form element
+ * @param [props.isClearable] - whether the select can be cleared
+ * @param [props.isDisabled] - whether the select is disabled
+ * @param [props.isSearchable] - whether the select can be cleared
+ * @param [props.isMulti] - whether the select is multiselect or not
+ * @param [props.menuPlacement] - determines where the menu is placed
+ * @param [props.placeholder] - The placeholder value for the select
+ * @param [props.styles] - custom styling for the select
  */
-export const GroupedSelect = ({ value, options, ...props }) => {
+export const Select = <
+  Value,
+  IsMulti extends boolean = false,
+  Option extends { value: Value; label?: string } = { value: Value; label: string },
+> ({ value, options = [], ...props }: SelectProps<Value, IsMulti, Option>) => {
+  useLabelAssert('Select', { ...props, allowId: true })
+
+  // Allows passing options as list of values instead of options objects.
+  // For example:
+  // options: ['foo', 'bar']
+  // instead of:
+  // options: [{ value: 'foo' }, { value: 'bar' }]
+  //
+  // Cast as Options[] is because TS can't figure out how the `!_isObject(options[0])` condition affects the type of newOptions.
+  const newOptions = (options && !_.isObject(options[0]) ? _.map(value => ({ value }), options) : options) as Option[]
+
+  const findValue = (target: Value) => (_.find({ value: target }, newOptions) || null) as Option | null
+
+  const ParameterizedBaseSelect = BaseSelect as typeof BaseSelect<Value, Option, IsMulti, never>
+  return h(ParameterizedBaseSelect, { value, options: newOptions, findValue, ...props })
+}
+
+export type GroupedSelectProps<
+  Value,
+  IsMulti extends boolean,
+  Option extends { value: Value; label?: string },
+  Group extends RSelectGroupBase<Option>,
+> = Omit<BaseSelectProps<Value, Option, IsMulti, Group>, 'findValue' | 'options'> & {
+  options: Group[]
+}
+
+/**
+ * @param props - see {@link https://react-select.com/props#select-props}
+ * @param props.value - a member of an inner options object
+ * @param props.options - an object with toplevel pairs of label:options where label is a group label and options is an array of objects containing value:label pairs
+ * @param [props.id] - The HTML ID to give the form element
+ */
+export const GroupedSelect = <
+  Value,
+  IsMulti extends boolean = false,
+  Option extends { value: Value; label?: string } = { value: Value; label?: string },
+  Group extends RSelectGroupBase<Option> = RSelectGroupBase<Option>,
+> ({ value, options = [], ...props }: GroupedSelectProps<Value, IsMulti, Option, Group>) => {
   useLabelAssert('GroupedSelect', { ...props, allowId: true })
 
-  const flattenedOptions = _.flatMap('options', options)
-  const findValue = target => _.find({ value: target }, flattenedOptions)
+  // cast because types don't carry through Lodash
+  const flattenedOptions = _.flatMap('options', options) as Option[]
+  const findValue = (target: Value) => (_.find({ value: target }, flattenedOptions) || null) as Option | null
 
-  return h(BaseSelect, { value, newOptions: options, findValue, ...props })
+  const ParameterizedBaseSelect = BaseSelect as typeof BaseSelect<Value, Option, IsMulti, Group>
+  return h(ParameterizedBaseSelect, { value, options, findValue, ...props })
 }
 
 export const AsyncCreatableSelect = props => {
@@ -136,13 +211,13 @@ export const AsyncCreatableSelect = props => {
 const VirtualizedMenuList = props => {
   const { children, focusedOption, getStyles, getValue, maxHeight } = props
 
-  const list = useRef()
+  const list = useRef<List>()
 
   const scrollToOptionForValue = useCallback(value => {
     const renderedOptions = Children.map(children, child => child.props.data)
     const valueIndex = renderedOptions.findIndex(opt => opt.value === value)
     if (valueIndex !== -1) {
-      list.current.scrollToRow(valueIndex)
+      list.current!.scrollToRow(valueIndex)
     }
   }, [children])
 
@@ -172,9 +247,11 @@ const VirtualizedMenuList = props => {
   const rowHeight = 40
   const height = _.clamp(rowHeight, maxHeight, rowHeight * rowCount)
 
+  // @ts-expect-error
   return h(AutoSizer, { disableHeight: true }, [
     ({ width }) => {
       return h(List, {
+        // @ts-expect-error
         ref: list,
         height,
         width,
