b24d64e573d15e1dc6c908235803311b98f371b2
Nick Watts <nwatts@broadinstitute.org>
2023-02-22 09:56:53 -0500

    Add comment

--- src/components/common/FocusTrapper.ts
+++ src/components/common/FocusTrapper.ts
@@ -3,6 +3,8 @@ import FocusLock from 'react-focus-lock'
 import { h } from 'react-hyperscript-helpers'
 
 
+// react-focus-lock does not export a type for FocusLock's props, but since FocusLock
+// is a function component, we can get the type from its parameters.
 type FocusLockProps = Parameters<typeof FocusLock>[0]
 
 export type FocusTrapperProps = FocusLockProps & {
