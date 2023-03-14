f8af5a7010b3dd1ebca7f34fda2ea356d119c9ee
trock-broadinst <123021647+trock-broadinst@users.noreply.github.com>
2023-03-14 11:26:06 -0400

    will pass tests with autoformat

---
+++ .eslintrc.js
@@ -0,0 +1,93 @@
+module.exports = {
+  extends: ['prettier', 'airbnb', 'airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          src: './src',
+          types: './types',
+        },
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  env: {
+    jest: true,
+  },
+  rules: {
+    indent: 'off',
+    'comma-dangle': 'off',
+
+    // TODO: ticket all of these to be fixed
+    'react/forbid-prop-types': 'off',
+    'react/prop-types': 'off',
+    'react/sort-comp': 'off',
+    'react/destructuring-assignment': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/require-default-props': 'off',
+    'react/static-property-placement': 'off',
+
+    'import/prefer-default-export': 'off',
+    'import/no-cycle': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+
+    '@typescript-eslint/default-param-last': 'off',
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off', // TODO: should be 'warn',
+    '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+
+    'no-case-declarations': 'off',
+    'no-nested-ternary': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-undef': 'off',
+    'no-await-in-loop': 'off',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'no-void': 'off',
+    'no-empty': 'off',
+    'no-constant-condition': 'off',
+    'no-promise-executor-return': 'off',
+    'no-continue': 'off',
+    'no-dupe-keys': 'off',
+
+    'prefer-promise-reject-errors': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'prefer-regex-literals': 'off',
+    'prefer-destructuring': 'off',
+
+    'symbol-description': 'off',
+    'operator-linebreak': 'off',
+    'max-len': 'off', // TODO: should be: ['warn', { code: 120 }],
+    radix: 'off',
+    'func-names': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'consistent-return': 'off',
+    'class-methods-use-this': 'off',
+    //
+
+    'no-unsafe-optional-chaining': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'all',
+        argsIgnorePattern: '_|^_|^props',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+  plugins: ['lodash-fp', 'import', 'simple-import-sort', 'prettier'],
+};
