f6081ffb910b7d32eb0cfbb9fa2a1878bd5e6b6c
trock-broadinst <123021647+trock-broadinst@users.noreply.github.com>
2023-03-15 13:24:47 -0400

    resolving conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
+/* eslint-disable quote-props */
 module.exports = {
-  extends: ['prettier', 'airbnb', 'airbnb-typescript'],
+  plugins: ['lodash-fp', 'simple-import-sort', 'prettier'],
+  extends: ['react-app', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
+  },
+  env: {
+    jest: true,
   },
   settings: {
     'import/resolver': {
@@ -14,81 +19,131 @@ module.exports = {
       },
     },
   },
-  env: {
-    jest: true,
-  },
   rules: {
+    // For prettier
     indent: 'off',
     'comma-dangle': 'off',
-    'import/prefer-default-export': 'off',
-
-    // TODO: ticket all of these to be fixed or considered
-    'react/forbid-prop-types': 'off',
-    'react/prop-types': 'off',
-    'react/sort-comp': 'off',
-    'react/destructuring-assignment': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-    'react/require-default-props': 'off',
-    'react/static-property-placement': 'off',
-
-    'import/no-cycle': 'off',
-    'import/no-named-as-default': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/extensions': 'off',
-    'import/no-relative-parent-imports': 'off',
-
-    '@typescript-eslint/default-param-last': 'off',
-    '@typescript-eslint/return-await': 'off',
-    '@typescript-eslint/no-shadow': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-unused-expressions': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off', // TODO: should be 'warn',
-    '@typescript-eslint/no-implied-eval': 'off',
-    '@typescript-eslint/no-throw-literal': 'off',
-    '@typescript-eslint/naming-convention': 'off',
-
-    'no-case-declarations': 'off',
-    'no-nested-ternary': 'off',
-    'no-console': 'off',
-    'no-plusplus': 'off',
-    'no-undef': 'off',
-    'no-await-in-loop': 'off',
-    'no-underscore-dangle': 'off',
-    'no-param-reassign': 'off',
-    'no-restricted-syntax': 'off',
-    'no-void': 'off',
-    'no-empty': 'off',
-    'no-constant-condition': 'off',
-    'no-promise-executor-return': 'off',
-    'no-continue': 'off',
-    'no-dupe-keys': 'off',
-
-    'prefer-promise-reject-errors': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'prefer-regex-literals': 'off',
-    'prefer-destructuring': 'off',
-
-    'symbol-description': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    semi: 'off',
+    'arrow-parens': 'off',
     'operator-linebreak': 'off',
-    'max-len': 'off', // TODO: should be: ['warn', { code: 120 }],
-    radix: 'off',
-    'func-names': 'off',
     'implicit-arrow-linebreak': 'off',
-    'consistent-return': 'off',
-    'class-methods-use-this': 'off',
-    //
 
-    'no-unsafe-optional-chaining': 'warn',
+    // Best Practices
+    'no-multi-spaces': 'warn',
+    'require-await': 'warn',
+    // Stylistic Issues
+    'array-bracket-newline': ['warn', 'consistent'],
+    'array-bracket-spacing': 'warn',
+    'block-spacing': 'warn',
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }],
+    camelcase: ['warn', { properties: 'never' }],
+    'comma-spacing': 'warn',
+    'comma-style': 'warn',
+    'computed-property-spacing': 'warn',
+    'eol-last': 'warn',
+    'func-call-spacing': 'warn',
+    'key-spacing': 'warn',
+    'keyword-spacing': 'warn',
+    'lines-between-class-members': 'warn',
+    'no-floating-decimal': 'warn',
+    'no-lonely-if': 'warn',
+    'no-multi-assign': 'warn',
+    'no-multiple-empty-lines': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-unneeded-ternary': 'warn',
+    'no-unused-vars': 'off', // prefer @typescript-eslint/no-unused-vars
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
         vars: 'all',
         args: 'all',
-        argsIgnorePattern: '_|^_|^props',
+        argsIgnorePattern: '^_|^props',
         ignoreRestSiblings: true,
       },
     ],
+    'no-whitespace-before-property': 'warn',
+    'nonblock-statement-body-position': 'warn',
+    'object-curly-newline': ['warn', { multiline: true, consistent: true }],
+    'object-curly-spacing': ['warn', 'always'],
+    'one-var': ['warn', 'never'],
+    'padded-blocks': ['warn', 'never'],
+    'quote-props': ['warn', 'as-needed'],
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'space-before-blocks': 'warn',
+    'space-before-function-paren': ['warn', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+    'space-in-parens': 'warn',
+    'space-infix-ops': 'warn',
+
+    // ES6
+    'arrow-spacing': 'warn',
+    'no-duplicate-imports': 'warn',
+    'no-restricted-syntax': [
+      'warn',
+      { selector: 'TSEnumDeclaration', message: 'Use a union of literals instead of an enum' },
+      { selector: 'JSXElement', message: 'Use react-hyperscript-helpers instead of JSX' },
+    ],
+    // TODO: Set 'variables' to 'true' after fixing the existing issues
+    'no-use-before-define': ['warn', { functions: true, classes: true, variables: false }],
+    'no-useless-rename': 'warn',
+    'no-var': 'warn',
+    'object-shorthand': 'warn',
+    'prefer-arrow-callback': 'warn',
+    'prefer-const': 'warn',
+    'prefer-template': 'warn',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'rest-spread-spacing': 'warn',
+    'template-curly-spacing': 'warn',
+    'vars-on-top': 'warn',
+
+    'lodash-fp/consistent-name': ['warn', '_'],
+    'lodash-fp/no-argumentless-calls': 'warn',
+    'lodash-fp/no-chain': 'warn',
+    'lodash-fp/no-extraneous-args': 'warn',
+    'lodash-fp/no-extraneous-function-wrapping': 'warn',
+    'lodash-fp/no-extraneous-iteratee-args': 'warn',
+    // 'lodash-fp/no-extraneous-partials': 'warn', // available in eslint-plugin-lodash-fp 2.2.0+
+    'lodash-fp/no-partial-of-curried': 'warn',
+    'lodash-fp/no-single-composition': 'warn',
+    'lodash-fp/no-submodule-destructuring': 'warn',
+    'lodash-fp/no-unused-result': 'warn',
+    'lodash-fp/prefer-compact': 'warn',
+    'lodash-fp/prefer-composition-grouping': 'warn',
+    'lodash-fp/prefer-flat-map': 'warn',
+    'lodash-fp/prefer-get': 'warn',
+    'lodash-fp/prefer-identity': ['warn', { arrowFunctions: false }],
+    'lodash-fp/preferred-alias': 'warn',
+    'lodash-fp/use-fp': 'warn',
+
+    'react/default-props-match-prop-types': 'warn',
+    'react/no-children-prop': 'warn',
+    'react/no-unused-state': 'warn',
+
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'simple-import-sort/imports': 'warn',
+
+    'import/named': 'warn',
+    'import/newline-after-import': ['warn', { count: 2 }],
+    'import/no-anonymous-default-export': ['warn', { allowObject: true }],
+    'import/no-internal-modules': [
+      'warn',
+      {
+        forbid: ['src/components/common/*'],
+      },
+    ],
+    'import/no-unresolved': 'warn',
+
+    'no-debugger': 'warn',
   },
-  plugins: ['lodash-fp', 'import', 'simple-import-sort', 'prettier'],
+  overrides: [
+    {
+      files: 'src/**/*.{js,ts}',
+      rules: {
+        'no-console': ['warn', { allow: ['assert', 'error'] }],
+      },
+    },
+  ],
 };
