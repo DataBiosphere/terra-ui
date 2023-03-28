d04b43de3603373cac2369bc3f99bdf658f10a18
n8landolt <nlandolt@broadinstitute.org>
2023-03-28 12:23:47 -0400

    Updated formatting of DeleteEnvironment.ts.

--- src/pages/workspaces/workspace/analysis/modals/DeleteEnvironment.ts
+++ src/pages/workspaces/workspace/analysis/modals/DeleteEnvironment.ts
@@ -38,7 +38,7 @@ export const DeleteEnvironment = ({
   toolLabel,
   computeRegion
 }: DeleteEnvironmentProps) => {
-  return (div({ style: { ...computeStyles.drawerContent, ...computeStyles.warningView } }, [
+  return div({ style: { ...computeStyles.drawerContent, ...computeStyles.warningView } }, [
     h(TitleBar, {
       id,
       style: computeStyles.titleBar,
@@ -56,80 +56,72 @@ export const DeleteEnvironment = ({
         [runtimeConfig && persistentDisk &&
           (!isGceConfig(runtimeConfig) || isGceWithPdConfig(runtimeConfig)) && // this line checks if the runtime is a GCE VM with a PD attached
           !isDataprocConfig(runtimeConfig) && //and this line makes sure it's not a Dataproc config
-          persistentDisk.id !== runtimeConfig?.persistentDiskId, () => {
-          return h(Fragment, [
-            h(RadioBlock, {
-              name: 'delete-persistent-disk',
-              labelText: 'Delete application configuration and cloud compute profile',
-              checked: !deleteDiskSelected,
-              onChange: () => setDeleteDiskSelected(false)
-            }, [
-              p({ style: { marginBottom: 0 } }, [
-                'Deletes your application configuration and cloud compute profile. This will also ',
-                span({ style: { fontWeight: 600 } }, ['delete all files on the built-in hard disk.'])
-              ])
-            ]),
-            h(RadioBlock, {
-              name: 'delete-persistent-disk',
-              labelText: 'Delete persistent disk',
-              checked: deleteDiskSelected,
-              onChange: () => setDeleteDiskSelected(true),
-              style: { marginTop: '1rem' }
-            }, [
-              p([
-                'Deletes your persistent disk, which will also ', span({ style: { fontWeight: 600 } }, ['delete all files on the disk.'])
-              ]),
-              p({ style: { marginBottom: 0 } }, [
-                'Since the persistent disk is not attached, the application configuration and cloud compute profile will remain.'
-              ])
-            ]),
-            toolLabel === runtimeToolLabels.RStudio ? h(SaveFilesHelpRStudio) : h(SaveFilesHelpAzure)
-          ])
-        }],
-        [runtimeConfig && persistentDisk, () => {
-          return h(
-            DeleteDiskChoices,
-            {
-              persistentDiskCostDisplay: Utils.formatUSD(getPersistentDiskCostMonthly(persistentDisk!, computeRegion)),
-              deleteDiskSelected,
-              setDeleteDiskSelected,
-              toolLabel,
-              cloudService: runtimeConfig?.cloudService
-            }
-          )
-        }],
-        [!runtimeConfig && persistentDisk, () => {
-          return h(Fragment, [
-            h(RadioBlock, {
-              name: 'delete-persistent-disk',
-              labelText: 'Delete persistent disk',
-              checked: true,
-              onChange: () => {}
-            }, [
-              p([
-                'Deletes your persistent disk, which will also ', span({ style: { fontWeight: 600 } }, ['delete all files on the disk.'])
-              ]),
-              p({ style: { marginBottom: 0 } }, [
-                'If you want to permanently save some files from the disk before deleting it, you will need to create a new cloud environment to access it.'
-              ])
-            ]),
-            // At this point there is no runtime (we're in the !existingRuntime block) to check the tool
-            h(SaveFilesHelpRStudio)
-          ])
-        }],
-        [Utils.DEFAULT, () => {
-          return h(Fragment, [
-            p([
-              'Deleting your application configuration and cloud compute profile will also ',
+          persistentDisk.id !== runtimeConfig?.persistentDiskId, () => h(Fragment, [
+          h(RadioBlock, {
+            name: 'delete-persistent-disk',
+            labelText: 'Delete application configuration and cloud compute profile',
+            checked: !deleteDiskSelected,
+            onChange: () => setDeleteDiskSelected(false)
+          }, [
+            p({ style: { marginBottom: 0 } }, [
+              'Deletes your application configuration and cloud compute profile. This will also ',
               span({ style: { fontWeight: 600 } }, ['delete all files on the built-in hard disk.'])
+            ])
+          ]),
+          h(RadioBlock, {
+            name: 'delete-persistent-disk',
+            labelText: 'Delete persistent disk',
+            checked: deleteDiskSelected,
+            onChange: () => setDeleteDiskSelected(true),
+            style: { marginTop: '1rem' }
+          }, [
+            p([
+              'Deletes your persistent disk, which will also ', span({ style: { fontWeight: 600 } }, ['delete all files on the disk.'])
             ]),
-            toolLabel === 'RStudio' ? h(SaveFilesHelpRStudio) : h(SaveFilesHelpAzure)
-          ])
-        }]
+            p({ style: { marginBottom: 0 } }, [
+              'Since the persistent disk is not attached, the application configuration and cloud compute profile will remain.'
+            ])
+          ]),
+          toolLabel === runtimeToolLabels.RStudio ? h(SaveFilesHelpRStudio) : h(SaveFilesHelpAzure)
+        ])],
+        [runtimeConfig && persistentDisk, () => h(
+          DeleteDiskChoices,
+          {
+            persistentDiskCostDisplay: Utils.formatUSD(getPersistentDiskCostMonthly(persistentDisk!, computeRegion)),
+            deleteDiskSelected,
+            setDeleteDiskSelected,
+            toolLabel,
+            cloudService: runtimeConfig?.cloudService
+          }
+        )],
+        [!runtimeConfig && persistentDisk, () => h(Fragment, [
+          h(RadioBlock, {
+            name: 'delete-persistent-disk',
+            labelText: 'Delete persistent disk',
+            checked: true,
+            onChange: () => {}
+          }, [
+            p([
+              'Deletes your persistent disk, which will also ', span({ style: { fontWeight: 600 } }, ['delete all files on the disk.'])
+            ]),
+            p({ style: { marginBottom: 0 } }, [
+              'If you want to permanently save some files from the disk before deleting it, you will need to create a new cloud environment to access it.'
+            ])
+          ]),
+          // At this point there is no runtime (we're in the !existingRuntime block) to check the tool
+          h(SaveFilesHelpRStudio)
+        ])],
+        [Utils.DEFAULT, () => h(Fragment, [
+          p([
+            'Deleting your application configuration and cloud compute profile will also ',
+            span({ style: { fontWeight: 600 } }, ['delete all files on the built-in hard disk.'])
+          ]),
+          toolLabel === 'RStudio' ? h(SaveFilesHelpRStudio) : h(SaveFilesHelpAzure)
+        ])]
       )
     ]),
     div({ style: { display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' } }, [
       renderActionButton()
     ])
-  ]))
+  ])
 }
