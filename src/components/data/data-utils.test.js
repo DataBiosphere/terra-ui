ffc6af1d6276514bdc229955a1223731a791b16f
yuliadub <yulia.dubinina@gmail.com>
2023-03-31 10:35:04 -0700

    fix test

--- src/components/data/data-utils.test.js
+++ src/components/data/data-utils.test.js
@@ -16,11 +16,11 @@ describe('getRootTypeForSetTable', () => {
 
 describe('getDownloadCommand', () => {
   it('gets download command for gsutil', () => {
-    expect(getDownloadCommand({ fileName: 'test.txt', uri: 'gs://demo-data/test.txt' })).toBe('gsutil cp gs://demo-data/test.txt test.txt')
+    expect(getDownloadCommand('test.txt', 'gs://demo-data/test.txt')).toBe('gsutil cp gs://demo-data/test.txt test.txt')
   })
 
   it('gets download command for azcopy', () => {
-    expect(getDownloadCommand({ fileName: 'test.txt', uri: 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt' })).toBe("azcopy 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt' test.txt")
+    expect(getDownloadCommand('test.txt', 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt')).toBe("azcopy copy 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt' test.txt")
   })
 })
 
