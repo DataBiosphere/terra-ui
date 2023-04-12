3ff59cfaf40984fa53e506cd59ae3ae5d5e537e1
Yulia Dubinina <yuliadub@users.noreply.github.com>
2023-04-12 16:33:48 -0600

    Aj-965 Add identifier for when a file is not present in current workspace (#3885)

--- src/components/data/data-utils.test.js
+++ src/components/data/data-utils.test.js
@@ -99,8 +99,7 @@ describe('entityAttributeText', () => {
 
 describe('renderDataCell', () => {
   const testGoogleWorkspace = { workspace: { bucketName: 'test-bucket', cloudPlatform: 'Gcp' } }
-  const testAzureWorkspace = { workspace: { bucketName: 'test-bucket', cloudPlatform: 'Azure' } }
-
+  const testAzureWorkspace = { workspace: { bucketName: 'test-bucket', cloudPlatform: 'Azure', workspaceId: '77397ce7-bb9b-4339-bc12-95e1f52956de' } }
   describe('basic data types', () => {
     it.each([
       { testWorkspace: testGoogleWorkspace },
@@ -218,6 +217,16 @@ describe('renderDataCell', () => {
 
     it('does not render a warning for GCS URLs in the workspace bucket', () => {
       const { queryByText } = render(renderDataCell('gs://test-bucket/file.txt', testGoogleWorkspace))
+      expect(queryByText('Some files are located outside of the current workspace')).toBeNull()
+    })
+
+    it('renders a warning for Azure URLs outside the workspace storage', () => {
+      const { getByText } = render(renderDataCell('https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/testContainer/test_file.tsv', testAzureWorkspace))
+      expect(getByText('Some files are located outside of the current workspace')).toBeTruthy()
+    })
+
+    it('does not render a warning for Azure URLs in the workspace storage', () => {
+      const { queryByText } = render(renderDataCell('https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-77397ce7-bb9b-4339-bc12-95e1f52956de/test_file.tsv', testAzureWorkspace))
       expect(queryByText('Some files are located outside of the current workspace')).toBeNull()
     })
 
