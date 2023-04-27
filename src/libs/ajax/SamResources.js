4041079f4d4fd6175d9f3dafa539370dee662e4c
tlangs <tlangs@broadinstitute.org>
2023-04-27 17:09:37 -0400

    jsonBody

--- src/libs/ajax/SamResources.js
+++ src/libs/ajax/SamResources.js
@@ -1,15 +1,10 @@
 import _ from 'lodash/fp'
-import { appIdentifier, authOpts, fetchSam } from 'src/libs/ajax/ajax-common'
+import { appIdentifier, authOpts, fetchSam, jsonBody } from 'src/libs/ajax/ajax-common'
 
 
 export const SamResources = () => ({
   leave: (samResourceType, samResourceId) => fetchSam(`api/resources/v2/${samResourceType}/${samResourceId}/leave`, _.mergeAll([authOpts(), appIdentifier, { method: 'DELETE' }])),
 
   getSignedUrl: (bucket, object, project) => fetchSam(`api/google/v1/user/petServiceAccount/${project}/signedUrlForBlob`,
-    _.mergeAll([authOpts(), appIdentifier, {
-      method: 'POST', body: {
-        bucketName: bucket,
-        blobName: object
-      }
-    }]))
+    _.mergeAll([authOpts(), appIdentifier, jsonBody({ bucketName: bucket, blobName: object }), { method: 'POST' }]))
 })
