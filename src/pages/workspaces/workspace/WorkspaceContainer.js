253bde27e265189d0f24408a0288a7e5cf8a2cef
tlangs <tlangs@broadinstitute.org>
2023-05-01 14:29:02 -0400

    ID-599 More metrics to track FastPass functionality

--- src/pages/workspaces/workspace/WorkspaceContainer.js
+++ src/pages/workspaces/workspace/WorkspaceContainer.js
@@ -142,7 +142,13 @@ const WorkspaceContainer = ({
 
   useEffect(() => {
     if (isGoogleWorkspaceSyncing) {
-      Ajax().Metrics.captureEvent(Events.permissionsSynchronizationDelayDisplayed, extractWorkspaceDetails(workspace))
+      Ajax().Metrics.captureEvent(Events.permissionsSynchronizationDelayDisplayed,
+        _.merge(extractWorkspaceDetails(workspace),
+          {
+            accessLevel: workspace.accessLevel,
+            createdDate: workspace.workspace.createdDate,
+            isWorkspaceCreator: workspace.workspace.createdBy === getUser().email
+          }))
     }
     // Only want to event when isGoogleWorkspaceSyncing changes state, not whenever any part of workspace changes.
   }, [isGoogleWorkspaceSyncing]) // eslint-disable-line react-hooks/exhaustive-deps
