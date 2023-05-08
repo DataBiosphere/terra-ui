338bf86e271cbbdaf3cf7cfb6ad86ffa514966c4
Sky Rubenstein <blrubenstein@wpi.edu>
2023-05-08 09:51:46 -0400

    Update src/pages/library/datasetBuilder/constants.ts
    Co-authored-by: Phil Shapiro <pshapiro@broadinstitute.org>

--- src/pages/library/datasetBuilder/constants.ts
+++ src/pages/library/datasetBuilder/constants.ts
@@ -1,3 +1,3 @@
 export const PAGE_PADDING_HEIGHT = 0;
 export const PAGE_PADDING_WIDTH = 3;
-export const PREPACKAGED_CONCEPT_SETS = [{ name: 'Demographics' }, { name: 'All surveys' }];
+export const PREPACKAGED_CONCEPT_SETS: ConceptSet[] = [{ name: 'Demographics' }, { name: 'All surveys' }];
