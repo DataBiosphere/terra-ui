8db4194fad58ee087dcfab8596152004390c839f
Sky Rubenstein <blrubenstein@wpi.edu>
2023-05-24 14:22:33 -0400

    [DC-696] Dataset builder three panel layout (#3918)
    Co-authored-by: Phil Shapiro <pshapiro@broadinstitute.org>

---
+++ src/pages/library/datasetBuilder/constants.ts
@@ -0,0 +1,5 @@
+import { ConceptSet } from 'src/pages/library/datasetBuilder/dataset-builder-types';
+
+export const PAGE_PADDING_HEIGHT = 0;
+export const PAGE_PADDING_WIDTH = 3;
+export const PREPACKAGED_CONCEPT_SETS: ConceptSet[] = [{ name: 'Demographics' }, { name: 'All surveys' }];
