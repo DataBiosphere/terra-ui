d6188e24b105d66a8d4d3cf3969b22069167359a
Robert Title <rtitle@broadinstitute.org>
2023-06-02 11:25:38 -0400

    Fix cookie setter logic for Azure apps

--- src/pages/workspaces/workspace/analysis/modals/CloudEnvironmentModal.ts
+++ src/pages/workspaces/workspace/analysis/modals/CloudEnvironmentModal.ts
@@ -406,7 +406,6 @@ export const CloudEnvironmentModal = ({
     // - For Azure, apps run in the domain of the Relay namespace in the landing zone; azureCookieReady stores whether a cookie has been set in that domain
     // - For Azure Runtimes, the cookie is set when opening the runtime.
     // - For GCP, apps are all behind a centralized Leo proxy; leoCookieReady stores whether a cookie has been set in Leo's domain.
-
     const cookieReady = Utils.cond(
       [cloudProvider === cloudProviderTypes.AZURE && toolLabel in appToolLabels, () => azureCookieReady.readyForApp],
       [Utils.DEFAULT, () => leoCookieReady]
@@ -529,13 +528,24 @@ export const CloudEnvironmentModal = ({
     const isCloudEnvForToolDisabled = isCloudEnvModalDisabled(toolLabel);
     return h(Fragment, [
       // We cannot attach the periodic cookie setter until we have a running Cromwell app for Azure because the relay is not guaranteed to be ready until then
-      toolLabel === appToolLabels.CROMWELL &&
-      app?.cloudContext?.cloudProvider === cloudProviderTypes.AZURE &&
-      app?.status === 'RUNNING'
-        ? h(PeriodicAzureCookieSetter, {
-            proxyUrl: app.proxyUrls['cbas-ui'],
-            // forCromwell: true //TODO: this is not yet supported behaviour
-          })
+      app?.cloudContext?.cloudProvider === cloudProviderTypes.AZURE && app?.status === 'RUNNING'
+        ? Utils.cond(
+            [
+              toolLabel === appToolLabels.CROMWELL,
+              () => h(PeriodicAzureCookieSetter, { proxyUrl: app.proxyUrls['cbas-ui'], forApp: true }),
+            ],
+            [
+              toolLabel === appToolLabels.HAIL_BATCH,
+              () => {
+                const batchUrl = app?.proxyUrls?.batch;
+                return h(PeriodicAzureCookieSetter, {
+                  proxyUrl: batchUrl.substring(0, batchUrl.lastIndexOf('/') + 1),
+                  forApp: true,
+                });
+              },
+            ],
+            [Utils.DEFAULT, () => null]
+          )
         : null,
       div({ style: toolPanelStyles }, [
         // Label at the top for each tool
