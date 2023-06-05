26dcea6f5ba8897fd068093eda4d6056bd80209d
n8landolt <nlandolt@broadinstitute.org>
2023-06-05 15:49:15 -0400

    Renamed capitalization on files.

---
+++ src/pages/workspaces/workspace/analysis/modals/ComputeModal/GcpPersistentDiskSection.ts
@@ -0,0 +1,48 @@
+import { div } from 'react-hyperscript-helpers';
+import { GcpPersistentDiskOptions, SharedPdType } from 'src/libs/ajax/leonardo/models/disk-models';
+import { CloudProvider } from 'src/libs/workspace-utils';
+import { AboutPersistentDiskSection } from 'src/pages/workspaces/workspace/analysis/modals/ComputeModal/AboutPersistentDiskSection';
+import { GcpPersistentDiskSizeNumberInput } from 'src/pages/workspaces/workspace/analysis/modals/ComputeModal/GcpPersistentDiskSizeNumberInput';
+import { PersistentDiskTypeInputContainer } from 'src/pages/workspaces/workspace/analysis/modals/ComputeModal/PersistentDiskTypeInputContainer';
+import { computeStyles } from 'src/pages/workspaces/workspace/analysis/modals/modalStyles';
+
+export interface GcpPersistentDiskSectionProps {
+  persistentDiskExists: boolean;
+  persistentDiskSize: number;
+  persistentDiskType: SharedPdType;
+  onChangePersistentDiskType: (type: SharedPdType) => void;
+  onChangePersistentDiskSize: (size: number) => void;
+  onClickAbout: () => void;
+  cloudPlatform: CloudProvider;
+}
+
+export const GcpPersistentDiskSection = (props: GcpPersistentDiskSectionProps) => {
+  const {
+    onClickAbout,
+    persistentDiskType,
+    persistentDiskSize,
+    onChangePersistentDiskType,
+    onChangePersistentDiskSize,
+    persistentDiskExists,
+  } = props;
+
+  const gridStyle = { display: 'grid', gridGap: '1rem', alignItems: 'center', marginTop: '1rem' };
+  return div({ style: { ...computeStyles.whiteBoxContainer, marginTop: '1rem' } }, [
+    AboutPersistentDiskSection({ onClick: onClickAbout }),
+    div({ style: { ...gridStyle, gridGap: '1rem', gridTemplateColumns: '15rem 5.5rem', marginTop: '0.75rem' } }, [
+      PersistentDiskTypeInputContainer({
+        persistentDiskExists,
+        value: persistentDiskType,
+        onChange: (e) => {
+          onChangePersistentDiskType(e.value);
+        },
+        options: GcpPersistentDiskOptions,
+      }),
+      GcpPersistentDiskSizeNumberInput({
+        persistentDiskSize,
+        isDisabled: persistentDiskExists,
+        onChangePersistentDiskSize,
+      }),
+    ]),
+  ]);
+};
---
+++ src/pages/workspaces/workspace/analysis/modals/ComputeModal/GcpPersistentDiskSizeNumberInput.test.ts
@@ -0,0 +1,50 @@
+import '@testing-library/dom';
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { h } from 'react-hyperscript-helpers';
+import {
+  GcpPersistentDiskSizeNumberInput,
+  GcpPersistentDiskSizeNumberInputProps,
+} from 'src/pages/workspaces/workspace/analysis/modals/ComputeModal/GcpPersistentDiskSizeNumberInput';
+
+const defaultGcpPersistentDiskSizeNumberInput: GcpPersistentDiskSizeNumberInputProps = {
+  persistentDiskSize: 64,
+  onChangePersistentDiskSize: jest.fn(),
+  isDisabled: false,
+};
+
+describe('GcpPersistentDiskSizeNumberInput', () => {
+  it('should render with default props', () => {
+    // Arrange
+    render(h(GcpPersistentDiskSizeNumberInput, defaultGcpPersistentDiskSizeNumberInput));
+
+    // Assert
+    expect(screen.getByLabelText('Disk Size (GB)')).toBeTruthy();
+  });
+
+  it('should call onChange when value is updated.', async () => {
+    // Arrange
+    render(h(GcpPersistentDiskSizeNumberInput, defaultGcpPersistentDiskSizeNumberInput));
+    // Act
+    const diskSizeNumberInput = screen.getByLabelText('Disk Size (GB)');
+    await userEvent.clear(diskSizeNumberInput);
+    await userEvent.type(diskSizeNumberInput, '1024');
+
+    // Assert
+    expect(defaultGcpPersistentDiskSizeNumberInput.onChangePersistentDiskSize).toBeCalledWith(10);
+    expect(defaultGcpPersistentDiskSizeNumberInput.onChangePersistentDiskSize).toBeCalledWith(102);
+    expect(defaultGcpPersistentDiskSizeNumberInput.onChangePersistentDiskSize).toBeCalledWith(1024);
+  });
+
+  it('should be disabled when isDisabled is true', () => {
+    // Arrange
+    render(h(GcpPersistentDiskSizeNumberInput, { ...defaultGcpPersistentDiskSizeNumberInput, isDisabled: true }));
+
+    // Assert
+    expect(screen.getByLabelText('Disk Size (GB)')).toBeDisabled();
+    expect(screen.getByLabelText('Disk Size (GB)')).toHaveValue(
+      defaultGcpPersistentDiskSizeNumberInput.persistentDiskSize
+    );
+  });
+});
