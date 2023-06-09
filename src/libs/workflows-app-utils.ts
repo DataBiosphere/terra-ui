ba345bf2d14c3e70bac3fcd40fe6c44d26b19e75
Saloni Shah <salonipshah11@gmail.com>
2023-06-09 17:06:11 -0400

    convert test file to TS

--- src/libs/workflows-app-utils.ts
+++ src/libs/workflows-app-utils.ts
@@ -1,17 +1,15 @@
-import { appStatuses, ListAppResponse } from 'src/libs/ajax/leonardo/models/app-models';
+import { ListAppResponse } from 'src/libs/ajax/leonardo/models/app-models';
 import { appToolLabels } from 'src/pages/workspaces/workspace/analysis/utils/tool-utils';
 
 export const resolveRunningCromwellAppUrl = (apps: ListAppResponse[], currentUser: string) => {
   // it looks for Kubernetes deployment status RUNNING expressed by Leo
   // See here for specific enumerations -- https://github.com/DataBiosphere/leonardo/blob/develop/core/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/kubernetesModels.scala
   // We explicitly look for a RUNNING app because if the CBAS app is not Running, we won't be able to send import method request.
-  const healthyState = appStatuses.running.status;
-  const cromwellAppType = appToolLabels.CROMWELL;
 
   // note: the requirement for checking if the app was created by user will not be needed when we move to multi-user Workflows app where users with
   // OWNER and WRITER roles will be able to import methods to app created by another user
   const filteredApps: ListAppResponse[] = apps.filter(
-    (app) => app.appType === cromwellAppType && app.status === healthyState && app.auditInfo.creator === currentUser
+    (app) => app.appType === appToolLabels.CROMWELL && app.status === 'RUNNING' && app.auditInfo.creator === currentUser
   );
   if (filteredApps.length === 1) {
     return {
