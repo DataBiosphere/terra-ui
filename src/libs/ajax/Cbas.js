bc30182e87c22e57b7487cc06edecaf3608581ef
Katrina Pierre <kpierre@broadinstitute.org>
2023-06-14 13:16:20 -0400

    add status endpoint

--- src/libs/ajax/Cbas.js
+++ src/libs/ajax/Cbas.js
@@ -2,6 +2,10 @@ import _ from 'lodash/fp';
 import { authOpts, fetchCbas, jsonBody } from 'src/libs/ajax/ajax-common';
 
 export const Cbas = (signal) => ({
+  status: async () => {
+    const res = fetchCbas(fetchCbas)('api/batch/v1/status', { signal });
+    return res.json();
+  },
   methods: {
     post: async (cbasUrlRoot, payload) => {
       const res = await fetchCbas(cbasUrlRoot)('api/batch/v1/methods', _.mergeAll([authOpts(), jsonBody(payload), { signal, method: 'POST' }]));
