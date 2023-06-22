30ce195e0df1b78e871f50e68e43757470086c7f
Sky Rubenstein <blrubenstein@wpi.edu>
2023-06-22 11:11:35 -0400

    [DC-708] Add a mock method for values (#3977)

--- src/pages/library/datasetBuilder/constants.ts
+++ src/pages/library/datasetBuilder/constants.ts
@@ -2,4 +2,7 @@ import { ConceptSet } from 'src/pages/library/datasetBuilder/dataset-builder-typ
 
 export const PAGE_PADDING_HEIGHT = 0;
 export const PAGE_PADDING_WIDTH = 3;
-export const PREPACKAGED_CONCEPT_SETS: ConceptSet[] = [{ name: 'Demographics' }, { name: 'All surveys' }];
+export const PREPACKAGED_CONCEPT_SETS: ConceptSet[] = [
+  { name: 'Demographics', featureValueGroupName: 'Person' },
+  { name: 'All surveys', featureValueGroupName: 'Surveys' },
+];
