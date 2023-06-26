baa8feeede8b53faf250a190eb05596e6a7a81e5
n8landolt <nlandolt@broadinstitute.org>
2023-06-26 16:10:40 -0400

    Updated tests for setAzureCookie on url.

--- src/analysis/setAzureCookieOnUrl.test.ts
+++ src/analysis/setAzureCookieOnUrl.test.ts
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { h } from 'react-hyperscript-helpers';
 import { PeriodicAzureCookieSetter, setAzureCookieOnUrl } from 'src/analysis/runtime-common-components';
+import { azureCookieReadyStore } from 'src/libs/state';
 
 const mockSetAzureCookie = jest.fn();
 
@@ -20,37 +21,33 @@ jest.mock('src/libs/ajax/leonardo/Runtimes', () => {
 describe('setAzureCookieOnUrl', () => {
   it('when forApp is true, sets cookie at "readyForApp"', async () => {
     // Arrange
+    azureCookieReadyStore.reset();
     const forApp = true;
     const url = 'https://app.terra.bio/#workspaces';
     // Act
-    await setAzureCookieOnUrl(forApp, url);
+    await setAzureCookieOnUrl(undefined, url, forApp);
     // Assert
     expect(mockSetAzureCookie).toHaveBeenCalled();
-  });
-
-  it('should call setAzureCookieOnUrl', async () => {
-    // Arrange
-    await render(h(PeriodicAzureCookieSetter));
-    // Assert
-    expect(mockSetAzureCookie).toHaveBeenCalled();
-  });
-
-  it('when forApp is true, sets cookie at "readyForApp"', async () => {
-    // Arrange
-    const forApp = true;
-    const url = 'https://app.terra.bio/#workspaces';
-    // Act
-    await setAzureCookieOnUrl(null, forApp, url);
-    // Assert
-    expect(mockSetAzureCookie).toHaveBeenCalled();
+    expect(azureCookieReadyStore.get().readyForApp).toBe(true);
+    expect(azureCookieReadyStore.get().readyForRuntime).toBe(false);
   });
 
   it('when forApp is false, sets cookie at "readyForRuntime"', async () => {
     // Arrange
+    azureCookieReadyStore.reset();
     const forApp = false;
     const url = 'https://app.terra.bio/#workspaces';
     // Act
-    await setAzureCookieOnUrl(null, forApp, url);
+    await setAzureCookieOnUrl(null, url, forApp);
+    // Assert
+    expect(mockSetAzureCookie).toHaveBeenCalled();
+    expect(azureCookieReadyStore.get().readyForRuntime).toBe(true);
+    expect(azureCookieReadyStore.get().readyForApp).toBe(false);
+  });
+
+  it('should call setAzureCookieOnUrl on cookie setter render', async () => {
+    // Arrange
+    await render(h(PeriodicAzureCookieSetter));
     // Assert
     expect(mockSetAzureCookie).toHaveBeenCalled();
   });
