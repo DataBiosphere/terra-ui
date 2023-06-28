504672fbfd508a6658c66fc923ac352606a104a1
Blake Geno <bgeno@broadinstitute.org>
2023-06-28 13:50:36 -0400

    add tests for overall modal

--- src/pages/workspaces/workspace/ShareWorkspaceModal/CurrentCollaborators.ts
+++ src/pages/workspaces/workspace/ShareWorkspaceModal/CurrentCollaborators.ts
@@ -1,9 +1,8 @@
 import _ from 'lodash/fp';
-import { Dispatch, Fragment, SetStateAction, useLayoutEffect, useRef } from 'react';
-import { div, h } from 'react-hyperscript-helpers';
+import { CSSProperties, Dispatch, Fragment, SetStateAction, useLayoutEffect, useRef } from 'react';
+import { div, h, h2 } from 'react-hyperscript-helpers';
 import { centeredSpinner } from 'src/components/icons';
 import * as Style from 'src/libs/style';
-import * as Utils from 'src/libs/utils';
 import { BaseWorkspace } from 'src/libs/workspace-utils';
 import { Collaborator } from 'src/pages/workspaces/workspace/ShareWorkspaceModal/Collaborator';
 import { aclEntryIsTerraSupport, WorkspaceAcl } from 'src/pages/workspaces/workspace/WorkspaceAcl';
@@ -24,26 +23,23 @@ export const CurrentCollaborators = ({ acl, loaded, ...props }: CurrentCollabora
     !!props.lastAddedEmail && list?.current?.scrollTo({ top: list?.current?.scrollHeight, behavior: 'smooth' });
   }, [props.lastAddedEmail]);
 
-  return div({ ref: list, role: 'list', style: styles }, [
-    h(
-      Fragment,
-      _.flow(
-        _.remove(aclEntryIsTerraSupport),
-        Utils.toIndexPairs,
-        _.map(([_, aclItem]) =>
-          h(Collaborator, {
-            aclItem,
-            acl,
-            ...props,
-          })
-        )
-      )(acl)
-    ),
-    !loaded && centeredSpinner(),
+  return h(Fragment, [
+    h2({ style: { ...Style.elements.sectionHeader, margin: '1rem 0 0.5rem 0' } }, ['Current Collaborators']),
+
+    div({ ref: list, role: 'list', style: styles }, [
+      h(
+        Fragment,
+        _.flow(
+          _.remove(aclEntryIsTerraSupport),
+          _.map((aclItem) => h(Collaborator, { aclItem, acl, ...props }))
+        )(acl)
+      ),
+      !loaded && centeredSpinner(),
+    ]),
   ]);
 };
 
-const styles: React.CSSProperties = {
+const styles: CSSProperties = {
   margin: '0.5rem -1.25rem 0',
   padding: '1rem 1.25rem',
   maxHeight: 550,
