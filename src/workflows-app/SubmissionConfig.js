8e6b1fdc5f36c936ae8416ccbe08e453db46d60f
dillydally414 <67511512+dillydally414@users.noreply.github.com>
2023-07-11 16:06:13 -0400

    integrate with config page

--- src/workflows-app/SubmissionConfig.js
+++ src/workflows-app/SubmissionConfig.js
@@ -16,6 +16,7 @@ import { useCancellation, useOnMount } from 'src/libs/react-utils';
 import * as Utils from 'src/libs/utils';
 import { maybeParseJSON } from 'src/libs/utils';
 import HelpfulLinksBox from 'src/workflows-app/components/HelpfulLinksBox';
+import OutputsTable from 'src/workflows-app/components/OutputsTable';
 import { convertArrayType, loadAppUrls, validateInputs, WdsPollInterval } from 'src/workflows-app/components/submission-common';
 import ViewWorkflowScriptModal from 'src/workflows-app/components/ViewWorkflowScriptModal';
 import { convertToRawUrl } from 'src/workflows-app/utils/method-common';
@@ -493,7 +494,7 @@ export const SubmissionConfig = wrapWorkflowsPage({ name: 'SubmissionConfig' })(
                 activeTab.key || 'select-data',
                 ['select-data', () => h2('TODO')],
                 ['inputs', () => h2('TODO')],
-                ['outputs', () => h2('TODO')]
+                ['outputs', () => h(OutputsTable, { configuredOutputDefinition, setConfiguredOutputDefinition })]
               ),
             ]
           ),
