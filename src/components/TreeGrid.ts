c59ecc9ed3a8470908c79fdef589c92d8fd1b4d2
Phil Shapiro <pshapiro@broadinstitute.org>
2023-07-11 11:18:47 -0400

    Collapse rows by hiding, not deleting

--- src/components/TreeGrid.ts
+++ src/components/TreeGrid.ts
@@ -23,16 +23,16 @@ type RowState = 'closed' | 'opening' | 'open';
 type Row<T extends RowContents> = {
   contents: T;
   depth: number;
-  isVisible: boolean;
+  isFetched: boolean;
   state: RowState;
 };
 
 const wrapContent =
-  (depth: number, isVisible: boolean) =>
+  (depth: number) =>
   <T extends RowContents>(contents: T): Row<T> => ({
     contents,
     depth,
-    isVisible,
+    isFetched: false,
     state: 'closed',
   });
 
@@ -44,17 +44,21 @@ export type TreeGridProps<T extends RowContents> = {
 
 // TODO
 //  - props arguments for row height, no content message
-//  - collapse by hiding, not by deleting and preserve expansion state
 //  - UX, styling
 //  - auto-size based on content (?)
 export const TreeGrid = <T extends RowContents>(props: TreeGridProps<T>) => {
   const { columns, initialRows, getChildren } = props;
-  const [data, setData] = useState(_.map(wrapContent(0, true), initialRows));
+  const [data, setData] = useState(_.map(wrapContent(0), initialRows));
   const rowHeight = 48;
   const expand = async (row: Row<T>) => {
+    if (row.isFetched) {
+      const index = _.findIndex((r) => r.contents.id === row.contents.id, data);
+      setData(_.flow(_.cloneDeep, _.set(`[${index}].state`, 'open'))(data));
+      return;
+    }
     // Mark as loading.
     setData((currentData) => {
-      const index = _.findIndex((r) => r.contents.id === row.contents.id, data);
+      const index = _.findIndex((r) => r.contents.id === row.contents.id, currentData);
       return _.flow(_.cloneDeep, _.set(`[${index}].state`, 'opening'))(currentData);
     });
 
@@ -63,37 +67,54 @@ export const TreeGrid = <T extends RowContents>(props: TreeGridProps<T>) => {
 
     // Mark as loaded and insert children.
     setData((currentData) => {
-      const index = _.findIndex((r) => r.contents.id === row.contents.id, data);
+      const index = _.findIndex((r) => r.contents.id === row.contents.id, currentData);
       // Node was deleted while loading.
       if (index === -1) {
         return currentData;
       }
       const currentRow = currentData[index];
-      const newData = _.flow(_.cloneDeep, _.set(`[${index}].state`, 'open'))(currentData);
-      newData.splice(index + 1, 0, ..._.map(wrapContent(currentRow.depth + 1, currentRow.isVisible), children));
+      const newData = _.flow(
+        _.cloneDeep,
+        _.set(`[${index}]`, { ...currentData[index], state: 'open', isFetched: true })
+      )(currentData);
+      newData.splice(index + 1, 0, ..._.map(wrapContent(currentRow.depth + 1), children));
       return newData;
     });
   };
-  const collapse = async (row: Row<T>) => {
-    const index = _.findIndex(_.isEqual(row), data);
-    const before = _.slice(0, index, data);
-    const after = _.flow(
-      _.slice(index + 1, data.length),
-      _.dropWhile((dataRow: Row<T>) => dataRow.depth > row.depth)
-    )(data);
-    setData([...before, { ...row, state: 'closed' }, ...after]);
+  const collapse = (row: Row<T>) => {
+    const index = _.findIndex((r) => r.contents.id === row.contents.id, data);
+    setData(_.flow(_.cloneDeep, _.set(`[${index}].state`, 'closed'))(data));
   };
+
+  // Create the visible row sublist.
+  const visibleRows: Row<T>[] = [];
+  // Root elements are always visible.
+  let visibleDepth = 0;
+  for (const row of data) {
+    // A row is visible if it's not deeper than the current deepest open row.
+    if (row.depth > visibleDepth) {
+      continue;
+    }
+    if (row.state === 'open') {
+      // If a row is open, then all rows deeper than it are visible.
+      visibleDepth = row.depth + 1;
+    } else {
+      // Reset the visible depth to the current row's depth.
+      visibleDepth = row.depth;
+    }
+    visibleRows.push(row);
+  }
 
   return h(Grid, {
     rowHeight,
-    height: rowHeight * data.length,
-    rowCount: data.length,
+    height: rowHeight * visibleRows.length,
+    rowCount: visibleRows.length,
     columnCount: columns.length,
     columnWidth: (index) => columns[index.index].width,
     width: _.sum(_.map((c) => c.width, columns)),
     noContentMessage: 'No matching data',
     cellRenderer: ({ rowIndex, columnIndex, style }) => {
-      const row = data[rowIndex];
+      const row = visibleRows[rowIndex];
       const [handler, iconName] = (() => {
         switch (row.state) {
           case 'closed':
@@ -114,8 +135,7 @@ export const TreeGrid = <T extends RowContents>(props: TreeGridProps<T>) => {
               0,
               () =>
                 div({ style: { paddingLeft: `${row.depth}rem`, display: 'flex' } }, [
-                  !data[rowIndex].contents.isLeaf &&
-                    h(Link, { onClick: () => handler(row) }, [icon(iconName, { size: 16 })]),
+                  !row.contents.isLeaf && h(Link, { onClick: () => handler(row) }, [icon(iconName, { size: 16 })]),
                   div({ style: { display: 'flex', marginLeft: row.contents.isLeaf ? 20 : 4 } }, [
                     columns[columnIndex].render(row.contents),
                   ]),
