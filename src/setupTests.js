05eac43b22f3ff02cbf9adba048d4e6857452085
Nick Watts <nwatts@broadinstitute.org>
2023-07-18 15:13:30 -0400

    Add comment

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,10 @@ import 'blob-polyfill';
 
 import { toHaveNoViolations } from 'jest-axe';
 
+// VirtualizedSelect uses react-virtualized's AutoSizer to size the options menu.
+// Left to its own devices, in the unit test environment, AutoSizer makes the menu
+// list 0px wide and no options are rendered. Mocking AutoSizer makes the virtualized
+// window large enough for options to be rendered.
 jest.mock('src/components/common/VirtualizedSelectAutoSizer', () => {
   return {
     AutoSizer: ({ children }) => children({ width: 300 }),
