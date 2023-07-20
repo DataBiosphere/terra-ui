e1c52f23fab30de279865c394c330813a27d3201
Kai <kfeldman@broadinstitute.org>
2023-07-20 16:35:44 -0400

    remove unused nps code prior to removing rex

--- integration-tests/utils/integration-helpers.js
+++ integration-tests/utils/integration-helpers.js
@@ -205,7 +205,7 @@ const clickNavChildAndLoad = async (page, tab) => {
 };
 
 const viewWorkspaceDashboard = async (page, token, workspaceName) => {
-  // Sign in to handle unexpected NPS survey popup and Loading Terra... spinner
+  // Sign in to handle "Loading Terra..." spinner
   await signIntoTerra(page, { token });
   await click(page, clickable({ textContains: 'View Workspaces' }));
   await dismissNotifications(page);
--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -208,17 +208,6 @@ const User = (signal) => ({
     return getFirstTimeStamp(getUser().token);
   },
 
-  // UX is testing using AppCues. If experiment is successful, all NpsSurvey code can be deleted.
-  lastNpsResponse: async () => {
-    const res = await fetchRex('npsResponses/lastTimestamp', _.merge(authOpts(), { signal }));
-    return res.json();
-  },
-
-  // UX is testing using AppCues. If experiment is successful, all NpsSurvey code can be deleted.
-  postNpsResponse: (body) => {
-    return fetchRex('npsResponses/create', _.mergeAll([authOpts(), jsonBody(body), { signal, method: 'POST' }]));
-  },
-
   getNihStatus: async () => {
     try {
       const res = await fetchOrchestration('api/nih/status', _.merge(authOpts(), { signal }));
