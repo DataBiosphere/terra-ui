d0d5b877351f6993fa8d125a5d076dd8832752c1
Nick Watts <nwatts@broadinstitute.org>
2023-07-21 16:12:24 -0400

    Convert feature-previews-config to TS

--- src/libs/feature-previews-config.ts
+++ src/libs/feature-previews-config.ts
@@ -4,7 +4,41 @@ export const HAIL_BATCH_AZURE_FEATURE_ID = 'hail-batch-azure';
 export const WORKFLOWS_TAB_AZURE_FEATURE_ID = 'workflows-tab-azure';
 export const DOCKSTORE_AZURE_FEATURE_ID = 'dockstore-azure';
 
-const featurePreviewsConfig = [
+export type FeaturePreview = {
+  /**
+   * ID for the feature. This is used to check if the feature is enabled and to toggle it enabled/disabled.
+   */
+  readonly id: string;
+
+  /**
+   * Name of the feature. Shown on the feature previews page.
+   */
+  readonly title: string;
+
+  /**
+   * Description for the feature. Shown on the feature previews page.
+   */
+  readonly description: string;
+
+  /**
+   * Optional list of groups. If specified, the feature will only appear on the feature previews page
+   * for users that are a member of at least one of the specified groups.
+   * This only applies in production. In dev environments, all features are available to all users.
+   */
+  readonly groups?: readonly string[];
+
+  /**
+   * Optional URL for feature documentation. Shown on the feature previews page.
+   */
+  readonly documentationUrl?: string;
+
+  /**
+   * Optional URL for users to provide feedback on the feature. Shown on the feature previews page.
+   */
+  readonly feedbackUrl?: string;
+};
+
+const featurePreviewsConfig: readonly FeaturePreview[] = [
   {
     id: 'data-table-versioning',
     title: 'Data Table Versioning',
@@ -16,7 +50,8 @@ const featurePreviewsConfig = [
   {
     id: 'data-table-provenance',
     title: 'Data Table Provenance',
-    description: 'Enabling this feature will allow you to view information about the workflow that generated data table columns and files.',
+    description:
+      'Enabling this feature will allow you to view information about the workflow that generated data table columns and files.',
     groups: ['preview-data-versioning-and-provenance'],
     feedbackUrl: `mailto:dsp-sue@broadinstitute.org?subject=${encodeURIComponent('Feedback on data table provenance')}`,
   },
@@ -32,7 +67,9 @@ const featurePreviewsConfig = [
     title: 'Workspace Files Browser',
     description: 'Enabling this feature will allow you to use the new workspace files browser.',
     groups: ['preview-workspace-files'],
-    feedbackUrl: `mailto:dsp-sue@broadinstitute.org?subject=${encodeURIComponent('Feedback on workspace files browser')}`,
+    feedbackUrl: `mailto:dsp-sue@broadinstitute.org?subject=${encodeURIComponent(
+      'Feedback on workspace files browser'
+    )}`,
   },
   {
     id: HAIL_BATCH_AZURE_FEATURE_ID,
