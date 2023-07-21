db2fa5edf32be341f30b970526ecf20a1f3a4d43
Nick Watts <nwatts@broadinstitute.org>
2023-07-20 21:31:18 -0400

    Upgrade React and React Testing Library

--- src/components/file-browser/useFileDownloadCommand.test.ts
+++ src/components/file-browser/useFileDownloadCommand.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import FileBrowserProvider, { FileBrowserFile } from 'src/libs/ajax/file-browser-providers/FileBrowserProvider';
 import { reportError } from 'src/libs/error';
 import { controlledPromise } from 'src/testing/test-utils';
