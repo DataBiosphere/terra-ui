df3ccbd607f584855a5e2118ef5404049ba01e91
Nick Watts <nwatts@broadinstitute.org>
2023-07-25 11:03:34 -0400

    Switch to createRoot

--- src/appLoader.js
+++ src/appLoader.js
@@ -1,7 +1,7 @@
 import 'src/style.css';
 
 import _ from 'lodash/fp';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { h } from 'react-hyperscript-helpers';
 import RModal from 'react-modal';
 import { initializeAuth, initializeClientId } from 'src/libs/auth';
@@ -10,16 +10,17 @@ import { startPollingServiceAlerts } from 'src/libs/service-alerts-polling';
 import { initializeTCell } from 'src/libs/tcell';
 import Main from 'src/pages/Main';
 
-const appRoot = document.getElementById('root');
+const rootElement = document.getElementById('root');
 
 RModal.defaultStyles = { overlay: {}, content: {} };
-RModal.setAppElement(appRoot);
+RModal.setAppElement(rootElement);
 window.SATURN_VERSION = process.env.REACT_APP_VERSION;
 
 window._ = _;
 
 initializeClientId().then(() => {
-  ReactDOM.render(h(Main), appRoot);
+  const root = createRoot(rootElement);
+  root.render(h(Main));
   initializeAuth();
   initializeTCell();
   startPollingServiceAlerts();
