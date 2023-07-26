f98b49a507dd8b07015bb0fc5aa9ba81fa0986ed
tlangs <tlangs@broadinstitute.org>
2023-07-26 12:50:10 -0400

    ID-423 Let Terra-UI determine DRS endpoint notebooks talk to

--- src/analysis/modals/ComputeModal/GcpComputeModal/GcpComputeModal.js
+++ src/analysis/modals/ComputeModal/GcpComputeModal/GcpComputeModal.js
@@ -309,6 +309,7 @@ export const GcpComputeModalBase = ({
     const { namespace, name, bucketName, googleProject } = getWorkspaceObject();
     const desiredToolLabel = getToolLabelFromCloudEnv(desiredRuntime);
     const terraDeploymentEnv = getConfig().terraDeploymentEnv;
+    const customDrsResolverArgs = getConfig().shouldUseDrsHub ? { DRS_RESOLVER_ENDPOINT: 'api/v4/drs/resolve' } : {};
 
     const customEnvVars = {
       WORKSPACE_NAME: name,
@@ -317,6 +318,7 @@ export const GcpComputeModalBase = ({
       GOOGLE_PROJECT: googleProject,
       CUSTOM_IMAGE: isCustomImage.toString(),
       ...(!!terraDeploymentEnv && { TERRA_DEPLOYMENT_ENV: terraDeploymentEnv }),
+      ...customDrsResolverArgs,
     };
 
     sendCloudEnvironmentMetrics();
