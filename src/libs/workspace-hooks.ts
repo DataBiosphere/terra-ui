0d5129466352e9c3db25bfe80f61d6bb7b7006e2
Nick Watts <nwatts@broadinstitute.org>
2023-08-02 09:48:11 -0400

    Remove useLoadedState

--- src/libs/workspace-hooks.ts
+++ src/libs/workspace-hooks.ts
@@ -1,38 +1,46 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Ajax } from 'src/libs/ajax';
-import { useLoadedData } from 'src/libs/ajax/loaded-data/useLoadedData';
 import { useCancellation } from 'src/libs/react-utils';
-import { exhaustiveGuard, renameKey } from 'src/libs/type-utils/type-helpers';
 import { WorkspaceWrapper } from 'src/libs/workspace-utils';
 
-export const useWorkspaceById = (workspaceId: string, fields?: string[]) => {
-  const [workspace, loadWorkspace] = useLoadedData<WorkspaceWrapper>();
+type UseWorkspaceResult =
+  | {
+      status: 'Loading';
+      workspace: null;
+    }
+  | {
+      status: 'Ready';
+      workspace: WorkspaceWrapper;
+    }
+  | {
+      status: 'Error';
+      workspace: null;
+      error: unknown;
+    };
+
+export const useWorkspaceById = (workspaceId: string, fields?: string[]): UseWorkspaceResult => {
+  const [state, setState] = useState<UseWorkspaceResult>({ status: 'Loading', workspace: null });
 
   const signal = useCancellation();
   useEffect(
     () => {
-      loadWorkspace(() => Ajax(signal).Workspaces.getById(workspaceId, fields));
+      const loadWorkspace = async () => {
+        setState({ status: 'Loading', workspace: null });
+        try {
+          const workspace = await Ajax(signal).Workspaces.getById(workspaceId, fields);
+          setState({ status: 'Ready', workspace });
+        } catch (error: unknown) {
+          setState({ status: 'Error', workspace: null, error });
+        }
+      };
+      loadWorkspace();
     },
     // Since fields is an array and will likely be defined inline (recreated on each render),
     // we can't use it directly in the dependencies list. Instead, using a joined list of the
     // fields provides the behavior we want, rerunning the effect when the fields change.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [loadWorkspace, signal, workspaceId, (fields || []).join(',')]
+    [signal, workspaceId, (fields || []).join(',')]
   );
 
-  const { status } = workspace;
-  switch (status) {
-    case 'None':
-      return { workspace: null, status: 'None' as const };
-    case 'Loading':
-      // useLoadedData keeps the previous state while loading new data.
-      // When we're loading a different workspace, we don't want to return the previous workspace.
-      return { workspace: null, status: 'Loading' as const };
-    case 'Ready':
-      return renameKey(workspace, 'state', 'workspace');
-    case 'Error':
-      return renameKey(workspace, 'state', 'workspace');
-    default:
-      return exhaustiveGuard(status);
-  }
+  return state;
 };
