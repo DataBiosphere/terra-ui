fa7aeb6e3cb2cf8eaceea7d90b82485d516fabb9
Michael Spector <michael.l.spector@gmail.com>
2023-08-02 11:26:29 -0400

    UX adjustments

--- src/workflows-app/components/WorkflowsAppLauncherCard.js
+++ src/workflows-app/components/WorkflowsAppLauncherCard.js
@@ -1,5 +1,6 @@
 import { div, h } from 'react-hyperscript-helpers';
 import { ButtonPrimary, Link } from 'src/components/common';
+import { centeredSpinner } from 'src/components/icons';
 import TitleBar from 'src/components/TitleBar';
 import * as Style from 'src/libs/style';
 import * as Utils from 'src/libs/utils';
@@ -19,22 +20,30 @@ export const WorkflowsAppLauncherCard = ({ onClick, disabled, ...props }) => {
       title: 'Launch the Workflows App to run workflows',
       style: { marginBottom: '0.5rem' },
     }),
-    div(['The Workflows App must be launched in order to explore, view, and submit workflows.']),
-    div({ style: { display: 'flex', marginTop: '2rem', justifyContent: 'flex-center' } }, [
-      !disabled && 'Would you like to get started?',
-      disabled &&
-        'Workflows App is being created. This could take several minutes. You may exit this page and return later without interrupting the creation process.',
-    ]),
     div({ style: { display: 'flex', marginTop: '1rem', justifyContent: 'flex-center' } }, [
-      h(
-        ButtonPrimary,
-        {
-          disabled,
-          tooltip: disabled ? 'Workflows App is being created' : 'Create Workflows App',
-          onClick,
-        },
-        ['Yes, launch the Workflows App']
-      ),
+      'The Workflows App must be launched in order to explore, view, and submit workflows.',
+    ]),
+    div({ style: { display: 'flex', marginTop: '.5rem', justifyContent: 'flex-center' } }, [
+      'Once launched, it will stay on until the workspace is deleted.',
+    ]),
+    div({ style: { display: 'flex', marginTop: '2rem', justifyContent: 'flex-center' } }, [
+      disabled
+        ? 'Workflows App is being created. You may exit this page and return later without interrupting the creation process.'
+        : 'Would you like to get started?',
+    ]),
+    div({ style: { display: 'flex', marginTop: '1rem', justifyContent: 'flex-center', width: '18rem' } }, [
+      disabled
+        ? div({ style: { marginLeft: '1rem' } }, [centeredSpinner({ size: 36 })])
+        : h(
+            ButtonPrimary,
+            {
+              disabled,
+              tooltip: disabled ? 'Workflows App is being created' : 'Create Workflows App',
+              onClick,
+              style: { width: '100%' },
+            },
+            ['Yes, launch the Workflows App ']
+          ),
     ]),
     h(
       Link,
