1065fc1d521aa8cec29cba12d50257f6ae10794b
cahrens <roberts@broadinstitute.org>
2023-08-03 15:29:21 -0400

    Publish packages to private google artifact repository (UIE-95).

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,20 @@
+npx google-artifactregistry-auth --yes
+for d in */ ; do
+    cd $d
+
+    PACKAGE_NAME=$(node -p "require('./package.json').name")
+    PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+    echo "Will check if version ${PACKAGE_VERSION} of ${PACKAGE_NAME} should be published."
+
+    PUBLISHED_VERSION=$(npm view "${PACKAGE_NAME}" version --no-workspaces)
+    echo "Current version of ${PACKAGE_NAME} in repository is ${PUBLISHED_VERSION}."
+
+    if [[ ${PUBLISHED_VERSION} != ${PACKAGE_VERSION} ]]; then
+      npm publish
+      echo "Successfully published version ${PACKAGE_VERSION} of ${PACKAGE_NAME}."
+    else
+       echo "Version ${PACKAGE_VERSION} of ${PACKAGE_NAME} has already been published, so no new version has been published."
+   fi
+
+   cd ..
+done
