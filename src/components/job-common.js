db9002929758d5d2d311f9613538e7a2c909ba81
Katrina Pierre <kpierre@broadinstitute.org>
2023-08-04 10:50:43 -0400

    submission details page

--- src/components/job-common.js
+++ src/components/job-common.js
@@ -1,9 +1,11 @@
+import { isEmpty, isNil, kebabCase } from 'lodash';
 import { Fragment } from 'react';
-import { div, h, h3, h4 } from 'react-hyperscript-helpers';
-import { Link } from 'src/components/common';
+import { div, h, h1, h3, h4, span } from 'react-hyperscript-helpers';
+import { ButtonOutline, Link } from 'src/components/common';
 import { icon } from 'src/components/icons';
 import { TooltipCell } from 'src/components/table';
 import colors from 'src/libs/colors';
+import { goToPath } from 'src/libs/nav';
 import * as Nav from 'src/libs/nav';
 import * as Style from 'src/libs/style';
 
@@ -13,6 +15,36 @@ export const addCountSuffix = (label, count = undefined) => {
 };
 
 export const statusType = {
+  aborted: {
+    id: 'aborted', // Must match variable name for collection unpacking.
+    label: () => 'Aborted',
+    icon: (style) => icon('error-standard', { size: iconSize, style: { color: colors.warning(), ...style } }),
+  },
+  initializing: {
+    id: 'initializing', // Must match variable name for collection unpacking.
+    label: () => 'Initializing',
+    icon: (style) => icon('sync', { size: iconSize, style: { color: colors.dark(), ...style } }),
+  },
+  canceled: {
+    id: 'canceled', // Must match variable name for collection unpacking.
+    label: () => 'Canceled',
+    icon: (style) => icon('warning-standard', { size: iconSize, style: { color: colors.dark(), ...style } }),
+  },
+  canceling: {
+    id: 'canceling', // Must match variable name for collection unpacking.
+    label: () => 'Canceling',
+    icon: (style) => icon('sync', { size: iconSize, style: { color: colors.dark(), ...style } }),
+  },
+  paused: {
+    id: 'paused', // Must match variable name for collection unpacking.
+    label: () => 'Paused',
+    icon: (style) => icon('pause', { size: iconSize, style: { color: colors.dark(), ...style } }),
+  },
+  queued: {
+    id: 'queued', // Must match variable name for collection unpacking.
+    label: () => 'Queued',
+    icon: (style) => icon('clock', { size: iconSize, style: { color: colors.dark(), ...style } }),
+  },
   succeeded: {
     id: 'succeeded', // Must match variable name for collection unpacking.
     label: () => 'Succeeded',
@@ -159,5 +191,56 @@ export const workflowDetailsBreadcrumbSubtitle = (namespace, workspaceName, subm
     ),
     breadcrumbHistoryCaret,
     h3({ style: Style.elements.sectionHeader }, [`Workflow ${workflowId}`]),
+  ]);
+};
+
+export const Breadcrumbs = ({ breadcrumbPathObjects, pageId }) => {
+  const links = breadcrumbPathObjects.map(({ label, path, params }, index) => {
+    const attributes = { key: `${kebabCase(label)}-breadcrumb-link` };
+    let component;
+    if (!isNil(path)) {
+      attributes.onClick = () => goToPath(path, params);
+      component = h(Link, { ...attributes }, [label]);
+    } else {
+      component = span({ ...attributes }, [label]);
+    }
+
+    const children = [component];
+
+    if (index < breadcrumbPathObjects.length - 1) {
+      children.push(breadcrumbHistoryCaret);
+    }
+
+    return span({ key: `${kebabCase(label)}-breadcrumb-link` }, children);
+  });
+
+  return div({ id: `${pageId}-breadcrumbs-container` }, links);
+};
+
+export const SubmitNewWorkflowButton = h(
+  ButtonOutline,
+  {
+    onClick: () => goToPath('root'),
+  },
+  ['Submit a new workflow']
+);
+
+export const PageHeader = ({ breadcrumbPathObjects, title }) => {
+  const pageId = kebabCase(title);
+  return div({ id: `${pageId}-header-container` }, [
+    h1(
+      {
+        /* Make adjustments if needed */
+      },
+      [title]
+    ),
+    h(Breadcrumbs, { isRendered: !isEmpty(breadcrumbPathObjects), breadcrumbPathObjects, pageId }),
+  ]);
+};
+
+export const HeaderSection = ({ title, breadcrumbPathObjects, button }) => {
+  return div({ id: 'header-section', style: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' } }, [
+    h(PageHeader, { breadcrumbPathObjects, title }),
+    button,
   ]);
 };
