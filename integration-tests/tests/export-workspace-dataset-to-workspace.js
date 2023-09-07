113072a94bda98cd704a758b0716d336add3d12f
Sky Rubenstein <blrubenstein@wpi.edu>
2023-09-07 10:23:37 -0400

    [DC-758] Delete run-catalog-workflow and move to using existing workspaces (#4188)

---
+++ integration-tests/tests/export-workspace-dataset-to-workspace.js
@@ -0,0 +1,18 @@
+const _ = require('lodash/fp');
+const { withWorkspace } = require('../utils/integration-helpers');
+const { registerTest } = require('../utils/jest-utils');
+const { withUserToken } = require('../utils/terra-sa-utils');
+const { testExportToWorkspace } = require('../utils/catalog-utils');
+
+const exportWorkspaceDatasetToWorkspace = _.flow(
+  withWorkspace,
+  withUserToken
+)(async ({ billingProject, page, testUrl, token, workspaceName }) => {
+  await testExportToWorkspace(billingProject, page, testUrl, token, 'Readable Catalog Workspace 1', workspaceName);
+});
+
+registerTest({
+  name: 'export-workspace-dataset-to-workspace',
+  fn: exportWorkspaceDatasetToWorkspace,
+  timeout: 2 * 60 * 1000,
+});
