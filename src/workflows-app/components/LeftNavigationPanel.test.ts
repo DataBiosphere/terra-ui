ea15a2c97af3cf7cead56b68a5d0025c84f27848
Katrina Pierre <kpierre@broadinstitute.org>
2023-09-08 15:43:36 -0400

    Left nav test

---
+++ src/workflows-app/components/LeftNavigationPanel.test.ts
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { h } from 'react-hyperscript-helpers';
+import { LeftNavigationPanel } from 'src/workflows-app/components/LeftNavigationPanel';
+import { mockAzureWorkspace } from 'src/workflows-app/utils/mock-responses';
+
+describe('Left Navigation Panel', () => {
+  it('renders headers', async () => {
+    const user = userEvent.setup();
+
+    render(
+      h(LeftNavigationPanel, {
+        name: 'test-azure-ws-name',
+        namespace: 'test-azure-ws-namespace',
+        workspace: mockAzureWorkspace,
+        loading: false,
+      })
+    );
+
+    // Assert
+    screen.getByText('Workflows in this workspace');
+    screen.getByText('Submission history');
+    screen.getByText('Find & add workflows');
+    screen.getByText('Featured workflows');
+    screen.getByText('Import a workflow');
+    screen.getByText('Dockstore');
+    screen.getByText('Have questions?');
+
+    // Act
+    const findAndAddWorkflowsCollapse = screen.getByText('Find & add workflows');
+    const featuredWorkflows = screen.getByText('Featured workflows');
+    const importWorkflows = screen.getByText('Import a workflow');
+
+    await user.click(findAndAddWorkflowsCollapse);
+
+    expect(featuredWorkflows).not.toBeInTheDocument();
+    expect(importWorkflows).not.toBeInTheDocument();
+  });
+});
