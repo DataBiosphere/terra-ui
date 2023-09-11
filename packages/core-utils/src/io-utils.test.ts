92ca64caa617507be8988004d319bb1606b2689a
Nick Watts <nwatts@broadinstitute.org>
2023-09-11 17:29:22 -0400

    Add test for error

--- packages/core-utils/src/io-utils.test.ts
+++ packages/core-utils/src/io-utils.test.ts
@@ -11,4 +11,29 @@ describe('readFileAsText', () => {
     // Assert
     expect(content).toEqual('Hello world');
   });
+
+  it('throws an error if read fails', async () => {
+    // Arrange
+    class MockFileReader {
+      onError: ((err: any) => void) | null = null;
+
+      readAsText() {
+        this.onError?.(new Error('Something went wrong'));
+      }
+    }
+
+    const FileReader = window.FileReader;
+    window.FileReader = MockFileReader as any;
+
+    const file = new File(['Hello world'], 'test-file.txt');
+
+    // Act
+    const result = readFileAsText(file);
+
+    // Assert
+    expect(result).rejects.toEqual(new Error('Something went wrong'));
+
+    // Cleanup
+    window.FileReader = FileReader;
+  });
 });
