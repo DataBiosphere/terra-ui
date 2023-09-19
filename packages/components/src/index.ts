3fed93e8ca25d4b1090649c6d19165b8ca2ddbe9
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-09-18 21:46:32 -0400

    [UIE-51] Add useUniqueId to components package (#4230)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,5 @@
 export * from './ErrorBoundary';
+export * from './hooks/useUniqueId';
 export * from './hooks/useWindowDimensions';
 export * from './icon';
 export type { IconId } from './icon-library';
