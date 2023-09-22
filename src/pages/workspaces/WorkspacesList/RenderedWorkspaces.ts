efae8b7f5226f90c9cbc11db427372aeb8870a64
Blake Geno <bgeno@broadinstitute.org>
2023-09-22 11:46:06 -0400

    create separate components for cells

--- src/pages/workspaces/WorkspacesList/RenderedWorkspaces.ts
+++ src/pages/workspaces/WorkspacesList/RenderedWorkspaces.ts
@@ -56,8 +56,6 @@ interface RenderedWorkspacesProps {
 }
 
 export const RenderedWorkspaces: FC<RenderedWorkspacesProps> = ({ workspaces, loadingSubmissionStats, ...props }) => {
-  const getWorkspace = (id: string): Workspace => _.find({ workspace: { workspaceId: id } }, workspaces)!;
-
   const {
     profile: { starredWorkspaces },
   } = useStore<AuthState>(authStore);
@@ -78,191 +76,6 @@ export const RenderedWorkspaces: FC<RenderedWorkspacesProps> = ({ workspaces, lo
     workspaces
   );
 
-  const columnDefinitions: FlexTableColumn<Workspace>[] = [
-    {
-      field: 'starred',
-      headerRenderer: () => div({ className: 'sr-only' }, ['Starred']),
-      cellRenderer: ({ rowIndex }) => {
-        const workspace = sortedWorkspaces[rowIndex];
-        return div(
-          {
-            style: {
-              ...styles.tableCellContainer,
-              justifyContent: 'center',
-              alignItems: 'center',
-              padding: '0.5rem 0',
-            },
-          },
-          [h(WorkspaceStarControl, { workspace })]
-        );
-      },
-      size: { basis: 40, grow: 0, shrink: 0 },
-    },
-    {
-      field: 'name',
-      headerRenderer: makeHeaderRenderer('name'),
-      cellRenderer: ({ rowIndex }) => {
-        const {
-          accessLevel,
-          workspace,
-          workspace: { workspaceId, namespace, name, attributes },
-        } = sortedWorkspaces[rowIndex];
-        const description = attributes?.description;
-        const canView = Utils.canRead(accessLevel);
-        const canAccessWorkspace = () =>
-          !canView ? updateWorkspaceActions({ requestingAccessWorkspaceId: workspaceId }) : undefined;
-
-        return div({ style: styles.tableCellContainer }, [
-          div({ style: styles.tableCellContent }, [
-            h(
-              Link,
-              {
-                'aria-haspopup': canView ? undefined : 'dialog',
-                style: {
-                  ...(canView ? {} : { color: colors.dark(0.8), fontStyle: 'italic' }),
-                  fontWeight: 600,
-                  fontSize: 16,
-                  ...Style.noWrapEllipsis,
-                },
-                href: canView ? getLink('workspace-dashboard', { namespace, name }) : undefined,
-                onClick: () => {
-                  canAccessWorkspace();
-                  !!canView &&
-                    Ajax().Metrics.captureEvent(Events.workspaceOpenFromList, extractWorkspaceDetails(workspace));
-                },
-                tooltip:
-                  !canView &&
-                  'You cannot access this workspace because it is protected by an Authorization Domain. Click to learn about gaining access.',
-                tooltipSide: 'right',
-              },
-              [name]
-            ),
-          ]),
-          div({ style: { ...styles.tableCellContent } }, [
-            h(
-              FirstParagraphMarkdownViewer,
-              {
-                style: {
-                  height: '1.5rem',
-                  margin: 0,
-                  ...Style.noWrapEllipsis,
-                  color: description ? undefined : colors.dark(0.75),
-                  fontSize: 14,
-                },
-              },
-              [description?.toString() || 'No description added']
-            ),
-          ]),
-        ]);
-      },
-      size: { basis: 400, grow: 2, shrink: 0 },
-    },
-    {
-      field: 'lastModified',
-      headerRenderer: makeHeaderRenderer('lastModified'),
-      cellRenderer: ({ rowIndex }) => {
-        const {
-          workspace: { lastModified },
-        } = sortedWorkspaces[rowIndex];
-
-        return div({ style: styles.tableCellContainer }, [
-          div({ style: styles.tableCellContent }, [
-            h(TooltipTrigger, { content: Utils.makeCompleteDate(lastModified) }, [
-              div([Utils.makeStandardDate(lastModified)]),
-            ]),
-          ]),
-        ]);
-      },
-      size: { basis: 100, grow: 1, shrink: 0 },
-    },
-    {
-      field: 'createdBy',
-      headerRenderer: makeHeaderRenderer('createdBy'),
-      cellRenderer: ({ rowIndex }) => {
-        const {
-          workspace: { createdBy },
-        } = sortedWorkspaces[rowIndex];
-
-        return div({ style: styles.tableCellContainer }, [
-          div({ style: styles.tableCellContent }, [span({ style: Style.noWrapEllipsis }, [createdBy])]),
-        ]);
-      },
-      size: { basis: 200, grow: 1, shrink: 0 },
-    },
-    {
-      field: 'accessLevel',
-      headerRenderer: makeHeaderRenderer('accessLevel'),
-      cellRenderer: ({ rowIndex }) => {
-        const { accessLevel } = sortedWorkspaces[rowIndex];
-
-        return div({ style: styles.tableCellContainer }, [
-          div({ style: styles.tableCellContent }, [Utils.normalizeLabel(accessLevel)]),
-        ]);
-      },
-      size: { basis: 120, grow: 1, shrink: 0 },
-    },
-    {
-      headerRenderer: () => div({ className: 'sr-only' }, ['Last Workflow Submitted Status']),
-      cellRenderer: ({ rowIndex }) => {
-        const workspace = sortedWorkspaces[rowIndex];
-        const lastRunStatus = workspaceSubmissionStatus(workspace);
-
-        return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
-          div({ style: styles.tableCellContent }, [
-            h(WorkspaceSubmissionStatusIcon, {
-              status: lastRunStatus,
-              loadingSubmissionStats,
-            }),
-          ]),
-        ]);
-      },
-      size: { basis: 30, grow: 0, shrink: 0 },
-    },
-    {
-      headerRenderer: () => div({ className: 'sr-only' }, ['Cloud Platform']),
-      cellRenderer: ({ rowIndex }) => {
-        const workspace = sortedWorkspaces[rowIndex];
-        return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
-          div({ style: styles.tableCellContent }, [
-            h(CloudProviderIcon, { cloudProvider: getCloudProviderFromWorkspace(workspace) }),
-          ]),
-        ]);
-      },
-      size: { basis: 30, grow: 0, shrink: 0 },
-    },
-    {
-      headerRenderer: () => div({ className: 'sr-only' }, ['Actions']),
-      cellRenderer: ({ rowIndex }) => {
-        const {
-          accessLevel,
-          workspace: { workspaceId, namespace, name },
-        } = sortedWorkspaces[rowIndex];
-        if (!Utils.canRead(accessLevel)) {
-          // No menu shown if user does not have read access.
-          return div({ className: 'sr-only' }, ['You do not have permission to perform actions on this workspace.']);
-        }
-        const onClone = () => updateWorkspaceActions({ cloningWorkspaceId: workspaceId });
-        const onDelete = () => updateWorkspaceActions({ deletingWorkspaceId: workspaceId });
-        const onLock = () => updateWorkspaceActions({ lockingWorkspaceId: workspaceId });
-        const onShare = (policies) =>
-          updateWorkspaceActions({ sharingWorkspace: { ...getWorkspace(workspaceId), policies } });
-        const onLeave = () => updateWorkspaceActions({ leavingWorkspaceId: workspaceId });
-
-        return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
-          div({ style: styles.tableCellContent }, [
-            h(WorkspaceMenu, {
-              iconSize: 20,
-              popupLocation: 'left',
-              callbacks: { onClone, onShare, onLock, onDelete, onLeave },
-              workspaceInfo: { namespace, name },
-            }),
-          ]),
-        ]);
-      },
-      size: { basis: 30, grow: 0, shrink: 0 },
-    },
-  ];
-
   return div({ style: { flex: 1, backgroundColor: 'white', padding: '0 1rem' } }, [
     h(AutoSizer, [
       ({ width, height }) =>
@@ -275,27 +88,218 @@ export const RenderedWorkspaces: FC<RenderedWorkspacesProps> = ({ workspaces, lo
           variant: 'light',
           rowHeight: 70,
           sort,
-          columns: columnDefinitions,
+          columns: [
+            {
+              field: 'starred',
+              headerRenderer: () => div({ className: 'sr-only' }, ['Starred']),
+              cellRenderer: ({ rowIndex }) => h(StarCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 40, grow: 0, shrink: 0 },
+            },
+            {
+              field: 'name',
+              headerRenderer: makeHeaderRenderer('name'),
+              cellRenderer: ({ rowIndex }) => h(NameCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 400, grow: 2, shrink: 0 },
+            },
+            {
+              field: 'lastModified',
+              headerRenderer: makeHeaderRenderer('lastModified'),
+              cellRenderer: ({ rowIndex }) => h(LastModifiedCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 100, grow: 1, shrink: 0 },
+            },
+            {
+              field: 'createdBy',
+              headerRenderer: makeHeaderRenderer('createdBy'),
+              cellRenderer: ({ rowIndex }) => h(CreatedByCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 200, grow: 1, shrink: 0 },
+            },
+            {
+              field: 'accessLevel',
+              headerRenderer: makeHeaderRenderer('accessLevel'),
+              cellRenderer: ({ rowIndex }) => h(AccessLevelCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 120, grow: 1, shrink: 0 },
+            },
+            {
+              headerRenderer: () => div({ className: 'sr-only' }, ['Last Workflow Submitted Status']),
+              cellRenderer: ({ rowIndex }) =>
+                h(SubmissionStatusCell, { workspace: sortedWorkspaces[rowIndex], loadingSubmissionStats }),
+              size: { basis: 30, grow: 0, shrink: 0 },
+            },
+            {
+              headerRenderer: () => div({ className: 'sr-only' }, ['Cloud Platform']),
+              cellRenderer: ({ rowIndex }) => h(CloudPlatformCell, { workspace: sortedWorkspaces[rowIndex] }),
+              size: { basis: 30, grow: 0, shrink: 0 },
+            },
+            {
+              headerRenderer: () => div({ className: 'sr-only' }, ['Actions']),
+              cellRenderer: ({ rowIndex }) =>
+                h(ActionsCell, { workspace: sortedWorkspaces[rowIndex], workspaces: sortedWorkspaces }),
+              size: { basis: 30, grow: 0, shrink: 0 },
+            },
+          ],
         }),
     ]),
   ]);
 };
 
-interface CellRendererProps<T> {
-  data: T;
-  columnIndex: number;
-  rowIndex: number;
+interface CellProps {
+  workspace: Workspace;
 }
 
-interface FlexTableColumn<T> {
-  field?: string;
-  headerRenderer: () => ReactNode;
-  cellRenderer: (props: CellRendererProps<T>) => ReactNode;
-  size: {
-    basis?: number;
-    grow?: number;
-    max?: number;
-    min?: number;
-    shrink?: number;
-  };
+const StarCell: FC<CellProps> = ({ workspace }) =>
+  div(
+    {
+      style: {
+        ...styles.tableCellContainer,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0.5rem 0',
+      },
+    },
+    [h(WorkspaceStarControl, { workspace })]
+  );
+
+const NameCell: FC<CellProps> = (props) => {
+  const {
+    accessLevel,
+    workspace,
+    workspace: { workspaceId, namespace, name, attributes },
+  } = props.workspace;
+  const description = attributes?.description;
+  const canView = Utils.canRead(accessLevel);
+  const canAccessWorkspace = () =>
+    !canView ? updateWorkspaceActions({ requestingAccessWorkspaceId: workspaceId }) : undefined;
+
+  return div({ style: styles.tableCellContainer }, [
+    div({ style: styles.tableCellContent }, [
+      h(
+        Link,
+        {
+          'aria-haspopup': canView ? undefined : 'dialog',
+          style: {
+            ...(canView ? {} : { color: colors.dark(0.8), fontStyle: 'italic' }),
+            fontWeight: 600,
+            fontSize: 16,
+            ...Style.noWrapEllipsis,
+          },
+          href: canView ? getLink('workspace-dashboard', { namespace, name }) : undefined,
+          onClick: () => {
+            canAccessWorkspace();
+            !!canView && Ajax().Metrics.captureEvent(Events.workspaceOpenFromList, extractWorkspaceDetails(workspace));
+          },
+          tooltip:
+            !canView &&
+            'You cannot access this workspace because it is protected by an Authorization Domain. Click to learn about gaining access.',
+          tooltipSide: 'right',
+        },
+        [name]
+      ),
+    ]),
+    div({ style: { ...styles.tableCellContent } }, [
+      h(
+        FirstParagraphMarkdownViewer,
+        {
+          style: {
+            height: '1.5rem',
+            margin: 0,
+            ...Style.noWrapEllipsis,
+            color: description ? undefined : colors.dark(0.75),
+            fontSize: 14,
+          },
+        },
+        [description?.toString() || 'No description added']
+      ),
+    ]),
+  ]);
+};
+
+const LastModifiedCell: FC<CellProps> = (props) => {
+  const {
+    workspace: { lastModified },
+  } = props.workspace;
+
+  return div({ style: styles.tableCellContainer }, [
+    div({ style: styles.tableCellContent }, [
+      h(TooltipTrigger, { content: Utils.makeCompleteDate(lastModified) }, [
+        div([Utils.makeStandardDate(lastModified)]),
+      ]),
+    ]),
+  ]);
+};
+
+const CreatedByCell: FC<CellProps> = (props) => {
+  const {
+    workspace: { createdBy },
+  } = props.workspace;
+
+  return div({ style: styles.tableCellContainer }, [
+    div({ style: styles.tableCellContent }, [span({ style: Style.noWrapEllipsis }, [createdBy])]),
+  ]);
+};
+
+const AccessLevelCell: FC<CellProps> = (props) => {
+  const { accessLevel } = props.workspace;
+
+  return div({ style: styles.tableCellContainer }, [
+    div({ style: styles.tableCellContent }, [Utils.normalizeLabel(accessLevel)]),
+  ]);
+};
+
+interface SubmissionStatusCellProps extends CellProps {
+  loadingSubmissionStats: boolean;
 }
+
+const SubmissionStatusCell: FC<SubmissionStatusCellProps> = ({ workspace, loadingSubmissionStats }) => {
+  const lastRunStatus = workspaceSubmissionStatus(workspace);
+
+  return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
+    div({ style: styles.tableCellContent }, [
+      h(WorkspaceSubmissionStatusIcon, {
+        status: lastRunStatus,
+        loadingSubmissionStats,
+      }),
+    ]),
+  ]);
+};
+
+const CloudPlatformCell: FC<CellProps> = ({ workspace }) => {
+  return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
+    div({ style: styles.tableCellContent }, [
+      h(CloudProviderIcon, { cloudProvider: getCloudProviderFromWorkspace(workspace) }),
+    ]),
+  ]);
+};
+
+interface ActionsCellProps extends CellProps {
+  workspaces: Workspace[];
+}
+
+const ActionsCell: FC<ActionsCellProps> = (props) => {
+  const {
+    accessLevel,
+    workspace: { workspaceId, namespace, name },
+  } = props.workspace;
+  if (!Utils.canRead(accessLevel)) {
+    // No menu shown if user does not have read access.
+    return div({ className: 'sr-only' }, ['You do not have permission to perform actions on this workspace.']);
+  }
+  const getWorkspace = (id: string): Workspace => _.find({ workspace: { workspaceId: id } }, props.workspaces)!;
+
+  const onClone = () => updateWorkspaceActions({ cloningWorkspaceId: workspaceId });
+  const onDelete = () => updateWorkspaceActions({ deletingWorkspaceId: workspaceId });
+  const onLock = () => updateWorkspaceActions({ lockingWorkspaceId: workspaceId });
+  const onShare = (policies) =>
+    updateWorkspaceActions({ sharingWorkspace: { ...getWorkspace(workspaceId), policies } });
+  const onLeave = () => updateWorkspaceActions({ leavingWorkspaceId: workspaceId });
+
+  return div({ style: { ...styles.tableCellContainer, paddingRight: 0 } }, [
+    div({ style: styles.tableCellContent }, [
+      h(WorkspaceMenu, {
+        iconSize: 20,
+        popupLocation: 'left',
+        callbacks: { onClone, onShare, onLock, onDelete, onLeave },
+        workspaceInfo: { namespace, name },
+      }),
+    ]),
+  ]);
+};
