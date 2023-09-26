4eb29b1321a0b3a8f8491adb61f5a96feaf09559
Nick Watts <nwatts@broadinstitute.org>
2023-09-26 13:33:35 -0400

    Move LocalVariablesContent to workspace-data

--- src/workspace-data/Data.js
+++ src/workspace-data/Data.js
@@ -11,7 +11,6 @@ import Collapse from 'src/components/Collapse';
 import { ButtonOutline, Clickable, DeleteConfirmationModal, Link, spinnerOverlay } from 'src/components/common';
 import { DataTableSaveVersionModal, DataTableVersion, DataTableVersions } from 'src/components/data/data-table-versions';
 import FileBrowser from 'src/components/data/FileBrowser';
-import LocalVariablesContent from 'src/components/data/LocalVariablesContent';
 import { icon, spinner } from 'src/components/icons';
 import { ConfirmedSearchInput } from 'src/components/input';
 import { MenuButton } from 'src/components/MenuButton';
@@ -50,6 +49,7 @@ import { getReferenceData } from './reference-data/reference-data-utils';
 import { ReferenceDataContent } from './reference-data/ReferenceDataContent';
 import { ReferenceDataDeleter } from './reference-data/ReferenceDataDeleter';
 import { ReferenceDataImporter } from './reference-data/ReferenceDataImporter';
+import { WorkspaceAttributes } from './WorkspaceAttributes';
 
 const styles = {
   tableContainer: {
@@ -1356,7 +1356,7 @@ export const WorkspaceData = _.flow(
                 [
                   workspaceDataTypes.localVariables,
                   () =>
-                    h(LocalVariablesContent, {
+                    h(WorkspaceAttributes, {
                       workspace,
                       refreshKey,
                     }),
