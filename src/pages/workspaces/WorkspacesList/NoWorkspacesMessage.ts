91c8fe317ab83ef40757e7875291c2646bc50308
Blake Geno <bgeno@broadinstitute.org>
2023-09-26 19:03:45 -0400

    WOR-1227 Part 1 - List Refactor (#4262)
    Co-authored-by: Nick Watts <1156625+nawatts@users.noreply.github.com>

---
+++ src/pages/workspaces/WorkspacesList/NoWorkspacesMessage.ts
@@ -0,0 +1,33 @@
+import _ from 'lodash/fp';
+import { Fragment, ReactNode, useContext } from 'react';
+import { div, h } from 'react-hyperscript-helpers';
+import { NoWorkspacesMessage } from 'src/components/workspace-utils';
+import { cond } from 'src/libs/utils';
+import { CategorizedWorkspaces } from 'src/pages/workspaces/WorkspacesList/CategorizedWorkspaces';
+import { WorkspaceFilterValues } from 'src/pages/workspaces/WorkspacesList/WorkspaceFilters';
+import { WorkspaceUserActionsContext } from 'src/pages/workspaces/WorkspacesList/WorkspaceUserActions';
+
+interface NoContentMessageProps {
+  loadingWorkspaces: boolean;
+  loadingSubmissionStats: boolean;
+  workspaces: CategorizedWorkspaces;
+  filters: WorkspaceFilterValues;
+}
+
+export const NoContentMessage = (props: NoContentMessageProps): ReactNode => {
+  const { loadingWorkspaces, loadingSubmissionStats, workspaces, filters } = props;
+  const { setUserActions } = useContext(WorkspaceUserActionsContext);
+
+  return cond(
+    [loadingWorkspaces, () => h(Fragment, ['Loading...'])],
+    [
+      _.isEmpty(workspaces.myWorkspaces) && filters.tab === 'myWorkspaces',
+      () =>
+        NoWorkspacesMessage({
+          onClick: () => setUserActions({ creatingNewWorkspace: true }),
+        }),
+    ],
+    [!_.isEmpty(filters.submissions) && loadingSubmissionStats, () => h(Fragment, ['Loading submission statuses...'])],
+    () => div({ style: { fontStyle: 'italic' } }, ['No matching workspaces'])
+  );
+};
