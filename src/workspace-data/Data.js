bda09164173a803a4def6cef46b73d21822ecd78
Ricky Magner <81349869+rickymagner@users.noreply.github.com>
2023-09-27 08:25:03 -0400

    Add reference aliases to allow for custom naming with species prefix. (#4259)

--- src/workspace-data/Data.js
+++ src/workspace-data/Data.js
@@ -47,6 +47,7 @@ import WDSContent from './data-table/wds/WDSContent';
 import { WdsTroubleshooter } from './data-table/wds/WdsTroubleshooter';
 import { useImportJobs } from './import-jobs';
 import { getReferenceData } from './reference-data/reference-data-utils';
+import { getReferenceLabel } from './reference-data/reference-metadata';
 import { ReferenceDataContent } from './reference-data/ReferenceDataContent';
 import { ReferenceDataDeleter } from './reference-data/ReferenceDataDeleter';
 import { ReferenceDataImporter } from './reference-data/ReferenceDataImporter';
@@ -1190,7 +1191,7 @@ export const WorkspaceData = _.flow(
                                   {
                                     style: { flex: 0 },
                                     disabled: !!WorkspaceUtils.editWorkspaceError(workspace),
-                                    tooltip: WorkspaceUtils.editWorkspaceError(workspace) || `Delete ${type}`,
+                                    tooltip: WorkspaceUtils.editWorkspaceError(workspace) || `Delete ${getReferenceLabel(type)} reference`,
                                     onClick: (e) => {
                                       e.stopPropagation();
                                       setDeletingReference(type);
@@ -1199,7 +1200,7 @@ export const WorkspaceData = _.flow(
                                   [icon('minus-circle', { size: 16 })]
                                 ),
                               },
-                              [type]
+                              [getReferenceLabel(type)]
                             ),
                           _.keys(referenceData)
                         ),
