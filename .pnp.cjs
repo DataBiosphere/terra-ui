e8f1647cd40678ab06e9d2e98817c0da58f2f477
Nick Watts <nwatts@broadinstitute.org>
2023-09-27 17:48:04 -0400

    Use FocusTrap from components

--- .pnp.cjs
+++ .pnp.cjs
@@ -134,7 +134,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-draggable", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.4.4"],\
             ["react-dropzone", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:11.4.0"],\
             ["react-favicon", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.3"],\
-            ["react-focus-lock", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
             ["react-modal", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:3.16.1"],\
             ["react-notifications-component", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.0.1"],\
@@ -5094,7 +5093,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["lodash", "npm:4.17.21"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"],\
-            ["react-focus-lock", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5"],\
+            ["react-focus-lock", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=289587"],\
             ["vite", "virtual:6fecf1af4cab542f4a06b7ce7d9f710277dce92700e0011a9519e41948eed6d8f54c9d0aa109ead6cf4295edce81cb49620f9e823313e99632229bf20d133cdb#npm:4.4.7"],\
@@ -5130,7 +5129,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["lodash", "npm:4.17.21"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"],\
-            ["react-focus-lock", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5"],\
+            ["react-focus-lock", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=289587"],\
             ["vite", "virtual:6fecf1af4cab542f4a06b7ce7d9f710277dce92700e0011a9519e41948eed6d8f54c9d0aa109ead6cf4295edce81cb49620f9e823313e99632229bf20d133cdb#npm:4.4.7"],\
@@ -15962,10 +15961,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.2.6", {\
-          "packageLocation": "./.yarn/__virtual__/react-clientside-effect-virtual-9b4ad1ec6a/0/cache/react-clientside-effect-npm-1.2.6-4c95acf94f-7db6110027.zip/node_modules/react-clientside-effect/",\
+        ["virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.2.6", {\
+          "packageLocation": "./.yarn/__virtual__/react-clientside-effect-virtual-b27eadef37/0/cache/react-clientside-effect-npm-1.2.6-4c95acf94f-7db6110027.zip/node_modules/react-clientside-effect/",\
           "packageDependencies": [\
-            ["react-clientside-effect", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.2.6"],\
+            ["react-clientside-effect", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.2.6"],\
             ["@babel/runtime", "npm:7.22.6"],\
             ["@types/react", "npm:18.2.15"],\
             ["react", "npm:18.2.0"]\
@@ -16107,18 +16106,18 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5", {\
-          "packageLocation": "./.yarn/__virtual__/react-focus-lock-virtual-1b21dd04f3/0/cache/react-focus-lock-npm-2.9.5-1f6a825257-93473a6f0a.zip/node_modules/react-focus-lock/",\
+        ["virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5", {\
+          "packageLocation": "./.yarn/__virtual__/react-focus-lock-virtual-66d8028368/0/cache/react-focus-lock-npm-2.9.5-1f6a825257-93473a6f0a.zip/node_modules/react-focus-lock/",\
           "packageDependencies": [\
-            ["react-focus-lock", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5"],\
+            ["react-focus-lock", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5"],\
             ["@babel/runtime", "npm:7.22.6"],\
             ["@types/react", "npm:18.2.15"],\
             ["focus-lock", "npm:0.11.6"],\
             ["prop-types", "npm:15.8.1"],\
             ["react", "npm:18.2.0"],\
-            ["react-clientside-effect", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.2.6"],\
-            ["use-callback-ref", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.3.0"],\
-            ["use-sidecar", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.1.2"]\
+            ["react-clientside-effect", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.2.6"],\
+            ["use-callback-ref", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.3.0"],\
+            ["use-sidecar", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.1.2"]\
           ],\
           "packagePeers": [\
             "@types/react",\
@@ -17941,7 +17940,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-draggable", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.4.4"],\
             ["react-dropzone", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:11.4.0"],\
             ["react-favicon", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.3"],\
-            ["react-focus-lock", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
             ["react-modal", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:3.16.1"],\
             ["react-notifications-component", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.0.1"],\
@@ -18539,10 +18537,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.3.0", {\
-          "packageLocation": "./.yarn/__virtual__/use-callback-ref-virtual-e2ef88cdfa/0/cache/use-callback-ref-npm-1.3.0-6c0773783f-7913df383a.zip/node_modules/use-callback-ref/",\
+        ["virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.3.0", {\
+          "packageLocation": "./.yarn/__virtual__/use-callback-ref-virtual-21d73db10b/0/cache/use-callback-ref-npm-1.3.0-6c0773783f-7913df383a.zip/node_modules/use-callback-ref/",\
           "packageDependencies": [\
-            ["use-callback-ref", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.3.0"],\
+            ["use-callback-ref", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.3.0"],\
             ["@types/react", "npm:18.2.15"],\
             ["react", "npm:18.2.0"],\
             ["tslib", "npm:2.6.0"]\
@@ -18651,10 +18649,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.1.2", {\
-          "packageLocation": "./.yarn/__virtual__/use-sidecar-virtual-de5eeb18c9/0/cache/use-sidecar-npm-1.1.2-dfc322e94a-925d1922f9.zip/node_modules/use-sidecar/",\
+        ["virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.1.2", {\
+          "packageLocation": "./.yarn/__virtual__/use-sidecar-virtual-c50f71cf5e/0/cache/use-sidecar-npm-1.1.2-dfc322e94a-925d1922f9.zip/node_modules/use-sidecar/",\
           "packageDependencies": [\
-            ["use-sidecar", "virtual:1b21dd04f3921513133e0367784f0f972a5e42e3f1aa110d3b7a38b308c83e87b9a6ca409d99632e37898f8ea34d8ab7be35432b6447dd54ecab8509f26636a4#npm:1.1.2"],\
+            ["use-sidecar", "virtual:66d8028368cbed1f4e36ed56c976e2a63a61d05bd3de5d871527f5dfb4e86edf080ce2bdbbb30e2a229eaed8a93161522c61708e270bb6273f80765f07b1feb6#npm:1.1.2"],\
             ["@types/react", "npm:18.2.15"],\
             ["detect-node-es", "npm:1.1.0"],\
             ["react", "npm:18.2.0"],\
