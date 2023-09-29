9c4621bd3a4ee3d6593805425e0308074fc5ac38
tlangs <tlangs@broadinstitute.org>
2023-09-29 14:38:24 -0400

    update to use new Sam endpoint body

--- src/libs/ajax/SamResources.ts
+++ src/libs/ajax/SamResources.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash/fp';
+import { Ajax } from 'src/libs/ajax';
 import { appIdentifier, authOpts, fetchSam, jsonBody } from 'src/libs/ajax/ajax-common';
 
 type RequesterPaysProject = undefined | string;
@@ -9,21 +10,22 @@ export const SamResources = (signal: AbortSignal) => ({
       `api/resources/v2/${samResourceType}/${samResourceId}/leave`,
       _.mergeAll([authOpts(), appIdentifier, { method: 'DELETE' }])
     ),
+  getRequesterPaysSignedUrl: async (
+    gsPath: string,
+    requesterPaysProject: RequesterPaysProject = undefined
+  ): Promise<string> => {
+    const res = await fetchSam(
+      'api/google/v1/user/signedUrlForBlob',
+      _.mergeAll([jsonBody({ gsPath, requesterPaysProject }), authOpts(), appIdentifier, { signal, method: 'POST' }])
+    );
+    return res.json();
+  },
 
   getSignedUrl: async (
     bucket: string,
     object: string,
     requesterPaysProject: RequesterPaysProject = undefined
   ): Promise<string> => {
-    const res = await fetchSam(
-      'api/google/v1/user/signedUrlForBlob',
-      _.mergeAll([
-        jsonBody({ bucketName: bucket, blobName: object, requesterPaysProject }),
-        authOpts(),
-        appIdentifier,
-        { signal, method: 'POST' },
-      ])
-    );
-    return res.json();
+    return Ajax(signal).SamResources.getRequesterPaysSignedUrl(`gs://${bucket}/${object}`, requesterPaysProject);
   },
 });
