eee21d615a80c89202265729e6505234a65399bc
Dillon Scott <67511512+dillydally414@users.noreply.github.com>
2023-10-02 16:11:21 -0400

    Add return types
    Co-authored-by: Nick Watts <1156625+nawatts@users.noreply.github.com>

--- src/libs/nav.ts
+++ src/libs/nav.ts
@@ -73,7 +73,7 @@ export const getPath = (name: string, params?: Record<string, any>): string => {
 /**
  * alias for getPath()
  */
-export const getLink = (name: string, pathParams?: Record<string, any>, queryParams?: Record<string, any>) => {
+export const getLink = (name: string, pathParams?: Record<string, any>, queryParams?: Record<string, any>): string => {
   const path = getPath(name, pathParams).slice(1); // slice off leading slash
   const queryString = queryParams ? qs.stringify(queryParams, { addQueryPrefix: true }) : '';
   return `#${path}${queryString}`;
@@ -82,7 +82,7 @@ export const getLink = (name: string, pathParams?: Record<string, any>, queryPar
 /**
  * navigate the application to the desired nav path.
  */
-export const goToPath = (name: string, pathParams?: Record<string, any>, queryParams?: Record<string, any>) => {
+export const goToPath = (name: string, pathParams?: Record<string, any>, queryParams?: Record<string, any>): void => {
   history.push({
     pathname: getPath(name, pathParams),
     ...(queryParams ? { search: qs.stringify(queryParams, { addQueryPrefix: true }) } : {}),
