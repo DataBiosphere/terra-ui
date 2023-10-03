9c2d05f102b754b4e49bee3da26af26b5a34aa3f
Greg Polumbo <>
2023-10-03 16:59:21 -0400

    ID-839 makin tests work

--- src/pages/profile/external-identities/FenceLink.test.ts
+++ src/pages/profile/external-identities/FenceLink.test.ts
@@ -2,16 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { h } from 'react-hyperscript-helpers';
 import { FenceLink } from 'src/pages/profile/external-identities/FenceLink';
 
-describe('User is not linked', () => {
-  jest.mock('src/libs/nav', () => ({
-    ...jest.requireActual('src/libs/nav'),
-    getLink: jest.fn(() => '/'),
-    useRoute: jest.fn(() => {
-      return { name: 'fence-callback' };
-    }),
-  }));
+jest.mock('src/libs/nav', () => ({
+  ...jest.requireActual('src/libs/nav'),
+  getLink: jest.fn(() => '/'),
+  useRoute: jest.fn(() => {
+    return { name: 'fence-callback' };
+  }),
+}));
 
+describe('User is not linked', () => {
   it('renders spinner while fence-callback is loading', async () => {
+    // @ts-ignore
+    delete window.location;
+    global.window = Object.create(window);
+    const state = btoa(JSON.stringify({ provider: 'fence' }));
+    // @ts-ignore
+    global.window.location = {
+      search: `?state=${state}`,
+    };
     // Arrange
     const props = {
       key: 'fence',
@@ -46,6 +54,6 @@ describe('User is not linked', () => {
   });
 });
 
-describe('User is linked', () => {
-  it('renders the renew and unlink buttons');
-});
+// describe('User is linked', () => {
+//   it('renders the renew and unlink buttons');
+// });
