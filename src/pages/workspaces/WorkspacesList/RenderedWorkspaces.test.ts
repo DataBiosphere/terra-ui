c0080a6ddc11be1d968f4c0669551069c58d6493
Christina Ahrens Roberts <roberts@broadinstitute.org>
2023-10-04 11:41:50 -0400

    [WOR-1227] Async Azure workspace deletion (#4291)
    Co-authored-by: Blake Geno <bgeno@broadinstitute.org>
    Co-authored-by: Nick Watts <1156625+nawatts@users.noreply.github.com>

--- src/pages/workspaces/WorkspacesList/RenderedWorkspaces.test.ts
+++ src/pages/workspaces/WorkspacesList/RenderedWorkspaces.test.ts
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { div, h } from 'react-hyperscript-helpers';
+import { WorkspaceWrapper } from 'src/libs/workspace-utils';
 import { RenderedWorkspaces } from 'src/pages/workspaces/WorkspacesList/RenderedWorkspaces';
 import { defaultAzureWorkspace, defaultGoogleWorkspace } from 'src/testing/workspace-fixtures';
 
@@ -22,7 +23,7 @@ jest.mock(
 
 describe('The behavior of the RenderedWorkspaces component', () => {
   it('should render all of the workspaces it is given', () => {
-    // Arange
+    // Arrange
     const workspaces = [defaultAzureWorkspace, defaultGoogleWorkspace];
     const label = 'myWorkspaces';
 
@@ -34,5 +35,77 @@ describe('The behavior of the RenderedWorkspaces component', () => {
     expect(renderedGoogleWS).not.toBeNull();
     const renderedAzureWS = screen.getAllByText(defaultAzureWorkspace.workspace.name);
     expect(renderedAzureWS).not.toBeNull();
+  });
+
+  it('should indicate when the workspace is in the process of deleting instead of displaying the description', () => {
+    // Arrange
+    const workspace: WorkspaceWrapper = {
+      ...defaultAzureWorkspace,
+      workspace: {
+        ...defaultAzureWorkspace.workspace,
+        state: 'Deleting',
+        attributes: { description: 'some description' },
+      },
+    };
+    const label = 'myWorkspaces';
+
+    // Act
+    render(
+      h(RenderedWorkspaces, { workspaces: [workspace], label, noContent: div({}), loadingSubmissionStats: false })
+    );
+
+    // Assert
+    const workspaceDescriptionDisplay = screen.queryAllByText('some description');
+    expect(workspaceDescriptionDisplay).toHaveLength(0);
+
+    const workspaceStateDisplay = screen.getAllByText('Workspace deletion in progress');
+    expect(workspaceStateDisplay).not.toBeNull();
+  });
+
+  it('should indicate when the workspace failed to delete instead of displaying the description', () => {
+    // Arrange
+    const workspace: WorkspaceWrapper = {
+      ...defaultAzureWorkspace,
+      workspace: {
+        ...defaultAzureWorkspace.workspace,
+        state: 'DeleteFailed',
+        attributes: { description: 'some description' },
+      },
+    };
+    const label = 'myWorkspaces';
+
+    // Act
+    render(
+      h(RenderedWorkspaces, { workspaces: [workspace], label, noContent: div({}), loadingSubmissionStats: false })
+    );
+
+    // Assert
+    const workspaceDescriptionDisplay = screen.queryAllByText('some description');
+    expect(workspaceDescriptionDisplay).toHaveLength(0);
+
+    const workspaceStateDisplay = screen.getAllByText('Error deleting workspace');
+    expect(workspaceStateDisplay).not.toBeNull();
+  });
+
+  it('should render the description when the workspace is not in the process of deleting', () => {
+    // Arrange
+    const workspace: WorkspaceWrapper = {
+      ...defaultAzureWorkspace,
+      workspace: {
+        ...defaultAzureWorkspace.workspace,
+        state: 'Ready',
+        attributes: { description: 'some description' },
+      },
+    };
+    const label = 'myWorkspaces';
+
+    // Act
+    render(
+      h(RenderedWorkspaces, { workspaces: [workspace], label, noContent: div({}), loadingSubmissionStats: false })
+    );
+
+    // Assert
+    const workspaceDescriptionDisplay = screen.queryAllByText('some description');
+    expect(workspaceDescriptionDisplay).toHaveLength(1);
   });
 });
