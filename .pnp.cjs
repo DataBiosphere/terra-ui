6d0ba0cba127aa8efe39e53b343c7698f90a111b
Nick Watts <1156625+nawatts@users.noreply.github.com>
2023-10-10 09:09:42 -0400

    [UIE-51] Move PopupTrigger to components (#4320)

--- .pnp.cjs
+++ .pnp.cjs
@@ -138,7 +138,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-modal", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:3.16.1"],\
             ["react-notifications-component", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.0.1"],\
             ["react-oidc-context", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.1.0"],\
-            ["react-onclickoutside", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:6.13.0"],\
             ["react-paginating", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:1.4.0"],\
             ["react-select", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.7.4"],\
             ["react-simplemde-editor", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.0.2"],\
@@ -5087,6 +5086,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/lodash", "npm:4.14.184"],\
             ["@types/react", "npm:18.2.15"],\
             ["@types/react-dom", "npm:18.2.7"],\
+            ["@types/react-onclickoutside", "npm:6.7.5"],\
             ["@types/testing-library__jest-dom", "npm:5.14.9"],\
             ["color", "npm:4.0.1"],\
             ["jest", "virtual:8c00fb6d848584930a97145ab92981f57dfda3a26acb7c186ce59ef8e1d0c5c900af7e36dd05e12ff96b7235e88575a55bb45434a9589e1fb111101a9a3d2f5e#npm:27.5.1"],\
@@ -5095,6 +5095,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"],\
             ["react-focus-lock", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
+            ["react-onclickoutside", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:6.13.0"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=289587"],\
             ["vite", "virtual:6fecf1af4cab542f4a06b7ce7d9f710277dce92700e0011a9519e41948eed6d8f54c9d0aa109ead6cf4295edce81cb49620f9e823313e99632229bf20d133cdb#npm:4.4.7"],\
             ["vite-plugin-svgr", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:3.2.0"]\
@@ -5123,6 +5124,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/jest", "npm:28.1.8"],\
             ["@types/lodash", "npm:4.14.184"],\
             ["@types/react", "npm:18.2.15"],\
+            ["@types/react-onclickoutside", "npm:6.7.5"],\
             ["@types/testing-library__jest-dom", "npm:5.14.9"],\
             ["color", "npm:4.0.1"],\
             ["jest", "virtual:8c00fb6d848584930a97145ab92981f57dfda3a26acb7c186ce59ef8e1d0c5c900af7e36dd05e12ff96b7235e88575a55bb45434a9589e1fb111101a9a3d2f5e#npm:27.5.1"],\
@@ -5131,6 +5133,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"],\
             ["react-focus-lock", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:2.9.5"],\
             ["react-hyperscript-helpers", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.0.0"],\
+            ["react-onclickoutside", "virtual:f14c7c2b623bfe35a608f25e5b8f5582c71bc9475deb0d9650188d23bcf292a9531c8cce96f863c9a827a93aa74f4453675de981d038283eed4a2d8e81e490f7#npm:6.13.0"],\
             ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=289587"],\
             ["vite", "virtual:6fecf1af4cab542f4a06b7ce7d9f710277dce92700e0011a9519e41948eed6d8f54c9d0aa109ead6cf4295edce81cb49620f9e823313e99632229bf20d133cdb#npm:4.4.7"],\
             ["vite-plugin-svgr", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:3.2.0"]\
@@ -5678,6 +5681,16 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@types-react-dom-npm-18.2.7-a20ba684a5-e02ea90828.zip/node_modules/@types/react-dom/",\
           "packageDependencies": [\
             ["@types/react-dom", "npm:18.2.7"],\
+            ["@types/react", "npm:18.2.15"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["@types/react-onclickoutside", [\
+        ["npm:6.7.5", {\
+          "packageLocation": "./.yarn/cache/@types-react-onclickoutside-npm-6.7.5-031ed658cb-2900327b4d.zip/node_modules/@types/react-onclickoutside/",\
+          "packageDependencies": [\
+            ["@types/react-onclickoutside", "npm:6.7.5"],\
             ["@types/react", "npm:18.2.15"]\
           ],\
           "linkType": "HARD"\
@@ -16266,12 +16279,29 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "SOFT"\
         }],\
-        ["virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:6.13.0", {\
-          "packageLocation": "./.yarn/__virtual__/react-onclickoutside-virtual-9b253fc8ed/0/cache/react-onclickoutside-npm-6.13.0-c9116fb62e-a7cfe62e91.zip/node_modules/react-onclickoutside/",\
+        ["virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:6.13.0", {\
+          "packageLocation": "./.yarn/__virtual__/react-onclickoutside-virtual-e422a72ac3/0/cache/react-onclickoutside-npm-6.13.0-c9116fb62e-a7cfe62e91.zip/node_modules/react-onclickoutside/",\
           "packageDependencies": [\
-            ["react-onclickoutside", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:6.13.0"],\
+            ["react-onclickoutside", "virtual:3cbce82a859ed6a4d5bf57a6b809aea2976beb02c6e1078f18e5e40f750e781222e51bcef76699e2500e623832ea4e45e9647ae7b14a57290a182e58c3827fe8#npm:6.13.0"],\
             ["@types/react", "npm:18.2.15"],\
             ["@types/react-dom", "npm:18.2.7"],\
+            ["react", "npm:18.2.0"],\
+            ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/react-dom",\
+            "@types/react",\
+            "react-dom",\
+            "react"\
+          ],\
+          "linkType": "HARD"\
+        }],\
+        ["virtual:f14c7c2b623bfe35a608f25e5b8f5582c71bc9475deb0d9650188d23bcf292a9531c8cce96f863c9a827a93aa74f4453675de981d038283eed4a2d8e81e490f7#npm:6.13.0", {\
+          "packageLocation": "./.yarn/__virtual__/react-onclickoutside-virtual-5a716641b6/0/cache/react-onclickoutside-npm-6.13.0-c9116fb62e-a7cfe62e91.zip/node_modules/react-onclickoutside/",\
+          "packageDependencies": [\
+            ["react-onclickoutside", "virtual:f14c7c2b623bfe35a608f25e5b8f5582c71bc9475deb0d9650188d23bcf292a9531c8cce96f863c9a827a93aa74f4453675de981d038283eed4a2d8e81e490f7#npm:6.13.0"],\
+            ["@types/react", "npm:18.2.15"],\
+            ["@types/react-dom", null],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:18.2.0"]\
           ],\
@@ -17944,7 +17974,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-modal", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:3.16.1"],\
             ["react-notifications-component", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:4.0.1"],\
             ["react-oidc-context", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:2.1.0"],\
-            ["react-onclickoutside", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:6.13.0"],\
             ["react-paginating", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:1.4.0"],\
             ["react-select", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.7.4"],\
             ["react-simplemde-editor", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.0.2"],\
---
+++ packages/components/src/PopupTrigger.test.ts
@@ -0,0 +1,242 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Fragment, RefObject } from 'react';
+import { button, h } from 'react-hyperscript-helpers';
+
+import { renderWithTheme } from './internal/test-utils';
+import { PopupTrigger, PopupTriggerProps, PopupTriggerRef } from './PopupTrigger';
+
+describe('PopupTrigger', () => {
+  const renderPopupTrigger = (
+    props: Partial<PopupTriggerProps> = {},
+    childProps: Partial<JSX.IntrinsicElements['button']> = {}
+  ): { trigger: HTMLElement } => {
+    renderWithTheme(
+      h(
+        PopupTrigger,
+        {
+          content: 'This is a popup',
+          ...props,
+        },
+        [button(childProps, ['Toggle Popup'])]
+      )
+    );
+
+    const trigger = screen.getByRole('button');
+    return { trigger };
+  };
+
+  it('toggles popup when trigger is clicked', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger();
+
+    // Act
+    const isShownBeforeClick = !!screen.queryByRole('dialog');
+
+    await user.click(trigger);
+    const isShownAfterFirstClick = !!screen.queryByRole('dialog');
+
+    await user.click(trigger);
+    const isShownAfterSecondClick = !!screen.queryByRole('dialog');
+
+    // Assert
+    expect(isShownBeforeClick).toBe(false);
+    expect(isShownAfterFirstClick).toBe(true);
+    expect(isShownAfterSecondClick).toBe(false);
+  });
+
+  it('has correct ARIA attributes when closed', async () => {
+    // Act
+    const { trigger } = renderPopupTrigger({ popupProps: { role: 'dialog' } });
+
+    // Arrange
+    expect(trigger).not.toHaveAttribute('aria-controls');
+    expect(trigger).toHaveAttribute('aria-expanded', 'false');
+    expect(trigger).toHaveAttribute('aria-haspopup', 'dialog');
+    expect(trigger).not.toHaveAttribute('aria-owns');
+  });
+
+  it('has correct ARIA attributes when open', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    // Act
+    const { trigger } = renderPopupTrigger({ popupProps: { role: 'dialog' } });
+    await user.click(trigger);
+    const popup = screen.getByRole('dialog');
+
+    // Arrange
+    const popupId = popup.id;
+    expect(trigger).toHaveAttribute('aria-controls', popupId);
+    expect(trigger).toHaveAttribute('aria-expanded', 'true');
+    expect(trigger).toHaveAttribute('aria-haspopup', 'dialog');
+    expect(trigger).toHaveAttribute('aria-owns', popupId);
+
+    expect(popup).toHaveAttribute('role', 'dialog');
+    expect(popup).toHaveAttribute('aria-modal', 'true');
+  });
+
+  it('labels the popup with the trigger element', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger();
+
+    // Act
+    await user.click(trigger);
+    const popup = screen.getByRole('dialog');
+
+    // Assert
+    expect(popup).toHaveAttribute('aria-labelledby', trigger.id);
+  });
+
+  it('calls onChange when popup is toggled', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const onChange = jest.fn();
+    const { trigger } = renderPopupTrigger({ onChange });
+
+    // Act
+    await user.click(trigger);
+
+    // Assert
+    expect(onChange).toHaveBeenCalledWith(true);
+
+    // Act
+    await user.click(trigger);
+
+    // Assert
+    expect(onChange).toHaveBeenCalledWith(false);
+  });
+
+  it('can close the popup when the popup is clicked', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger({ closeOnClick: true });
+
+    // Act
+    await user.click(trigger);
+    const popup = screen.getByRole('dialog');
+    await user.click(popup);
+
+    // Assert
+    expect(screen.queryByRole('dialog')).toBeNull();
+  });
+
+  it('closes the popup when something outside the popup/trigger is clicked', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger();
+
+    // Act
+    await user.click(trigger);
+    await user.click(document.body);
+
+    // Assert
+    expect(screen.queryByRole('dialog')).toBeNull();
+  });
+
+  it('traps focus in the popup', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    renderWithTheme(
+      h(Fragment, [
+        h(
+          PopupTrigger,
+          {
+            content: h(Fragment, [button(['Button A']), button(['Button B'])]),
+          },
+          [button(['Toggle Popup'])]
+        ),
+        button(['Button C']),
+      ])
+    );
+
+    const trigger = screen.getByRole('button', { name: 'Toggle Popup' });
+
+    // Act
+    await user.click(trigger);
+
+    const popup = screen.getByRole('dialog');
+
+    // When the popup is opened, focus should be moved to focus trap inside the popup.
+    const focusReceiver = document.activeElement;
+    expect(popup.contains(focusReceiver)).toBe(true);
+
+    const [buttonA, buttonB] = ['Button A', 'Button B'].map((btnText) => screen.getByText(btnText));
+
+    // Tab 4 times.
+    // Focus should go through the buttons in the popup, then to the focus trap element, and then back to button A,
+    // never reaching button C outside of the popup.
+    const tabOrder: Element[] = [];
+    for (let i = 0; i < 4; i += 1) {
+      await user.tab();
+      tabOrder.push(document.activeElement!);
+    }
+
+    // Assert
+    expect(tabOrder).toEqual([buttonA, buttonB, focusReceiver, buttonA]);
+  });
+
+  it('closes the popup when the escape key is pressed', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger();
+
+    // Act
+    await user.click(trigger);
+    await user.keyboard('{Escape}');
+
+    // Assert
+    expect(screen.queryByRole('dialog')).toBeNull();
+  });
+
+  it('provides an imperative handle to close the popup', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const ref: RefObject<PopupTriggerRef> = { current: null };
+    renderWithTheme(
+      h(
+        PopupTrigger,
+        {
+          ref,
+          content: 'This is a popup',
+        },
+        [button(['Toggle Popup'])]
+      )
+    );
+
+    const trigger = screen.getByRole('button', { name: 'Toggle Popup' });
+
+    // Act
+    await user.click(trigger);
+    act(() => {
+      ref.current!.close();
+    });
+
+    // Assert
+    expect(screen.queryByRole('dialog')).toBeNull();
+  });
+
+  it('should move focus back to trigger when popup is closed', async () => {
+    // Arrange
+    const user = userEvent.setup();
+
+    const { trigger } = renderPopupTrigger();
+
+    // Act
+    await user.click(trigger);
+    await user.keyboard('{Escape}');
+
+    // Assert
+    expect(trigger).toHaveFocus();
+  });
+});
