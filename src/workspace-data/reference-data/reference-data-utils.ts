bb8a054f94f9002a4745660fd63985d000122367
Nick Watts <nwatts@broadinstitute.org>
2023-10-10 09:33:29 -0400

    Convert reference-data-utils to TS

--- src/workspace-data/reference-data/reference-data-utils.ts
+++ src/workspace-data/reference-data/reference-data-utils.ts
@@ -6,12 +6,14 @@ export const getReferenceData = _.flow(
   _.toPairs,
   _.filter(([key]) => key.startsWith('referenceData_')),
   _.map(([k, value]) => {
-    const [, datum, key] = /referenceData_([^_]+)_(.+)/.exec(k);
+    // Assume that all keys starting with "referenceData_" were created by
+    // importing reference data and thus match this format.
+    const [, datum, key] = /referenceData_([^_]+)_(.+)/.exec(k)!;
     return { datum, key, value };
   }),
   _.groupBy('datum')
 );
 
-export const getReferenceLabel = (referenceName) => {
+export const getReferenceLabel = (referenceName: string): string => {
   return `${referenceMetadata[referenceName].species}: ${referenceName}`;
 };
