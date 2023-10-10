e40b1997a146d3fdcf31830e9b867df0e80dcebd
Nick Watts <nwatts@broadinstitute.org>
2023-10-10 08:51:17 -0400

    Block cross-platform TDR imports

--- src/import-data/ImportData.test.ts
+++ src/import-data/ImportData.test.ts
@@ -59,7 +59,22 @@ jest.mock('src/pages/library/dataBrowser-utils', (): DataBrowserUtilsExports => 
   };
 });
 
-const snapshotFixture: Snapshot = {
+const azureSnapshotFixture: Snapshot = {
+  id: 'aaaabbbb-cccc-dddd-0000-111122223333',
+  name: 'test-snapshot',
+  source: [
+    {
+      dataset: {
+        id: 'aaaabbbb-cccc-dddd-0000-111122223333',
+        name: 'test-dataset',
+        secureMonitoringEnabled: false,
+      },
+    },
+  ],
+  cloudPlatform: 'azure',
+};
+
+const googleSnapshotFixture: Snapshot = {
   id: '00001111-2222-3333-aaaa-bbbbccccdddd',
   name: 'test-snapshot',
   source: [
@@ -84,8 +99,11 @@ const setup = async (opts: SetupOptions) => {
   const mockDataRepo = {
     snapshot: (snapshotId: string): Partial<ReturnType<DataRepoContract['snapshot']>> => ({
       details: jest.fn().mockImplementation(() => {
-        if (snapshotId === snapshotFixture.id) {
-          return snapshotFixture;
+        if (snapshotId === azureSnapshotFixture.id) {
+          return azureSnapshotFixture;
+        }
+        if (snapshotId === googleSnapshotFixture.id) {
+          return googleSnapshotFixture;
         }
         throw new Response('{"message":"Snapshot not found"}', { status: 404 });
       }),
@@ -261,18 +279,17 @@ describe('ImportData', () => {
 
   describe('TDR', () => {
     describe('snapshot exports', () => {
-      const queryParams = {
-        format: 'tdrexport',
-        snapshotId: '00001111-2222-3333-aaaa-bbbbccccdddd',
-        tdrmanifest: 'https://example.com/path/to/manifest.json',
-        tdrSyncPermissions: 'true',
-        url: 'https://data.terra.bio',
-      };
-
       it('imports snapshot exports into Google workspaces', async () => {
         // Arrange
         const user = userEvent.setup();
 
+        const queryParams = {
+          format: 'tdrexport',
+          snapshotId: googleSnapshotFixture.id,
+          tdrmanifest: 'https://example.com/path/to/manifest.json',
+          tdrSyncPermissions: 'true',
+          url: 'https://data.terra.bio',
+        };
         const { getWorkspaceApi, importJob } = await setup({ queryParams });
 
         // Act
@@ -291,6 +308,13 @@ describe('ImportData', () => {
         // Arrange
         const user = userEvent.setup();
 
+        const queryParams = {
+          format: 'tdrexport',
+          snapshotId: azureSnapshotFixture.id,
+          tdrmanifest: 'https://example.com/path/to/manifest.json',
+          tdrSyncPermissions: 'true',
+          url: 'https://data.terra.bio',
+        };
         const { importTdr, wdsProxyUrl } = await setup({ queryParams });
 
         // Act
@@ -312,7 +336,7 @@ describe('ImportData', () => {
 
         const queryParams = {
           format: 'snapshot',
-          snapshotId: '00001111-2222-3333-aaaa-bbbbccccdddd',
+          snapshotId: googleSnapshotFixture.id,
         };
         const { getWorkspaceApi, importSnapshot } = await setup({ queryParams });
 
@@ -325,7 +349,7 @@ describe('ImportData', () => {
           defaultGoogleWorkspace.workspace.name
         );
 
-        expect(importSnapshot).toHaveBeenCalledWith(queryParams.snapshotId, snapshotFixture.name);
+        expect(importSnapshot).toHaveBeenCalledWith(queryParams.snapshotId, googleSnapshotFixture.name);
       });
     });
   });
--- src/import-data/ImportDataDestination.ts
+++ src/import-data/ImportDataDestination.ts
@@ -22,7 +22,7 @@ import * as Utils from 'src/libs/utils';
 import { WorkspaceInfo } from 'src/libs/workspace-utils';
 
 import { ImportRequest, TemplateWorkspaceInfo } from './import-types';
-import { canImportIntoWorkspace } from './import-utils';
+import { canImportIntoWorkspace, getCloudPlatformRequiredForImport } from './import-utils';
 import { isProtectedSource } from './protected-data-utils';
 
 const styles = {
@@ -105,6 +105,7 @@ export const ImportDataDestination = (props: ImportDataDestinationProps): ReactN
   } = props;
 
   const isProtectedData = isProtectedSource(importRequest);
+  const requiredCloudPlatform = getCloudPlatformRequiredForImport(importRequest);
 
   // Some import types are finished in a single request.
   // For most though, the import request starts a background task that takes time to complete.
@@ -163,6 +164,7 @@ export const ImportDataDestination = (props: ImportDataDestinationProps): ReactN
               workspaces: workspaces.filter((workspace) => {
                 return canImportIntoWorkspace(
                   {
+                    cloudPlatform: requiredCloudPlatform,
                     isProtectedData,
                     requiredAuthorizationDomain,
                   },
