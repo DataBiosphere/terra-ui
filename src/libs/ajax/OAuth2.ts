e086299b6f99789503fbce0402f18710d9060f79
Sebastian Kobori <sjkobori@gmail.com>
2023-10-16 01:11:03 -0400

    Added authOpts back in to getConfiguration to see if that is the reason for integration tests failing

--- src/libs/ajax/OAuth2.ts
+++ src/libs/ajax/OAuth2.ts
@@ -1,4 +1,5 @@
-import { fetchOrchestration } from 'src/libs/ajax/ajax-common';
+import _ from 'lodash/fp';
+import { authOpts, fetchOrchestration } from 'src/libs/ajax/ajax-common';
 
 export type OidcConfig = {
   authorityEndpoint?: string;
@@ -8,7 +9,7 @@ export const OAuth2 = (signal?: AbortSignal) => ({
   getConfiguration: async (): Promise<OidcConfig> => {
     // authOpts does not have a token yet since the user has not logged in
     // so this method should not be using authOpts
-    const res = await fetchOrchestration('/oauth2/configuration', { signal });
+    const res = await fetchOrchestration('/oauth2/configuration', _.merge(authOpts(), { signal }));
     return res.json();
   },
 });
