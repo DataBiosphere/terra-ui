95205ae3d7b960f91c4b82c5e865633fdce14272
Nick Watts <nwatts@broadinstitute.org>
2023-10-18 17:23:13 -0400

    Remove Clickable module

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,6 @@
-export { ButtonOutline, ButtonPrimary, ButtonSecondary, Link } from '@terra-ui-packages/components';
-export type { LinkProps } from '@terra-ui-packages/components';
+export { ButtonOutline, ButtonPrimary, ButtonSecondary, Clickable, Link } from '@terra-ui-packages/components';
+export type { ClickableProps, LinkProps } from '@terra-ui-packages/components';
 export * from './Checkbox';
-export * from './Clickable';
 export * from './DeleteConfirmationModal';
 export * from './IdContainer';
 export * from './RadioButton';
