3c7a6a85120f6b2198ec1e12d63220436b2b1f72
tlangs <tlangs@broadinstitute.org>
2023-11-09 11:36:57 -0500

    new typescript

--- packages/components/src/buttons.ts
+++ packages/components/src/buttons.ts
@@ -1,4 +1,4 @@
-import { CSSProperties, ReactElement, ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import { h } from 'react-hyperscript-helpers';
 
 import { Clickable, ClickableProps } from './Clickable';
@@ -20,7 +20,7 @@ export interface ButtonPrimaryProps extends ClickableProps {
   danger?: boolean;
 }
 
-export const ButtonPrimary = (props: ButtonPrimaryProps): ReactElement => {
+export const ButtonPrimary = (props: ButtonPrimaryProps): ReactNode => {
   const { children, danger = false, disabled, hover, style, ...otherProps } = props;
 
   const { colors } = useThemeFromContext();
@@ -53,7 +53,7 @@ export const ButtonPrimary = (props: ButtonPrimaryProps): ReactElement => {
 
 export type ButtonSecondaryProps = ClickableProps;
 
-export const ButtonSecondary = (props: ButtonSecondaryProps): ReactElement => {
+export const ButtonSecondary = (props: ButtonSecondaryProps): ReactNode => {
   const { children, disabled, hover, style, ...otherProps } = props;
 
   const { colors } = useThemeFromContext();
