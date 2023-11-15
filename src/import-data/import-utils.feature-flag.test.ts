4be56bf23d1dc13983f838bc555c8d9db8f391a2
David An <davidan@broadinstitute.org>
2023-11-15 09:23:35 -0500

    ts type for mock

--- src/import-data/import-utils.feature-flag.test.ts
+++ src/import-data/import-utils.feature-flag.test.ts
@@ -1,10 +1,15 @@
 import { ImportRequest } from './import-types';
 import { getCloudPlatformRequiredForImport } from './import-utils';
 
-jest.mock('src/libs/feature-previews', () => ({
-  ...jest.requireActual('src/libs/feature-previews'),
-  isFeaturePreviewEnabled: jest.fn().mockReturnValue(true),
-}));
+type FeaturePreviewExports = typeof import('src/libs/feature-previews');
+
+jest.mock(
+  'src/libs/feature-previews',
+  (): FeaturePreviewExports => ({
+    ...jest.requireActual('src/libs/feature-previews'),
+    isFeaturePreviewEnabled: jest.fn().mockReturnValue(true),
+  })
+);
 
 // Note that behavior of getRequiredCloudPlatformForImport when the feature flag is set to false is tested
 // in import-utils.test.ts. This file only tests behavior when the feature flag is set to true.
