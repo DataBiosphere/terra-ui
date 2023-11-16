8e1d6a3ccdba9fdba85475eeb830090ce3308e42
Justin H Yang <juyang@broadinstitute.org>
2023-11-16 09:29:57 -0800

    update logging

--- integration-tests/utils/terra-sa-utils.js
+++ integration-tests/utils/terra-sa-utils.js
@@ -2,7 +2,7 @@ const _ = require('lodash/fp');
 const { JWT } = require('google-auth-library');
 const { getSecrets, userEmail } = require('./integration-config');
 
-// generates a JWT using the private key
+// generates a JWT using json key, and a subject to be impersonated
 const makeAuthClient = _.memoize((subject, { client_email: email, private_key: key }) => {
   return new JWT({
     email,
@@ -12,6 +12,7 @@ const makeAuthClient = _.memoize((subject, { client_email: email, private_key: k
   });
 });
 
+// Gets a token for an (impersonated) user account using a service account key
 const getToken = async (subject, key) => {
   const { token } = await makeAuthClient(subject, key).getAccessToken();
   return token;
@@ -20,9 +21,11 @@ const getToken = async (subject, key) => {
 const withUserToken = (testFn) => async (options) => {
   const { terraSaToken, terraSaKeyJson } = await getSecrets();
   if (terraSaToken != null) {
+    console.log("using terraSaToken...");
     return testFn({ ...options, terraSaToken });
   }
   const token = await getToken(userEmail, JSON.parse(terraSaKeyJson));
+  console.log("using getToken token...");
   return testFn({ ...options, token });
 };
 
