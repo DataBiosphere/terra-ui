9799bd27473cef8c8b3d41a5e63a7d86469cccd0
tlangs <tlangs@broadinstitute.org>
2023-11-17 13:17:14 -0500

    new sam endpoints

--- src/libs/ajax/TermsOfService.ts
+++ src/libs/ajax/TermsOfService.ts
@@ -3,11 +3,11 @@ import { fetchSam } from 'src/libs/ajax/ajax-common';
 export const TermsOfService = (signal?: AbortSignal) => {
   return {
     getTermsOfServiceText: async (): Promise<string> => {
-      const res = await fetchSam('tos/text', { signal });
+      const res = await fetchSam('termsOfService/v1/docs?doc=termsOfService', { signal });
       return res.text();
     },
     getPrivacyText: async (): Promise<string> => {
-      const res = await fetchSam('privacy/text', { signal });
+      const res = await fetchSam('termsOfService/v1/docs?doc=privacyPolicy', { signal });
       return res.text();
     },
   };
