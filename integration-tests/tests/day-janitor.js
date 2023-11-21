dd3b65c5bd0720d90bdb387838f180a37588cf98
Mneme Nolting <mneme@broadinstitute.org>
2023-11-21 15:53:52 -0500

    [IA-4706] Improve "day" janitor by adding configurable age target, deleting workspaces with V2 approach, prechecking child resources for deletability

--- integration-tests/tests/day-janitor.js
+++ integration-tests/tests/day-janitor.js
@@ -1,14 +1,26 @@
 // This test is owned by the Workspaces Team.
 const dateFns = require('date-fns/fp');
 const _ = require('lodash/fp');
-const { testWorkspaceNamePrefix } = require('../utils/integration-helpers');
+const { deleteWorkspaceV2AsUser, testWorkspaceNamePrefix } = require('../utils/integration-helpers');
 const { signIntoTerra } = require('../utils/integration-utils');
 const { registerTest } = require('../utils/jest-utils');
 const { withUserToken } = require('../utils/terra-sa-utils');
 
-const olderThanDays = 2;
+const olderThanTimeUnits = 3;
+const timeUnit =
+  // 'days';
+  'hours';
+// 'minutes';
+const getTimeDifference =
+  // .differenceInDays;
+  dateFns.differenceInHours;
+// .differenceInMinutes;
 
-const runJanitor = withUserToken(async ({ page, testUrl, token }) => {
+/**
+ * Attempts to delete any workspaces which are named with the auto-generated test workspace name prefix,
+ * and are more than a certain configured age.
+ */
+const runDayJanitor = withUserToken(async ({ page, testUrl, token }) => {
   // Sign into Terra so we have the correct credentials.
   await signIntoTerra(page, { token, testUrl });
 
@@ -17,30 +29,30 @@ const runJanitor = withUserToken(async ({ page, testUrl, token }) => {
   // ci test runs also sometimes leak workspaces.
   const workspaces = await page.evaluate(async () => await window.Ajax().Workspaces.list());
   const oldWorkspaces = _.filter(({ workspace: { name, createdDate } }) => {
-    const age = dateFns.differenceInDays(new Date(createdDate), new Date());
+    const age = getTimeDifference(new Date(createdDate), new Date());
     // console.info(`${namespace} ${name}, age ${age} days`)
-    return _.startsWith(testWorkspaceNamePrefix, name) && age > olderThanDays;
+    return _.startsWith(testWorkspaceNamePrefix, name) && age > olderThanTimeUnits;
   }, workspaces);
 
   console.log(
-    `Attempting to delete ${oldWorkspaces.length} workspaces with prefix "${testWorkspaceNamePrefix}" created more than ${olderThanDays} days ago.`
+    `Attempting to delete ${oldWorkspaces.length} workspaces with prefix "${testWorkspaceNamePrefix}" created more than ${olderThanTimeUnits} ${timeUnit} ago.`
   );
   console.log(`${_.filter({ workspace: { cloudPlatform: 'Azure' } }, oldWorkspaces).length} of the old workspaces are Azure workspaces.`);
   return Promise.all(
     _.map(async ({ workspace: { namespace, name, cloudPlatform } }) => {
       try {
         // Unlock the workspace in case it was left in a locked state during a test (locked workspaces can't be deleted).
-        await page.evaluate((namespace, name) => window.Ajax().Workspaces.workspace(namespace, name).unlock(), namespace, name);
-        await page.evaluate((namespace, name) => window.Ajax().Workspaces.workspace(namespace, name).delete(), namespace, name);
-        console.info(`Deleted old workspace: ${name}`);
+        await page.evaluate(async (namespace, name) => await window.Ajax().Workspaces.workspace(namespace, name).unlock(), namespace, name);
+        await deleteWorkspaceV2AsUser({ page, billingProject: namespace, workspaceName: name });
+        console.info(`Triggered delete for old workspace: ${name}`);
       } catch (e) {
-        console.info(`Failed to delete old ${cloudPlatform} workspace: ${name} with billing project ${namespace}`);
+        console.error(`Failed to delete old ${cloudPlatform} workspace: ${name} with billing project ${namespace}`);
       }
     }, oldWorkspaces)
   );
 });
 
 registerTest({
-  name: 'janitor',
-  fn: runJanitor,
+  name: 'day-janitor',
+  fn: runDayJanitor,
 });
