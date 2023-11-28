9103ed2da6e8a00a19220dc51f6601be8fd53d42
Sebastian Kobori <sjkobori@gmail.com>
2023-11-28 11:15:53 -0500

    Fixed terms-of-service-alerts tests

--- src/registration/terms-of-service/terms-of-service-alerts.test.ts
+++ src/registration/terms-of-service/terms-of-service-alerts.test.ts
@@ -1,14 +1,9 @@
 import { act } from '@testing-library/react';
 import { h } from 'react-hyperscript-helpers';
 import { AlertsIndicator } from 'src/alerts/Alerts';
-import { Ajax } from 'src/libs/ajax';
-import { Groups } from 'src/libs/ajax/Groups';
-import { Metrics } from 'src/libs/ajax/Metrics';
-import { TermsOfService } from 'src/libs/ajax/TermsOfService';
-import { User } from 'src/libs/ajax/User';
-import { authStore } from 'src/libs/state';
-import * as TosAlerts from 'src/libs/terms-of-service-alerts';
-import { asMockedFn, renderWithAppContexts as render } from 'src/testing/test-utils';
+import { authStore, TermsOfServiceStatus } from 'src/libs/state';
+import * as TosAlerts from 'src/registration/terms-of-service/terms-of-service-alerts';
+import { renderWithAppContexts as render } from 'src/testing/test-utils';
 
 jest.mock('src/libs/ajax');
 
@@ -26,44 +21,15 @@ jest.mock('react-notifications-component', () => {
   };
 });
 
-const setupMockAjax = (termsOfService) => {
-  const getTermsOfServiceText = jest.fn().mockReturnValue(Promise.resolve('some text'));
-  const getTermsOfServiceComplianceStatus = jest.fn().mockReturnValue(Promise.resolve(termsOfService));
-  const getStatus = jest.fn().mockReturnValue(Promise.resolve({}));
-  type AjaxContract = ReturnType<typeof Ajax>;
-  type UserContract = ReturnType<typeof User>;
-  type MetricsContract = ReturnType<typeof Metrics>;
-  type GroupsContract = ReturnType<typeof Groups>;
-  type TermsOfServiceContract = ReturnType<typeof TermsOfService>;
-
-  asMockedFn(Ajax).mockImplementation( () =>
-      ({
-        Metrics: {
-          captureEvent: jest.fn(),
-        } as Partial<MetricsContract>,
-        User: {
-          profile: {
-            get: jest.fn().mockReturnValue(Promise.resolve({keyValuePairs: []})),
-          } as Partial<UserContract>['profile'],
-          getStatus,
-        } as Partial<UserContract>,
-        TermsOfService: {
-          getTermsOfServiceText,
-        } as Partial<TermsOfServiceContract>,
-      } as Partial<AjaxContract> as AjaxContract)
-  }));
-};
-
 afterEach(() => {
   jest.restoreAllMocks();
   TosAlerts.tosAlertsStore.reset();
 });
 
-const renderAlerts = async (termsOfService) => {
+const renderAlerts = async (termsOfService: TermsOfServiceStatus) => {
   await act(async () => {
     render(h(AlertsIndicator));
   });
-  setupMockAjax(termsOfService);
 
   const signInStatus = 'authenticated';
   await act(async () => {
@@ -87,7 +53,7 @@ describe('terms-of-service-alerts', () => {
     // Assert
     expect(TosAlerts.useTermsOfServiceAlerts).toHaveBeenCalled();
     expect(TosAlerts.tosAlertsStore.get().length).toEqual(1);
-    expect(TosAlerts.tosAlertsStore.get()[0].id).toEqual('terms-of-service-needs-accepting-grace-period');
+    expect(TosAlerts.tosAlertsStore.get()[0].id).toEqual('new-terms-of-service-alert');
   });
 
   it('does not add a notification when the user does not have a new Terms of Service to accept', async () => {
