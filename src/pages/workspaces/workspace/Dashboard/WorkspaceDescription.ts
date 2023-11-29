32c973df94d9658e0b48e05f6353f438a2d4ebdf
Blake Geno <bgeno@broadinstitute.org>
2023-11-29 09:34:29 -0500

    switch usage to desciption component

--- src/pages/workspaces/workspace/Dashboard/WorkspaceDescription.ts
+++ src/pages/workspaces/workspace/Dashboard/WorkspaceDescription.ts
@@ -11,7 +11,7 @@ import { InitializedWorkspaceWrapper as Workspace } from 'src/pages/workspaces/h
 
 interface WorkspaceDescriptionProps {
   workspace: Workspace;
-  save: (description?: string) => void;
+  save: (description?: string) => Promise<void>;
   saving: boolean;
 }
 
@@ -26,43 +26,49 @@ export const WorkspaceDescription = (props: WorkspaceDescriptionProps): ReactNod
   // @ts-expect-error
   const { value: canEdit, message: editErrorMessage } = canEditWorkspace(workspace);
 
-  return div({ style: { flex: 1, display: 'flex' } }, [
-    div({ style: Style.dashboard.leftBox }, [
-      div({ style: Style.dashboard.header }, [
-        'About the workspace',
-        !isEditing &&
-          h(
-            Link,
-            {
-              style: { marginLeft: '0.5rem' },
-              disabled: !canEdit,
-              tooltip: canEdit ? 'Edit description' : editErrorMessage,
-              onClick: () => setEditDescription(description?.toString()),
-            },
-            [icon('edit')]
-          ),
-      ]),
-      cond(
-        [
-          isEditing,
-          () =>
-            h(Fragment, [
-              // @ts-expect-error
-              h(MarkdownEditor, {
-                placeholder: 'Enter a description',
-                value: editDescription,
-                onChange: setEditDescription,
-              }),
-              div({ style: { display: 'flex', justifyContent: 'flex-end', margin: '1rem' } }, [
-                h(ButtonSecondary, { onClick: () => setEditDescription(undefined) }, ['Cancel']),
-                h(ButtonPrimary, { style: { marginLeft: '1rem' }, onClick: () => save(editDescription) }, ['Save']),
-              ]),
-              saving && spinnerOverlay,
-            ]),
-        ],
-        [!!description, () => h(MarkdownViewer, [description?.toString()])],
-        () => div({ style: { fontStyle: 'italic' } }, ['No description added'])
-      ),
+  const onSave = async () => {
+    try {
+      await save(editDescription);
+    } finally {
+      setEditDescription(undefined);
+    }
+  };
+
+  return h(Fragment, [
+    div({ style: Style.dashboard.header }, [
+      'About the workspace',
+      !isEditing &&
+        h(
+          Link,
+          {
+            style: { marginLeft: '0.5rem' },
+            disabled: !canEdit,
+            tooltip: canEdit ? 'Edit description' : editErrorMessage,
+            onClick: () => setEditDescription(description?.toString()),
+          },
+          [icon('edit')]
+        ),
     ]),
+    cond(
+      [
+        isEditing,
+        () =>
+          h(Fragment, [
+            // @ts-expect-error
+            h(MarkdownEditor, {
+              placeholder: 'Enter a description',
+              value: editDescription,
+              onChange: setEditDescription,
+            }),
+            div({ style: { display: 'flex', justifyContent: 'flex-end', margin: '1rem' } }, [
+              h(ButtonSecondary, { onClick: () => setEditDescription(undefined) }, ['Cancel']),
+              h(ButtonPrimary, { style: { marginLeft: '1rem' }, onClick: onSave }, ['Save']),
+            ]),
+            saving && spinnerOverlay,
+          ]),
+      ],
+      [!!description, () => h(MarkdownViewer, [description?.toString()])],
+      () => div({ style: { fontStyle: 'italic' } }, ['No description added'])
+    ),
   ]);
 };
