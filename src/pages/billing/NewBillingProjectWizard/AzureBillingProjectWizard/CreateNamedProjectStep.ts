f07865e8d39767c6407022839b17984cc4810b87
Nick Watts <nwatts@broadinstitute.org>
2023-11-29 13:36:05 -0500

    Update billing project/workspace cost notices

--- src/pages/billing/NewBillingProjectWizard/AzureBillingProjectWizard/CreateNamedProjectStep.ts
+++ src/pages/billing/NewBillingProjectWizard/AzureBillingProjectWizard/CreateNamedProjectStep.ts
@@ -38,7 +38,7 @@ const AzureCostWarnings = () => {
     div({ style: rowEntry }, [
       icon('warning-standard', { size: 16, style: { marginRight: '0.5rem', color: colors.warning() } }),
       div([
-        'Creating a Terra on Azure Billing Project has a starting cost of $10.58/day and increases with usage. ',
+        'A Terra on Azure Billing Project has a starting cost of $10-15/day depending on region. ',
         h(ExternalLink, {
           url: 'https://support.terra.bio/hc/en-us/articles/12029087819291',
           text: 'Learn more and follow changes',
