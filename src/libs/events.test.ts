068262880340daaa394a4e64911a736bf74cc386
Chris Llanwarne <cjllanwarne@gmail.com>
2023-12-04 14:56:37 -0500

    Fixup event test expectations

--- src/libs/events.test.ts
+++ src/libs/events.test.ts
@@ -38,6 +38,7 @@ describe('extractWorkspaceDetails', () => {
       workspaceNamespace: defaultGoogleWorkspace.workspace.namespace,
       cloudPlatform: 'GCP',
       hasProtectedData: false,
+      workspaceAccessLevel: 'OWNER',
     });
   });
 
