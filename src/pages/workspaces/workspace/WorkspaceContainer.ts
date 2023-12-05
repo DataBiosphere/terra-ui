1c7b4b21d5e4e4fd600993af6e40fdbb4330b61d
Christina Ahrens Roberts <roberts@broadinstitute.org>
2023-12-05 15:11:06 -0500

    [WOR-1381] Delete the Azure warnings (#4528)

--- src/pages/workspaces/workspace/WorkspaceContainer.ts
+++ src/pages/workspaces/workspace/WorkspaceContainer.ts
@@ -6,7 +6,6 @@ import AnalysisNotificationManager from 'src/analysis/AnalysisNotificationManage
 import { ContextBar } from 'src/analysis/ContextBar';
 import { ButtonPrimary, Link, spinnerOverlay } from 'src/components/common';
 import FooterWrapper from 'src/components/FooterWrapper';
-import { icon } from 'src/components/icons';
 import LeaveResourceModal from 'src/components/LeaveResourceModal';
 import TitleBar from 'src/components/TitleBar';
 import TopBar from 'src/components/TopBar';
@@ -16,7 +15,7 @@ import { withDisplayName } from 'src/libs/react-utils';
 import { getTerraUser, workspaceStore } from 'src/libs/state';
 import * as Style from 'src/libs/style';
 import * as Utils from 'src/libs/utils';
-import { azureControlledAccessRequestMessage, isAzureWorkspace, isGoogleWorkspace } from 'src/libs/workspace-utils';
+import { azureControlledAccessRequestMessage, isGoogleWorkspace } from 'src/libs/workspace-utils';
 import { AppDetails, useAppPolling } from 'src/pages/workspaces/hooks/useAppPolling';
 import {
   CloudEnvironmentDetails,
@@ -34,24 +33,6 @@ import DeleteWorkspaceModal from 'src/workspaces/DeleteWorkspaceModal/DeleteWork
 import LockWorkspaceModal from 'src/workspaces/LockWorkspaceModal/LockWorkspaceModal';
 import NewWorkspaceModal from 'src/workspaces/NewWorkspaceModal/NewWorkspaceModal';
 import ShareWorkspaceModal from 'src/workspaces/ShareWorkspaceModal/ShareWorkspaceModal';
-
-const TitleBarWarning = (props: PropsWithChildren): ReactNode => {
-  return h(TitleBar, {
-    title: div(
-      {
-        role: 'alert',
-        style: { display: 'flex', alignItems: 'center', margin: '1rem' },
-      },
-      [
-        icon('warning-standard', { size: 32, style: { color: colors.danger(), marginRight: '0.5rem' } }),
-        span({ style: { color: colors.dark(), fontSize: 14 } }, [props.children]),
-      ]
-    ),
-    style: { backgroundColor: colors.accent(0.35), borderBottom: `1px solid ${colors.accent()}` },
-    onDismiss: () => {},
-    hideCloseButton: true,
-  });
-};
 
 const TitleBarSpinner = (props: PropsWithChildren): ReactNode => {
   return h(TitleBar, {
@@ -72,13 +53,6 @@ const TitleBarSpinner = (props: PropsWithChildren): ReactNode => {
     style: { backgroundColor: colors.warning(0.1), borderBottom: `1px solid ${colors.warning()}` },
     onDismiss: () => {},
   });
-};
-
-const AzureWarning = (): ReactNode => {
-  const warningMessage = [
-    'Do not store Unclassified Confidential Information in this platform, as it violates US Federal Policy (ie FISMA, FIPS-199, etc) unless explicitly authorized by the dataset manager or governed by your own agreements.',
-  ];
-  return h(TitleBarWarning, warningMessage);
 };
 
 const GooglePermissionsSpinner = (): ReactNode => {
@@ -153,7 +127,6 @@ export const WorkspaceContainer = (props: WorkspaceContainerProps) => {
       setShowLockWorkspaceModal,
     }),
     h(WorkspaceDeletingBanner, { workspace }),
-    workspaceLoaded && isAzureWorkspace(workspace) && h(AzureWarning),
     isGoogleWorkspaceSyncing && h(GooglePermissionsSpinner),
     div({ role: 'main', style: Style.elements.pageContentContainer }, [
       div({ style: { flex: 1, display: 'flex' } }, [
