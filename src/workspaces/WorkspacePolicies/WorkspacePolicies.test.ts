f972e9ab784a63e6904cd9ab24fb4b12a6229876
cahrens <roberts@broadinstitute.org>
2023-12-06 15:52:24 -0500

    Add test coverage.

--- src/workspaces/WorkspacePolicies/WorkspacePolicies.test.ts
+++ src/workspaces/WorkspacePolicies/WorkspacePolicies.test.ts
@@ -3,6 +3,11 @@ import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
 import { h } from 'react-hyperscript-helpers';
 import { AzureWorkspace } from 'src/libs/workspace-utils';
+import {
+  azureBillingProject,
+  azureProtectedDataBillingProject,
+  gcpBillingProject,
+} from 'src/testing/billing-project-fixtures';
 import { renderWithAppContexts as render } from 'src/testing/test-utils';
 import {
   defaultAzureWorkspace,
@@ -14,80 +19,123 @@ import {
 import { WorkspacePolicies } from 'src/workspaces/WorkspacePolicies/WorkspacePolicies';
 
 describe('WorkspacePolicies', () => {
-  it('renders nothing if the policy array is empty', () => {
-    // Act
-    render(
-      h(WorkspacePolicies, {
-        policySource: defaultAzureWorkspace,
-      })
-    );
+  const policyLabel = /This workspace has the following/;
 
-    // Assert
-    expect(screen.queryByText(/This workspace has the following/)).toBeNull();
+  describe('handles a workspace as the source of the policies', () => {
+    it('renders nothing if the policy array is empty', () => {
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: defaultAzureWorkspace,
+        })
+      );
+
+      // Assert
+      expect(screen.queryByText(policyLabel)).toBeNull();
+    });
+
+    it('renders nothing if the policy array does not contain known policies', () => {
+      // Arrange
+      const nonProtectedAzureWorkspace: AzureWorkspace = {
+        ...defaultAzureWorkspace,
+        policies: [
+          {
+            additionalData: [],
+            namespace: 'terra',
+            name: 'some-other-policy',
+          },
+        ],
+      };
+
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: nonProtectedAzureWorkspace,
+        })
+      );
+
+      // Assert
+      expect(screen.queryByText(policyLabel)).toBeNull();
+    });
+
+    it('renders policies with no accessibility errors', async () => {
+      // Arrange
+      const workspaceWithAllPolicies: AzureWorkspace = {
+        ...defaultAzureWorkspace,
+        policies: [protectedDataPolicy, groupConstraintPolicy, regionConstraintPolicy],
+      };
+
+      // Act
+      const { container } = render(
+        h(WorkspacePolicies, {
+          policySource: workspaceWithAllPolicies,
+        })
+      );
+
+      // Assert
+      expect(await axe(container)).toHaveNoViolations();
+      expect(screen.getByText('This workspace has the following policies:')).toBeInTheDocument();
+      screen.getByText('Additional security monitoring');
+      screen.getByText('Data access controls');
+      screen.getByText('Region constraint');
+    });
+
+    it('renders a tooltip', async () => {
+      // Arrange
+      const user = userEvent.setup();
+
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: protectedAzureWorkspace,
+        })
+      );
+
+      // Act, click on the info button to get tooltip text to render.
+      await user.click(screen.getByLabelText('More info'));
+
+      // Assert
+      expect(screen.getAllByText(policyLabel)).not.toBeNull();
+    });
   });
 
-  it('renders nothing if the policy array does not contain known policies', () => {
-    // Arrange
-    const nonProtectedAzureWorkspace: AzureWorkspace = {
-      ...defaultAzureWorkspace,
-      policies: [
-        {
-          additionalData: [],
-          namespace: 'terra',
-          name: 'some-other-policy',
-        },
-      ],
-    };
+  describe('handles a billing project as the source of the policies', () => {
+    it('renders nothing for a GCP billing project', () => {
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: gcpBillingProject,
+        })
+      );
 
-    // Act
-    render(
-      h(WorkspacePolicies, {
-        policySource: nonProtectedAzureWorkspace,
-      })
-    );
+      // Assert
+      expect(screen.queryByText(policyLabel)).toBeNull();
+    });
 
-    // Assert
-    expect(screen.queryByText(/This workspace has the following/)).toBeNull();
-  });
+    it('renders nothing for an Azure unprotected billing project', () => {
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: azureBillingProject,
+        })
+      );
 
-  it('renders policies with no accessibility errors', async () => {
-    // Arrange
-    const workspaceWithAllPolicies: AzureWorkspace = {
-      ...defaultAzureWorkspace,
-      policies: [protectedDataPolicy, groupConstraintPolicy, regionConstraintPolicy],
-    };
+      // Assert
+      expect(screen.queryByText(policyLabel)).toBeNull();
+    });
 
-    // Act
-    const { container } = render(
-      h(WorkspacePolicies, {
-        policySource: workspaceWithAllPolicies,
-      })
-    );
+    it('renders policies for an Azure protected data billing project', async () => {
+      // Act
+      render(
+        h(WorkspacePolicies, {
+          policySource: azureProtectedDataBillingProject,
+        })
+      );
 
-    // Assert
-    expect(await axe(container)).toHaveNoViolations();
-    expect(screen.getByText(/This workspace has the following/i)).toBeInTheDocument();
-    screen.getByText('Additional security monitoring');
-    screen.getByText('Data access controls');
-    screen.getByText('Region constraint');
-  });
-
-  it('renders a tooltip', async () => {
-    // Arrange
-    const user = userEvent.setup();
-
-    // Act
-    render(
-      h(WorkspacePolicies, {
-        policySource: protectedAzureWorkspace,
-      })
-    );
-
-    // Act, click on the info button to get tooltip text to render.
-    await user.click(screen.getByLabelText('More info'));
-
-    // Assert
-    expect(screen.getAllByText(/controlled-access data/)).not.toBeNull();
+      // Assert
+      expect(screen.getByText(policyLabel)).toBeInTheDocument();
+      screen.getByText('Additional security monitoring');
+    });
   });
 
   it('allows passing a title and label about the list', async () => {
