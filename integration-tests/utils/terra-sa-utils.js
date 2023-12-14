521e00763ba79284b8c17767d5dd0417682359fa
jyang-broad <85525681+jyang-broad@users.noreply.github.com>
2023-12-14 12:31:39 -0800

    [DDO-3170] auth using access/id tokens instead of sa keys. (#4444)

--- integration-tests/utils/terra-sa-utils.js
+++ integration-tests/utils/terra-sa-utils.js
@@ -2,6 +2,7 @@ const _ = require('lodash/fp');
 const { JWT } = require('google-auth-library');
 const { getSecrets, userEmail } = require('./integration-config');
 
+// generates a JWT using json key, and a subject to be impersonated
 const makeAuthClient = _.memoize((subject, { client_email: email, private_key: key }) => {
   return new JWT({
     email,
@@ -11,14 +12,23 @@ const makeAuthClient = _.memoize((subject, { client_email: email, private_key: k
   });
 });
 
+// Gets a token for an (impersonated) user account using a service account key
 const getToken = async (subject, key) => {
   const { token } = await makeAuthClient(subject, key).getAccessToken();
   return token;
 };
 
+// runs a command with a user bearer access token
+// if no token exists, uses a service account key to generate a user access token as set in integration-config.userEmail
 const withUserToken = (testFn) => async (options) => {
-  const { terraSaKeyJson } = await getSecrets();
+  const { userAccessToken, terraSaKeyJson } = await getSecrets();
+  if (userAccessToken != null) {
+    console.log('using userAccessToken...');
+    const token = userAccessToken;
+    return testFn({ ...options, token });
+  }
   const token = await getToken(userEmail, JSON.parse(terraSaKeyJson));
+  console.log('using getToken token...');
   return testFn({ ...options, token });
 };
 
