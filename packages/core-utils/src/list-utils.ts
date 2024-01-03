b5dcb4ed24d21a6ee2642751a6378198f2d2e45c
Eric Rollins <erollins@broadinstitute.org>
2024-01-03 10:53:21 -0500

    Removed irrelevant comment.

--- packages/core-utils/src/list-utils.ts
+++ packages/core-utils/src/list-utils.ts
@@ -1,8 +1,3 @@
-/**
- * Returns a promise that will never resolve or reject.
- *
- * Useful for cancelling async flows.
- */
 export const append = <Type>(value: Type): ((list: Type[]) => Type[]) => {
   return (list: Type[]): Type[] => {
     return [...list, value];
