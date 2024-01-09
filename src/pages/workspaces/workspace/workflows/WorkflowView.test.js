52a799e05ff49a6ec3d5ba163e0914826f48c00b
Katrina Pierre <kpierre@broadinstitute.org>
2024-01-09 12:39:27 -0500

    wip

--- src/pages/workspaces/workspace/workflows/WorkflowView.test.js
+++ src/pages/workspaces/workspace/workflows/WorkflowView.test.js
@@ -6,11 +6,16 @@ import { renderWithAppContexts as render } from 'src/testing/test-utils';
 
 jest.mock('src/libs/ajax');
 jest.mock('src/libs/nav', () => ({
-  ...jest.requireActual('src/libs/nav'),
-  getLink: jest.fn().mockImplementation((_) => _),
+  getCurrentUrl: jest.fn().mockReturnValue(new URL('https://app.terra.bio')),
+  getLink: jest.fn(),
+  goToPath: jest.fn(),
 }));
 
-describe('Workflow View', () => {
+jest.mock('src/libs/notifications', () => ({
+  notify: jest.fn(),
+}));
+
+describe('Workflow View (GCP)', () => {
   it('view workflow in workspace from mock import', async () => {
     const selectionKey = 'foobar';
 
@@ -71,6 +76,118 @@ describe('Workflow View', () => {
         namespace: 'gatk',
       },
     ];
+import { act } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { h } from 'react-hyperscript-helpers';
+import { Ajax } from 'src/libs/ajax';
+import { WorkflowView } from 'src/pages/workspaces/workspace/workflows/WorkflowView';
+import { renderWithAppContexts as render, SelectHelper } from 'src/testing/test-utils';
+
+jest.mock('src/libs/ajax');
+
+jest.mock('src/libs/nav', () => ({
+  getCurrentUrl: jest.fn().mockReturnValue(new URL('https://app.terra.bio')),
+  getLink: jest.fn(),
+  goToPath: jest.fn(),
+}));
+
+jest.mock('src/libs/notifications', () => ({
+  notify: jest.fn(),
+}));
+
+describe('Workflow View (GCP)', () => {
+  const namespace = 'testNamespace';
+  const name = 'testName';
+  const ws = {
+    name,
+    namespace,
+    cloudPlatform: 'Gcp',
+  };
+  const onDismiss = jest.fn();
+  const initializedGoogleWorkspace = {
+    accessLevel: 'OWNER',
+    owners: ['bar@foo.com'],
+    workspace: {
+      attributes: {
+        description: '',
+      },
+      authorizationDomain: [],
+      billingAccount: 'billingAccounts/google-billing-account',
+      bucketName: 'bucket-name',
+      cloudPlatform: 'Gcp',
+      completedCloneWorkspaceFileTransfer: '2023-02-03T22:29:04.319Z',
+      createdBy: 'bar@foo.com',
+      createdDate: '2023-02-03T22:26:06.124Z',
+      googleProject: 'google-project-id',
+      isLocked: false,
+      lastModified: '2023-02-03T22:26:06.202Z',
+      name: 'testName',
+      namespace: 'gatk',
+      workspaceId: 'google-workspace-id',
+      workspaceType: 'rawls',
+      workspaceVersion: 'v2',
+    },
+    canShare: true,
+    canCompute: true,
+    workspaceInitialized: true,
+  };
+  const mockStorageDetails = {
+    fetchedLocation: 'SUCCESS',
+  };
+
+  const methodList = [
+    {
+      name: 'echo_to_file',
+      createDate: '2019-11-21T19:10:23Z',
+      url: 'http://agora.dsde-dev.broadinstitute.org/api/v1/methods/gatk/echo_to_file/12',
+      synopsis: '',
+      entityType: 'Workflow',
+      snapshotComment: '',
+      snapshotId: 12,
+      namespace: 'gatk',
+    },
+  ];
+
+  it('renders Workflow View page', async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const props = { namespace, name, ws, onDismiss };
+    const selectionKey = 'foobar';
+    const mockAgoraResponse = [
+      {
+        name: 'echo_to_file',
+        createDate: '2019-11-21T19:10:23Z',
+        url: 'http://agora.dsde-dev.broadinstitute.org/api/v1/methods/gatk/echo_to_file/12',
+        synopsis: '',
+        entityType: 'Workflow',
+        snapshotComment: '',
+        snapshotId: 12,
+        namespace: 'gatk',
+      },
+    ];
+    const mockEntityMetadata = {
+      sra: {
+        attributeNames: ['biosample_accession', 'output'],
+        count: 6,
+        idName: 'sra_id',
+      },
+      sra_set: {
+        attributeNames: ['sras'],
+        count: 1,
+        idName: 'sra_set_id',
+      },
+    };
+    // const payload = {
+    //   name: 'sra',
+    //   entityType: 'Workflow',
+    //   attributes: {
+    //     [`${selectedEntityType}s`]: {
+    //       itemsType: 'EntityReference',
+    //       items: _.map((entityName) => ({ entityName, entityType: selectedEntityType }), selectedEntityNames),
+    //     },
+    //   },
+    // };
 
     Ajax.mockImplementation(() => {
       return {
@@ -87,7 +204,7 @@ describe('Workflow View', () => {
             storageCostEstimate: jest.fn(),
             bucketUsage: jest.fn(),
             checkBucketLocation: jest.fn().mockResolvedValue(mockStorageDetails),
-            entityMetadata: jest.fn().mockReturnValue({}),
+            entityMetadata: jest.fn().mockReturnValue(mockEntityMetadata),
             listSnapshots: jest.fn().mockResolvedValue({
               gcpDataRepoSnapshots: [],
             }),
@@ -96,13 +213,13 @@ describe('Workflow View', () => {
               get: jest.fn().mockResolvedValue({
                 methodRepoMethod: {
                   methodNamespace: 'gatk',
-                  methodName: 'joint-discovery-gatk4',
+                  methodName: 'echo_to_file',
                   sourceRepo: 'agora',
-                  methodPath: 'agora://gatk/joint-discovery-gatk4/1',
-                  methodVersion: 1,
+                  methodPath: 'agora://gatk/echo_to_file/12',
+                  methodVersion: 12,
                 },
-                rootEntityType: 'participant',
-                name: 'joint-discovery-gatk4',
+                rootEntityType: 'sra',
+                name: 'echo_to_file-configured',
               }),
             }),
           }),
@@ -112,15 +229,20 @@ describe('Workflow View', () => {
 
     // Act
     await act(async () => {
-      render(h(WorkflowView, { queryParams: { selectionKey } }));
+      render(h(WorkflowView, { props, queryParams: { selectionKey } }));
     });
 
-    expect(
-      screen.getByRole('button', {
-        name: /inputs/i,
-      })
-    );
+    const selectDataButton = screen.getAllByRole('button').filter((button) => button.textContent.includes('Select Data'))[0];
+    expect(selectDataButton).toHaveTextContent('Select Data');
+    expect(screen.getByText('sra')).toBeInTheDocument();
+    // screen.debug(undefined, 300000);
+    const dropdown = screen.getByLabelText('Entity type selector');
+    const dropdownHelper = new SelectHelper(dropdown, user);
+    // console.log(dropdownHelper.getSelectedOptions());
+    await dropdownHelper.selectOption('sra');
+    screen.debug(undefined, 300000);
 
-    expect(screen.getByText('joint-discovery-gatk4'));
+    // console.log(selectDataButton);
+    screen.logTestingPlaygroundURL();
   });
 });
