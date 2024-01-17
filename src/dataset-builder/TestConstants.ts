8e0e2fa95e0e157f27a60269c5fa2e383efe4c8b
Evan Suslovich <esuslovi@broadinstitute.org>
2024-01-17 15:54:11 -0500

    dumyGetNodeFromHierarchy, gets specific node from a hierarchy

--- src/dataset-builder/TestConstants.ts
+++ src/dataset-builder/TestConstants.ts
@@ -90,27 +90,76 @@ export const dummyDatasetModel = (): DatasetModel => ({
 export const dummyConcepts = [
   // IDs must be unique.
   { id: 100, name: 'Condition', count: 100, hasChildren: true },
-  { id: 101, name: 'Clinical Finding', count: 100, hasChildren: true },
-  { id: 102, name: 'Disease', count: 100, hasChildren: true },
-  { id: 103, name: 'Disorder by body site', count: 100, hasChildren: false },
-  { id: 104, name: 'Inflammatory disorder', count: 100, hasChildren: false },
-  { id: 105, name: 'Degenerative disorder', count: 100, hasChildren: false },
-  { id: 106, name: 'Metabolic disease', count: 100, hasChildren: false },
-  { id: 107, name: 'Finding by site', count: 100, hasChildren: false },
-  { id: 108, name: 'Neurological finding', count: 100, hasChildren: false },
-
-  { id: 200, name: 'Procedure', count: 100, hasChildren: true },
-  { id: 201, name: 'Procedure', count: 100, hasChildren: true },
-  { id: 202, name: 'Surgery', count: 100, hasChildren: false },
-  { id: 203, name: 'Heart Surgery', count: 100, hasChildren: false },
-  { id: 204, name: 'Cancer Surgery', count: 100, hasChildren: false },
-
-  { id: 300, name: 'Observation', count: 100, hasChildren: true },
-  { id: 301, name: 'Blood Pressure', count: 100, hasChildren: false },
-  { id: 302, name: 'Weight', count: 100, hasChildren: false },
-  { id: 303, name: 'Height', count: 100, hasChildren: false },
+  // { id: 101, name: 'Clinical Finding', count: 100, hasChildren: true },
+  // { id: 102, name: 'Disease', count: 100, hasChildren: true },
+  // { id: 103, name: 'Disorder by body site', count: 100, hasChildren: false },
+  // { id: 104, name: 'Inflammatory disorder', count: 100, hasChildren: false },
+  // { id: 105, name: 'Degenerative disorder', count: 100, hasChildren: false },
+  // { id: 106, name: 'Metabolic disease', count: 100, hasChildren: false },
+  // { id: 107, name: 'Finding by site', count: 100, hasChildren: false },
+  // { id: 108, name: 'Neurological finding', count: 100, hasChildren: false },
+  //
+  // { id: 200, name: 'Procedure', count: 100, hasChildren: true },
+  // { id: 201, name: 'Procedure', count: 100, hasChildren: true },
+  // { id: 202, name: 'Surgery', count: 100, hasChildren: false },
+  // { id: 203, name: 'Heart Surgery', count: 100, hasChildren: false },
+  // { id: 204, name: 'Cancer Surgery', count: 100, hasChildren: false },
+  //
+  // { id: 300, name: 'Observation', count: 100, hasChildren: true },
+  // { id: 301, name: 'Blood Pressure', count: 100, hasChildren: false },
+  // { id: 302, name: 'Weight', count: 100, hasChildren: false },
+  // { id: 303, name: 'Height', count: 100, hasChildren: false },
+  //
+  { id: 400, name: 'Carcinoma of lung parenchyma', count: 100, hasChildren: true },
+  { id: 401, name: 'Squamous cell carcinoma of lung', count: 100, hasChildren: true },
+  { id: 402, name: 'Non-small cell lung cancer', count: 100, hasChildren: true },
+  { id: 403, name: 'Epidermal growth factor receptor negative ...', count: 100, hasChildren: false },
+  { id: 404, name: 'Non-small cell lung cancer with mutation in epidermal..', count: 100, hasChildren: true },
+  { id: 405, name: 'Non-small cell cancer of lung biopsy..', count: 100, hasChildren: false },
 ];
 
 export const dummyGetConceptForId = (id: number): Concept => {
   return _.find({ id }, dummyConcepts)!;
 };
+
+export const dummyHierarchy = [
+  {
+    id: 405,
+    concept: dummyGetConceptForId(405),
+    children: [],
+    parent: 404,
+  },
+  {
+    id: 404,
+    concept: dummyGetConceptForId(404),
+    children: [405],
+    parent: 401,
+  },
+  {
+    id: 403,
+    concept: dummyGetConceptForId(403),
+    children: [],
+    parent: 401,
+  },
+  {
+    id: 402,
+    concept: dummyGetConceptForId(402),
+    children: [],
+    parent: 401,
+  },
+  {
+    id: 401,
+    concept: dummyGetConceptForId(401),
+    children: [402, 403, 404],
+    parent: 400,
+  },
+  {
+    id: 400,
+    concept: dummyGetConceptForId(400),
+    children: [401],
+  },
+];
+
+// export const dummyGetNodeFromHierarchy = (id: number) => {
+//   return _.find({ id }, dummyHierarchy)!;
+// };
