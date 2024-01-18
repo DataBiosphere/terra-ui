89f139a0531c52261cfefb64b6be977cd6517a30
Eric Rollins <erollins@broadinstitute.org>
2024-01-18 15:32:01 -0500

    De-currying append function to help with readability.

--- packages/core-utils/src/list-utils.ts
+++ packages/core-utils/src/list-utils.ts
@@ -1,17 +1,14 @@
 /**
- * A curried function that takes an item and adds it to the end of an array of
+ * A function that takes an item and adds it to the end of an array of
  * items of the same type.
  * @param {Type} value - The item being appended to an array
- * @returns { (list:Type[]) => Type[]} - Returns a function that accepts an
- * array of items with the same type as value. The function then adds the
- * value to the end of a copy  of the array provided without modifying the
- * array passed as a parameter(honors immutability)
+ * @param {Type} list - The list the item is being appended to.
+ * @returns {Type[]} - Returns a copy of the inputted list with the provided
+ * value being appended to the end of the list.
  * @example
  * // returns ['a', 'b', 'c']
- * append('c')(['a','b'])
+ * append('c', ['a','b'])
  */
-export const append = <Type>(value: Type): ((list: Type[]) => Type[]) => {
-  return (list: Type[]): Type[] => {
-    return [...list, value];
-  };
+export const append = <Type>(value: Type, list: Type[]): Type[] => {
+  return [...list, value];
 };
