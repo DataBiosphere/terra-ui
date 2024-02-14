7d22eb35ceed226285968d8cb85afe448f99dfcc
Evan Suslovich <esuslovi@broadinstitute.org>
2024-02-14 10:12:22 -0500

    cleaning up and adding comments

--- src/libs/ajax/DataRepo.ts
+++ src/libs/ajax/DataRepo.ts
@@ -229,9 +229,10 @@ export const DataRepo = (signal?: AbortSignal): DataRepoContract => ({
       getConcepts: async (parent: SnapshotBuilderConcept): Promise<GetConceptsResponse> =>
         callDataRepo(`repository/v1/datasets/${datasetId}/snapshotBuilder/concepts/${parent.id}`),
       searchConcepts: async (_domain: SnapshotBuilderConcept, searchText: string): Promise<GetConceptsResponse> =>
+        // we use encodeURIComponent to encode special characters like spaces
         callDataRepo(
           `repository/v1/datasets/${datasetId}/snapshotBuilder/concepts/${_domain.name}/${encodeURIComponent(
-            searchText !== '' ? searchText : ' '
+            searchText
           )}`
         ),
       queryDatasetColumnStatisticsById: (programDataOption) =>
