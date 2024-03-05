55e4df4907572d006f60c7713f030394aff6c940
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2024-03-05 11:09:41 -0500

    get GPU config from desiredRuntime

--- src/analysis/modals/ComputeModal/GcpComputeModal/GcpComputeModal.js
+++ src/analysis/modals/ComputeModal/GcpComputeModal/GcpComputeModal.js
@@ -477,7 +477,7 @@ export const GcpComputeModalBase = ({
     const desiredNumberOfWorkers = isDataprocCluster(runtimeType) ? computeConfig.numberOfWorkers : 0;
 
     return {
-      hasGpu: computeConfig.hasGpu,
+      gpuEnabled: computeConfig.gpuEnabled,
       autopauseThreshold: computeConfig.autopauseThreshold,
       runtime: Utils.cond(
         [
@@ -648,7 +648,7 @@ export const GcpComputeModalBase = ({
   };
 
   const sendCloudEnvironmentMetrics = () => {
-    const { runtime: desiredRuntime, persistentDisk: desiredPersistentDisk } = getDesiredEnvironmentConfig();
+    const { runtime: desiredRuntime, persistentDisk: desiredPersistentDisk, gpuEnabled: desiredGpuEnabled } = getDesiredEnvironmentConfig();
     const { runtime: existingRuntime, persistentDisk: existingPersistentDisk } = getExistingEnvironmentConfig();
     const desiredMachineType =
       desiredRuntime && (desiredRuntime.cloudService === cloudServices.GCE ? desiredRuntime.machineType : desiredRuntime.masterMachineType);
@@ -667,7 +667,7 @@ export const GcpComputeModalBase = ({
       ..._.mapKeys((key) => `desiredRuntime_${key}`, desiredRuntime),
       desiredRuntime_exists: !!desiredRuntime,
       desiredRuntime_cpus: desiredRuntime ? desiredRuntimeCpus : undefined,
-      desiredRuntime_gpuEnabled: desiredRuntime ? desiredRuntime.gpuConfig?.numOfGpus > 0 : undefined,
+      desiredRuntime_gpuEnabled: desiredRuntime ? desiredGpuEnabled : undefined,
       desiredRuntime_memory: desiredRuntime ? desiredRuntimeMemory : undefined,
       desiredRuntime_costPerHour: desiredRuntime ? runtimeConfigCost(formatRuntimeConfigForCosts()) : undefined,
       desiredRuntime_pausedCostPerHour: desiredRuntime ? runtimeConfigBaseCost(formatRuntimeConfigForCosts()) : undefined,
