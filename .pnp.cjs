2c280f0b510005df4a85d6552cc744d0b3072734
Katrina Pierre <kpierre@broadinstitute.org>
2024-03-05 10:49:18 -0500

    update es5 to v0.10.63

--- .pnp.cjs
+++ .pnp.cjs
@@ -13342,7 +13342,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["cli-color", "npm:2.0.3"],\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["es6-iterator", "npm:2.0.3"],\
             ["memoizee", "npm:0.4.15"],\
             ["timers-ext", "npm:0.1.7"]\
@@ -14066,7 +14066,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/d-npm-1.0.1-64afbbc689-49ca0639c7.zip/node_modules/d/",\
           "packageDependencies": [\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["type", "npm:1.2.0"]\
           ],\
           "linkType": "HARD"\
@@ -14996,12 +14996,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["es5-ext", [\
-        ["npm:0.10.62", {\
-          "packageLocation": "./.yarn/unplugged/es5-ext-npm-0.10.62-f20aca46cb/node_modules/es5-ext/",\
+        ["npm:0.10.64", {\
+          "packageLocation": "./.yarn/unplugged/es5-ext-npm-0.10.64-c30cdc3d60/node_modules/es5-ext/",\
           "packageDependencies": [\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["es6-iterator", "npm:2.0.3"],\
             ["es6-symbol", "npm:3.1.3"],\
+            ["esniff", "npm:2.0.1"],\
             ["next-tick", "npm:1.1.0"]\
           ],\
           "linkType": "HARD"\
@@ -15013,7 +15014,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["es6-iterator", "npm:2.0.3"],\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["es6-symbol", "npm:3.1.3"]\
           ],\
           "linkType": "HARD"\
@@ -15036,7 +15037,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["es6-weak-map", "npm:2.0.3"],\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["es6-iterator", "npm:2.0.3"],\
             ["es6-symbol", "npm:3.1.3"]\
           ],\
@@ -15909,6 +15910,19 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["esniff", [\
+        ["npm:2.0.1", {\
+          "packageLocation": "./.yarn/cache/esniff-npm-2.0.1-26cea8766c-d814c0e5c3.zip/node_modules/esniff/",\
+          "packageDependencies": [\
+            ["esniff", "npm:2.0.1"],\
+            ["d", "npm:1.0.1"],\
+            ["es5-ext", "npm:0.10.64"],\
+            ["event-emitter", "npm:0.3.5"],\
+            ["type", "npm:2.7.2"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["espree", [\
         ["npm:9.6.0", {\
           "packageLocation": "./.yarn/cache/espree-npm-9.6.0-8f679a5d0a-1287979510.zip/node_modules/espree/",\
@@ -15999,7 +16013,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["event-emitter", "npm:0.3.5"],\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"]\
+            ["es5-ext", "npm:0.10.64"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -20270,7 +20284,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/lru-queue-npm-0.1.0-8e1c90dde8-7f2c53c5e7.zip/node_modules/lru-queue/",\
           "packageDependencies": [\
             ["lru-queue", "npm:0.1.0"],\
-            ["es5-ext", "npm:0.10.62"]\
+            ["es5-ext", "npm:0.10.64"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -20516,7 +20530,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["memoizee", "npm:0.4.15"],\
             ["d", "npm:1.0.1"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["es6-weak-map", "npm:2.0.3"],\
             ["event-emitter", "npm:0.3.5"],\
             ["is-promise", "npm:2.2.2"],\
@@ -25945,7 +25959,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/timers-ext-npm-0.1.7-7edcefbfb5-ef3f27a070.zip/node_modules/timers-ext/",\
           "packageDependencies": [\
             ["timers-ext", "npm:0.1.7"],\
-            ["es5-ext", "npm:0.10.62"],\
+            ["es5-ext", "npm:0.10.64"],\
             ["next-tick", "npm:1.1.0"]\
           ],\
           "linkType": "HARD"\
