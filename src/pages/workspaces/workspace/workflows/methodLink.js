80dbe38f72e251419bfbd08d58063046f787be53
Chris Llanwarne <cjllanwarne@users.noreply.github.com>
2024-03-26 16:51:59 -0400

    WM-2538 Redirect method repo links (behind feature flag) (#4723)

--- src/pages/workspaces/workspace/workflows/methodLink.js
+++ src/pages/workspaces/workspace/workflows/methodLink.js
@@ -1,11 +1,19 @@
 import { getEnabledBrand } from 'src/libs/brand-utils';
 import { getConfig } from 'src/libs/config';
+import { isFeaturePreviewEnabled } from 'src/libs/feature-previews';
+import { FIRECLOUD_UI_MIGRATION } from 'src/libs/feature-previews-config';
+import * as Nav from 'src/libs/nav';
 
 export const methodLink = (config) => {
   const {
     methodRepoMethod: { sourceRepo, methodVersion, methodNamespace, methodName, methodPath },
   } = config;
-  return sourceRepo === 'agora'
-    ? `${getConfig().firecloudUrlRoot}/?return=${getEnabledBrand().queryName}#methods/${methodNamespace}/${methodName}/${methodVersion}`
-    : `${getConfig().dockstoreUrlRoot}/workflows/${methodPath}:${methodVersion}`;
+
+  if (sourceRepo === 'agora') {
+    if (isFeaturePreviewEnabled(FIRECLOUD_UI_MIGRATION)) {
+      return Nav.getLink('workflow-dashboard', { namespace: methodNamespace, name: methodName, snapshotId: methodVersion });
+    }
+    return `${getConfig().firecloudUrlRoot}/?return=${getEnabledBrand().queryName}#methods/${methodNamespace}/${methodName}/${methodVersion}`;
+  }
+  return `${getConfig().dockstoreUrlRoot}/workflows/${methodPath}:${methodVersion}`;
 };
