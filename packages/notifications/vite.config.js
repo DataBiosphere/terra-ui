9bbc653cdc3999db70956be26e87babae698e232
Miguel Silva <msilva@broadinstitute.org>
2024-03-27 11:15:17 -0400

    UIE-153 Code Sharing - Environments Storybook prep - simplified vite.config.js

--- packages/notifications/vite.config.js
+++ packages/notifications/vite.config.js
@@ -1,12 +1,4 @@
 import { viteConfig as defineBaseConfig } from '@terra-ui-packages/build-utils';
 import { defineConfig } from 'vite';
 
-export default defineConfig((args) => {
-  const baseConfig = defineBaseConfig(args)
-  return {
-    ...baseConfig,
-    plugins: [
-      ...baseConfig.plugins,
-    ]
-  }
-});
+export default defineConfig((args) => defineBaseConfig(args));
