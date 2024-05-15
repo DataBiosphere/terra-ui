b68de3de1b92859eb2721e3c0c228bcdb3f1c6a3
jdcanas <jcanas@broadinstitute.com>
2024-05-15 12:57:40 -0700

    poke

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -3,7 +3,7 @@
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
 
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
+# Use the error status of the first failure, rather than that of the last item in a pipeline. Also fail explicitly on any exit code.
 set -eo pipefail
 
 counter=0
