87972bcfa7826c85ca50211ef32aa9e2ac8c5490
Miguel Silva <msilva@broadinstitute.org>
2024-05-16 10:52:48 -0400

    UIE-165 error.ts decoupling pt2 followup  - moved basic error utils not entangled with notifications to be in core utils package.  - fixed references in Environments component, but kept rest of codebase going through src/libs/error.ts export proxy for now.

---
+++ packages/core-utils/src/error-utils.test.ts
@@ -0,0 +1,36 @@
+import { withErrorHandling, withErrorIgnoring } from './error-utils';
+
+describe('withErrorHandling', () => {
+  it('calls callback on error', async () => {
+    // Arrange
+    const willThrow = async () => {
+      throw new Error('BOOM!');
+    };
+    const callback = jest.fn();
+    const callMe = withErrorHandling(callback)(willThrow);
+
+    // Act
+    await callMe();
+
+    // Assert
+    expect(callback).toBeCalledTimes(1);
+    expect(callback).toBeCalledWith(new Error('BOOM!'));
+  });
+});
+
+describe('withErrorIgnoring', () => {
+  it('calls callback on error', async () => {
+    // Arrange
+    const willThrow = async () => {
+      throw new Error('BOOM!');
+    };
+    const callMe = withErrorIgnoring(willThrow);
+
+    // Act
+    await callMe();
+
+    // Assert
+    // Nothing should happen.
+    // if test gets here, all is well.
+  });
+});
