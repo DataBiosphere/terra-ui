6636f452ea1985f366d20d055161ff0dee5f48d0
jdcanas <jcanas@broadinstitute.com>
2024-05-16 13:38:46 -0700

    revert test code

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -9,18 +9,18 @@ set -eo pipefail
 counter=0
 
 # Wait up to 25 minutes for job to finish. A job can run on multiple nodes: parallelism > 1
-# while [ "$counter" -le 1500 ]; do
-#   # Find number of nodes in running
-#   job_detail=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM")
-#   job_running_nodes_count=$(echo "$job_detail" | jq -r '.parallel_runs[] | select(.status == "running") | select(.index != '"$CIRCLE_NODE_INDEX"')' | grep -c "running")
+while [ "$counter" -le 1500 ]; do
+  # Find number of nodes in running
+  job_detail=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM")
+  job_running_nodes_count=$(echo "$job_detail" | jq -r '.parallel_runs[] | select(.status == "running") | select(.index != '"$CIRCLE_NODE_INDEX"')' | grep -c "running")
 
-#   if [ "$job_running_nodes_count" -eq 0 ]; then
-#     exit 0
-#   fi
+  if [ "$job_running_nodes_count" -eq 0 ]; then
+    exit 0
+  fi
 
-#   sleep 10
-#   counter=$(($counter + 10))
-# done
+  sleep 10
+  counter=$(($counter + 10))
+done
 
 echo "Waited total $counter seconds"
 date
