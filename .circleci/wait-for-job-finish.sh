d6b00a20f2e59b5762b7ee9376d7a5881be7ae21
Justin Canas <justincanas16@gmail.com>
2024-05-17 08:38:37 -0700

    [IA-4962] timeout change (#4831)
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -3,8 +3,8 @@
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
 
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
+# Use the error status of the first failure, rather than that of the last item in a pipeline. Also fail explicitly on any exit code.
+set -eo pipefail
 
 counter=0
 
