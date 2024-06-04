b587d74a5911c512d74e1ee189466aab01318f01
aweng98 <bweng@broadinstitute.org>
2024-06-03 20:44:26 -0400

    sleep 10 sec first

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -16,22 +16,24 @@ URL_BASE="https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui"
 
 # Wait up to 30 minutes for job to finish. A job can run on multiple nodes: parallelism > 1
 while [ "$counter" -le 1800 ]; do
+  counter=$(($counter + 10))
+  sleep 10
+
   # Get job details
   job_detail=$(curl -s "$URL_BASE/job/$CIRCLE_BUILD_NUM")
 
   # Wait for all nodes with status==running excluding self node
   nodes=$(echo "$job_detail" | jq -r '.parallel_runs[]')
   running_nodes=$(echo "$nodes" | jq -r --arg IDX "$CIRCLE_NODE_INDEX" 'select(.status=="running") | select(.index|tostring!=$IDX)')
-  count=$(echo "$running_nodes" | grep -c -e "running" || test $? = 1;)
+  count=$(echo "$running_nodes" | length)
+
   if [ "$count" -eq 0 ]; then
       echo "Checking from NODE_INDEX #$CIRCLE_NODE_INDEX: Parallel running nodes have finished. Waited $counter seconds."
       echo "$running_nodes"
       exit 0
   fi
 
-  echo "Checking from NODE_INDEX #$CIRCLE_NODE_INDEX: Waiting for parallel running nodes to finish. Sleep 10 seconds."
-  sleep 10
-  counter=$(($counter + 10))
+  echo "Checking from NODE_INDEX #$CIRCLE_NODE_INDEX: Waiting for parallel running nodes to finish."
 done
 
 # Something is wrong. Log response for error troubleshooting
