0014e2b7fe378046843bd62c3b892565609f85ff
Alex Weng <35533885+aweng98@users.noreply.github.com>
2024-06-05 15:07:27 -0400

    [QA-2325] Fix test result notification failure (#4878)

--- .circleci/wait-for-job-finish.sh
+++ .circleci/wait-for-job-finish.sh
@@ -8,8 +8,8 @@ set -eo pipefail
 
 counter=0
 
-# Wait up to 25 minutes for job to finish. A job can run on multiple nodes: parallelism > 1
-while [ "$counter" -le 1500 ]; do
+# Wait up to 30 minutes for job to finish. A job can run on multiple nodes: parallelism > 1
+while [ "$counter" -le 1800 ]; do
   # Find number of nodes in running
   job_detail=$(curl -s "https://circleci.com/api/v2/project/github/DataBiosphere/terra-ui/job/$CIRCLE_BUILD_NUM")
   job_running_nodes_count=$(echo "$job_detail" | jq -r '[.parallel_runs[] | select(.status == "running") | select(.index != '"$CIRCLE_NODE_INDEX"')] | length')
@@ -18,8 +18,8 @@ while [ "$counter" -le 1500 ]; do
     exit 0
   fi
 
-  sleep 10
-  counter=$(($counter + 10))
+  sleep 30
+  counter=$(($counter + 30))
 done
 
 echo "Waited total $counter seconds"
