08b68f51565a6f00aefe2e87f8f456bf0bcecd99
kpierre <kpierre@broadinstitute.org>
2024-06-26 14:40:42 -0400

    re-add expandSubWorkflows option to metadata

--- src/workflows-app/utils/cromwell-metadata-utils.ts
+++ src/workflows-app/utils/cromwell-metadata-utils.ts
@@ -6,6 +6,7 @@ export type MetadataOptions = {
   includeKeys: string[];
   signal: AbortSignal;
   workflowId: string;
+  expandSubWorkflows: boolean;
 };
 
 export type WorkflowMetadata = {
@@ -36,6 +37,8 @@ export type WorkflowMetadata = {
 };
 
 export const fetchMetadata = async (options: MetadataOptions): Promise<WorkflowMetadata> =>
-  Ajax(options.signal)
-    .CromwellApp.workflows(options.workflowId)
-    .metadata(options.cromwellProxyUrl, { includeKey: options.includeKeys, excludeKey: options.excludeKeys });
+  Ajax(options.signal).CromwellApp.workflows(options.workflowId).metadata(options.cromwellProxyUrl, {
+    includeKey: options.includeKeys,
+    excludeKey: options.excludeKeys,
+    expandSubWorkflows: options.expandSubWorkflows,
+  });
