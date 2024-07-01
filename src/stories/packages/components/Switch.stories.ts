4981a64475699e64b30eae3461b7bea1146bb18a
aherbst-broad <aherbst@broadinstitute.org>
2024-07-01 16:07:57 -0400

    Switch storybook

--- src/stories/packages/components/Switch.stories.ts
+++ src/stories/packages/components/Switch.stories.ts
@@ -4,12 +4,45 @@ import { Switch } from '@terra-ui-packages/components';
 const meta: Meta<typeof Switch> = {
   title: 'Packages/Components/Switch',
   component: Switch,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    checked: {
+      control: 'boolean',
+      description: 'state of the switch',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'disable the switch',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    onLabel: {
+      control: 'text',
+      description: 'text for the "on" state',
+    },
+    offLabel: {
+      control: 'text',
+      description: 'text for the "off" state',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Switch>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    checked: false,
+    disabled: false,
+    onLabel: 'On',
+    offLabel: 'Off',
+  },
   parameters: {},
 };
