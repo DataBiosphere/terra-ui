f6a423ea97de827e995c1d914771309a7a71b45a
tlangs <tlangs@broadinstitute.org>
2024-07-15 13:59:29 -0400

    ID-1298 eRA Commons Login - Dev & Staging

--- src/profile/external-identities/OAuth2Providers.test.ts
+++ src/profile/external-identities/OAuth2Providers.test.ts
@@ -1,7 +1,7 @@
 import { oauth2Provider, OAuth2ProviderKey } from 'src/profile/external-identities/OAuth2Providers';
 
 describe('OAuth2Provider', () => {
-  it.each(['github', 'ras', 'fence', 'dcf-fence', 'kids-first', 'anvil'] as Array<OAuth2ProviderKey>)(
+  it.each(['github', 'ras', 'era-commons', 'fence', 'dcf-fence', 'kids-first', 'anvil'] as Array<OAuth2ProviderKey>)(
     'should return the correct provider for %s',
     (providerKey) => {
       const provider = oauth2Provider(providerKey);
