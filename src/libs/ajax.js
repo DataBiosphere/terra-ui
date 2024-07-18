b3f1c66c97db21ab8f472b28719ea531114f6e86
tlangs <tlangs@broadinstitute.org>
2024-07-18 09:42:43 -0400

    try request without identifier

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -3,7 +3,6 @@ import * as qs from 'qs';
 import {
   appIdentifier,
   authOpts,
-  drsAppIdentifier,
   fetchAgora,
   fetchDrsHub,
   fetchGoogleForms,
@@ -698,13 +697,7 @@ const DrsUriResolver = (signal) => ({
   getSignedUrl: async ({ bucket, object, dataObjectUri, googleProject }) => {
     const res = await fetchDrsHub(
       '/api/v4/gcs/getSignedUrl',
-      _.mergeAll([
-        jsonBody({ bucket, object, dataObjectUri, googleProject }),
-        authOpts(),
-        appIdentifier,
-        drsAppIdentifier,
-        { signal, method: 'POST' },
-      ])
+      _.mergeAll([jsonBody({ bucket, object, dataObjectUri, googleProject }), authOpts(), appIdentifier, { signal, method: 'POST' }])
     );
     return res.json();
   },
@@ -712,7 +705,7 @@ const DrsUriResolver = (signal) => ({
   getDataObjectMetadata: async (url, fields) => {
     const res = await fetchDrsHub(
       '/api/v4/drs/resolve',
-      _.mergeAll([jsonBody({ url, fields }), authOpts(), appIdentifier, drsAppIdentifier, { signal, method: 'POST' }])
+      _.mergeAll([jsonBody({ url, fields }), authOpts(), appIdentifier, { signal, method: 'POST' }])
     );
     return res.json();
   },
