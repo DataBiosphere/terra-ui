b35e4801bc13bf1ab89f37a5cafd395f0e1b5189
Bria Morgan <calypsomatic@gmail.com>
2024-07-26 13:51:32 -0400

    AJ-1930 toggle with switch (#4961)

--- src/workspace-data/data-table/uri-viewer/uri-viewer-utils.test.ts
+++ src/workspace-data/data-table/uri-viewer/uri-viewer-utils.test.ts
@@ -1,20 +1,39 @@
 import { getDownloadCommand } from './uri-viewer-utils';
 
 describe('getDownloadCommand', () => {
-  it('gets download command for gcloud storage', () => {
-    expect(getDownloadCommand('test.txt', 'gs://demo-data/test.txt')).toBe(
+  it('gets download command for gcloud storage using file directory', () => {
+    expect(getDownloadCommand('test.txt', 'gs://demo-data/test.txt', true)).toBe(
       "gcloud storage cp 'gs://demo-data/test.txt' test.txt"
     );
   });
 
-  it('gets download command for azcopy', () => {
+  it('gets download command for gcloud storage using current directory', () => {
+    expect(getDownloadCommand('test.txt', 'gs://demo-data/test.txt', false)).toBe(
+      "gcloud storage cp 'gs://demo-data/test.txt' ."
+    );
+  });
+
+  it('gets download command for azcopy using file directory', () => {
     expect(
       getDownloadCommand(
         'test.txt',
-        'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt'
+        'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt',
+        true
       )
     ).toBe(
       "azcopy copy 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt' test.txt"
+    );
+  });
+
+  it('gets download command for azcopy using current directory', () => {
+    expect(
+      getDownloadCommand(
+        'test.txt',
+        'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt',
+        false
+      )
+    ).toBe(
+      "azcopy copy 'https://lz8a3d793f17ede9b79635cc.blob.core.windows.net/sc-4b638f1f-b0a3-4161-a3fa-70e48edd981d/test.txt' ."
     );
   });
 });
