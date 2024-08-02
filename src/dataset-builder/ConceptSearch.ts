2e6f763af2f291aadf57628d361567905fd295c2
cinyecai <cinyecai@gmail.com>
2024-08-02 15:22:42 -0400

    Aligning checkboxes with text and re-adding right side padding in the table

--- src/dataset-builder/ConceptSearch.ts
+++ src/dataset-builder/ConceptSearch.ts
@@ -98,30 +98,32 @@ export const ConceptSearch = (props: ConceptSearchProps) => {
             rowStyle: {
               backgroundColor: 'white',
               ...tableLeftPadding,
+              paddingRight: '2rem',
             },
             headerRowStyle: {
               ...tableHeaderStyle,
               ...tableLeftPadding,
+              paddingRight: '2rem',
               marginTop: '1rem',
             },
             cellStyle: {
-              paddingTop: 10,
-              paddingBottom: 10,
+              paddingTop: 13,
+              paddingBottom: 13,
             },
             columns: [
               { header: strong(['Concept name']), key: 'name', size: { grow: 2.3 } },
               { header: strong(['Concept ID']), key: 'id', size: { grow: 0.5 } },
               { header: strong(['Code']), key: 'code', size: { grow: 0.75 } },
               { header: strong(['# Participants']), key: 'count', size: { grow: 0.5 } },
-              { key: 'hierarchy', size: { grow: 0.5 } },
+              { key: 'hierarchy', size: { grow: 0.4 } },
             ],
             rows: _.map((concept) => {
               return {
-                name: div({ style: { display: 'flex' } }, [
+                name: div({ style: { display: 'flex', alignItems: 'start' } }, [
                   h(
                     LabeledCheckbox,
                     {
-                      style: { padding: 12 },
+                      style: { marginRight: 5, marginTop: 1 },
                       checked: _.contains(concept, cart),
                       onChange: () => setCart(_.xor(cart, [concept])),
                     },
