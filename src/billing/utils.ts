1791b070bc1c00080dcc4a4becc76b7b95360478
cahrens <roberts@broadinstitute.org>
2024-08-08 16:37:09 -0400

    Move workspaces-related code to a file (WOR-1800).

--- src/billing/utils.ts
+++ src/billing/utils.ts
@@ -1,4 +1,6 @@
+import { IconProps } from '@terra-ui-packages/components';
 import { BillingProject } from 'src/billing-core/models';
+import colors from 'src/libs/colors';
 
 export const billingRoles = {
   owner: 'Owner',
@@ -21,3 +23,15 @@ export const isCreating = (project: BillingProject) =>
   project.status === 'Creating' || project.status === 'CreatingLandingZone';
 export const isDeleting = (project: BillingProject) => project.status === 'Deleting';
 export const isErrored = (project: BillingProject) => project.status === 'Error' || project.status === 'DeletionFailed';
+
+export const billingAccountIconSize = 16;
+
+const billingAccountIconProps: Record<string, IconProps> = {
+  updating: { icon: 'sync', color: colors.warning(), size: billingAccountIconSize },
+  done: { icon: 'check', color: colors.accent(), size: billingAccountIconSize },
+  error: { icon: 'warning-standard', color: colors.danger(), size: billingAccountIconSize },
+};
+
+export const getBillingAccountIconProps = (status: string): IconProps => {
+  return billingAccountIconProps[status];
+};
