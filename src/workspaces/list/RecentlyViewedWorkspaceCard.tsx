04aa7d9012e1af94e0b193faabb37794e8900882
trholdridge <trholdridge@gmail.com>
2024-08-09 16:25:47 -0400

    Convert to TSX

--- src/workspaces/list/RecentlyViewedWorkspaceCard.tsx
+++ src/workspaces/list/RecentlyViewedWorkspaceCard.tsx
@@ -1,5 +1,5 @@
 import { Clickable } from '@terra-ui-packages/components';
-import { div, h } from 'react-hyperscript-helpers';
+import React from 'react';
 import { CloudProviderIcon } from 'src/components/CloudProviderIcon';
 import { Ajax } from 'src/libs/ajax';
 import colors from 'src/libs/colors';
@@ -22,36 +22,34 @@ export const RecentlyViewedWorkspaceCard = (props: RecentlyViewedWorkspaceCardPr
 
   const dateViewed = Utils.makeCompleteDate(new Date(parseInt(timestamp)).toString());
 
-  return h(
-    Clickable,
-    {
-      style: {
+  return (
+    <Clickable
+      style={{
         ...Style.elements.card.container,
         maxWidth: 'calc(25% - 10px)',
         margin: '0 0.25rem',
         lineHeight: '1.5rem',
         flex: '0 1 calc(25% - 10px)',
-      },
-      href: Nav.getLink('workspace-dashboard', { namespace, name }),
-      onClick: () => {
+      }}
+      href={Nav.getLink('workspace-dashboard', { namespace, name })}
+      onClick={() => {
         Ajax().Metrics.captureEvent(
           Events.workspaceOpenFromRecentlyViewed,
           extractWorkspaceDetails(workspace.workspace)
         );
-      },
-    },
-    [
-      div({ style: { flex: 'none' } }, [
-        div({ style: { color: colors.accent(), ...Style.noWrapEllipsis, fontSize: 16, marginBottom: 7 } }, [name]),
-        div({ style: { display: 'flex', justifyContent: 'space-between' } }, [
-          div({ style: { ...Style.noWrapEllipsis, whiteSpace: 'pre-wrap', fontStyle: 'italic' } }, [
-            `Viewed ${dateViewed}`,
-          ]),
-          div({ style: { display: 'flex', alignItems: 'center' } }, [
-            h(CloudProviderIcon, { cloudProvider: getCloudProviderFromWorkspace(workspace), style: { marginLeft: 5 } }),
-          ]),
-        ]),
-      ]),
-    ]
+      }}
+    >
+      <div style={{ flex: 'none' }}>
+        <div style={{ color: colors.accent(), ...Style.noWrapEllipsis, fontSize: 16, marginBottom: 7 }}>{name}</div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ ...Style.noWrapEllipsis, whiteSpace: 'pre-wrap', fontStyle: 'italic' }}>
+            Viewed {dateViewed}
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <CloudProviderIcon cloudProvider={getCloudProviderFromWorkspace(workspace)} style={{ marginLeft: 5 }} />
+          </div>
+        </div>
+      </div>
+    </Clickable>
   );
 };
