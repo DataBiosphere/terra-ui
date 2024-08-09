f875771826bfa183489cbae1183251604fb8b872
trholdridge <trholdridge@gmail.com>
2024-08-09 16:02:25 -0400

    Convert to TSX

---
+++ src/workspaces/common/NoWorkspacesMessage.tsx
@@ -0,0 +1,26 @@
+import { Clickable, Link } from '@terra-ui-packages/components';
+import React from 'react';
+import * as Utils from 'src/libs/utils';
+
+interface NoWorkspacesMessageProps {
+  onClick: () => void;
+}
+
+export const NoWorkspacesMessage = (props: NoWorkspacesMessageProps): React.ReactNode => {
+  const { onClick } = props;
+  return (
+    <div style={{ fontSize: 20, margin: '1rem' }}>
+      <div>
+        {'To get started, '}
+        <Clickable onClick={onClick} style={{ fontWeight: 600 }}>
+          Create a New Workspace
+        </Clickable>
+      </div>
+      <div style={{ marginTop: '1rem', fontSize: 16 }}>
+        <Link {...Utils.newTabLinkProps} href='https://support.terra.bio/hc/en-us/articles/360024743371'>
+          What&#39;s a workspace?
+        </Link>
+      </div>
+    </div>
+  );
+};
