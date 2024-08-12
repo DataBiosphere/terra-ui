5104ec3d4c39323856ee7a4e8bfc162344d799f2
trholdridge <trholdridge@gmail.com>
2024-08-12 09:39:04 -0400

    Fix linebreak

--- src/workspaces/common/NoWorkspacesMessage.tsx
+++ src/workspaces/common/NoWorkspacesMessage.tsx
@@ -1,5 +1,6 @@
 import { Clickable, Link } from '@terra-ui-packages/components';
 import React from 'react';
+import colors from 'src/libs/colors';
 import * as Utils from 'src/libs/utils';
 
 interface NoWorkspacesMessageProps {
@@ -12,7 +13,7 @@ export const NoWorkspacesMessage = (props: NoWorkspacesMessageProps): React.Reac
     <div style={{ fontSize: 20, margin: '1rem' }}>
       <div>
         {'To get started, '}
-        <Clickable onClick={onClick} style={{ fontWeight: 600 }}>
+        <Clickable onClick={onClick} style={{ display: 'inline', fontWeight: 600, color: colors.accent() }}>
           Create a New Workspace
         </Clickable>
       </div>
