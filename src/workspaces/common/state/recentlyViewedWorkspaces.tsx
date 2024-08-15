0724508a131e715e1833890eced47f28691ed3d5
trholdridge <trholdridge@gmail.com>
2024-08-15 14:01:27 -0400

    Convert recentlyViewedWorkspaces to TSX and test

--- src/workspaces/common/state/recentlyViewedWorkspaces.tsx
+++ src/workspaces/common/state/recentlyViewedWorkspaces.tsx
@@ -3,11 +3,13 @@ import { getLocalPref, setLocalPref } from 'src/libs/prefs';
 
 export const recentlyViewedPersistenceId = 'workspaces/recentlyViewed';
 
-export const updateRecentlyViewedWorkspaces = (workspaceId) => {
-  const recentlyViewed = getLocalPref(recentlyViewedPersistenceId)?.recentlyViewed || [];
+type RecentlyViewed = { workspaceId: string; timestamp: number };
+
+export const updateRecentlyViewedWorkspaces = (workspaceId: string) => {
+  const recentlyViewed: RecentlyViewed[] = getLocalPref(recentlyViewedPersistenceId)?.recentlyViewed || [];
   // Recently viewed workspaces are limited to 4. Additionally, if a user clicks a workspace multiple times,
   // we only want the most recent instance stored in the list.
-  const updatedRecentlyViewed = _.flow(
+  const updatedRecentlyViewed: RecentlyViewed[] = _.flow(
     _.remove({ workspaceId }),
     _.concat([{ workspaceId, timestamp: Date.now() }]),
     _.orderBy(['timestamp'], ['desc']),
