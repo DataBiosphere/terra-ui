13225569199625a03ab071cb8c322b4be6f16e58
cahrens <roberts@broadinstitute.org>
2024-08-21 09:58:03 -0400

    Convert to TSX.

--- src/billing/List/BillingList.tsx
+++ src/billing/List/BillingList.tsx
@@ -2,8 +2,7 @@ import { SpinnerOverlay } from '@terra-ui-packages/components';
 import { withHandlers } from '@terra-ui-packages/core-utils';
 import _ from 'lodash/fp';
 import * as qs from 'qs';
-import { useEffect, useRef, useState } from 'react';
-import { div, h, h2, p, span } from 'react-hyperscript-helpers';
+import React, { CSSProperties, ReactNode, useEffect, useRef, useState } from 'react';
 import * as Auth from 'src/auth/auth';
 import { CreateBillingProjectControl } from 'src/billing/List/CreateBillingProjectControl';
 import { GCPNewBillingProjectModal } from 'src/billing/List/GCPNewBillingProjectModal';
@@ -27,17 +26,16 @@ import * as Utils from 'src/libs/utils';
 import { useWorkspaces } from 'src/workspaces/common/state/useWorkspaces';
 import { CloudProvider, cloudProviderTypes } from 'src/workspaces/utils';
 
-const BillingProjectSubheader = ({ title, children }) =>
-  h(
-    Collapse,
-    {
-      title: span({ style: { fontWeight: 'bold' } }, [title]),
-      initialOpenState: true,
-      titleFirst: true,
-      summaryStyle: { padding: '1rem 1rem 1rem 2rem' },
-    },
-    [children]
-  );
+const BillingProjectSubheader: React.FC<{ title: string; children: ReactNode }> = ({ title, children }) => (
+  <Collapse
+    title={<span style={{ fontWeight: 'bold' }}>{title}</span>}
+    initialOpenState
+    titleFirst
+    summaryStyle={{ padding: '1rem 1rem 1rem 2rem' }}
+  >
+    {children}
+  </Collapse>
+);
 
 interface BillingListProps {
   queryParams: {
@@ -155,158 +153,132 @@ export const BillingList = (props: BillingListProps) => {
     };
   };
 
-  return div({ role: 'main', style: { display: 'flex', flex: 1, height: `calc(100% - ${Style.topBarHeight}px)` } }, [
-    div(
-      {
-        style: {
+  const renderContent = (): ReactNode => {
+    if (!!selectedName && !_.some({ projectName: selectedName }, billingProjects)) {
+      return (
+        <div style={{ margin: '1rem auto 0 auto' }}>
+          <div>
+            <h2>Error loading selected billing project.</h2>
+            <p>It may not exist, or you may not have access to it.</p>
+          </div>
+        </div>
+      );
+    }
+    if (azureUserWithNoBillingProjects || creatingAzureBillingProject) {
+      return (
+        <AzureBillingProjectWizard
+          onSuccess={(billingProjectName, protectedData) => {
+            Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
+              billingProjectName,
+              cloudPlatform: cloudProviderTypes.AZURE,
+              protectedData,
+            });
+            setCreatingBillingProjectType(null);
+            loadProjects();
+          }}
+        />
+      );
+    }
+    if (!isLoadingProjects && _.isEmpty(billingProjects) && !Auth.isAzureUser()) {
+      return (
+        <GCPBillingProjectWizard
+          billingAccounts={billingAccounts}
+          authorizeAndLoadAccounts={authorizeAndLoadAccounts}
+          onSuccess={(billingProjectName) => {
+            Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
+              billingProjectName,
+              cloudPlatform: cloudProviderTypes.GCP,
+            });
+            setCreatingBillingProjectType(null);
+            loadProjects();
+            Nav.history.push({
+              pathname: Nav.getPath('billing'),
+              search: qs.stringify({ selectedName: billingProjectName, type: 'project' }),
+            });
+          }}
+        />
+      );
+    }
+    if (!!selectedName && _.some({ projectName: selectedName }, billingProjects)) {
+      const billingProject = billingProjects.find(({ projectName }) => projectName === selectedName);
+      return (
+        <ProjectDetail
+          key={selectedName}
+          // We know from the condition that the billingProject does exist.
+          // @ts-ignore
+          billingProject={billingProject}
+          billingAccounts={billingAccounts}
+          authorizeAndLoadAccounts={authorizeAndLoadAccounts}
+          reloadBillingProject={() => reloadBillingProject(billingProject).catch(loadProjects)}
+          isOwner={projectsOwned.some(({ projectName }) => projectName === selectedName)}
+          workspaces={allWorkspaces}
+          refreshWorkspaces={refreshWorkspaces}
+        />
+      );
+    }
+    if (!_.isEmpty(projectsOwned) && !selectedName) {
+      return <div style={{ margin: '1rem auto 0 auto' } as CSSProperties}>Select a Billing Project</div>;
+    }
+  };
+
+  return (
+    <div role='main' style={{ display: 'flex', flex: 1, height: `calc(100% - ${Style.topBarHeight}px)` }}>
+      <div
+        style={{
           minWidth: billingProjectListWidth,
           maxWidth: billingProjectListWidth,
           boxShadow: '0 2px 5px 0 rgba(0,0,0,0.25)',
           overflowY: 'auto',
-        },
-      },
-      [
-        div(
-          {
-            role: 'navigation',
-            style: {
-              fontSize: 16,
-              fontWeight: 600,
-              padding: '2rem 1rem 1rem',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              textTransform: 'uppercase',
-              color: colors.dark(),
-            },
-          },
-          [
-            h2({ style: { fontSize: 16 } }, ['Billing Projects']),
-            h(CreateBillingProjectControl, { showCreateProjectModal }),
-          ]
-        ),
-        h(BillingProjectSubheader, { title: 'Owned by You' }, [
-          div({ role: 'list' }, [
-            _.map(
-              (project) => h(ProjectListItem, { key: project.projectName, ...makeProjectListItemProps(project) }),
-              projectsOwned
-            ),
-          ]),
-        ]),
-        h(BillingProjectSubheader, { title: 'Shared with You' }, [
-          div({ role: 'list' }, [
-            _.map(
-              (project) => h(ProjectListItem, { key: project.projectName, ...makeProjectListItemProps(project) }),
-              projectsShared
-            ),
-          ]),
-        ]),
-      ]
-    ),
-    creatingBillingProjectType === 'GCP' &&
-      h(GCPNewBillingProjectModal, {
-        billingAccounts,
-        loadAccounts,
-        onDismiss: () => setCreatingBillingProjectType(null),
-        onSuccess: (billingProjectName) => {
-          Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
-            billingProjectName,
-            cloudPlatform: cloudProviderTypes.GCP,
-          });
-          setCreatingBillingProjectType(null);
-          loadProjects();
-        },
-      }),
-    div(
-      {
-        style: {
-          overflowY: 'auto',
-          flexGrow: 1,
-          display: 'flex',
-          flexDirection: 'column',
-        },
-      },
-      [
-        Utils.cond(
-          [
-            !!selectedName && !_.some({ projectName: selectedName }, billingProjects),
-            () =>
-              div(
-                {
-                  style: {
-                    margin: '1rem auto 0 auto',
-                  },
-                },
-                [
-                  div([
-                    h2(['Error loading selected billing project.']),
-                    p(['It may not exist, or you may not have access to it.']),
-                  ]),
-                ]
-              ),
-          ],
-          [
-            azureUserWithNoBillingProjects || creatingAzureBillingProject,
-            () =>
-              h(AzureBillingProjectWizard, {
-                onSuccess: (billingProjectName, protectedData) => {
-                  Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
-                    billingProjectName,
-                    cloudPlatform: cloudProviderTypes.AZURE,
-                    protectedData,
-                  });
-                  setCreatingBillingProjectType(null);
-                  loadProjects();
-                },
-              }),
-          ],
-          [
-            !isLoadingProjects && _.isEmpty(billingProjects) && !Auth.isAzureUser(),
-            () =>
-              h(GCPBillingProjectWizard, {
-                billingAccounts,
-                onSuccess: (billingProjectName) => {
-                  Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
-                    billingProjectName,
-                    cloudPlatform: cloudProviderTypes.GCP,
-                  });
-                  setCreatingBillingProjectType(null);
-                  loadProjects();
-                  Nav.history.push({
-                    pathname: Nav.getPath('billing'),
-                    search: qs.stringify({ selectedName: billingProjectName, type: 'project' }),
-                  });
-                },
-                authorizeAndLoadAccounts,
-              }),
-          ],
-          [
-            !!selectedName && _.some({ projectName: selectedName }, billingProjects),
-            () => {
-              const billingProject = _.find({ projectName: selectedName }, billingProjects);
-              return h(ProjectDetail, {
-                key: selectedName,
-                // We know from the condition that the billingProject does exist.
-                // @ts-ignore
-                billingProject,
-                billingAccounts,
-                authorizeAndLoadAccounts,
-                reloadBillingProject: () => reloadBillingProject(billingProject).catch(loadProjects),
-                isOwner: _.some({ projectName: selectedName }, projectsOwned),
-                workspaces: allWorkspaces,
-                refreshWorkspaces,
+        }}
+      >
+        <div
+          role='navigation'
+          style={{
+            fontSize: 16,
+            fontWeight: 600,
+            padding: '2rem 1rem 1rem',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            textTransform: 'uppercase',
+            color: colors.dark(),
+          }}
+        >
+          <h2 style={{ fontSize: 16 }}>Billing Projects</h2>
+          <CreateBillingProjectControl showCreateProjectModal={showCreateProjectModal} />
+        </div>
+        <BillingProjectSubheader title='Owned by You'>
+          <div role='list'>
+            {projectsOwned.map((project) => (
+              <ProjectListItem key={project.projectName} {...makeProjectListItemProps(project)} />
+            ))}
+          </div>
+        </BillingProjectSubheader>
+        <BillingProjectSubheader title='Shared with You'>
+          <div role='list'>
+            {projectsShared.map((project) => (
+              <ProjectListItem key={project.projectName} {...makeProjectListItemProps(project)} />
+            ))}
+          </div>
+        </BillingProjectSubheader>
+        {creatingBillingProjectType === 'GCP' && (
+          <GCPNewBillingProjectModal
+            billingAccounts={billingAccounts}
+            loadAccounts={loadAccounts}
+            onDismiss={() => setCreatingBillingProjectType(null)}
+            onSuccess={(billingProjectName) => {
+              Ajax().Metrics.captureEvent(Events.billingCreationBillingProjectCreated, {
+                billingProjectName,
+                cloudPlatform: cloudProviderTypes.GCP,
               });
-            },
-          ],
-          [
-            !_.isEmpty(projectsOwned) && !selectedName,
-            () => {
-              return div({ style: { margin: '1rem auto 0 auto' } }, ['Select a Billing Project']);
-            },
-          ]
-        ),
-      ]
-    ),
-    (isLoadingProjects || isAuthorizing || isLoadingAccounts) && h(SpinnerOverlay, { mode: 'FullScreen' } as const),
-  ]);
+              setCreatingBillingProjectType(null);
+              loadProjects();
+            }}
+          />
+        )}
+      </div>
+      <div style={{ overflowY: 'auto', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>{renderContent()}</div>
+      {(isLoadingProjects || isAuthorizing || isLoadingAccounts) && <SpinnerOverlay mode='FullScreen' />}
+    </div>
+  );
 };
