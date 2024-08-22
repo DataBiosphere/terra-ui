048d0d02447eeee3e594670319ea9a8f0eba0cfb
kpierre <kpierre@broadinstitute.org>
2024-08-22 13:57:33 -0400

    add styling; add form labels to boxes

--- src/pages/workflows/workflow/common/WorkflowModal.tsx
+++ src/pages/workflows/workflow/common/WorkflowModal.tsx
@@ -1,6 +1,7 @@
 import { ButtonPrimary, Modal } from '@terra-ui-packages/components';
 import React from 'react';
 import { TextInput } from 'src/components/input';
+import { FormLabel } from 'src/libs/forms';
 
 interface WorkflowModalProps {
   setCreateWorkflowModalOpen: (x: boolean) => void;
@@ -32,23 +33,22 @@ const NamespaceNameSection = (props: NamespaceNameSectionProps) => {
 
   return (
     <>
-      <div style={{ flex: '1 0 auto', marginRight: '1em' }}>
-        <div style={{ marginBottom: '0.1667em' }}>Namespace</div>
+      <div style={{ flexWrap: 'wrap', flexGrow: 1, flexBasis: '400px' }}>
+        <div style={{ marginBottom: '0.1667em' }}>
+          <FormLabel>Namespace</FormLabel>
+        </div>
         <div>
-          <TextInput
-            style={{ width: '100%' }}
-            value={namespace}
-            onChange={(value: string) => setWorkflowNamespace(value)}
-          />
+          <TextInput value={namespace} onChange={(value: string) => setWorkflowNamespace(value)} />
         </div>
         <div style={{ fontStyle: 'italic' }}>{namingMessage}</div>
       </div>
-      <div style={{ flex: '1 0 auto' }}>
-        <div style={{ marginBottom: '0.1667em' }}>Name</div>
-        <TextInput style={{ width: '100%' }} value={name} onChange={(value: string) => setWorkflowName(value)} />
+      <div style={{ display: 'flex', flexWrap: 'wrap', flexGrow: 1, flexBasis: '400px' }}>
+        <div style={{ marginBottom: '0.1667em' }}>
+          <FormLabel>Name</FormLabel>
+        </div>
+        <TextInput value={name} onChange={(value: string) => setWorkflowName(value)} />
         <div style={{ fontStyle: 'italic' }}>{namingMessage}</div>
       </div>
-      ;
     </>
   );
 };
@@ -58,11 +58,15 @@ const SynopsisSnapshotSection = (props: SynopsisSnapshotSectionProps) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <div style={{ paddingTop: '1.5rem' }}>
-        <div style={{ marginBottom: '0.1667em' }}>Synopsis (optional, 80 characters max)</div>
+        <div style={{ marginBottom: '0.1667em' }}>
+          <FormLabel>Synopsis (optional, 80 characters max)</FormLabel>
+        </div>
         <TextInput value={synopsis} onChange={(value) => setWorkflowSynopsis(value)} />
       </div>
       <div style={{ paddingTop: '1.5rem' }}>
-        <div style={{ marginBottom: '0.1667em' }}>Snapshot Comment (optional)</div>
+        <div style={{ marginBottom: '0.1667em' }}>
+          <FormLabel>Snapshot Comment (optional)</FormLabel>
+        </div>
         <TextInput />
       </div>
     </div>
@@ -86,12 +90,12 @@ export const WorkflowModal = (props: WorkflowModalProps) => {
     <Modal
       onDismiss={() => setCreateWorkflowModalOpen(false)}
       title={title}
-      width='70%'
+      width='65rem'
       /* eslint-disable-next-line no-alert */
       okButton={<ButtonPrimary onClick={() => alert('not yet implemented')}>{buttonAction}</ButtonPrimary>}
     >
       <div style={{ padding: '0.5rem 0' }}>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', alignItems: 'flex-end', flexWrap: 'wrap', gap: '16px' }}>
           <NamespaceNameSection
             namespace={namespace}
             name={name}
