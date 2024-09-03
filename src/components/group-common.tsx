f47e5033938ace7ea1447ac17657099d9ba6dead
Blake Geno <bgeno@broadinstitute.org>
2024-09-03 11:45:13 -0400

    convert to tsx with minimal changes

--- src/components/group-common.tsx
+++ src/components/group-common.tsx
@@ -1,9 +1,7 @@
-import { Modal, TooltipTrigger } from '@terra-ui-packages/components';
+import { Icon, Modal, TooltipTrigger, useUniqueId } from '@terra-ui-packages/components';
 import _ from 'lodash/fp';
-import React, { Fragment, useState } from 'react';
-import { b, div, h, label } from 'react-hyperscript-helpers';
-import { ButtonPrimary, IdContainer, LabeledCheckbox, Link, spinnerOverlay } from 'src/components/common';
-import { icon } from 'src/components/icons';
+import React, { useState } from 'react';
+import { ButtonPrimary, LabeledCheckbox, Link, spinnerOverlay } from 'src/components/common';
 import { InfoBox } from 'src/components/InfoBox';
 import { AutocompleteTextInput } from 'src/components/input';
 import { MenuButton } from 'src/components/MenuButton';
@@ -32,61 +30,45 @@ interface AdminNotifierCheckboxProps {
   checked: boolean;
   onChange: (v: boolean) => void;
 }
-export const AdminNotifierCheckbox = (props: AdminNotifierCheckboxProps) => {
-  const { checked, onChange } = props;
-  return div({ style: { marginTop: '0.5rem', display: 'flex', alignItems: 'center' } }, [
-    h(
-      LabeledCheckbox,
-      {
-        style: { marginRight: '0.25rem' },
-        checked,
-        onChange,
-      },
-      ['Allow anyone to request access']
-    ),
-    h(InfoBox, { style: { marginLeft: '0.3rem' } }, [
-      'Any user will be able to request to become a member of this group. This will send an email to the group admins.',
-    ]),
-  ]);
-};
+export const AdminNotifierCheckbox = (props: AdminNotifierCheckboxProps) => (
+  <div style={{ marginTop: '0.5rem', display: 'flex', alignItems: 'center' }}>
+    <LabeledCheckbox style={{ marginRight: '0.25rem' }} checked={props.checked} onChange={props.onChange}>
+      Allow anyone to request access
+    </LabeledCheckbox>
+    ,
+    <InfoBox style={{ marginLeft: '0.3rem' }}>
+      Any user will be able to request to become a member of this group. This will send an email to the group admins.
+    </InfoBox>
+  </div>
+);
 
 interface NewUserCardProps {
   onClick: () => void;
 }
-export const NewUserCard = (props: NewUserCardProps) => {
-  return h(
-    ButtonPrimary,
-    {
-      style: { textTransform: 'none' },
-      onClick: props.onClick,
-    },
-    [icon('plus', { size: 14 }), div({ style: { marginLeft: '0.5rem' } }, ['Add User'])]
-  );
-};
+export const NewUserCard = (props: NewUserCardProps) => (
+  <ButtonPrimary style={{ textTransform: 'none' }} onClick={props.onClick}>
+    <Icon icon='plus' size={14} />,<div style={{ marginLeft: '0.5rem' }}>Add User</div>
+  </ButtonPrimary>
+);
 
 interface UserMenuContentProps {
   onEdit: () => void;
   onDelete: () => void;
 }
 
-const UserMenuContent = (props: UserMenuContentProps) => {
-  return h(Fragment, [
-    h(
-      MenuButton,
-      {
-        onClick: props.onEdit,
-      },
-      [makeMenuIcon('edit'), 'Edit Role']
-    ),
-    h(
-      MenuButton,
-      {
-        onClick: props.onDelete,
-      },
-      [makeMenuIcon('trash'), 'Remove User']
-    ),
-  ]);
-};
+const UserMenuContent = (props: UserMenuContentProps) => (
+  <>
+    <MenuButton onClick={props.onEdit}>
+      {makeMenuIcon('edit')}
+      Edit Role
+    </MenuButton>
+    ,
+    <MenuButton onClick={props.onDelete}>
+      {makeMenuIcon('trash')}
+      Remove User
+    </MenuButton>
+  </>
+);
 
 const menuCardSize = 20;
 
@@ -102,25 +84,26 @@ interface MemberCardHeadersProps {
 
 export const MemberCardHeaders: React.FC<MemberCardHeadersProps> = memoWithName(
   'MemberCardHeaders',
-  (props: MemberCardHeadersProps) => {
-    const { sort, onSort } = props;
-    return div(
-      {
-        role: 'row',
-        style: { display: 'flex', justifyContent: 'space-between', marginTop: '1.5rem', padding: '0 1rem' },
-      },
-      [
-        div({ role: 'columnheader', 'aria-sort': ariaSort(sort, 'email'), style: { flex: 1 } }, [
-          h(HeaderRenderer, { sort, onSort, name: 'email' }),
-        ]),
-        div({ role: 'columnheader', 'aria-sort': ariaSort(sort, 'roles'), style: { flex: 1 } }, [
-          h(HeaderRenderer, { sort, onSort, name: 'roles' }),
-        ]),
+  (props: MemberCardHeadersProps) => (
+    <div
+      role='row'
+      style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1.5rem', padding: '0 1rem' }}
+    >
+      <div role='columnheader' aria-sort={ariaSort(props.sort, 'email')} style={{ flex: 1 }}>
+        <HeaderRenderer sort={props.sort} onSort={props.onSort} name='email' />
+      </div>
+      <div role='columnheader' aria-sort={ariaSort(props.sort, 'roles')} style={{ flex: 1 }}>
+        <HeaderRenderer sort={props.sort} onSort={props.onSort} name='roles' />
+      </div>
+      <div
+        role='columnheader'
         // Width is the same as the menu icon.
-        div({ role: 'columnheader', style: { width: menuCardSize } }, [div({ className: 'sr-only' }, ['Actions'])]),
-      ]
-    );
-  }
+        style={{ width: menuCardSize }}
+      >
+        <div className='sr-only'>Actions</div>
+      </div>
+    </div>
+  )
 );
 
 export interface User {
@@ -151,40 +134,33 @@ export const MemberCard: React.FC<MemberCardProps> = memoWithName('MemberCard', 
   const canEdit = adminCanEdit || !_.includes(adminLabel, roles);
   const tooltip = !canEdit && `This user is the only ${adminLabel}`;
 
-  return div(
-    {
-      role: 'row',
-      style: Style.cardList.longCardShadowless,
-    },
-    [
-      div(
-        {
-          role: 'rowheader',
-          style: { flex: '1', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden', height: '1rem' },
-        },
-        [email]
-      ),
-      div({ role: 'cell', style: { flex: '1', textTransform: 'capitalize', height: '1rem' } }, [
-        _.includes(adminLabel, roles) ? adminLabel : userLabel,
-      ]),
-      isOwner &&
-        div({ role: 'cell', style: { flex: 'none' } }, [
-          h(
-            MenuTrigger,
-            {
-              side: 'left',
-              style: { height: menuCardSize, width: menuCardSize },
-              closeOnClick: true,
-              content: h(UserMenuContent, { onEdit, onDelete }),
-            },
-            [
-              h(Link, { 'aria-label': `Menu for User: ${email}`, disabled: !canEdit, tooltip, tooltipSide: 'left' }, [
-                icon('cardMenuIcon', { size: menuCardSize }),
-              ]),
-            ]
-          ),
-        ]),
-    ]
+  return (
+    <div role='row' style={Style.cardList.longCardShadowless}>
+      <div
+        role='rowheader'
+        style={{ flex: '1', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden', height: '1rem' }}
+      >
+        {email}
+      </div>
+      <div role='cell' style={{ flex: '1', textTransform: 'capitalize', height: '1rem' }}>
+        {_.includes(adminLabel, roles) ? adminLabel : userLabel}
+      </div>
+      {isOwner && (
+        <div role='cell' style={{ flex: 'none' }}>
+          <MenuTrigger
+            side='left'
+            style={{ height: menuCardSize, width: menuCardSize }}
+            closeOnClick
+            content={<UserMenuContent onEdit={onEdit} onDelete={onDelete} />}
+          >
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <Link aria-label={`Menu for User: ${email}`} disabled={!canEdit} tooltip={tooltip} tooltipSide='left'>
+              <Icon icon='cardMenuIcon' size={menuCardSize} />
+            </Link>
+          </MenuTrigger>
+        </div>
+      )}
+    </div>
   );
 });
 
@@ -229,7 +205,6 @@ export const NewUserModal = (props: NewUserModalProps) => {
 
       setSuggestions(suggestions);
     });
-
     loadData();
   });
 
@@ -269,86 +244,70 @@ export const NewUserModal = (props: NewUserModalProps) => {
 
   const canAdd = (value) => value !== userEmail || !errors;
 
+  const emailInputId = useUniqueId();
+
   return Utils.cond(
     [
       confirmAddUser,
-      () =>
-        h(
-          Modal,
-          {
-            title: 'User is not registered',
-            okButton: h(ButtonPrimary, { onClick: inviteUser }, ['Yes']),
-            cancelText: 'No',
-            onDismiss: () => setConfirmAddUser(false),
-          },
-          ['Add ', b([userEmail]), ' to the group anyway?', busy && spinnerOverlay]
-        ),
+      () => (
+        <Modal
+          title='User is not registered'
+          okButton={<ButtonPrimary onClick={inviteUser}>Yes</ButtonPrimary>}
+          cancelText='No'
+          onDismiss={() => setConfirmAddUser(false)}
+        >
+          Add <b>{userEmail}</b> to the group anyway?
+          {busy && spinnerOverlay}
+        </Modal>
+      ),
     ],
-    () =>
-      h(
-        Modal,
-        {
-          onDismiss,
-          title,
-          okButton: h(
-            ButtonPrimary,
-            {
-              tooltip: Utils.summarizeErrors(errors),
-              onClick: addUser,
-              disabled: errors,
-            },
-            ['Add User']
-          ),
-        },
-        [
-          h(IdContainer, [
-            (id) =>
-              h(Fragment, [
-                h(FormLabel, { id, required: true }, ['User email']),
-                h(AutocompleteTextInput, {
-                  labelId: id,
-                  autoFocus: true,
-                  openOnFocus: false,
-                  value: userEmail,
-                  onChange: setUserEmail,
-                  renderSuggestion: (suggestion) =>
-                    div({ style: styles.suggestionContainer }, [
-                      div({ style: { flex: 1 } }, [
-                        !canAdd(suggestion) &&
-                          h(
-                            TooltipTrigger,
-                            {
-                              content: 'Not a valid email address',
-                            },
-                            [icon('warning-standard', { style: { color: colors.danger(), marginRight: '0.5rem' } })]
-                          ),
-                        suggestion,
-                      ]),
-                    ]),
-                  suggestions: [
-                    ...(!!userEmail && !suggestions.includes(userEmail) ? [userEmail] : []),
-                    ...suggestions,
-                  ],
-                  style: { fontSize: 16 },
-                  type: undefined,
-                }),
-              ]),
-          ]),
-          h(FormLabel, ['Role']),
-          h(
-            LabeledCheckbox,
-            {
-              checked: isAdmin,
-              onChange: () => setRoles([isAdmin ? userLabel : adminLabel]),
-            },
-            [label({ style: { margin: '0 2rem 0 0.25rem' } }, [`Can manage users (${adminLabel})`])]
-          ),
-          footer && div({ style: { marginTop: '1rem' } }, [footer]),
-          submitError &&
-            div({ style: { marginTop: '0.5rem', textAlign: 'right', color: colors.danger() } }, [submitError]),
-          busy && spinnerOverlay,
-        ]
-      )
+    () => (
+      <Modal
+        onDismiss={onDismiss}
+        title={title}
+        okButton={
+          <ButtonPrimary tooltip={Utils.summarizeErrors(errors)} onClick={addUser} disabled={!!errors}>
+            Add User
+          </ButtonPrimary>
+        }
+      >
+        <FormLabel id={emailInputId} required>
+          User email
+        </FormLabel>
+        <AutocompleteTextInput
+          labelId={emailInputId}
+          autoFocus
+          openOnFocus={false}
+          value={userEmail}
+          onChange={setUserEmail}
+          renderSuggestion={(suggestion) => (
+            <div style={styles.suggestionContainer}>
+              <div style={{ flex: 1 }}>
+                {!canAdd(suggestion) && (
+                  <TooltipTrigger content='Not a valid email address'>
+                    <Icon icon='warning-standard' style={{ color: colors.danger(), marginRight: '0.5rem' }} />
+                  </TooltipTrigger>
+                )}
+                {suggestion}
+              </div>
+            </div>
+          )}
+          suggestions={[...(!!userEmail && !suggestions.includes(userEmail) ? [userEmail] : []), ...suggestions]}
+          style={{ fontSize: 16 }}
+          type={undefined}
+        />
+        <FormLabel>Role</FormLabel>
+        <LabeledCheckbox checked={isAdmin} onChange={() => setRoles([isAdmin ? userLabel : adminLabel])}>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label style={{ margin: '0 2rem 0 0.25rem' }}>Can manage users {adminLabel}</label>
+        </LabeledCheckbox>
+        {footer && <div style={{ marginTop: '1rem' }}>{footer}</div>}
+        {submitError && (
+          <div style={{ marginTop: '0.5rem', textAlign: 'right', color: colors.danger() }}>{submitError}</div>
+        )}
+        {busy && spinnerOverlay}
+      </Modal>
+    )
   );
 };
 
@@ -385,31 +344,21 @@ export const EditUserModal = (props: EditUserModalProps) => {
     onSuccess();
   });
 
-  return h(
-    Modal,
-    {
-      onDismiss,
-      title: 'Edit Roles',
-      okButton: h(
-        ButtonPrimary,
-        {
-          onClick: submit,
-        },
-        ['Change Role']
-      ),
-    },
-    [
-      div({ style: { marginBottom: '0.25rem' } }, ['Edit role for ', b([email])]),
-      h(
-        LabeledCheckbox,
-        {
-          checked: isAdmin,
-          onChange: () => setIsAdmin(!isAdmin),
-        },
-        [label({ style: { margin: '0 2rem 0 0.25rem' } }, [`Can manage users (${adminLabel})`])]
-      ),
-      submitting && spinnerOverlay,
-    ]
+  return (
+    <Modal
+      onDismiss={onDismiss}
+      title='Edit Roles'
+      okButton={<ButtonPrimary onClick={submit}>Change Role</ButtonPrimary>}
+    >
+      <div style={{ marginBottom: '0.25rem' }}>
+        Edit role for <b>{email}</b>
+      </div>
+      <LabeledCheckbox checked={isAdmin} onChange={() => setIsAdmin(!isAdmin)}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label style={{ margin: '0 2rem 0 0.25rem' }}>Can manage users {adminLabel}</label>
+      </LabeledCheckbox>
+      {submitting && spinnerOverlay}
+    </Modal>
   );
 };
 
@@ -418,21 +367,15 @@ interface DeleteUserModalProps {
   onSubmit: () => void;
   userEmail: string;
 }
-export const DeleteUserModal = (props: DeleteUserModalProps) => {
-  const { onDismiss, onSubmit, userEmail } = props;
-  return h(
-    Modal,
-    {
-      onDismiss,
-      title: 'Confirm',
-      okButton: h(
-        ButtonPrimary,
-        {
-          onClick: onSubmit,
-        },
-        ['Remove']
-      ),
-    },
-    [div(['Are you sure you want to remove']), b([`${userEmail}?`])]
-  );
-};
+
+export const DeleteUserModal = (props: DeleteUserModalProps) => (
+  <Modal
+    onDismiss={props.onDismiss}
+    title='Confirm'
+    okButton={<ButtonPrimary onClick={props.onSubmit}>Remove</ButtonPrimary>}
+  >
+    <div>
+      Are you sure you want to remove , <b>{props.userEmail}</b>?
+    </div>
+  </Modal>
+);
