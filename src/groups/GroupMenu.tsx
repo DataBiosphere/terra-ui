9689b1b30ce225596016b89b10c87e9250e72596
Blake Geno <bgeno@broadinstitute.org>
2024-09-05 10:01:48 -0400

    add types and removed incorrect focus property

--- src/groups/GroupMenu.tsx
+++ src/groups/GroupMenu.tsx
@@ -1,14 +1,31 @@
 import { Icon } from '@terra-ui-packages/components';
-import React from 'react';
-import { Clickable } from 'src/components/common';
+import React, { ReactNode } from 'react';
+import { Clickable, ClickableProps } from 'src/components/common';
 import { MenuButton } from 'src/components/MenuButton';
 import { makeMenuIcon, MenuTrigger } from 'src/components/PopupTrigger';
 
-const GroupMenu = ({ groupName, isAdmin, iconSize, popupLocation, callbacks: { onDelete, onLeave } }) => {
-  const navIconProps = {
+interface GroupMenuProps {
+  groupName: string;
+  isAdmin: boolean;
+  iconSize;
+  popupLocation;
+  callbacks: {
+    onDelete: () => void;
+    onLeave: () => void;
+  };
+}
+
+export const GroupMenu = (props: GroupMenuProps): ReactNode => {
+  const {
+    groupName,
+    isAdmin,
+    iconSize,
+    popupLocation,
+    callbacks: { onDelete, onLeave },
+  } = props;
+  const navIconProps: Partial<ClickableProps> = {
     style: { opacity: 0.65, marginRight: '1rem', height: iconSize },
     hover: { opacity: 1 },
-    focus: 'hover',
   };
 
   return (
@@ -35,7 +52,6 @@ const GroupMenuContent = ({ isAdmin, onLeave, onDelete }) => {
         {makeMenuIcon('arrowRight')}
         Leave
       </MenuButton>
-      ,
       <MenuButton
         disabled={!isAdmin}
         tooltip={!isAdmin && 'You must be an admin of this group'}
