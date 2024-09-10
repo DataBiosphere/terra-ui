e9bd9086e34d490a10ab0958679d7f02ca8fc61a
Miguel Silva <msilva@broadinstitute.org>
2024-09-10 13:09:48 -0400

    UIE-200 Ajax cleanup pt6  - broke out Submissions sub area out of ajax.ts and into sub-module.  - only minimal conversion to Typescript.

--- src/libs/ajax.ts
+++ src/libs/ajax.ts
@@ -1,7 +1,5 @@
-import _ from 'lodash/fp';
 import * as qs from 'qs';
-import { authOpts } from 'src/auth/auth-session';
-import { fetchGoogleForms, fetchRawls } from 'src/libs/ajax/ajax-common';
+import { fetchGoogleForms } from 'src/libs/ajax/ajax-common';
 import { AzureStorage } from 'src/libs/ajax/AzureStorage';
 import { Billing } from 'src/libs/ajax/Billing';
 import { Catalog } from 'src/libs/ajax/Catalog';
@@ -19,6 +17,7 @@ import { Methods } from 'src/libs/ajax/methods/Methods';
 import { Metrics } from 'src/libs/ajax/Metrics';
 import { OAuth2 } from 'src/libs/ajax/OAuth2';
 import { SamResources } from 'src/libs/ajax/SamResources';
+import { Submissions } from 'src/libs/ajax/submissions/Submissions';
 import { Support } from 'src/libs/ajax/Support';
 import { TermsOfService } from 'src/libs/ajax/TermsOfService';
 import { User } from 'src/libs/ajax/User';
@@ -29,13 +28,6 @@ import { WorkflowScript } from 'src/libs/ajax/workflows-app/WorkflowScript';
 import { WorkspaceData } from 'src/libs/ajax/WorkspaceDataService';
 import { WorkspaceManagerResources } from 'src/libs/ajax/WorkspaceManagerResources';
 import { Workspaces } from 'src/libs/ajax/workspaces/Workspaces';
-
-const Submissions = (signal?: AbortSignal) => ({
-  queueStatus: async () => {
-    const res = await fetchRawls('submissions/queueStatus', _.merge(authOpts(), { signal }));
-    return res.json();
-  },
-});
 
 const Surveys = (signal?: AbortSignal) => ({
   submitForm: (formId, data) => fetchGoogleForms(`${formId}/formResponse?${qs.stringify(data)}`, { signal }),
