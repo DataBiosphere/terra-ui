300704f33770243f8c6affa1211fe0a5c7d77cdb
Bria Morgan <bmorgan@broadinstitute.org>
2024-09-10 15:26:12 -0400

    useDataTableProvider tests

--- src/workspace-data/useDataTableProvider.test.ts
+++ src/workspace-data/useDataTableProvider.test.ts
@@ -1,10 +1,14 @@
 import { DeepPartial } from '@terra-ui-packages/core-utils';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { Ajax } from 'src/libs/ajax';
+import { fetchWDS } from 'src/libs/ajax/ajax-common';
 import { ListAppItem } from 'src/libs/ajax/leonardo/models/app-models';
 import { asMockedFn } from 'src/testing/test-utils';
 
 import { useDataTableProvider } from './useDataTableProvider';
+
+const CWDS_WORKSPACE_ID = 'cwdsWorkspaceId';
+const WDS_APP_WORKSPACE_ID = 'wdsAppWorkspaceId';
 
 type AjaxCommonExports = typeof import('src/libs/ajax/ajax-common');
 
@@ -21,8 +25,23 @@ jest.mock('src/libs/ajax/ajax-common', (): AjaxCommonExports => {
   return {
     ...jest.requireActual<AjaxCommonExports>('src/libs/ajax/ajax-common'),
     fetchWDS: jest.fn().mockImplementation(() => {
-      return jest.fn().mockResolvedValue({
-        json: jest.fn().mockResolvedValue({}),
+      return jest.fn((path: string) => {
+        // collections/v1/{workspaceId}
+        if (path.includes(CWDS_WORKSPACE_ID)) {
+          return Promise.resolve({
+            status: 200,
+            json: jest.fn().mockResolvedValue([
+              {
+                id: CWDS_WORKSPACE_ID,
+                name: 'default',
+                description: 'default',
+              },
+            ]),
+          });
+        }
+        return Promise.resolve({
+          json: jest.fn().mockResolvedValue([]),
+        });
       });
     }),
   };
@@ -63,20 +82,41 @@ describe('useDataTableProvider', () => {
 
   it('should only check CWDS once', async () => {
     // Arrange
-    const workspaceId = 'workspaceId';
-
     // Act
-    renderHook(() => useDataTableProvider(workspaceId));
+    await act(() => {
+      renderHook(() => useDataTableProvider(WDS_APP_WORKSPACE_ID)); // It doesn't matter which workspaceId is used in this test
+    });
 
     // Assert
     // Fetch wds might be called for other purpose, we only care about the call to check for collections
-    // const mockFetchWDS = fetchWDS as jest.Mock;
-    // console.log(mockFetchWDS.mock.results);
-    // const mockInnerFunction = mockFetchWDS.mock.results[0].value;
+    const mockFetchWDS = fetchWDS as jest.Mock;
+    const mockInnerFunction = mockFetchWDS.mock.results[0].value;
 
-    // // TODO make the mock function return something so there's no error in the test
-    // // Now verify that we only called CWDS once
-    // expect(mockInnerFunction).toHaveBeenCalledTimes(1);
-    // expect(mockInnerFunction).toHaveBeenCalledWith(`collections/v1/${workspaceId}`, expect.anything());
+    // Now verify that we only called CWDS once
+    expect(mockInnerFunction).toHaveBeenCalledTimes(1);
+    expect(mockInnerFunction).toHaveBeenCalledWith(`collections/v1/${WDS_APP_WORKSPACE_ID}`, expect.anything());
+  });
+
+  // This test is disabled until a later PR for AJ-1965 uses the useCwds variable to determine whether or not to call listAppsV2
+  //   it('should not call list apps if cwds is in use', async () => {
+  //     // Arrange
+  //     // Act
+  //     await act(() => {
+  //       renderHook(() => useDataTableProvider(CWDS_WORKSPACE_ID));
+  //     });
+
+  //     // Assert
+  //     expect(mockListAppsV2).not.toHaveBeenCalled();
+  //   });
+
+  it('should call list apps if cwds is not in use', async () => {
+    // Arrange
+    // Act
+    await act(() => {
+      renderHook(() => useDataTableProvider(WDS_APP_WORKSPACE_ID));
+    });
+
+    // Assert
+    expect(mockListAppsV2).toHaveBeenCalled();
   });
 });
