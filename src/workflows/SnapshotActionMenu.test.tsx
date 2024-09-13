a7ef70d71a0ddc6457753c3350cc267fb0c29df3
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-09-13 15:05:40 -0400

    Update snapshot action menu tests

--- src/workflows/SnapshotActionMenu.test.tsx
+++ src/workflows/SnapshotActionMenu.test.tsx
@@ -7,19 +7,46 @@ import SnapshotActionMenu from 'src/workflows/SnapshotActionMenu';
 const mockOnDelete = jest.fn();
 
 describe('snapshot action menu', () => {
-  it('renders the menu buttons when you click the kebab menu icon', async () => {
+  it('renders and enables the menu buttons if you are the snapshot owner', async () => {
     // Arrange
     const user: UserEvent = userEvent.setup();
 
     // Act
     await act(async () => {
-      render(<SnapshotActionMenu onDelete={mockOnDelete} />);
+      render(<SnapshotActionMenu isSnapshotOwner onDelete={mockOnDelete} />);
     });
 
     await user.click(screen.getByRole('button', { name: 'Snapshot action menu' }));
 
+    const deleteSnapshotButton = screen.getByRole('button', { name: 'Delete snapshot' });
+
+    await user.pointer({ target: deleteSnapshotButton });
+
     // Assert
-    expect(screen.getByRole('button', { name: 'Delete snapshot' })).toBeInTheDocument();
+    expect(deleteSnapshotButton).toBeInTheDocument();
+    expect(deleteSnapshotButton).toHaveAttribute('aria-disabled', 'false');
+    expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+  });
+
+  it('renders the menu buttons but disables the proper ones if you are not the snapshot owner', async () => {
+    // Arrange
+    const user: UserEvent = userEvent.setup();
+
+    // Act
+    await act(async () => {
+      render(<SnapshotActionMenu isSnapshotOwner={false} onDelete={mockOnDelete} />);
+    });
+
+    await user.click(screen.getByRole('button', { name: 'Snapshot action menu' }));
+
+    const deleteSnapshotButton = screen.getByRole('button', { name: 'Delete snapshot' });
+
+    await user.pointer({ target: deleteSnapshotButton });
+
+    // Assert
+    expect(deleteSnapshotButton).toBeInTheDocument();
+    expect(deleteSnapshotButton).toHaveAttribute('aria-disabled', 'true');
+    expect(screen.getByRole('tooltip')).toBeInTheDocument();
   });
 
   it('closes and calls the onDelete callback when you press the delete snapshot menu button', async () => {
@@ -28,7 +55,7 @@ describe('snapshot action menu', () => {
 
     // Act
     await act(async () => {
-      render(<SnapshotActionMenu onDelete={mockOnDelete} />);
+      render(<SnapshotActionMenu isSnapshotOwner onDelete={mockOnDelete} />);
     });
 
     await user.click(screen.getByRole('button', { name: 'Snapshot action menu' }));
