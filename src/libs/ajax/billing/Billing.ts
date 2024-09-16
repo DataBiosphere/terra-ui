97736ea6262192827709f8beb0e9edd935907423
cahrens <roberts@broadinstitute.org>
2024-09-16 11:12:54 -0400

    Cleanup

--- src/libs/ajax/billing/Billing.ts
+++ src/libs/ajax/billing/Billing.ts
@@ -137,7 +137,7 @@ export const Billing = (signal?: AbortSignal) => ({
     return res.json();
   },
 
-  addProjectUser: async (projectName: string, roles: BillingRole[], email: string): Promise<Response> => {
+  addProjectUser: async (projectName: string, roles: BillingRole[], email: string): Promise<void> => {
     let userRoles: BillingProjectMember[] = [];
     roles.forEach((role) => {
       userRoles = _.concat(userRoles, [{ email, role }]);
