c7921bc8dab17fc4847ecb2d41a69e6a87aafe94
kpierre <kpierre@broadinstitute.org>
2024-09-25 11:02:41 -0400

    wip

--- src/pages/workflows/workflow/common/PermissionsModal.test.tsx
+++ src/pages/workflows/workflow/common/PermissionsModal.test.tsx
@@ -50,7 +50,7 @@ describe('PermissionsModal', () => {
         <PermissionsModal
           name='test'
           namespace='namespace'
-          workflowOrNamespace='workflow'
+          methodOrNamespace='method'
           selectedSnapshot='3'
           setPermissionsModalOpen={jest.fn()}
         />
@@ -71,7 +71,7 @@ describe('PermissionsModal', () => {
         <PermissionsModal
           name='test'
           namespace='namespace'
-          workflowOrNamespace='workflow'
+          methodOrNamespace='method'
           selectedSnapshot='3'
           setPermissionsModalOpen={jest.fn()}
         />
@@ -93,7 +93,7 @@ describe('PermissionsModal', () => {
         <PermissionsModal
           name='test'
           namespace='namespace'
-          workflowOrNamespace='workflow'
+          methodOrNamespace='method'
           selectedSnapshot='3'
           setPermissionsModalOpen={jest.fn()}
         />
@@ -131,7 +131,7 @@ describe('PermissionsModal', () => {
         <PermissionsModal
           name='test'
           namespace='namespace'
-          workflowOrNamespace='workflow'
+          methodOrNamespace='method'
           selectedSnapshot='3'
           setPermissionsModalOpen={jest.fn()}
         />
@@ -165,7 +165,7 @@ describe('PermissionsModal', () => {
         <PermissionsModal
           name='test'
           namespace='namespace'
-          workflowOrNamespace='workflow'
+          methodOrNamespace='method'
           selectedSnapshot='3'
           setPermissionsModalOpen={jest.fn()}
         />
