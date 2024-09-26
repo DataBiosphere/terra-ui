90d058693524b26e7d903d8f4fdbbafc9aa01da4
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-09-26 10:56:14 -0400

    [WX-1758] Implement deleting method snapshots from methods repository (#5075)

---
+++ src/workflows/modals/DeleteSnapshotModal.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode } from 'react';
+import { DeleteConfirmationModal } from 'src/components/common';
+
+interface DeleteSnapshotModalProps {
+  /** The namespace of the workflow snapshot to be deleted (for display). */
+  namespace: string;
+
+  /** The name of the workflow snapshot to be deleted (for display). */
+  name: string;
+
+  /** The snapshot ID of the workflow snapshot to be deleted (for display). */
+  snapshotId: string;
+
+  /**
+   * The action to be performed if the deletion is confirmed (should include the
+   * API call to delete the snapshot).
+   */
+  onConfirm: () => void;
+
+  /** The action to be performed if the modal is dismissed. */
+  onDismiss: () => void;
+}
+
+/**
+ * A modal to confirm the deletion of a workflow snapshot from the Broad Methods
+ * Repository.
+ *
+ * The onConfirm prop is passed directly to the underlying modal; this component
+ * does not already include the API call to perform the snapshot deletion.
+ */
+const DeleteSnapshotModal = (props: DeleteSnapshotModalProps): ReactNode => {
+  const { namespace, name, snapshotId, onConfirm, onDismiss } = props;
+
+  return (
+    <DeleteConfirmationModal
+      objectType='snapshot'
+      objectName={`${namespace}/${name}/${snapshotId}`}
+      onConfirm={onConfirm}
+      onDismiss={onDismiss}
+    >
+      <p>
+        Are you sure you want to delete snapshot <b>{snapshotId}</b> of the method{' '}
+        <b>
+          {namespace}/{name}
+        </b>
+        {/*
+         */}
+        ?
+      </p>
+      <p>Exports of this snapshot in workspaces will no longer run.</p>
+      <p>
+        <strong>This cannot be undone.</strong>
+      </p>
+    </DeleteConfirmationModal>
+  );
+};
+
+export default DeleteSnapshotModal;
