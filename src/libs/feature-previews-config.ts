08d1adf51f38db730e52ab223fe2ae63b549ad41
David An <davidan@broadinstitute.org>
2024-10-15 12:00:11 -0400

    CORE-56: release provenance and versions (#5132)

--- src/libs/feature-previews-config.ts
+++ src/libs/feature-previews-config.ts
@@ -46,26 +46,6 @@ export type FeaturePreview = {
 
 const featurePreviewsConfig: readonly FeaturePreview[] = [
   {
-    id: 'data-table-versioning',
-    title: 'Data Table Versioning',
-    description:
-      'Enabling this feature will allow you to save uniquely named versions of data tables. These saved versions will appear in the Data tab and can be restored at any time.',
-    groups: ['preview-data-versioning-and-provenance'],
-    feedbackUrl: `mailto:dsp-core-services@broadinstitute.org?subject=${encodeURIComponent(
-      'Feedback on data table versioning'
-    )}`,
-  },
-  {
-    id: 'data-table-provenance',
-    title: 'Data Table Provenance',
-    description:
-      'Enabling this feature will allow you to view information about the workflow that generated data table columns and files.',
-    groups: ['preview-data-versioning-and-provenance'],
-    feedbackUrl: `mailto:dsp-core-services@broadinstitute.org?subject=${encodeURIComponent(
-      'Feedback on data table provenance'
-    )}`,
-  },
-  {
     id: JUPYTERLAB_GCP_FEATURE_ID,
     title: 'JupyterLab on GCP',
     description: 'Enabling this feature will allow you to launch notebooks using JupyterLab in GCP workspaces.',
