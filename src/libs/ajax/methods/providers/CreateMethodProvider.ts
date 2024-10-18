8950b0e27905d3b136fd5d6d59de78afe97b903a
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-10-18 13:53:38 -0400

    Implement create method provider

---
+++ src/libs/ajax/methods/providers/CreateMethodProvider.ts
@@ -0,0 +1,41 @@
+import { AbortOption } from '@terra-ui-packages/data-client-core';
+import { Methods } from 'src/libs/ajax/methods/Methods';
+import { MethodResponse } from 'src/libs/ajax/methods/methods-models';
+
+export interface CreateMethodProvider {
+  create: (
+    namespace: string,
+    name: string,
+    wdl: string,
+    documentation: string,
+    synopsis: string,
+    snapshotComment: string,
+    options?: AbortOption
+  ) => Promise<MethodResponse>;
+}
+
+export const createMethodProvider: CreateMethodProvider = {
+  create: async (
+    namespace: string,
+    name: string,
+    wdl: string,
+    documentation: string,
+    synopsis: string,
+    snapshotComment: string,
+    options: AbortOption = {}
+  ): Promise<MethodResponse> => {
+    const { signal } = options;
+
+    const workflowPayload = {
+      namespace,
+      name,
+      synopsis,
+      snapshotComment,
+      documentation,
+      payload: wdl,
+      entityType: 'Workflow',
+    };
+
+    return await Methods(signal).postMethods(workflowPayload);
+  },
+};
