6b231fa7feed42582aaa52b397ab7f95f626393b
David An <davidan@broadinstitute.org>
2024-10-22 10:04:42 -0400

    tweak transformers for jest 28

--- packages/test-utils/transforms/cssTransform.cjs
+++ packages/test-utils/transforms/cssTransform.cjs
@@ -7,7 +7,9 @@
 
 module.exports = {
   process() {
-    return 'module.exports = {};';
+    return {
+      code: 'module.exports = {};'
+    };
   },
   getCacheKey() {
     // The output is always the same.
--- packages/test-utils/transforms/fileTransform.cjs
+++ packages/test-utils/transforms/fileTransform.cjs
@@ -19,7 +19,7 @@ module.exports = {
         pascalCase: true,
       });
       const componentName = `Svg${pascalCaseFilename}`;
-      return `const React = require('react');
+      return {code: `const React = require('react');
       module.exports = {
         __esModule: true,
         default: ${assetFilename},
@@ -34,9 +34,11 @@ module.exports = {
             })
           };
         }),
-      };`;
+      };`};
     }
 
-    return `module.exports = ${assetFilename};`;
+    return {
+      code: `module.exports = ${assetFilename};`
+    };
   },
 };
