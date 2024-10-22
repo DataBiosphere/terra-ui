05088c3060477cd055131bcff4d53900dbd9bc07
David An <davidan@broadinstitute.org>
2024-10-22 17:03:54 -0400

    code comment

--- integration-tests/jest-puppeteer.config.js
+++ integration-tests/jest-puppeteer.config.js
@@ -6,6 +6,10 @@ module.exports = {
     slowMo: process.env.SLOW === 'true' ? 250 : undefined,
     defaultViewport: { width: 1200, height: 800 },
     protocolTimeout: 720_000, // 12 minutes
+    // TODO: the below code explicitly allows third-party cookies in the Chrome used for integration tests.
+    //    This is necessary to allow the `run-rstudio` and `run-analysis` tests to pass, which require 3p cookies.
+    //    When Terra no longer requires third-party cookies, remove this exception.
+    //    For flag info, see https://developers.google.com/privacy-sandbox/cookies/prepare/test-for-breakage
     args: ['--flag-switches-begin', '--disable-features=TrackingProtection3pcd', '--flag-switches-end'],
   },
 };
