2080828ba417c716c05209181d369af802d2eb45
Kevin Marete <kevinmmarete@gmail.com>
2025-02-18 13:38:18 -0500

    Turn Dropdown into TextInput

--- src/workspaces/NewWorkspaceModal/NewWorkspaceModal.tsx
+++ src/workspaces/NewWorkspaceModal/NewWorkspaceModal.tsx
@@ -16,8 +16,8 @@ import {
   VirtualizedSelect,
 } from 'src/components/common';
 import { InfoBox } from 'src/components/InfoBox';
-import { TextArea, ValidatedInput } from 'src/components/input';
-import { allRegions, availableBucketRegions, isSupportedBucketLocation } from 'src/components/region-common';
+import { TextArea, TextInput, ValidatedInput } from 'src/components/input';
+import { isSupportedBucketLocation } from 'src/components/region-common';
 import { AzureStorage } from 'src/libs/ajax/AzureStorage';
 import { Billing } from 'src/libs/ajax/billing/Billing';
 import { resolveWdsApp } from 'src/libs/ajax/data-table-providers/WdsDataTableProvider';
@@ -126,7 +126,7 @@ export const NewWorkspaceModal = withDisplayName(
     const [sourceAzureWorkspaceRegion, setSourceAzureWorkspaceRegion] = useState<string>('');
     const [sourceGCPWorkspaceRegion, setSourceGcpWorkspaceRegion] = useState<string>(defaultLocation);
     const [sourceGCPWorkspaceRegionError, setSourceGCPWorkspaceRegionError] = useState(false);
-    const [isAlphaRegionalityUser, setIsAlphaRegionalityUser] = useState(false);
+    const [_isAlphaRegionalityUser, setIsAlphaRegionalityUser] = useState(false);
     const [phiTracking, setPhiTracking] = useState<boolean | undefined>(undefined);
     const signal = useCancellation();
 
@@ -583,12 +583,7 @@ export const NewWorkspaceModal = withDisplayName(
                             </Link>
                           </InfoBox>
                         </FormLabel>
-                        <Select<string>
-                          id={id}
-                          value={bucketLocation}
-                          onChange={(opt) => setBucketLocation(opt!.value)}
-                          options={isAlphaRegionalityUser ? allRegions : availableBucketRegions}
-                        />
+                        <TextInput id={id} defaultValue={bucketLocation} readOnly />
                       </>
                     )}
                   </IdContainer>
