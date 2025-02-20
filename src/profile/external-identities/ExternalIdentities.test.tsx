5c98023b61bea1275c7142349319550142324d78
Kevin Marete <kevinmmarete@gmail.com>
2025-02-20 10:59:11 -0500

    Use array for expected provider order test

--- src/profile/external-identities/ExternalIdentities.test.tsx
+++ src/profile/external-identities/ExternalIdentities.test.tsx
@@ -62,15 +62,17 @@ describe('ExternalIdentities', () => {
 
     // Assert
     const mainElement = screen.getByRole('main');
-    const providerElements = Array.from(mainElement.querySelectorAll('div')).map((div) => div.textContent);
-
-    expect(providerElements).toHaveLength(desiredOrder.length);
-    expect(providerElements[0]).toBe('NIH Researcher Auth Service (RAS)');
-    expect(providerElements[1]).toBe('NHLBI BioData Catalyst Framework Services');
-    expect(providerElements[2]).toBe('NCI CRDC Framework Services');
-    expect(providerElements[3]).toBe('Kids First DRC Framework Services');
-    expect(providerElements[4]).toBe('NHGRI AnVIL Data Commons Framework Services');
-    expect(providerElements[5]).toBe('eRA Commons');
-    expect(providerElements[6]).toBe('Nih Account');
+    const actualProviderElements = Array.from(mainElement.querySelectorAll('div')).map((div) => div.textContent);
+    const expectedProviderElements = [
+      'NIH Researcher Auth Service (RAS)',
+      'NHLBI BioData Catalyst Framework Services',
+      'NCI CRDC Framework Services',
+      'Kids First DRC Framework Services',
+      'NHGRI AnVIL Data Commons Framework Services',
+      'eRA Commons',
+      'Nih Account',
+    ];
+    expect(actualProviderElements).toHaveLength(desiredOrder.length);
+    expect(actualProviderElements).toStrictEqual(expectedProviderElements);
   });
 });
