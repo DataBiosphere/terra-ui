12290895b3881adb40877a6d9f6b5d9d42a3dac2
David An <davidan@broadinstitute.org>
2025-02-21 13:17:52 -0500

    move per-workflow cost threshold to workflows section

--- src/pages/workspaces/workspace/submissionHistory/SubmissionDetails.js
+++ src/pages/workspaces/workspace/submissionHistory/SubmissionDetails.js
@@ -3,7 +3,7 @@ import { delay } from '@terra-ui-packages/core-utils';
 import { differenceInDays, parseISO } from 'date-fns/fp';
 import _ from 'lodash/fp';
 import { Fragment, useEffect, useState } from 'react';
-import { div, h, h4 } from 'react-hyperscript-helpers';
+import { b, div, h, h4 } from 'react-hyperscript-helpers';
 import { AutoSizer } from 'react-virtualized';
 import { bucketBrowserUrl } from 'src/auth/auth';
 import * as breadcrumbs from 'src/components/breadcrumbs';
@@ -28,6 +28,8 @@ import colors from 'src/libs/colors';
 import { getConfig } from 'src/libs/config';
 import { withErrorReporting } from 'src/libs/error';
 import Events, { extractWorkspaceDetails } from 'src/libs/events';
+import { isFeaturePreviewEnabled } from 'src/libs/feature-previews';
+import { PREVIEW_COST_CAPPING } from 'src/libs/feature-previews-config';
 import * as Nav from 'src/libs/nav';
 import { forwardRefWithName, useCancellation } from 'src/libs/react-utils';
 import * as Style from 'src/libs/style';
@@ -77,7 +79,7 @@ const SubmissionWorkflowsTable = ({ workspace, submission }) => {
   const {
     workspace: { namespace, name },
   } = workspace;
-  const { submissionId, submissionRoot, workflows = [] } = submission;
+  const { perWorkflowCostCap, submissionId, submissionRoot, workflows = [] } = submission;
 
   const [statusFilter, setStatusFilter] = useState([]);
   const [textFilter, setTextFilter] = useState('');
@@ -116,6 +118,11 @@ const SubmissionWorkflowsTable = ({ workspace, submission }) => {
         }),
       ]),
       h(Link, { onClick: () => downloadWorkflows(filteredWorkflows, submissionId) }, ['Download TSV']),
+      isFeaturePreviewEnabled(PREVIEW_COST_CAPPING) &&
+        div({ style: { marginLeft: 'auto' } }, [
+          b({}, ['Per Workflow Cost Limit: ']),
+          perWorkflowCostCap ? Utils.formatUSD(perWorkflowCostCap) : 'N/A',
+        ]),
     ]),
     // 48px is based on the default row height of FlexTable
     div({ style: { flex: `1 0 ${(1 + _.min([filteredWorkflows.length, 5.5])) * tableRowHeight}px` } }, [
@@ -429,7 +436,6 @@ const SubmissionDetails = _.flow(
    */
   const {
     cost,
-    perWorkflowCostCap,
     methodConfigurationName: workflowName,
     methodConfigurationNamespace: workflowNamespace,
     submissionDate,
@@ -532,7 +538,7 @@ const SubmissionDetails = _.flow(
                 ),
               ]),
               makeSection('Submitted by', [div([submitter]), Utils.makeCompleteDate(submissionDate)]),
-              makeSection('Total Run Cost', [cost ? Utils.formatUSD(cost) : 'N/A']),
+              makeSection('Total Run Cost', [cost ? `${Utils.formatUSD(cost)} Actual cost` : 'N/A']),
               makeSection('Data Entity', [div([entityName]), div([entityType])]),
               makeSection('Submission ID', [
                 h(Link, { href: bucketBrowserUrl(submissionRoot.replace('gs://', '')), ...Utils.newTabLinkProps }, submissionId),
@@ -543,7 +549,6 @@ const SubmissionDetails = _.flow(
                   text: submissionRoot.split('/').pop(),
                 }),
               ]),
-              makeSection('Per Workflow Cost Limit', [perWorkflowCostCap ? Utils.formatUSD(perWorkflowCostCap) : 'N/A']),
               makeSection('Call Caching', [useCallCache ? 'Enabled' : 'Disabled']),
               makeSection('Delete Intermediate Outputs', [deleteIntermediateOutputFiles ? 'Enabled' : 'Disabled']),
               makeSection('Use Reference Disks', [useReferenceDisks ? 'Enabled' : 'Disabled']),
