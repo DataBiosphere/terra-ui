9b73a73659ee27a13767b6478b7bcb0813d38a9b
Miguel Silva <108079391+msilva-broad@users.noreply.github.com>
2025-02-21 08:52:46 -0500

    UIE-206 app startup - unit tests (#5254)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import 'src/outdated-browser-message';
+import { doAppBoot } from 'src/libs/startup/app-boot';
 
-import { loadedConfigStore } from 'src/configStore';
-
-const loadApp = async () => {
-  const [config, buildInfo] = await Promise.all([fetch('/config.json').then((r) => r.json()), fetch('/build-info.json').then((r) => r.json())]);
-  loadedConfigStore.current = { ...config, ...buildInfo };
-
-  import('src/appLoader');
-};
-
-const loadOauthRedirect = () => import('src/auth/oauth-redirect-loader');
-
-window.location.pathname.startsWith('/redirect-from-oauth') ? loadOauthRedirect() : loadApp();
-
-// This is needed for libraries that assume access to global, as vite does not define global.
-// react-collapse in particular requires this.
-window.global ||= window;
+doAppBoot();
