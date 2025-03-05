b403ddd2ef052fff90fdc086a3318fbd99245b6c
Kevin Marete <kevinmmarete@gmail.com>
2025-03-05 10:36:34 -0500

    removed files banner

--- src/workspace-data/Data.js
+++ src/workspace-data/Data.js
@@ -1194,14 +1194,6 @@ export const WorkspaceData = _.flow(
                           },
                           ['Workspace Data']
                         ),
-                        div(
-                          // File Browser Banner
-                          { style: { padding: '1rem', margin: '0.75rem', backgroundColor: colors.dark(0.1), borderRadius: '0.5rem' } },
-                          [
-                            span({ style: { fontWeight: 'bold' } }, ['Looking for the Files folder?']),
-                            div(['Use the folder icon in the right side-bar to access the workspace Bucket directory.']),
-                          ]
-                        ),
                       ]
                     ),
                 ]),
