2aaa5b728263421cd4ad6f876a2cffd53a1cdead
David An <davidan@broadinstitute.org>
2025-03-13 11:33:42 -0400

    FireCloud Data Library now powered by DUOS

--- src/pages/library/Datasets.js
+++ src/pages/library/Datasets.js
@@ -21,7 +21,6 @@ import targetLogo from 'src/images/library/datasets/target_logo.jpeg';
 import tcgaLogo from 'src/images/library/datasets/TCGALogo.jpg';
 import topMedLogo from 'src/images/library/datasets/TopMed@2x.png';
 import { Metrics } from 'src/libs/ajax/Metrics';
-import { getEnabledBrand } from 'src/libs/brand-utils';
 import colors from 'src/libs/colors';
 import { getConfig } from 'src/libs/config';
 import Events from 'src/libs/events';
@@ -400,7 +399,7 @@ const fcDataLib = () =>
         {
           'aria-label': 'Browse Broad Institute datasets',
           tooltip: 'Search for dataset workspaces',
-          href: `${getConfig().firecloudUrlRoot}/?return=${getEnabledBrand().queryName}#library`,
+          href: `${getConfig().duosUrlRoot}/datalibrary/firecloud`,
           onClick: () => captureBrowseDataEvent('Broad Institute Datasets'),
           ...Utils.newTabLinkProps,
         },
@@ -554,7 +553,7 @@ const target = () =>
         ButtonPrimary,
         {
           'aria-label': 'Browse TARGET data',
-          href: `${getConfig().firecloudUrlRoot}/?return=${getEnabledBrand().queryName}&project=TARGET#library`,
+          href: `${getConfig().duosUrlRoot}/datalibrary/firecloud`,
           onClick: () => captureBrowseDataEvent('TARGET'),
           ...Utils.newTabLinkProps,
         },
@@ -583,7 +582,7 @@ const tcga = () =>
         ButtonPrimary,
         {
           'aria-label': 'Browse Cancer Genome Atlas data',
-          href: `${getConfig().firecloudUrlRoot}/?return=${getEnabledBrand().queryName}&project=TCGA#library`,
+          href: `${getConfig().duosUrlRoot}/datalibrary/firecloud`,
           onClick: () => captureBrowseDataEvent('Cancer Genome Atlas'),
           ...Utils.newTabLinkProps,
         },
