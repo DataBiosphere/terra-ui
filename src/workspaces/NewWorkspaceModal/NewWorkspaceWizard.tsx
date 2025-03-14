1a5ff12088fbfb8ffd2ce6183b9f41d3784144b5
Kevin Marete <kevinmmarete@gmail.com>
2025-03-14 18:07:50 -0400

    Adjust modal width and height

--- src/workspaces/NewWorkspaceModal/NewWorkspaceWizard.tsx
+++ src/workspaces/NewWorkspaceModal/NewWorkspaceWizard.tsx
@@ -385,8 +385,8 @@ export const NewWorkspaceWizard = withDisplayName(
                 )}
               </ButtonPrimary>
             }
-            width={620}
-            styles={{ modal: { height: '72vh' } }}
+            width={720}
+            styles={{ modal: { height: 720 } }}
           >
             {creating ? (
               <CreatingWorkspaceMessage />
