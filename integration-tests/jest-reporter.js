ced34a88a45bb9679e3ad10aa888ee0ff2481a65
LizBaldo <baldo@broadinstitute.org>
2025-03-18 16:42:20 -0400

    more relax string matching for json response

--- integration-tests/jest-reporter.js
+++ integration-tests/jest-reporter.js
@@ -47,7 +47,10 @@ module.exports = class JestReporter {
       }
       // Only write failure details if they do not contain a JWT (even if it is an invalid one) as the logs are public
       // See https://broadworkbench.atlassian.net/browse/AN-464
-      if (!_.isEmpty(failureDetails) && !failureDetails.response.config.data.grant_type === 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
+      if (
+        !_.isEmpty(failureDetails) &&
+        !_.some(failureDetails, (value) => typeof value === 'string' && value.includes('urn:ietf:params:oauth:grant-type:jwt-bearer'))
+      ) {
         writableStream.write(`Failure details: ${JSON.stringify(failureDetails, null, 2)}\n`);
       }
       writableStream.write('\n');
