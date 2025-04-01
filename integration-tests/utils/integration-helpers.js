71f0055d3d596a458090877f289ed3a780b6641f
Bria Morgan <bmorgan@broadinstitute.org>
2025-04-01 13:04:17 -0400

    add logging

--- integration-tests/utils/integration-helpers.js
+++ integration-tests/utils/integration-helpers.js
@@ -344,6 +344,7 @@ const deleteRuntimesV2 = async ({ page, billingProject, workspaceId }) => {
   const deletableRuntimes = await page.evaluate(async (workspaceId) => {
     return await window.Ajax().Runtimes.listV2WithWorkspace(workspaceId, { role: 'creator' });
   }, workspaceId);
+  console.log(`deletableRuntimes for ${workspaceId}:`, deletableRuntimes);
   const deletedRuntimes = await Promise.all(
     deletableRuntimes.map(async (runtime) => {
       const isRuntimeDeleted = await patientlyDeleteRuntime(page, runtime);
