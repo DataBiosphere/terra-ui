fb7e913bb35147ee659980f22cf51381404b2667
Kevin Marete <kevinmmarete@gmail.com>
2025-04-07 19:36:20 -0400

    Change button label

--- src/workspace-data/data-table/entity-service/EntitiesContent.js
+++ src/workspace-data/data-table/entity-service/EntitiesContent.js
@@ -511,7 +511,7 @@ const EntitiesContent = ({
                     onClick: showColumnSettingsModal,
                     tooltip: 'Change the order and visibility of columns in the table',
                   },
-                  [icon('cog', { style: { marginRight: '0.5rem' } }), 'Settings']
+                  [icon('cog', { style: { marginRight: '0.5rem' } }), 'Select Columns']
                 ),
               div({ style: { margin: '0 1.5rem', height: '100%', borderLeft: Style.standardLine } }),
               div(
