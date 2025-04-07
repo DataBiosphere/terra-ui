cb61dbab12b7d846bc6212d9d83d3c441cae738a
Kevin Marete <kevinmmarete@gmail.com>
2025-04-07 16:25:46 -0400

    Load all bp workspaces spend data

--- src/billing/Workspaces/Workspaces.tsx
+++ src/billing/Workspaces/Workspaces.tsx
@@ -222,7 +222,7 @@ export const Workspaces = (props: WorkspacesProps): ReactNode => {
         // Create a map of spend data by workspace identifier
         const spendDataMap = _.keyBy(
           (spendItem: WorkspaceSpendData) => `${spendItem.workspace.namespace}-${spendItem.workspace.name}`,
-          (billingProjectSpentReport.spendDetails[0] as AggregatedWorkspaceSpendData).spendData
+          _.flatMap((detail: AggregatedWorkspaceSpendData) => detail.spendData, billingProjectSpentReport.spendDetails)
         );
 
         // Update each workspace with spend data or default values
