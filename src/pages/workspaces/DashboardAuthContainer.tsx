c3d50a77c42fcdfed17010df38808f8e4f4c5028
Kevin Marete <kevinmmarete@gmail.com>
2025-04-16 18:09:00 -0400

    Handled resolving workspaceId

--- src/pages/workspaces/DashboardAuthContainer.tsx
+++ src/pages/workspaces/DashboardAuthContainer.tsx
@@ -3,23 +3,43 @@ import _ from 'lodash/fp';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { centeredSpinner } from 'src/components/icons';
 import { FirecloudBucket } from 'src/libs/ajax/firecloud/FirecloudBucket';
-import { useStore } from 'src/libs/react-utils';
+import { WorkspaceWrapper } from 'src/libs/ajax/workspaces/workspace-models';
+import { Workspaces } from 'src/libs/ajax/workspaces/Workspaces';
+import { useCancellation, useStore } from 'src/libs/react-utils';
 import { authStore } from 'src/libs/state';
 import SignIn from 'src/pages/SignIn';
 import DashboardPublic from 'src/pages/workspaces/DashboardPublic';
 import { WorkspaceDashboardPage } from 'src/workspaces/dashboard/WorkspaceDashboardPage';
 
-export interface DashboardAuthContainerProps {
+export interface DashboardAuthContainerNameProps {
   namespace: string;
   name: string;
 }
 
+export interface DashboardAuthContainerIdProps {
+  id: string;
+}
+
+export type DashboardAuthContainerProps = DashboardAuthContainerNameProps | DashboardAuthContainerIdProps;
+
+const getWorkspaceNamespaceAndName = async (
+  props: DashboardAuthContainerProps,
+  signal: AbortSignal
+): Promise<DashboardAuthContainerNameProps> => {
+  const { id } = props as DashboardAuthContainerIdProps;
+  const workspace: WorkspaceWrapper = await Workspaces(signal).getById(id, []);
+  return { namespace: workspace.workspace.namespace, name: workspace.workspace.name };
+};
+
 export const DashboardAuthContainer = (props: DashboardAuthContainerProps): ReactNode => {
-  const { namespace, name } = props;
+  const [namespace, setNamespace] = useState<string>('');
+  const [name, setName] = useState<string>('');
   const { signInStatus } = useStore(authStore);
   const [featuredWorkspaces, setFeaturedWorkspaces] = useState<{ name: string; namespace: string }[]>();
 
   const isAuthInitialized = signInStatus !== 'uninitialized';
+
+  const signal = useCancellation();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +50,23 @@ export const DashboardAuthContainer = (props: DashboardAuthContainerProps): Reac
     }
   }, [signInStatus]);
 
+  useEffect(() => {
+    const fetchNamespaceAndName = async () => {
+      if (signInStatus === 'userLoaded') {
+        const { namespace, name } = await getWorkspaceNamespaceAndName(props, signal);
+        setNamespace(namespace);
+        setName(name);
+        document.title = `${name} - Dashboard`;
+      }
+    };
+    if ('namespace' in props && 'name' in props) {
+      setNamespace(props.namespace);
+      setName(props.name);
+    } else {
+      fetchNamespaceAndName();
+    }
+  }, [props, signal, signInStatus]);
+
   const isFeaturedWorkspace = () => _.some((ws) => ws.namespace === namespace && ws.name === name, featuredWorkspaces);
 
   return cond(
@@ -39,6 +76,6 @@ export const DashboardAuthContainer = (props: DashboardAuthContainerProps): Reac
     ],
     [signInStatus === 'signedOut' && isFeaturedWorkspace(), () => <DashboardPublic {...{ name, namespace }} />],
     [signInStatus === 'signedOut', () => <SignIn />],
-    [DEFAULT, () => <WorkspaceDashboardPage {...{ name, namespace }} />]
+    [DEFAULT, () => namespace && name && <WorkspaceDashboardPage {...{ name, namespace }} />]
   );
 };
