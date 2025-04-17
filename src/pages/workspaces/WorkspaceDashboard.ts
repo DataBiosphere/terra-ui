76beddde46462cd690994d94ad14cc26bd2cf523
Kevin Marete <49183352+kevinmarete@users.noreply.github.com>
2025-04-17 14:57:59 -0400

    [CORE-456] Update UI to Support UUID Workspace URL and Add Permalink Clipboard (#5312)

--- src/pages/workspaces/WorkspaceDashboard.ts
+++ src/pages/workspaces/WorkspaceDashboard.ts
@@ -1,11 +1,20 @@
 import { DashboardAuthContainer } from 'src/pages/workspaces/DashboardAuthContainer';
 
+const workspacePathConfig = {
+  name: 'workspace-dashboard',
+  component: DashboardAuthContainer,
+  public: true,
+};
+
 export const navPaths = [
   {
-    name: 'workspace-dashboard',
     path: '/workspaces/:namespace/:name',
-    component: DashboardAuthContainer,
     title: ({ name }) => `${name} - Dashboard`,
-    public: true,
+    ...workspacePathConfig,
+  },
+  {
+    path: '/workspaces/:id',
+    title: '',
+    ...workspacePathConfig,
   },
 ];
