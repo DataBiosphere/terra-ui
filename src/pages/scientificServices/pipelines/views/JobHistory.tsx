344ddb463cf5fe58dbda06dc36ea21a547223089
Matt Bemis <MatthewBemis@users.noreply.github.com>
2025-06-12 13:02:24 -0400

    [TSPS-520] Pre-select pipeline if there's only one + update job history table styling (#5340)

--- src/pages/scientificServices/pipelines/views/JobHistory.tsx
+++ src/pages/scientificServices/pipelines/views/JobHistory.tsx
@@ -56,7 +56,7 @@ export const JobHistory = () => {
           <div>
             For support, email{' '}
             <a
-              style={{ color: '#46A3E9', textDecoration: 'underline' }}
+              style={{ color: '#46A3E9', textDecoration: 'underline', fontWeight: 'bold' }}
               href='mailto:scientific-services-support@broadinstitute.org'
             >
               scientific-services-support@broadinstitute.org
@@ -81,6 +81,7 @@ export const JobHistory = () => {
                   noContentMessage={pipelineRunsResponse.totalResults > 0 ? ' ' : 'Nothing to display'}
                   tabIndex={-1}
                   variant={undefined}
+                  styleHeader={() => ({ backgroundColor: '#eff0f1' })}
                 />
               )}
             </AutoSizer>
