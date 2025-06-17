b91a2890a094dad1fe5508dcbaabd6d147ecd938
Saman Ehsan <sehsan@broadinstitute.org>
2025-06-17 15:02:02 -0700

    Fix tests

--- src/analysis/_testData/testData.ts
+++ src/analysis/_testData/testData.ts
@@ -805,7 +805,7 @@ export const generateTestDisk = (overrides: Partial<PersistentDisk> = {}): Persi
   },
   id: getRandomInt(randomMaxInt),
   workspaceId: null,
-  labels: { saturnApplication: 'galaxy' }, // Note 'galaxy' vs. 'GALAXY', to represent our older naming scheme
+  labels: { saturnApplication: 'galaxy', saturnWorkspaceName: 'test-workspace' }, // Note 'galaxy' vs. 'GALAXY', to represent our older naming scheme
   name: 'saturn-pd-026594ac-d829-423d-a8df-76fe96f5b4e7',
   size: 500,
   status: 'Ready',
@@ -831,8 +831,8 @@ export const galaxyDisk: PersistentDisk = {
     cloudResource: 'terra-test-e4000484',
   },
   id: 10,
-  workspaceId: null,
-  labels: { saturnApplication: 'galaxy' }, // Note 'galaxy' vs. 'GALAXY', to represent our older naming scheme
+  workspaceId: '9000df2c-4d3d-44e6-a459-5bf778acbeb1',
+  labels: { saturnApplication: 'galaxy', saturnWorkspaceName: 'test-workspace' }, // Note 'galaxy' vs. 'GALAXY', to represent our older naming scheme
   name: 'saturn-pd-026594ac-d829-423d-a8df-76fe96f5b4e7',
   size: 500,
   status: 'Ready',
--- src/workspaces/common/state/useCloudEnvironmentPolling.ts
+++ src/workspaces/common/state/useCloudEnvironmentPolling.ts
@@ -33,6 +33,8 @@ export const useCloudEnvironmentPolling = (
   const [persistentDisks, setPersistentDisks] = useState<PersistentDisk[]>();
   const [appDataDisks, setAppDataDisks] = useState<PersistentDisk[]>();
 
+  const saturnWorkspaceName = workspace?.workspace?.name;
+
   const reschedule = (ms) => {
     clearTimeout(timeout.current);
     timeout.current = setTimeout(refreshRuntimesSilently, ms);
@@ -41,6 +43,7 @@ export const useCloudEnvironmentPolling = (
     try {
       const cloudEnvFilters = _.pickBy((l) => !_.isUndefined(l), {
         role: 'creator',
+        saturnWorkspaceName,
       }) as Record<string, string>; // we literally just filtered out the undefined values, but ts doesn't know this
 
       // Disks.list API takes includeLabels to specify which labels to return in the response
@@ -49,7 +52,7 @@ export const useCloudEnvironmentPolling = (
         leoDiskProvider.list(
           {
             ...cloudEnvFilters,
-            includeLabels: 'saturnApplication',
+            includeLabels: 'saturnApplication,saturnWorkspaceName',
           },
           { signal: controller.current.signal }
         ),
