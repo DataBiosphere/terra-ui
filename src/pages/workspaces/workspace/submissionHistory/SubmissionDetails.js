465378975189132fd3e0dec76f65b3b3a3147259
Adam Nichols <aednichols@gmail.com>
2025-06-20 14:54:41 -0400

    Revert "Minor rename"
    This reverts commit 74fb09ea83b04811a1c955d9b97d2aba9260ed7b.

--- src/pages/workspaces/workspace/submissionHistory/SubmissionDetails.js
+++ src/pages/workspaces/workspace/submissionHistory/SubmissionDetails.js
@@ -43,7 +43,7 @@ const workflowStatuses = ['Queued', 'Launching', 'Submitted', 'Running', 'Aborti
 const deletionDelayDays = 190;
 const isDeleted = (statusLastChangedDate) => differenceInDays(parseISO(statusLastChangedDate), Date.now()) > deletionDelayDays;
 
-const deletedStyleIcon = ({ name, icon: iconName }) => {
+const deletedInfoIcon = ({ name, icon: iconName }) => {
   return h(
     InfoBox,
     {
@@ -221,8 +221,8 @@ export const SubmissionWorkflowsTable = ({ workspace, submission }) => {
                     h(Fragment, [
                       isDeleted(filteredWorkflows[rowIndex].statusLastChangedDate)
                         ? [
-                            deletedStyleIcon({ name: 'Job Manager', icon: 'tasks' }),
-                            deletedStyleIcon({ name: 'Workflow Dashboard', icon: 'tachometer' }),
+                            deletedInfoIcon({ name: 'Job Manager', icon: 'tasks' }),
+                            deletedInfoIcon({ name: 'Workflow Dashboard', icon: 'tachometer' }),
                           ]
                         : [
                             h(
