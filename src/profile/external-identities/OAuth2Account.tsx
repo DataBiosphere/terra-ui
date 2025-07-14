e3e0cb985214a4484db96b242f83d449b481071d
Kevin Marete <kevinmmarete@gmail.com>
2025-07-14 14:23:05 -0400

    Add check for displaying resources

--- src/profile/external-identities/OAuth2Account.tsx
+++ src/profile/external-identities/OAuth2Account.tsx
@@ -146,7 +146,7 @@ export const OAuth2Account = (props: OAuth2AccountProps) => {
           </>
         )}
       </div>
-      {isRASProvider && (
+      {externalUserId && isRASProvider && (
         <NihResources authorizedDatasets={authorizedDatasets} unauthorizedDatasets={unauthorizedDatasets} />
       )}
     </div>
