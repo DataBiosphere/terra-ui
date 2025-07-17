886db0afaf105b66bf8881132e9bec00480b4cf9
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2025-07-17 10:12:23 -0400

    formatting shifts

--- src/analysis/modals/GalaxyModal.js
+++ src/analysis/modals/GalaxyModal.js
@@ -289,11 +289,14 @@ export const GalaxyModalBase = withDisplayName('GalaxyModal')(
               div([
                 div({ style: { ...computeStyles.headerText, marginTop: '0.5rem' } }, ['Ongoing costs']),
                 div({ style: { lineHeight: 1.5 } }, [
-                  div(['Autopause is not supported. Delete the app once you have']),
-                  div(['completed your analysis to avoid unnecessary costs.']),
-                  h(Link, { href: 'https://support.terra.bio/hc/en-us/articles/360050566271', ...Utils.newTabLinkProps }, [
-                    'Pausing/Resuming a Galaxy instance.',
-                    icon('pop-out', { size: 12, style: { marginTop: '0.5rem', marginLeft: '0.25rem' } }),
+                  div([span({ style: { fontWeight: 600 } }, ['Autopause is not supported.']), ' Delete the app once you have']),
+                  div(['completed your analysis to avoid unnecessary costs. For more ']),
+                  div([
+                    'information, see ',
+                    h(Link, { href: 'https://support.terra.bio/hc/en-us/articles/360050566271', ...Utils.newTabLinkProps }, [
+                      'Galaxy in Terra',
+                      icon('pop-out', { size: 12, style: { marginTop: '0.5rem', marginLeft: '0.25rem' } }),
+                    ]),
                   ]),
                 ]),
               ]),
