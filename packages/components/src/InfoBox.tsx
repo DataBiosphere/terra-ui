ef277148cc3bcd34e50ac4ae6dc5223e7d37451f
David An <davidan@broadinstitute.org>
2025-07-24 09:55:39 -0400

    revert changes to InfoBox

--- packages/components/src/InfoBox.tsx
+++ packages/components/src/InfoBox.tsx
@@ -12,17 +12,16 @@ export interface InfoBoxProps {
   size?: number;
   style?: CSSProperties;
   tooltip?: ReactNode;
-  label?: string;
 }
 
 export const InfoBox = (props: InfoBoxProps): ReactNode => {
-  const { children, icon = 'info-circle', side, size, style, tooltip, label = 'More info' } = props;
+  const { children, icon = 'info-circle', side, size, style, tooltip } = props;
 
   const { colors } = useThemeFromContext();
 
   return (
     <PopupTrigger content={<div style={{ padding: '0.5rem', width: 300 }}>{children}</div>} side={side}>
-      <Clickable aria-label={label} tagName='span' tooltip={tooltip}>
+      <Clickable aria-label='More info' tagName='span' tooltip={tooltip}>
         <Icon icon={icon} size={size} style={{ color: colors.accent(), cursor: 'pointer', ...style }} />
       </Clickable>
     </PopupTrigger>
