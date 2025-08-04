89bc118bdbe30bd816f93540c16a1d01bd0e87d0
Matt Bemis <mbemis@broadinstitute.org>
2025-08-04 19:10:54 -0400

    update cli auth path

--- src/pages/scientificServices/NavPaths.tsx
+++ src/pages/scientificServices/NavPaths.tsx
@@ -2,8 +2,8 @@ import { CliAuth } from 'src/pages/scientificServices/cli-auth/CliAuth';
 
 export const navPaths = [
   {
-    name: 'pipelines-cli-auth',
-    path: '/services/pipelines/cli-auth',
+    name: 'cli-auth',
+    path: '/pipelines/cli-auth',
     component: CliAuth,
     title: 'Sign in to the terralab CLI',
     public: true,
