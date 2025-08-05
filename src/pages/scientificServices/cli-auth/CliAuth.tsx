fdc929fb9d77d5a948084e3ead9e097a27985ce5
Matt Bemis <mbemis@broadinstitute.org>
2025-08-05 19:10:11 -0400

    lint fix

--- src/pages/scientificServices/cli-auth/CliAuth.tsx
+++ src/pages/scientificServices/cli-auth/CliAuth.tsx
@@ -22,8 +22,8 @@ export const CliAuth = () => {
         this or another machine. If this is not the case, close this tab.
       </div>
       <div style={{ margin: '2rem 0' }}>
-        Enter the following verification code in the terralab CLI. This is a credential similar to your
-        password and <span style={{ fontWeight: 'bold' }}>should not be shared with others</span>.
+        Enter the following verification code in the terralab CLI. This is a credential similar to your password and{' '}
+        <span style={{ fontWeight: 'bold' }}>should not be shared with others</span>.
       </div>
       {authCode ? (
         <>
