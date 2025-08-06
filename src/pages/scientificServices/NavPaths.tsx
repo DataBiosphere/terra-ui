bb28b8543fa159e30369344cd4f7269d2bf8e404
Matt Bemis <MatthewBemis@users.noreply.github.com>
2025-08-06 15:16:44 -0400

    [TSPS-558] Add terralab CLI auth page (#5372)

---
+++ src/pages/scientificServices/NavPaths.tsx
@@ -0,0 +1,11 @@
+import { CliAuth } from 'src/pages/scientificServices/cli-auth/CliAuth';
+
+export const navPaths = [
+  {
+    name: 'cli-auth',
+    path: '/pipelines/cli-auth',
+    component: CliAuth,
+    title: 'Sign in to the terralab CLI',
+    public: true,
+  },
+];
