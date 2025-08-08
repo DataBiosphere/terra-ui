e1d77f2c6c8ab4afba15fd104080bb8af001dd3a
Matt Bemis <mbemis@broadinstitute.org>
2025-08-08 15:26:09 -0400

    private preview gate

--- src/pages/scientificServices/pipelines/common/scientific-services-common.tsx
+++ src/pages/scientificServices/pipelines/common/scientific-services-common.tsx
@@ -2,6 +2,8 @@ import _ from 'lodash/fp';
 import React from 'react';
 import { TabBar } from 'src/components/tabBars';
 import { TopBar } from 'src/components/TopBar';
+import { isFeaturePreviewEnabled } from 'src/libs/feature-previews';
+import { IMPUTATION_UI } from 'src/libs/feature-previews-config';
 import * as Nav from 'src/libs/nav';
 
 export const SCIENTIFIC_SERVICES_SUPPORT_EMAIL = 'scientific-services-support@broadinstitute.org';
@@ -13,20 +15,24 @@ const TAB_LINKS = {
 };
 
 export const pipelinesTopBar = (activeTab: string) => {
+  const isFeatureEnabled = !isFeaturePreviewEnabled(IMPUTATION_UI);
+
   return (
     <>
       <TopBar title='' href={Nav.getLink('root')} />
-      <TabBar
-        aria-label='pipelines menu'
-        activeTab={activeTab}
-        tabNames={_.keys(TAB_LINKS)}
-        getHref={(currentTab) => {
-          return Nav.getLink(TAB_LINKS[currentTab]);
-        }}
-      >
-        {/* TabBar doesn't need any children */}
-        {null}
-      </TabBar>
+      {isFeatureEnabled && (
+        <TabBar
+          aria-label='pipelines menu'
+          activeTab={activeTab}
+          tabNames={_.keys(TAB_LINKS)}
+          getHref={(currentTab) => {
+            return Nav.getLink(TAB_LINKS[currentTab]);
+          }}
+        >
+          {/* TabBar doesn't need any children */}
+          {null}
+        </TabBar>
+      )}
     </>
   );
 };
