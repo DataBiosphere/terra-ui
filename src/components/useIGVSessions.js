96eb77fd569bca3bed18c13b4418399d7f6673dc
Bria Morgan <bmorgan@broadinstitute.org>
2025-08-26 14:30:44 -0400

    refresh sessions

--- src/components/useIGVSessions.js
+++ src/components/useIGVSessions.js
@@ -16,6 +16,21 @@ export const useIGVSessions = (workspaceId) => {
     }
   };
 
+  const refreshSessions = () => {
+    if (!workspaceId) {
+      setSavedSessions([]);
+      return;
+    }
+
+    try {
+      const sessionListKey = `igv-session-list-${workspaceId}`;
+      const list = localStorage.getItem(sessionListKey);
+      setSavedSessions(list ? JSON.parse(list) : []);
+    } catch {
+      setSavedSessions([]);
+    }
+  };
+
   const loadSession = withErrorReporting('Unable to load session')(async (sessionName) => {
     try {
       const sessionKey = `igvSession-${workspaceId}-${sessionName}`;
@@ -112,5 +127,6 @@ export const useIGVSessions = (workspaceId) => {
     loadSession,
     saveSession,
     deleteSession,
+    refreshSessions,
   };
 };
