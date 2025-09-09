ef5fd6a8d87f570f7bf5819e37c60aba0e071921
Matt Bemis <mbemis@broadinstitute.org>
2025-09-09 16:27:14 -0400

    clean up query string

--- src/libs/ajax/teaspoons/Teaspoons.ts
+++ src/libs/ajax/teaspoons/Teaspoons.ts
@@ -1,5 +1,6 @@
 import { jsonBody } from '@terra-ui-packages/data-client-core';
 import _ from 'lodash/fp';
+import * as qs from 'qs';
 import { authOpts } from 'src/auth/auth-session';
 import { fetchTeaspoons } from 'src/libs/ajax/ajax-common';
 import {
@@ -41,9 +42,15 @@ export const Teaspoons = (signal?: AbortSignal) => ({
     sortProperty?: string,
     sortDirection?: string
   ): Promise<GetPipelineRunsResponse> => {
-    const queryString = `?limit=${pageSize}&pageNumber=${pageNumber}${
-      sortProperty ? `&sortProperty=${sortProperty}` : ''
-    }${sortDirection ? `&sortDirection=${sortDirection}` : ''}`;
+    const queryString = qs.stringify(
+      {
+        pageSize,
+        pageNumber,
+        ...(sortProperty ? { sortProperty } : {}),
+        ...(sortDirection ? { sortDirection } : {}),
+      },
+      { addQueryPrefix: true }
+    );
     const res = await fetchTeaspoons(`pipelineruns/v2/pipelineruns${queryString}`, _.merge(authOpts(), { signal }));
     return res.json();
   },
