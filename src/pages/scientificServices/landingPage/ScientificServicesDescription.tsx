cc32fba698091f420518295d9f58ba9be1816b18
Matt Bemis <mbemis@broadinstitute.org>
2025-09-11 14:07:00 -0400

    remove empty div

--- src/pages/scientificServices/landingPage/ScientificServicesDescription.tsx
+++ src/pages/scientificServices/landingPage/ScientificServicesDescription.tsx
@@ -17,7 +17,6 @@ export const ScientificServicesDescription = () => {
         Our scientific services provide the community with fast, scalable, and secure analysis capabilities that
         leverage valuable data resources.
       </div>
-      <div style={{ fontWeight: 'bold', marginTop: '2rem', marginBottom: '1rem' }} />
       <div style={{ fontWeight: 'bold', marginTop: '2rem' }}>Learn more about our current offerings:</div>
       <div
         style={{
