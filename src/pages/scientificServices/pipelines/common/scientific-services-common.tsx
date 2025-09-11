17a93d2e78777937a94d8753606b76f1690c206f
Matt Bemis <mbemis@broadinstitute.org>
2025-09-11 18:16:28 -0400

    make imputation ui generally available

--- src/pages/scientificServices/pipelines/common/scientific-services-common.tsx
+++ src/pages/scientificServices/pipelines/common/scientific-services-common.tsx
@@ -2,8 +2,6 @@ import _ from 'lodash/fp';
 import React from 'react';
 import { TabBar } from 'src/components/tabBars';
 import { TopBar } from 'src/components/TopBar';
-import { isFeaturePreviewEnabled } from 'src/libs/feature-previews';
-import { IMPUTATION_UI } from 'src/libs/feature-previews-config';
 import * as Nav from 'src/libs/nav';
 
 export const SCIENTIFIC_SERVICES_SUPPORT_EMAIL = 'scientific-services-support@broadinstitute.org';
@@ -15,40 +13,36 @@ const TAB_LINKS = {
 };
 
 export const pipelinesTopBar = (activeTab: string) => {
-  const isFeatureEnabled = isFeaturePreviewEnabled(IMPUTATION_UI);
-
   return (
     <>
       <TopBar title='' href={Nav.getLink('root')} />
-      {isFeatureEnabled && (
-        <TabBar
-          aria-label='pipelines menu'
-          styleOverrides={{
-            tab: {
-              backgroundColor: '#ffffff',
-            },
-            active: {
-              backgroundColor: '#e3f1fc',
-              borderBottom: '4px solid #46A3E9',
-            },
-            hover: {
-              backgroundColor: '#f5f5f5',
-            },
-            container: {
-              backgroundColor: '#ffffff',
-              borderBottom: '1px solid #46A3E9',
-            },
-          }}
-          activeTab={activeTab}
-          tabNames={_.keys(TAB_LINKS)}
-          getHref={(currentTab) => {
-            return Nav.getLink(TAB_LINKS[currentTab]);
-          }}
-        >
-          {/* TabBar doesn't need any children */}
-          {null}
-        </TabBar>
-      )}
+      <TabBar
+        aria-label='pipelines menu'
+        styleOverrides={{
+          tab: {
+            backgroundColor: '#ffffff',
+          },
+          active: {
+            backgroundColor: '#e3f1fc',
+            borderBottom: '4px solid #46A3E9',
+          },
+          hover: {
+            backgroundColor: '#f5f5f5',
+          },
+          container: {
+            backgroundColor: '#ffffff',
+            borderBottom: '1px solid #46A3E9',
+          },
+        }}
+        activeTab={activeTab}
+        tabNames={_.keys(TAB_LINKS)}
+        getHref={(currentTab) => {
+          return Nav.getLink(TAB_LINKS[currentTab]);
+        }}
+      >
+        {/* TabBar doesn't need any children */}
+        {null}
+      </TabBar>
     </>
   );
 };
--- src/pages/scientificServices/pipelines/views/JobHistory.tsx
+++ src/pages/scientificServices/pipelines/views/JobHistory.tsx
@@ -16,7 +16,6 @@ import {
   pipelinesTopBar,
   SCIENTIFIC_SERVICES_SUPPORT_EMAIL,
 } from 'src/pages/scientificServices/pipelines/common/scientific-services-common';
-import { ImputationPrivatePreviewGate } from 'src/pages/scientificServices/pipelines/components/ImputationPrivatePreviewGate';
 import { ViewErrorModal } from 'src/pages/scientificServices/pipelines/views/modals/ViewErrorModal';
 import { ViewOutputsModal } from 'src/pages/scientificServices/pipelines/views/modals/ViewOutputsModal';
 
@@ -66,78 +65,76 @@ export const JobHistory = () => {
   return (
     <FooterWrapper alwaysShow>
       {pipelinesTopBar('job history')}
-      <ImputationPrivatePreviewGate>
-        <main
-          style={{
-            paddingLeft: '2rem',
-            paddingRight: '2rem',
-            paddingTop: '1rem',
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            rowGap: '1rem',
-          }}
-        >
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
-            <h3>Job History</h3>
-            <div style={{ marginBottom: '0.25rem' }}>
-              All files associated with jobs will be automatically deleted after 2 weeks from completion.
-            </div>
-            <div>
-              For support, email{' '}
-              <a
-                style={{ color: '#46A3E9', textDecoration: 'underline', fontWeight: 'bold' }}
-                href={`mailto:${SCIENTIFIC_SERVICES_SUPPORT_EMAIL}`}
-              >
-                {SCIENTIFIC_SERVICES_SUPPORT_EMAIL}
-              </a>
-            </div>
+      <main
+        style={{
+          paddingLeft: '2rem',
+          paddingRight: '2rem',
+          paddingTop: '1rem',
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          rowGap: '1rem',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
+          <h3>Job History</h3>
+          <div style={{ marginBottom: '0.25rem' }}>
+            All files associated with jobs will be automatically deleted after 2 weeks from completion.
           </div>
-          <div style={{ flex: 1, marginTop: '1rem' }}>
-            {pipelineRunsResponse && !isLoading ? (
-              <AutoSizer>
-                {({ width, height }) => (
-                  <FlexTable
-                    aria-label='job history table'
-                    width={width}
-                    height={height}
-                    rowHeight={55}
-                    rowCount={pipelineRunsResponse.results.length}
-                    columns={getColumns(pipelineRunsResponse.results, sort, (sort) => {
-                      setSort(sort);
-                      setPageNumber(1);
-                    })}
-                    noContentMessage={pipelineRunsResponse.totalResults > 0 ? ' ' : 'Nothing to display'}
-                    tabIndex={-1}
-                    variant={undefined}
-                    styleHeader={() => ({ backgroundColor: '#eff0f1' })}
-                  />
-                )}
-              </AutoSizer>
-            ) : (
-              <Spinner />
-            )}
+          <div>
+            For support, email{' '}
+            <a
+              style={{ color: '#46A3E9', textDecoration: 'underline', fontWeight: 'bold' }}
+              href={`mailto:${SCIENTIFIC_SERVICES_SUPPORT_EMAIL}`}
+            >
+              {SCIENTIFIC_SERVICES_SUPPORT_EMAIL}
+            </a>
           </div>
-          {!_.isEmpty(pipelineRunsResponse?.results) && (
-            <div style={{ marginBottom: '0.5rem' }}>
-              {/* @ts-ignore */}
-              <Paginator
-                filteredDataLength={pipelineRunsResponse?.totalResults ?? 0}
-                unfilteredDataLength={pipelineRunsResponse?.totalResults ?? 0}
-                pageNumber={pageNumber}
-                setPageNumber={(v) => {
-                  setPageNumber(v);
-                }}
-                itemsPerPage={itemsPerPage}
-                setItemsPerPage={(v) => {
-                  setPageNumber(1);
-                  setItemsPerPage(v);
-                }}
-              />
-            </div>
+        </div>
+        <div style={{ flex: 1, marginTop: '1rem' }}>
+          {pipelineRunsResponse && !isLoading ? (
+            <AutoSizer>
+              {({ width, height }) => (
+                <FlexTable
+                  aria-label='job history table'
+                  width={width}
+                  height={height}
+                  rowHeight={55}
+                  rowCount={pipelineRunsResponse.results.length}
+                  columns={getColumns(pipelineRunsResponse.results, sort, (sort) => {
+                    setSort(sort);
+                    setPageNumber(1);
+                  })}
+                  noContentMessage={pipelineRunsResponse.totalResults > 0 ? ' ' : 'Nothing to display'}
+                  tabIndex={-1}
+                  variant={undefined}
+                  styleHeader={() => ({ backgroundColor: '#eff0f1' })}
+                />
+              )}
+            </AutoSizer>
+          ) : (
+            <Spinner />
           )}
-        </main>
-      </ImputationPrivatePreviewGate>
+        </div>
+        {!_.isEmpty(pipelineRunsResponse?.results) && (
+          <div style={{ marginBottom: '0.5rem' }}>
+            {/* @ts-ignore */}
+            <Paginator
+              filteredDataLength={pipelineRunsResponse?.totalResults ?? 0}
+              unfilteredDataLength={pipelineRunsResponse?.totalResults ?? 0}
+              pageNumber={pageNumber}
+              setPageNumber={(v) => {
+                setPageNumber(v);
+              }}
+              itemsPerPage={itemsPerPage}
+              setItemsPerPage={(v) => {
+                setPageNumber(1);
+                setItemsPerPage(v);
+              }}
+            />
+          </div>
+        )}
+      </main>
     </FooterWrapper>
   );
 };
