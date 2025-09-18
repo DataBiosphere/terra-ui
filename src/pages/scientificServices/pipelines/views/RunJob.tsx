3efc614b011f3a1e685aa5de2b9e568d0e4f6415
Matt Bemis <mbemis@broadinstitute.org>
2025-09-18 12:24:41 -0400

    add spinner to submitting button

--- src/pages/scientificServices/pipelines/views/RunJob.tsx
+++ src/pages/scientificServices/pipelines/views/RunJob.tsx
@@ -292,7 +292,14 @@ export const RunJob = () => {
                       style={{ margin: '1rem 0', padding: '1rem', fontSize: '1rem', width: 500 }}
                       onClick={handleSubmit}
                     >
-                      {isSubmitting ? 'Submitting...' : 'Submit'}
+                      {isSubmitting ? (
+                        <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem', justifyContent: 'center' }}>
+                          <Spinner />
+                          Submitting...
+                        </div>
+                      ) : (
+                        'Submit'
+                      )}
                     </ButtonPrimary>
                   </>
                 )}
