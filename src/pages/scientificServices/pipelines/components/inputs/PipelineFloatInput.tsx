fa2930854e9bff3a5ae74b2c3ff18336f5a97d6d
Morgan Taylor <marymorg@broadinstitute.org>
2025-10-09 14:56:36 -0400

    revert to old way

--- src/pages/scientificServices/pipelines/components/inputs/PipelineFloatInput.tsx
+++ src/pages/scientificServices/pipelines/components/inputs/PipelineFloatInput.tsx
@@ -25,27 +25,27 @@ export const PipelineFloatInput: React.FC<PipelineFloatInputProps> = ({
       <h3 style={{ marginBottom: '0.5rem' }}>
         {label || input.name} {input.isRequired ? <span style={{ color: '#DB3214' }}> *</span> : null}
       </h3>
-      <div style={{ width: 400 }}>
-        <ValidatedInput
-          value={value || ''}
-          placeholder={placeholder || ''}
-          aria-label={`${input.name} float input`}
-          onChange={(e) => {
-            // eslint-disable-next-line no-console
-            console.log(e);
+      <ValidatedInput
+        width={400}
+        error={validationError}
+        inputProps={{
+          'aria-label': `${input.name} float input`,
+          type: 'text',
+          value: value || '',
+          placeholder: placeholder || '',
+          onChange: (e) => {
             onChange(e);
             if (validationRegex) {
               const regex = new RegExp(validationRegex);
-              if (!regex.test(e) && e !== null) {
+              if (!regex.test(e.trim()) && e.length > 0) {
                 onValidation('Invalid float value');
               } else {
                 onValidation(undefined);
               }
             }
-          }}
-        />
-      </div>
-      {validationError && <div style={{ color: 'red', marginTop: '0.5rem' }}>{validationError}</div>}
+          },
+        }}
+      />
       {helpText && <div style={{ marginTop: '0.5rem', marginBottom: '2rem', fontStyle: 'italic' }}>{helpText}</div>}
     </>
   );
