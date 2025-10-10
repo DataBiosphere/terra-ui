c2bfc716fe9f35b41d1075b13bf7595c5bb78089
Matt Bemis <mbemis@broadinstitute.org>
2025-10-10 14:46:31 -0400

    remove more hardcoded values

--- src/pages/scientificServices/pipelines/views/JobHistory.tsx
+++ src/pages/scientificServices/pipelines/views/JobHistory.tsx
@@ -80,7 +80,8 @@ export const JobHistory = () => {
         <div style={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
           <h3>Job History</h3>
           <div style={{ marginBottom: '0.25rem' }}>
-            All files associated with jobs will be automatically deleted after 2 weeks from completion.
+            All files associated with jobs will be automatically deleted after {TEASPOONS_FILE_OUTPUT_TTL_DAYS / 7}{' '}
+            weeks from completion.
           </div>
           <div>
             For support, email{' '}
@@ -349,7 +350,9 @@ const ActionCell = ({ pipelineRun }: CellProps): ReactNode => {
           <TooltipTrigger
             content={
               jobOutputsDeleted
-                ? 'The outputs for this job have been deleted. Outputs are available for 2 weeks after job completion.'
+                ? `The outputs for this job have been deleted. Outputs are available for ${
+                    TEASPOONS_FILE_OUTPUT_TTL_DAYS / 7
+                  } weeks after job completion.`
                 : undefined
             }
             side='top'
