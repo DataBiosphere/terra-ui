2c5c1e1bd69e2f1ca8268e4d2ed6c926c0e48db6
Matt Bemis <mbemis@broadinstitute.org>
2025-10-22 21:37:33 -0400

    add text transform

--- src/pages/scientificServices/pipelines/widgets/PipelineOutputsWidget.tsx
+++ src/pages/scientificServices/pipelines/widgets/PipelineOutputsWidget.tsx
@@ -44,7 +44,9 @@ const OutputDetails = ({ output }: { output: PipelineOutput }) => {
   return (
     <div style={{ marginTop: '1rem', borderLeft: '3px solid #e4e5e6', paddingLeft: '0.5rem' }}>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ fontWeight: 500, paddingBottom: '0.5rem' }}>{displayName || name}</div>
+        <div style={{ fontWeight: 500, paddingBottom: '0.5rem', textTransform: 'capitalize' }}>
+          {displayName || name}
+        </div>
         <div
           style={{
             backgroundColor: '#e7f3fb',
