d0d9fca29495d8fbfaf4796b21bb40a53545afeb
Matt Bemis <MatthewBemis@users.noreply.github.com>
2025-10-24 14:59:20 -0400

    [TSPS-662] Show hamburger menu for logged out views in Teaspoons brand (#5443)

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -21,7 +21,7 @@ const styles = {
 };
 
 const SignIn = () => {
-  return h(HeroWrapper, { showMenu: false, showDocLink: true }, [
+  return h(HeroWrapper, { showMenu: true, showDocLink: true }, [
     div({ style: { maxWidth: 600 } }, [
       div({ style: { fontSize: 16, lineHeight: 1.5, marginBottom: '2rem' } }, [
         'If you are a new user or returning user, click sign in to continue.',
