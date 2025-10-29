c2e367e082f5d7522da923c1d7bbfa1c3d665394
Saman Ehsan <sehsan@manifold.ai>
2025-10-29 15:48:35 -0400

    Delete compact data tables setting and feature flag

--- src/libs/ajax/workspaces/workspace-models.ts
+++ src/libs/ajax/workspaces/workspace-models.ts
@@ -37,7 +37,6 @@ export type WorkspaceSetting =
   | SoftDeleteSetting
   | RequesterPaysSetting
   | SeparateSubmissionFinalOutputsSetting
-  | ImprovedDataTablesSetting
   | WorkspaceAnalysisLogRetentionSetting;
 
 export interface BucketLifecycleSetting {
@@ -52,11 +51,6 @@ export interface SoftDeleteSetting {
 
 export interface RequesterPaysSetting {
   settingType: 'GcpBucketRequesterPays';
-  config: { enabled: boolean };
-}
-
-export interface ImprovedDataTablesSetting {
-  settingType: 'CompactDataTables';
   config: { enabled: boolean };
 }
 
