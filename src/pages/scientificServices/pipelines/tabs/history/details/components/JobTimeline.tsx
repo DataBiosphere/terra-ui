e47904afc52f6b7806a3f052d2219fe13549c476
Matt Bemis <mbemis@broadinstitute.org>
2025-11-28 21:11:37 -0500

    style

--- src/pages/scientificServices/pipelines/tabs/history/details/components/JobTimeline.tsx
+++ src/pages/scientificServices/pipelines/tabs/history/details/components/JobTimeline.tsx
@@ -18,50 +18,65 @@ const MOCK_TIMELINE_EVENTS = [
 export const JobTimeline = ({ pipelineRunResult }: JobTimelineProps) => {
   return (
     <PipelineWidgetContainer title='Timeline' border='1px solid #d7d9dc'>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         {MOCK_TIMELINE_EVENTS.map((event, index) => (
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              padding: '0.75rem',
-              borderLeft: '3px solid #5CC88D',
-              backgroundColor: 'white',
-              borderBottomRightRadius: '4px',
-              borderTopRightRadius: '4px',
-              minHeight: '4rem',
-            }}
-          >
-            <div style={{ flex: 1 }}>
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  width: '100%',
-                }}
-              >
-                <div style={{ fontWeight: 'bold' }}>{event.event}</div>
-                <div>{new Date(event.timestamp).toLocaleString()}</div>
-              </div>
-
-              {/* Conditional QC message */}
-              {event.event === 'Passed QC' && (
+          <div key={index} style={{ position: 'relative' }}>
+            {/* Timeline event */}
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '0.75rem',
+                backgroundColor: 'white',
+                borderRadius: '4px',
+                minHeight: '4rem',
+              }}
+            >
+              <div style={{ flex: 1 }}>
                 <div
                   style={{
-                    marginTop: '0.25rem',
-                    fontSize: 14,
-                    color: '#46A3E9',
-                    fontStyle: 'italic',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '100%',
                   }}
                 >
-                  <a target='_blank' href='https://app.terra.bio' rel='noreferrer'>
-                    Learn more about the QC process
-                    <Icon icon='pop-out' size={12} style={{ marginLeft: '0.25rem' }} />
-                  </a>
+                  <div style={{ fontWeight: 'bold' }}>{event.event}</div>
+                  <div>{new Date(event.timestamp).toLocaleString()}</div>
                 </div>
-              )}
+
+                {/* Conditional QC message */}
+                {event.event === 'Passed QC' && (
+                  <div
+                    style={{
+                      marginTop: '0.25rem',
+                      fontSize: 14,
+                      color: '#46A3E9',
+                      fontStyle: 'italic',
+                    }}
+                  >
+                    <a target='_blank' href='https://app.terra.bio' rel='noreferrer'>
+                      Learn more about the QC process
+                      <Icon icon='pop-out' size={12} style={{ marginLeft: '0.25rem' }} />
+                    </a>
+                  </div>
+                )}
+              </div>
             </div>
+
+            {/* Connecting line to next event */}
+            {index < MOCK_TIMELINE_EVENTS.length - 1 && (
+              <div
+                style={{
+                  width: '3px',
+                  height: '2rem',
+                  backgroundColor: colors.light(0.2),
+                  marginLeft: 'calc(50% - 1px)',
+                  position: 'relative',
+                  zIndex: 0,
+                }}
+              />
+            )}
           </div>
         ))}
       </div>
