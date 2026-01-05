8fe3fef7d3276b89101c4858bfe2da7d7a4d4f59
Matt Bemis <mbemis@broadinstitute.org>
2026-01-05 14:51:33 -0500

    job inputs view tests

---
+++ src/pages/scientificServices/pipelines/tabs/history/details/sections/io/JobInputsView.test.tsx
@@ -0,0 +1,82 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { PipelineInput } from 'src/libs/ajax/teaspoons/teaspoons-models';
+import { mockPipelineWithDetails } from 'src/pages/scientificServices/pipelines/utils/mock-utils';
+import { renderWithAppContexts as render } from 'src/testing/test-utils';
+
+import { JobInputsView } from './JobInputsView';
+
+describe('JobInputsView', () => {
+  const mockInputDefinitions: PipelineInput[] = mockPipelineWithDetails('array_imputation').inputs;
+
+  const mockInputs = {
+    minDr2ForInclusion: 0.8,
+    allowChunkFailures: 'true',
+    multiSampleVcf: 'gs://bucket/path/to/file.vcf',
+    outputBasename: 'outputs',
+  };
+
+  it('renders all input items when inputs are provided', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={mockInputs} />);
+
+    expect(screen.getByText('minimum imputation quality for inclusion')).toBeInTheDocument();
+    expect(screen.getByText('Allow chunk failures')).toBeInTheDocument();
+    expect(screen.getByText('multi-sample VCF file')).toBeInTheDocument();
+    expect(screen.getByText('output basename')).toBeInTheDocument();
+  });
+
+  it('renders input values', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={mockInputs} />);
+
+    expect(screen.getByText('gs://bucket/path/to/file.vcf')).toBeInTheDocument();
+    expect(screen.getByText('0.8')).toBeInTheDocument();
+    expect(screen.getByText('true')).toBeInTheDocument();
+    expect(screen.getByText('outputs')).toBeInTheDocument();
+  });
+
+  it('renders input type badges', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={mockInputs} />);
+
+    expect(screen.getByText('file')).toBeInTheDocument();
+    expect(screen.getByText('float')).toBeInTheDocument();
+    expect(screen.getByText('boolean')).toBeInTheDocument();
+    expect(screen.getByText('string')).toBeInTheDocument();
+  });
+
+  it('uses display name when available', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={mockInputs} />);
+
+    expect(screen.getByText('output basename')).toBeInTheDocument();
+    expect(screen.queryByText('outputBasename')).not.toBeInTheDocument();
+  });
+
+  it('falls back to input key when display name is not available', () => {
+    const inputsWithUnknownKey = {
+      unknownInput: 'some value',
+    };
+
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={inputsWithUnknownKey} />);
+
+    expect(screen.getByText('unknownInput')).toBeInTheDocument();
+  });
+
+  it('renders "There are no inputs to display" when inputs object is empty', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={{}} />);
+
+    expect(screen.getByText('There are no inputs to display')).toBeInTheDocument();
+  });
+
+  it('renders "There are no inputs to display" when inputs is undefined', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={undefined as any} />);
+
+    expect(screen.getByText('There are no inputs to display')).toBeInTheDocument();
+  });
+
+  it('does not render input items when inputs object is empty', () => {
+    render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={{}} />);
+
+    expect(screen.queryByText('Input File')).not.toBeInTheDocument();
+    expect(screen.queryByText('Threshold')).not.toBeInTheDocument();
+    expect(screen.queryByText('Enable Feature')).not.toBeInTheDocument();
+  });
+});
