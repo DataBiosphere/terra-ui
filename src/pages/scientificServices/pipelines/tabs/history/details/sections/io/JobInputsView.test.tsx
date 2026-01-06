a40e93432c6fb2bcb67a8d0b2b09bb3ada7954a9
Matt Bemis <MatthewBemis@users.noreply.github.com>
2026-01-06 14:22:39 -0500

    Update JobInputsView.test.tsx

--- src/pages/scientificServices/pipelines/tabs/history/details/sections/io/JobInputsView.test.tsx
+++ src/pages/scientificServices/pipelines/tabs/history/details/sections/io/JobInputsView.test.tsx
@@ -75,8 +75,9 @@ describe('JobInputsView', () => {
   it('does not render input items when inputs object is empty', () => {
     render(<JobInputsView inputDefinitions={mockInputDefinitions} inputs={{}} />);
 
-    expect(screen.queryByText('Input File')).not.toBeInTheDocument();
-    expect(screen.queryByText('Threshold')).not.toBeInTheDocument();
-    expect(screen.queryByText('Enable Feature')).not.toBeInTheDocument();
+    expect(screen.getByText('minimum imputation quality for inclusion')).not.toBeInTheDocument();
+    expect(screen.getByText('Allow chunk failures')).not.toBeInTheDocument();
+    expect(screen.getByText('multi-sample VCF file')).not.toBeInTheDocument();
+    expect(screen.getByText('output basename')).not.toBeInTheDocument();
   });
 });
