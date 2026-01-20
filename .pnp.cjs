e9ab05972b91fc2421dc9269e87a67e9d0d31deb
Janet Dewar <jdewar@broadinstitute.org>
2026-01-20 15:20:33 -0500

    Try this

--- .pnp.cjs
+++ .pnp.cjs
@@ -171,6 +171,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:2.8.8"],\
             ["prismjs", "npm:1.30.0"],\
             ["prop-types", "npm:15.8.1"],\
+            ["qs", "npm:6.14.1"],\
             ["rc-slider", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:10.2.1"],\
             ["react", "npm:18.2.0"],\
             ["react-collapse", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.1.0"],\
@@ -22106,6 +22107,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:2.8.8"],\
             ["prismjs", "npm:1.30.0"],\
             ["prop-types", "npm:15.8.1"],\
+            ["qs", "npm:6.14.1"],\
             ["rc-slider", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:10.2.1"],\
             ["react", "npm:18.2.0"],\
             ["react-collapse", "virtual:3bc50e11628962e2d3d040387b897fa78149010dc0c7837774133f031c81b063c69d97afa708f6f7b77daf0a0d6419898bc26265121b2bec06dfc7ebf0feed1d#npm:5.1.0"],\
