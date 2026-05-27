42f3d2f377bdce6ecf4e1226f4afcc2e6e85db30
Bria Morgan <bmorgan@manifold.ai>
2026-05-27 09:41:05 -0400

    remove nih alert

--- src/libs/link-expiration-alerts.test.ts
+++ src/libs/link-expiration-alerts.test.ts
@@ -1,4 +1,4 @@
-import { addDays, addHours, setMilliseconds } from 'date-fns/fp';
+import { addDays } from 'date-fns/fp';
 import _ from 'lodash/fp';
 import { ReactElement } from 'react';
 import { AppConfigSettings, getConfig } from 'src/libs/config';
@@ -42,63 +42,6 @@ describe('getLinkExpirationAlerts', () => {
 
   afterAll(() => {
     jest.useRealTimers();
-  });
-
-  describe('NIH link', () => {
-    it('includes alert if NIH link has expired', () => {
-      const expirationDate = setMilliseconds(0, addDays(-1, new Date()));
-      const alerts = getLinkExpirationAlerts({
-        nihStatus: {
-          linkedNihUsername: 'user@example.com',
-          linkExpireTime: expirationDate.getTime() / 1000,
-          datasetPermissions: [],
-        },
-      } as unknown as AuthState);
-
-      expect(alerts).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            id: 'nih-link-expiration',
-            title: 'Your access to NIH Controlled Access workspaces and data has expired.',
-          }),
-        ])
-      );
-    });
-
-    it('includes alert if link will expire within the next 24 hours', () => {
-      const expirationDate = setMilliseconds(0, addHours(6, new Date()));
-
-      const alerts = getLinkExpirationAlerts({
-        nihStatus: {
-          linkedNihUsername: 'user@example.com',
-          linkExpireTime: expirationDate.getTime() / 1000,
-          datasetPermissions: [],
-        },
-      } as unknown as AuthState);
-
-      expect(alerts).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            id: 'nih-link-expiration',
-            title: 'Your access to NIH Controlled Access workspaces and data will expire soon.',
-          }),
-        ])
-      );
-    });
-
-    it('does not show notification if link will not expire within the next 24 hours', () => {
-      const expirationDate = setMilliseconds(0, addDays(7, new Date()));
-
-      const alerts = getLinkExpirationAlerts({
-        nihStatus: {
-          linkedNihUsername: 'user@example.com',
-          linkExpireTime: expirationDate.getTime() / 1000,
-          datasetPermissions: [],
-        },
-      } as unknown as AuthState);
-
-      expect(alerts).toEqual([]);
-    });
   });
 
   describe('fence links', () => {
