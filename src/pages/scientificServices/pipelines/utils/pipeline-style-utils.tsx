776111fb249072475299f51094805f09f60ad9af
Matt Bemis <mbemis@broadinstitute.org>
2026-06-09 11:38:42 -0400

    add new pipeline colors

--- src/pages/scientificServices/pipelines/utils/pipeline-style-utils.tsx
+++ src/pages/scientificServices/pipelines/utils/pipeline-style-utils.tsx
@@ -35,7 +35,9 @@ export const getPipelineColor = (pipelineRun: PipelineRun): string => {
   switch (pipelineRun.pipelineName) {
     case 'array_imputation':
       return '#4D72AA4D';
-    default:
+    case 'low_pass_imputation':
       return '#AA4D8B4D';
+    default:
+      return '#8FBC8A4D';
   }
 };
