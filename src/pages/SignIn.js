90f83f8bce7e227b7bf204fecc2ed0ee0465e35e
Matt Bemis <MatthewBemis@users.noreply.github.com>
2026-08-10 14:50:48 -0400

    Update src/pages/SignIn.js
    Co-authored-by: M. Morgan Aster <marymorg@broadinstitute.org>

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -24,7 +24,7 @@ const SignIn = () => {
   return h(HeroWrapper, { showMenu: true, showDocLink: true }, [
     div({ style: { maxWidth: 600 } }, [
       div({ style: { fontSize: 16, lineHeight: 1.5, marginBottom: '2rem', fontWeight: isScientificServices() ? 'bold' : 'normal' } }, [
-        'If you are a new user or returning user, click sign in to continue.',
+        'Click sign in to continue. If you are a new user, you will be prompted to create an account.',
       ]),
       h(SignInButton),
       !isAnvil() &&
