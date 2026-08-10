0bc447b4136b6bc41f41e456547113ae9407d8f8
Matt Bemis <mbemis@broadinstitute.org>
2026-08-10 15:05:09 -0400

    tweak

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -23,8 +23,13 @@ const styles = {
 const SignIn = () => {
   return h(HeroWrapper, { showMenu: true, showDocLink: true }, [
     div({ style: { maxWidth: 600 } }, [
-      div({ style: { fontSize: 16, lineHeight: 1.5, marginBottom: '2rem', fontWeight: isScientificServices() ? 'bold' : 'normal' } }, [
-        'Click sign in to continue. If you are a new user, you will be prompted to create an account.',
+      div({ style: { fontSize: 16, lineHeight: 1.5, marginBottom: '2rem' } }, [
+        isScientificServices()
+          ? h(Fragment, [
+              span({ style: { fontWeight: 'bold' } }, ['Click sign in to continue.']),
+              ' If you are a new user, you will be prompted to create an account.',
+            ])
+          : 'If you are a new user or returning user, click sign in to continue.',
       ]),
       h(SignInButton),
       !isAnvil() &&
