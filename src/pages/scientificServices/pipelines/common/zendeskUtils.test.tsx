eec83219706dd0f47309930e78356a8e59202f32
Saloni Shah <salonipshah11@gmail.com>
2026-09-04 17:08:33 -0400

    tests

--- src/pages/scientificServices/pipelines/common/zendeskUtils.test.tsx
+++ src/pages/scientificServices/pipelines/common/zendeskUtils.test.tsx
@@ -10,6 +10,24 @@ describe('zendeskUtils', () => {
       );
     });
 
+    it('returns the correct URL for CLOUD_INPUTS', () => {
+      expect(zendeskUrl(DocsKey.CLOUD_INPUTS)).toBe(
+        'https://broadscientificservices.zendesk.com/hc/en-us/articles/47099858889243'
+      );
+    });
+
+    it('returns the correct URL for CLOUD_OUTPUTS', () => {
+      expect(zendeskUrl(DocsKey.CLOUD_OUTPUTS)).toBe(
+        'https://broadscientificservices.zendesk.com/hc/en-us/articles/48878810499483'
+      );
+    });
+
+    it('returns the correct URL for LOW_PASS_IMPUTATION_INPUT_REQ', () => {
+      expect(zendeskUrl(DocsKey.LOW_PASS_IMPUTATION_INPUT_REQ)).toBe(
+        'https://broadscientificservices.zendesk.com/hc/en-us/articles/50837430347675-Input-Requirements'
+      );
+    });
+
     it('returns the default URL for an unknown DocsKey', () => {
       // @ts-expect-error Testing invalid key
       expect(zendeskUrl('UNKNOWN_KEY')).toBe(
@@ -33,6 +51,29 @@ describe('zendeskUtils', () => {
         'https://broadscientificservices.zendesk.com/hc/en-us/articles/39903092619035'
       );
       expect(linkElement).toHaveStyle('font-weight: italics');
+    });
+
+    it('renders correctly for the CLOUD_INPUTS docs key', () => {
+      const { getByText } = render(
+        <ZendeskLink docsKey={DocsKey.CLOUD_INPUTS}>provide inputs from the cloud</ZendeskLink>
+      );
+
+      const linkElement = getByText('provide inputs from the cloud');
+      expect(linkElement).toBeInTheDocument();
+      expect(linkElement).toHaveAttribute(
+        'href',
+        'https://broadscientificservices.zendesk.com/hc/en-us/articles/47099858889243'
+      );
+    });
+
+    it('opens in a new tab and has the expected default style when no additionalStyle is provided', () => {
+      const { getByText } = render(<ZendeskLink docsKey={DocsKey.CLOUD_OUTPUTS}>cloud outputs link</ZendeskLink>);
+
+      const linkElement = getByText('cloud outputs link');
+      expect(linkElement).toHaveAttribute('target', '_blank');
+      expect(linkElement).toHaveAttribute('rel', 'noreferrer');
+      expect(linkElement).toHaveStyle('color: #46A3E9');
+      expect(linkElement).toHaveStyle('text-decoration: underline');
     });
   });
 });
