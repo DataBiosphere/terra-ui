6192cb6a4b55ec01139b608a9a9dedf366754ec5
Morgan Taylor <marymorg@broadinstitute.org>
2026-09-17 11:02:22 -0400

    fix wording, update broken links

--- src/pages/scientificServices/pipelines/common/zendeskUtils.tsx
+++ src/pages/scientificServices/pipelines/common/zendeskUtils.tsx
@@ -14,8 +14,8 @@ export enum DocsKey {
 
 // Mapping of documentation keys to their respective Zendesk URLs
 const ZENDESK_PAGES: Record<DocsKey, string> = {
-  GETTING_STARTED: 'https://broadscientificservices.zendesk.com/hc/en-us/sections/39901025462171',
-  ABOUT_SERVICE: 'https://broadscientificservices.zendesk.com/hc/en-us/categories/39900993442459',
+  GETTING_STARTED: 'https://broadscientificservices.zendesk.com/hc/en-us/categories/55722344621979',
+  ABOUT_SERVICE: 'https://broadscientificservices.zendesk.com/',
   ARRAY_IMPUTATION_INPUT_REQ: 'https://broadscientificservices.zendesk.com/hc/en-us/articles/40161675448859',
   LOW_PASS_IMPUTATION_INPUT_REQ: 'https://broadscientificservices.zendesk.com/hc/en-us/articles/50837430347675',
   SV_IMPUTATION_INPUT_REQ: 'https://broadscientificservices.zendesk.com/hc/en-us/articles/55678264693915',
